6800bdf717d6c4c6c8ff95196e5f259f6d9ba024
dest <dest.dn@gmail.com>
2016-01-06 23:29:13 +0100

    Update c700908068.lua
    Add missing checks/categorys/flags + general fixes

--- scripts/c700908068.lua
+++ scripts/c700908068.lua
@@ -8,7 +8,7 @@ function c700908068.initial_effect(c)
 	--atkup
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_FIELD)
-	e2:SetRange(LOCATION_SZONE)
+	e2:SetRange(LOCATION_FZONE)
 	e2:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
 	e2:SetCode(EFFECT_UPDATE_ATTACK)
 	e2:SetTarget(aux.TargetBoolFunction(Card.IsSetCard,0xab))
@@ -20,7 +20,9 @@ function c700908068.initial_effect(c)
 	--reveal
 	local e4=Effect.CreateEffect(c)
 	e4:SetDescription(aux.Stringid(c70090868,0))
+	e4:SetCategory(CATEGORY_TODECK+CATEGORY_DRAW)
 	e4:SetType(EFFECT_TYPE_IGNITION)
+	e4:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
 	e4:SetCountLimit(1)
 	e4:SetRange(LOCATION_FZONE)
 	e4:SetTarget(c70090868.target)
@@ -29,6 +31,7 @@ function c700908068.initial_effect(c)
 	--todeck
 	local e5=Effect.CreateEffect(c)
 	e5:SetDescription(aux.Stringid(c70090868,1))
+	e5:SetCategory(CATEGORY_TODECK)
 	e5:SetType(EFFECT_TYPE_IGNITION)
 	e5:SetRange(LOCATION_FZONE)
 	e5:SetCost(c70090868.cost2)
@@ -37,26 +40,30 @@ function c700908068.initial_effect(c)
 	c:RegisterEffect(e5)
 end
 function c70090868.filter(c)
-	return c:IsSetCard(0xab) and not c:IsPublic()
+	return c:IsSetCard(0xab) and c:IsAbleToDeck() and not c:IsPublic()
 end
 function c70090868.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(c70090868.filter,tp,LOCATION_HAND,0,1,nil) end
+	if chk==0 then return Duel.IsPlayerCanDraw(tp)
+		and Duel.IsExistingMatchingCard(c70090868.filter,tp,LOCATION_HAND,0,1,nil) end
+	Duel.SetTargetPlayer(tp)
+	Duel.SetOperationInfo(0,CATEGORY_TODECK,nil,1,tp,LOCATION_HAND)
 end
 function c70090868.operation(e,tp,eg,ep,ev,re,r,rp)
 	if not e:GetHandler():IsRelateToEffect(e) then return end
-	local g=Duel.GetMatchingGroup(Card.IsFaceup,tp,LOCATION_MZONE,LOCATION_MZONE,nil)
-	if g:GetCount()==0 then return end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CONFIRM)
-	local cg=Duel.SelectMatchingCard(tp,c70090868.filter,tp,LOCATION_HAND,0,1,99,nil)
-	Duel.ConfirmCards(1-tp,cg)
-	Duel.ShuffleHand(tp)
-	Duel.SendtoDeck(cg,nil,2,REASON_EFFECT)
-	Duel.ShuffleDeck(tp)
-	local ct=cg:GetCount()
-	Duel.Draw(tp,ct,REASON_EFFECT)
+	local p=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER)
+	Duel.Hint(HINT_SELECTMSG,p,HINTMSG_TODECK)
+	local g=Duel.SelectMatchingCard(tp,c70090868.filter,tp,LOCATION_HAND,0,1,99,nil)
+	if g:GetCount()>0 then
+		Duel.ConfirmCards(1-p,g)
+		local ct=Duel.SendtoDeck(g,nil,2,REASON_EFFECT)
+		Duel.ShuffleDeck(p)
+		Duel.BreakEffect()
+		Duel.Draw(p,ct,REASON_EFFECT)
+		Duel.ShuffleHand(p)
+	end
 end
 function c70090868.cfilter(c)
-	return c:IsSetCard(0xab) and c:IsAbleToRemoveAsCost() and not c:IsCode(72090869)
+	return c:IsSetCard(0xab) and c:IsAbleToRemoveAsCost() and not c:IsCode(70090868)
 end
 function c70090868.cost2(e,tp,eg,ep,ev,re,r,rp,chk)
 	local g=Duel.GetMatchingGroup(c70090868.cfilter,tp,LOCATION_GRAVE+LOCATION_ONFIELD,0,nil)
@@ -73,12 +80,15 @@ function c70090868.cost2(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.Remove(rg,POS_FACEUP,REASON_COST)
 end
 function c70090868.target2(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return true end
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsAbleToDeck,tp,0,LOCATION_ONFIELD+LOCATION_GRAVE+LOCATION_HAND,1,nil) end
 	local g=Duel.GetMatchingGroup(Card.IsAbleToDeck,tp,0,LOCATION_ONFIELD+LOCATION_GRAVE+LOCATION_HAND,nil)
-	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,g:GetCount(),0,0)
+	Duel.SetOperationInfo(0,CATEGORY_TODECK,g,g:GetCount(),0,0)
 end
 function c70090868.operation2(e,tp,eg,ep,ev,re,r,rp)
 	if not e:GetHandler():IsRelateToEffect(e) then return end
 	local g=Duel.GetMatchingGroup(Card.IsAbleToDeck,tp,0,LOCATION_ONFIELD+LOCATION_GRAVE+LOCATION_HAND,nil)
-	Duel.SendtoDeck(g,nil,2,REASON_EFFECT)
+	if g:GetCount()>0 then
+		Duel.SendtoDeck(g,nil,2,REASON_EFFECT)
+		Duel.ShuffleDeck(1-tp)
+	end
 end
