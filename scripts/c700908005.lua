f50279be149b45b19ea33f886e3f9c268b3149da
mercury233 <me@mercury233.me>
2016-01-07 16:06:38 +0800

    add EMホタルクス SCRIPT ONLY

---
+++ scripts/c700908005.lua
@@ -0,0 +1,45 @@
+--EMホタルクス
+function c700908005.initial_effect(c)
+	--pendulum summon
+	aux.EnablePendulumAttribute(c)
+	--actlimit
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetCode(EFFECT_CANNOT_ACTIVATE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetTargetRange(0,1)
+	e1:SetValue(c700908005.aclimit)
+	e1:SetCondition(c700908005.actcon)
+	c:RegisterEffect(e1)
+	--negate attack
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e2:SetRange(LOCATION_PZONE)
+	e2:SetCode(EVENT_ATTACK_ANNOUNCE)
+	e2:SetCondition(c700908005.condition)
+	e2:SetCost(c700908005.cost)
+	e2:SetOperation(c700908005.operation)
+	c:RegisterEffect(e2)
+end
+function c700908005.aclimit(e,re,tp)
+	return not re:GetHandler():IsImmuneToEffect(e)
+end
+function c700908005.actcon(e)
+	local c=Duel.GetAttacker()
+	local p=e:GetHandler():GetControler()
+	return c and c:IsControler(p) and (c:IsSetCard(0x9f) or c:IsSetCard(0x99))
+end
+function c700908005.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetTurnPlayer()~=tp
+end
+function c700908005.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.CheckReleaseGroup(tp,Card.IsSetCard,1,nil,0x9f) end
+	local g=Duel.SelectReleaseGroup(tp,Card.IsSetCard,1,1,nil,0x9f)
+	Duel.Release(g,REASON_COST)
+end
+function c700908005.operation(e,tp,eg,ep,ev,re,r,rp)
+	if e:GetHandler():IsRelateToEffect(e) and Duel.NegateAttack() then
+		Duel.SkipPhase(1-tp,PHASE_BATTLE,RESET_PHASE+PHASE_BATTLE,1)
+	end
+end
