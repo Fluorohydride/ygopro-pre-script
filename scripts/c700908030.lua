989020d2871f8f17958a4585c1728352eb4ea661
mercury233 <me@mercury233.me>
2016-01-07 22:05:42 +0800

    add アモルファージ・ノーテス SCRIPT ONLY

---
+++ scripts/c700908030.lua
@@ -0,0 +1,53 @@
+--アモルファージ・ノーテス
+--Script by mercury233
+function c700908030.initial_effect(c)
+	--pendulum summon
+	aux.EnablePendulumAttribute(c)
+	--maintain
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e2:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE)
+	e2:SetRange(LOCATION_PZONE)
+	e2:SetCode(EVENT_PHASE+PHASE_STANDBY)
+	e2:SetCountLimit(1)
+	e2:SetCondition(c700908030.descon)
+	e2:SetOperation(c700908030.desop)
+	c:RegisterEffect(e2)
+	--spsummon limit
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+	e3:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_OATH)
+	e3:SetTargetRange(1,1)
+	e3:SetTarget(c700908030.sumlimit)
+	c:RegisterEffect(e3)
+	--disable search
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_FIELD)
+	e4:SetCode(EFFECT_CANNOT_TO_HAND)
+	e4:SetRange(LOCATION_PZONE)
+	e4:SetCondition(c700908030.limcon)
+	e4:SetTargetRange(LOCATION_DECK,LOCATION_DECK)
+	c:RegisterEffect(e4)
+end
+function c700908030.descon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetTurnPlayer()==tp
+end
+function c700908030.desop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	Duel.Hint(HINT_CARD,0,c:GetCode())
+	if Duel.CheckReleaseGroup(tp,Card.IsReleasableByEffect,1,c) and Duel.SelectYesNo(tp,500) then
+		local g=Duel.SelectReleaseGroup(tp,Card.IsReleasableByEffect,1,1,c)
+		Duel.Release(g,REASON_RULE)
+	else Duel.Destroy(c,REASON_RULE) end
+end
+function c700908030.sumlimit(e,c,sump,sumtype,sumpos,targetp,se)
+	return c:IsLocation(LOCATION_EXTRA) and not c:IsSetCard(0x1374)
+end
+function c700908030.cfilter(c)
+	return c:IsFaceup() and c:IsSetCard(0x1374)
+end
+function c700908030.limcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsExistingMatchingCard(c700908030.cfilter,e:GetHandlerPlayer(),LOCATION_MZONE,0,1,nil)
+end
