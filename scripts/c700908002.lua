d95201979d8f2a98b6260797c75d25bdfd6aeccd
mercury233 <me@mercury233.me>
2016-01-07 16:40:14 +0800

    add EMオオヤヤドカリ SCRIPT ONLY

---
+++ scripts/c700908002.lua
@@ -0,0 +1,71 @@
+--EMオオヤヤドカリ
+--Script by mercury233
+function c700908002.initial_effect(c)
+	--pendulum summon
+	aux.EnablePendulumAttribute(c)
+	--atk
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetCountLimit(1)
+	e1:SetTarget(c700908002.atktg)
+	e1:SetOperation(c700908002.atkop)
+	c:RegisterEffect(e1)
+	local e2=Effect.CreateEffect(c)
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_BATTLE_DESTROYED)
+	e2:SetRange(LOCATION_PZONE)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetCountLimit(1)
+	e2:SetCondition(c700908002.spcon)
+	e2:SetTarget(c700908002.sptg)
+	e2:SetOperation(c700908002.spop)
+	c:RegisterEffect(e2)
+end
+function c700908002.filter1(c)
+	return c:IsFaceup() and c:IsType(TYPE_PENDULUM)
+end
+function c700908002.atktg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return c:IsLocation(LOCATION_MZONE) and c:IsControler(tp) and c700908002.filter1(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(c700908002.filter1,tp,LOCATION_MZONE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	Duel.SelectTarget(tp,c700908002.filter1,tp,LOCATION_MZONE,0,1,1,nil)
+end
+function c700908002.filter2(c)
+	return c:IsFaceup() and c:IsSetCard(0x9f)
+end
+function c700908002.atkop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	local ct=Duel.GetMatchingGroupCount(c700908002.filter2,tp,LOCATION_MZONE,0,nil)
+	if tc:IsFaceup() and tc:IsRelateToEffect(e) then
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetValue(ct*300)
+		e1:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END)
+		tc:RegisterEffect(e1)
+	end
+end
+function c700908002.filter3(c,tp)
+	return c:IsSetCard(0x9f) and c:GetPreviousControler()==tp
+end
+function c700908002.filter4(c,e,tp)
+	return (c:IsSetCard(0x9f) or c:IsSetCard(0x99)) and (c:GetSequence()==6 or c:GetSequence()==7) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function c700908002.spcon(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(c700908002.filter3,1,nil,tp)
+end
+function c700908002.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingTarget(c700908002.filter4,tp,LOCATION_SZONE,0,1,nil,e,tp) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectTarget(tp,c700908002.filter4,tp,LOCATION_SZONE,0,1,1,nil,e,tp)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
+end
+function c700908002.spop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) then
+		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
