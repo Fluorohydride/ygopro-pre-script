13dd32bcc7bced631a330a06d43a4eea6b9b919e
StormWulf <auronzeroo@aim.com>
2016-01-11 13:39:31 -0500

    Fix Lunalight Tiger's ID
    It wasn't missing from the repository, it just had the wrong ID.

--- scripts/SHVI-JP/c83199280.lua
+++ scripts/SHVI-JP/c83199280.lua
@@ -1,91 +1,100 @@
-﻿--月光虎
+--月光虎
 --Lunalight Tiger
-function c83199280.initial_effect(c)
+--Script by mercury233
+function c83190280.initial_effect(c)
 	--pendulum summon
 	aux.EnablePendulumAttribute(c)
-	--spsummon (pendulum)
+	--spsummon
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(83199280,0))
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetRange(LOCATION_PZONE)
-	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetCountLimit(1)
-	e1:SetTarget(c83199280.sptg)
-	e1:SetOperation(c83199280.spop)
+	e1:SetTarget(c83190280.sptg)
+	e1:SetOperation(c83190280.spop)
 	c:RegisterEffect(e1)
-	--spsummon (destroyed)
+	--spsummon
 	local e2=Effect.CreateEffect(c)
 	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e2:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
 	e2:SetCode(EVENT_DESTROYED)
-	e2:SetCountLimit(1,83199280)
-	e2:SetCondition(c83199280.spcon)
-	e2:SetTarget(c83199280.sptg2)
-	e2:SetOperation(c83199280.spop2)
+	e2:SetCountLimit(1)
+	e2:SetCondition(c83190280.spcon2)
+	e2:SetTarget(c83190280.sptg2)
+	e2:SetOperation(c83190280.spop2)
 	c:RegisterEffect(e2)
 end
-
-function c83199280.filter(c,e,tp)
-	return c:IsSetCard(0xdf) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+function c83190280.filter(c,e,tp)
+	return c:IsSetCard(0xdf) and c:IsType(TYPE_MONSTER) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
-function c83199280.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and c83199280.filter(chkc,e,tp) end
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingTarget(c83199280.filter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
+function c83190280.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and c83190280.filter(chkc,e,tp) end
+	if chk==0 then return Duel.IsExistingTarget(c83190280.filter,tp,LOCATION_GRAVE,0,1,nil,e,tp)
+		and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectTarget(tp,c83199280.filter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
+	local g=Duel.SelectTarget(tp,c83190280.filter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
 end
-function c83199280.spop(e,tp,eg,ep,ev,re,r,rp)
-	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+function c83190280.spop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local tc=Duel.GetFirstTarget()
 	if tc:IsRelateToEffect(e) and Duel.SpecialSummonStep(tc,0,tp,tp,false,false,POS_FACEUP) then
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_DISABLE)
+		e1:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
 		e1:SetReset(RESET_EVENT+0x1fe0000)
-		tc:RegisterEffect(e1,true)
+		tc:RegisterEffect(e1)
 		local e2=Effect.CreateEffect(c)
 		e2:SetType(EFFECT_TYPE_SINGLE)
 		e2:SetCode(EFFECT_DISABLE_EFFECT)
+		e2:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
 		e2:SetReset(RESET_EVENT+0x1fe0000)
-		tc:RegisterEffect(e2,true)
-		local e3=Effect.CreateEffect(c)
-		e3:SetType(EFFECT_TYPE_SINGLE)
-		e3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-		e3:SetCode(EFFECT_CANNOT_ATTACK)
-		e3:SetReset(RESET_EVENT+0x1fe0000)
-		tc:RegisterEffect(e3,true)
-		Duel.SpecialSummonComplete()
+		tc:RegisterEffect(e2)
+        local e3=Effect.CreateEffect(c)
+        e3:SetType(EFFECT_TYPE_SINGLE)
+        e3:SetCode(EFFECT_CANNOT_ATTACK)
+        e3:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
+        e3:SetReset(RESET_EVENT+0x1fe0000)
+        tc:RegisterEffect(e3)
+        tc:RegisterFlagEffect(83190280,RESET_EVENT+0x1fe0000,0,1)
 		local e4=Effect.CreateEffect(c)
 		e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-		e4:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
-		e4:SetRange(LOCATION_MZONE)
 		e4:SetCode(EVENT_PHASE+PHASE_END)
-		e4:SetOperation(c83199280.desop)
-		e4:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END)
 		e4:SetCountLimit(1)
-		tc:RegisterEffect(e4,true)
+		e4:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
+		e4:SetLabelObject(tc)
+		e4:SetCondition(c83190280.descon)
+		e4:SetOperation(c83190280.desop)
+		Duel.RegisterEffect(e4,tp)
+	end
+	Duel.SpecialSummonComplete()
+end
+function c83190280.descon(e,tp,eg,ep,ev,re,r,rp)
+	local tc=e:GetLabelObject()
+	if tc:GetFlagEffect(83190280)~=0 then
+		return true
+	else
+		e:Reset()
+		return false
 	end
 end
-function c83199280.desop(e,tp,eg,ep,ev,re,r,rp)
-	Duel.Destroy(e:GetHandler(),REASON_EFFECT)
+function c83190280.desop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=e:GetLabelObject()
+	Duel.Destroy(tc,REASON_EFFECT)
 end
-
-function c83199280.spcon(e,tp,eg,ep,ev,re,r,rp)
+function c83190280.spcon2(e,tp,eg,ep,ev,re,r,rp)
 	return bit.band(r,REASON_EFFECT+REASON_BATTLE)~=0 and e:GetHandler():IsPreviousLocation(LOCATION_ONFIELD)
 end
-function c83199280.sptg2(e,tp,eg,ep,ev,re,r,rp,chk)
+function c83190280.sptg2(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingTarget(c83199280.filter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
+		and Duel.IsExistingMatchingCard(c83190280.filter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectTarget(tp,c83199280.filter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
+	local g=Duel.SelectTarget(tp,c83190280.filter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
 end
-function c83199280.spop2(e,tp,eg,ep,ev,re,r,rp)
+function c83190280.spop2(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
 	local tc=Duel.GetFirstTarget()
 	if tc:IsRelateToEffect(e) then
