2d4327f7d36ab1a129f328f66eeead4cc136ab1a
StormWulf <auronzeroo@aim.com>
2016-01-11 17:52:33 -0500

    Added name comments to datas.sql

--- datas.sql
+++ datas.sql
@@ -1,19 +1,31 @@
---VJMP-JP
+﻿--VJMP-JP
+----邪神官 チラム・サバク/Wicked Priest Chilam Sabak
 INSERT OR REPLACE INTO "datas" VALUES ("700200103","1","0","0","33","2500","0","8","524288","32","262144");
+----ギャラクシー・ワーム/Galaxy Worm
 INSERT OR REPLACE INTO "datas" VALUES ("700200104","1","0","4063355","33","1200","1000","3","2048","16","262144");
+----月光紅狐/Lunalight Crimson Fox
 INSERT OR REPLACE INTO "datas" VALUES ("700200112","1","0","223","33","1800","600","4","32768","32","0");
 
 --MVPC-JP
+----破滅竜ガンドラＸ/Gandora X the Dragon of Destruction
 INSERT OR REPLACE INTO "datas" VALUES ("700201001","1","0","0","33","0","0","8","8192","32","4202498");
+----暗黒騎士ガイアロード/Lord Gaia The Fierce Knight
 INSERT OR REPLACE INTO "datas" VALUES ("700201002","1","0","189","33","2300","2100","7","1","1","0");
+----レモン・マジシャン・ガール/Lemon Magician Girl
 INSERT OR REPLACE INTO "datas" VALUES ("700201003","1","0","0","33","800","600","2","2","16","0");
 
 --???
+----エルフの聖剣士/Sacred Celtic Guard
 INSERT OR REPLACE INTO "datas" VALUES ("700202002","1","0","544866532","33","2100","700","4","1","1","262400");
 
 --SR02
+----巨神竜フェルグラント/Felgrand, the Great Divine Dragon
 INSERT OR REPLACE INTO "datas" VALUES ("700302001","1","0","0","33","2800","2800","8","8192","16","0");
+----巨竜の聖騎士/Paladin of Felgrand
 INSERT OR REPLACE INTO "datas" VALUES ("700302002","1","0","0","33","1700","300","4","1","16","0");
+----巨神竜の遺跡/Ruins of the Great Divine Dragon
 INSERT OR REPLACE INTO "datas" VALUES ("700302023","1","0","0","131074","0","0","0","0","0","0");
+----復活の福音/Gospel of Revival
 INSERT OR REPLACE INTO "datas" VALUES ("700302024","1","0","0","2","0","0","0","0","0","262144");
+----巨竜トークン/Great Dragon Token
 INSERT OR REPLACE INTO "datas" VALUES ("700302123","1","0","0","16401","0","0","1","8192","16","0");
