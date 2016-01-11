4379841809633653798bc1dcb709ebb79b8c6876
mercury233 <me@mercury233.me>
2016-01-11 10:31:00 +0800

    fix

--- scripts/c99000107.lua
+++ scripts/c99000107.lua
@@ -3,14 +3,14 @@
 --Scripted by Eerie Code
 function c99000107.initial_effect(c)
 	--Cannot activate
-	local e6=Effect.CreateEffect(c)
-	e6:SetType(EFFECT_TYPE_FIELD)
-	e6:SetRange(LOCATION_MZONE)
-	e6:SetCode(EFFECT_CANNOT_ACTIVATE)
-	e6:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-	e6:SetTargetRange(1,1)
-	e6:SetValue(c99000107.aclimit)
-	c:RegisterEffect(e6)
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCode(EFFECT_CANNOT_ACTIVATE)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetTargetRange(1,1)
+	e1:SetValue(c99000107.aclimit)
+	c:RegisterEffect(e1)
 	--
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_SINGLE)
