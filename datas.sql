71851d04dd5cb0359613f8ea35a26653e48ab7c8
StormWulf <auronzeroo@aim.com>
2016-01-24 22:35:46 -0500

    Fixing Sacred Celtic's name

--- datas.sql
+++ datas.sql
@@ -15,7 +15,7 @@ INSERT OR REPLACE INTO "datas" VALUES ("100201002","1","0","189","33","2300","21
 INSERT OR REPLACE INTO "datas" VALUES ("100201003","1","0","8354","33","800","600","2","2","16","0");
 
 --MVPL-JP
-----エルフの聖剣士/Sacred Celtic Guard
+----エルフの聖剣士/Sacred Celtic Guard of Noble Arms
 INSERT OR REPLACE INTO "datas" VALUES ("100202002","1","0","544866529","33","2100","700","4","1","1","262400");
 
 --SR02
