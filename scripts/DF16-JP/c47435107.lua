830caf25714c52fb59136aeaa407e75d7e94cca6
nekrozar <unyuho.buster@gmail.com>
2016-02-11 16:30:57 +0900

    change ID

--- scripts/DF16-JP/c47435107.lua
+++ scripts/DF16-JP/c47435107.lua
@@ -1,8 +1,8 @@
 --原初の叫喚
 --Primal Cry
 --Scripted by Eerie Code-6123
-function c100203010.initial_effect(c)
-	aux.AddRitualProcGreaterCode(c,100203000)
+function c47435107.initial_effect(c)
+	aux.AddRitualProcGreaterCode(c,10441498)
 	--Special Summon
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
@@ -10,31 +10,31 @@ function c100203010.initial_effect(c)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetRange(LOCATION_GRAVE)
 	e1:SetCountLimit(1)
-	e1:SetCondition(c100203010.spcon)
-	e1:SetCost(c100203010.spcost)
-	e1:SetTarget(c100203010.sptg)
-	e1:SetOperation(c100203010.spop)
+	e1:SetCondition(c47435107.spcon)
+	e1:SetCost(c47435107.spcost)
+	e1:SetTarget(c47435107.sptg)
+	e1:SetOperation(c47435107.spop)
 	c:RegisterEffect(e1)
 end
-function c100203010.spcon(e,tp,eg,ep,ev,re,r,rp)
+function c47435107.spcon(e,tp,eg,ep,ev,re,r,rp)
 	return Duel.GetTurnPlayer()==tp
 end
-function c100203010.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+function c47435107.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return e:GetHandler():IsAbleToRemoveAsCost() end
 	Duel.Remove(e:GetHandler(),POS_FACEUP,REASON_COST)
 end
-function c100203010.spfil(c,e,tp,turn)
+function c47435107.spfil(c,e,tp,turn)
 	return c:IsType(TYPE_RITUAL) and c:IsCanBeSpecialSummoned(e,0,tp,false,false) and c:IsPreviousLocation(LOCATION_ONFIELD) and c:GetTurnID()==turn
 end
-function c100203010.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+function c47435107.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	local turn=Duel.GetTurnCount()
-	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and c100203010.spfil(chkc,e,tp,turn) end
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.IsExistingTarget(c100203010.spfil,tp,LOCATION_GRAVE,0,1,nil,e,tp,turn) end
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and c47435107.spfil(chkc,e,tp,turn) end
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.IsExistingTarget(c47435107.spfil,tp,LOCATION_GRAVE,0,1,nil,e,tp,turn) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectTarget(tp,c100203010.spfil,tp,LOCATION_GRAVE,0,1,1,nil,e,tp,turn)
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,g:GetCount(),0,0)
+	local g=Duel.SelectTarget(tp,c47435107.spfil,tp,LOCATION_GRAVE,0,1,1,nil,e,tp,turn)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
 end
-function c100203010.spop(e,tp,eg,ep,ev,re,r,rp)
+function c47435107.spop(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<1 then return end
 	local tc=Duel.GetFirstTarget()
 	if tc:IsRelateToEffect(e) then
--- scripts/DF16-JP/c73820802.lua
+++ scripts/DF16-JP/c73820802.lua
@@ -1,41 +1,41 @@
 --鬼くじ
 --Fiendish Fortune
 --Script by nekrozar
-function c100203011.initial_effect(c)
+function c73820802.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetHintTiming(TIMING_STANDBY_PHASE,0)
-	e1:SetTarget(c100203011.target1)
-	e1:SetOperation(c100203011.operation)
+	e1:SetTarget(c73820802.target1)
+	e1:SetOperation(c73820802.operation)
 	c:RegisterEffect(e1)
 	--instant(chain)
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(100203011,0))
+	e2:SetDescription(aux.Stringid(73820802,0))
 	e2:SetCategory(CATEGORY_DRAW+CATEGORY_HANDES)
 	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
 	e2:SetRange(LOCATION_SZONE)
 	e2:SetCode(EVENT_PHASE+PHASE_STANDBY)
 	e2:SetLabel(1)
-	e2:SetCondition(c100203011.condition)
-	e2:SetTarget(c100203011.target2)
-	e2:SetOperation(c100203011.operation)
+	e2:SetCondition(c73820802.condition)
+	e2:SetTarget(c73820802.target2)
+	e2:SetOperation(c73820802.operation)
 	c:RegisterEffect(e2)
 end
-function c100203011.target1(e,tp,eg,ep,ev,re,r,rp,chk)
+function c73820802.target1(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
 	if Duel.GetTurnPlayer()==tp and Duel.GetCurrentPhase()==PHASE_STANDBY and Duel.SelectYesNo(tp,94) then
 		e:SetCategory(CATEGORY_DRAW+CATEGORY_HANDES)
 		e:SetLabel(1)
-		e:GetHandler():RegisterFlagEffect(100203011,RESET_PHASE+PHASE_END,0,1)
+		e:GetHandler():RegisterFlagEffect(73820802,RESET_PHASE+PHASE_END,0,1)
 		e:GetHandler():RegisterFlagEffect(0,RESET_CHAIN,EFFECT_FLAG_CLIENT_HINT,1,0,65)
 	else
 		e:SetCategory(0)
 		e:SetLabel(0)
 	end
 end
-function c100203011.operation(e,tp,eg,ep,ev,re,r,rp)
+function c73820802.operation(e,tp,eg,ep,ev,re,r,rp)
 	if not e:GetHandler():IsRelateToEffect(e) then return end
 	if e:GetLabel()==0 then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CARDTYPE)
@@ -51,10 +51,10 @@ function c100203011.operation(e,tp,eg,ep,ev,re,r,rp)
 	end
 	Duel.MoveSequence(tc,1)
 end
-function c100203011.condition(e,tp,eg,ep,ev,re,r,rp)
+function c73820802.condition(e,tp,eg,ep,ev,re,r,rp)
 	return tp==Duel.GetTurnPlayer()
 end
-function c100203011.target2(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return e:GetHandler():GetFlagEffect(100203011)==0 end
-	e:GetHandler():RegisterFlagEffect(100203011,RESET_PHASE+PHASE_END,0,1)
+function c73820802.target2(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():GetFlagEffect(73820802)==0 end
+	e:GetHandler():RegisterFlagEffect(73820802,RESET_PHASE+PHASE_END,0,1)
 end
