162e9c3acda38c58074266fa5e2d631b7ea82785
nekrozar <unyuho.buster@gmail.com>
2016-02-11 10:30:28 +0900

    add DF16(script only)

---
+++ scripts/DF16-JP/c100203011.lua
@@ -0,0 +1,60 @@
+--鬼くじ
+--Fiendish Fortune
+--Script by nekrozar
+function c100203011.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetHintTiming(TIMING_STANDBY_PHASE,0)
+	e1:SetTarget(c100203011.target1)
+	e1:SetOperation(c100203011.operation)
+	c:RegisterEffect(e1)
+	--instant(chain)
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(100203011,0))
+	e2:SetCategory(CATEGORY_DRAW+CATEGORY_HANDES)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetCode(EVENT_PHASE+PHASE_STANDBY)
+	e2:SetLabel(1)
+	e2:SetCondition(c100203011.condition)
+	e2:SetTarget(c100203011.target2)
+	e2:SetOperation(c100203011.operation)
+	c:RegisterEffect(e2)
+end
+function c100203011.target1(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	if Duel.GetTurnPlayer()==tp and Duel.GetCurrentPhase()==PHASE_STANDBY and Duel.SelectYesNo(tp,94) then
+		e:SetCategory(CATEGORY_DRAW+CATEGORY_HANDES)
+		e:SetLabel(1)
+		e:GetHandler():RegisterFlagEffect(100203011,RESET_PHASE+PHASE_END,0,1)
+		e:GetHandler():RegisterFlagEffect(0,RESET_CHAIN,EFFECT_FLAG_CLIENT_HINT,1,0,65)
+	else
+		e:SetCategory(0)
+		e:SetLabel(0)
+	end
+end
+function c100203011.operation(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetHandler():IsRelateToEffect(e) then return end
+	if e:GetLabel()==0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CARDTYPE)
+	local op=Duel.SelectOption(tp,70,71,72)
+	Duel.ConfirmDecktop(tp,1)
+	local g=Duel.GetDecktopGroup(tp,1)
+	local tc=g:GetFirst()
+	if (op==0 and tc:IsType(TYPE_MONSTER)) or (op==1 and tc:IsType(TYPE_SPELL)) or (op==2 and tc:IsType(TYPE_TRAP)) then
+		Duel.Draw(1-tp,1,REASON_EFFECT)
+	else
+		local sg=Duel.GetFieldGroup(1-tp,LOCATION_HAND,0):RandomSelect(1-tp,1)
+		Duel.SendtoGrave(sg,REASON_DISCARD+REASON_EFFECT)
+	end
+	Duel.MoveSequence(tc,1)
+end
+function c100203011.condition(e,tp,eg,ep,ev,re,r,rp)
+	return tp==Duel.GetTurnPlayer()
+end
+function c100203011.target2(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():GetFlagEffect(100203011)==0 end
+	e:GetHandler():RegisterFlagEffect(100203011,RESET_PHASE+PHASE_END,0,1)
+end
