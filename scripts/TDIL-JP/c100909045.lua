e572a95e2af8d4dcfc8a5ca1ec71707796871566
mercury233 <me@mercury233.me>
2016-02-17 22:43:53 +0800

    add TDIL-045 SCRIPT ONLY

--- scripts/TDIL-JP/c100909045.lua
+++ scripts/TDIL-JP/c100909045.lua
@@ -1,4 +1,11 @@
+--メタルフォーゼ・カーディナル
 --Metalphosis Cardinal
+--Script by mercury233
 function c100909045.initial_effect(c)
-	
+	--fusion material
+	c:EnableReviveLimit()
+	aux.AddFusionProcFunFunRep(c,aux.FilterBoolFunction(Card.IsFusionSetCard,0xe2),c100909045.matfilter,2,2,true)
+end
+function c100909045.matfilter(c)
+	return c:IsType(TYPE_MONSTER) and c:GetAttack()<=3000
 end
