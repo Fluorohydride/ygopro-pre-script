0f6f4349596b39f7a55dcf5825565650dfa5c1f0
dest <dest.dn@gmail.com>
2016-02-19 04:13:59 +0100

    Add TDIL-052 057 SCRIPT ONLY

---
+++ scripts/TDIL-JP/c100909052.lua
@@ -0,0 +1,76 @@
+--虚空の黒魔導師
+--Ebon Void Magician
+--Script by dest
+function c100909052.initial_effect(c)
+	--xyz summon
+	aux.AddXyzProcedure(c,aux.FilterBoolFunction(Card.IsRace,RACE_SPELLCASTER),7,2)
+	c:EnableReviveLimit()
+	--act qp/trap in hand
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_QP_ACT_IN_NTPHAND)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetTargetRange(LOCATION_HAND,0)
+	e1:SetCondition(c100909052.handcon)
+	c:RegisterEffect(e1)
+	local e2=e1:Clone()
+	e2:SetCode(EFFECT_TRAP_ACT_IN_HAND)
+	c:RegisterEffect(e2)
+	--activate cost
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetCode(EFFECT_ACTIVATE_COST)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e3:SetTargetRange(1,0)
+	e3:SetTarget(c100909052.costtg)
+	e3:SetOperation(c100909052.costop)
+	c:RegisterEffect(e3)
+	--spsummon
+	local e4=Effect.CreateEffect(c)
+	e4:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_DESTROY)
+	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e4:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
+	e4:SetCode(EVENT_TO_GRAVE)
+	e4:SetCondition(c100909052.spcon)
+	e4:SetTarget(c100909052.sptg)
+	e4:SetOperation(c100909052.spop)
+	c:RegisterEffect(e4)
+end
+function c100909052.handcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetTurnPlayer()~=tp and e:GetHandler():GetOverlayCount()~=0
+end
+function c100909052.costtg(e,te,tp)
+	return te:GetHandler():IsLocation(LOCATION_HAND) and te:GetHandler():IsType(TYPE_SPELL+TYPE_TRAP)
+end
+function c100909052.costop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetTurnPlayer()==tp then return end
+	e:GetHandler():RemoveOverlayCard(tp,1,1,REASON_EFFECT)
+end
+function c100909052.spcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return ((rp~=tp and c:IsReason(REASON_EFFECT) and c:GetPreviousControler()==tp and c:IsPreviousLocation(LOCATION_MZONE)) 
+		or c:IsReason(REASON_BATTLE)) and bit.band(c:GetSummonType(),SUMMON_TYPE_XYZ)==SUMMON_TYPE_XYZ
+end
+function c100909052.spfilter(c,e,tp)
+	return c:IsRace(RACE_SPELLCASTER) and c:IsAttribute(ATTRIBUTE_DARK) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function c100909052.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(c100909052.spfilter,tp,LOCATION_HAND+LOCATION_DECK,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND+LOCATION_DECK)
+end
+function c100909052.spop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,c100909052.spfilter,tp,LOCATION_HAND+LOCATION_DECK,0,1,1,nil,e,tp)
+	if g:GetCount()>0 and Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+	and Duel.SelectYesNo(tp,aux.Stringid(100909052,0)) then
+		Duel.BreakEffect()
+		local dg=Duel.GetMatchingGroup(Card.IsDestructable,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,nil)
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+		local des=dg:Select(tp,1,1,nil)
+		Duel.HintSelection(des)
+		Duel.Destroy(des,REASON_EFFECT)
+	end
+end
---
+++ scripts/TDIL-JP/c100909057.lua
@@ -0,0 +1,65 @@
+--黒の魔導陣
+--Dark Magic Circle
+--Script by dest
+--e1 needs updated files from https://github.com/Fluorohydride/ygopro-scripts/pull/273 in order to work
+function c100909057.initial_effect(c)
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_TOHAND)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCountLimit(1,100909057)
+	e1:SetOperation(c100909057.activate)
+	c:RegisterEffect(e1)
+	--remove
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(100909057,1))
+	e2:SetCategory(CATEGORY_REMOVE)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_SUMMON_SUCCESS)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DELAY)
+	e2:SetCountLimit(1,100909157)
+	e2:SetCondition(c100909057.rmcon)
+	e2:SetTarget(c100909057.rmtg)
+	e2:SetOperation(c100909057.rmop)
+	c:RegisterEffect(e2)
+	local e3=e2:Clone()
+	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
+	c:RegisterEffect(e3)
+end
+function c100909057.filter(c)
+	return (c.dark_magician_list or c:IsCode(46986414)) and c:IsAbleToHand()
+end
+function c100909057.activate(e,tp,eg,ep,ev,re,r,rp,chk)
+	if Duel.GetFieldGroupCount(tp,LOCATION_DECK,0)<3 then return end
+	local g=Duel.GetDecktopGroup(tp,3)
+	Duel.ConfirmCards(tp,g)
+	if g:IsExists(c100909057.filter,1,nil) and Duel.SelectYesNo(tp,aux.Stringid(100909057,0)) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+		local sg=g:FilterSelect(tp,c100909057.filter,1,1,nil)
+		Duel.DisableShuffleCheck()
+		Duel.SendtoHand(sg,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,sg)
+		Duel.ShuffleHand(tp)
+		Duel.SortDecktop(tp,tp,2)
+	else Duel.SortDecktop(tp,tp,3) end
+end
+function c100909057.cfilter(c,tp)
+	return c:IsCode(46986414) and c:IsControler(tp)
+end
+function c100909057.rmcon(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(c100909057.cfilter,1,nil,tp)
+end
+function c100909057.rmtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsOnField() and chkc:IsControler(1-tp) and chkc:IsAbleToRemove() end
+	if chk==0 then return Duel.IsExistingTarget(Card.IsAbleToRemove,tp,0,LOCATION_ONFIELD,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local g=Duel.SelectTarget(tp,Card.IsAbleToRemove,tp,0,LOCATION_ONFIELD,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_REMOVE,g,1,0,0)
+end
+function c100909057.rmop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) then
+		Duel.Remove(tc,POS_FACEUP,REASON_EFFECT)
+	end
+end
