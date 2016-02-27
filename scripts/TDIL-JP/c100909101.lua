be1b0244d2584cd5538c50303f269bed785da1c6
nekrozar <unyuho.buster@gmail.com>
2016-02-27 09:37:25 +0900

    fix Performapal Bot-Eyes Lizard
    "Send 1 "Odd-Eyes" monster from your Deck to the Graveyard" is not
    effect.

--- scripts/TDIL-JP/c100909101.lua
+++ scripts/TDIL-JP/c100909101.lua
@@ -2,18 +2,16 @@
 --Performapal Bot-Eyes Lizard
 --ygohack137-13790916
 function c100909101.initial_effect(c)
-	--tograve
+	--copy
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(100909101,0))
-	e1:SetCategory(CATEGORY_TOGRAVE)
 	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetCountLimit(1)
-	e1:SetCondition(c100909101.tgcon)
-	e1:SetTarget(c100909101.tgtg)
-	e1:SetOperation(c100909101.tgop)
+	e1:SetCondition(c100909101.copycon)
+	e1:SetCost(c100909101.copycost)
+	e1:SetOperation(c100909101.copyop)
 	c:RegisterEffect(e1)
-
 	if not c100909101.global_check then
 		c100909101.global_check=true
 		local ge1=Effect.CreateEffect(c)
@@ -25,33 +23,48 @@ function c100909101.initial_effect(c)
 		Duel.RegisterEffect(ge1,0)
 		local ge2=ge1:Clone()
 		ge2:SetCode(EVENT_SPSUMMON_SUCCESS)
-		ge2:SetLabel(100909101)
 		Duel.RegisterEffect(ge2,0)
 	end
 end
-function c100909101.tgcon(e,tp,eg,ep,ev,re,r,rp)
+function c100909101.copycon(e,tp,eg,ep,ev,re,r,rp)
 	return e:GetHandler():GetFlagEffect(100909101)>0
 end
-function c100909101.tgfilter(c)
-	return c:IsSetCard(0x99) and c:IsAbleToGrave()
+function c100909101.costfilter(c)
+	return c:IsSetCard(0x99) and c:IsType(TYPE_MONSTER) and c:IsAbleToGrave()
 end
-function c100909101.tgtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(c100909101.tgfilter,tp,LOCATION_DECK,0,1,nil) end
-	Duel.Hint(HINT_OPSELECTED,1-tp,e:GetDescription())
-	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_DECK)
-end
-function c100909101.tgop(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
+function c100909101.copycost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c100909101.costfilter,tp,LOCATION_DECK,0,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
-	local g=Duel.SelectMatchingCard(tp,c100909101.tgfilter,tp,LOCATION_DECK,0,1,1,nil)
-	local tc=g:GetFirst()
-	if tc and Duel.SendtoGrave(tc,REASON_EFFECT)~=0 and tc:IsLocation(LOCATION_GRAVE) then
-		local e1=Effect.CreateEffect(e:GetHandler())
-		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetCode(EFFECT_CHANGE_CODE)
-		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-		e1:SetValue(tc:GetCode())
-		e1:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END)
-		c:RegisterEffect(e1)
-	end
+	local g=Duel.SelectMatchingCard(tp,c100909101.costfilter,tp,LOCATION_DECK,0,1,1,nil)
+	Duel.SendtoGrave(g,REASON_COST)
+	e:SetLabel(g:GetFirst():GetCode())
+end
+function c100909101.copyop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not c:IsRelateToEffect(e) or c:IsFacedown() then return end
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_CHANGE_CODE)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e1:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END)
+	e1:SetValue(e:GetLabel())
+	c:RegisterEffect(e1)
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(100909101,1))
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e2:SetCode(EVENT_PHASE+PHASE_END)
+	e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e2:SetCountLimit(1)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END)
+	e2:SetLabelObject(e1)
+	e2:SetOperation(c100909101.rstop)
+	c:RegisterEffect(e2)
+end
+function c100909101.rstop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local e1=e:GetLabelObject()
+	e1:Reset()
+	Duel.HintSelection(Group.FromCards(c))
+	Duel.Hint(HINT_OPSELECTED,1-tp,e:GetDescription())
 end
