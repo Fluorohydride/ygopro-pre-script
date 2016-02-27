ad944abc169a8640760a32eaeff55b39e38e5ac7
nekrozar <unyuho.buster@gmail.com>
2016-02-27 18:55:12 +0900

    add script TDIL

---
+++ scripts/TDIL-JP/c100909040.lua
@@ -0,0 +1,88 @@
+--チューニングガム
+--Tunung Gum
+--Script by nekrozar
+function c100909040.initial_effect(c)
+	--tuner
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(100909040,0))
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1,100909040)
+	e1:SetCost(c100909040.cost)
+	e1:SetTarget(c100909040.target)
+	e1:SetOperation(c100909040.operation)
+	c:RegisterEffect(e1)
+	--negate
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(100909040,1))
+	e2:SetCategory(CATEGORY_NEGATE)
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetCode(EVENT_CHAINING)
+	e2:SetRange(LOCATION_GRAVE)
+	e2:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DAMAGE_CAL)
+	e2:SetCondition(c100909040.negcon)
+	e2:SetCost(c100909040.negcost)
+	e2:SetTarget(c100909040.negtg)
+	e2:SetOperation(c100909040.negop)
+	c:RegisterEffect(e2)
+	Duel.AddCustomActivityCounter(100909040,ACTIVITY_ATTACK,c100909040.counterfilter)
+end
+function c100909040.counterfilter(c)
+    return c:IsType(TYPE_SYNCHRO)
+end
+function c100909040.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetCustomActivityCount(100909040,tp,ACTIVITY_ATTACK)==0 end
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_CANNOT_ATTACK)
+	e1:SetProperty(EFFECT_FLAG_OATH+EFFECT_FLAG_IGNORE_IMMUNE)
+	e1:SetTarget(c100909040.atklimit)
+	e1:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+	e1:SetReset(RESET_PHASE+PHASE_END)
+	Duel.RegisterEffect(e1,tp)
+end
+function c100909040.atklimit(e,c)
+	return not c:IsType(TYPE_SYNCHRO)
+end
+function c100909040.filter(c)
+	return c:IsFaceup() and not c:IsType(TYPE_TUNER)
+end
+function c100909040.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and c100909040.filter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(c100909040.filter,tp,LOCATION_MZONE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	Duel.SelectTarget(tp,c100909040.filter,tp,LOCATION_MZONE,0,1,1,nil)
+end
+function c100909040.operation(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) and tc:IsFaceup() then
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_ADD_TYPE)
+		e1:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END)
+		e1:SetValue(TYPE_TUNER)
+		tc:RegisterEffect(e1)
+	end
+end
+function c100909040.cfilter(c,tp)
+	return c:IsControler(tp) and c:IsLocation(LOCATION_MZONE) and c:IsFaceup() and c:IsType(TYPE_SYNCHRO)
+end
+function c100909040.negcon(e,tp,eg,ep,ev,re,r,rp)
+	if not re:IsHasProperty(EFFECT_FLAG_CARD_TARGET) then return false end
+	local g=Duel.GetChainInfo(ev,CHAININFO_TARGET_CARDS)
+	if not g or g:GetCount()~=1 then return false end
+	return tc:IsControler(tp) and tc:IsFaceup() and tc:IsType(TYPE_SYNCHRO) and tc:IsLocation(LOCATION_MZONE)
+		and Duel.IsChainNegatable(ev)
+end
+function c100909040.negcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsAbleToRemoveAsCost() end
+	Duel.Remove(e:GetHandler(),POS_FACEUP,REASON_COST)
+end
+function c100909040.negtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
+end
+function c100909040.negop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.NegateActivation(ev)
+end
