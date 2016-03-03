95af873dae9a938a20d9866aa90f605643e0d763
nekrozar <unyuho.buster@gmail.com>
2016-03-03 16:35:40 +0900

    add script TDIL

---
+++ scripts/TDIL-JP/c100909022.lua
@@ -0,0 +1,43 @@
+--メタルフォーゼ・シルバード
+--Metalphosis Silvbird
+--Script by nekrozar
+function c100909022.initial_effect(c)
+	--pendulum summon
+	aux.EnablePendulumAttribute(c)
+	--destroy and set
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_DESTROY)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_PZONE)
+	e1:SetCountLimit(1)
+	e1:SetTarget(c100909022.target)
+	e1:SetOperation(c100909022.operation)
+	c:RegisterEffect(e1)
+end
+function c100909022.desfilter(c)
+	return c:IsFaceup() and c:IsDestructable()
+end
+function c100909022.filter(c)
+	return c:IsSetCard(0xe2) and c:IsType(TYPE_SPELL+TYPE_TRAP) and c:IsSSetable()
+end
+function c100909022.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsOnField() and chkc:IsControler(tp) and c100909022.desfilter(chkc) and chkc~=e:GetHandler() end
+	if chk==0 then return Duel.IsExistingTarget(c100909022.desfilter,tp,LOCATION_ONFIELD,0,1,e:GetHandler())
+		and Duel.IsExistingMatchingCard(c100909022.filter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local g=Duel.SelectTarget(tp,c100909022.desfilter,tp,LOCATION_ONFIELD,0,1,1,e:GetHandler())
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
+end
+function c100909022.operation(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tc=Duel.GetFirstTarget()
+	if c:IsRelateToEffect(e) and tc:IsRelateToEffect(e) and Duel.Destroy(tc,REASON_EFFECT)~=0 then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SET)
+		local g=Duel.SelectMatchingCard(tp,c100909022.filter,tp,LOCATION_DECK,0,1,1,nil)
+		if g:GetCount()>0 then
+			Duel.SSet(tp,g:GetFirst())
+			Duel.ConfirmCards(1-tp,g)
+		end
+	end
+end
