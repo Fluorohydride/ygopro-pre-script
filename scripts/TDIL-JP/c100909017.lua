f9c0ab9f2d192da36a3503d721c00062c46eb7fb
dest <dest.dn@gmail.com>
2016-03-07 16:26:04 +0100

    fix
    https://github.com/Fluorohydride/ygopro-pre-script/issues/14

--- scripts/TDIL-JP/c100909017.lua
+++ scripts/TDIL-JP/c100909017.lua
@@ -68,7 +68,8 @@ end
 function c100909017.target2(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and c100909017.filter(chkc,e,tp) end
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingTarget(c100909017.filter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
+		and Duel.IsExistingTarget(c100909017.filter,tp,LOCATION_GRAVE,0,1,nil,e,tp)
+		and e:GetHandler():IsLocation(LOCATION_MZONE) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 	local g=Duel.SelectTarget(tp,c100909017.filter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
