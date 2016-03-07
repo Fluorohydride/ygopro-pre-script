032522e7c335c8939afe987ed24a662f4b5321e3
mercury233 <me@mercury233.me>
2016-03-07 21:44:20 +0800

    add Odd-Eyes Persona Dragon

---
+++ scripts/VJMP-JP/c100200114.lua
@@ -0,0 +1,109 @@
+--オッドアイズ・ペルソナ・ドラゴン
+--Odd-Eyes Persona Dragon
+--Script by mercury233
+function c100200114.initial_effect(c)
+	--pendulum summon
+	aux.EnablePendulumAttribute(c)
+	--spsummon reg
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
+	e1:SetCode(EVENT_BECOME_TARGET)
+	e1:SetRange(LOCATION_PZONE)
+	e1:SetOperation(c100200114.regop1)
+	c:RegisterEffect(e1)
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
+	e2:SetCode(EVENT_CHAIN_SOLVED)
+	e2:SetRange(LOCATION_PZONE)
+	e2:SetOperation(c100200114.regop2)
+	c:RegisterEffect(e2)
+	e2:SetLabelObject(e1)
+	--spsummon
+	local e3=Effect.CreateEffect(c)
+	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e3:SetRange(LOCATION_PZONE)
+	e3:SetCode(EVENT_PHASE+PHASE_END)
+	e3:SetCountLimit(1)
+	e3:SetCondition(c100200114.spcon)
+	e3:SetTarget(c100200114.sptg)
+	e3:SetOperation(c100200114.spop)
+	c:RegisterEffect(e3)
+	--disable
+	local e4=Effect.CreateEffect(c)
+	e4:SetCategory(CATEGORY_DISABLE)
+	e4:SetType(EFFECT_TYPE_QUICK_O)
+	e4:SetCode(EVENT_FREE_CHAIN)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e4:SetCountLimit(1)
+	e4:SetTarget(c100200114.distg)
+	e4:SetOperation(c100200114.disop)
+	c:RegisterEffect(e4)
+end
+function c100200114.regfilter(c)
+	return c:IsType(TYPE_PENDULUM) and c:IsFaceup() and c:IsSetCard(0x99)
+end
+function c100200114.regop1(e,tp,eg,ep,ev,re,r,rp)
+	if rp==tp or not re:IsHasProperty(EFFECT_FLAG_CARD_TARGET) or eg:GetCount()~=1
+		or not eg:IsExists(c100200114.regfilter,1,nil) then
+		e:SetLabelObject(nil)
+	else e:SetLabelObject(re) end
+end
+function c100200114.regop2(e,tp,eg,ep,ev,re,r,rp)
+	local pe=e:GetLabelObject():GetLabelObject()
+	if pe and pe==re then
+		e:GetHandler():RegisterFlagEffect(100200114,RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END,0,1)
+	end
+end
+function c100200114.penfilter(c)
+	return c:IsSetCard(0x99) and c:IsType(TYPE_PENDULUM) and not c:IsCode(100200114) and not c:IsForbidden()
+end
+function c100200114.spcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():GetFlagEffect(100200114)~=0
+end
+function c100200114.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false)
+		and Duel.IsExistingMatchingCard(c100200114.penfilter,tp,LOCATION_EXTRA,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
+end
+function c100200114.spop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) then
+		Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOFIELD)
+		local g=Duel.SelectMatchingCard(tp,c100200114.penfilter,tp,LOCATION_EXTRA,0,1,1,nil)
+		local tc=g:GetFirst()
+		if tc then
+			Duel.MoveToField(tc,tp,tp,LOCATION_SZONE,POS_FACEUP,true)
+		end
+	end
+end
+function c100200114.disfilter(c)
+	return c:IsFaceup() and c:GetSummonLocation()==LOCATION_EXTRA and not c:IsDisabled()
+end
+function c100200114.distg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and c100200114.disfilter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(c100200114.disfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+	local g=Duel.SelectTarget(tp,c100200114.disfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_DISABLE,g,1,0,0)
+end
+function c100200114.disop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsFaceup() and tc:IsRelateToEffect(e) then
+		Duel.NegateRelatedChain(tc,RESET_TURN_SET)
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_DISABLE)
+		e1:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END)
+		tc:RegisterEffect(e1)
+		local e2=Effect.CreateEffect(e:GetHandler())
+		e2:SetType(EFFECT_TYPE_SINGLE)
+		e2:SetCode(EFFECT_DISABLE_EFFECT)
+		e2:SetValue(RESET_TURN_SET)
+		e2:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END)
+		tc:RegisterEffect(e2)
+	end
+end
