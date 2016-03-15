3be72d68ac65e78006feaea4808956ee50739133
mercury233 <me@mercury233.me>
2016-03-15 21:45:14 +0800

    init EM Uni and Corn

---
+++ scripts/CPF1-JP/c100206002.lua
@@ -0,0 +1,6 @@
+--EMユニ
+--Performance Uni
+--Script by mercury233
+function c100206002.initial_effect(c)
+	
+end
---
+++ scripts/CPF1-JP/c100206003.lua
@@ -0,0 +1,6 @@
+--EMコン
+--Performance Corn
+--Script by mercury233
+function c100206003.initial_effect(c)
+	
+end
