c31a38af8f85d754d2a0a01a70023d0266a98e1f
mercury233 <me@mercury233.me>
2016-03-16 10:43:38 +0800

    fix Sangan
    should only limit the active of card(P card), not the active of effect

--- scripts/RB03-JP/c26202165.lua
+++ scripts/RB03-JP/c26202165.lua
@@ -45,5 +45,5 @@ function c26202165.operation(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 function c26202165.aclimit(e,re,tp)
-	return re:GetHandler():IsCode(e:GetLabel())
+	return re:GetHandler():IsCode(e:GetLabel()) and re:IsHasType(EFFECT_TYPE_ACTIVATE)
 end
