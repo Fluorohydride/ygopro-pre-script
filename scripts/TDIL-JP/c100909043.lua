98b2a09004064b3021c4271473a5bbee4c10c1c7
Mercury233 <me@mercury233.me>
2016-03-26 10:37:45 +0800

    fix

--- scripts/TDIL-JP/c100909043.lua
+++ scripts/TDIL-JP/c100909043.lua
@@ -7,5 +7,5 @@ function c100909043.initial_effect(c)
 	aux.AddFusionProcFunFunRep(c,aux.FilterBoolFunction(Card.IsFusionSetCard,0xe2),c100909043.matfilter,1,1,true)
 end
 function c100909043.matfilter(c)
-	return c:IsType(TYPE_MONSTER) and c:GetAttack()<=3000
+	return c:IsType(TYPE_MONSTER) and c:GetAttack()<=2500
 end
