ba966aa545fb6564a12ed1b22cccc138b1fb4c75
mercury233 <me@mercury233.me>
2016-03-30 19:50:13 +0800

    add Tramid Cruiser

---
+++ scripts/TDIL-JP/c100909063.lua
@@ -0,0 +1,86 @@
+--トラミッド・クルーザー
+--Tramid Cruiser
+--Script by mercury233
+function c100909063.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e1)
+	--lp recover
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(100909063,0))
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e2:SetCode(EVENT_SUMMON_SUCCESS)
+	e2:SetRange(LOCATION_FZONE)
+	e2:SetTarget(c100909063.lptg)
+	e2:SetOperation(c100909063.lpop)
+	c:RegisterEffect(e2)
+	--draw
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(100909063,1))
+	e3:SetCategory(CATEGORY_DRAW+CATEGORY_HANDES)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e3:SetCode(EVENT_SUMMON_SUCCESS)
+	e3:SetProperty(EFFECT_FLAG_DELAY)
+	e3:SetRange(LOCATION_FZONE)
+	e3:SetTarget(c100909063.drtg)
+	e3:SetOperation(c100909063.drop)
+	c:RegisterEffect(e3)
+	--to hand
+	local e4=Effect.CreateEffect(c)
+	e4:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e4:SetDescription(aux.Stringid(100909063,2))
+	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e4:SetCode(EVENT_TO_GRAVE)
+	e4:SetCondition(c100909063.thcon)
+	e4:SetTarget(c100909063.thtg)
+	e4:SetOperation(c100909063.thop)
+	c:RegisterEffect(e4)
+end
+function c100909063.lpfilter(c)
+	return c:IsRace(RACE_ROCK)
+end
+function c100909063.lptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return eg:IsExists(c100909063.lpfilter,1,nil) end
+end
+function c100909063.lpop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_CARD,0,e:GetHandler():GetCode())
+	Duel.Recover(tp,500,REASON_EFFECT)
+end
+function c100909063.drfilter(c)
+	return c:IsSetCard(0xe4) and c:IsType(TYPE_MONSTER)
+end
+function c100909063.drtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return eg:IsExists(c100909063.drfilter,1,nil) 
+		and Duel.IsPlayerCanDraw(tp,1) end
+	Duel.SetOperationInfo(0,CATEGORY_HANDES,nil,0,tp,1)
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
+end
+function c100909063.drop(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetHandler():IsRelateToEffect(e) then return end
+	if Duel.Draw(tp,1,REASON_EFFECT)~=0 then
+		Duel.BreakEffect()
+		Duel.DiscardHand(tp,nil,1,1,REASON_EFFECT+REASON_DISCARD)
+	end
+end
+function c100909063.thfilter(c,e,tp)
+	return c:IsSetCard(0xe4) and c:IsType(TYPE_MONSTER) and c:IsAbleToHand()
+end
+function c100909063.thcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return c:IsPreviousLocation(LOCATION_ONFIELD) and c:GetPreviousSequence()==5 and c:IsPreviousPosition(POS_FACEUP)
+end
+function c100909063.thtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chk==0 then return Duel.IsExistingMatchingCard(c100909063.thfilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function c100909063.thop(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetHandler():IsRelateToEffect(e) then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,c100909063.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if g:GetCount()>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end
