7424be84577bb0dcd37a2476e36413933130cc1b
mercury233 <me@mercury233.me>
2016-04-07 21:34:57 +0800

    add TDIL-JP039 Totem Five

---
+++ scripts/TDIL-JP/c100909039.lua
@@ -0,0 +1,33 @@
+--トーテム・ファイブ
+--Totem Five
+--Script by mercury233
+function c100909039.initial_effect(c)
+	--destroy
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_DESTROY+CATEGORY_DAMAGE)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_DAMAGE_STEP)
+	e1:SetCondition(c100909039.descon)
+	e1:SetTarget(c100909039.destg)
+	e1:SetOperation(c100909039.desop)
+	c:RegisterEffect(e1)
+end
+function c100909039.descon(e,tp,eg,ep,ev,re,r,rp)
+	return eg:GetCount()==5 and eg:IsContains(e:GetHandler())
+end
+function c100909039.destg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	local g=Duel.GetMatchingGroup(Card.IsDestructable,tp,0,LOCATION_ONFIELD,nil)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,g:GetCount(),0,0)
+	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,g:GetCount()*500)
+end
+function c100909039.desop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(Card.IsDestructable,tp,0,LOCATION_ONFIELD,nil)
+	Duel.Destroy(g,REASON_EFFECT)
+	local sg=Duel.GetOperatedGroup()
+	if sg:GetCount()>0 then
+		Duel.Damage(1-tp,sg:GetCount()*500,REASON_EFFECT)
+	end
+end
