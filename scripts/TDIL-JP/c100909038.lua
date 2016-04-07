0c2d8b105dc2b71fd343cc1f87b8f434c47a6dac
dest <dest.dn@gmail.com>
2016-04-07 15:48:47 +0200

    Add Electrifying Mustangun

---
+++ scripts/TDIL-JP/c100909038.lua
@@ -0,0 +1,50 @@
+--放電ムスタンガン
+--Electrifying Mustangun
+--Script by dest
+function c100909038.initial_effect(c)
+	c:EnableUnsummonable()
+	--splimit
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e1:SetCode(EFFECT_SPSUMMON_CONDITION)
+	e1:SetValue(c100909038.splimit)
+	c:RegisterEffect(e1)
+	--indes
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCode(EFFECT_INDESTRUCTABLE_COUNT)
+	e2:SetCountLimit(2)
+	e2:SetValue(c100909038.valcon)
+	c:RegisterEffect(e2)
+	--disable spsummon
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+	e3:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e3:SetTargetRange(1,0)
+	e3:SetCondition(c100909038.spcon)
+	c:RegisterEffect(e3)
+	local e4=e3:Clone()
+	e4:SetTargetRange(0,1)
+	e4:SetCondition(c100909038.spcon2)
+	c:RegisterEffect(e4)
+end
+function c100909038.splimit(e,se,sp,st)
+	return se:IsHasType(EFFECT_TYPE_ACTIONS) and Duel.GetActivityCount(tp,ACTIVITY_SPSUMMON)==0
+		and Duel.GetCurrentPhase()==PHASE_MAIN1
+end
+function c100909038.valcon(e,re,r,rp)
+	return bit.band(r,REASON_BATTLE)~=0
+end
+function c100909038.spcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetTurnPlayer()==tp 
+		and Duel.GetActivityCount(tp,ACTIVITY_SPSUMMON)>=Duel.GetActivityCount(tp,ACTIVITY_ATTACK)
+end
+function c100909038.spcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetTurnPlayer()~=tp 
+		and Duel.GetActivityCount(1-tp,ACTIVITY_SPSUMMON)>=Duel.GetActivityCount(1-tp,ACTIVITY_ATTACK)
+end
