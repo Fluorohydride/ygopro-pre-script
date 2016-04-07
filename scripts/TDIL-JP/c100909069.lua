537b5bd53e32991fb26ed93e81b1695b44682a91
mercury233 <me@mercury233.me>
2016-04-07 22:38:42 +0800

    fix Fusion Doom Waltz

--- scripts/TDIL-JP/c100909069.lua
+++ scripts/TDIL-JP/c100909069.lua
@@ -12,21 +12,21 @@ function c100909069.initial_effect(c)
 	e1:SetOperation(c100909069.activate)
 	c:RegisterEffect(e1)
 end
-function c100909069.filter1(c)
-	return c:IsFaceup() and c:IsType(TYPE_FUSION) and c:IsSetCard(0xad)
+function c100909069.filter1(c,tp,chk)
+	return c:IsFaceup() and c:IsType(TYPE_FUSION) and c:IsSetCard(0xad) and ((not chk)
+		or Duel.IsExistingTarget(c100909069.filter2,tp,0,LOCATION_MZONE,1,nil))
 end
-function c100909069.filter2(c)
+function c100909069.filter2(c,tp,ex2)
 	return c:IsFaceup() and c:IsType(TYPE_FUSION)
+		and Duel.IsExistingMatchingCard(c100909069.filter3,tp,LOCATION_MZONE,LOCATION_MZONE,1,c,ex2)
 end
 function c100909069.filter3(c,ex2)
 	return c~=ex2 and bit.band(c:GetSummonType(),SUMMON_TYPE_SPECIAL)==SUMMON_TYPE_SPECIAL and c:IsDestructable()
 end
 function c100909069.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingTarget(c100909069.filter1,tp,LOCATION_MZONE,0,1,nil)
-		and Duel.IsExistingTarget(c100909069.filter2,tp,0,LOCATION_MZONE,1,nil)
-		and Duel.IsExistingMatchingCard(c100909069.filter3,tp,LOCATION_MZONE,LOCATION_MZONE,3,nil,nil) end
-	local g1=Duel.SelectTarget(tp,c100909069.filter1,tp,LOCATION_MZONE,0,1,1,nil)
-	local g2=Duel.SelectTarget(tp,c100909069.filter2,tp,0,LOCATION_MZONE,1,1,nil)
+	if chk==0 then return Duel.IsExistingTarget(c100909069.filter1,tp,LOCATION_MZONE,0,1,nil,tp,true) end
+	local g1=Duel.SelectTarget(tp,c100909069.filter1,tp,LOCATION_MZONE,0,1,1,nil,tp,false)
+	local g2=Duel.SelectTarget(tp,c100909069.filter2,tp,0,LOCATION_MZONE,1,1,nil,tp,g1:GetFirst())
 	local g3=Duel.GetMatchingGroup(c100909069.filter3,tp,LOCATION_MZONE,LOCATION_MZONE,g1:GetFirst(),g2:GetFirst())
 	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g3,g3:GetCount(),0,0)
 	local dam=g3:GetSum(Card.GetAttack)
