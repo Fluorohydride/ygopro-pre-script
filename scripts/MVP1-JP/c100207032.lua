5d7f134cddf9cbc2219bd7fbb4aff8d871605e3b
nekrozar <unyuho.buster@gmail.com>
2016-04-16 09:11:01 +0900

    add script MVP1

---
+++ scripts/MVP1-JP/c100207032.lua
@@ -0,0 +1,80 @@
+--方界胤ヴィジャム
+--Houkai Spawn Vijam
+--Script by nekrozar
+function c100207032.initial_effect(c)
+	--cannot destroy
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetValue(1)
+	c:RegisterEffect(e1)
+	--battle
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(100207032,0))
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_DAMAGE_STEP_END)
+	e2:SetTarget(c100207032.distg)
+	e2:SetOperation(c100207032.disop)
+	c:RegisterEffect(e2)
+	--spsummon
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(100207032,1))
+	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e3:SetType(EFFECT_TYPE_IGNITION)
+	e3:SetRange(LOCATION_SZONE)
+	e3:SetCondition(c100207032.spcon)
+	e3:SetTarget(c100207032.sptg)
+	e3:SetOperation(c100207032.spop)
+	c:RegisterEffect(e3)
+end
+function c100207032.distg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local bc=e:GetHandler():GetBattleTarget()
+	if chk==0 then return bc and bc:IsFaceup() and bc:IsRelateToBattle() end
+end
+function c100207032.disop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not c:IsRelateToEffect(e) or c:IsImmuneToEffect(e) then return end
+	Duel.MoveToField(c,tp,tp,LOCATION_SZONE,POS_FACEUP,true)
+	local e1=Effect.CreateEffect(c)
+	e1:SetCode(EFFECT_CHANGE_TYPE)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e1:SetReset(RESET_EVENT+0x1fc0000)
+	e1:SetValue(TYPE_SPELL+TYPE_CONTINUOUS)
+	c:RegisterEffect(e1)
+	c:RegisterFlagEffect(100207032,RESET_EVENT+0x1fc0000,0,1)
+	local bc=c:GetBattleTarget()
+	if bc:IsRelateToBattle() and bc:IsFaceup() then
+		bc:AddCounter(0x1039,1)
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_CANNOT_ATTACK)
+		e1:SetCondition(c100207032.condition)
+		e1:SetReset(RESET_EVENT+0x1fe0000)
+		bc:RegisterEffect(e1)
+		local e2=e1:Clone()
+		e2:SetCode(EFFECT_DISABLE)
+		bc:RegisterEffect(e2)
+	end
+end
+function c100207032.condition(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():GetCounter(0x1039)>0
+end
+function c100207032.spcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():GetFlagEffect(100207032)~=0
+end
+function c100207032.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
+end
+function c100207032.spop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not c:IsRelateToEffect(e) then return end
+	if Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)==0 and Duel.GetLocationCount(tp,LOCATION_MZONE)<=0
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false) then
+		Duel.SendtoGrave(c,REASON_RULE)
+	end
+end
