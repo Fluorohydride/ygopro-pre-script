c647feefb123141507d391e022216a6957a3d203
dest <dest.dn@gmail.com>
2016-04-16 19:02:29 +0200

    fix

--- scripts/CPF1-JP/c100206019.lua
+++ scripts/CPF1-JP/c100206019.lua
@@ -23,7 +23,7 @@ function c100206019.atlimit(e,c)
 	return c~=e:GetHandler()
 end
 function c100206019.deffilter(c)
-	return c:GetBaseDefence()>=0 and c:IsPositions(POS_FACEUP_DEFENCE)
+	return c:GetBaseDefence()>=0 and c:IsPosition(POS_FACEUP_DEFENCE)
 end
 function c100206019.defval(e,c)
 	local g=Duel.GetMatchingGroup(c100206019.deffilter,c:GetControler(),LOCATION_MZONE,0,c)
