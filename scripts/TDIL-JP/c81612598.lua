59da0d03b564bddb51095d3fa70a0fda0090ef82
mercury233 <me@mercury233.me>
2016-04-19 15:13:53 +0800

    rm TDIL and Sangan

--- scripts/TDIL-JP/c81612598.lua
+++
@@ -1,6 +0,0 @@
---メタルフォーゼ・アダマンテ
-function c81612598.initial_effect(c)
-	--fusion material
-	c:EnableReviveLimit()
-	aux.AddFusionProcFun2(c,aux.FilterBoolFunction(Card.IsFusionSetCard,0xe1),aux.FilterBoolFunction(Card.IsAttackBelow,2500),true)
-end
