1d8c1839948af08f613c9b308a37d0345520c6fe
mercury233 <me@mercury233.me>
2016-04-19 16:31:20 +0800

    create SDMY-JP001,002,003

---
+++ scripts/SDMY-JP/c100301002.lua
@@ -0,0 +1,6 @@
+--電磁石の戦士β
+--Electromagnet Warrior Beta
+--Script by mercury233
+function c100301002.initial_effect(c)
+
+end
---
+++ scripts/SDMY-JP/c100301003.lua
@@ -0,0 +1,6 @@
+--電磁石の戦士γ
+--Electromagnet Warrior Gamma
+--Script by mercury233
+function c100301003.initial_effect(c)
+
+end
