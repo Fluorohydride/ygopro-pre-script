51309171cd19a73ff32bf2c64df96ee3ee443c05
mercury233 <me@mercury233.me>
2016-04-20 00:55:26 +0800

    fix Electromagnet Warrior Gamma

--- scripts/SDMY-JP/c100301003.lua
+++ scripts/SDMY-JP/c100301003.lua
@@ -5,13 +5,13 @@ function c100301003.initial_effect(c)
 	--to hand
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(100301003,0))
-	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_TRIGGER_O+EFFECT_TYPE_SINGLE)
 	e1:SetProperty(EFFECT_FLAG_DELAY)
 	e1:SetCode(EVENT_SUMMON_SUCCESS)
 	e1:SetCountLimit(1,100301003)
-	e1:SetTarget(c100301003.thtg)
-	e1:SetOperation(c100301003.thop)
+	e1:SetTarget(c100301003.target)
+	e1:SetOperation(c100301003.operation)
 	c:RegisterEffect(e1)
 	local e2=e1:Clone()
 	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
@@ -29,19 +29,20 @@ function c100301003.initial_effect(c)
 	e3:SetOperation(c100301003.spop)
 	c:RegisterEffect(e3)
 end
-function c100301003.thfilter(c)
-	return (c:IsSetCard(0x2066) or c:IsCode(99785935,39256679,11549357)) and not c:IsCode(100301003) and c:IsLevelBelow(4) and c:IsAbleToHand()
+function c100301003.filter(c,e,tp)
+	return (c:IsSetCard(0x2066) or c:IsCode(99785935,39256679,11549357)) and not c:IsCode(100301003) and c:IsLevelBelow(4) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
-function c100301003.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(c100301003.thfilter,tp,LOCATION_DECK,0,1,nil) end
-	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+function c100301003.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(c100301003.filter,tp,LOCATION_HAND,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
 end
-function c100301003.thop(e,tp,eg,ep,ev,re,r,rp)
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-	local g=Duel.SelectMatchingCard(tp,c100301003.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+function c100301003.operation(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,c100301003.filter,tp,LOCATION_HAND,0,1,1,nil,e,tp)
 	if g:GetCount()>0 then
-		Duel.SendtoHand(g,nil,REASON_EFFECT)
-		Duel.ConfirmCards(1-tp,g)
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
 	end
 end
 function c100301003.spcon(e,tp,eg,ep,ev,re,r,rp)
