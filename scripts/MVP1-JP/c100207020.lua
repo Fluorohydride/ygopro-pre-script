619c78e2ba308e4564c324672cdcfc5f1d8a751d
dest <dest.dn@gmail.com>
2016-04-21 16:08:07 +0200

    Add MVP1 20, 21, 22

---
+++ scripts/MVP1-JP/c100207020.lua
@@ -0,0 +1,35 @@
+--魔導契約の扉
+--Gate of the Magical Contract
+--Script by dest
+function c100207020.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetTarget(c100207020.target)
+	e1:SetOperation(c100207020.activate)
+	c:RegisterEffect(e1)
+end
+function c100207020.filter(c)
+	return (c:GetLevel()==7 or c:GetLevel()==8) and c:IsAttribute(ATTRIBUTE_DARK) and c:IsAbleToHand()
+end
+function c100207020.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsType,tp,LOCATION_HAND,0,1,e:GetHandler(),TYPE_SPELL)
+		and Duel.IsExistingMatchingCard(c100207020.filter,tp,LOCATION_DECK,0,1,nil) end
+end
+function c100207020.activate(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local ag=Duel.SelectMatchingCard(tp,Card.IsType,tp,LOCATION_HAND,0,1,1,nil,TYPE_SPELL)
+	if ag>0 then
+		Duel.SendtoHand(ag,1-tp,REASON_EFFECT)
+		Duel.ConfirmCards(tp,ag)
+		Duel.BreakEffect()
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+		local g=Duel.SelectMatchingCard(tp,c100207020.filter,tp,LOCATION_DECK,0,1,1,nil)
+		if g:GetCount()>0 then
+			Duel.SendtoHand(g,nil,REASON_EFFECT)
+			Duel.ConfirmCards(1-tp,g)
+		end
+	end
+end
---
+++ scripts/MVP1-JP/c100207022.lua
@@ -0,0 +1,55 @@
+--運命の発掘
+--Unearthing Destiny
+--Script by dest
+function c100207022.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_DRAW)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_BATTLE_DAMAGE)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetCondition(c100207022.condition)
+	e1:SetTarget(c100207022.target)
+	e1:SetOperation(c100207022.activate)
+	c:RegisterEffect(e1)
+	--draw
+	local e2=Effect.CreateEffect(c)
+	e2:SetCategory(CATEGORY_DRAW)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e2:SetCode(EVENT_DESTROYED)
+	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e2:SetCondition(c100207022.drcon)
+	e2:SetTarget(c100207022.drtg)
+	e2:SetOperation(c100207022.drop)
+	c:RegisterEffect(e2)
+end
+function c100207022.condition(e,tp,eg,ep,ev,re,r,rp)
+	return ep==tp
+end
+function c100207022.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsPlayerCanDraw(tp,1) end
+	Duel.SetTargetPlayer(tp)
+	Duel.SetTargetParam(1)
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
+end
+function c100207022.activate(e,tp,eg,ep,ev,re,r,rp)
+	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+	Duel.Draw(p,d,REASON_EFFECT)
+end
+function c100207022.drcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return rp~=tp and c:IsReason(REASON_EFFECT) 
+		and c:GetPreviousControler()==tp and c:IsPreviousLocation(LOCATION_ONFIELD)
+end
+function c100207022.drtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local g=Duel.GetMatchingGroupCount(Card.IsCode,tp,LOCATION_GRAVE,0,nil,100207022)
+	if chk==0 then return true end
+	Duel.SetTargetPlayer(tp)
+	Duel.SetTargetParam(g)
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,g)
+end
+function c100207022.activate(e,tp,eg,ep,ev,re,r,rp)
+	local p=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER)
+	local g=Duel.GetMatchingGroupCount(Card.IsCode,tp,LOCATION_GRAVE,0,nil,100207022)
+	Duel.Draw(p,g,REASON_EFFECT)
+end
