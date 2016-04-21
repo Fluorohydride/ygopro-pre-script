ad2f19da32f8825d8deb956e61a25cc487d8f381
mercury233 <me@mercury233.me>
2016-04-21 11:25:12 +0800

    add MVP1-JP023 Dimension Sphinx
    may need fix : https://github.com/Fluorohydride/ygopro/issues/1729

---
+++ scripts/MVP1-JP/c100207023.lua
@@ -0,0 +1,95 @@
+--ディメンション・スフィンクス
+--Dimension Sphinx
+--Script by mercury233
+function c100207023.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetHintTiming(0,0x1e0)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetTarget(c100207023.target)
+	c:RegisterEffect(e1)
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetCode(EVENT_CHAIN_SOLVED)
+	e2:SetCondition(c100207023.tgcon)
+	e2:SetOperation(c100207023.tgop)
+	c:RegisterEffect(e2)
+	--damage
+	local e5=Effect.CreateEffect(c)
+	e5:SetDescription(aux.Stringid(100207023,0))
+	e5:SetCategory(CATEGORY_DAMAGE)
+	e5:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_QUICK_O)
+	e5:SetCode(EVENT_FREE_CHAIN)
+	e5:SetRange(LOCATION_SZONE)
+	e5:SetCountLimit(1)
+	e5:SetHintTiming(TIMING_BATTLE_PHASE)
+	e5:SetCondition(c100207023.damcon)
+	e5:SetTarget(c100207023.damtg)
+	e5:SetOperation(c100207023.damop)
+	c:RegisterEffect(e5)
+	--Destroy
+	local e7=Effect.CreateEffect(c)
+	e7:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
+	e7:SetRange(LOCATION_SZONE)
+	e7:SetCode(EVENT_LEAVE_FIELD)
+	e7:SetProperty(EFFECT_FLAG_DELAY)
+	e7:SetCondition(c100207023.descon)
+	e7:SetOperation(c100207023.desop)
+	c:RegisterEffect(e7)
+end
+function c100207023.filter(c)
+	return c:IsPosition(POS_FACEUP_ATTACK)
+end
+function c100207023.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and c100207023.filter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(c100207023.filter,tp,LOCATION_MZONE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	Duel.SelectTarget(tp,c100207023.filter,tp,LOCATION_MZONE,0,1,1,nil)
+end
+function c100207023.tgcon(e,tp,eg,ep,ev,re,r,rp)
+	return re:GetHandler()==e:GetHandler()
+end
+function c100207023.tgop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tc=Duel.GetChainInfo(ev,CHAININFO_TARGET_CARDS):GetFirst()
+	if c:IsRelateToEffect(re) and tc:IsFaceup() and tc:IsRelateToEffect(re) then
+		c:SetCardTarget(tc)
+	end
+	e:Reset()
+end
+function c100207023.damcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetCurrentPhase()>=PHASE_BATTLE_START and Duel.GetCurrentPhase()<=PHASE_BATTLE
+end
+function c100207023.damtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	local tc=c:GetFirstCardTarget()
+	local at=Duel.GetAttacker()
+	if chk==0 then return tc and at and Duel.GetAttackTarget()==tc
+		and at:GetAttack()>tc:GetAttack() end
+	local dam=math.abs(at:GetAttack()-tc:GetAttack())
+	Duel.SetTargetPlayer(1-tp)
+	Duel.SetTargetParam(dam)
+	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,dam)
+end
+function c100207023.damop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not c:IsRelateToEffect(e) then return end
+	local tc=c:GetFirstCardTarget()
+	if not tc then return false end
+	local at=Duel.GetAttacker()
+	if at:IsRelateToBattle() then
+		local p=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER)
+		Duel.Damage(p,math.abs(at:GetAttack()-tc:GetAttack()),REASON_EFFECT)
+	end
+end
+function c100207023.descon(e,tp,eg,ep,ev,re,r,rp)
+	local tc=e:GetHandler():GetFirstCardTarget()
+	return tc and eg:IsContains(tc)
+end
+function c100207023.desop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Destroy(e:GetHandler(),REASON_EFFECT)
+end
