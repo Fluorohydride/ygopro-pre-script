a57f29775674b8445a96f15d7144b40ae0619cde
nekrozar <unyuho.buster@gmail.com>
2016-04-23 10:30:33 +0900

    change ID

--- scripts/MVPC-JP/c71525232.lua
+++ scripts/MVPC-JP/c71525232.lua
@@ -1,42 +1,42 @@
 --破滅竜ガンドラX
 --Gandora X the Dragon of Destruction
 --Scripted by Eerie Code
-function c100201001.initial_effect(c)
+function c71525232.initial_effect(c)
 	--destroy
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(100201001,0))
+	e1:SetDescription(aux.Stringid(71525232,0))
 	e1:SetCategory(CATEGORY_DESTROY+CATEGORY_DAMAGE+CATEGORY_ATKCHANGE)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e1:SetCode(EVENT_SUMMON_SUCCESS)
-	e1:SetCondition(c100201001.condition)
-	e1:SetTarget(c100201001.target)
-	e1:SetOperation(c100201001.operation)
+	e1:SetCondition(c71525232.condition)
+	e1:SetTarget(c71525232.target)
+	e1:SetOperation(c71525232.operation)
 	c:RegisterEffect(e1)
 	local e2=e1:Clone()
 	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
 	c:RegisterEffect(e2)
 	--halve LP
 	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(100201001,1))
+	e3:SetDescription(aux.Stringid(71525232,1))
 	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
 	e3:SetCode(EVENT_PHASE+PHASE_END)
 	e3:SetRange(LOCATION_MZONE)
 	e3:SetCountLimit(1)
-	e3:SetCondition(c100201001.hvcon)
-	e3:SetOperation(c100201001.hvop)
+	e3:SetCondition(c71525232.hvcon)
+	e3:SetOperation(c71525232.hvop)
 	c:RegisterEffect(e3)
 end
-function c100201001.condition(e,tp,eg,ep,ev,re,r,rp)
+function c71525232.condition(e,tp,eg,ep,ev,re,r,rp)
 	return e:GetHandler():IsPreviousLocation(LOCATION_HAND)
 end
-function c100201001.target(e,tp,eg,ep,ev,re,r,rp,chk)
+function c71525232.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsDestructable,tp,LOCATION_MZONE,LOCATION_MZONE,1,e:GetHandler()) end
 	local g=Duel.GetMatchingGroup(Card.IsDestructable,tp,LOCATION_MZONE,LOCATION_MZONE,e:GetHandler())
 	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,g:GetCount(),0,0)
 	local atk=g:GetMaxGroup(Card.GetAttack)
 	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,atk)
 end
-function c100201001.operation(e,tp,eg,ep,ev,re,r,rp)
+function c71525232.operation(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local g=Duel.GetMatchingGroup(Card.IsDestructable,tp,LOCATION_MZONE,LOCATION_MZONE,c)
 	if g:GetCount()>0 and Duel.Destroy(g,REASON_EFFECT)~=0 then
@@ -53,9 +53,9 @@ function c100201001.operation(e,tp,eg,ep,ev,re,r,rp)
 		end
 	end
 end
-function c100201001.hvcon(e,tp,eg,ep,ev,re,r,rp)
+function c71525232.hvcon(e,tp,eg,ep,ev,re,r,rp)
 	return Duel.GetTurnPlayer()==tp
 end
-function c100201001.hvop(e,tp,eg,ep,ev,re,r,rp)
+function c71525232.hvop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.SetLP(tp,math.ceil(Duel.GetLP(tp)/2))
 end
