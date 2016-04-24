251697e08352bcff8daf59f1ae7110fa01097a27
mercury233 <me@mercury233.me>
2016-04-24 22:06:36 +0800

    fix The blazing MARS

--- scripts/CPF1-JP/c100206007.lua
+++ scripts/CPF1-JP/c100206007.lua
@@ -41,9 +41,9 @@ function c100206007.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 end
 function c100206007.spop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	if not c:IsRelateToEffect(e) then return end
-	if Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)==0 and Duel.GetLocationCount(tp,LOCATION_MZONE)<=0
-		and c:IsCanBeSpecialSummoned(e,0,tp,false,false) and c:IsLocation(LOCATION_HAND) then
+	if c:IsRelateToEffect(e) and Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)==0
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+		and Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 and c:IsLocation(LOCATION_HAND) then
 		Duel.SendtoGrave(c,REASON_RULE)
 	end
 	local e1=Effect.CreateEffect(c)
