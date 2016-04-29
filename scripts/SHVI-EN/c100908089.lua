b4013175d46f47b615d2272b451d98bd9fcb9ace
mercury233 <me@mercury233.me>
2016-04-29 10:43:05 +0800

    fix Kaiju Files

--- scripts/SHVI-EN/c100908089.lua
+++ scripts/SHVI-EN/c100908089.lua
@@ -62,7 +62,7 @@ function c100908089.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_MZONE) and c100908089.filter(chkc,e,tp) end
 	if chk==0 then return Duel.IsExistingTarget(c100908089.filter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil,e,tp) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
-	local g=Duel.SelectTarget(tp,c100908089.filter,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
+	local g=Duel.SelectTarget(tp,c100908089.filter,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil,e,tp)
 	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
 end
