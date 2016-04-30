acca3af3f20e3fcf51afa89d26fea1109c2850e0
nekrozar <unyuho.buster@gmail.com>
2016-04-30 10:30:30 +0900

    change ID

--- scripts/MVPC-JP/c8910971.lua
+++ scripts/MVPC-JP/c8910971.lua
@@ -1,41 +1,41 @@
 --暗黒騎士ガイアロード
 --Lord Gaia The Fierce Knight
-function c100201002.initial_effect(c)
+function c8910971.initial_effect(c)
 	--special summon
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetCode(EFFECT_SPSUMMON_PROC)
 	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
 	e1:SetRange(LOCATION_HAND)
-	e1:SetCondition(c100201002.spcon)
+	e1:SetCondition(c8910971.spcon)
 	c:RegisterEffect(e1)
 	--atkup
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(100201002,0))
+	e2:SetDescription(aux.Stringid(8910971,0))
 	e2:SetCategory(CATEGORY_ATKCHANGE)
 	e2:SetProperty(EFFECT_FLAG_DELAY)
 	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
 	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
 	e2:SetRange(LOCATION_MZONE)
 	e2:SetCountLimit(1)
-	e2:SetCondition(c100201002.condition)
-	e2:SetOperation(c100201002.operation)
+	e2:SetCondition(c8910971.condition)
+	e2:SetOperation(c8910971.operation)
 	c:RegisterEffect(e2)
 end
-function c100201002.spcon(e,c)
+function c8910971.spcon(e,c)
 	if c==nil then return true end
 	return Duel.GetFieldGroupCount(c:GetControler(),LOCATION_MZONE,0)==0
 		and	Duel.GetFieldGroupCount(c:GetControler(),0,LOCATION_MZONE)>0
 		and Duel.GetLocationCount(c:GetControler(),LOCATION_MZONE)>0
 end
-function c100201002.cfilter(c,tp,atk)
+function c8910971.cfilter(c,tp,atk)
 	return c:IsFaceup() and c:IsControler(1-tp) and c:GetAttack()>atk
 end
-function c100201002.condition(e,tp,eg,ep,ev,re,r,rp)
+function c8910971.condition(e,tp,eg,ep,ev,re,r,rp)
 	local atk=e:GetHandler():GetAttack()
-	return eg:IsExists(c100201002.cfilter,1,nil,tp,atk)
+	return eg:IsExists(c8910971.cfilter,1,nil,tp,atk)
 end
-function c100201002.operation(e,tp,eg,ep,ev,re,r,rp)
+function c8910971.operation(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if c:IsFaceup() and c:IsRelateToEffect(e) then
 		local e1=Effect.CreateEffect(c)
