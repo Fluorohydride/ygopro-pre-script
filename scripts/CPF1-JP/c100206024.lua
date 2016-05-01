288ec525bcbcfb4aad9601d4c04c4c8c1cd226a2
nekrozar <unyuho.buster@gmail.com>
2016-05-01 18:28:47 +0900

    add script CPF1

---
+++ scripts/CPF1-JP/c100206024.lua
@@ -0,0 +1,65 @@
+--No.51 怪腕のフィニッシュ・ホールド
+--Number 51: Finish Hold the Amazing
+--Script by nekrozar
+function c100206024.initial_effect(c)
+	c:EnableCounterPermit(0x140)
+	c:SetCounterLimit(0x140,3)
+	--xyz summon
+	aux.AddXyzProcedure(c,nil,3,3)
+	c:EnableReviveLimit()
+	--battle indestructable
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+	e1:SetValue(1)
+	c:RegisterEffect(e1)
+	--return
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(100206024,0))
+	e2:SetCategory(CATEGORY_COUNTER)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_DAMAGE_STEP_END)
+	e2:SetCost(c100206024.ctcost)
+	e2:SetTarget(c100206024.cttg)
+	e2:SetOperation(c100206024.ctop)
+	c:RegisterEffect(e2)
+	--destroy
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(100206024,1))
+	e3:SetCategory(CATEGORY_DESTROY)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e3:SetCode(EVENT_PHASE+PHASE_BATTLE)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCountLimit(1)
+	e3:SetCondition(c100206024.descon)
+	e3:SetTarget(c100206024.destg)
+	e3:SetOperation(c100206024.desop)
+	c:RegisterEffect(e3)
+end
+function c100206024.ctcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():CheckRemoveOverlayCard(tp,1,REASON_COST) end
+	e:GetHandler():RemoveOverlayCard(tp,1,1,REASON_COST)
+end
+function c100206024.cttg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_COUNTER,nil,1,0,0x140)
+end
+function c100206024.ctop(e,tp,eg,ep,ev,re,r,rp)
+	if e:GetHandler():IsRelateToEffect(e) then
+		e:GetHandler():AddCounter(0x140,1)
+	end
+end
+function c100206024.descon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():GetBattledGroupCount()>0 and e:GetHandler():GetCounter(0x140)==3
+end
+function c100206024.destg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsDestructable,tp,0,LOCATION_ONFIELD,1,nil) end
+	local g=Duel.GetMatchingGroup(Card.IsDestructable,tp,0,LOCATION_ONFIELD,nil)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,g:GetCount(),0,0)
+end
+function c100206024.desop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(Card.IsDestructable,tp,0,LOCATION_ONFIELD,nil)
+	if g:GetCount()>0 then
+		Duel.Destroy(g,REASON_EFFECT)
+	end
+end
