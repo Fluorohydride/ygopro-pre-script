deb39274704faed3a5c943b14d214d9822a3da8c
nekrozar <unyuho.buster@gmail.com>
2016-05-04 17:11:48 +0900

    fix

--- scripts/CPF1-JP/c100206100.lua
+++ scripts/CPF1-JP/c100206100.lua
@@ -1,4 +1,4 @@
---Ｎｏ．７８ ナンバーズ・アーカイブ
+--No.78 ナンバーズ・アーカイブ
 --Number 78: Number Archive
 --Script by dest
 function c100206100.initial_effect(c)
@@ -23,12 +23,12 @@ function c100206100.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return e:GetHandler():CheckRemoveOverlayCard(tp,1,REASON_COST) end
 	e:GetHandler():RemoveOverlayCard(tp,1,1,REASON_COST)
 end
-function c100206100.filter(c,e,tp)
+function c100206100.filter(c,e,tp,mc)
 	local m=_G["c"..c:GetCode()]
 	if not m then return false end
 	local no=m.xyz_number
 	return no and no>=1 and no<=99 and c:IsSetCard(0x48)
-		and e:GetHandler():IsCanBeXyzMaterial(c)
+		and mc:IsCanBeXyzMaterial(c)
 		and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_XYZ,tp,false,false)
 end
 function c100206100.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
@@ -38,14 +38,12 @@ function c100206100.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 end
 function c100206100.spop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
 	local g=Duel.GetMatchingGroup(nil,tp,LOCATION_EXTRA,0,nil)
-	if g:GetCount()>0 and ft>-1 and c:IsFaceup() and c:IsRelateToEffect(e) 
-		and c:IsControler(tp) and not c:IsImmuneToEffect(e) then
-		Duel.Hint(HINT_SELECTMSG,1-tp,HINTMSG_SPSUMMON)
-		local tg=g:Select(1-tp,1,1,nil)
+	if g:GetCount()>0 and Duel.GetLocationCount(tp,LOCATION_MZONE)>-1
+		and c:IsFaceup() and c:IsRelateToEffect(e) and c:IsControler(tp) and not c:IsImmuneToEffect(e) then
+		local tg=g:RandomSelect(1-tp,1)
 		Duel.ConfirmCards(1-tp,tg)
-		if tg:IsExists(c100206100.filter,1,nil,e,tp) then
+		if tg:IsExists(c100206100.filter,1,nil,e,tp,c) then
 			local tc=tg:GetFirst()
 			local mg=c:GetOverlayGroup()
 			if mg:GetCount()~=0 then
@@ -54,25 +52,36 @@ function c100206100.spop(e,tp,eg,ep,ev,re,r,rp)
 			tc:SetMaterial(Group.FromCards(c))
 			Duel.Overlay(tc,Group.FromCards(c))
 			Duel.SpecialSummon(tc,SUMMON_TYPE_XYZ,tp,tp,false,false,POS_FACEUP)
+			local fid=c:GetFieldID()
+			tc:RegisterFlagEffect(100206100,RESET_EVENT+0x1fe0000,0,1,fid)
+			tc:CompleteProcedure()
 			local e1=Effect.CreateEffect(c)
 			e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-			e1:SetRange(LOCATION_MZONE)
+			e1:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
 			e1:SetCode(EVENT_PHASE+PHASE_END)
-			e1:SetOperation(c100206100.rmop)
-			e1:SetReset(RESET_EVENT+0xfe0000)
 			e1:SetCountLimit(1)
-			tc:RegisterEffect(e1)
-			tc:CompleteProcedure()
+			e1:SetLabel(fid)
+			e1:SetLabelObject(tc)
+			e1:SetCondition(c100206100.rmcon)
+			e1:SetOperation(c100206100.rmop)
+			Duel.RegisterEffect(e1,tp)
 		end
 	end
-	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-	e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
-	e1:SetReset(RESET_PHASE+PHASE_END)
-	e1:SetTargetRange(1,0)
-	Duel.RegisterEffect(e1,tp)
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e2:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+	e2:SetReset(RESET_PHASE+PHASE_END)
+	e2:SetTargetRange(1,0)
+	Duel.RegisterEffect(e2,tp)
+end
+function c100206100.rmcon(e,tp,eg,ep,ev,re,r,rp)
+	if e:GetLabelObject():GetFlagEffectLabel(100206100)~=e:GetLabel() then
+		e:Reset()
+		return false
+	else return true end
 end
 function c100206100.rmop(e,tp,eg,ep,ev,re,r,rp)
-	Duel.Remove(e:GetHandler(),POS_FACEUP,REASON_EFFECT)
+	local tc=e:GetLabelObject()
+	Duel.Remove(tc,POS_FACEUP,REASON_EFFECT)
 end
