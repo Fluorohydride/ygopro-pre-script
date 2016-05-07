cf5b83a4833320539e99a1fed15e08be02e3b96a
nekrozar <unyuho.buster@gmail.com>
2016-05-07 17:06:34 +0900

    change ID

--- scripts/MVPC-JP/c34318086.lua
+++ scripts/MVPC-JP/c34318086.lua
@@ -1,65 +1,65 @@
 --レモン・マジシャン・ガール
 --Lemon Magician Girl
-function c100201003.initial_effect(c)
+function c34318086.initial_effect(c)
 	--search
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(100201003,0))
+	e1:SetDescription(aux.Stringid(34318086,0))
 	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
 	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetCountLimit(1)
-	e1:SetCost(c100201003.thcost)
-	e1:SetTarget(c100201003.thtg)
-	e1:SetOperation(c100201003.thop)
+	e1:SetCost(c34318086.thcost)
+	e1:SetTarget(c34318086.thtg)
+	e1:SetOperation(c34318086.thop)
 	c:RegisterEffect(e1)
 	--Special Summon + change battle target
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(100201003,1))
+	e2:SetDescription(aux.Stringid(34318086,1))
 	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e2:SetProperty(EFFECT_FLAG_DELAY)
 	e2:SetCode(EVENT_BE_BATTLE_TARGET)
 	e2:SetCountLimit(1)
-	e2:SetTarget(c100201003.sptg)
-	e2:SetOperation(c100201003.spop)
+	e2:SetTarget(c34318086.sptg)
+	e2:SetOperation(c34318086.spop)
 	c:RegisterEffect(e2)
 end
-function c100201003.cfilter(c)
-	return c:IsSetCard(0x20a2) and not c:IsCode(100201003)
+function c34318086.cfilter(c)
+	return c:IsSetCard(0x20a2) and not c:IsCode(34318086)
 end
-function c100201003.thcost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.CheckReleaseGroup(tp,c100201003.cfilter,1,nil) end
-	local g=Duel.SelectReleaseGroup(tp,c100201003.cfilter,1,1,nil)
+function c34318086.thcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.CheckReleaseGroup(tp,c34318086.cfilter,1,nil) end
+	local g=Duel.SelectReleaseGroup(tp,c34318086.cfilter,1,1,nil)
 	Duel.Release(g,REASON_COST)
 end
-function c100201003.filter(c)
+function c34318086.filter(c)
 	return c:IsRace(RACE_SPELLCASTER) and c:IsAbleToHand()
 end
-function c100201003.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(c100201003.filter,tp,LOCATION_DECK,0,1,nil) end
+function c34318086.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c34318086.filter,tp,LOCATION_DECK,0,1,nil) end
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
 end
-function c100201003.thop(e,tp,eg,ep,ev,re,r,rp)
+function c34318086.thop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-	local g=Duel.SelectMatchingCard(tp,c100201003.filter,tp,LOCATION_DECK,0,1,1,nil)
+	local g=Duel.SelectMatchingCard(tp,c34318086.filter,tp,LOCATION_DECK,0,1,1,nil)
 	if g:GetCount()>0 then
 		Duel.SendtoHand(g,nil,REASON_EFFECT)
 		Duel.ConfirmCards(1-tp,g)
 	end
 end
-function c100201003.spfilter(c,e,tp)
+function c34318086.spfilter(c,e,tp)
 	return c:IsRace(RACE_SPELLCASTER) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
-function c100201003.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+function c34318086.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingMatchingCard(c100201003.spfilter,tp,LOCATION_HAND,0,1,nil,e,tp) end
+		and Duel.IsExistingMatchingCard(c34318086.spfilter,tp,LOCATION_HAND,0,1,nil,e,tp) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
 end
-function c100201003.spop(e,tp,eg,ep,ev,re,r,rp)
+function c34318086.spop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectMatchingCard(tp,c100201003.spfilter,tp,LOCATION_HAND,0,1,1,nil,e,tp)
+	local g=Duel.SelectMatchingCard(tp,c34318086.spfilter,tp,LOCATION_HAND,0,1,1,nil,e,tp)
 	local tc=g:GetFirst()
 	if tc and Duel.SpecialSummonStep(tc,0,tp,tp,false,false,POS_FACEUP) then
 		local e1=Effect.CreateEffect(c)
@@ -74,7 +74,7 @@ function c100201003.spop(e,tp,eg,ep,ev,re,r,rp)
 		tc:RegisterEffect(e2)
 		Duel.SpecialSummonComplete()
 		local at=Duel.GetAttacker()
-		if at and at:IsAttackable() and at:IsFaceup() and not at:IsImmuneToEffect(e) and not at:IsStatus(STATUS_ATTACK_CANCELED) then
+		if at and not at:IsImmuneToEffect(e) and Duel.ChangeAttackTarget(tc) then
 			Duel.BreakEffect()
 			Duel.ChangeAttackTarget(tc)
 			local e3=Effect.CreateEffect(c)
