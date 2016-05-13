a3269c831a5543958d89c6accd3bf49912843aec
mercury233 <me@mercury233.me>
2016-05-13 18:58:08 +0800

    add CPF1-JP031 Cardian - Matsu

---
+++ scripts/CPF1-JP/c100206031.lua
@@ -0,0 +1,49 @@
+--花札衛－松－
+--Cardian - Matsu
+--Script by mercury233
+function c100206031.initial_effect(c)
+	--draw (summon)
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_DRAW)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e1:SetCode(EVENT_SUMMON_SUCCESS)
+	e1:SetCountLimit(1,100206031)
+	e1:SetTarget(c100206031.drtg)
+	e1:SetOperation(c100206031.drop1)
+	c:RegisterEffect(e1)
+	--draw (destroy)
+	local e2=Effect.CreateEffect(c)
+	e2:SetCategory(CATEGORY_DRAW)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_TO_GRAVE)
+	e2:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
+	e2:SetCondition(c100206031.drcon)
+	e2:SetTarget(c100206031.drtg)
+	e2:SetOperation(c100206031.drop2)
+	c:RegisterEffect(e2)
+end
+function c100206031.drtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsPlayerCanDraw(tp,1) end
+	Duel.SetTargetPlayer(tp)
+	Duel.SetTargetParam(1)
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
+end
+function c100206031.drop1(e,tp,eg,ep,ev,re,r,rp)
+	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+	if Duel.Draw(p,d,REASON_EFFECT)==0 then return end
+	local tc=Duel.GetOperatedGroup():GetFirst()
+	Duel.ConfirmCards(1-tp,tc)
+	if not (tc:IsSetCard(0x1e5) and tc:IsType(TYPE_MONSTER)) then
+		Duel.SendtoGrave(tc,REASON_EFFECT)
+	end
+	Duel.ShuffleHand(tp)
+end
+function c100206031.drcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return c:IsReason(REASON_BATTLE)
+		or (rp~=tp and c:IsReason(REASON_DESTROY) and c:GetPreviousControler()==tp)
+end
+function c100206031.drop2(e,tp,eg,ep,ev,re,r,rp)
+	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+	Duel.Draw(p,d,REASON_EFFECT)
+end
