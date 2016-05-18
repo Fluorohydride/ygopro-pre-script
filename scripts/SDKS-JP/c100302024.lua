9b4fb8eae08bfa7c5f9c5a3fc9dfdffe84751622
mercury233 <me@mercury233.me>
2016-05-18 21:38:13 +0800

    add SDKS-JP020 Union Hangar

---
+++ scripts/SDKS-JP/c100302024.lua
@@ -0,0 +1,81 @@
+--ユニオン格納庫
+--Union Hangar
+--Script by mercury233
+function c100302024.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCountLimit(1,100302024+EFFECT_COUNT_CODE_OATH)
+	e1:SetOperation(c100302024.activate)
+	c:RegisterEffect(e1)
+	--add counter
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(100302024,1))
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_SUMMON_SUCCESS)
+	e2:SetRange(LOCATION_FZONE)
+	e2:SetCountLimit(1,EFFECT_COUNT_CODE_SINGLE)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DELAY)
+	e2:SetTarget(c100302024.eqtg)
+	e2:SetOperation(c100302024.eqop)
+	c:RegisterEffect(e2)
+	local e3=e2:Clone()
+	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
+	c:RegisterEffect(e3)
+end
+function c100302024.thfilter(c)
+	return c:IsRace(RACE_MACHINE) and c:IsAttribute(ATTRIBUTE_LIGHT)
+		and c:IsType(TYPE_UNION) and c:IsAbleToHand()
+end
+function c100302024.activate(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetHandler():IsRelateToEffect(e) then return end
+	local g=Duel.GetMatchingGroup(c100302024.thfilter,tp,LOCATION_DECK,0,nil)
+	if g:GetCount()>0 and Duel.SelectYesNo(tp,aux.Stringid(100302024,0)) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+		local sg=g:Select(tp,1,1,nil)
+		Duel.SendtoHand(sg,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,sg)
+	end
+end
+function c100302024.tgfilter(c,e,tp)
+	return c:IsRace(RACE_MACHINE) and c:IsAttribute(ATTRIBUTE_LIGHT) and c:IsType(TYPE_UNION)
+		and c:IsFaceup() and c:IsControler(tp) and c:IsCanBeEffectTarget(e)
+		and Duel.IsExistingTarget(c100302024.cfilter,tp,LOCATION_DECK,0,1,nil,c)
+end
+function c100302024.cfilter(c,ec)
+	return c:IsRace(RACE_MACHINE) and c:IsAttribute(ATTRIBUTE_LIGHT)
+		and c:IsType(TYPE_UNION) and c:CheckEquipTarget(ec) and not c:IsCode(ec:GetCode())
+end
+function c100302024.eqtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsRace(RACE_MACHINE) and chkc:IsAttribute(ATTRIBUTE_LIGHT)
+		and chkc:IsType(TYPE_UNION) and chkc:IsFaceup() and chkc:IsControler(tp) end
+	local g=eg:Filter(c100302024.tgfilter,nil,e,tp)
+	if chk==0 then return g:GetCount()>0 and Duel.GetLocationCount(tp,LOCATION_SZONE)>0 end
+	if g:GetCount()==1 then
+		Duel.SetTargetCard(g:GetFirst())
+	else
+		local tc=g:Select(tp,1,1,nil)
+		Duel.SetTargetCard(tc)
+	end
+end
+function c100302024.eqop(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetHandler():IsRelateToEffect(e) then return end
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) and tc:IsFaceup() then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)
+		local sg=Duel.SelectMatchingCard(tp,c100302024.cfilter,tp,LOCATION_DECK,0,1,1,nil,tc)
+		local ec=sg:GetFirst()
+		if ec and Duel.Equip(tp,ec,tc) then
+			ec:SetStatus(STATUS_UNION,true)
+			local e1=Effect.CreateEffect(e:GetHandler())
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE+EFFECT_FLAG_CANNOT_DISABLE)
+			e1:SetRange(LOCATION_SZONE)
+			e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+			e1:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END)
+			ec:RegisterEffect(e1)
+		end
+	end
+end
