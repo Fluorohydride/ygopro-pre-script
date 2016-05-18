2eed4885d0e21f7c00af5033ede6a723c9454c59
mercury233 <me@mercury233.me>
2016-05-18 20:08:28 +0800

    add DP17-JP009 Tribute Burial

---
+++ scripts/DP17-JP/c100417009.lua
@@ -0,0 +1,59 @@
+--埋葬されし生け贄
+--Tribute Burial
+--Script by mercury233
+function c100417009.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetOperation(c100417009.activate)
+	c:RegisterEffect(e1)
+end
+function c100417009.activate(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(100417009,0))
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_SUMMON_PROC)
+	e1:SetTargetRange(LOCATION_HAND,0)
+	e1:SetProperty(EFFECT_FLAG_SET_AVAILABLE)
+	e1:SetCondition(c100417009.otcon)
+	e1:SetTarget(c100417009.ottg)
+	e1:SetOperation(c100417009.otop)
+	e1:SetCountLimit(1)
+	e1:SetReset(RESET_PHASE+PHASE_END)
+	Duel.RegisterEffect(e1,tp)
+	local e2=e1:Clone()
+	e2:SetCode(EFFECT_SET_PROC)
+	Duel.RegisterEffect(e2,tp)
+	--
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e3:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+	e3:SetReset(RESET_PHASE+PHASE_END)
+	e3:SetTargetRange(1,0)
+	Duel.RegisterEffect(e3,tp)
+end
+function c100417009.rmfilter(c)
+	return c:IsType(TYPE_MONSTER) and c:IsAbleToRemoveAsCost()
+end
+function c100417009.otcon(e,c)
+	if c==nil then return true end
+	local tp=c:GetControler()
+	return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(c100417009.rmfilter,tp,LOCATION_GRAVE,0,1,nil)
+		and Duel.IsExistingMatchingCard(c100417009.rmfilter,tp,0,LOCATION_GRAVE,1,nil)
+end
+function c100417009.ottg(e,c)
+	local mi,ma=c:GetTributeRequirement()
+	return mi==2 or ma==2
+end
+function c100417009.otop(e,tp,eg,ep,ev,re,r,rp,c)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local g1=Duel.SelectMatchingCard(tp,c100417009.rmfilter,tp,LOCATION_GRAVE,0,1,1,nil)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local g2=Duel.SelectMatchingCard(tp,c100417009.rmfilter,tp,0,LOCATION_GRAVE,1,1,nil)
+	g1:Merge(g2)
+	Duel.Remove(g1,POS_FACEUP,REASON_COST)
+end
