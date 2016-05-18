d79dab55c695b9f166fbca6710ed81ad0597b308
nekrozar <unyuho.buster@gmail.com>
2016-05-18 17:02:14 +0900

    fix

--- scripts/INOV-JP/c100910038.lua
+++ scripts/INOV-JP/c100910038.lua
@@ -29,7 +29,7 @@ function c100910038.initial_effect(c)
 	c:RegisterEffect(e2)
 	--destroy
 	local e3=Effect.CreateEffect(c)
-	e3SetDescription(aux.Stringid(100910038,3))
+	e3:SetDescription(aux.Stringid(100910038,3))
 	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e3:SetProperty(EFFECT_FLAG_DELAY)
