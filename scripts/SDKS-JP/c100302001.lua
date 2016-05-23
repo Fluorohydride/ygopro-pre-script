7e2be8f6658cb800835409840b39a343f80c7b9e
nekrozar <unyuho.buster@gmail.com>
2016-05-23 18:48:47 +0900

    fix
    https://github.com/Fluorohydride/ygopro-scripts/pull/421

--- scripts/SDKS-JP/c100302001.lua
+++ scripts/SDKS-JP/c100302001.lua
@@ -90,8 +90,10 @@ function c100302001.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 end
 function c100302001.spop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	if c:IsRelateToEffect(e) then
-		Duel.SpecialSummon(c,0,tp,tp,true,false,POS_FACEUP)
+	if not c:IsRelateToEffect(e) then return end
+	if Duel.SpecialSummon(c,0,tp,tp,true,false,POS_FACEUP)==0 and Duel.GetLocationCount(tp,LOCATION_MZONE)<=0
+		and c:IsCanBeSpecialSummoned(e,0,tp,true,false) then
+		Duel.SendtoGrave(c,REASON_RULE)
 	end
 end
 function c100302001.repval(e,re,r,rp)
