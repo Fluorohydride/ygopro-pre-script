5c46caa1adae6da3f47ccfb985b7881dbd1c2388
mercury233 <me@mercury233.me>
2016-06-18 11:09:33 +0800

    add SPDS-JP018, 024

---
+++ scripts/SPDS-JP/c100405018.lua
@@ -0,0 +1,102 @@
+--魔界劇団－ビッグ・スター
+--Abyss Actor - Superstar
+--Script by mercury233
+function c100405018.initial_effect(c)
+	--pendulum summon
+	aux.EnablePendulumAttribute(c)
+	--to hand
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(100405018,0))
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetRange(LOCATION_PZONE)
+	e1:SetCountLimit(1)
+	e1:SetCost(c100405018.thcost)
+	e1:SetTarget(c100405018.thtg)
+	e1:SetOperation(c100405018.thop)
+	c:RegisterEffect(e1)
+	--act limit
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e2:SetCode(EVENT_SUMMON_SUCCESS)
+	e2:SetOperation(c100405018.limop)
+	c:RegisterEffect(e2)
+	local e3=e2:Clone()
+	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
+	c:RegisterEffect(e3)
+	--set
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_IGNITION)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetCountLimit(1)
+	e4:SetTarget(c100405018.settg)
+	e4:SetOperation(c100405018.setop)
+	c:RegisterEffect(e4)
+end
+function c100405018.thcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.CheckReleaseGroup(tp,Card.IsSetCard,1,nil,0x11ed) end
+	local g=Duel.SelectReleaseGroup(tp,Card.IsSetCard,1,1,nil,0x11ed)
+	Duel.Release(g,REASON_COST)
+end
+function c100405018.thfilter(c)
+	return c:IsSetCard(0x21ed) and c:IsType(TYPE_SPELL) and c:IsAbleToHand()
+end
+function c100405018.thtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and c100405018.thfilter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(c100405018.thfilter,tp,LOCATION_GRAVE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectTarget(tp,c100405018.thfilter,tp,LOCATION_GRAVE,0,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,0,0)
+end
+function c100405018.thop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) then
+		Duel.SendtoHand(tc,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,tc)
+	end
+end
+function c100405018.limop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.SetChainLimitTillChainEnd(c100405018.chlimit)
+end
+function c100405018.chlimit(e,rp,tp)
+	return tp==rp and e:IsActiveType(TYPE_SPELL+TYPE_TRAP)
+end
+function c100405018.cfilter(c)
+	return c:IsSetCard(0x21ed) and c:IsType(TYPE_SPELL) and c:IsSSetable(true)
+end
+function c100405018.settg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_SZONE)>-1
+		and Duel.IsExistingMatchingCard(c100405018.cfilter,tp,LOCATION_DECK,0,1,nil) end
+end
+function c100405018.setop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_SZONE)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SET)
+	local g=Duel.SelectMatchingCard(tp,c100405018.cfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if g:GetCount()>0 then
+		local c=e:GetHandler()
+		local tc=g:GetFirst()
+		local fid=c:GetFieldID()
+		Duel.SSet(tp,tc)
+		tc:RegisterFlagEffect(100405018,RESET_EVENT+0x1fe0000,0,1,fid)
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		e1:SetCode(EVENT_PHASE+PHASE_END)
+		e1:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
+		e1:SetCountLimit(1)
+		e1:SetLabel(fid)
+		e1:SetLabelObject(tc)
+		e1:SetCondition(c100405018.tgcon)
+		e1:SetOperation(c100405018.tgop)
+		e1:SetReset(RESET_PHASE+PHASE_END)
+		Duel.RegisterEffect(e1,tp)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end
+function c100405018.tgcon(e,tp,eg,ep,ev,re,r,rp)
+	local tc=e:GetLabelObject()
+	return tc:GetFlagEffectLabel(100405018)==e:GetLabel()
+end
+function c100405018.tgop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=e:GetLabelObject()
+	Duel.SendtoGrave(tc,REASON_EFFECT)
+end
