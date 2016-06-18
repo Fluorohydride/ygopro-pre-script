3cfb95d5f6565469abd725da2b2e8080650e81ef
mercury233 <me@mercury233.me>
2016-06-18 15:42:00 +0800

    fix Spark Field

--- scripts/INOV-JP/c100910059.lua
+++ scripts/INOV-JP/c100910059.lua
@@ -62,49 +62,32 @@ function c100910059.desop(e,tp,eg,ep,ev,re,r,rp)
 	if not c:IsRelateToEffect(e) then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
 	local g=Duel.SelectMatchingCard(tp,c100910059.rmfilter,tp,LOCATION_MZONE,0,1,1,nil)
-	local tg=g:GetFirst()
-	if g:GetCount()>0 and Duel.Remove(g,0,REASON_EFFECT+REASON_TEMPORARY)~=0 and tg:IsLocation(LOCATION_REMOVED) then
+	local rc=g:GetFirst()
+	if rc and Duel.Remove(rc,0,REASON_EFFECT+REASON_TEMPORARY)~=0 and rc:IsLocation(LOCATION_REMOVED) then
 		local tc=Duel.GetFirstTarget()
 		if tc:IsRelateToEffect(e) then
 			Duel.Destroy(tc,REASON_EFFECT)
 		end
-		local fid=c:GetFieldID()
-		tg:RegisterFlagEffect(100910059,RESET_PHASE+PHASE_END+RESET_OPPO_TURN,0,1,fid)
+		rc:RegisterFlagEffect(100910059,RESET_PHASE+PHASE_END+RESET_OPPO_TURN,0,1)
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 		e1:SetCode(EVENT_PHASE+PHASE_END)
 		e1:SetCountLimit(1)
-		e1:SetLabel(fid)
-		e1:SetLabelObject(tg)
+		e1:SetLabelObject(rc)
 		e1:SetCondition(c100910059.retcon)
 		e1:SetOperation(c100910059.retop)
 		e1:SetReset(RESET_PHASE+PHASE_END+RESET_OPPO_TURN)
 		Duel.RegisterEffect(e1,tp)
 	end
 end
-function c100910059.retfilter(c,fid)
-	return c:GetFlagEffectLabel(100910059)==fid
-end
 function c100910059.retcon(e,tp,eg,ep,ev,re,r,rp)
-	if Duel.GetTurnPlayer()==tp then return false end
-	local g=e:GetLabelObject()
-	if not g:IsExists(c100910059.retfilter,1,nil,e:GetLabel()) then
-		g:DeleteGroup()
-		e:Reset()
-		return false
-	else return true end
+	return Duel.GetTurnPlayer()==1-tp and e:GetLabelObject():GetFlagEffect(100910059)~=0
 end
 function c100910059.retop(e,tp,eg,ep,ev,re,r,rp)
-	local g=e:GetLabelObject()
-	local sg=g:Filter(c100910059.retfilter,nil,e:GetLabel())
-	g:DeleteGroup()
-	local tc=sg:GetFirst()
-	while tc do
-		if tc:IsForbidden() then 
-			Duel.SendtoGrave(tc,REASON_RULE)
-		else
-			Duel.ReturnToField(tc)
-		end
-		tc=sg:GetNext()
+	local tc=e:GetLabelObject()
+	if tc:IsForbidden() then
+		Duel.SendtoGrave(tc,REASON_RULE)
+	else
+		Duel.ReturnToField(tc)
 	end
 end
