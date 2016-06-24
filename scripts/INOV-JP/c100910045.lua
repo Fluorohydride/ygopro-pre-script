a614ad80165727a8948b41019b78b7cc73cd375b
mercury233 <me@mercury233.me>
2016-06-25 00:44:45 +0800

    fix

--- scripts/INOV-JP/c100910045.lua
+++ scripts/INOV-JP/c100910045.lua
@@ -2,6 +2,9 @@
 --Crystron Ametrix
 --Script by nekrozar
 function c100910045.initial_effect(c)
+	--synchro summon
+	aux.AddSynchroProcedure(c,nil,aux.NonTuner(nil),1)
+	c:EnableReviveLimit()
 	--position
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(100910045,0))
