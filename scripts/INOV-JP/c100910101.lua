0c559243f2c30b5bf3e62007530d9a8b0c5fb762
nekrozar <unyuho.buster@gmail.com>
2016-06-25 09:08:36 +0900

    add script INOV

---
+++ scripts/INOV-JP/c100910101.lua
@@ -0,0 +1,44 @@
+--竜星の九支
+--Script by nekrozar
+function c100910101.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_NEGATE+CATEGORY_TODECK+CATEGORY_DESTROY)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_CHAINING)
+	e1:SetCondition(c100910101.condition)
+	e1:SetTarget(c100910101.target)
+	e1:SetOperation(c100910101.activate)
+	c:RegisterEffect(e1)
+end
+function c100910101.cfilter(c)
+	return c:IsFaceup() and c:IsSetCard(0x9e)
+end
+function c100910101.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsExistingMatchingCard(c100910101.cfilter,tp,LOCATION_ONFIELD,0,1,nil)
+		and (re:IsActiveType(TYPE_MONSTER) or re:IsHasType(EFFECT_TYPE_ACTIVATE)) and Duel.IsChainNegatable(ev)
+end
+function c100910101.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
+	if re:GetHandler():IsRelateToEffect(re) then
+		Duel.SetOperationInfo(0,CATEGORY_TODECK,eg,1,0,0)
+	end
+end
+function c100910101.desfilter(c)
+	return c:IsFaceup() and c:IsSetCard(0x9e) and c:IsDestructable()
+end
+function c100910101.activate(e,tp,eg,ep,ev,re,r,rp)
+	local ec=re:GetHandler()
+	Duel.NegateActivation(ev)
+	if ec:IsRelateToEffect(re) then
+		ec:CancelToGrave()
+		local g=Duel.GetMatchingGroup(c100910101.desfilter,tp,LOCATION_ONFIELD,0,e:GetHandler())
+		if Duel.SendtoDeck(ec,nil,2,REASON_EFFECT)~=0 and ec:IsLocation(LOCATION_DECK+LOCATION_EXTRA)
+			and g:GetCount()>0 then
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+			local sg=g:Select(tp,1,1,nil)
+			Duel.Destroy(sg,REASON_EFFECT)
+		end
+	end
+end
