62b633f05f6d5bdbd1ea6d3c671a230b5af32a9f
mercury233 <me@mercury233.me>
2016-06-26 21:35:59 +0800

    change id

--- scripts/AGMM-JP/c82103466.lua
+++ scripts/AGMM-JP/c82103466.lua
@@ -1,7 +1,5 @@
 --蛇神ゲー
---Divine Serpent Geh
---Script by mercury233
-function c100299001.initial_effect(c)
+function c82103466.initial_effect(c)
 	c:EnableReviveLimit()
 	--connot special summon
 	local e0=Effect.CreateEffect(c)
@@ -11,16 +9,16 @@ function c100299001.initial_effect(c)
 	c:RegisterEffect(e0)
 	--special summon
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(100299001,0))
+	e1:SetDescription(aux.Stringid(82103466,0))
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY+EFFECT_FLAG_CHAIN_UNIQUE)
 	e1:SetCode(EVENT_DESTROYED)
 	e1:SetRange(LOCATION_HAND)
-	e1:SetCondition(c100299001.spcon)
-	e1:SetCost(c100299001.spcost)
-	e1:SetTarget(c100299001.sptg)
-	e1:SetOperation(c100299001.spop)
+	e1:SetCondition(c82103466.spcon)
+	e1:SetCost(c82103466.spcost)
+	e1:SetTarget(c82103466.sptg)
+	e1:SetOperation(c82103466.spop)
 	c:RegisterEffect(e1)
 	--cannot be target
 	local e2=Effect.CreateEffect(c)
@@ -36,15 +34,15 @@ function c100299001.initial_effect(c)
 	e3:SetCode(EFFECT_DISABLE)
 	e3:SetRange(LOCATION_MZONE)
 	e3:SetTargetRange(0,LOCATION_MZONE)
-	e3:SetCondition(c100299001.adcon)
-	e3:SetTarget(c100299001.adtg)
+	e3:SetCondition(c82103466.adcon)
+	e3:SetTarget(c82103466.adtg)
 	c:RegisterEffect(e3)
 	local e4=e3:Clone()
 	e4:SetCode(EFFECT_DISABLE_EFFECT)
 	c:RegisterEffect(e4)
 	local e5=e3:Clone()
 	e5:SetCode(EFFECT_SET_ATTACK_FINAL)
-	e5:SetValue(c100299001.atkval)
+	e5:SetValue(c82103466.atkval)
 	c:RegisterEffect(e5)
 	--atk up
 	local e6=Effect.CreateEffect(c)
@@ -52,29 +50,29 @@ function c100299001.initial_effect(c)
 	e6:SetType(EFFECT_TYPE_QUICK_O)
 	e6:SetRange(LOCATION_MZONE)
 	e6:SetCode(EVENT_PRE_DAMAGE_CALCULATE)
-	e6:SetCondition(c100299001.atkcon)
-	e6:SetTarget(c100299001.atktg)
-	e6:SetOperation(c100299001.atkop)
+	e6:SetCondition(c82103466.atkcon)
+	e6:SetTarget(c82103466.atktg)
+	e6:SetOperation(c82103466.atkop)
 	c:RegisterEffect(e6)
 end
-function c100299001.cfilter(c,tp)
+function c82103466.cfilter(c,tp)
 	return c:GetPreviousControler()==tp and c:IsPreviousLocation(LOCATION_MZONE)
 		and ((c:IsReason(REASON_BATTLE) and Duel.GetAttacker():IsControler(1-tp))
 		or (c:IsReason(REASON_EFFECT) and c:GetReasonPlayer()==1-tp))
 end
-function c100299001.spcon(e,tp,eg,ep,ev,re,r,rp)
-	return eg:IsExists(c100299001.cfilter,1,nil,tp)
+function c82103466.spcon(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(c82103466.cfilter,1,nil,tp)
 end
-function c100299001.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+function c82103466.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
 	Duel.PayLPCost(tp,math.floor(Duel.GetLP(tp)/2))
 end
-function c100299001.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+function c82103466.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
 		and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,true,false) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
 end
-function c100299001.spop(e,tp,eg,ep,ev,re,r,rp)
+function c82103466.spop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if not c:IsRelateToEffect(e) then return end
 	if Duel.SpecialSummon(c,0,tp,tp,true,false,POS_FACEUP)~=0 then
@@ -84,32 +82,32 @@ function c100299001.spop(e,tp,eg,ep,ev,re,r,rp)
 		Duel.SendtoGrave(c,REASON_RULE)
 	end
 end
-function c100299001.adcon(e)
+function c82103466.adcon(e)
 	local c=e:GetHandler()
 	return Duel.GetAttacker()==c and c:GetBattleTarget()
 		and (Duel.GetCurrentPhase()==PHASE_DAMAGE or Duel.GetCurrentPhase()==PHASE_DAMAGE_CAL)
 end
-function c100299001.adtg(e,c)
+function c82103466.adtg(e,c)
 	return c==e:GetHandler():GetBattleTarget()
 end
-function c100299001.atkval(e,c)
+function c82103466.atkval(e,c)
 	return math.ceil(c:GetBaseAttack()/2)
 end
-function c100299001.atkcon(e,tp,eg,ep,ev,re,r,rp)
+function c82103466.atkcon(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	return c==Duel.GetAttacker() or c==Duel.GetAttackTarget()
 end
-function c100299001.atktg(e,tp,eg,ep,ev,re,r,rp,chk)
+function c82103466.atktg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
 	if chk==0 then
 		local g=Duel.GetMatchingGroup(Card.IsFaceup,tp,LOCATION_MZONE,LOCATION_MZONE,c)
 		if g:GetCount()==0 then return false end
 		local g1,atk=g:GetMaxGroup(Card.GetBaseAttack)
-		return c:GetAttack()~=atk and c:GetFlagEffect(100299001)==0
+		return c:GetAttack()~=atk and c:GetFlagEffect(82103466)==0
 	end
-	c:RegisterFlagEffect(100299001,RESET_CHAIN,0,1)
+	c:RegisterFlagEffect(82103466,RESET_CHAIN,0,1)
 end
-function c100299001.atkop(e,tp,eg,ep,ev,re,r,rp)
+function c82103466.atkop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local g=Duel.GetMatchingGroup(Card.IsFaceup,tp,LOCATION_MZONE,LOCATION_MZONE,c)
 	if g:GetCount()==0 then return end
