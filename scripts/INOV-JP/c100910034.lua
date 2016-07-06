3e52ae5d98d49e0425272b017c90ba3bb3180651
Mercury233 <me@mercury233.me>
2016-07-06 22:18:26 +0800

    fix

--- scripts/INOV-JP/c100910034.lua
+++ scripts/INOV-JP/c100910034.lua
@@ -44,7 +44,7 @@ function c100910034.penop(e,tp,eg,ep,ev,re,r,rp)
 	if not c:IsRelateToEffect(e) then return end
 	local tc=Duel.GetFirstTarget()
 	if tc:IsRelateToEffect(e) and Duel.Destroy(tc,REASON_EFFECT)~=0 then
-		Duel.MoveToField(e:GetHandler(),1-tp,tp,LOCATION_SZONE,POS_FACEUP,true)
+		Duel.MoveToField(c,tp,1-tp,LOCATION_SZONE,POS_FACEUP,true)
 	end
 end
 function c100910034.drcon(e)
