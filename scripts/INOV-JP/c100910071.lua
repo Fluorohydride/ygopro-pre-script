c93fda966cd67dc2fe94c625c09de1d5cf849183
mercury233 <me@mercury233.me>
2016-07-07 16:56:35 +0800

    fix Crystron Entry

--- scripts/INOV-JP/c100910071.lua
+++ scripts/INOV-JP/c100910071.lua
@@ -77,7 +77,7 @@ function c100910071.lvop(e,tp,eg,ep,ev,re,r,rp)
 			e1:SetType(EFFECT_TYPE_SINGLE)
 			e1:SetCode(EFFECT_CHANGE_LEVEL)
 			e1:SetValue(gc:GetLevel())
-			e1:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END)
+			e1:SetReset(RESET_EVENT+0x1fe0000)
 			tc:RegisterEffect(e1)
 		end
 	end
