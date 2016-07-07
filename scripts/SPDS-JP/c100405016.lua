aa3d0f0d86076297618ba344f82f49d60c3f92af
mercury233 <me@mercury233.me>
2016-07-07 17:46:00 +0800

    add SPDS-JP016 Abyss Actor - Evil Heel

---
+++ scripts/SPDS-JP/c100405016.lua
@@ -0,0 +1,99 @@
+--魔界劇団－デビル・ヒール
+--Abyss Actor - Evil Heel
+--Script by mercury233
+function c100405016.initial_effect(c)
+	--pendulum summon
+	aux.EnablePendulumAttribute(c)
+	--atk down (p zone)
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(100405016,0))
+	e1:SetCategory(CATEGORY_ATKCHANGE)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetRange(LOCATION_PZONE)
+	e1:SetCountLimit(1)
+	e1:SetCost(c100405016.atkcost1)
+	e1:SetTarget(c100405016.atktg)
+	e1:SetOperation(c100405016.atkop1)
+	c:RegisterEffect(e1)
+	--atk down (summon)
+	local e2=Effect.CreateEffect(c)
+	e2:SetCategory(CATEGORY_ATKCHANGE)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_SUMMON_SUCCESS)
+	e2:SetCondition(c100405016.atkcon2)
+	e2:SetTarget(c100405016.atktg)
+	e2:SetOperation(c100405016.atkop2)
+	c:RegisterEffect(e2)
+	local e3=e2:Clone()
+	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
+	c:RegisterEffect(e3)
+	--set
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e4:SetCode(EVENT_BATTLE_DESTROYING)
+	e4:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e4:SetCondition(aux.bdocon)
+	e4:SetTarget(c100405016.settg)
+	e4:SetOperation(c100405016.setop)
+	c:RegisterEffect(e4)
+end
+function c100405016.atkcost1(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.CheckReleaseGroup(tp,Card.IsSetCard,1,nil,0x11ed) end
+	local g=Duel.SelectReleaseGroup(tp,Card.IsSetCard,1,1,nil,0x11ed)
+	e:SetLabel(g:GetFirst():GetBaseAttack())
+	Duel.Release(g,REASON_COST)
+end
+function c100405016.atktg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) and chkc:IsFaceup() end
+	if chk==0 then return Duel.IsExistingTarget(Card.IsFaceup,tp,0,LOCATION_MZONE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	local g=Duel.SelectTarget(tp,Card.IsFaceup,tp,0,LOCATION_MZONE,1,1,nil)
+end
+function c100405016.atkop1(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tc=Duel.GetFirstTarget()
+	if c:IsRelateToEffect(e) and tc:IsFaceup() and tc:IsRelateToEffect(e) then
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetValue(-e:GetLabel())
+		e1:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END)
+		tc:RegisterEffect(e1)
+	end
+end
+function c100405016.atkfilter(c)
+	return c:IsSetCard(0x11ed) and c:IsFaceup()
+end
+function c100405016.atkcon2(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetMatchingGroupCount(c100405016.atkfilter,tp,LOCATION_MZONE,0,nil)>0
+end
+function c100405016.atkop2(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tc=Duel.GetFirstTarget()
+	if tc:IsFaceup() and tc:IsRelateToEffect(e) then
+		local atkval=Duel.GetMatchingGroupCount(c100405016.atkfilter,tp,LOCATION_MZONE,0,nil)*1000
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetValue(-atkval)
+		e1:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END)
+		tc:RegisterEffect(e1)
+	end
+end
+function c100405016.cfilter(c)
+	return c:IsSetCard(0x21ed) and c:IsType(TYPE_SPELL) and c:IsSSetable(true)
+end
+function c100405016.settg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_SZONE)>0
+		and Duel.IsExistingMatchingCard(c100405016.cfilter,tp,LOCATION_GRAVE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SET)
+	local g=Duel.SelectTarget(tp,c100405016.cfilter,tp,LOCATION_GRAVE,0,1,1,nil)
+end
+function c100405016.setop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) and Duel.GetLocationCount(tp,LOCATION_SZONE)>0 then
+		Duel.SSet(tp,tc)
+		Duel.ConfirmCards(1-tp,tc)
+	end
+end
