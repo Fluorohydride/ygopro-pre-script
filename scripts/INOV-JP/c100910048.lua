759030a3c43b80adaf9a961be6414d327373d802
mercury233 <me@mercury233.me>
2016-07-08 11:47:17 +0800

    update Denglong, Origin of the Yang Zing

--- scripts/INOV-JP/c100910048.lua
+++ scripts/INOV-JP/c100910048.lua
@@ -13,8 +13,8 @@ function c100910048.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
 	e1:SetProperty(EFFECT_FLAG_DELAY)
-	e1:SetTarget(c100910048.thtg1)
-	e1:SetOperation(c100910048.tgop1)
+	e1:SetTarget(c100910048.thtg)
+	e1:SetOperation(c100910048.thop)
 	c:RegisterEffect(e1)
 	--level
 	local e2=Effect.CreateEffect(c)
@@ -25,28 +25,28 @@ function c100910048.initial_effect(c)
 	e2:SetCost(c100910048.lvcost)
 	e2:SetOperation(c100910048.lvop)
 	c:RegisterEffect(e2)
-	--search
+	--spsummon
 	local e3=Effect.CreateEffect(c)
 	e3:SetDescription(aux.Stringid(100910048,2))
-	e3:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e3:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_DAMAGE_STEP)
 	e3:SetCode(EVENT_LEAVE_FIELD)
-	e3:SetCondition(c100910048.thcon)
-	e3:SetTarget(c100910048.thtg2)
-	e3:SetOperation(c100910048.thop2)
+	e3:SetCondition(c100910048.spcon)
+	e3:SetTarget(c100910048.sptg)
+	e3:SetOperation(c100910048.spop)
 	c:RegisterEffect(e3)
 end
-function c100910048.thfilter1(c)
+function c100910048.thfilter(c)
 	return c:IsSetCard(0x9e) and c:IsAbleToHand()
 end
-function c100910048.thtg1(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(c100910048.thfilter1,tp,LOCATION_DECK,0,1,nil) end
+function c100910048.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c100910048.thfilter,tp,LOCATION_DECK,0,1,nil) end
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
 end
-function c100910048.tgop1(e,tp,eg,ep,ev,re,r,rp)
+function c100910048.thop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-	local g=Duel.SelectMatchingCard(tp,c100910048.thfilter1,tp,LOCATION_DECK,0,1,1,nil)
+	local g=Duel.SelectMatchingCard(tp,c100910048.thfilter,tp,LOCATION_DECK,0,1,1,nil)
 	if g:GetCount()>0 then
 		Duel.SendtoHand(g,nil,REASON_EFFECT)
 		Duel.ConfirmCards(1-tp,g)
@@ -76,22 +76,24 @@ function c100910048.lvop(e,tp,eg,ep,ev,re,r,rp)
 		c:RegisterEffect(e1)
 	end
 end
-function c100910048.thcon(e,tp,eg,ep,ev,re,r,rp)
+function c100910048.spcon(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	return c:IsPreviousPosition(POS_FACEUP) and c:GetLocation()~=LOCATION_DECK
 end
-function c100910048.thfilter2(c)
-	return c:IsSetCard(0x9e) and c:IsType(TYPE_MONSTER) and c:IsAbleToHand()
+function c100910048.spfilter(c,e,tp)
+	return c:IsSetCard(0x9e) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
-function c100910048.thtg2(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(c100910048.thfilter2,tp,LOCATION_DECK,0,1,nil) end
-	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+function c100910048.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(c100910048.spfilter,tp,LOCATION_DECK,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
 end
-function c100910048.thop2(e,tp,eg,ep,ev,re,r,rp)
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-	local g=Duel.SelectMatchingCard(tp,c100910048.thfilter2,tp,LOCATION_DECK,0,1,1,nil)
-	if g:GetCount()>0 then
-		Duel.SendtoHand(g,nil,REASON_EFFECT)
-		Duel.ConfirmCards(1-tp,g)
+function c100910048.spop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,c100910048.spfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
+	local tc=g:GetFirst()
+	if tc then
+		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
 	end
 end
