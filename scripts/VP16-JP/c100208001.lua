81d655f04e09b7ffcf544b00e62d418c81fa6515
mercury233 <me@mercury233.me>
2016-07-14 00:50:07 +0800

    fix Destiny HERO - Deadlyguy
    Stellarknight Constellar Diamond

--- scripts/VP16-JP/c100208001.lua
+++ scripts/VP16-JP/c100208001.lua
@@ -14,7 +14,7 @@ function c100208001.initial_effect(c)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
 	e1:SetCountLimit(1,100208001)
-	e1:SetHintTiming(TIMING_DAMAGE_STEP)
+	e1:SetHintTiming(TIMING_DAMAGE_STEP,TIMING_DAMAGE_STEP+TIMING_END_PHASE)
 	e1:SetCondition(c100208001.atkcon)
 	e1:SetCost(c100208001.atcost)
 	e1:SetTarget(c100208001.attg)
@@ -27,20 +27,24 @@ end
 function c100208001.atkcon(e,tp,eg,ep,ev,re,r,rp)
 	return Duel.GetCurrentPhase()~=PHASE_DAMAGE or not Duel.IsDamageCalculated()
 end
+function c100208001.tgfilter(c)
+	return c:IsSetCard(0xc008) and c:IsType(TYPE_MONSTER) and c:IsAbleToGrave()
+end
 function c100208001.atcost(e,tp,eg,ep,ev,re,r,rp,chk)
-	local g=Duel.GetMatchingGroup(Card.IsSetCard,tp,LOCATION_HAND+LOCATION_DECK,0,nil,0xc008)
-	if g:GetCount()==1 then
-		if chk==0 then return Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,1,g:GetFirst()) end
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DISCARD)
-		local tg=Duel.SelectMatchingCard(tp,Card.IsDiscardable,tp,LOCATION_HAND,0,1,1,g:GetFirst())
-		Duel.SendtoGrave(tg,REASON_COST+REASON_DISCARD)
-	else
-		if chk==0 then return Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,1,nil) end
-		Duel.DiscardHand(tp,Card.IsDiscardable,1,1,REASON_COST+REASON_DISCARD)
-	end
+	local exc
+	local g=Duel.GetMatchingGroup(c100208001.tgfilter,tp,LOCATION_HAND+LOCATION_DECK,0,nil)
+	if g:GetCount()==1 then exc=g:GetFirst() end
+	if chk==0 then return g:GetCount()>0
+		and Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,1,exc) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DISCARD)
+	local tg=Duel.SelectMatchingCard(tp,Card.IsDiscardable,tp,LOCATION_HAND,0,1,1,exc)
+	Duel.SendtoGrave(tg,REASON_COST+REASON_DISCARD)
 end
 function c100208001.atfilter(c)
 	return c:IsSetCard(0xc008) and c:IsFaceup()
+end
+function c100208001.cfilter(c)
+	return c:IsSetCard(0xc008) and c:IsType(TYPE_MONSTER)
 end
 function c100208001.atktg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(c100208001.atfilter,tp,LOCATION_MZONE,0,1,nil) end
@@ -48,12 +52,12 @@ function c100208001.atktg(e,tp,eg,ep,ev,re,r,rp,chk)
 end
 function c100208001.atkop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
-	local g=Duel.SelectMatchingCard(tp,Card.IsSetCard,tp,LOCATION_HAND+LOCATION_DECK,0,1,1,nil,0xc008)
+	local g=Duel.SelectMatchingCard(tp,c100208001.tgfilter,tp,LOCATION_HAND+LOCATION_DECK,0,1,1,nil)
 	local tc=g:GetFirst()
 	if tc and Duel.SendtoGrave(tc,REASON_EFFECT)~=0 and tc:IsLocation(LOCATION_GRAVE) then
-		local tg=Duel.GetMatchingGroup(c100208001.atfilter,tp,LOCATION_MZONE,0,nil,0xc008)
+		local tg=Duel.GetMatchingGroup(c100208001.atfilter,tp,LOCATION_MZONE,0,nil)
 		if tg:GetCount()<=0 then return end
-		local sg=Duel.GetMatchingGroupCount(Card.IsSetCard,tp,LOCATION_GRAVE,0,nil,0xc008)
+		local sg=Duel.GetMatchingGroupCount(c100208001.cfilter,tp,LOCATION_GRAVE,0,nil)
 		tc=tg:GetFirst()
 		while tc do
 			local e1=Effect.CreateEffect(e:GetHandler())
