10fa4c3f2d33bb576267d6a0472a4586a98ffc45
mercury233 <me@mercury233.me>
2016-07-18 01:40:52 +0800

    fix Abyss Actor - Funky Comedian

--- scripts/SPDS-JP/c100405017.lua
+++ scripts/SPDS-JP/c100405017.lua
@@ -76,7 +76,7 @@ end
 function c100405017.atkop2(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if c:IsFaceup() and c:IsRelateToEffect(e) then
-		local atkval=Duel.GetMatchingGroupCount(c100405017.atkfilter,tp,LOCATION_MZONE,0,nil)*300
+		local atkval=Duel.GetMatchingGroupCount(c100405017.atkfilter2,tp,LOCATION_MZONE,0,nil)*300
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_UPDATE_ATTACK)
