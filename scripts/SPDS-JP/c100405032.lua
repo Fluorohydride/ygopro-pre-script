baab0c118f3d67658a7d5996b9dc657e848c5f9f
mercury233 <me@mercury233.me>
2016-07-21 01:50:18 +0800

    add SPDS-JP032 Darklord Mastema

---
+++ scripts/SPDS-JP/c100405032.lua
@@ -0,0 +1,86 @@
+--堕天使マスティマ
+--Darklord Mastema
+--Script by mercury233
+function c100405032.initial_effect(c)
+	c:SetSPSummonOnce(100405032)
+	--spsummon
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(100405032,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCost(c100405032.spcost)
+	e1:SetTarget(c100405032.sptg)
+	e1:SetOperation(c100405032.spop)
+	c:RegisterEffect(e1)
+	--copy effect
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(100405032,1))
+	e2:SetCategory(CATEGORY_TODECK)
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetCountLimit(1,100405032)
+	e2:SetCost(c100405032.cpcost)
+	e2:SetTarget(c100405032.cptg)
+	e2:SetOperation(c100405032.cpop)
+	c:RegisterEffect(e2)
+end
+function c100405032.cfilter(c)
+	return (c:IsSetCard(0x1ef) or c:IsCode(67316075,57579381,47664723,85771019,11260714,40921744,55690251)) and c:IsDiscardable()
+end
+function c100405032.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c100405032.cfilter,tp,LOCATION_HAND,0,2,e:GetHandler()) end
+	Duel.DiscardHand(tp,c100405032.cfilter,2,2,REASON_COST+REASON_DISCARD,e:GetHandler())
+end
+function c100405032.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
+end
+function c100405032.spop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not c:IsRelateToEffect(e) then return end
+	if Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)==0 and Duel.GetLocationCount(tp,LOCATION_MZONE)<=0
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false) then
+		Duel.SendtoGrave(c,REASON_RULE)
+	end
+end
+function c100405032.cpcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.CheckLPCost(tp,1000) end
+	Duel.PayLPCost(tp,1000)
+end
+function c100405032.cpfilter(c)
+	return (c:IsSetCard(0x1ef) or c:IsCode(67316075,57579381,47664723,85771019,11260714,40921744,55690251)) and c:IsType(TYPE_SPELL+TYPE_TRAP) and c:IsAbleToDeck() and c:CheckActivateEffect(false,true,false)~=nil
+end
+function c100405032.cptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then
+		local te=e:GetLabelObject()
+		local tg=te:GetTarget()
+		return tg and tg(e,tp,eg,ep,ev,re,r,rp,0,chkc)
+	end
+	if chk==0 then return Duel.IsExistingTarget(c100405032.cpfilter,tp,LOCATION_GRAVE,0,1,nil) end
+	e:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+	local g=Duel.SelectTarget(tp,c100405032.cpfilter,tp,LOCATION_GRAVE,0,1,1,nil)
+	local te,ceg,cep,cev,cre,cr,crp=g:GetFirst():CheckActivateEffect(false,true,true)
+	Duel.ClearTargetCard()
+	g:GetFirst():CreateEffectRelation(e)
+	local tg=te:GetTarget()
+	e:SetCategory(te:GetCategory())
+	e:SetProperty(te:GetProperty())
+	if tg then tg(e,tp,ceg,cep,cev,cre,cr,crp,1) end
+	te:SetLabelObject(e:GetLabelObject())
+	e:SetLabelObject(te)
+	Duel.SetOperationInfo(0,CATEGORY_TODECK,g,1,0,0)
+end
+function c100405032.cpop(e,tp,eg,ep,ev,re,r,rp)
+	local te=e:GetLabelObject()
+	if not te then return end
+	if not te:GetHandler():IsRelateToEffect(e) then return end
+	e:SetLabelObject(te:GetLabelObject())
+	local op=te:GetOperation()
+	if op then op(e,tp,eg,ep,ev,re,r,rp) end
+	Duel.BreakEffect()
+	Duel.SendtoDeck(te:GetHandler(),nil,2,REASON_EFFECT)
+end
