437633f64a8102921be34b33876a2f775cef2ad1
nekrozar <unyuho.buster@gmail.com>
2016-07-29 16:25:18 +0900

    change ID

--- scripts/XXXX-JP/c65029288.lua
+++ scripts/XXXX-JP/c65029288.lua
@@ -1,7 +1,7 @@
 --EMオッドアイズ・メタル・クロウ
 --Performapal Odd-Eyes Metal Claw
 --Script by nekrozar
-function c100210001.initial_effect(c)
+function c65029288.initial_effect(c)
 	--fusion material
 	c:EnableReviveLimit()
 	aux.AddFusionProcFun2(c,aux.FilterBoolFunction(Card.IsFusionSetCard,0x99),aux.FilterBoolFunction(Card.IsFusionSetCard,0x9f),true)
@@ -17,37 +17,37 @@ function c100210001.initial_effect(c)
 	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
 	e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
 	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
-	e2:SetCondition(c100210001.immcon)
-	e2:SetOperation(c100210001.immop)
+	e2:SetCondition(c65029288.immcon)
+	e2:SetOperation(c65029288.immop)
 	c:RegisterEffect(e2)
 	--atkup
 	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(100210001,1))
+	e3:SetDescription(aux.Stringid(65029288,1))
 	e3:SetCategory(CATEGORY_ATKCHANGE)
 	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
 	e3:SetCode(EVENT_ATTACK_ANNOUNCE)
-	e3:SetOperation(c100210001.atkop)
+	e3:SetOperation(c65029288.atkop)
 	c:RegisterEffect(e3)
 end
-function c100210001.immcon(e,tp,eg,ep,ev,re,r,rp)
+function c65029288.immcon(e,tp,eg,ep,ev,re,r,rp)
 	return re and re:GetHandler():IsCode(24094653) and bit.band(e:GetHandler():GetSummonType(),SUMMON_TYPE_FUSION)==SUMMON_TYPE_FUSION
 end
-function c100210001.immop(e,tp,eg,ep,ev,re,r,rp)
+function c65029288.immop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(100210001,0))
+	e1:SetDescription(aux.Stringid(65029288,0))
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE+EFFECT_FLAG_CLIENT_HINT)
 	e1:SetCode(EFFECT_IMMUNE_EFFECT)
 	e1:SetRange(LOCATION_MZONE)
-	e1:SetValue(c100210001.efilter)
+	e1:SetValue(c65029288.efilter)
 	e1:SetReset(RESET_EVENT+0x1fe0000)
 	c:RegisterEffect(e1)
 end
-function c100210001.efilter(e,te)
+function c65029288.efilter(e,te)
 	return te:GetOwner()~=e:GetOwner()
 end
-function c100210001.atkop(e,tp,eg,ep,ev,re,r,rp)
+function c65029288.atkop(e,tp,eg,ep,ev,re,r,rp)
 	local g=Duel.GetMatchingGroup(Card.IsFaceup,tp,LOCATION_MZONE,0,nil)
 	local tc=g:GetFirst()
 	while tc do
