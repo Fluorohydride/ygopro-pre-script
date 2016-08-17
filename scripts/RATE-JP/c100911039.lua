8ca296ca23f5989dae35bfdabf9b3ec7e85dc1ab
mercury233 <me@mercury233.me>
2016-08-18 01:32:24 +0800

    fix

--- scripts/RATE-JP/c100911039.lua
+++ scripts/RATE-JP/c100911039.lua
@@ -4,7 +4,7 @@
 function c100911039.initial_effect(c)
 	--fusion material
 	c:EnableReviveLimit()
-	aux.AddFusionProcFun2(c,c100911039.ffilter,aux.FilterBoolFunction(Card.IsRace,RACE_WARROIR),true)
+	aux.AddFusionProcFun2(c,c100911039.ffilter,aux.FilterBoolFunction(Card.IsRace,RACE_WARRIOR),true)
 	--atk
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(100911039,0))
@@ -20,8 +20,8 @@ function c100911039.initial_effect(c)
 	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 	e2:SetCode(EVENT_CHAIN_SOLVING)
 	e2:SetRange(LOCATION_MZONE)
-	e2:SetCondition(c101180260.discon)
-	e2:SetOperation(c101180260.disop)
+	e2:SetCondition(c100911039.discon)
+	e2:SetOperation(c100911039.disop)
 	c:RegisterEffect(e2)
 	--remove
 	local e3=Effect.CreateEffect(c)
@@ -47,10 +47,11 @@ function c100911039.atktg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(c100911039.atkfilter,tp,0,LOCATION_MZONE,1,nil) end
 end
 function c100911039.atkop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
 	local g=Duel.GetMatchingGroup(c100911039.atkfilter,tp,0,LOCATION_MZONE,nil)
 	local tc=g:GetFirst()
 	while tc do
-		local e1=Effect.CreateEffect(e:GetHandler())
+		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_SET_ATTACK_FINAL)
 		e1:SetValue(0)
@@ -58,6 +59,17 @@ function c100911039.atkop(e,tp,eg,ep,ev,re,r,rp)
 		tc:RegisterEffect(e1)
 		tc=g:GetNext()
 	end
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_CANNOT_ATTACK)
+	e2:SetTargetRange(LOCATION_MZONE,0)
+	e2:SetTarget(c100911039.ftarget)
+	e2:SetLabel(c:GetFieldID())
+	e2:SetReset(RESET_PHASE+PHASE_END)
+	Duel.RegisterEffect(e2,tp)
+end
+function c100911039.ftarget(e,c)
+	return e:GetLabel()~=c:GetFieldID()
 end
 function c100911039.discon(e,tp,eg,ep,ev,re,r,rp)
 	if e:GetHandler():IsStatus(STATUS_BATTLE_DESTROYED) then return false end
