2a1e84799563e1b57c34070ca8750ffa55b30da3
mercury233 <me@mercury233.me>
2016-08-18 20:58:05 +0800

    fix

--- scripts/RATE-JP/c100911005.lua
+++ scripts/RATE-JP/c100911005.lua
@@ -18,15 +18,17 @@ function c100911005.initial_effect(c)
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(100911005,1))
 	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetHintTiming(TIMING_BATTLE_STEP_END+TIMING_BATTLE_END)
 	e2:SetCondition(c100911005.sccon)
 	e2:SetTarget(c100911005.sctg)
 	e2:SetOperation(c100911005.scop)
 	c:RegisterEffect(e2)
 	--deckdes
 	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(100911005,0))
+	e3:SetDescription(aux.Stringid(100911005,2))
 	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e3:SetCode(EVENT_BE_MATERIAL)
 	e3:SetCondition(c100911005.ddcon)
@@ -35,7 +37,8 @@ function c100911005.initial_effect(c)
 	c:RegisterEffect(e3)
 end
 function c100911005.spcon(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.GetCurrentPhase()>=PHASE_BATTLE_START and Duel.GetCurrentPhase()<=PHASE_BATTLE and ep==tp and bit.band(r,REASON_BATTLE+REASON_EFFECT)~=0
+	return Duel.GetCurrentPhase()>=PHASE_BATTLE_START and Duel.GetCurrentPhase()<=PHASE_BATTLE
+		and ep==tp and bit.band(r,REASON_BATTLE+REASON_EFFECT)~=0
 end
 function c100911005.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
@@ -45,25 +48,29 @@ end
 function c100911005.spop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if not c:IsRelateToEffect(e) then return end
-	if Duel.SpecialSummon(c,1,tp,tp,false,false,POS_FACEUP)==0 and Duel.GetLocationCount(tp,LOCATION_MZONE)<=0
+	if Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)~=0 then
+		c:RegisterFlagEffect(100911005,RESET_EVENT+0x1ec0000+RESET_PHASE+PHASE_BATTLE,0,1)
+	elseif Duel.GetLocationCount(tp,LOCATION_MZONE)<=0
 		and c:IsCanBeSpecialSummoned(e,0,tp,false,false) then
 		Duel.SendtoGrave(c,REASON_RULE)
 	end
 end
 function c100911005.sccon(e,tp,eg,ep,ev,re,r,rp)
-	return e:GetHandler():GetSummonType()==SUMMON_TYPE_SPECIAL+1
+	return e:GetHandler():GetFlagEffect(100911005)~=0
 end
 function c100911005.sctg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then
+		local c=e:GetHandler()
 		local mg=Duel.GetMatchingGroup(Card.IsAttribute,tp,LOCATION_MZONE,0,nil,ATTRIBUTE_WIND)
-		return Duel.IsExistingMatchingCard(Card.IsSynchroSummonable,tp,LOCATION_EXTRA,0,1,nil,e:GetHandler(),mg)
+		return not c:IsStatus(STATUS_CHAINING)
+			and Duel.IsExistingMatchingCard(Card.IsSynchroSummonable,tp,LOCATION_EXTRA,0,1,nil,c,mg)
 	end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
 end
 function c100911005.scop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	local mg=Duel.GetMatchingGroup(Card.IsAttribute,tp,LOCATION_MZONE,0,nil,ATTRIBUTE_WIND)
 	if c:GetControler()~=tp or not c:IsRelateToEffect(e) then return end
+	local mg=Duel.GetMatchingGroup(Card.IsAttribute,tp,LOCATION_MZONE,0,nil,ATTRIBUTE_WIND)
 	local g=Duel.GetMatchingGroup(Card.IsSynchroSummonable,tp,LOCATION_EXTRA,0,nil,c,mg)
 	if g:GetCount()>0 then
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
@@ -84,7 +91,8 @@ function c100911005.ddop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local rc=c:GetReasonCard()
 	local tc=Duel.GetOperatedGroup():GetFirst()
-	if tc and rc:IsRelateToEffect(e) and rc:IsFaceup() and tc:IsSetCard(0x2016) and tc:IsType(TYPE_MONSTER) and tc:IsLocation(LOCATION_GRAVE) then
+	if tc and rc:IsRelateToEffect(e) and rc:IsFaceup() and tc:IsSetCard(0x2016)
+		and tc:IsType(TYPE_MONSTER) and tc:IsLocation(LOCATION_GRAVE) then
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_UPDATE_ATTACK)
