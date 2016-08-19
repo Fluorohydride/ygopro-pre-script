ca7220ffbdca53644bd1bc0b3dd4e1f21c636084
nekrozar <unyuho.buster@gmail.com>
2016-08-20 08:31:50 +0900

    fix

--- scripts/RATE-JP/c100911007.lua
+++ scripts/RATE-JP/c100911007.lua
@@ -92,8 +92,8 @@ end
 function c100911007.damtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
 	Duel.SetTargetPlayer(1-tp)
-	Duel.SetTargetParam(1000)
-	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,1000)
+	Duel.SetTargetParam(500)
+	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,500)
 end
 function c100911007.damop(e,tp,eg,ep,ev,re,r,rp)
 	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
