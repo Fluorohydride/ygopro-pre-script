597e9b5da02cb95e8ae84e613f71a5df7bf9b773
nekrozar <unyuho.buster@gmail.com>
2016-08-20 08:30:28 +0900

    add script RATE, update Cipher Spectrum

---
+++ scripts/RATE-JP/c100911007.lua
@@ -0,0 +1,101 @@
+--WW－アイス・ベル
+--Wind Witch - Ice Bell
+--Script by nekrozar
+function c100911007.initial_effect(c)
+	--special summon
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(100911007,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCountLimit(1,100911007)
+	e1:SetCondition(c100911007.spcon)
+	e1:SetTarget(c100911007.sptg)
+	e1:SetOperation(c100911007.spop)
+	c:RegisterEffect(e1)
+	--damage
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(100911007,0))
+	e2:SetCategory(CATEGORY_DAMAGE)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_DELAY)
+	e2:SetCode(EVENT_SUMMON_SUCCESS)
+	e2:SetCountLimit(1,100911907)
+	e2:SetTarget(c100911007.damtg)
+	e2:SetOperation(c100911007.damop)
+	c:RegisterEffect(e2)
+	local e3=e2:Clone()
+	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
+	c:RegisterEffect(e3)
+	Duel.AddCustomActivityCounter(100911007,ACTIVITY_SPSUMMON,c100911007.counterfilter)
+end
+function c100911007.counterfilter(c)
+	return c:GetSummonLocation()~=LOCATION_EXTRA or (c:IsLevelAbove(5) and c:IsAttribute(ATTRIBUTE_WIND))
+end
+function c100911007.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetCustomActivityCount(100911007,tp,ACTIVITY_SPSUMMON)==0 end
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_OATH)
+	e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+	e1:SetTargetRange(1,0)
+	e1:SetTarget(c100911007.splimit)
+	e1:SetReset(RESET_PHASE+PHASE_END)
+	Duel.RegisterEffect(e1,tp)
+end
+function c100911007.splimit(e,c,sump,sumtype,sumpos,targetp,se)
+	return not c:IsSetCard(0xad) and c:IsLocation(LOCATION_EXTRA)
+end
+function c100911007.spcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0)==0
+end
+function c100911007.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
+end
+function c100911007.spfilter(c,e,tp)
+	return c:IsSetCard(0x1f0) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function c100911007.spop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not c:IsRelateToEffect(e) then return end
+	if Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)~=0 then
+		if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+		local g=Duel.GetMatchingGroup(c100911007.spfilter,tp,LOCATION_DECK,0,nil,e,tp)
+		if g:GetCount()>0 and Duel.SelectYesNo(tp,aux.Stringid(100911007,1)) then
+			Duel.BreakEffect()
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+			local sg=g:Select(tp,1,1,nil)
+			local tc=sg:GetFirst()
+			if tc then
+				Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP)
+				local e1=Effect.CreateEffect(c)
+				e1:SetType(EFFECT_TYPE_SINGLE)
+				e1:SetCode(EFFECT_UNRELEASABLE_SUM)
+				e1:SetValue(1)
+				e1:SetReset(RESET_EVENT+0x1fe0000)
+				tc:RegisterEffect(e1)
+				local e2=Effect.CreateEffect(c)
+				e2:SetType(EFFECT_TYPE_SINGLE)
+				e2:SetCode(EFFECT_UNRELEASABLE_NONSUM)
+				e2:SetValue(1)
+				e2:SetReset(RESET_EVENT+0x1fe0000)
+				tc:RegisterEffect(e2)
+			end
+		end
+	elseif Duel.GetLocationCount(tp,LOCATION_MZONE)<=0
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false) then
+		Duel.SendtoGrave(c,REASON_RULE)
+	end
+end
+function c100911007.damtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetTargetPlayer(1-tp)
+	Duel.SetTargetParam(1000)
+	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,1000)
+end
+function c100911007.damop(e,tp,eg,ep,ev,re,r,rp)
+	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+	Duel.Damage(p,d,REASON_EFFECT)
+end
--- scripts/RATE-JP/c100911069.lua
+++ scripts/RATE-JP/c100911069.lua
@@ -2,62 +2,62 @@
 --Cipher Spectrum
 --Scripted by Eerie Code
 function c100911069.initial_effect(c)
-    --
-    local e1=Effect.CreateEffect(c)
-    e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
-    e1:SetType(EFFECT_TYPE_ACTIVATE)
-    e1:SetCode(EVENT_TO_GRAVE)
-    e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
-    e1:SetTarget(c100911069.target)
-    e1:SetOperation(c100911069.activate)
-    c:RegisterEffect(e1)
-    if not c100911069.global_check then
-        c100911069.global_check=true
-        local ge1=Effect.CreateEffect(c)
-        ge1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-        ge1:SetCode(EVENT_LEAVE_FIELD_P)
-        ge1:SetOperation(c100911069.checkop)
-        Duel.RegisterEffect(ge1,tp)
-    end
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_TO_GRAVE)
+	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
+	e1:SetTarget(c100911069.target)
+	e1:SetOperation(c100911069.activate)
+	c:RegisterEffect(e1)
+	if not c100911069.global_check then
+		c100911069.global_check=true
+		local ge1=Effect.CreateEffect(c)
+		ge1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		ge1:SetCode(EVENT_LEAVE_FIELD_P)
+		ge1:SetOperation(c100911069.checkop)
+		Duel.RegisterEffect(ge1,tp)
+	end
 end
-
-function c100911069.checkfil(c,tp)
-    return c:IsSetCard(0xe5) and c:IsType(TYPE_XYZ) and c:IsControler(tp) and c:GetOverlayCount()>0
+function c100911069.cfilter(c,tp)
+	return c:IsSetCard(0xe5) and c:IsType(TYPE_XYZ) and c:IsControler(tp) and c:GetOverlayCount()>0
 end
 function c100911069.checkop(e,tp,eg,ep,ev,re,r,rp)
-    if not eg then return end
-    local sg=eg:Filter(c100911069.checkfil,nil,tp)
-    local tc=sg:GetFirst()
-    while tc do
-        tc:RegisterFlagEffect(100911069,RESET_EVENT+0x1fe0000-EVENT_TO_GRAVE,0,1)
-        tc=sg:GetNext()
-    end
+	if not eg then return end
+	local sg=eg:Filter(c100911069.cfilter,nil,tp)
+	local tc=sg:GetFirst()
+	while tc do
+		tc:RegisterFlagEffect(100911069,RESET_EVENT+0x1fe0000-EVENT_TO_GRAVE,0,1)
+		tc=sg:GetNext()
+	end
 end
-
-function c100911069.cfil(c,e,tp)
-    return c:GetFlagEffect(100911069)~=0 and c:IsLocation(LOCATION_GRAVE) and c:IsControler(tp) and (c:IsReason(REASON_BATTLE) or c:IsReason(REASON_EFFECT) and c:GetReasonPlayer()~=tp) and c:IsCanBeEffectTarget(e) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
-        and Duel.IsExistingMatchingCard(c100911069.fil,tp,LOCATION_EXTRA,0,1,nil,e,tp,c:GetCode())
+function c100911069.filter(c,e,tp)
+	return c:GetFlagEffect(100911069)~=0 and c:IsLocation(LOCATION_GRAVE) and c:IsControler(tp)
+		and (c:IsReason(REASON_BATTLE) or c:IsReason(REASON_EFFECT) and c:GetReasonPlayer()~=tp)
+		and c:IsCanBeEffectTarget(e) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+		and Duel.IsExistingMatchingCard(c100911069.spfilter,tp,LOCATION_EXTRA,0,1,nil,e,tp,c:GetCode())
 end
-function c100911069.fil(c,e,tp,cd)
-    return c:IsType(TYPE_XYZ) and c:IsCode(cd) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+function c100911069.spfilter(c,e,tp,cd)
+	return c:IsType(TYPE_XYZ) and c:IsCode(cd) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function c100911069.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-    if chkc then return eg:IsContains(chkc) and c100911069.cfil(chkc,e,tp) end
-    if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>1 and eg:IsExists(c100911069.cfil,1,nil,e,tp) end
-    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-    local g=eg:FilterSelect(tp,c100911069.cfil,1,1,nil,e,tp)
-    Duel.SetTargetCard(g)
-    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
+	if chkc then return eg:IsContains(chkc) and c100911069.filter(chkc,e,tp) end
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>1 and eg:IsExists(c100911069.filter,1,nil,e,tp) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=eg:FilterSelect(tp,c100911069.filter,1,1,nil,e,tp)
+	Duel.SetTargetCard(g)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
 end
 function c100911069.activate(e,tp,eg,ep,ev,re,r,rp)
-    if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
-    local tc=Duel.GetFirstTarget()
-    if tc:IsRelateToEffect(e) and Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)~=0 then
-        if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
-        Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-        local g2=Duel.SelectMatchingCard(tp,c100911069.fil,tp,LOCATION_EXTRA,0,1,1,nil,e,tp,tc:GetCode())
-        if g2:GetCount()>0 then
-            Duel.SpecialSummon(g2,0,tp,tp,false,false,POS_FACEUP)
-        end
-    end
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) and Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)~=0 then
+		if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+		local g2=Duel.SelectMatchingCard(tp,c100911069.spfilter,tp,LOCATION_EXTRA,0,1,1,nil,e,tp,tc:GetCode())
+		if g2:GetCount()>0 then
+			Duel.SpecialSummon(g2,0,tp,tp,false,false,POS_FACEUP)
+		end
+	end
 end
