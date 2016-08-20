163995d48aa09685577719378394baa0a4e91731
nekrozar <unyuho.buster@gmail.com>
2016-08-20 09:21:30 +0900

    change ID

--- scripts/VJMP-JP/c12262393.lua
+++ scripts/VJMP-JP/c12262393.lua
@@ -1,69 +1,69 @@
 --磁石の戦士δ
 --Delta The Magnet Warrior
 --Script by nekrozar
-function c100200121.initial_effect(c)
+function c12262393.initial_effect(c)
 	--to grave
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(100200121,0))
+	e1:SetDescription(aux.Stringid(12262393,0))
 	e1:SetCategory(CATEGORY_TOGRAVE)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e1:SetProperty(EFFECT_FLAG_DELAY)
 	e1:SetCode(EVENT_SUMMON_SUCCESS)
-	e1:SetCountLimit(1,100200121)
-	e1:SetTarget(c100200121.tgtg)
-	e1:SetOperation(c100200121.tgop)
+	e1:SetCountLimit(1,12262393)
+	e1:SetTarget(c12262393.tgtg)
+	e1:SetOperation(c12262393.tgop)
 	c:RegisterEffect(e1)
 	local e2=e1:Clone()
 	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
 	c:RegisterEffect(e2)
 	--special summon
 	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(100200121,1))
+	e3:SetDescription(aux.Stringid(12262393,1))
 	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e3:SetProperty(EFFECT_FLAG_DELAY)
 	e3:SetCode(EVENT_TO_GRAVE)
-	e3:SetCountLimit(1,100201121)
-	e3:SetCost(c100200121.spcost)
-	e3:SetTarget(c100200121.sptg)
-	e3:SetOperation(c100200121.spop)
+	e3:SetCountLimit(1,12262394)
+	e3:SetCost(c12262393.spcost)
+	e3:SetTarget(c12262393.sptg)
+	e3:SetOperation(c12262393.spop)
 	c:RegisterEffect(e3)
 end
-function c100200121.tgfilter(c)
+function c12262393.tgfilter(c)
 	return c:IsType(TYPE_MONSTER) and c:IsSetCard(0x2066) and c:IsLevelBelow(4) and c:IsAbleToGrave()
 end
-function c100200121.tgtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(c100200121.tgfilter,tp,LOCATION_DECK,0,1,nil) end
+function c12262393.tgtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c12262393.tgfilter,tp,LOCATION_DECK,0,1,nil) end
 	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_DECK)
 end
-function c100200121.tgop(e,tp,eg,ep,ev,re,r,rp)
+function c12262393.tgop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
-	local g=Duel.SelectMatchingCard(tp,c100200121.tgfilter,tp,LOCATION_DECK,0,1,1,nil)
+	local g=Duel.SelectMatchingCard(tp,c12262393.tgfilter,tp,LOCATION_DECK,0,1,1,nil)
 	if g:GetCount()>0 then
 		Duel.SendtoGrave(g,REASON_EFFECT)
 	end
 end
-function c100200121.cfilter(c)
-	return c:IsType(TYPE_MONSTER) and c:IsSetCard(0x2066) and c:IsLevelBelow(4) and not c:IsCode(100200121) and c:IsAbleToRemoveAsCost()
+function c12262393.cfilter(c)
+	return c:IsType(TYPE_MONSTER) and c:IsSetCard(0x2066) and c:IsLevelBelow(4) and not c:IsCode(12262393) and c:IsAbleToRemoveAsCost()
 end
-function c100200121.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(c100200121.cfilter,tp,LOCATION_GRAVE,0,3,nil) end
+function c12262393.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c12262393.cfilter,tp,LOCATION_GRAVE,0,3,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
-	local g=Duel.SelectMatchingCard(tp,c100200121.cfilter,tp,LOCATION_GRAVE,0,3,3,nil)
+	local g=Duel.SelectMatchingCard(tp,c12262393.cfilter,tp,LOCATION_GRAVE,0,3,3,nil)
 	Duel.Remove(g,POS_FACEUP,REASON_COST)
 end
-function c100200121.spfilter(c,e,tp)
+function c12262393.spfilter(c,e,tp)
 	return c:IsCode(75347539) and c:IsCanBeSpecialSummoned(e,0,tp,true,false)
 end
-function c100200121.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+function c12262393.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingMatchingCard(c100200121.spfilter,tp,LOCATION_HAND+LOCATION_DECK,0,1,nil,e,tp) end
+		and Duel.IsExistingMatchingCard(c12262393.spfilter,tp,LOCATION_HAND+LOCATION_DECK,0,1,nil,e,tp) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND+LOCATION_DECK)
 end
-function c100200121.spop(e,tp,eg,ep,ev,re,r,rp)
+function c12262393.spop(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectMatchingCard(tp,c100200121.spfilter,tp,LOCATION_HAND+LOCATION_DECK,0,1,1,nil,e,tp)
+	local g=Duel.SelectMatchingCard(tp,c12262393.spfilter,tp,LOCATION_HAND+LOCATION_DECK,0,1,1,nil,e,tp)
 	if g:GetCount()>0 then
 		Duel.SpecialSummon(g,0,tp,tp,true,false,POS_FACEUP)
 	end
