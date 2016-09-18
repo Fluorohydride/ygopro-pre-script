ec462c22d70d83ffc0a6b0f56523e2f7181704e5
dest <dest.dn@gmail.com>
2016-09-18 16:54:24 +0200

    fix

--- scripts/RATE-JP/c100911036.lua
+++ scripts/RATE-JP/c100911036.lua
@@ -78,6 +78,7 @@ function c100911036.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
 end
 function c100911036.operation(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
 	local c=e:GetHandler()
 	local tc=Duel.GetFirstTarget()
 	if c:IsRelateToEffect(e) and Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)>0
