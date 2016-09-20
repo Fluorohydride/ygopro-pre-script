eff9e0aade89b60265269dc6fda20fcddadd7ea8
mercury233 <me@mercury233.me>
2016-09-20 19:13:47 +0800

    fix Ancient Gear Reborn

--- scripts/RATE-JP/c100911070.lua
+++ scripts/RATE-JP/c100911070.lua
@@ -47,8 +47,8 @@ function c100911070.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and c100911070.spfilter(chkc,e,tp) end
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
 		and Duel.IsExistingTarget(c100911070.spfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp)
-		and e:GetHandler():GetFlagEffect(tp,100911070)==0 end
-	e:GetHandler():RegisterFlagEffect(tp,100911070,RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END,0,1)
+		and e:GetHandler():GetFlagEffect(100911070)==0 end
+	e:GetHandler():RegisterFlagEffect(100911070,RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END,0,1)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 	local g=Duel.SelectTarget(tp,c100911070.spfilter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
