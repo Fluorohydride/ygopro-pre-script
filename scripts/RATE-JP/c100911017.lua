436e76f7d936ef21b98b6f2a0efae9dc022d4dc1
DiabladeZat <diamondbladezat@gmail.com>
2016-09-27 19:16:28 +0200

    Less complex scripting for the second effect of "Juunishishi Thoroughblades" second effect
    Simplified the "get effect" effect in Juunshishi Thoroughblade.

--- scripts/RATE-JP/c100911017.lua
+++ scripts/RATE-JP/c100911017.lua
@@ -1,60 +1,66 @@
 --十二獣サラブレード
 --Juunishishi Thoroughblade
---Script by mercury233
---The "get effect" effect is temporary
+--Scripted by DiabladeZat
 function c100911017.initial_effect(c)
-	--draw
+	--Draw
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(100911017,0))
 	e1:SetCategory(CATEGORY_DRAW)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
 	e1:SetCode(EVENT_SUMMON_SUCCESS)
-	e1:SetProperty(EFFECT_FLAG_DELAY)
+	e1:SetCost(c100911017.drcost)
 	e1:SetTarget(c100911017.drtg)
-	e1:SetOperation(c100911017.drop)
+	e1:SetOperation(c100911017.drop)	
 	c:RegisterEffect(e1)
 	local e2=e1:Clone()
 	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
 	c:RegisterEffect(e2)
-	--get effect
-	if not c100911017.global_check then
-		c100911017.global_check=true
-		local ge1=Effect.CreateEffect(c)
-		ge1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-		ge1:SetCode(EVENT_SPSUMMON_SUCCESS)
-		ge1:SetOperation(c100911017.checkop)
-		Duel.RegisterEffect(ge1,0)
-	end
+	--effect gain
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e3:SetCode(EVENT_BE_MATERIAL)
+	e3:SetCondition(c100911017.efcon)
+	e3:SetOperation(c100911017.efop)
+	c:RegisterEffect(e3)
 end
-function c100911017.filter(c)
-	return c:IsSetCard(0x1f2) and c:IsDiscardable(REASON_EFFECT)
+function c100911017.drfilter(c)
+	return c:IsSetCard(0x1f2) and c:IsDiscardable()
+end
+function c100911017.drcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c100911017.drfilter,tp,LOCATION_HAND,0,1,e:GetHandler()) end
+	Duel.DiscardHand(tp,c100911017.drfilter,1,1,REASON_COST+REASON_DISCARD)
 end
 function c100911017.drtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsPlayerCanDraw(tp,1)
-		and Duel.IsExistingMatchingCard(c100911017.filter,tp,LOCATION_HAND,0,1,nil) end
+	if chk==0 then return Duel.IsPlayerCanDraw(tp,1) end
+	Duel.SetTargetPlayer(tp)
+	Duel.SetTargetParam(1)
 	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
 end
 function c100911017.drop(e,tp,eg,ep,ev,re,r,rp)
-	if Duel.DiscardHand(tp,c100911017.filter,1,1,REASON_EFFECT+REASON_DISCARD,nil)~=0 then
-		Duel.Draw(tp,1,REASON_EFFECT)
-	end
+	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+	Duel.Draw(p,d,REASON_EFFECT)
 end
-function c100911017.checkop(e,tp,eg,ep,ev,re,r,rp)
-	local tc=eg:GetFirst()
-	while tc do
-		if tc:IsType(TYPE_XYZ) and tc:GetOriginalRace()==RACE_BEASTWARRIOR
-			and tc:GetFlagEffect(100911017)==0 then
-			local e1=Effect.CreateEffect(e:GetHandler())
+function c100911017.efcon(e,tp,eg,ep,ev,re,r,rp)
+	return r==REASON_XYZ
+end
+function c100911017.efop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local rc=c:GetReasonCard()
+	if rc:GetOriginalRace()==RACE_BEASTWARRIOR and rc:IsType(TYPE_XYZ)then
+		local e1=Effect.CreateEffect(rc)
 			e1:SetDescription(aux.Stringid(100911017,1))
 			e1:SetType(EFFECT_TYPE_SINGLE)
 			e1:SetCode(EFFECT_PIERCE)
-			e1:SetCondition(c100911017.condition)
-			tc:RegisterEffect(e1,true)
-			tc:RegisterFlagEffect(100911017,0,0,1)
+			rc:RegisterEffect(e1,true)
+		if not rc:IsType(TYPE_EFFECT) then
+			local e2=Effect.CreateEffect(c)
+			e2:SetType(EFFECT_TYPE_SINGLE)
+			e2:SetCode(EFFECT_ADD_TYPE)
+			e2:SetValue(TYPE_EFFECT)
+			e2:SetReset(RESET_EVENT+0x1fe0000)
+			rc:RegisterEffect(e2,true)
 		end
-		tc=eg:GetNext()
 	end
 end
-function c100911017.condition(e,tp,eg,ep,ev,re,r,rp)
-	return e:GetHandler():GetOverlayGroup():IsExists(Card.IsCode,1,nil,100911017)
-end
+
