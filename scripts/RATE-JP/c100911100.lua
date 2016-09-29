13c0b9e8296f2f46b899b1414542c8b3702c4e44
nekrozar <unyuho.buster@gmail.com>
2016-09-29 19:14:38 +0900

    add script RATE, SPFE

---
+++ scripts/RATE-JP/c100911100.lua
@@ -0,0 +1,82 @@
+--ゾディアックS
+--Zodiac Sign
+--Script by nekrozar
+function c100911100.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e1)
+	--atk & def
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_UPDATE_ATTACK)
+	e2:SetRange(LOCATION_FZONE)
+	e2:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+	e2:SetTarget(aux.TargetBoolFunction(Card.IsSetCard,0x1f2))
+	e2:SetValue(300)
+	c:RegisterEffect(e2)
+	local e3=e2:Clone()
+	e3:SetCode(EFFECT_UPDATE_DEFENSE)
+	c:RegisterEffect(e3)
+	--cannot be target
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_FIELD)
+	e4:SetCode(EFFECT_CANNOT_SELECT_BATTLE_TARGET)
+	e4:SetRange(LOCATION_FZONE)
+	e4:SetTargetRange(0,LOCATION_MZONE)
+	e4:SetValue(c100911100.atlimit)
+	c:RegisterEffect(e4)
+	--destroy replace
+	local e5=Effect.CreateEffect(c)
+	e5:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e5:SetCode(EFFECT_DESTROY_REPLACE)
+	e5:SetRange(LOCATION_FZONE)
+	e5:SetTarget(c100911100.reptg)
+	e5:SetValue(c100911100.repval)
+	e5:SetOperation(c100911100.repop)
+	c:RegisterEffect(e5)
+end
+function c100911100.atfilter(c,atk)
+	return c:IsFaceup() and c:IsRace(RACE_BEASTWARRIOR) and c:GetAttack()>atk
+end
+function c100911100.atlimit(e,c)
+	return c:IsFaceup() and c:IsRace(RACE_BEASTWARRIOR) and Duel.IsExistingMatchingCard(c34822850.atfilter,c:GetControler(),LOCATION_MZONE,0,1,nil,c:GetAttack())
+end
+function c100911100.repfilter(c,tp,e)
+	return c:IsFaceup() and c:IsControler(tp) and c:IsLocation(LOCATION_MZONE)
+		and c:IsSetCard(0x1f2) and c:IsReason(REASON_EFFECT) and c:GetFlagEffect(100911100)==0
+end
+function c100911100.desfilter(c,tp)
+	return c:IsControler(tp) and c:IsLocation(LOCATION_MZONE+LOCATION_HAND)
+		and not c:IsStatus(STATUS_DESTROY_CONFIRMED+STATUS_BATTLE_DESTROYED)
+end
+function c100911100.reptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c100911100.desfilter,tp,LOCATION_MZONE+LOCATION_HAND,0,1,nil,tp)
+		and eg:IsExists(c100911100.repfilter,1,nil,tp,e) end
+	if Duel.SelectYesNo(tp,aux.Stringid(100911100,0)) then
+		local g=eg:Filter(c100911100.repfilter,nil,tp,e)
+		if g:GetCount()==1 then
+			e:SetLabelObject(g:GetFirst())
+		else
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESREPLACE)
+			local cg=g:Select(tp,1,1,nil)
+			e:SetLabelObject(cg:GetFirst())
+		end
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESREPLACE)
+		local tg=Duel.SelectMatchingCard(tp,c100911100.desfilter,tp,LOCATION_MZONE+LOCATION_HAND,0,1,1,nil,tp)
+		Duel.HintSelection(tg)
+		Duel.SetTargetCard(tg)
+		tg:GetFirst():RegisterFlagEffect(100911100,RESET_EVENT+0x1fc0000+RESET_CHAIN,0,1)
+		tg:GetFirst():SetStatus(STATUS_DESTROY_CONFIRMED,true)
+		return true
+	else return false end
+end
+function c100911100.repval(e,c)
+	return c==e:GetLabelObject()
+end
+function c100911100.repop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	tc:SetStatus(STATUS_DESTROY_CONFIRMED,false)
+	Duel.Destroy(tc,REASON_EFFECT+REASON_REPLACE)
+end
---
+++ scripts/SPFE-JP/c100406011.lua
@@ -0,0 +1,73 @@
+--プレデター・プランター
+--Predator Planter
+--Script by nekrozar
+function c100406011.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e1)
+	--maintain
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e2:SetCode(EVENT_PHASE+PHASE_STANDBY)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetCountLimit(1)
+	e2:SetCondition(c100406011.mtcon)
+	e2:SetOperation(c100406011.mtop)
+	c:RegisterEffect(e2)
+	--special summon
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(100406011,0))
+	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e3:SetType(EFFECT_TYPE_IGNITION)
+	e3:SetRange(LOCATION_SZONE)
+	e3:SetCountLimit(1)
+	e3:SetTarget(c100406011.sptg)
+	e3:SetOperation(c100406011.spop)
+	c:RegisterEffect(e3)
+end
+function c100406011.mtcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetTurnPlayer()==tp
+end
+function c100406011.mtop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.CheckLPCost(tp,800) and Duel.SelectYesNo(tp,aux.Stringid(100406011,1)) then
+		Duel.PayLPCost(tp,800)
+	else
+		Duel.Destroy(e:GetHandler(),REASON_COST)
+	end
+end
+function c100406011.spfilter(c,e,tp)
+	return (c:IsFusionSetCard(0x1f3) or c:IsFusionCode(96622984,22011689,69105797)) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function c100406011.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(c100406011.spfilter,tp,LOCATION_HAND+LOCATION_GRAVE,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND+LOCATION_GRAVE)
+end
+function c100406011.spop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not c:IsRelateToEffect(e) or Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,c100406011.filter,tp,LOCATION_HAND+LOCATION_GRAVE,0,1,1,nil,e,tp)
+	local tc=g:GetFirst()
+	if not tc then return end
+	if tc:IsHasEffect(EFFECT_NECRO_VALLEY) and Duel.IsChainDisablable(0) then
+		Duel.NegateEffect(0)
+		return
+	end
+	if Duel.SpecialSummonStep(tc,0,tp,tp,false,false,POS_FACEUP) then
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_DISABLE)
+		e1:SetReset(RESET_EVENT+0x1fe0000)
+		tc:RegisterEffect(e1)
+		local e2=Effect.CreateEffect(c)
+		e2:SetType(EFFECT_TYPE_SINGLE)
+		e2:SetCode(EFFECT_DISABLE_EFFECT)
+		e2:SetReset(RESET_EVENT+0x1fe0000)
+		tc:RegisterEffect(e2)
+	end
+	Duel.SpecialSummonComplete()
+end
