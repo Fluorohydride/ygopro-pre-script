95f72f2cc2f9259abfcb8397c17a21a8b72d0e97
nekrozar <unyuho.buster@gmail.com>
2016-10-04 16:09:14 +0900

    change ID

--- scripts/YA02-JP/c90036274.lua
+++ scripts/YA02-JP/c90036274.lua
@@ -1,7 +1,7 @@
 --クリアウィング・ファスト・ドラゴン
 --Clearwing Fast Dragon
 --Script by dest
-function c100211001.initial_effect(c)
+function c90036274.initial_effect(c)
 	--synchro summon
 	aux.AddSynchroProcedure(c,nil,aux.NonTuner(Card.IsAttribute,ATTRIBUTE_WIND),1)
 	c:EnableReviveLimit()
@@ -9,18 +9,18 @@ function c100211001.initial_effect(c)
 	aux.EnablePendulumAttribute(c,false)
 	--special summon
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(100211001,0))
+	e1:SetDescription(aux.Stringid(90036274,0))
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetRange(LOCATION_PZONE)
-	e1:SetCountLimit(1,100211001)
-	e1:SetCost(c100211001.spcost)
-	e1:SetTarget(c100211001.sptg)
-	e1:SetOperation(c100211001.spop)
+	e1:SetCountLimit(1,90036274)
+	e1:SetCost(c90036274.spcost)
+	e1:SetTarget(c90036274.sptg)
+	e1:SetOperation(c90036274.spop)
 	c:RegisterEffect(e1)
 	--atk
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(100211001,1))
+	e2:SetDescription(aux.Stringid(90036274,1))
 	e2:SetCategory(CATEGORY_ATKCHANGE+CATEGORY_DISABLE)
 	e2:SetType(EFFECT_TYPE_QUICK_O)
 	e2:SetCode(EVENT_FREE_CHAIN)
@@ -28,43 +28,43 @@ function c100211001.initial_effect(c)
 	e2:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DAMAGE_STEP)
 	e2:SetHintTiming(TIMING_DAMAGE_STEP,TIMING_DAMAGE_STEP+0x1c0)
 	e2:SetCountLimit(1,100211101)
-	e2:SetCondition(c100211001.condition)
-	e2:SetTarget(c100211001.target)
-	e2:SetOperation(c100211001.operation)
+	e2:SetCondition(c90036274.condition)
+	e2:SetTarget(c90036274.target)
+	e2:SetOperation(c90036274.operation)
 	c:RegisterEffect(e2)
 	--pendulum
 	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(100211001,2))
+	e3:SetDescription(aux.Stringid(90036274,2))
 	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e3:SetCode(EVENT_DESTROYED)
 	e3:SetProperty(EFFECT_FLAG_DELAY)
-	e3:SetCondition(c100211001.pencon)
-	e3:SetTarget(c100211001.pentg)
-	e3:SetOperation(c100211001.penop)
+	e3:SetCondition(c90036274.pencon)
+	e3:SetTarget(c90036274.pentg)
+	e3:SetOperation(c90036274.penop)
 	c:RegisterEffect(e3)
 end
-function c100211001.cfilter1(c)
+function c90036274.cfilter1(c)
 	return c:IsSetCard(0x2016) and c:IsType(TYPE_TUNER) and c:IsAbleToGraveAsCost() and c:IsLevelBelow(7)
-		and Duel.IsExistingMatchingCard(c100211001.cfilter2,tp,LOCATION_MZONE,0,1,c,c:GetLevel())
+		and Duel.IsExistingMatchingCard(c90036274.cfilter2,tp,LOCATION_MZONE,0,1,c,c:GetLevel())
 end
-function c100211001.cfilter2(c,lv)
+function c90036274.cfilter2(c,lv)
 	return c:IsNotTuner() and c:GetLevel()==7-lv and c:IsAbleToGraveAsCost()
 end
-function c100211001.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(c100211001.cfilter1,tp,LOCATION_MZONE,0,1,nil) end
+function c90036274.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c90036274.cfilter1,tp,LOCATION_MZONE,0,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
-	local g1=Duel.SelectMatchingCard(tp,c100211001.cfilter1,tp,LOCATION_MZONE,0,1,1,nil)
+	local g1=Duel.SelectMatchingCard(tp,c90036274.cfilter1,tp,LOCATION_MZONE,0,1,1,nil)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
-	local g2=Duel.SelectMatchingCard(tp,c100211001.cfilter2,tp,LOCATION_MZONE,0,1,1,g1:GetFirst(),g1:GetFirst():GetLevel())
+	local g2=Duel.SelectMatchingCard(tp,c90036274.cfilter2,tp,LOCATION_MZONE,0,1,1,g1:GetFirst(),g1:GetFirst():GetLevel())
 	g1:Merge(g2)
 	Duel.SendtoGrave(g1,REASON_COST)
 end
-function c100211001.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+function c90036274.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>-2
 		and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
 end
-function c100211001.spop(e,tp,eg,ep,ev,re,r,rp)
+function c90036274.spop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if not c:IsRelateToEffect(e) then return end
 	if Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)==0 and Duel.GetLocationCount(tp,LOCATION_MZONE)<=0
@@ -72,19 +72,19 @@ function c100211001.spop(e,tp,eg,ep,ev,re,r,rp)
 		Duel.SendtoGrave(c,REASON_RULE)
 	end
 end
-function c100211001.condition(e,tp,eg,ep,ev,re,r,rp)
+function c90036274.condition(e,tp,eg,ep,ev,re,r,rp)
 	return Duel.GetCurrentPhase()~=PHASE_DAMAGE or not Duel.IsDamageCalculated()
 end
-function c100211001.filter(c)
+function c90036274.filter(c)
 	return c:GetSummonLocation()==LOCATION_EXTRA and not (c:GetAttack()==0 and c:IsDisabled())
 end
-function c100211001.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) and c100211001.filter(chkc) end
-	if chk==0 then return Duel.IsExistingTarget(c100211001.filter,tp,0,LOCATION_MZONE,1,nil) end
+function c90036274.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) and c90036274.filter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(c90036274.filter,tp,0,LOCATION_MZONE,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
-	Duel.SelectTarget(tp,c100211001.filter,tp,0,LOCATION_MZONE,1,1,nil)
+	Duel.SelectTarget(tp,c90036274.filter,tp,0,LOCATION_MZONE,1,1,nil)
 end
-function c100211001.operation(e,tp,eg,ep,ev,re,r,rp)
+function c90036274.operation(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local tc=Duel.GetFirstTarget()
 	if tc:IsFaceup() and tc:IsRelateToEffect(e) then
@@ -115,13 +115,13 @@ function c100211001.operation(e,tp,eg,ep,ev,re,r,rp)
 		end
 	end
 end
-function c100211001.pencon(e,tp,eg,ep,ev,re,r,rp)
+function c90036274.pencon(e,tp,eg,ep,ev,re,r,rp)
 	return bit.band(r,REASON_EFFECT+REASON_BATTLE)~=0 and e:GetHandler():IsPreviousLocation(LOCATION_MZONE)
 end
-function c100211001.pentg(e,tp,eg,ep,ev,re,r,rp,chk)
+function c90036274.pentg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.CheckLocation(tp,LOCATION_SZONE,6) or Duel.CheckLocation(tp,LOCATION_SZONE,7) end
 end
-function c100211001.penop(e,tp,eg,ep,ev,re,r,rp)
+function c90036274.penop(e,tp,eg,ep,ev,re,r,rp)
 	if not Duel.CheckLocation(tp,LOCATION_SZONE,6) and not Duel.CheckLocation(tp,LOCATION_SZONE,7) then return false end
 	local c=e:GetHandler()
 	if c:IsRelateToEffect(e) then
