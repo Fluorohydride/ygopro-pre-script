8d72314d1e18ea1650eb79fc6d4feb3b3915c6da
nekrozar <unyuho.buster@gmail.com>
2016-10-05 17:59:52 +0900

    add script INOV-EN

---
+++ scripts/INOV-EN/c100910086.lua
@@ -0,0 +1,110 @@
+--Charming Resort Staff
+--Script by nekrozar
+function c100910086.initial_effect(c)
+	--atkdown
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(100910086,0))
+	e1:SetCategory(CATEGORY_ATKCHANGE)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_ATTACK_ANNOUNCE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCondition(c100910086.atkcon)
+	e1:SetTarget(c100910086.atktg)
+	e1:SetOperation(c100910086.atkop)
+	c:RegisterEffect(e1)
+	--special summon
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(100910086,1))
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetProperty(EFFECT_FLAG_DELAY)
+	e2:SetCode(EVENT_DESTROYED)
+	e2:SetCondition(c100910086.spcon)
+	e2:SetTarget(c100910086.sptg)
+	e2:SetOperation(c100910086.spop)
+	c:RegisterEffect(e2)
+	--to hand
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(100910086,2))
+	e3:SetCategory(CATEGORY_TOHAND)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e3:SetProperty(EFFECT_FLAG_DELAY)
+	e3:SetCode(EVENT_DESTROYED)
+	e3:SetRange(LOCATION_GRAVE)
+	e3:SetCondition(c100910086.thcon)
+	e3:SetCost(c100910086.thcost)
+	e3:SetTarget(c100910086.thtg)
+	e3:SetOperation(c100910086.thop)
+	c:RegisterEffect(e3)
+	local e4=e3:Clone()
+	e4:SetCode(EVENT_TO_GRAVE)
+	c:RegisterEffect(e4)
+end
+function c100910086.atkcon(e,tp,eg,ep,ev,re,r,rp)
+	local a=Duel.GetAttacker()
+	local d=Duel.GetAttackTarget()
+	if not d or a:GetControler()==d:GetControler() then return end
+	if a:IsControler(tp) and a:IsFaceup() and a:IsCode(41091257) then e:SetLabelObject(d)
+	elseif d:IsFaceup() and d:IsCode(41091257) then e:SetLabelObject(a)
+	else return false end
+	return true
+end
+function c100910086.atktg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local tc=e:GetLabelObject()
+	if chk==0 then return tc:IsOnField() end
+	Duel.SetTargetCard(tc)
+end
+function c100910086.atkop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) and tc:IsFaceup() and tc:IsControler(1-tp) then
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_SET_ATTACK_FINAL)
+		e1:SetValue(0)
+		e1:SetReset(RESET_EVENT+0x1fe0000)
+		tc:RegisterEffect(e1)
+	end
+end
+function c100910086.spcon(e,tp,eg,ep,ev,re,r,rp)
+	return bit.band(r,REASON_EFFECT+REASON_BATTLE)~=0
+end
+function c100910086.spfilter(c,e,tp)
+	return c:IsCode(41091257) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function c100910086.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(c100910086.spfilter,tp,LOCATION_DECK,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
+end
+function c100910086.spop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	local tg=Duel.GetFirstMatchingCard(c100910086.spfilter,tp,LOCATION_DECK,0,nil,e,tp)
+	if tg then
+		Duel.SpecialSummon(tg,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
+function c100910086.cfilter(c,tp)
+	return c:IsCode(41091257) and c:GetPreviousControler()==tp
+end
+function c100910086.thcon(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(c100910086.cfilter,1,nil,tp)
+end
+function c100910086.thcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsAbleToRemoveAsCost() end
+	Duel.Remove(e:GetHandler(),POS_FACEUP,REASON_COST)
+end
+function c100910086.thfilter(c)
+	return c:IsCode(41091257) and c:IsAbleToHand()
+end
+function c100910086.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c100910086.thfilter,tp,LOCATION_GRAVE,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_GRAVE)
+end
+function c100910086.thop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,c100910086.thfilter,tp,LOCATION_GRAVE,0,1,1,nil)
+	if g:GetCount()>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end
