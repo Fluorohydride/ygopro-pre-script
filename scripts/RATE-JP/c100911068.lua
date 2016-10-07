2de231de7b43a32816eba9a1ae04073f4bced323
mercury233 <me@mercury233.me>
2016-10-07 09:44:47 +0800

    add RATE-JP068 Bewildering Wind

---
+++ scripts/RATE-JP/c100911068.lua
@@ -0,0 +1,80 @@
+--迷い風
+--Bewildering Wind
+--Script by mercury233
+function c100911068.initial_effect(c)
+	--activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(100911068,0))
+	e1:SetCategory(CATEGORY_DISABLE)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetHintTiming(0,0x1c0)
+	e1:SetTarget(c100911068.target)
+	e1:SetOperation(c100911068.activate)
+	c:RegisterEffect(e1)
+	--set
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(100911068,1))
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e2:SetProperty(EFFECT_FLAG_DELAY)
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e2:SetRange(LOCATION_GRAVE)
+	e2:SetCondition(c100911068.setcon)
+	e2:SetTarget(c100911068.settg)
+	e2:SetOperation(c100911068.setop)
+	c:RegisterEffect(e2)
+end
+function c100911068.filter(c)
+	return c:IsFaceup() and not c:IsDisabled()
+		and bit.band(c:GetSummonType(),SUMMON_TYPE_SPECIAL)==SUMMON_TYPE_SPECIAL
+end
+function c100911068.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and c100911068.filter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(c100911068.filter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	Duel.SelectTarget(tp,c100911068.filter,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
+end
+function c100911068.activate(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) and tc:IsFaceup() and not tc:IsDisabled() then
+		Duel.NegateRelatedChain(tc,RESET_TURN_SET)
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_DISABLE)
+		e1:SetReset(RESET_EVENT+0x1fe0000)
+		tc:RegisterEffect(e1)
+		local e2=e1:Clone()
+		e2:SetCode(EFFECT_DISABLE_EFFECT)
+		e2:SetValue(RESET_TURN_SET)
+		tc:RegisterEffect(e2)
+		local e3=e1:Clone()
+		e3:SetCode(EFFECT_SET_BASE_ATTACK)
+		e3:SetValue(tc:GetBaseAttack()/2)
+		tc:RegisterEffect(e3)
+	end
+end
+function c100911068.cfilter(c,tp)
+	return c:GetSummonLocation()==LOCATION_EXTRA and c:GetPreviousControler()==1-tp
+end
+function c100911068.setcon(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(c100911068.cfilter,1,nil,tp)
+end
+function c100911068.settg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsSSetable() end
+	Duel.SetOperationInfo(0,CATEGORY_LEAVE_GRAVE,e:GetHandler(),1,0,0)
+end
+function c100911068.setop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) and c:IsSSetable() then
+		Duel.SSet(tp,c)
+		Duel.ConfirmCards(1-tp,c)
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_LEAVE_FIELD_REDIRECT)
+		e1:SetReset(RESET_EVENT+0x47e0000)
+		e1:SetValue(LOCATION_REMOVED)
+		c:RegisterEffect(e1)
+	end
+end
