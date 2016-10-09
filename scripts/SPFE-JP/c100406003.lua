f8ee843e9863f15f9e19c8fdac42f1f0ff8cb4fd
nekrozar <unyuho.buster@gmail.com>
2016-10-09 18:44:32 +0900

    add script SPFE

---
+++ scripts/SPFE-JP/c100406003.lua
@@ -0,0 +1,65 @@
+--捕食植物プテロペンテス 
+--Predator Plant Pteropenthes
+--Script by nekrozar
+function c100406003.initial_effect(c)
+	--counter
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(100406003,0))
+	e1:SetCategory(CATEGORY_COUNTER)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetCode(EVENT_BATTLE_DAMAGE)
+	e1:SetCondition(c100406003.ctcon1)
+	e1:SetTarget(c100406003.cttg1)
+	e1:SetOperation(c100406003.ctop1)
+	c:RegisterEffect(e1)
+	--control
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(100406003,1))
+	e2:SetCategory(CATEGORY_CONTROL)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1)
+	e2:SetTarget(c100406003.cttg2)
+	e2:SetOperation(c100406003.ctop2)
+	c:RegisterEffect(e2)
+end
+function c100406003.ctcon1(e,tp,eg,ep,ev,re,r,rp)
+	return ep~=tp
+end
+function c100406003.cttg1(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsControler(1-tp) and chkc:IsCanAddCounter(0x1041,1) end
+	if chk==0 then return Duel.IsExistingTarget(Card.IsCanAddCounter,tp,0,LOCATION_MZONE,1,nil,0x1041,1) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	Duel.SelectTarget(tp,Card.IsCanAddCounter,tp,0,LOCATION_MZONE,1,1,nil,0x1041,1)
+end
+function c100406003.ctop1(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) and tc:AddCounter(0x1041,1) and tc:GetLevel()>1 then
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_CHANGE_LEVEL)
+		e1:SetValue(1)
+		e1:SetCondition(c100406003.lvcon)
+		e1:SetReset(RESET_EVENT+0x1fe0000)
+		tc:RegisterEffect(e1)
+	end
+end
+function c100406003.ctfilter2(c,mc)
+	return c:IsFaceup() and c:IsLevelBelow(mc:GetLevel()) and c:IsControlerCanBeChanged()
+end
+function c100406003.cttg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	local c=e:GetHandler()
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) and c100406003.ctfilter2(chkc,c) end
+	if chk==0 then return Duel.IsExistingTarget(c100406003.ctfilter2,tp,0,LOCATION_MZONE,1,nil,c) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CONTROL)
+	local g=Duel.SelectTarget(tp,c100406003.ctfilter2,tp,0,LOCATION_MZONE,1,1,nil,c)
+	Duel.SetOperationInfo(0,CATEGORY_CONTROL,g,1,0,0)
+end
+function c100406003.ctop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) then
+		Duel.GetControl(tc,tp,PHASE_END,1)
+	end
+end
