096d67c38b3ec7da807e1617f181e3452bd35bed
nekrozar <unyuho.buster@gmail.com>
2016-10-18 18:30:58 +0900

    add script 20AP

---
+++ scripts/20AP-JP/c100213005.lua
@@ -0,0 +1,84 @@
+--E・HERO オネスティ・ネオス
+--Elemental HERO Honesty Neos
+--Script by nekrozar
+function c100213005.initial_effect(c)
+	--atkup
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(100213005,0))
+	e1:SetCategory(CATEGORY_ATKCHANGE)
+	e1:SetType(EFFECT_TYPE_QUICK_O)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DAMAGE_STEP)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetHintTiming(TIMING_DAMAGE_STEP)
+	e1:SetCountLimit(1,100213005)
+	e1:SetCondition(c100213005.atkcon)
+	e1:SetCost(c100213005.atkcost1)
+	e1:SetTarget(c100213005.atktg)
+	e1:SetOperation(c100213005.atkop1)
+	c:RegisterEffect(e1)
+	--atkup
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(100213005,1))
+	e2:SetCategory(CATEGORY_ATKCHANGE)
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
+	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetHintTiming(TIMING_DAMAGE_STEP)
+	e2:SetCountLimit(1,100213105)
+	e2:SetCondition(c100213005.atkcon)
+	e2:SetTarget(c100213005.atkcost2)
+	e2:SetOperation(c100213005.atkop2)
+	c:RegisterEffect(e2)
+end
+function c100213005.atkcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetCurrentPhase()~=PHASE_DAMAGE or not Duel.IsDamageCalculated()
+end
+function c100213005.atkcost1(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsDiscardable() end
+	Duel.SendtoGrave(e:GetHandler(),REASON_COST+REASON_DISCARD)
+end
+function c100213005.atkfilter(c)
+	return c:IsFaceup() and c:IsSetCard(0x8)
+end
+function c100213005.atktg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_MZONE) and c100213005.atkfilter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(c100213005.atkfilter,tp,LOCATION_MZONE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+	Duel.SelectTarget(tp,c100213005.atkfilter,tp,LOCATION_MZONE,0,1,1,nil)
+end
+function c100213005.atkop1(e,tp,eg,ep,ev,re,r,rp,chk)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) and tc:IsFaceup() then
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetValue(2500)
+		e1:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END)
+		tc:RegisterEffect(e1)
+	end
+end
+function c100213005.costfilter(c)
+	return c:IsSetCard(0x8) and c:GetAttack()>0 and c:IsDiscardable()
+end
+function c100213005.atkcost2(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c100213005.costfilter,tp,LOCATION_HAND,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DISCARD)
+	local g=Duel.SelectMatchingCard(tp,c100213005.costfilter,tp,LOCATION_HAND,0,1,1,nil)
+	e:SetLabelObject(g:GetFirst())
+	Duel.SendtoGrave(g,REASON_COST+REASON_DISCARD)
+end
+function c100213005.atkop2(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tc=e:GetLabelObject()
+	local atk=tc:GetAttack()
+	if c:IsRelateToEffect() and c:IsFaceup() then
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetValue(atk)
+		e1:SetReset(RESET_EVENT+0x1ff0000+RESET_PHASE+PHASE_END)
+		c:RegisterEffect(e1)
+	end
+end
