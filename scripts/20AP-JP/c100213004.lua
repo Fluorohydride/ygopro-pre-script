d9f04e69c6524e9c8f320b427cda724c6440438b
dest <dest.dn@gmail.com>
2016-10-19 00:11:54 +0200

    Add 20AP 04+Power Wall

---
+++ scripts/20AP-JP/c100213004.lua
@@ -0,0 +1,80 @@
+--ハーピィの羽根吹雪
+--Harpie's Feather Storm
+--Script by dest
+function c100213004.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCondition(c100213004.condition)
+	e1:SetOperation(c100213004.activate)
+	c:RegisterEffect(e1)
+	--act in hand
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetCode(EFFECT_TRAP_ACT_IN_HAND)
+	e2:SetCondition(c100213004.handcon)
+	c:RegisterEffect(e2)
+	--search
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(100213004,0))
+	e3:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e3:SetCode(EVENT_DESTROYED)
+	e3:SetProperty(EFFECT_FLAG_DELAY)
+	e3:SetCondition(c100213004.thcon)
+	e3:SetTarget(c100213004.thtg)
+	e3:SetOperation(c100213004.thop)
+	c:RegisterEffect(e3)
+end
+function c100213004.disfilter(c)
+	return c:IsFaceup() and c:IsRace(RACE_WINDBEAST) and c:IsAttribute(ATTRIBUTE_WIND)
+end
+function c100213004.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsExistingMatchingCard(c100213004.disfilter,tp,LOCATION_MZONE,0,1,nil)
+end
+function c100213004.activate(e,tp,eg,ep,ev,re,r,rp)
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetCode(EVENT_CHAIN_SOLVING)
+	e1:SetCondition(c100213004.discon)
+	e1:SetOperation(c100213004.disop)
+	e1:SetReset(RESET_PHASE+PHASE_END)
+	Duel.RegisterEffect(e1,tp)
+end
+function c100213004.discon(e,tp,eg,ep,ev,re,r,rp)
+	return re:IsActiveType(TYPE_MONSTER) and rp==1-tp
+end
+function c100213004.disop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.NegateEffect(ev)
+end
+function c100213004.filter(c)
+	return c:IsFaceup() and c:IsSetCard(0x64)
+end
+function c100213004.handcon(e)
+	return Duel.IsExistingMatchingCard(c100213004.filter,e:GetHandlerPlayer(),LOCATION_MZONE,0,1,nil)
+end
+function c100213004.thcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return rp~=tp and c:IsReason(REASON_EFFECT) 
+		and c:GetPreviousControler()==tp and c:IsPreviousLocation(LOCATION_SZONE)
+end
+function c100213004.thfilter(c)
+	return c:IsCode(18144506) and c:IsAbleToHand()
+end
+function c100213004.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c100213004.thfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK+LOCATION_GRAVE)
+end
+function c100213004.thop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,c100213004.thfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil)
+	if g:GetCount()>0 then
+		if g:GetFirst():IsHasEffect(EFFECT_NECRO_VALLEY) and Duel.IsChainDisablable(0) then
+			Duel.NegateEffect(0)
+			return
+		end
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end
