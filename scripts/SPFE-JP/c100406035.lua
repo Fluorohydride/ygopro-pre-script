eb02001c78042c3cbc079d3b0d8b76aac8cd0218
nekrozar <unyuho.buster@gmail.com>
2016-10-23 19:41:53 +0900

    update
    https://github.com/Fluorohydride/ygopro-scripts/pull/618

--- scripts/SPFE-JP/c100406035.lua
+++ scripts/SPFE-JP/c100406035.lua
@@ -5,7 +5,7 @@ function c100406035.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(100406035,0))
-	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_FUSION_SUMMON)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetTarget(c100406035.sptg)
