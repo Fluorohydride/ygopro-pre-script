8340916c001ef83241d56b52b9c659ea93abdf25
nekrozar <unyuho.buster@gmail.com>
2016-10-28 18:24:29 +0900

    add script SPFE

---
+++ scripts/SPFE-JP/c100406001.lua
@@ -0,0 +1,89 @@
+--捕食植物セラセニアント
+--Predator Plant Sarraceniant
+--Script by nekrozar
+function c100406001.initial_effect(c)
+	--special summon
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(100406001,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_ATTACK_ANNOUNCE)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCondition(c100406001.spcon)
+	e1:SetTarget(c100406001.sptg)
+	e1:SetOperation(c100406001.spop)
+	c:RegisterEffect(e1)
+	--destroy
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(100406001,1))
+	e2:SetCategory(CATEGORY_DESTROY)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_BATTLED)
+	e2:SetTarget(c100406001.destg)
+	e2:SetOperation(c100406001.desop)
+	c:RegisterEffect(e2)
+	--search
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(100406001,2))
+	e3:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e3:SetProperty(EFFECT_FLAG_DELAY)
+	e3:SetCode(EVENT_BATTLE_DESTROYED)
+	e3:SetCountLimit(1,100406001)
+	e3:SetTarget(c100406001.thtg)
+	e3:SetOperation(c100406001.thop)
+	c:RegisterEffect(e3)
+	local e4=e3:Clone()
+	e4:SetCode(EVENT_TO_GRAVE)
+	e4:SetCondition(c100406001.thcon)
+	c:RegisterEffect(e4)
+end
+function c100406001.spcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetAttacker():GetControler()~=tp and Duel.GetAttackTarget()==nil
+end
+function c100406001.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,0,0)
+end
+function c100406001.spop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not c:IsRelateToEffect(e) then return end
+	if Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)==0 and Duel.GetLocationCount(tp,LOCATION_MZONE)<=0
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false) then
+		Duel.SendtoGrave(c,REASON_RULE)
+	end
+end
+function c100406001.destg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local tc=Duel.GetAttacker()
+	if tc==e:GetHandler() then tc=Duel.GetAttackTarget() end
+	if chk==0 then return tc and not tc:IsStatus(STATUS_BATTLE_DESTROYED) and tc:IsRelateToBattle() end
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,tc,1,0,0)
+end
+function c100406001.desop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetAttacker()
+	if tc==e:GetHandler() then tc=Duel.GetAttackTarget() end
+	if tc:IsRelateToBattle() then
+		Duel.Destroy(tc,REASON_EFFECT)
+	end
+end
+function c100406001.thcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return c:IsReason(REASON_EFFECT) and c:IsPreviousLocation(LOCATION_ONFIELD)
+end
+function c100406001.thfilter(c)
+	return c:IsSetCard(0x1f3) and c:IsCode(100406001) and c:IsAbleToHand()
+end
+function c100406001.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c100406001.thfilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function c100406001.thop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,c100406001.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if g:GetCount()>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end
---
+++ scripts/SPFE-JP/c100406002.lua
@@ -0,0 +1,71 @@
+--捕食植物ドロソフィルム・ヒドラ
+--Predator Plant Drosophyllum Hydra
+--Script by nekrozar
+function c100406002.initial_effect(c)
+	--special summon
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e1:SetCode(EFFECT_SPSUMMON_PROC)
+	e1:SetRange(LOCATION_HAND+LOCATION_GRAVE)
+	e1:SetCountLimit(1,100406002)
+	e1:SetCondition(c100406002.hspcon)
+	e1:SetOperation(c100406002.hspop)
+	c:RegisterEffect(e1)
+	--atkdown
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(100406002,0))
+	e2:SetCategory(CATEGORY_ATKCHANGE)
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DAMAGE_STEP)
+	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetRange(LOCATION_MZONE+LOCATION_GRAVE)
+	e2:SetCountLimit(1,100406102)
+	e2:SetHintTiming(TIMING_DAMAGE_STEP)
+	e2:SetCondition(c100406001.atkcon)
+	e2:SetCost(c100406001.atkcost)
+	e2:SetTarget(c100406002.atktg)
+	e2:SetOperation(c100406002.atkop)
+	c:RegisterEffect(e2)
+end
+function c100406001.rfilter(c)
+	return c:GetCounter(0x1041)>0
+end
+function c100406002.hspcon(e,c)
+	if c==nil then return true end
+	return Duel.GetLocationCount(c:GetControler(),LOCATION_MZONE)>-1
+		and Duel.CheckReleaseGroup(c:GetControler(),c100406001.rfilter,1,nil)
+end
+function c100406002.hspop(e,tp,eg,ep,ev,re,r,rp,c)
+	local g=Duel.SelectReleaseGroup(c:GetControler(),c100406001.rfilter,1,1,nil)
+	Duel.Release(g,REASON_COST)
+end
+function c100406002.atkcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetCurrentPhase()~=PHASE_DAMAGE or not Duel.IsDamageCalculated()
+end
+function c100406002.cfilter(c)
+	return c:IsSetCard(0x11f3) and c:IsType(TYPE_MONSTER) and c:IsAbleToRemoveAsCost()
+end
+function c100406002.atkcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c100406002.cfilter,tp,LOCATION_GRAVE,0,1,e:GetHandler()) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local g=Duel.SelectMatchingCard(tp,c100406002.cfilter,tp,LOCATION_GRAVE,0,1,1,e:GetHandler())
+	Duel.Remove(g,POS_FACEUP,REASON_COST)
+end
+function c100406002.atktg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsFaceup() end
+	if chk==0 then return Duel.IsExistingTarget(Card.IsFaceup,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	Duel.SelectTarget(tp,Card.IsFaceup,tp,0,LOCATION_MZONE,1,1,nil)
+end
+function c100406002.atkop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) and tc:IsFaceup() then
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetReset(RESET_EVENT+0x1fe0000)
+		e1:SetValue(-500)
+		tc:RegisterEffect(e1)
+	end
+end
