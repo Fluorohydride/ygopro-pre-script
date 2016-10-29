d971a613a5f3e503c819916c89dc259a5364fe02
mercury233 <me@mercury233.me>
2016-10-29 13:27:19 +0800

    fix

--- scripts/SPFE-JP/c100406002.lua
+++ scripts/SPFE-JP/c100406002.lua
@@ -29,15 +29,22 @@ function c100406002.initial_effect(c)
 	c:RegisterEffect(e2)
 end
 function c100406002.rfilter(c)
-	return c:GetCounter(0x1041)>0
+	return c:GetCounter(0x1041)>0 and c:IsReleasable()
 end
 function c100406002.hspcon(e,c)
 	if c==nil then return true end
-	return Duel.GetLocationCount(c:GetControler(),LOCATION_MZONE)>-1
-		and Duel.CheckReleaseGroup(c:GetControler(),c100406002.rfilter,1,nil)
+	local tp=c:GetControler()
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	local m=0
+	if ft>0 then m=LOCATION_MZONE end
+	return ft>-1 and Duel.IsExistingMatchingCard(c100406002.rfilter,tp,LOCATION_MZONE,m,1,nil)
 end
 function c100406002.hspop(e,tp,eg,ep,ev,re,r,rp,c)
-	local g=Duel.SelectReleaseGroup(c:GetControler(),c100406002.rfilter,1,1,nil)
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	local m=0
+	if ft>0 then m=LOCATION_MZONE end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
+	local g=Duel.SelectMatchingCard(tp,c100406002.rfilter,tp,LOCATION_MZONE,m,1,1,nil)
 	Duel.Release(g,REASON_COST)
 end
 function c100406002.atkcon(e,tp,eg,ep,ev,re,r,rp)
@@ -56,7 +63,7 @@ function c100406002.atktg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsFaceup() end
 	if chk==0 then return Duel.IsExistingTarget(Card.IsFaceup,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
-	Duel.SelectTarget(tp,Card.IsFaceup,tp,0,LOCATION_MZONE,1,1,nil)
+	Duel.SelectTarget(tp,Card.IsFaceup,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
 end
 function c100406002.atkop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
