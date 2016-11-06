21d1972a939a6e5fc6f40e13e25717a9e092e80e
mercury233 <me@mercury233.me>
2016-11-07 00:53:38 +0800

    change id

--- scripts/INOV-EN/c39581190.lua
+++ scripts/INOV-EN/c39581190.lua
@@ -1,66 +1,65 @@
 --Subterror Nemesis Archer
---Script by dest
-function c100910082.initial_effect(c)
+function c39581190.initial_effect(c)
 	--to deck
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(100910082,0))
+	e1:SetDescription(aux.Stringid(39581190,0))
 	e1:SetCategory(CATEGORY_TODECK)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e1:SetCode(EVENT_BATTLE_START)
-	e1:SetCountLimit(1,100910082)
-	e1:SetCondition(c100910082.tdcon)
-	e1:SetTarget(c100910082.tdtg)
-	e1:SetOperation(c100910082.tdop)
+	e1:SetCountLimit(1,39581190)
+	e1:SetCondition(c39581190.tdcon)
+	e1:SetTarget(c39581190.tdtg)
+	e1:SetOperation(c39581190.tdop)
 	c:RegisterEffect(e1)
 	--spsummon
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(100910082,1))
+	e2:SetDescription(aux.Stringid(39581190,1))
 	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e2:SetProperty(EFFECT_FLAG_DELAY)
 	e2:SetCode(EVENT_TO_GRAVE)
-	e2:SetCountLimit(1,100910182)
-	e2:SetCondition(c100910082.spcon)
-	e2:SetTarget(c100910082.sptg)
-	e2:SetOperation(c100910082.spop)
+	e2:SetCountLimit(1,39581191)
+	e2:SetCondition(c39581190.spcon)
+	e2:SetTarget(c39581190.sptg)
+	e2:SetOperation(c39581190.spop)
 	c:RegisterEffect(e2)
 end
-function c100910082.cfilter(c)
+function c39581190.cfilter(c)
 	return c:IsFaceup() and c:IsSetCard(0xed)
 end
-function c100910082.tdcon(e,tp,eg,ep,ev,re,r,rp)
+function c39581190.tdcon(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local d=Duel.GetAttackTarget()
 	return c==Duel.GetAttacker() and d and d:IsPosition(POS_FACEDOWN_DEFENSE)
-		and Duel.IsExistingMatchingCard(c100910082.cfilter,tp,LOCATION_MZONE,0,1,c)
+		and Duel.IsExistingMatchingCard(c39581190.cfilter,tp,LOCATION_MZONE,0,1,c)
 end
-function c100910082.tdtg(e,tp,eg,ep,ev,re,r,rp,chk)
+function c39581190.tdtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local d=Duel.GetAttackTarget()
 	if chk==0 then return d:IsAbleToDeck() end
 	Duel.SetOperationInfo(0,CATEGORY_TODECK,d,1,0,0)
 end
-function c100910082.tdop(e,tp,eg,ep,ev,re,r,rp)
+function c39581190.tdop(e,tp,eg,ep,ev,re,r,rp)
 	local d=Duel.GetAttackTarget()
 	if d:IsRelateToBattle() and d:IsPosition(POS_FACEDOWN_DEFENSE) then
 		Duel.SendtoDeck(d,nil,2,REASON_EFFECT)
 	end
 end
-function c100910082.spcon(e,tp,eg,ep,ev,re,r,rp)
+function c39581190.spcon(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	return c:IsPreviousLocation(LOCATION_ONFIELD) and c:IsReason(REASON_DESTROY) and c:IsReason(REASON_BATTLE+REASON_EFFECT)
 end
-function c100910082.spfilter(c,e,tp)
+function c39581190.spfilter(c,e,tp)
 	return c:IsSetCard(0xed) and (c:IsCanBeSpecialSummoned(e,0,tp,false,false) or c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEDOWN))
 end
-function c100910082.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+function c39581190.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingMatchingCard(c100910082.spfilter,tp,LOCATION_DECK,0,1,nil,e,tp) end
+		and Duel.IsExistingMatchingCard(c39581190.spfilter,tp,LOCATION_DECK,0,1,nil,e,tp) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
 end
-function c100910082.spop(e,tp,eg,ep,ev,re,r,rp)
+function c39581190.spop(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectMatchingCard(tp,c100910082.spfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
+	local g=Duel.SelectMatchingCard(tp,c39581190.spfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
 	local tc=g:GetFirst()
 	if not tc then return end
 	local spos=0
--- scripts/INOV-EN/c54631665.lua
+++ scripts/INOV-EN/c54631665.lua
@@ -1,6 +1,5 @@
 --SPYRAL Resort
---Script by nekrozar
-function c100910089.initial_effect(c)
+function c54631665.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
@@ -12,18 +11,18 @@ function c100910089.initial_effect(c)
 	e2:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
 	e2:SetRange(LOCATION_FZONE)
 	e2:SetTargetRange(LOCATION_ONFIELD,0)
-	e2:SetTarget(c100910089.tgtg)
+	e2:SetTarget(c54631665.tgtg)
 	e2:SetValue(aux.tgoval)
 	c:RegisterEffect(e2)
 	--search
 	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(100910089,0))
+	e3:SetDescription(aux.Stringid(54631665,0))
 	e3:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
 	e3:SetType(EFFECT_TYPE_IGNITION)
 	e3:SetRange(LOCATION_FZONE)
 	e3:SetCountLimit(1)
-	e3:SetTarget(c100910089.thtg)
-	e3:SetOperation(c100910089.thop)
+	e3:SetTarget(c54631665.thtg)
+	e3:SetOperation(c54631665.thop)
 	c:RegisterEffect(e3)
 	--maintain
 	local e4=Effect.CreateEffect(c)
@@ -32,44 +31,44 @@ function c100910089.initial_effect(c)
 	e4:SetCode(EVENT_PHASE+PHASE_END)
 	e4:SetRange(LOCATION_FZONE)
 	e4:SetCountLimit(1)
-	e4:SetCondition(c100910089.mtcon)
-	e4:SetOperation(c100910089.mtop)
+	e4:SetCondition(c54631665.mtcon)
+	e4:SetOperation(c54631665.mtop)
 	c:RegisterEffect(e4)
 end
-function c100910089.tgtg(e,c)
+function c54631665.tgtg(e,c)
 	return c:IsSetCard(0xee) and c~=e:GetHandler()
 end
-function c100910089.thfilter(c)
+function c54631665.thfilter(c)
 	return c:IsSetCard(0xee) and c:IsType(TYPE_MONSTER) and c:IsAbleToHand()
 end
-function c100910089.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(c100910089.thfilter,tp,LOCATION_DECK,0,1,nil) end
+function c54631665.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c54631665.thfilter,tp,LOCATION_DECK,0,1,nil) end
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
 end
-function c100910089.thop(e,tp,eg,ep,ev,re,r,rp)
+function c54631665.thop(e,tp,eg,ep,ev,re,r,rp)
 	if not e:GetHandler():IsRelateToEffect(e) then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-	local g=Duel.SelectMatchingCard(tp,c100910089.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+	local g=Duel.SelectMatchingCard(tp,c54631665.thfilter,tp,LOCATION_DECK,0,1,1,nil)
 	if g:GetCount()>0 then
 		Duel.SendtoHand(g,nil,REASON_EFFECT)
 		Duel.ConfirmCards(1-tp,g)
 	end
 end
-function c100910089.mtcon(e,tp,eg,ep,ev,re,r,rp)
+function c54631665.mtcon(e,tp,eg,ep,ev,re,r,rp)
 	return Duel.GetTurnPlayer()==tp
 end
-function c100910089.cfilter(c)
+function c54631665.cfilter(c)
 	return c:IsType(TYPE_MONSTER) and c:IsAbleToDeckAsCost()
 end
-function c100910089.mtop(e,tp,eg,ep,ev,re,r,rp)
+function c54631665.mtop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	Duel.HintSelection(Group.FromCards(c))
-	local g=Duel.GetMatchingGroup(c100910089.cfilter,tp,LOCATION_GRAVE,0,nil)
+	local g=Duel.GetMatchingGroup(c54631665.cfilter,tp,LOCATION_GRAVE,0,nil)
 	local sel=1
 	if g:GetCount()~=0 then
-		sel=Duel.SelectOption(tp,aux.Stringid(100910089,1),aux.Stringid(100910089,2))
+		sel=Duel.SelectOption(tp,aux.Stringid(54631665,1),aux.Stringid(54631665,2))
 	else
-		sel=Duel.SelectOption(tp,aux.Stringid(100910089,2))+1
+		sel=Duel.SelectOption(tp,aux.Stringid(54631665,2))+1
 	end
 	if sel==0 then
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
