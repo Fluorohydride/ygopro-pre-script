2287e1b803da0b12121325a85586b2d40d42d06f
nekrozar <unyuho.buster@gmail.com>
2016-11-10 17:51:50 +0900

    update

--- scripts/SPFE-JP/c100406033.lua
+++ scripts/SPFE-JP/c100406033.lua
@@ -4,7 +4,7 @@
 function c100406033.initial_effect(c)
 	--fusion material
 	c:EnableReviveLimit()
-	aux.AddFusionProcFun2(c,aux.FilterBoolFunction(Card.IsFusionSetCard,0x1f4),c100406033.ffilter,false)
+	aux.AddFusionProcFun2(c,aux.FilterBoolFunction(Card.IsFusionSetCard,0x1f4),aux.FilterEqualFunction(Card.GetSummonLocation,LOCATION_EXTRA),false)
 	--spsummon condition
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
@@ -33,9 +33,6 @@ function c100406033.initial_effect(c)
 	e3:SetTarget(c100406033.rmtg)
 	e3:SetOperation(c100406033.rmop)
 	c:RegisterEffect(e3)
-end
-function c100406033.ffilter(c)
-	return c:GetSummonLocation()==LOCATION_EXTRA
 end
 function c100406033.splimit(e,se,sp,st)
 	return not e:GetHandler():IsLocation(LOCATION_EXTRA) or aux.fuslimit(e,se,sp,st)
