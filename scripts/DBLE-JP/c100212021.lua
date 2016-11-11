a16776dacaf8f26ed0a6154fe1ad30e1e4ef6f38
nekrozar <unyuho.buster@gmail.com>
2016-11-11 20:53:42 +0900

    add script DBLE

---
+++ scripts/DBLE-JP/c100212021.lua
@@ -0,0 +1,73 @@
+--魔界劇団－ティンクル・リトルスター
+--Abyss Actor - Twinkle Littlestar
+--Script by nekrozar
+function c100212021.initial_effect(c)
+	--pendulum summon
+	aux.EnablePendulumAttribute(c)
+	--splimit
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_CANNOT_NEGATE)
+	e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+	e1:SetRange(LOCATION_PZONE)
+	e1:SetTargetRange(1,0)
+	e1:SetTarget(c100212021.splimit)
+	c:RegisterEffect(e1)
+	--attack thrice
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(100212021,0))
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetRange(LOCATION_PZONE)
+	e2:SetCountLimit(1)
+	e2:SetCondition(c100212021.atkcon)
+	e2:SetTarget(c100212021.atktg)
+	e2:SetOperation(c100212021.atkop)
+	c:RegisterEffect(e2)
+	--attack thrice
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_SINGLE)
+	e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e3:SetCode(EFFECT_EXTRA_ATTACK_MONSTER)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetValue(2)
+	c:RegisterEffect(e3)
+end
+function c100212021.splimit(e,c,tp,sumtp,sumpos)
+	return not c:IsSetCard(0x10ec) and bit.band(sumtp,SUMMON_TYPE_PENDULUM)==SUMMON_TYPE_PENDULUM
+end
+function c100212021.atkcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsAbleToEnterBP()
+end
+function c100212021.atkfilter(c)
+	return c:IsFaceup() and c:IsSetCard(0x10ec)
+end
+function c100212021.atktg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and c100212021.atkfilter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(c100212021.atkfilter,tp,LOCATION_MZONE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	Duel.SelectTarget(tp,c100212021.atkfilter,tp,LOCATION_MZONE,0,1,1,nil)
+end
+function c100212021.atkop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) then
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_EXTRA_ATTACK_MONSTER)
+		e1:SetValue(2)
+		e1:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END)
+		tc:RegisterEffect(e1)
+	end
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_CANNOT_ATTACK)
+	e2:SetTargetRange(LOCATION_MZONE,0)
+	e2:SetTarget(c100212021.ftarget)
+	e2:SetLabel(tc:GetFieldID())
+	e2:SetReset(RESET_PHASE+PHASE_END)
+	Duel.RegisterEffect(e2,tp)
+end
+function c100212021.ftarget(e,c)
+	return e:GetLabel()~=c:GetFieldID()
+end
