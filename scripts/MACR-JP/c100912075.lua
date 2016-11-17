f2411c0778aa6019a8370c218fa04343b37d11df
EerieCode <eeriecode@gmail.com>
2016-11-17 12:13:13 +0100

    Update and rename c100912071.lua to c100912075.lua
    The Wikia now lists it as MACR-JP075

--- scripts/MACR-JP/c100912075.lua
+++ scripts/MACR-JP/c100912075.lua
@@ -1,53 +1,53 @@
 --破壊剣士の揺籃
 --Destruction Swordsman's Cradle
 --Scripted by Eerie Code
-function c100912071.initial_effect(c)
+function c100912075.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetCountLimit(1,100912071+EFFECT_COUNT_CODE_OATH)
-	e1:SetCost(c100912071.cost)
-	e1:SetTarget(c100912071.target)
-	e1:SetOperation(c100912071.activate)
+	e1:SetCountLimit(1,100912075+EFFECT_COUNT_CODE_OATH)
+	e1:SetCost(c100912075.cost)
+	e1:SetTarget(c100912075.target)
+	e1:SetOperation(c100912075.activate)
 	c:RegisterEffect(e1)
 	--Protect
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_QUICK_O)
 	e2:SetCode(EVENT_FREE_CHAIN)
 	e2:SetRange(LOCATION_GRAVE)
-	e2:SetCost(c100912071.immcost)
-	e2:SetOperation(c100912071.immop)
+	e2:SetCost(c100912075.immcost)
+	e2:SetOperation(c100912075.immop)
 	c:RegisterEffect(e2)
 end
-function c100912071.cfilter1(c,tp)
-	return c:IsSetCard(0xd6) and not c:IsCode(100912071) and c:IsAbleToGraveAsCost() 
-		and Duel.IsExistingMatchingCard(c100912071.cfilter2,tp,LOCATION_DECK,0,1,c)
+function c100912075.cfilter1(c,tp)
+	return c:IsSetCard(0xd6) and not c:IsCode(100912075) and c:IsAbleToGraveAsCost() 
+		and Duel.IsExistingMatchingCard(c100912075.cfilter2,tp,LOCATION_DECK,0,1,c)
 end
-function c100912071.cfilter2(c)
+function c100912075.cfilter2(c)
 	return c:IsSetCard(0xd7) and c:IsType(TYPE_MONSTER) and c:IsAbleToGraveAsCost()
 end
-function c100912071.cost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(c100912071.cfilter1,tp,LOCATION_DECK,0,1,nil,tp) end
+function c100912075.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c100912075.cfilter1,tp,LOCATION_DECK,0,1,nil,tp) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
-	local g1=Duel.SelectMatchingCard(tp,c100912071.cfilter1,tp,LOCATION_DECK,0,1,1,nil,tp)
+	local g1=Duel.SelectMatchingCard(tp,c100912075.cfilter1,tp,LOCATION_DECK,0,1,1,nil,tp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
-	local g2=Duel.SelectMatchingCard(tp,c100912071.cfilter2,tp,LOCATION_DECK,0,1,1,g1:GetFirst())
+	local g2=Duel.SelectMatchingCard(tp,c100912075.cfilter2,tp,LOCATION_DECK,0,1,1,g1:GetFirst())
 	g1:Merge(g2)
 	Duel.SendtoGrave(g1,REASON_COST)
 end
-function c100912071.filter(c,e,tp)
+function c100912075.filter(c,e,tp)
 	return c:IsCode(11790356) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
-function c100912071.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	local g=Duel.GetMatchingGroup(c100912071.filter,tp,LOCATION_EXTRA+LOCATION_GRAVE,0,nil,e,tp)
+function c100912075.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	local g=Duel.GetMatchingGroup(c100912075.filter,tp,LOCATION_EXTRA+LOCATION_GRAVE,0,nil,e,tp)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and g:GetCount()>0 end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
 end
-function c100912071.activate(e,tp,eg,ep,ev,re,r,rp)
+function c100912075.activate(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<1 then return end
-	local g=Duel.SelectMatchingCard(tp,c100912071.filter,tp,LOCATION_EXTRA+LOCATION_GRAVE,0,1,1,nil,e,tp)
+	local g=Duel.SelectMatchingCard(tp,c100912075.filter,tp,LOCATION_EXTRA+LOCATION_GRAVE,0,1,1,nil,e,tp)
 	local tc=g:GetFirst()
 	if not tc then return end
 	if tc:IsHasEffect(EFFECT_NECRO_VALLEY) and Duel.IsChainDisablable(0) then
@@ -55,13 +55,13 @@ function c100912071.activate(e,tp,eg,ep,ev,re,r,rp)
 		return
 	end
 	if Duel.SpecialSummonStep(tc,0,tp,tp,false,false,POS_FACEUP) then
-		tc:RegisterFlagEffect(100912071,RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END,0,2)
+		tc:RegisterFlagEffect(100912075,RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END,0,2)
 		local e1=Effect.CreateEffect(e:GetHandler())
 		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 		e1:SetCode(EVENT_PHASE+PHASE_END)
 		e1:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
-		e1:SetCondition(c100912071.descon)
-		e1:SetOperation(c100912071.desop)
+		e1:SetCondition(c100912075.descon)
+		e1:SetOperation(c100912075.desop)
 		e1:SetReset(RESET_PHASE+PHASE_END,2)
 		e1:SetCountLimit(1)
 		e1:SetLabel(Duel.GetTurnCount())
@@ -70,19 +70,19 @@ function c100912071.activate(e,tp,eg,ep,ev,re,r,rp)
 		Duel.SpecialSummonComplete()
 	end
 end
-function c100912071.descon(e,tp,eg,ep,ev,re,r,rp)
+function c100912075.descon(e,tp,eg,ep,ev,re,r,rp)
 	local tc=e:GetLabelObject()
-	return Duel.GetTurnCount()~=e:GetLabel() and tc:GetFlagEffect(100912071)~=0
+	return Duel.GetTurnCount()~=e:GetLabel() and tc:GetFlagEffect(100912075)~=0
 end
-function c100912071.desop(e,tp,eg,ep,ev,re,r,rp)
+function c100912075.desop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=e:GetLabelObject()
 	Duel.Destroy(tc,REASON_EFFECT)
 end
-function c100912071.immcost(e,tp,eg,ep,ev,re,r,rp,chk)
+function c100912075.immcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return e:GetHandler():IsAbleToRemoveAsCost() end
 	Duel.Remove(e:GetHandler(),POS_FACEUP,REASON_COST)
 end
-function c100912071.immop(e,tp,eg,ep,ev,re,r,rp)
+function c100912075.immop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
