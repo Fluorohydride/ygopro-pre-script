d303b592858a8bee1472861057aadb53c3405a6f
EerieCode <eeriecode@gmail.com>
2016-11-18 20:47:26 +0100

    Add SD31-JP006 --紫毒の魔術師

---
+++ scripts/SD31-JP/c100331006.lua
@@ -0,0 +1,75 @@
+--紫毒の魔術師
+--Violet Poison Magician
+--Scripted by Eerie Code
+function c100331006.initial_effect(c)
+    aux.EnablePendulumAttribute(c)
+    --Increase ATK
+    local e1=Effect.CreateEffect(c)
+    e1:SetDescription(aux.Stringid(100331006,0))
+    e1:SetCategory(CATEGORY_ATKCHANGE+CATEGORY_DESTROY)
+    e1:SetType(EFFECT_TYPE_QUICK_O)
+    e1:SetRange(LOCATION_PZONE)
+    e1:SetCode(EVENT_PRE_DAMAGE_CALCULATE)
+    e1:SetCountLimit(1)
+    e1:SetCondition(c100331006.atkcon)
+    e1:SetTarget(c100331006.atktg)
+    e1:SetOperation(c100331006.atkop)
+    c:RegisterEffect(e1)
+    --Destroy
+    local e3=Effect.CreateEffect(c)
+    e3:SetDescription(aux.Stringid(100331006,1))
+    e3:SetCategory(CATEGORY_DESTROY)
+    e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+    e3:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
+    e3:SetCode(EVENT_DESTROYED)
+    e3:SetCondition(c100331006.descon)
+    e3:SetTarget(c100331006.destg)
+    e3:SetOperation(c100331006.desop)
+    c:RegisterEffect(e3)
+end
+function c100331006.atkcon(e,tp,eg,ep,ev,re,r,rp)
+    local a=Duel.GetAttacker()
+    local d=Duel.GetAttackTarget()
+    if not a:IsControler(tp) then
+        if not d then return false end
+        a=d
+    end
+    return a:IsControler(tp) and a:IsAttribute(ATTRIBUTE_DARK) and a:IsRace(RACE_SPELLCASTER)
+end
+function c100331006.atktg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return e:GetHandler():IsDestructable() end
+    Duel.SetOperationInfo(0,CATEGORY_DESTROY,e:GetHandler(),1,0,0)
+end
+function c100331006.atkop(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    if not c:IsRelateToEffect(e) then return end
+    local tc=Duel.GetAttacker()
+    if tc:IsControler(1-tp) then tc=Duel.GetAttackTarget() end
+    local e1=Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_SINGLE)
+    e1:SetCode(EFFECT_UPDATE_ATTACK)
+    e1:SetValue(1200)
+    e1:SetReset(RESET_PHASE+PHASE_DAMAGE)
+    tc:RegisterEffect(e1)
+    Duel.BreakEffect()
+    Duel.Destroy(c,REASON_EFFECT)
+end
+function c100331006.descon(e,tp,eg,ep,ev,re,r,rp)
+    return bit.band(r,REASON_EFFECT+REASON_BATTLE)~=0
+end
+function c100331006.desfilter(c)
+    return c:IsFaceup() and c:IsDestructable()
+end
+function c100331006.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+    if chkc then return chkc:IsOnField() and c100331006.desfilter(chkc) end
+    if chk==0 then return Duel.IsExistingTarget(c100331006.desfilter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil) end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+    local g=Duel.SelectTarget(tp,c100331006.desfilter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil)
+    Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
+end
+function c100331006.desop(e,tp,eg,ep,ev,re,r,rp)
+    local tc=Duel.GetFirstTarget()
+    if tc:IsRelateToEffect(e) then
+        Duel.Destroy(tc,REASON_EFFECT)
+    end
+end
