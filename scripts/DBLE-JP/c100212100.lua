3f252cab8a98eeeed210f6361405d58f61cccae3
EerieCode <eeriecode@gmail.com>
2016-11-18 13:28:40 +0100

    Add DBLE-JP0?? --捕食生成

---
+++ scripts/DBLE-JP/c100212100.lua
@@ -0,0 +1,72 @@
+--捕食生成
+--Predator Craft
+--Scripted by Eerie Code
+function c100212100.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_COUNTER)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetTarget(c100212100.target)
+	e1:SetOperation(c100212100.activate)
+	c:RegisterEffect(e1)
+	--destroy replace
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e2:SetCode(EFFECT_DESTROY_REPLACE)
+	e2:SetRange(LOCATION_GRAVE)
+	e2:SetTarget(c100212100.reptg)
+	e2:SetValue(c100212100.repval)
+	e2:SetOperation(c100212100.repop)
+	c:RegisterEffect(e2)
+end
+function c100212100.cfilter(c)
+	return c:IsSetCard(0xf3) and not c:IsPublic()
+end
+function c100212100.filter(c,e)
+	return c:IsCanAddCounter(0x1041,1) and c:IsCanBeEffectTarget(e)
+end
+function c100212100.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	local hg=Duel.GetMatchingGroup(c100212100.cfilter,tp,LOCATION_HAND,0,e:GetHandler())
+	local tc=Duel.GetMatchingGroupCount(c100212100.filter,tp,0,LOCATION_MZONE,nil,e)
+	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_MZONE) and chkc:IsCanAddCounter(0x1041,1) end
+	if chk==0 then return hg:GetCount()>0 and tc>0 end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CONFIRM)
+	local g=hg:Select(tp,1,tc,nil)
+	Duel.ConfirmCards(1-tp,g)
+	Duel.SelectTarget(tp,Card.IsCanAddCounter,tp,0,LOCATION_MZONE,g:GetCount(),g:GetCount(),nil,0x1041,1)
+end
+function c100212100.activate(e,tp,eg,ep,ev,re,r,rp)
+	local tg=Duel.GetChaininfo(0,CHAININFO_TARGET_CARDS):Filter(Card.IsRelateToEffect,nil,e)
+	local tc=tg:GetFirst()
+	while tc do
+		if tc:AddCounter(0x1041,1) and tc:GetLevel()>1 then
+			local e1=Effect.CreateEffect(e:GetHandler())
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetCode(EFFECT_CHANGE_LEVEL)
+			e1:SetReset(RESET_EVENT+0x1fe0000)
+			e1:SetCondition(c100212100.lvcon)
+			e1:SetValue(1)
+			tc:RegisterEffect(e1)
+		end
+		tc=tg:GetNext()
+	end
+end
+function c100212100.lvcon(e)
+	return e:GetHandler():GetCounter(0x1041)>0
+end
+function c100212100.repfilter(c,tp)
+	return c:IsFaceup() and c:IsSetCard(0x10f3) and c:IsLocation(LOCATION_MZONE)
+		and c:IsControler(tp) and c:IsReason(REASON_BATTLE)
+end
+function c100212100.reptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsAbleToRemove() and eg:IsExists(c100212100.repfilter,1,nil,tp) end
+	return Duel.SelectYesNo(tp,aux.Stringid(100212100,0))
+end
+function c100212100.repval(e,c)
+	return c100212100.repfilter(c,e:GetHandlerPlayer())
+end
+function c100212100.repop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Remove(e:GetHandler(),POS_FACEUP,REASON_EFFECT)
+end
