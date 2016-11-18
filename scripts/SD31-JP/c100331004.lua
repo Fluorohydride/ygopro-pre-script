5210393e6f7a540df7f65e9c4411a8645c8c618f
EerieCode <eeriecode@gmail.com>
2016-11-18 14:34:25 +0100

    Added SD31-JP004 --黒牙の魔術師

---
+++ scripts/SD31-JP/c100331004.lua
@@ -0,0 +1,69 @@
+--黒牙の魔術師
+--Black Fang Magician
+--Scripted by Eerie Code
+function c100331004.initial_effect(c)
+	aux.EnablePendulumAttribute(c)
+	--Halve ATK
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(100331004,0))
+	e1:SetCategory(CATEGORY_ATKCHANGE+CATEGORY_DESTROY)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_PZONE)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetTarget(c100331004.atktg)
+	e1:SetOperation(c100331004.atkop)
+	c:RegisterEffect(e1)
+	--Special Summon
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(100331004,1))
+	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e3:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
+	e3:SetCode(EVENT_DESTROYED)
+	e3:SetCondition(c100331004.spcon)
+	e3:SetTarget(c100331004.sptg)
+	e3:SetOperation(c100331004.spop)
+	c:RegisterEffect(e3)
+end
+function c100331004.atktg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) and chkc:IsFaceup() end
+	if chk==0 then return Duel.IsExistingTarget(Card.IsFaceup,tp,0,LOCATION_MZONE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	Duel.SelectTarget(tp,Card.IsFaceup,tp,0,LOCATION_MZONE,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,e:GetHandler(),1,0,0)
+end
+function c100331004.atkop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:Gethandler()
+	local tc=Duel.GetFirstTarget()
+	if c:IsRelateToEffect(e) and tc:IsFaceup() and tc:IsRelateToEffect(e) then
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_SET_ATTACK_FINAL)
+		e1:SetValue(math.floor(tc:GetAttack()/2))
+		e1:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END)
+		tc:RegisterEffect(e1)
+		Duel.BreakEffect()
+		Duel.Destroy(c,REASON_EFFECT)
+	end
+end
+function c100331004.spcon(e,tp,eg,ep,ev,re,r,rp)
+	return bit.band(r,REASON_EFFECT+REASON_BATTLE)~=0
+end
+function c100331004.spfilter(c,e,tp)
+	return c:IsAttribute(ATTRIBUTE_DARK) and c:IsRace(RACE_SPELLCASTER) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function c100331004.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and c100331004.spfilter(chkc,e,tp) end
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingTarget(c100331004.spfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectTarget(tp,c100331004.spfilter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
+end
+function c100331004.spop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<1 then return end
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) then
+		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
