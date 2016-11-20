59464289770ae114ca114202f37b0874bf0a82cc
mercury233 <me@mercury233.me>
2016-11-20 21:48:22 +0800

    tab

--- scripts/SD31-JP/c100331001.lua
+++ scripts/SD31-JP/c100331001.lua
@@ -2,167 +2,167 @@
 --Astrograph Sorcerer
 --Scripted by Eerie Code
 function c100331001.initial_effect(c)
-    aux.EnablePendulumAttribute(c)
-    --Replace
-    local e1=Effect.CreateEffect(c)
-    e1:SetDescription(aux.Stringid(100331001,0))
-    e1:SetCategory(CATEGORY_DESTROY)
-    e1:SetType(EFFECT_TYPE_IGNITION)
-    e1:SetRange(LOCATION_PZONE)
-    e1:SetCountLimit(1,100331001)
-    e1:SetTarget(c100331001.rptg)
-    e1:SetOperation(c100331001.rpop)
-    c:RegisterEffect(e1)
-    --Special Summon
-    local e2=Effect.CreateEffect(c)
-    e2:SetDescription(aux.Stringid(100331001,2))
-    e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
-    e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-    e2:SetCode(EVENT_DESTROYED)
-    e2:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
-    e2:SetRange(LOCATION_HAND)
-    e2:SetCondition(c100331001.spcon)
-    e2:SetTarget(c100331001.sptg)
-    e2:SetOperation(c100331001.spop)
-    c:RegisterEffect(e2)
-    --Hitotsu Ni
-    local e3=Effect.CreateEffect(c)
-    e3:SetDescription(aux.Stringid(100331001,4))
-    e3:SetType(EFFECT_TYPE_IGNITION)
-    e3:SetRange(LOCATION_MZONE)
-    e3:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_FUSION_SUMMON)
-    e3:SetCost(c100331001.hncost)
-    e3:SetTarget(c100331001.hntg)
-    e3:SetOperation(c100331001.hnop)
-    c:RegisterEffect(e3)
-    --Register destroyed monsters
-    if not c100331001.global_flag then
-        c100331001.global_flag=true
-        c100331001.desgroup=Group.CreateGroup()
-        c100331001.desgroup:KeepAlive()
-        local ge1=Effect.CreateEffect(c)
-        ge1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-        ge1:SetCode(EVENT_DESTROYED)
-        ge1:SetOperation(c100331001.regop)
-        Duel.RegisterEffect(ge1,0)
-        local ge2=Effect.CreateEffect(c)
-        ge2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-        ge2:SetCode(EVENT_PHASE_START+PHASE_DRAW)
-        ge2:SetOperation(c100331001.resetop)
-        Duel.RegisterEffect(ge2,0)
-    end
+	aux.EnablePendulumAttribute(c)
+	--Replace
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(100331001,0))
+	e1:SetCategory(CATEGORY_DESTROY)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_PZONE)
+	e1:SetCountLimit(1,100331001)
+	e1:SetTarget(c100331001.rptg)
+	e1:SetOperation(c100331001.rpop)
+	c:RegisterEffect(e1)
+	--Special Summon
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(100331001,2))
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_DESTROYED)
+	e2:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
+	e2:SetRange(LOCATION_HAND)
+	e2:SetCondition(c100331001.spcon)
+	e2:SetTarget(c100331001.sptg)
+	e2:SetOperation(c100331001.spop)
+	c:RegisterEffect(e2)
+	--Hitotsu Ni
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(100331001,4))
+	e3:SetType(EFFECT_TYPE_IGNITION)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_FUSION_SUMMON)
+	e3:SetCost(c100331001.hncost)
+	e3:SetTarget(c100331001.hntg)
+	e3:SetOperation(c100331001.hnop)
+	c:RegisterEffect(e3)
+	--Register destroyed monsters
+	if not c100331001.global_flag then
+		c100331001.global_flag=true
+		c100331001.desgroup=Group.CreateGroup()
+		c100331001.desgroup:KeepAlive()
+		local ge1=Effect.CreateEffect(c)
+		ge1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		ge1:SetCode(EVENT_DESTROYED)
+		ge1:SetOperation(c100331001.regop)
+		Duel.RegisterEffect(ge1,0)
+		local ge2=Effect.CreateEffect(c)
+		ge2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		ge2:SetCode(EVENT_PHASE_START+PHASE_DRAW)
+		ge2:SetOperation(c100331001.resetop)
+		Duel.RegisterEffect(ge2,0)
+	end
 end
 function c100331001.rpfilter(c,e,tp)
-    return c:IsCode(94415058) and (not c:IsForbidden() or 
-        (Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and c:IsCanBeSpecialSummoned(e,0,tp,false,false)))
+	return c:IsCode(94415058) and (not c:IsForbidden() or 
+		(Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and c:IsCanBeSpecialSummoned(e,0,tp,false,false)))
 end
 function c100331001.rptg(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return e:GetHandler():IsDestructable()
-        and Duel.IsExistingMatchingCard(c100331001.rpfilter,tp,LOCATION_HAND+LOCATION_DECK,0,1,nil,e,tp) end
-    Duel.SetOperationInfo(0,CATEGORY_DESTROY,e:GetHandler(),1,0,0)
+	if chk==0 then return e:GetHandler():IsDestructable()
+		and Duel.IsExistingMatchingCard(c100331001.rpfilter,tp,LOCATION_HAND+LOCATION_DECK,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,e:GetHandler(),1,0,0)
 end
 function c100331001.rpop(e,tp,eg,ep,ev,re,r,rp)
-    local c=e:GetHandler()
-    if c:IsRelateToEffect(e) and Duel.Destroy(c,REASON_EFFECT)>0 then
-        Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
-        local g=Duel.SelectMatchingCard(tp,c100331001.rpfilter,tp,LOCATION_HAND+LOCATION_DECK,0,1,1,nil,e,tp)
-        local tc=g:GetFirst()
-        local op=0
-        if Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and tc:IsCanBeSpecialSummoned(e,0,tp,false,false) then
-            op=Duel.SelectOption(tp,aux.Stringid(100331001,1),aux.Stringid(100331001,2))
-        else
-            op=Duel.SelectOption(tp,aux.Stringid(100331001,1))
-        end
-        if op==0 then
-            Duel.MoveToField(tc,tp,tp,LOCATION_SZONE,POS_FACEUP,true)
-        else
-            Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
-        end
-    end
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) and Duel.Destroy(c,REASON_EFFECT)>0 then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+		local g=Duel.SelectMatchingCard(tp,c100331001.rpfilter,tp,LOCATION_HAND+LOCATION_DECK,0,1,1,nil,e,tp)
+		local tc=g:GetFirst()
+		local op=0
+		if Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and tc:IsCanBeSpecialSummoned(e,0,tp,false,false) then
+			op=Duel.SelectOption(tp,aux.Stringid(100331001,1),aux.Stringid(100331001,2))
+		else
+			op=Duel.SelectOption(tp,aux.Stringid(100331001,1))
+		end
+		if op==0 then
+			Duel.MoveToField(tc,tp,tp,LOCATION_SZONE,POS_FACEUP,true)
+		else
+			Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
+		end
+	end
 end
 function c100331001.regop(e,tp,eg,ep,ev,re,r,rp)
-    if not eg then return end
-    local tc=eg:GetFirst()
-    while tc do
-        c100331001.desgroup:AddCard(tc)
-        tc=eg:GetNext()
-    end
+	if not eg then return end
+	local tc=eg:GetFirst()
+	while tc do
+		c100331001.desgroup:AddCard(tc)
+		tc=eg:GetNext()
+	end
 end
 function c100331001.resetop(e,tp,eg,ep,ev,re,r,rp)
-    c100331001.desgroup:Clear()
+	c100331001.desgroup:Clear()
 end
 function c100331001.spcfilter(c,tp)
-    return c:IsReason(REASON_BATTLE+REASON_EFFECT) and c:GetPreviousControler()==tp
+	return c:IsReason(REASON_BATTLE+REASON_EFFECT) and c:GetPreviousControler()==tp
 end
 function c100331001.spcon(e,tp,eg,ep,ev,re,r,rp)
-    return eg and eg:IsExists(c100331001.spcfilter,1,nil,tp)
+	return eg and eg:IsExists(c100331001.spcfilter,1,nil,tp)
 end
 function c100331001.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
-    local c=e:GetHandler()
-    if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and c:IsCanBeSpecialSummoned(e,0,tp,false,false) end
-    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,0,0)
+	local c=e:GetHandler()
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and c:IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,0,0)
 end
 function c100331001.thfilter(c)
-    return c:IsType(TYPE_MONSTER) and c:IsAbleToHand() and c100331001.desgroup:IsExists(c100331001.thfilter2,1,nil,c:GetOriginalCode())
+	return c:IsType(TYPE_MONSTER) and c:IsAbleToHand() and c100331001.desgroup:IsExists(c100331001.thfilter2,1,nil,c:GetOriginalCode())
 end
 function c100331001.thfilter2(c,cd)
-    return c:GetOriginalCode()==cd
+	return c:GetOriginalCode()==cd
 end
 function c100331001.spop(e,tp,eg,ep,ev,re,r,rp)
-    local c=e:GetHandler()
-    if not c:IsRelateToEffect(e) then return end
-    if Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)==0 and Duel.GetLocationCount(tp,LOCATION_MZONE)<=0
-        and c:IsCanBeSpecialSummoned(e,0,tp,false,false) then
-        Duel.SendtoGrave(c,REASON_RULE)
-        return
-    end
-    local g=Duel.GetMatchingGroup(c100331001.thfilter,tp,LOCATION_DECK,0,nil)
-    if g:GetCount()>0 and Duel.SelectYesNo(tp,aux.Stringid(100331001,3)) then
+	local c=e:GetHandler()
+	if not c:IsRelateToEffect(e) then return end
+	if Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)==0 and Duel.GetLocationCount(tp,LOCATION_MZONE)<=0
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false) then
+		Duel.SendtoGrave(c,REASON_RULE)
+		return
+	end
+	local g=Duel.GetMatchingGroup(c100331001.thfilter,tp,LOCATION_DECK,0,nil)
+	if g:GetCount()>0 and Duel.SelectYesNo(tp,aux.Stringid(100331001,3)) then
 		Duel.BreakEffect()
-        Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-        local sg=g:Select(tp,1,1,nil)
-        Duel.SendtoHand(sg,nil,REASON_EFFECT)
-        Duel.ConfirmCards(1-tp,sg)
-    end
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+		local sg=g:Select(tp,1,1,nil)
+		Duel.SendtoHand(sg,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,sg)
+	end
 end
 function c100331001.hncfilter(c,sc)
-    return c:IsSetCard(sc) and c:IsType(TYPE_MONSTER) and c:IsAbleToRemoveAsCost()
+	return c:IsSetCard(sc) and c:IsType(TYPE_MONSTER) and c:IsAbleToRemoveAsCost()
 end
 function c100331001.hncost(e,tp,eg,ep,ev,re,r,rp,chk)
-    local c=e:GetHandler()
-    if chk==0 then return c:IsAbleToRemoveAsCost()
-        and Duel.IsExistingMatchingCard(c100331001.hncfilter,tp,LOCATION_HAND+LOCATION_MZONE+LOCATION_GRAVE,0,1,nil,0xf2)
-        and Duel.IsExistingMatchingCard(c100331001.hncfilter,tp,LOCATION_HAND+LOCATION_MZONE+LOCATION_GRAVE,0,1,nil,0x1046)
-        and Duel.IsExistingMatchingCard(c100331001.hncfilter,tp,LOCATION_HAND+LOCATION_MZONE+LOCATION_GRAVE,0,1,nil,0x1f5)
-        and Duel.IsExistingMatchingCard(c100331001.hncfilter,tp,LOCATION_HAND+LOCATION_MZONE+LOCATION_GRAVE,0,1,nil,0x2073)
-    end
-    local g=Group.FromCards(c)
-    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
-    local g1=Duel.SelectMatchingCard(tp,c100331001.hncfilter,tp,LOCATION_HAND+LOCATION_MZONE+LOCATION_GRAVE,0,1,1,nil,0xf2)
-    g:Merge(g1)
-    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
-    local g1=Duel.SelectMatchingCard(tp,c100331001.hncfilter,tp,LOCATION_HAND+LOCATION_MZONE+LOCATION_GRAVE,0,1,1,nil,0x1046)
-    g:Merge(g1)
-    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
-    local g1=Duel.SelectMatchingCard(tp,c100331001.hncfilter,tp,LOCATION_HAND+LOCATION_MZONE+LOCATION_GRAVE,0,1,1,nil,0x1f5)
-    g:Merge(g1)
-    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
-    local g1=Duel.SelectMatchingCard(tp,c100331001.hncfilter,tp,LOCATION_HAND+LOCATION_MZONE+LOCATION_GRAVE,0,1,1,nil,0x2073)
-    g:Merge(g1)
-    Duel.Remove(g,POS_FACEUP,REASON_COST)
+	local c=e:GetHandler()
+	if chk==0 then return c:IsAbleToRemoveAsCost()
+		and Duel.IsExistingMatchingCard(c100331001.hncfilter,tp,LOCATION_HAND+LOCATION_MZONE+LOCATION_GRAVE,0,1,nil,0xf2)
+		and Duel.IsExistingMatchingCard(c100331001.hncfilter,tp,LOCATION_HAND+LOCATION_MZONE+LOCATION_GRAVE,0,1,nil,0x1046)
+		and Duel.IsExistingMatchingCard(c100331001.hncfilter,tp,LOCATION_HAND+LOCATION_MZONE+LOCATION_GRAVE,0,1,nil,0x1f5)
+		and Duel.IsExistingMatchingCard(c100331001.hncfilter,tp,LOCATION_HAND+LOCATION_MZONE+LOCATION_GRAVE,0,1,nil,0x2073)
+	end
+	local g=Group.FromCards(c)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local g1=Duel.SelectMatchingCard(tp,c100331001.hncfilter,tp,LOCATION_HAND+LOCATION_MZONE+LOCATION_GRAVE,0,1,1,nil,0xf2)
+	g:Merge(g1)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local g1=Duel.SelectMatchingCard(tp,c100331001.hncfilter,tp,LOCATION_HAND+LOCATION_MZONE+LOCATION_GRAVE,0,1,1,nil,0x1046)
+	g:Merge(g1)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local g1=Duel.SelectMatchingCard(tp,c100331001.hncfilter,tp,LOCATION_HAND+LOCATION_MZONE+LOCATION_GRAVE,0,1,1,nil,0x1f5)
+	g:Merge(g1)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local g1=Duel.SelectMatchingCard(tp,c100331001.hncfilter,tp,LOCATION_HAND+LOCATION_MZONE+LOCATION_GRAVE,0,1,1,nil,0x2073)
+	g:Merge(g1)
+	Duel.Remove(g,POS_FACEUP,REASON_COST)
 end
 function c100331001.hnfilter(c,e,tp)
-    return c:IsFacedown() and c:IsCode(100912036) and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_FUSION,tp,false,false)
+	return c:IsFacedown() and c:IsCode(100912036) and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_FUSION,tp,false,false)
 end
 function c100331001.hntg(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>-1 and Duel.IsExistingMatchingCard(c100331001.hnfilter,tp,LOCATION_EXTRA,0,1,nil,e,tp) end
-    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>-1 and Duel.IsExistingMatchingCard(c100331001.hnfilter,tp,LOCATION_EXTRA,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
 end
 function c100331001.hnop(e,tp,eg,ep,ev,re,r,rp)
-    if Duel.GetLocationCount(tp,LOCATION_MZONE)<1 then return end
-    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-    local g=Duel.SelectMatchingCard(tp,c100331001.hnfilter,tp,LOCATION_EXTRA,0,1,1,nil,e,tp)
-    if g:GetCount()>0 then
-        Duel.SpecialSummon(g,SUMMON_TYPE_FUSION,tp,tp,false,false,POS_FACEUP)
-    end
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<1 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,c100331001.hnfilter,tp,LOCATION_EXTRA,0,1,1,nil,e,tp)
+	if g:GetCount()>0 then
+		Duel.SpecialSummon(g,SUMMON_TYPE_FUSION,tp,tp,false,false,POS_FACEUP)
+	end
 end
