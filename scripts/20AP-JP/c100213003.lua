bb65711ab03604afc13eb1dc87a62b99b530785d
EerieCode <eeriecode@gmail.com>
2016-11-21 07:11:59 +0100

    Add 20AP-JP003 --ディアバウンド・カーネル

---
+++ scripts/20AP-JP/c100213003.lua
@@ -0,0 +1,74 @@
+--ディアバウンド・カーネル
+--Diabound Kernel
+--Scripted by Eerie Code
+function c100213003.initial_effect(c)
+    --Increase ATK
+    local e1=Effect.CreateEffect(c)
+    e1:SetDescription(aux.Stringid(100213003,0))
+    e1:SetCategory(CATEGORY_ATKCHANGE)
+    e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+    e1:SetCode(EVENT_ATTACK_ANNOUNCE)
+    e1:SetOperation(c100213003.atkup)
+    c:RegisterEffect(e1)
+    --Decrease ATK
+    local e2=Effect.CreateEffect(c)
+    e2:SetDescription(aux.Stringid(100213003,1))
+    e2:SetCategory(CATEGORY_ATKCHANGE+CATEGORY_REMOVE)
+    e2:SetType(EFFECT_TYPE_QUICK_O)
+    e2:SetCode(EVENT_FREE_CHAIN)
+    e2:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_CARD_TARGET)
+    e2:SetTarget(c100213003.atktg)
+    e2:SetOperation(c100213003.atkop)
+    c:RegisterEffect(e2)
+end
+function c100213003.atkup(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    local e1=Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_SINGLE)
+    e1:SetCode(EFFECT_UPDATE_ATTACK)
+    e1:SetValue(600)
+    e1:SetReset(RESET_EVENT+0x1fe0000)
+    c:RegisterEffect(e1)
+end
+function c100213003.atktg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+    if chkc then chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) and chkc:IsFaceup() end
+    if chk==0 then return e:GetHandler():IsAbleToRemove() and Duel.IsExistingTarget(Card.IsFaceup,tp,0,LOCATION_MZONE,1,nil) end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+    Duel.SelectTarget(tp,Card.IsFaceup,tp,0,LOCATION_MZONE,1,1,nil)
+    Duel.SetOperationInfo(0,CATEGORY_REMOVE,e:GetHandler(),1,0,0)
+end
+function c100213003.atkop(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    local tc=Duel.GetFirstTarget()
+    if c:IsRelateToEffect(e) and c:IsFaceup() and tc:IsRelateToEffect(e) and tc:IsFaceup() then
+        local atk=c:GetAttack()
+        local e1=Effect.CreateEffect(c)
+        e1:SetType(EFFECT_TYPE_SINGLE)
+        e1:SetCode(EFFECT_UPDATE_ATTACK)
+        e1:SetValue(-atk)
+        e1:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END)
+        tc:RegisterEffect(e1)
+        Duel.BreakEffect()
+        if Duel.Remove(c,0,REASON_EFFECT+REASON_TEMPORARY)==0 then return end
+        local e2=Effect.CreateEffect(c)
+        e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+        e2:SetCode(EVENT_PHASE+PHASE_STANDBY)
+        e2:SetCountLimit(1)
+        e2:SetLabel(Duel.GetTurnCount())
+        e2:SetLabelObject(c)
+        e2:SetCondition(c100213003.retcon)
+        e2:SetOperation(c100213003.retop)
+        if Duel.GetCurrentPhase()==PHASE_STANDBY then
+            e2:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_STANDBY,2)
+        else
+            e2:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_STANDBY)
+        end
+        Duel.RegisterEffect(e2,tp) 
+    end
+end
+function c100213003.retcon(e,tp,eg,ep,ev,re,r,rp)
+    return Duel.GetTurnCount()>e:GetLabel()
+end
+function c100213003.retop(e,tp,eg,ep,ev,re,r,rp)
+    Duel.ReturnToField(e:GetLabelObject())
+end
