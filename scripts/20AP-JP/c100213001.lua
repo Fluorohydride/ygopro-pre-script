e3f60a6f7bb021ddb75e8e9a5d3c85c505ceaf56
EerieCode <eeriecode@gmail.com>
2016-11-21 11:35:02 +0100

    Update 20AP-JP001 --ディアバウンド・カーネル

--- scripts/20AP-JP/c100213001.lua
+++ scripts/20AP-JP/c100213001.lua
@@ -1,27 +1,27 @@
 --ディアバウンド・カーネル
 --Diabound Kernel
 --Scripted by Eerie Code
-function c100213003.initial_effect(c)
+function c100213001.initial_effect(c)
     --Increase ATK
     local e1=Effect.CreateEffect(c)
-    e1:SetDescription(aux.Stringid(100213003,0))
+    e1:SetDescription(aux.Stringid(100213001,0))
     e1:SetCategory(CATEGORY_ATKCHANGE)
     e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
     e1:SetCode(EVENT_ATTACK_ANNOUNCE)
-    e1:SetOperation(c100213003.atkup)
+    e1:SetOperation(c100213001.atkup)
     c:RegisterEffect(e1)
     --Decrease ATK
     local e2=Effect.CreateEffect(c)
-    e2:SetDescription(aux.Stringid(100213003,1))
+    e2:SetDescription(aux.Stringid(100213001,1))
     e2:SetCategory(CATEGORY_ATKCHANGE+CATEGORY_REMOVE)
     e2:SetType(EFFECT_TYPE_QUICK_O)
     e2:SetCode(EVENT_FREE_CHAIN)
     e2:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_CARD_TARGET)
-    e2:SetTarget(c100213003.atktg)
-    e2:SetOperation(c100213003.atkop)
+    e2:SetTarget(c100213001.atktg)
+    e2:SetOperation(c100213001.atkop)
     c:RegisterEffect(e2)
 end
-function c100213003.atkup(e,tp,eg,ep,ev,re,r,rp)
+function c100213001.atkup(e,tp,eg,ep,ev,re,r,rp)
     local c=e:GetHandler()
     local e1=Effect.CreateEffect(c)
     e1:SetType(EFFECT_TYPE_SINGLE)
@@ -30,14 +30,14 @@ function c100213003.atkup(e,tp,eg,ep,ev,re,r,rp)
     e1:SetReset(RESET_EVENT+0x1fe0000)
     c:RegisterEffect(e1)
 end
-function c100213003.atktg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+function c100213001.atktg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
     if chkc then chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) and chkc:IsFaceup() end
     if chk==0 then return e:GetHandler():IsAbleToRemove() and Duel.IsExistingTarget(Card.IsFaceup,tp,0,LOCATION_MZONE,1,nil) end
     Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
     Duel.SelectTarget(tp,Card.IsFaceup,tp,0,LOCATION_MZONE,1,1,nil)
     Duel.SetOperationInfo(0,CATEGORY_REMOVE,e:GetHandler(),1,0,0)
 end
-function c100213003.atkop(e,tp,eg,ep,ev,re,r,rp)
+function c100213001.atkop(e,tp,eg,ep,ev,re,r,rp)
     local c=e:GetHandler()
     local tc=Duel.GetFirstTarget()
     if c:IsRelateToEffect(e) and c:IsFaceup() and tc:IsRelateToEffect(e) and tc:IsFaceup() then
@@ -56,8 +56,8 @@ function c100213003.atkop(e,tp,eg,ep,ev,re,r,rp)
         e2:SetCountLimit(1)
         e2:SetLabel(Duel.GetTurnCount())
         e2:SetLabelObject(c)
-        e2:SetCondition(c100213003.retcon)
-        e2:SetOperation(c100213003.retop)
+        e2:SetCondition(c100213001.retcon)
+        e2:SetOperation(c100213001.retop)
         if Duel.GetCurrentPhase()==PHASE_STANDBY then
             e2:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_STANDBY,2)
         else
@@ -66,9 +66,9 @@ function c100213003.atkop(e,tp,eg,ep,ev,re,r,rp)
         Duel.RegisterEffect(e2,tp) 
     end
 end
-function c100213003.retcon(e,tp,eg,ep,ev,re,r,rp)
+function c100213001.retcon(e,tp,eg,ep,ev,re,r,rp)
     return Duel.GetTurnCount()>e:GetLabel()
 end
-function c100213003.retop(e,tp,eg,ep,ev,re,r,rp)
+function c100213001.retop(e,tp,eg,ep,ev,re,r,rp)
     Duel.ReturnToField(e:GetLabelObject())
 end
