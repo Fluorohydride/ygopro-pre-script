d81789709cd8ab35398a41c66649d0921ab0a369
mercury233 <me@mercury233.me>
2016-11-24 23:35:30 +0800

    fix

--- scripts/20AP-JP/c100213007.lua
+++ scripts/20AP-JP/c100213007.lua
@@ -32,7 +32,7 @@ function c100213007.spfilter(c,e,tp)
 	return c:IsCode(6007213,32491822,69890967) and c:IsCanBeSpecialSummoned(e,0,tp,true,false)
 end
 function c100213007.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>-1
 		and Duel.IsExistingMatchingCard(c100213007.spfilter,tp,LOCATION_HAND+LOCATION_DECK,0,1,nil,e,tp) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND+LOCATION_DECK)
 end
