504b9a65c7d3e5dc9290c78e00163aa028d74415
dest <dest.dn@gmail.com>
2016-11-24 03:24:34 +0100

    fix True King V.F.D.
    fix this: monsters of the declared attribute in the pendulum zones can't activate their effects

--- scripts/MACR-JP/c100912046.lua
+++ scripts/MACR-JP/c100912046.lua
@@ -64,7 +64,7 @@ function c100912046.atop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.RegisterEffect(e3,tp)
 end
 function c100912046.aclimit(e,re,tp)
-	return re:GetHandler():IsAttribute(e:GetLabel())
+	return re:GetHandler():IsAttribute(e:GetLabel()) and not re:GetHandler():IsLocation(LOCATION_SZONE)
 end
 function c100912046.atktarget(e,c)
 	return c:IsAttribute(e:GetLabel())
