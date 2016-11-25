abf44e19ad788d4212e42d0feadb2bcc5274b9d0
nekrozar <unyuho.buster@gmail.com>
2016-11-26 06:48:26 +0900

    fix

--- scripts/SD31-JP/c100331023.lua
+++ scripts/SD31-JP/c100331023.lua
@@ -21,7 +21,7 @@ function c100331023.initial_effect(c)
 	e3:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
 	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
 	e3:SetCode(EVENT_LEAVE_FIELD)
-	e3:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
+	e3:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
 	e3:SetRange(LOCATION_SZONE)
 	e3:SetCountLimit(1,100331023)
 	e3:SetCondition(c100331023.thcon)
@@ -46,7 +46,7 @@ function c100331023.thfilter(c)
 	return c:IsType(TYPE_PENDULUM) and c:IsSetCard(0x98) and c:IsAbleToHand()
 end
 function c100331023.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(c100331023.thfilter,tp,LOCATION_DECK,0,1,nil) end
+	if chk==0 then return true end
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
 end
 function c100331023.thop(e,tp,eg,ep,ev,re,r,rp)
