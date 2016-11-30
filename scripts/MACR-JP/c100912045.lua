b6a57004e5f3d5c914b652f69886872ec07d7132
EerieCode <eeriecode@gmail.com>
2016-11-30 21:12:00 +0100

    fix

--- scripts/MACR-JP/c100912045.lua
+++ scripts/MACR-JP/c100912045.lua
@@ -2,80 +2,81 @@
 --D/D/D Boulder King Darius
 --Scripted by Eerie Code
 function c100912045.initial_effect(c)
-    c:EnableReviveLimit()
-    aux.AddXyzProcedure(c,aux.FilterBoolFunction(Card.IsRace,RACE_FIEND),3,2)
-    --Destroy and Draw
-    local e1=Effect.CreateEffect(c)
-    e1:SetDescription(aux.Stringid(100912045,0))
-    e1:SetType(EFFECT_TYPE_QUICK_O)
-    e1:SetCode(EVENT_FREE_CHAIN)
-    e1:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_PLAYER_TARGET)
-    e1:SetCountLimit(1)
-    e1:SetCost(c100912045.cost)
-    e1:SetTarget(c100912045.ddtg)
-    e1:SetOperation(c100912045.ddop)
-    c:RegisterEffect(e1)
-    --
-    local e2=Effect.CreateEffect(c)
-    e2:SetDescription(aux.Stringid(100912045,1))
-    e2:SetType(EFFECT_TYPE_TRIGGER_O+EFFECT_TYPE_SINGLE)
-    e2:SetCode(EVENT_PRE_DAMAGE_CALCULATE)
-    e2:SetCondition(c100912045.incon)
-    e2:SetCost(c100912045.cost)
-    e2:SetOperation(c100912045.inop)
-    c:RegisterEffect(e2)
+	c:EnableReviveLimit()
+	aux.AddXyzProcedure(c,aux.FilterBoolFunction(Card.IsRace,RACE_FIEND),3,2)
+	--Destroy and Draw
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(100912045,0))
+	e1:SetType(EFFECT_TYPE_QUICK_O)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetCost(c100912045.cost)
+	e1:SetTarget(c100912045.ddtg)
+	e1:SetOperation(c100912045.ddop)
+	c:RegisterEffect(e1)
+	--
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(100912045,1))
+	e2:SetType(EFFECT_TYPE_TRIGGER_O+EFFECT_TYPE_SINGLE)
+	e2:SetCode(EVENT_PRE_DAMAGE_CALCULATE)
+	e2:SetCondition(c100912045.incon)
+	e2:SetCost(c100912045.cost)
+	e2:SetOperation(c100912045.inop)
+	c:RegisterEffect(e2)
 end
 function c100912045.cost(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return e:GetHandler():CheckRemoveOverlayCard(tp,1,REASON_COST) end
-    e:GetHandler():RemoveOverlayCard(tp,1,1,REASON_COST)
+	if chk==0 then return e:GetHandler():CheckRemoveOverlayCard(tp,1,REASON_COST) end
+	e:GetHandler():RemoveOverlayCard(tp,1,1,REASON_COST)
 end
 function c100912045.ddfilter(c)
-    return c:IsFaceup() and c:IsSetCard(0xae)
+	return c:IsFaceup() and c:IsSetCard(0xae)
 end
 function c100912045.ddtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-    if chkc then return chkc:IsLocation(LOCATION_SZONE) and chkc:IsControler(tp) and c100912045.ddfilter(chkc) end
-    if chk==0 then return Duel.IsPlayerCanDraw(tp,1)
-        and Duel.IsExistingTarget(c100912045.ddfilter,tp,LOCATION_SZONE,0,1,nil) end
-    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
-    local g=Duel.SelectTarget(tp,c100912045.ddfilter,tp,LOCATION_SZONE,0,1,1,nil)
-    Duel.SetTargetPlayer(tp)
-    Duel.SetTargetParam(1)
-    Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
-    Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
+	if chkc then return chkc:IsLocation(LOCATION_SZONE) and chkc:IsControler(tp) and c100912045.ddfilter(chkc) end
+	if chk==0 then return Duel.IsPlayerCanDraw(tp,1)
+		and Duel.IsExistingTarget(c100912045.ddfilter,tp,LOCATION_SZONE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local g=Duel.SelectTarget(tp,c100912045.ddfilter,tp,LOCATION_SZONE,0,1,1,nil)
+	Duel.SetTargetPlayer(tp)
+	Duel.SetTargetParam(1)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
 end
 function c100912045.ddop(e,tp,eg,ep,ev,re,r,rp)
-    local g,p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
-    g=g:Filter(Card.IsRelateToEffect,nil,e)
-    if g:GetCount()>0 and Duel.Destroy(g,REASON_EFFECT)>0 then
-        Duel.Draw(p,d,REASON_EFFECT)
-    end
+	local g,p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+	g=g:Filter(Card.IsRelateToEffect,nil,e)
+	if g:GetCount()>0 and Duel.Destroy(g,REASON_EFFECT)>0 then
+		Duel.Draw(p,d,REASON_EFFECT)
+	end
 end
 function c100912045.incon(e,tp,eg,ep,ev,re,r,rp)
-    local c=e:GetHandler()
-    local bc=c:GetBattleTarget()
-    return bc and bc:IsControler(1-tp)
+	local c=e:GetHandler()
+	local bc=c:GetBattleTarget()
+	return bc and bc:IsControler(1-tp)
 end
 function c100912045.inop(e,tp,eg,ep,ev,re,r,rp)
-    local c=e:GetHandler()
-    if c:IsRelateToEffect(e) then
-        local e1=Effect.CreateEffect(c)
-        e1:SetType(EFFECT_TYPE_SINGLE)
-        e1:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
-        e1:SetValue(1)
-        e1:SetReset(RESET_PHASE+PHASE_DAMAGE)
-        c:RegisterEffect(e1)
-        local e3=Effect.CreateEffect(c)
-        e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
-        e3:SetCode(EVENT_BATTLED)
-        e3:SetOperation(c100912045.desop)
-        e3:SetReset(RESET_PHASE+PHASE_DAMAGE)
-        c:RegisterEffect(e3)
-    end
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) then
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+		e1:SetValue(1)
+		e1:SetReset(RESET_PHASE+PHASE_DAMAGE)
+		c:RegisterEffect(e1)
+		local e3=Effect.CreateEffect(c)
+		e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+		e3:SetCode(EVENT_BATTLED)
+		e3:SetOperation(c100912045.desop)
+		e3:SetReset(RESET_PHASE+PHASE_DAMAGE)
+		c:RegisterEffect(e3)
+	end
 end
 function c100912045.desop(e,tp,eg,ep,ev,re,r,rp)
-    local c=e:GetHandler()
-    local dc=c:GetBattleTarget()
-    if dc and Duel.Destroy(dc,REASON_EFFECT)>0 then
-        Duel.Damage(1-tp,500,REASON_EFFECT)
-    end
+	local c=e:GetHandler()
+	local dc=c:GetBattleTarget()
+	if dc and Duel.Destroy(dc,REASON_EFFECT)>0 then
+		Duel.Damage(1-tp,500,REASON_EFFECT)
+	end
 end
