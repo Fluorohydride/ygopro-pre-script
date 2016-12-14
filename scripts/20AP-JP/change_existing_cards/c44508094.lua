ecd9855db52ffe897fb531df5925b19d27929020
EerieCode <eeriecode@gmail.com>
2016-12-14 14:33:39 +0100

    Update "Stardust Dragon" for "Cosmic Flare"

---
+++ scripts/20AP-JP/change_existing_cards/c44508094.lua
@@ -0,0 +1,80 @@
+--スターダスト・ドラゴン
+function c44508094.initial_effect(c)
+	--synchro summon
+	aux.AddSynchroProcedure(c,nil,aux.NonTuner(nil),1)
+	c:EnableReviveLimit()
+	--Negate
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(44508094,0))
+	e1:SetCategory(CATEGORY_NEGATE+CATEGORY_DESTROY)
+	e1:SetType(EFFECT_TYPE_QUICK_O)
+	e1:SetCode(EVENT_CHAINING)
+	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DAMAGE_CAL)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCondition(c44508094.condition)
+	e1:SetCost(c44508094.cost)
+	e1:SetTarget(c44508094.target)
+	e1:SetOperation(c44508094.operation)
+	c:RegisterEffect(e1)
+	--Revive
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(44508094,1))
+	e2:SetType(EFFECT_TYPE_TRIGGER_O+EFFECT_TYPE_FIELD)
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetCode(EVENT_PHASE+PHASE_END)
+	e2:SetRange(LOCATION_GRAVE)
+	e2:SetCountLimit(1)
+	e2:SetTarget(c44508094.sumtg)
+	e2:SetOperation(c44508094.sumop)
+	c:RegisterEffect(e2)
+end
+function c44508094.condition(e,tp,eg,ep,ev,re,r,rp)
+	if e:GetHandler():IsStatus(STATUS_BATTLE_DESTROYED) or not Duel.IsChainNegatable(ev) then return false end
+	if re:IsHasCategory(CATEGORY_NEGATE)
+		and Duel.GetChainInfo(ev-1,CHAININFO_TRIGGERING_EFFECT):IsHasType(EFFECT_TYPE_ACTIVATE) then return false end
+	local ex,tg,tc=Duel.GetOperationInfo(ev,CATEGORY_DESTROY)
+	return ex and tg~=nil and tc+tg:FilterCount(Card.IsOnField,nil)-tg:GetCount()>0
+end
+function c44508094.cfcost(c)
+	return c:IsCode(100213053) and c:IsAbleToRemoveAsCost()
+end
+function c44508094.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	local b1=e:GetHandler():IsReleasable()
+	local b2=Duel.IsExistingMatchingCard(c44508094.cfcost,tp,LOCATION_GRAVE,0,1,nil)
+	if chk==0 then return b1 or b2 end
+	if not b1 or Duel.SelectYesNo(tp,aux.Stringid(100213053,0)) then
+		local tg=Duel.GetFirstMatchingCard(c44508094.cfcost,tp,LOCATION_GRAVE,0,nil)
+		Duel.Remove(tg,POS_FACEUP,REASON_COST)
+		e:SetLabel(0)
+	else
+		Duel.Release(e:GetHandler(),REASON_COST)
+		e:SetLabel(1)
+	end	
+end
+function c44508094.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
+	if re:GetHandler():IsDestructable() and re:GetHandler():IsRelateToEffect(re) then
+		Duel.SetOperationInfo(0,CATEGORY_DESTROY,eg,1,0,0)
+	end
+end
+function c44508094.operation(e,tp,eg,ep,ev,re,r,rp)
+	Duel.NegateActivation(ev)
+	if re:GetHandler():IsRelateToEffect(re) then
+		Duel.Destroy(eg,REASON_EFFECT)
+	end
+	if e:GetLabel()==1 then
+		e:GetHandler():RegisterFlagEffect(44508094,RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END,0,0)
+	end
+end
+function c44508094.sumtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and c:GetFlagEffect(44508094)>0
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,0,0)
+end
+function c44508094.sumop(e,tp,eg,ep,ev,re,r,rp)
+	if e:GetHandler():IsRelateToEffect(e) then
+		Duel.SpecialSummon(e:GetHandler(),0,tp,tp,false,false,POS_FACEUP)
+	end
+end
