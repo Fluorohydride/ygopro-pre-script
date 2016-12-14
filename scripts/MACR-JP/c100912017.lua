4126ceef8975a383068a7374d0d671d0b39b221f
EerieCode <eeriecode@gmail.com>
2016-12-14 13:17:51 +0100

    Add MACR-JP017 --覇王門零

---
+++ scripts/MACR-JP/c100912017.lua
@@ -0,0 +1,155 @@
+--覇王門零
+--Supreme King Gate Zero
+--Scripted by Eerie Code
+function c100912017.initial_effect(c)
+	aux.EnablePendulumAttribute(c)
+	--avoid damage
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_CHANGE_DAMAGE)
+	e1:SetRange(LOCATION_PZONE)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_AVAILABLE_BD)
+	e1:SetTargetRange(1,0)
+	e1:SetCondition(c100912017.ndcon)
+	e1:SetValue(0)
+	c:RegisterEffect(e1)
+	--search
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(100912017,0))
+	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_PZONE)
+	e2:SetCountLimit(1)
+	e2:SetCondition(c100912017.thcon)
+	e2:SetTarget(c100912017.thtg)
+	e2:SetOperation(c100912017.thop)
+	c:RegisterEffect(e2)
+	--special summon
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(100912017,1))
+	e3:SetCategory(CATEGORY_DESTROY+CATEGORY_SPECIAL_SUMMON)
+	e3:SetType(EFFECT_TYPE_IGNITION)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e3:SetCountLimit(1)
+	e3:SetTarget(c100912017.sptg)
+	e3:SetOperation(c100912017.spop)
+	c:RegisterEffect(e3)
+	--pendulum
+	local e6=Effect.CreateEffect(c)
+	e6:SetDescription(aux.Stringid(100912017,2))
+	e6:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e6:SetCode(EVENT_DESTROYED)
+	e6:SetProperty(EFFECT_FLAG_DELAY)
+	e6:SetCondition(c100912017.pencon)
+	e6:SetTarget(c100912017.pentg)
+	e6:SetOperation(c100912017.penop)
+	c:RegisterEffect(e6)
+end
+function c100912017.ndcfilter(c)
+	return c:IsFaceup() and c:IsCode(100912039)
+end
+function c100912017.ndcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsExistingMatchingCard(c100912017.ndcfilter,tp,LOCATION_MZONE,0,1,nil)
+end
+function c100912017.thcon(e,tp,eg,ep,ev,re,r,rp)
+	local seq=e:GetHandler():GetSequence()
+	local pc=Duel.GetFieldCard(tp,LOCATION_SZONE,13-seq)
+	return pc and pc:IsCode(100912017)
+end
+function c100912017.thfilter(c)
+	return c:IsType(TYPE_SPELL) and c:IsSetCard(0x46) and c:IsAbleToHand()
+end
+function c100912017.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c100912017.thfilter,tp,LOCATION_DECK,0,1,nil) end
+	local c=e:GetHandler()
+	local pc=Duel.GetFieldCard(tp,LOCATION_SZONE,13-c:GetSequence())
+	local g=Group.FromCards(c,pc)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,2,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function c100912017.thop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not c:IsRelateToEffect(e) then return end
+	local pc=Duel.GetFieldCard(tp,LOCATION_SZONE,13-c:GetSequence())
+	if not pc:IsRelateToEffect(e) then return end
+	local g=Group.FromCards(c,pc)
+	if Duel.Destroy(g,REASON_EFFECT)==2 then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+		local sg=Duel.SelectMatchingCard(tp,c100912017.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+		if sg:GetCount()>0 then
+			Duel.SendtoHand(sg,nil,REASON_EFFECT)
+			Duel.ConfirmCards(1-tp,sg)
+		end
+	end
+end
+function c100912017.spfilter(c,e,tp)
+	return c:IsType(TYPE_FUSION+TYPE_SYNCHRO) and c:IsRace(RACE_DRAGON) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function c100912017.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	local c=e:GetHandler()
+	if chkc then return chkc:IsLocation(LOCATION_ONFIELD) and chkc:IsControler(tp) and chkc:IsFaceup() and chkc~=c end
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>-1
+		and Duel.IsExistingTarget(Card.IsFaceup,tp,LOCATION_ONFIELD,0,1,c)
+		and Duel.IsExistingMatchingCard(c100912017.spfilter,tp,LOCATION_EXTRA,0,1,nil,e,tp) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local g=Duel.SelectTarget(tp,Card.IsFaceup,tp,LOCATION_ONFIELD,0,1,1,c)
+	g:AddCard(c)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,2,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
+end
+function c100912017.spop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tc=Duel.GetFirstTarget()
+	if not c:IsRelateToEffect(e) or not tc:IsRelateToEffect(e) then return end
+	local dg=Group.FromCards(c,tc)
+	if Duel.Destroy(dg,REASON_EFFECT)==2 then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+		local g=Duel.SelectMatchingCard(tp,c100912017.spfilter,tp,LOCATION_EXTRA,0,1,1,nil,e,tp)
+		if g:GetCount()==0 then return end
+		local sc=g:GetFirst()
+		if Duel.SpecialSummonStep(sc,0,tp,tp,false,false,POS_FACEUP) then
+				local e1=Effect.CreateEffect(c)
+				e1:SetType(EFFECT_TYPE_SINGLE)
+				e1:SetCode(EFFECT_DISABLE)
+				e1:SetReset(RESET_EVENT+0x1fe0000)
+				sc:RegisterEffect(e1,true)
+				local e2=Effect.CreateEffect(c)
+				e2:SetType(EFFECT_TYPE_SINGLE)
+				e2:SetCode(EFFECT_DISABLE_EFFECT)
+				e2:SetReset(RESET_EVENT+0x1fe0000)
+				sc:RegisterEffect(e2,true)
+				local e3=Effect.CreateEffect(c)
+				e3:SetType(EFFECT_TYPE_SINGLE)
+				e3:SetCode(EFFECT_SET_ATTACK_FINAL)
+				e3:SetValue(0)
+				e3:SetReset(RESET_EVENT+0x1fe0000)
+				sc:RegisterEffect(e3,true)
+				local e4=e3:Clone()
+				e4:SetCode(EFFECT_SET_DEFENSE_FINAL)
+				sc:RegisterEffect(e4,true)
+				local e5=e3:Clone()
+				e5:SetCode(EFFECT_CANNOT_BE_SYNCHRO_MATERIAL)
+				e5:SetValue(1)
+				sc:RegisterEffect(e5,true)
+				local e6=e5:Clone()
+				e6:SetCode(EFFECT_CANNOT_BE_XYZ_MATERIAL)
+				sc:RegisterEffect(e6,true)				
+			end
+		end
+	end
+end
+function c100912017.pencon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return c:IsPreviousLocation(LOCATION_MZONE) and c:IsFaceup()
+end
+function c100912017.pentg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.CheckLocation(tp,LOCATION_SZONE,6) or Duel.CheckLocation(tp,LOCATION_SZONE,7) end
+end
+function c100912017.penop(e,tp,eg,ep,ev,re,r,rp)
+	if not Duel.CheckLocation(tp,LOCATION_SZONE,6) and not Duel.CheckLocation(tp,LOCATION_SZONE,7) then return false end
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) then
+		Duel.MoveToField(c,tp,tp,LOCATION_SZONE,POS_FACEUP,true)
+	end
+end
