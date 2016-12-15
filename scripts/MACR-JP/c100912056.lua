501db26aea711b130d3ccb595a935f87648f9418
mercury233 <me@mercury233.me>
2016-12-15 14:23:50 +0800

    add MACR-JP056 Pacifis, City of Mythic Radiance

---
+++ scripts/MACR-JP/c100912056.lua
@@ -0,0 +1,95 @@
+--幻煌の都 パシフィス
+--Pacifis, City of Mythic Radiance
+--Script by mercury233
+function c100912056.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e1)
+	--to hand
+	local e2=Effect.CreateEffect(c)
+	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e2:SetProperty(EFFECT_FLAG_DELAY)
+	e2:SetCode(EVENT_SUMMON_SUCCESS)
+	e2:SetCountLimit(1)
+	e2:SetRange(LOCATION_FZONE)
+	e2:SetCondition(c100912056.thcon)
+	e2:SetCost(c100912056.cost)
+	e2:SetTarget(c100912056.thtg)
+	e2:SetOperation(c100912056.thop)
+	c:RegisterEffect(e2)
+	local e3=e2:Clone()
+	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
+	c:RegisterEffect(e3)
+	--token
+	local e4=Effect.CreateEffect(c)
+	e4:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e4:SetType(EFFECT_TYPE_QUICK_O)
+	e4:SetCode(EVENT_CHAINING)
+	e4:SetRange(LOCATION_FZONE)
+	e4:SetCondition(c100912056.spcon)
+	e4:SetCost(c100912056.cost)
+	e4:SetTarget(c100912056.sptg)
+	e4:SetOperation(c100912056.spop)
+	c:RegisterEffect(e4)
+	--
+	Duel.AddCustomActivityCounter(100912056,ACTIVITY_SUMMON,c100912056.counterfilter)
+	Duel.AddCustomActivityCounter(100912056,ACTIVITY_SPSUMMON,c100912056.counterfilter)
+end
+function c100912056.counterfilter(c)
+	return not c:IsType(TYPE_EFFECT)
+end
+function c100912056.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetCustomActivityCount(100912056,tp,ACTIVITY_SUMMON)==0
+		and Duel.GetCustomActivityCount(100912056,tp,ACTIVITY_SPSUMMON)==0 end
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_OATH)
+	e1:SetCode(EFFECT_CANNOT_SUMMON)
+	e1:SetReset(RESET_PHASE+PHASE_END)
+	e1:SetTargetRange(1,0)
+	e1:SetTarget(c100912056.splimit)
+	Duel.RegisterEffect(e1,tp)
+	local e2=e1:Clone()
+	e2:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+	Duel.RegisterEffect(e2,tp)
+end
+function c100912056.splimit(e,c,sump,sumtype,sumpos,targetp,se)
+	return c:IsType(TYPE_EFFECT)
+end
+function c100912056.thcon(e,tp,eg,ep,ev,re,r,rp)
+	local tc=eg:GetFirst()
+	return eg:GetCount()==1 and tc:GetSummonPlayer()==tp and tc:IsFaceup() and tc:IsType(TYPE_NORMAL)
+end
+function c100912056.thfilter(c)
+	return c:IsSetCard(0x1fc) and c:IsAbleToHand()
+end
+function c100912056.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c100912056.thfilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function c100912056.thop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,c100912056.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if g:GetCount()>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end
+function c100912056.spcon(e,tp,eg,ep,ev,re,r,rp)
+	return rp~=tp and not Duel.IsExistingMatchingCard(Card.IsType,tp,LOCATION_MZONE,0,1,nil,TYPE_TOKEN)
+end
+function c100912056.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsPlayerCanSpecialSummonMonster(tp,100912156,0x1fc,0x4011,2000,2000,6,RACE_WYRM,ATTRIBUTE_WATER) end
+	Duel.SetOperationInfo(0,CATEGORY_TOKEN,nil,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,0,0)
+end
+function c100912056.spop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	if not Duel.IsPlayerCanSpecialSummonMonster(tp,100912156,0x1fc,0x4011,2000,2000,6,RACE_WYRM,ATTRIBUTE_WATER) then return end
+	local token=Duel.CreateToken(tp,100912156)
+	Duel.SpecialSummon(token,0,tp,tp,false,false,POS_FACEUP)
+end
