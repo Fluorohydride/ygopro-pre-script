09d438c4884b3ff1fd3a565cef77a2408b060f82
nekrozar <unyuho.buster@gmail.com>
2016-12-17 11:15:32 +0900

    change ID

--- scripts/VJMP-JP/c58330108.lua
+++ scripts/VJMP-JP/c58330108.lua
@@ -1,7 +1,7 @@
 --破壊竜ガンドラ－ギガ・レイズ
 --Gandora Giga Rays the Dragon of Destruction
 --Scripted by Eerie Code
-function c100200125.initial_effect(c)
+function c58330108.initial_effect(c)
 	c:EnableReviveLimit()
 	--special summon rule
 	local e1=Effect.CreateEffect(c)
@@ -9,8 +9,8 @@ function c100200125.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetRange(LOCATION_HAND)
 	e1:SetCode(EFFECT_SPSUMMON_PROC)
-	e1:SetCondition(c100200125.hspcon)
-	e1:SetOperation(c100200125.hspop)
+	e1:SetCondition(c58330108.hspcon)
+	e1:SetOperation(c58330108.hspop)
 	c:RegisterEffect(e1)
 	--atk gain
 	local e2=Effect.CreateEffect(c)
@@ -18,35 +18,35 @@ function c100200125.initial_effect(c)
 	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
 	e2:SetRange(LOCATION_MZONE)
 	e2:SetCode(EFFECT_UPDATE_ATTACK)
-	e2:SetValue(c100200125.value)
+	e2:SetValue(c58330108.value)
 	c:RegisterEffect(e2)
 	--variable effect
 	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(100200125,0))
+	e3:SetDescription(aux.Stringid(58330108,0))
 	e3:SetType(EFFECT_TYPE_IGNITION)
 	e3:SetRange(LOCATION_MZONE)
 	e3:SetCountLimit(1)
-	e3:SetCost(c100200125.cost)
-	e3:SetTarget(c100200125.target)
-	e3:SetOperation(c100200125.operation)
+	e3:SetCost(c58330108.cost)
+	e3:SetTarget(c58330108.target)
+	e3:SetOperation(c58330108.operation)
 	c:RegisterEffect(e3)
 end
-function c100200125.hspfilter(c)
+function c58330108.hspfilter(c)
 	return c:IsType(TYPE_MONSTER) and c:IsAbleToGraveAsCost()
 end
-function c100200125.hspcon(e,c)
+function c58330108.hspcon(e,c)
 	if c==nil then return true end
 	local tp=c:GetControler()
 	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
-	local g=Duel.GetMatchingGroup(c100200125.hspfilter,tp,LOCATION_MZONE+LOCATION_HAND,0,c)
+	local g=Duel.GetMatchingGroup(c58330108.hspfilter,tp,LOCATION_MZONE+LOCATION_HAND,0,c)
 	return g:GetCount()>=2 and ft>=0 and (ft>0 or g:FilterCount(Card.IsLocation,nil,LOCATION_MZONE)>0)
 end
-function c100200125.hspop(e,tp,eg,ep,ev,re,r,rp,c)
+function c58330108.hspop(e,tp,eg,ep,ev,re,r,rp,c)
 	local tp=c:GetControler()
 	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
 	if ft<0 then return end
 	local hc=2
-	local g=Duel.GetMatchingGroup(c100200125.hspfilter,tp,LOCATION_MZONE+LOCATION_HAND,0,c)
+	local g=Duel.GetMatchingGroup(c58330108.hspfilter,tp,LOCATION_MZONE+LOCATION_HAND,0,c)
 	local sg=Group.CreateGroup()
 	if ft==0 then
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
@@ -60,19 +60,19 @@ function c100200125.hspop(e,tp,eg,ep,ev,re,r,rp,c)
 	sg:Merge(sg2)
 	Duel.SendtoGrave(sg,REASON_COST)
 end
-function c100200125.value(e,c)
+function c58330108.value(e,c)
 	return Duel.GetFieldGroupCount(c:GetControler(),LOCATION_REMOVED,LOCATION_REMOVED)*300
 end
-function c100200125.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+function c58330108.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
 	Duel.PayLPCost(tp,math.floor(Duel.GetLP(tp)/2))
 end
-function c100200125.filter(c)
+function c58330108.filter(c)
 	return c:IsType(TYPE_MONSTER) and c:IsSetCard(0xf5)
 end
-function c100200125.target(e,tp,eg,ep,ev,re,r,rp,chk)
+function c58330108.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
-	local gc=Duel.GetMatchingGroup(c100200125.filter,tp,LOCATION_GRAVE,0,nil):GetClassCount(Card.GetCode)
+	local gc=Duel.GetMatchingGroup(c58330108.filter,tp,LOCATION_GRAVE,0,nil):GetClassCount(Card.GetCode)
 	if chk==0 then
 		local b1=Duel.IsExistingMatchingCard(nil,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,c)
 		local b2=Duel.IsExistingMatchingCard(Card.IsAbleToRemove,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,c)
@@ -91,9 +91,9 @@ function c100200125.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	local g=Duel.GetMatchingGroup(rec,tp,loc,loc,c)
 	Duel.SetOperationInfo(0,cat,g,g:GetCount(),0,0)
 end
-function c100200125.operation(e,tp,eg,ep,ev,re,r,rp)
+function c58330108.operation(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	local gc=Duel.GetMatchingGroup(c100200125.filter,tp,LOCATION_GRAVE,0,nil):GetClassCount(Card.GetCode)
+	local gc=Duel.GetMatchingGroup(c58330108.filter,tp,LOCATION_GRAVE,0,nil):GetClassCount(Card.GetCode)
 	if gc==1 then
 		local g=Duel.GetMatchingGroup(nil,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,c)
 		Duel.Destroy(g,REASON_EFFECT)
