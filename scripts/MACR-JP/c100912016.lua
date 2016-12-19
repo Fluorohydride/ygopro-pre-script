55804912741743370424da6abc42bb44b82f87e0
mercury233 <me@mercury233.me>
2016-12-19 14:46:45 +0800

    fix

--- scripts/MACR-JP/c100912016.lua
+++ scripts/MACR-JP/c100912016.lua
@@ -31,7 +31,7 @@ function c100912016.target1(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_MZONE) and c100912016.filter1(chkc) end
 	if chk==0 then return Duel.IsExistingTarget(c100912016.filter1,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
-	Duel.SelectTarget(tp,c1009120161.filter1,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
+	Duel.SelectTarget(tp,c100912016.filter1,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
 end
 function c100912016.operation(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
