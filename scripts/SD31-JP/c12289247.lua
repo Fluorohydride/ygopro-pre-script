d3d11c2206e73e9f044169c0d2679f1ca5b3b504
EerieCode <eeriecode@gmail.com>
2016-12-24 16:00:30 +0100

    fix

--- scripts/SD31-JP/c12289247.lua
+++ scripts/SD31-JP/c12289247.lua
@@ -106,7 +106,7 @@ function c12289247.hncost(e,tp,eg,ep,ev,re,r,rp,chk)
 		and g:IsExists(Card.IsSetCard,1,nil,0x1046)
 		and (ft>0 or g:IsExists(Card.IsLocation,ct,nil,LOCATION_MZONE))
 	end
-	local g=Group.FromCards(c)
+	local rg=Group.FromCards(c)
 	for i=1,4 do
 		local tc=nil
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
