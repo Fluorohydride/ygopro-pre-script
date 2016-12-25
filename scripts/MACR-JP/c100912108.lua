b8e7c8cb5578f85d7df38aca36f07a7f7938e4e1
EerieCode <eeriecode@gmail.com>
2016-12-25 12:07:12 +0100

    Add MACR-JP0?? --セフィラの星戦

---
+++ scripts/MACR-JP/c100912108.lua
@@ -0,0 +1,47 @@
+--セフィラの星戦
+--Celestial Crusade of the Zefra
+--Scripted by Eerie Code
+function c100912108.initial_effect(c)
+	--activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_DESTROY)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetCountLimit(1,100912108+EFFECT_COUNT_CODE_OATH)
+	e1:SetTarget(c100912108.target)
+	e1:SetOperation(c100912108.activate)
+	c:RegisterEffect(e1)
+	--act in hand
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetCode(EFFECT_TRAP_ACT_IN_HAND)
+	e2:SetCondition(c100912108.handcon)
+	c:RegisterEffect(e2)
+end
+function c100912108.filter(c)
+	return c:IsFaceup() and c:IsSetCard(0xc4)
+end
+function c100912108.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return false end
+	if chk==0 then return Duel.IsExistingTarget(c100912108.filter,tp,LOCATION_ONFIELD,0,1,nil)
+		and Duel.IsExistingTarget(nil,tp,0,LOCATION_ONFIELD,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local g1=Duel.SelectTarget(tp,c100912108.filter,tp,LOCATION_ONFIELD,0,1,1,nil)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local g2=Duel.SelectTarget(tp,nil,tp,0,LOCATION_ONFIELD,1,1,nil)
+	g1:Merge(g2)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g1,2,0,0)
+end
+function c100912108.activate(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS):Filter(Card.IsRelateToEffect,nil,e)
+	if g:GetCount()>0 then
+		Duel.Destroy(g,REASON_EFFECT)
+	end
+end
+function c100912108.handcon(e)
+	local tp=e:GetHandlerPlayer()
+	local tc1=Duel.GetFieldCard(tp,LOCATION_SZONE,6)
+	local tc2=Duel.GetFieldCard(tp,LOCATION_SZONE,7)
+	return tc1 and tc1:IsSetCard(0xc4) and tc2 and tc2:IsSetCard(0xc4)
+end
