1b6b53b6f21f963967632b2a8444137ac7085027
EerieCode <eeriecode@gmail.com>
2016-12-29 14:06:42 +0100

    Add MACR-JP0?? --十二獣クックル

---
+++ scripts/MACR-JP/c100912110.lua
@@ -0,0 +1,100 @@
+--十二獣クックル
+--Zoodiac Cuckle
+--Scripted by Eerie Code
+--Temporary alterations for compatibility with older versions of Ygopro
+function c100912110.initial_effect(c)
+	--shuffle
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(100912110,0))
+	e1:SetCategory(CATEGORY_TODECK)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_DESTROYED)
+	e1:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
+	e1:SetCondition(c100912110.tdcon)
+	e1:SetTarget(c100912110.tdtg)
+	e1:SetOperation(c100912110.tdop)
+	c:RegisterEffect(e1)
+	--get effect
+	if EFFECT_TYPE_XMATERIAL then
+		local e2=Effect.CreateEffect(c)
+		e2:SetDescription(aux.Stringid(100912110,1))
+		e2:SetCategory(CATEGORY_DISABLE)
+		e2:SetType(EFFECT_TYPE_XMATERIAL+EFFECT_TYPE_QUICK_O)
+		e2:SetCode(EVENT_CHAINING)
+		e2:SetCondition(c100912110.discon)
+		e2:SetCost(c100912110.discost)
+		e2:SetTarget(c100912110.distg)
+		e2:SetOperation(c100912110.disop)
+		c:RegisterEffect(e2)
+	else
+		if not c100912110.global_check then
+			c100912110.global_check=true
+			local ge1=Effect.CreateEffect(c)
+			ge1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+			ge1:SetCode(EVENT_SPSUMMON_SUCCESS)
+			ge1:SetOperation(c100912110.checkop)
+			Duel.RegisterEffect(ge1,0)
+		end
+	end
+end
+function c100912110.tdcon(e,tp,eg,ep,ev,re,r,rp)
+	return bit.band(r,REASON_EFFECT+REASON_BATTLE)~=0
+end
+function c100912110.tdfilter(c)
+	return c:IsSetCard(0xf1) and not c:IsCode(100912110) and c:IsAbleToDeck()
+end
+function c100912110.tdtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_GRAVE) and c100912110.tdfilter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(c100912110.tdfilter,tp,LOCATION_GRAVE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
+	local g=Duel.SelectTarget(tp,c100912110.tdfilter,tp,LOCATION_GRAVE,0,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_TODECK,g,1,0,0)
+end
+function c100912110.tdop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) then
+		Duel.SendtoDeck(tc,nil,2,REASON_EFFECT)
+	end
+end
+function c100912110.checkop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=eg:GetFirst()
+	while tc do
+		if tc:IsType(TYPE_XYZ) and tc:GetOriginalRace()==RACE_BEASTWARRIOR
+			and tc:GetFlagEffect(100912110)==0 then
+			local e1=Effect.CreateEffect(e:GetHandler())
+			e1:SetDescription(aux.Stringid(100912110,1))
+			e1:SetCategory(CATEGORY_DISABLE)
+			e1:SetType(EFFECT_TYPE_QUICK_O)
+			e1:SetCode(EVENT_CHAINING)
+			e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DAMAGE_CAL)
+			e1:SetRange(LOCATION_MZONE)
+			e1:SetCondition(c100912110.discon)
+			e1:SetCost(c100912110.discost)
+			e1:SetTarget(c100912110.distg)
+			e1:SetOperation(c100912110.disop)
+			tc:RegisterEffect(e1,true)
+			tc:RegisterFlagEffect(100912110,0,0,1)
+		end
+		tc=eg:GetNext()
+	end
+end
+function c100912110.discon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return c:GetOriginalRace()==RACE_BEASTWARRIOR
+		and not c:IsStatus(STATUS_BATTLE_DESTROYED) and ep==1-tp
+		and re:IsActiveType(TYPE_MONSTER) and Duel.IsChainDisablable(ev)
+		and re:IsHasProperty(EFFECT_FLAG_CARD_TARGET)
+		and Duel.GetChainInfo(ev,CHAININFO_TARGET_CARDS):IsContains(c)
+end
+function c100912110.discost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():CheckRemoveOverlayCard(tp,1,REASON_COST) end
+	e:GetHandler():RemoveOverlayCard(tp,1,1,REASON_COST)
+end
+function c100912110.distg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.Hint(HINT_OPSELECTED,1-tp,e:GetDescription())
+	Duel.SetOperationInfo(0,CATEGORY_DISABLE,eg,1,0,0)
+end
+function c100912110.disop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.NegateEffect(ev)
+end
