120d5ab2f29a7c2a9ba70d89211d1b6907108f7f
EerieCode <eeriecode@gmail.com>
2016-12-29 13:35:12 +0100

    fix

--- scripts/MACR-JP/c100912041.lua
+++ scripts/MACR-JP/c100912041.lua
@@ -53,7 +53,7 @@ function c100912041.initial_effect(c)
 	end
 end
 function c100912041.checkfilter(c,tp)
-	return c:IsType(TYPE_XYZ) and c:IsSetCard(0x1f8) and c:GetOverlayCount()>0
+	return c:IsType(TYPE_XYZ) and c:GetOverlayCount()>0
 end
 function c100912041.checkop(e,tp,eg,ep,ev,re,r,rp)
 	if not eg then return  end
