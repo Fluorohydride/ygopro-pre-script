be049efccb63c1945ce48d589ce73277582f5a1d
YGO5upp0r73r <YGO5upp0r73r@users.noreply.github.com>
2017-01-12 19:46:01 +0200

    Added Draw Phase Hint for Dark Contract with the Eternal Darkness
    To aid the lock down effect, similar to "True King V.F.D., The Beast"'s:

--- scripts/MACR-JP/c100912068.lua
+++ scripts/MACR-JP/c100912068.lua
@@ -6,6 +6,7 @@ function c100912068.initial_effect(c)
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetHintTiming(0,TIMING_DRAW_PHASE)
 	c:RegisterEffect(e1)
 	--cannot be target
 	local e2=Effect.CreateEffect(c)
