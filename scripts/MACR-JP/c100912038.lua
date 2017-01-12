8685b324eb9680da1019b76b5836f7a1c9a00e5f
dest <dest.dn@gmail.com>
2017-01-12 18:16:55 +0100

    Add MACR 38 Fairy Tail – Kaguya

---
+++ scripts/MACR-JP/c100912038.lua
@@ -0,0 +1,69 @@
+--妖精伝姫- カグヤ
+--Fairy Tail – Kaguya
+--Script by dest
+function c100912038.initial_effect(c)
+	--search
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(100912038,0))
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_SUMMON_SUCCESS)
+	e1:SetTarget(c100912038.target)
+	e1:SetOperation(c100912038.operation)
+	c:RegisterEffect(e1)
+	--to hand
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(100912038,1))
+	e2:SetCategory(CATEGORY_TOHAND)
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1)
+	e2:SetHintTiming(0,0x1e0)
+	e2:SetTarget(c100912038.thtg)
+	e2:SetOperation(c100912038.thop)
+	c:RegisterEffect(e2)
+end
+function c100912038.filter(c)
+	return c:GetAttack()==1850 and c:IsRace(RACE_SPELLCASTER) and c:IsAbleToHand()
+end
+function c100912038.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c100912038.filter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function c100912038.operation(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,c100912038.filter,tp,LOCATION_DECK,0,1,1,nil)
+	if g:GetCount()>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end
+function c100912038.thfilter(c)
+	return c:IsFaceup() and c:IsAbleToHand()
+end
+function c100912038.thtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsAbleToHand() end
+	if chk==0 then return Duel.IsExistingTarget(c100912038.thfilter,tp,0,LOCATION_MZONE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RTOHAND)
+	local g=Duel.SelectTarget(tp,c100912038.thfilter,tp,0,LOCATION_MZONE,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,0,0)
+end
+function c100912038.cfilter(c,code)
+	return c:IsCode(code) and c:IsAbleToGraveAsCost()
+end
+function c100912038.thop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if Duel.IsExistingMatchingCard(c100912038.cfilter,tp,LOCATION_DECK+LOCATION_EXTRA,0,1,nil,tc:GetCode())
+		and tc:IsFaceup() and Duel.SelectYesNo(1-tp,aux.Stringid(100912038,2)) then
+		Duel.PayLPCost(1-tp,1000)
+		if Duel.IsChainDisablable(0) then
+			Duel.NegateEffect(0)
+			return
+		end
+	end
+	if tc and tc:IsRelateToEffect(e) and tc:IsFaceup() then
+		Duel.SendtoHand(tc,nil,REASON_EFFECT)
+	end
+end
