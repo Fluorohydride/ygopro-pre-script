f107b361e91ef0325daaffb9e4c112768f63c989
EerieCode <eeriecode@gmail.com>
2017-01-13 11:53:57 +0100

    Add MACR-JP030 --智天の神凰龍

---
+++ scripts/MACR-JP/c100912030.lua
@@ -0,0 +1,178 @@
+--智天の神凰龍
+--Zefratorah Grammaton
+--Scripted by Eerie Code
+function c100912030.initial_effect(c)
+	c:EnableReviveLimit()
+	aux.EnablePendulumAttribute(c)
+	--change scale
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(100912030,0))
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_PZONE)
+	e1:SetCountLimit(1,100912030)
+	e1:SetTarget(c100912030.sctg)
+	e1:SetOperation(c100912030.scop)
+	c:RegisterEffect(e1)
+	--spsummon condition
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e2:SetCode(EFFECT_SPSUMMON_CONDITION)
+	c:RegisterEffect(e2)
+	--special summon (self)
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetCode(EFFECT_SPSUMMON_PROC)
+	e3:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+	e3:SetRange(LOCATION_EXTRA)
+	e3:SetCondition(c100912030.hspcon)
+	e3:SetOperation(c100912030.hspop)
+	c:RegisterEffect(e3)
+	--special summon
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(100912030,3))
+	e4:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e4:SetType(EFFECT_TYPE_IGNITION)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetCountLimit(1)
+	e4:SetCost(c100912030.spcost)
+	e4:SetTarget(c100912030.sptg)
+	e4:SetOperation(c100912030.spop)
+	c:RegisterEffect(e4)
+end
+function c100912030.scfilter(c)
+	return c:IsType(TYPE_PENDULUM) and c:IsSetCard(0xc4) and not c:IsForbidden()
+end
+function c100912030.sctg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c100912030.scfilter,tp,LOCATION_DECK,0,1,nil) end
+end
+function c100912030.scop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not c:IsRelateToEffect(e) then return end
+	Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(100912030,1))
+	local g=Duel.SelectMatchingCard(tp,c100912030.scfilter,tp,LOCATION_DECK,0,1,1,nil)
+	local tc=g:GetFirst()
+	if tc and Duel.SendtoExtraP(tc,tp,REASON_EFFECT)>0 then
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_CHANGE_LSCALE)
+		e1:SetValue(tc:GetLeftScale())
+		e1:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END)
+		c:RegisterEffect(e1)
+		local e2=e1:Clone()
+		e2:SetCode(EFFECT_CHANGE_RSCALE)
+		e2:SetValue(tc:GetRightScale())
+		c:RegisterEffect(e2)
+	end
+end
+function c100912030.hspcon(e,c)
+	if c==nil then return true end
+	local tp=c:GetControler()
+	local g=Duel.GetFieldGroup(tp,LOCATION_MZONE,0)
+	local rg=Duel.GetReleaseGroup(tp)
+	return (g:GetCount()>0 or rg:GetCount()>0) and g:FilterCount(Card.IsReleasable,nil)==g:GetCount()
+		and g:FilterCount(Card.IsSetCard,nil,0xc4)>=3
+end
+function c100912030.hspop(e,tp,eg,ep,ev,re,r,rp,c)
+	local g=Duel.GetReleaseGroup(tp)
+	Duel.Release(g,REASON_COST)
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(100912030,2))
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_SPSUMMON_PROC_G)
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE)
+	e1:SetRange(LOCATION_PZONE)
+	e1:SetCountLimit(1,100912030+100)
+	e1:SetCondition(c100912030.pencon)
+	e1:SetOperation(c100912030.penop)
+	e1:SetValue(SUMMON_TYPE_PENDULUM)
+	c:RegisterEffect(e1)
+end
+function c100912030.penfilter(c,e,tp,lscale,rscale)
+	local lv=0
+	if c.pendulum_level then
+		lv=c.pendulum_level
+	else
+		lv=c:GetLevel()
+	end
+	return (c:IsLocation(LOCATION_HAND) or (c:IsFaceup() and c:IsType(TYPE_PENDULUM)))
+		and lv>lscale and lv<rscale and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_PENDULUM,tp,false,false)
+		and not c:IsForbidden() and c:IsSetCard(0xc4)
+end
+function c100912030.pencon(e,c,og)
+	if c==nil then return true end
+	local tp=c:GetControler()
+	if c:GetSequence()~=6 then return false end
+	local rpz=Duel.GetFieldCard(tp,LOCATION_SZONE,7)
+	if rpz==nil then return false end
+	local lscale=c:GetLeftScale()
+	local rscale=rpz:GetRightScale()
+	if lscale>rscale then lscale,rscale=rscale,lscale end
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	if ft<=0 then return false end
+	if og then
+		return og:IsExists(Auxiliary.PConditionFilter,1,nil,e,tp,lscale,rscale)
+	else
+		return Duel.IsExistingMatchingCard(Auxiliary.PConditionFilter,tp,LOCATION_HAND+LOCATION_EXTRA,0,1,nil,e,tp,lscale,rscale)
+	end
+end
+function c100912030.penop(e,tp,eg,ep,ev,re,r,rp,c,sg,og)
+	local rpz=Duel.GetFieldCard(tp,LOCATION_SZONE,7)
+	local lscale=c:GetLeftScale()
+	local rscale=rpz:GetRightScale()
+	if lscale>rscale then lscale,rscale=rscale,lscale end
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	if Duel.IsPlayerAffectedByEffect(tp,59822133) then ft=1 end
+	local tg=nil
+	if og then
+		tg=og:Filter(tp,Auxiliary.PConditionFilter,nil,e,tp,lscale,rscale)
+	else
+		tg=Duel.GetMatchingGroup(Auxiliary.PConditionFilter,tp,LOCATION_HAND+LOCATION_EXTRA,0,nil,e,tp,lscale,rscale)
+	end
+	local ect=c29724053 and Duel.IsPlayerAffectedByEffect(tp,29724053) and c29724053[tp]
+	if ect and (ect<=0 or ect>ft) then ect=nil end
+	if ect==nil or tg:FilterCount(Card.IsLocation,nil,LOCATION_EXTRA)<=ect then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+		local g=tg:Select(tp,1,ft,nil)
+		sg:Merge(g)
+	else
+		repeat
+			local ct=math.min(ft,ect)
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+			local g=tg:Select(tp,1,ct,nil)
+			tg:Sub(g)
+			sg:Merge(g)
+			ft=ft-g:GetCount()
+			ect=ect-g:FilterCount(Card.IsLocation,nil,LOCATION_EXTRA)
+		until ft==0 or ect==0 or not Duel.SelectYesNo(tp,210)
+		local hg=tg:Filter(Card.IsLocation,nil,LOCATION_HAND)
+		if ft>0 and ect==0 and hg:GetCount()>0 and Duel.SelectYesNo(tp,210) then
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+			local g=hg:Select(tp,1,ft,nil)
+			sg:Merge(g)
+		end
+	end
+	Duel.HintSelection(Group.FromCards(c))
+	Duel.HintSelection(Group.FromCards(rpz))
+end
+function c100912030.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.CheckReleaseGroup(tp,nil,1,nil) end
+	local sg=Duel.SelectReleaseGroup(tp,nil,1,1,nil)
+	Duel.Release(sg,REASON_COST)
+end
+function c100912030.spfilter(c,e,tp)
+	return c:IsSetCard(0xc4) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function c100912030.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>-1
+		and Duel.IsExistingMatchingCard(c100912030.spfilter,tp,LOCATION_DECK,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
+end
+function c100912030.spop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,c100912030.spfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
+	if g:GetCount()>0 then
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
