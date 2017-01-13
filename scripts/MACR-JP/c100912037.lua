2acb1aec0fa175c394beb5686257645f767a9057
nekrozar <unyuho.buster@gmail.com>
2017-01-13 10:14:33 +0900

    add script MACR-JP037,064

---
+++ scripts/MACR-JP/c100912037.lua
@@ -0,0 +1,70 @@
+--憑依装着－ライナ
+--Familiar-Possessed Lyna
+--Script by nekrozar
+function c100912037.initial_effect(c)
+	--special summon proc
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+	e1:SetCode(EFFECT_SPSUMMON_PROC)
+	e1:SetRange(LOCATION_HAND+LOCATION_DECK)
+	e1:SetCondition(c100912037.spcon)
+	e1:SetOperation(c100912037.spop)
+	e1:SetValue(1)
+	c:RegisterEffect(e1)
+	--search
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(100912037,0))
+	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e2:SetCondition(c100912037.condition)
+	e2:SetTarget(c100912037.thtg)
+	e2:SetOperation(c100912037.thop)
+	c:RegisterEffect(e2)
+	--pierce
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_SINGLE)
+	e3:SetCode(EFFECT_PIERCE)
+	e3:SetCondition(c100912037.condition)
+	c:RegisterEffect(e3)
+end
+function c100912037.spfilter1(c,tp)
+	return c:IsFaceup() and c:IsCode(19327348) and c:IsAbleToGraveAsCost()
+		and Duel.IsExistingMatchingCard(c100912037.spfilter2,tp,LOCATION_MZONE,0,1,c)
+end
+function c100912037.spfilter2(c)
+	return c:IsFaceup() and c:IsAttribute(ATTRIBUTE_DARK) and c:IsAbleToGraveAsCost()
+end
+function c100912037.spcon(e,c)
+	if c==nil then return true end
+	local tp=c:GetControler()
+	return Duel.GetLocationCount(tp,LOCATION_MZONE)>-2
+		and Duel.IsExistingMatchingCard(c100912037.spfilter1,tp,LOCATION_MZONE,0,1,nil,tp)
+end
+function c100912037.spop(e,tp,eg,ep,ev,re,r,rp,c)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local g1=Duel.SelectMatchingCard(tp,c100912037.spfilter1,tp,LOCATION_MZONE,0,1,1,nil,tp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local g2=Duel.SelectMatchingCard(tp,c100912037.spfilter2,tp,LOCATION_MZONE,0,1,1,g1:GetFirst())
+	g1:Merge(g2)
+	Duel.SendtoGrave(g1,REASON_COST)
+end
+function c100912037.thfilter(c)
+	return c:GetDefense()==1500 and c:IsRace(RACE_SPELLCASTER) and not c:IsCode(100912037) and c:IsAbleToHand()
+end
+function c100912037.condition(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():GetSummonType()==SUMMON_TYPE_SPECIAL+1
+end
+function c100912037.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c100912037.thfilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function c100912037.thop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,c100912037.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if g:GetCount()>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end
---
+++ scripts/MACR-JP/c100912064.lua
@@ -0,0 +1,70 @@
+--盆回し
+--Festival Spinning
+--Script by nekrozar
+function c100912064.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetTarget(c100912064.target)
+	e1:SetOperation(c100912064.operation)
+	c:RegisterEffect(e1)
+end
+function c100912064.filter(c)
+	return c:IsType(TYPE_FIELD) and c:IsSSetable()
+end
+function c100912064.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	local g=Duel.GetMatchingGroup(c100912064.filter,tp,LOCATION_DECK,0,nil)
+	if chk==0 then return g:GetClassCount(Card.GetCode)>1 end
+end
+function c100912064.operation(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(c100912064.filter,tp,LOCATION_DECK,0,nil)
+	if g:GetClassCount(Card.GetCode)<2 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(100912064,0))
+	local tg1=g:Select(tp,1,1,nil)
+	g:Remove(Card.IsCode,nil,tg1:GetFirst():GetCode())
+	Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(100912064,1))
+	local tg2=g:Select(tp,1,1,nil)
+	local fc1=Duel.GetFieldCard(tp,LOCATION_SZONE,5)
+	if fc1 then
+		Duel.SendtoGrave(fc1,REASON_RULE)
+		Duel.BreakEffect()
+	end
+	Duel.SSet(tp,tg1)
+	local fc2=Duel.GetFieldCard(1-tp,LOCATION_SZONE,5)
+	if fc2 then
+		Duel.SendtoGrave(fc2,REASON_RULE)
+		Duel.BreakEffect()
+	end
+	Duel.SSet(1-tp,tg2)
+	tg1:GetFirst():RegisterFlagEffect(100912064,RESET_EVENT+0x1fe0000,0,1)
+	tg2:GetFirst():RegisterFlagEffect(100912064,RESET_EVENT+0x1fe0000,0,1)
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_CANNOT_ACTIVATE)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetTargetRange(1,1)
+	e1:SetCondition(c100912064.con)
+	e1:SetValue(c100912064.actlimit)
+	Duel.RegisterEffect(e1,tp)
+	local e2=Effect.CreateEffect(e:GetHandler())
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_CANNOT_SSET)
+	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e2:SetTargetRange(1,1)
+	e2:SetCondition(c100912064.con)
+	e2:SetTarget(c100912064.setlimit)
+	Duel.RegisterEffect(e2,tp)
+end
+function c100912064.cfilter(c)
+	return c:GetSequence()==5 and c:GetFlagEffect(100912064)~=0
+end
+function c100912064.con(e)
+	return Duel.IsExistingMatchingCard(c100912064.cfilter,e:GetHandlerPlayer(),LOCATION_SZONE,LOCATION_SZONE,1,nil)
+end
+function c100912064.actlimit(e,re,tp)
+	return re:IsActiveType(TYPE_FIELD) and re:IsHasType(EFFECT_TYPE_ACTIVATE)
+end
+function c100912064.setlimit(e,c,tp)
+	return c:IsType(TYPE_FIELD)
+end
