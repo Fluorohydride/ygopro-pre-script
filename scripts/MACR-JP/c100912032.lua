2a1eaaec94a12c4daca1c27936fdd90cd6ae48ab
nekrozar <unyuho.buster@gmail.com>
2017-01-13 11:13:15 +0900

    add script MACR-JP032

---
+++ scripts/MACR-JP/c100912032.lua
@@ -0,0 +1,110 @@
+--巨大戦艦 ビッグ・コアMk－Ⅲ
+--B.E.S. Big Core MK-3
+--Script by nekrozar
+function c100912032.initial_effect(c)
+	c:EnableCounterPermit(0x1f)
+	--special summon
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_SPSUM_PARAM)
+	e1:SetCode(EFFECT_SPSUMMON_PROC)
+	e1:SetTargetRange(POS_FACEUP_DEFENSE,0)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCondition(c100912032.sprcon)
+	c:RegisterEffect(e1)
+	--counter
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(100912032,0))
+	e2:SetCategory(CATEGORY_COUNTER)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e2:SetCode(EVENT_SUMMON_SUCCESS)
+	e2:SetTarget(c100912032.cttg)
+	e2:SetOperation(c100912032.ctop)
+	c:RegisterEffect(e2)
+	local e3=e2:Clone()
+	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
+	c:RegisterEffect(e3)
+	--indes
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_SINGLE)
+	e4:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+	e4:SetValue(1)
+	c:RegisterEffect(e4)
+	--remove counter
+	local e5=Effect.CreateEffect(c)
+	e5:SetDescription(aux.Stringid(100912032,1))
+	e5:SetCategory(CATEGORY_DESTROY)
+	e5:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e5:SetCode(EVENT_DAMAGE_STEP_END)
+	e5:SetTarget(c100912032.rcttg)
+	e5:SetOperation(c100912032.rctop)
+	c:RegisterEffect(e5)
+	--destroy
+	local e6=Effect.CreateEffect(c)
+	e6:SetDescription(aux.Stringid(100912032,2))
+	e6:SetCategory(CATEGORY_DESTROY)
+	e6:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e6:SetCode(EVENT_DAMAGE_STEP_END)
+	e6:SetCondition(c100912032.descon)
+	e6:SetTarget(c100912032.destg)
+	e6:SetOperation(c100912032.desop)
+	c:RegisterEffect(e6)
+	--to deck
+	local e7=Effect.CreateEffect(c)
+	e7:SetDescription(aux.Stringid(100912032,3))
+	e7:SetCategory(CATEGORY_TODECK)
+	e7:SetType(EFFECT_TYPE_IGNITION)
+	e7:SetRange(LOCATION_GRAVE)
+	e7:SetCost(c100912032.tdcost)
+	e7:SetTarget(c100912032.tdtg)
+	e7:SetOperation(c100912032.tdop)
+	c:RegisterEffect(e7)
+end
+function c100912032.sprcon(e,c)
+	if c==nil then return true end
+	local tp=c:GetControler()
+	return Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0)==0
+		and Duel.GetFieldGroupCount(tp,0,LOCATION_MZONE)>0
+		and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+end
+function c100912032.cttg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_COUNTER,nil,3,0,0x1f)
+end
+function c100912032.ctop(e,tp,eg,ep,ev,re,r,rp)
+	if e:GetHandler():IsRelateToEffect(e) then
+		e:GetHandler():AddCounter(0x1f,3)
+	end
+end
+function c100912032.rcttg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	if e:GetHandler():IsCanRemoveCounter(tp,0x1f,1,REASON_EFFECT) then
+		Duel.SetOperationInfo(0,CATEGORY_DESTROY,e:GetHandler(),1,0,0)
+	end
+end
+function c100912032.rctop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsFaceup() and c:IsRelateToEffect(e) then
+		if c:IsCanRemoveCounter(tp,0x1f,1,REASON_EFFECT) then
+			c:RemoveCounter(tp,0x1f,1,REASON_EFFECT)
+		else
+			Duel.Destroy(c,REASON_EFFECT)
+		end
+	end
+end
+function c100912032.tdcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsAbleToRemoveAsCost() end
+	Duel.Remove(e:GetHandler(),POS_FACEUP,REASON_COST)
+end
+function c100912032.tdfilter(c)
+	return c:IsSetCard(0x15) and c:IsAbleToDeck()
+end
+function c100912032.tdtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c100912032.tdfilter,tp,LOCATION_GRAVE,0,1,nil) end
+	local g=Duel.GetMatchingGroup(c100912032.tdfilter,tp,LOCATION_GRAVE,0,nil)
+	Duel.SetOperationInfo(0,CATEGORY_TODECK,g,1,0,0)
+end
+function c100912032.tdop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(c100912032.tdfilter,tp,LOCATION_GRAVE,0,nil)
+	Duel.SendtoDeck(g,nil,2,REASON_EFFECT)
+end
