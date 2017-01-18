f0f5e9bcf8973879e514569438cbb51380206d50
nekrozar <unyuho.buster@gmail.com>
2017-01-18 16:30:18 +0900

    add script VB19

---
+++ scripts/VB19-JP/c100215001.lua
@@ -0,0 +1,145 @@
+--FNo.0 未来皇ホープ－フューチャー・スラッシュ
+--Number F0: Utopic Future - Future Slash
+--Script by nekrozar
+function c100215001.initial_effect(c)
+	--xyz summon
+	c:EnableReviveLimit()
+	local e1=Effect.CreateEffect(c)
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_SPSUMMON_PROC)
+	e1:SetRange(LOCATION_EXTRA)
+	e1:SetCondition(c100215001.xyzcon)
+	e1:SetOperation(c100215001.xyzop)
+	e1:SetValue(SUMMON_TYPE_XYZ)
+	c:RegisterEffect(e1)
+	--atkup
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e2:SetCode(EFFECT_UPDATE_ATTACK)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetValue(c100215001.atkval)
+	c:RegisterEffect(e2)
+	--indes
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_SINGLE)
+	e3:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+	e3:SetValue(1)
+	c:RegisterEffect(e3)
+	--multi attack
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(100215001,0))
+	e4:SetType(EFFECT_TYPE_IGNITION)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetCountLimit(1)
+	e4:SetCondition(c100215001.atkcon)
+	e4:SetCost(c100215001.atkcost)
+	e4:SetTarget(c100215001.atktg)
+	e4:SetOperation(c100215001.atkop)
+	c:RegisterEffect(e4)
+end
+c100215001.xyz_number=0
+function c100215001.ovfilter(c,xyzc)
+	return c:IsFaceup() and (c:IsSetCard(0x107f) or c:IsCode(65305468)) and c:IsCanBeXyzMaterial(xyzc)
+end
+function c100215001.mfilter(c,xyzc)
+	return c:IsFaceup() and c:IsType(TYPE_XYZ) and not c:IsSetCard(0x48) and c:IsCanBeXyzMaterial(xyzc)
+end
+function c100215001.xyzfilter1(c,g)
+	return g:IsExists(c100215001.xyzfilter2,1,c,c:GetRank())
+end
+function c100215001.xyzfilter2(c,rk)
+	return c:GetRank()==rk
+end
+function c100215001.xyzcon(e,c,og,min,max)
+	if c==nil then return true end
+	local tp=c:GetControler()
+	local ct=-ft
+	if 2<=ct then return false end
+	if min and (min>2 or max<2) then return false end
+	local mg=nil
+	if og then
+		mg=og:Filter(c100215001.mfilter,nil,c)
+	else
+		mg=Duel.GetMatchingGroup(c100215001.mfilter,tp,LOCATION_MZONE,0,nil,c)
+	end
+	if ct<1 and (not min or min<=1) and mg:IsExists(c100215001.ovfilter,1,nil,c) then
+		return true
+	end
+	return mg:IsExists(c100215001.xyzfilter1,1,nil,mg)
+end
+function c100215001.xyzop(e,tp,eg,ep,ev,re,r,rp,c,og,min,max)
+	local g=nil
+	local sg=Group.CreateGroup()
+	if og and not min then
+		g=og
+		local tc=og:GetFirst()
+		while tc do
+			sg:Merge(tc:GetOverlayGroup())
+			tc=og:GetNext()
+		end
+		Duel.SendtoGrave(sg,REASON_RULE)
+	else
+		local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+		local ct=-ft
+		local mg=nil
+		if og then
+			mg=og:Filter(c100215001.mfilter,nil,c)
+		else
+			mg=Duel.GetMatchingGroup(c100215001.mfilter,tp,LOCATION_MZONE,0,nil,c)
+		end
+		local b1=mg:IsExists(c100215001.xyzfilter1,1,nil,mg)
+		local b2=ct<1 and (not min or min<=1) and mg:IsExists(c100215001.ovfilter,1,nil,c)
+		local g=nil
+		if b2 and (not b1 or Duel.SelectYesNo(tp,aux.Stringid(100215001,0))) then
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_XMATERIAL)
+			g=mg:FilterSelect(tp,c100215001.ovfilter,1,1,nil,c)
+			local g2=g:GetFirst():GetOverlayGroup()
+			if g2:GetCount()~=0 then
+				Duel.Overlay(c,g2)
+			end
+		else
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_XMATERIAL)
+			g=mg:FilterSelect(tp,c100215001.xyzfilter1,1,1,nil,mg)
+			local tc1=g:GetFirst()
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_XMATERIAL)
+			local g2=mg:FilterSelect(tp,c100215001.xyzfilter2,1,1,tc1,tc1:GetRank())
+			local tc2=g2:GetFirst()
+			g:Merge(g2)
+			sg:Merge(tc1:GetOverlayGroup())
+			sg:Merge(tc2:GetOverlayGroup())
+			Duel.SendtoGrave(sg,REASON_RULE)
+		end
+	end
+	c:SetMaterial(g)
+	Duel.Overlay(c,g)
+end
+function c100215001.atkfilter(c)
+	return c:IsType(TYPE_XYZ) and c:IsSetCard(0x48)
+end
+function c100215001.atkval(e,c)
+	return Duel.GetMatchingGroupCount(c100215001.atkfilter,c:GetControler(),LOCATION_GRAVE,LOCATION_GRAVE,nil)*500
+end
+function c100215001.atkcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsAbleToEnterBP()
+end
+function c100215001.atkcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():CheckRemoveOverlayCard(tp,1,REASON_COST) end
+	e:GetHandler():RemoveOverlayCard(tp,1,1,REASON_COST)
+end
+function c100215001.atktg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():GetEffectCount(EFFECT_EXTRA_ATTACK)==0 end
+end
+function c100215001.atkop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) then
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e1:SetCode(EFFECT_EXTRA_ATTACK)
+		e1:SetValue(1)
+		e1:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END)
+		c:RegisterEffect(e1)
+	end
+end
