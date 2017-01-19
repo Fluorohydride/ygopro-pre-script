11dc00e8987e4ad0a8cacaa5ea7f971f365119ae
mercury233 <me@mercury233.me>
2017-01-19 20:08:22 +0800

    update Zefratorah Grammaton
    the utility.lua should add description for
    Auxiliary.EnablePendulumAttribute too

--- scripts/MACR-JP/c100912030.lua
+++ scripts/MACR-JP/c100912030.lua
@@ -79,16 +79,47 @@ function c100912030.hspop(e,tp,eg,ep,ev,re,r,rp,c)
 	local g=Duel.GetReleaseGroup(tp)
 	Duel.Release(g,REASON_COST)
 	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
+	e1:SetCode(EVENT_CHAIN_SOLVED)
+	e1:SetOperation(c100912030.checkop)
+	e1:SetReset(RESET_PHASE+PHASE_END)
+	Duel.RegisterEffect(e1,tp)
+	c100912030.checkop(e,tp)
+	local olpz=Duel.GetFieldCard(1-tp,LOCATION_SZONE,6)
+	local orpz=Duel.GetFieldCard(1-tp,LOCATION_SZONE,7)
+	if olpz~=nil and orpz~=nil and olpz:GetFlagEffectLabel(31531170)==orpz:GetFieldID()
+		and orpz:GetFlagEffectLabel(31531170)==olpz:GetFieldID() then
+		local e2=Effect.CreateEffect(e:GetHandler())
+		e2:SetDescription(aux.Stringid(100912030,2))
+		e2:SetType(EFFECT_TYPE_FIELD)
+		e2:SetCode(EFFECT_SPSUMMON_PROC_G)
+		e2:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_BOTH_SIDE)
+		e2:SetRange(LOCATION_PZONE)
+		e2:SetCountLimit(1,100912130)
+		e2:SetCondition(c100912030.pencon2)
+		e2:SetOperation(c100912030.penop2)
+		e2:SetValue(SUMMON_TYPE_PENDULUM)
+		e2:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END)
+		olpz:RegisterEffect(e2)
+		olpz:RegisterFlagEffect(100912030,RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END,0,1)
+	end
+end
+function c100912030.checkop(e,tp)
+	local lpz=Duel.GetFieldCard(tp,LOCATION_SZONE,6)
+	if lpz==nil or lpz:GetFlagEffect(100912030)>0 then return end
+	local e1=Effect.CreateEffect(e:GetHandler())
 	e1:SetDescription(aux.Stringid(100912030,2))
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetCode(EFFECT_SPSUMMON_PROC_G)
 	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE)
-	e1:SetRange(LOCATION_MZONE)
+	e1:SetRange(LOCATION_PZONE)
 	e1:SetCountLimit(1,100912130)
 	e1:SetCondition(c100912030.pencon)
 	e1:SetOperation(c100912030.penop)
 	e1:SetValue(SUMMON_TYPE_PENDULUM)
-	c:RegisterEffect(e1)
+	e1:SetReset(RESET_PHASE+PHASE_END)
+	lpz:RegisterEffect(e1)
+	lpz:RegisterFlagEffect(100912030,RESET_PHASE+PHASE_END,0,1)
 end
 function c100912030.penfilter(c,e,tp,lscale,rscale)
 	local lv=0
@@ -104,10 +135,10 @@ end
 function c100912030.pencon(e,c,og)
 	if c==nil then return true end
 	local tp=c:GetControler()
-	local lpz=Duel.GetFieldCard(tp,LOCATION_SZONE,6)
+	if c:GetSequence()~=6 then return false end
 	local rpz=Duel.GetFieldCard(tp,LOCATION_SZONE,7)
-	if lpz==nil or rpz==nil then return false end
-	local lscale=lpz:GetLeftScale()
+	if rpz==nil then return false end
+	local lscale=c:GetLeftScale()
 	local rscale=rpz:GetRightScale()
 	if lscale>rscale then lscale,rscale=rscale,lscale end
 	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
@@ -119,9 +150,9 @@ function c100912030.pencon(e,c,og)
 	end
 end
 function c100912030.penop(e,tp,eg,ep,ev,re,r,rp,c,sg,og)
-	local lpz=Duel.GetFieldCard(tp,LOCATION_SZONE,6)
+	Duel.Hint(HINT_CARD,0,100912030)
 	local rpz=Duel.GetFieldCard(tp,LOCATION_SZONE,7)
-	local lscale=lpz:GetLeftScale()
+	local lscale=c:GetLeftScale()
 	local rscale=rpz:GetRightScale()
 	if lscale>rscale then lscale,rscale=rscale,lscale end
 	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
@@ -156,6 +187,64 @@ function c100912030.penop(e,tp,eg,ep,ev,re,r,rp,c,sg,og)
 		end
 	end
 	Duel.HintSelection(Group.FromCards(lpz))
+	Duel.HintSelection(Group.FromCards(rpz))
+end
+function c100912030.pencon2(e,c,og)
+	if c==nil then return true end
+	local tp=e:GetOwnerPlayer()
+	local rpz=Duel.GetFieldCard(1-tp,LOCATION_SZONE,7)
+	if rpz==nil or rpz:GetFieldID()~=c:GetFlagEffectLabel(31531170) then return false end
+	local lscale=c:GetLeftScale()
+	local rscale=rpz:GetRightScale()
+	if lscale>rscale then lscale,rscale=rscale,lscale end
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	if ft<=0 then return false end
+	if og then
+		return og:IsExists(c100912030.penfilter,1,nil,e,tp,lscale,rscale)
+	else
+		return Duel.IsExistingMatchingCard(c100912030.penfilter,tp,LOCATION_EXTRA,0,1,nil,e,tp,lscale,rscale)
+	end
+end
+function c100912030.penop2(e,tp,eg,ep,ev,re,r,rp,c,sg,og)
+	Duel.Hint(HINT_CARD,0,31531170)
+	Duel.Hint(HINT_CARD,0,100912030)
+	local tp=e:GetOwnerPlayer()
+	local rpz=Duel.GetFieldCard(1-tp,LOCATION_SZONE,7)
+	local lscale=c:GetLeftScale()
+	local rscale=rpz:GetRightScale()
+	if lscale>rscale then lscale,rscale=rscale,lscale end
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	if Duel.IsPlayerAffectedByEffect(tp,59822133) then ft=1 end
+	local tg=nil
+	if og then
+		tg=og:Filter(tp,c100912030.penfilter,nil,e,tp,lscale,rscale)
+	else
+		tg=Duel.GetMatchingGroup(c100912030.penfilter,tp,LOCATION_EXTRA,0,nil,e,tp,lscale,rscale)
+	end
+	local ect=c29724053 and Duel.IsPlayerAffectedByEffect(tp,29724053) and c29724053[tp]
+	if ect and (ect<=0 or ect>ft) then ect=nil end
+	if ect==nil or tg:FilterCount(Card.IsLocation,nil,LOCATION_EXTRA)<=ect then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+		local g=tg:Select(tp,1,ft,nil)
+		sg:Merge(g)
+	else
+		repeat
+			local ct=math.min(ft,ect)
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+			local g=tg:Select(tp,1,ct,nil)
+			tg:Sub(g)
+			sg:Merge(g)
+			ft=ft-g:GetCount()
+			ect=ect-g:FilterCount(Card.IsLocation,nil,LOCATION_EXTRA)
+		until ft==0 or ect==0 or not Duel.SelectYesNo(tp,210)
+		local hg=tg:Filter(Card.IsLocation,nil,LOCATION_HAND)
+		if ft>0 and ect==0 and hg:GetCount()>0 and Duel.SelectYesNo(tp,210) then
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+			local g=hg:Select(tp,1,ft,nil)
+			sg:Merge(g)
+		end
+	end
+	Duel.HintSelection(Group.FromCards(c))
 	Duel.HintSelection(Group.FromCards(rpz))
 end
 function c100912030.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
