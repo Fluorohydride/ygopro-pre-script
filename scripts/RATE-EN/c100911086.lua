fdf9d5004ce6d540ab39fa13eb50e76e56b47aa8
EerieCode <eeriecode@gmail.com>
2017-01-24 21:38:29 +0100

    Add RATE-EN086 --SPYGAL Misty

---
+++ scripts/RATE-EN/c100911086.lua
@@ -0,0 +1,66 @@
+--SPYGAL Misty
+--Scripted by Eerie Code
+function c100911086.initial_effect(c)
+	--reveal
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(100911086,0))
+	e1:SetCategory(CATEGORY_DRAW)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_SUMMON_SUCCESS)
+	e1:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetTarget(c100911086.drtg)
+	e1:SetOperation(c100911086.drop)
+	c:RegisterEffect(e1)
+	local e2=e1:Clone()
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	c:RegisterEffect(e2)
+	--rtohand
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(100911086,1))
+	e3:SetCategory(CATEGORY_TOHAND)
+	e3:SetType(EFFECT_TYPE_QUICK_O)
+	e3:SetCode(EVENT_FREE_CHAIN)
+	e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e3:SetCountLimit(1)
+	e3:SetTarget(c100911086.thtg)
+	e3:SetOperation(c100911086.thop)
+	c:RegisterEffect(e3)
+end
+function c100911086.drtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsPlayerCanDraw(tp,1)
+		and Duel.GetFieldGroupCount(tp,0,LOCATION_DECK)>0 end
+	Duel.SetTargetPlayer(tp)
+	Duel.SetTargetParam(1)
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
+end
+function c100911086.drop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetFieldGroupCount(tp,0,LOCATION_DECK)==0 then return end
+	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+	Duel.ConfirmDecktop(1-tp,1)
+	local g=Duel.GetDecktopGroup(1-tp,1)
+	local tc=g:GetFirst()
+	local opt=e:GetLabel()
+	if (opt==0 and tc:IsType(TYPE_MONSTER)) or (opt==1 and tc:IsType(TYPE_SPELL)) or (opt==2 and tc:IsType(TYPE_TRAP)) then
+		Duel.Draw(p,d,REASON_EFFECT)
+	end
+end
+function c100911086.thfilter(c)
+	return c:IsFaceup() and c:IsCode(41091257) and c:IsAbleToHand()
+end
+function c100911086.thtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return false end
+	if chk==0 then return Duel.IsExistingTarget(c100911086.thfilter,tp,LOCATION_MZONE,0,1,nil)
+		and Duel.IsExistingTarget(Card.IsAbleToHand,tp,0,LOCATION_MZONE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RTOHAND)
+	local g1=Duel.SelectTarget(tp,c100911086.thfilter,tp,LOCATION_MZONE,0,1,1,nil)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RTOHAND)
+	local g2=Duel.SelectTarget(tp,Card.IsAbleToHand,tp,0,LOCATION_MZONE,1,1,nil)
+	g1:Merge(g2)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g1,2,0,0)
+end
+function c100911086.thop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS):Filter(Card.IsRelateToEffect,nil,e)
+	if g:GetCount()==2 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+	end
+end
