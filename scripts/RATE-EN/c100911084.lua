971b38e92815b89922ebba12e712aa892203c50e
EerieCode <eeriecode@gmail.com>
2017-01-27 22:02:34 +0100

    Add RATE-EN084 --Subterror Behemoth Voltelluric

---
+++ scripts/RATE-EN/c100911084.lua
@@ -0,0 +1,88 @@
+--Subterror Behemoth Voltelluric
+--Scripted by Eerie Code
+function c100911084.initial_effect(c)
+	--flip
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(100911084,0))
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_FLIP+EFFECT_TYPE_TRIGGER_O)
+	e1:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
+	e1:SetCountLimit(1,100911084)
+	e1:SetTarget(c100911084.target)
+	e1:SetOperation(c100911084.operation)
+	c:RegisterEffect(e1)
+	--special summon
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(100911084,1))
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e2:SetRange(LOCATION_HAND)
+	e2:SetCode(EVENT_CHANGE_POS)
+	e2:SetCondition(c100911084.spcon)
+	e2:SetTarget(c100911084.sptg)
+	e2:SetOperation(c100911084.spop)
+	c:RegisterEffect(e2)
+	--turn set
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(100911084,2))
+	e3:SetCategory(CATEGORY_POSITION)
+	e3:SetType(EFFECT_TYPE_IGNITION)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetTarget(c100911084.postg)
+	e3:SetOperation(c100911084.posop)
+	c:RegisterEffect(e3)
+end
+function c100911084.filter(c)
+	return c:IsFacedown() and c:IsControlerCanBeChanged()
+end
+function c100911084.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) and c100911084.filter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(c100911084.filter,tp,0,LOCATION_MZONE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CONTROL)
+	local g=Duel.SelectTarget(tp,c100911084.filter,tp,0,LOCATION_MZONE,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_CONTROL,g,1,0,0)
+end
+function c100911084.operation(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	local tct=1
+	if Duel.GetTurnPlayer()~=tp then tct=2
+	elseif Duel.GetCurrentPhase()==PHASE_END then tct=3 end
+	if tc:IsFaceup() and tc:IsRelateToEffect(e) then
+		Duel.GetControl(tc,tp,PHASE_END,tct)
+	end
+end
+function c100911084.indval(e,re,rp)
+	return rp~=e:GetHandlerPlayer()
+end
+function c100911084.cfilter(c,tp)
+	return c:IsPreviousPosition(POS_FACEUP) and c:IsFacedown() and c:IsControler(tp)
+end
+function c100911084.spcon(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(c100911084.cfilter,1,nil,tp)
+		and not Duel.IsExistingMatchingCard(Card.IsFaceup,tp,LOCATION_MZONE,0,1,nil)
+end
+function c100911084.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
+end
+function c100911084.spop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not c:IsRelateToEffect(e) then return end
+	if Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP_DEFENSE)==0
+		and Duel.GetLocationCount(tp,LOCATION_MZONE)<=0
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false) then
+		Duel.SendtoGrave(c,REASON_RULE)
+	end
+end
+function c100911084.postg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return c:IsCanTurnSet() and c:GetFlagEffect(100911084)==0 end
+	c:RegisterFlagEffect(100911084,RESET_EVENT+0x1fc0000+RESET_PHASE+PHASE_END,0,1)
+	Duel.SetOperationInfo(0,CATEGORY_POSITION,c,1,0,0)
+end
+function c100911084.posop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) and c:IsFaceup() then
+		Duel.ChangePosition(c,POS_FACEDOWN_DEFENSE)
+	end
+end
