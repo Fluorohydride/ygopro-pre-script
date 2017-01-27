54f8211e0c8256918a16d6338d2874fae15d8299
EerieCode <eeriecode@gmail.com>
2017-01-27 22:11:18 +0100

    Add RATE-EN085 --Clash in the Subterror Caverns

---
+++ scripts/RATE-EN/c100911085.lua
@@ -0,0 +1,57 @@
+--Clash in the Subterror Caverns
+--Scripted by Eerie Code
+function c100911085.initial_effect(c)
+	--activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e1)
+	--atk up
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_UPDATE_ATTACK)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetTargetRange(LOCATION_MZONE,0)
+	e2:SetTarget(c100911085.atktg)
+	e2:SetValue(c100911085.atkval)
+	c:RegisterEffect(e2)
+	--
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(100911085,0))
+	e3:SetCategory(CATEGORY_TOHAND)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e3:SetRange(LOCATION_SZONE)
+	e3:SetCode(EVENT_BATTLE_DAMAGE)
+	e3:SetCondition(c100911085.thcon)
+	e3:SetTarget(c100911085.thtg)
+	e3:SetOperation(c100911085.thop)
+	c:RegisterEffect(e3)
+end
+function c100911085.atktg(e,c)
+	return c:IsSetCard(0xed)
+end
+function c100911085.atkval(e,c)
+	return Duel.GetMatchingGroupCount(Card.IsFacedown,c:GetControler(),LOCATION_MZONE,LOCATION_MZONE,nil)*500
+end
+function c100911085.thcon(e,tp,eg,ep,ev,re,r,rp)
+	if ep==tp then return false end
+	local rc=eg:GetFirst()
+	return rc:IsControler(tp) and rc:IsSetCard(0xed)
+end
+function c100911085.thfilter(c)
+	return c:IsSetCard(0xed) and not c:IsCode(100911085) and c:IsAbleToHand()
+end
+function c100911085.thtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and c100911085.thfilter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(c100911085.thfilter,tp,LOCATION_GRAVE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local sg=Duel.SelectTarget(tp,c100911085.thfilter,tp,LOCATION_GRAVE,0,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,sg,sg:GetCount(),0,0)
+end
+function c100911085.thop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) then
+		Duel.SendtoHand(tc,nil,REASON_EFFECT)
+	end
+end
