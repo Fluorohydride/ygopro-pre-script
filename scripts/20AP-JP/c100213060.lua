f6a2f2db328eef68504358510468773ae2013533
nekrozar <unyuho.buster@gmail.com>
2017-02-11 11:33:55 +0900

    re-write
    http://www.db.yugioh-card.com/yugiohdb/faq_search.action?ope=5&fid=20602&keyword=&tag=-1
    
    http://www.db.yugioh-card.com/yugiohdb/faq_search.action?ope=5&fid=20603&keyword=&tag=-1
    
    http://www.db.yugioh-card.com/yugiohdb/faq_search.action?ope=5&fid=20605&keyword=&tag=-1
    
    http://www.db.yugioh-card.com/yugiohdb/faq_search.action?ope=5&fid=20647&keyword=&tag=-1

--- scripts/20AP-JP/c100213060.lua
+++ scripts/20AP-JP/c100213060.lua
@@ -7,153 +7,109 @@ function c100213060.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	c:RegisterEffect(e1)
-	--on special summon
+	--draw
 	local e2=Effect.CreateEffect(c)
-	e2:SetCategory(CATEGORY_DRAW)
 	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
 	e2:SetRange(LOCATION_FZONE)
-	e2:SetOperation(c100213060.spop)
+	e2:SetCountLimit(1)
+	e2:SetCondition(c100213060.spcon1)
+	e2:SetOperation(c100213060.drop1)
 	c:RegisterEffect(e2)
-	--battled 5 times
 	local e3=e2:Clone()
-	e3:SetCode(EVENT_BATTLED)
-	e3:SetOperation(c100213060.batop)
+	e3:SetCondition(c100213060.spcon2)
+	e3:SetOperation(c100213060.drop2)
 	c:RegisterEffect(e3)
-	--on chain 5
 	local e4=e2:Clone()
-	e4:SetCode(EVENT_CHAINING)
-	e4:SetOperation(c100213060.chainop)
+	e4:SetCode(EVENT_BATTLED)
+	e4:SetCondition(c100213060.btcon1)
 	c:RegisterEffect(e4)
-	--gambled 5 times
-	local e5=e2:Clone()
-	e5:SetCode(EVENT_CUSTOM+100213060)
-	e5:SetOperation(c100213060.gamop)
+	local e5=e3:Clone()
+	e5:SetCode(EVENT_BATTLED)
+	e5:SetCondition(c100213060.btcon2)
 	c:RegisterEffect(e5)
-	--damage
+	local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e0:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e0:SetCode(EVENT_CHAINING)
+	e0:SetRange(LOCATION_FZONE)
+	e0:SetOperation(aux.chainreg)
+	c:RegisterEffect(e0)
 	local e6=e2:Clone()
-	e6:SetCode(EVENT_DAMAGE)
-	e6:SetOperation(c100213060.damop)
+	e6:SetCode(EVENT_CHAIN_SOLVING)
+	e6:SetCondition(c100213060.chcon1)
 	c:RegisterEffect(e6)
-	--event counters
-	if not c100213060.global_flag then
-		c100213060.global_flag=true
-		c100213060[0]=0
-		c100213060[1]=0
-		--Checks effects that make a player toss a coin or throw a dice
-		local ge1=Effect.CreateEffect(c)
-		ge1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-		ge1:SetCode(EVENT_CHAINING)
-		ge1:SetOperation(c100213060.checkop)
-		Duel.RegisterEffect(ge1,0)
-		--Checks effects that allow a player to redo a coin toss or dice roll
-		local ge2=Effect.CreateEffect(c)
-		ge2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-		ge2:SetCode(EVENT_CUSTOM+100213060+100)
-		ge2:SetOperation(c100213060.checkop2)
-		Duel.RegisterEffect(ge2,0)
-		--Counts the number of times a monster has battled
-		local ge3=Effect.CreateEffect(c)
-		ge3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-		ge3:SetCode(EVENT_BATTLE_CONFIRM)
-		ge3:SetOperation(c100213060.checkop3)
-		Duel.RegisterEffect(ge3,0)
-		--Reset at the start of the next Draw Phase
-		local ge0=Effect.CreateEffect(c)
-		ge0:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-		ge0:SetCode(EVENT_PHASE_START+PHASE_DRAW)
-		ge0:SetOperation(c100213060.clear)
-		--Duel.RegisterEffect(ge0,0)
-	end
+	local e7=e3:Clone()
+	e7:SetCode(EVENT_CHAIN_SOLVING)
+	e7:SetCondition(c100213060.chcon2)
+	c:RegisterEffect(e7)
+	local e8=e2:Clone()
+	e8:SetCode(EVENT_CHAIN_SOLVED)
+	e8:SetCondition(c100213060.tosscon1)
+	c:RegisterEffect(e8)
+	local e9=e3:Clone()
+	e9:SetCode(EVENT_CHAIN_SOLVED)
+	e9:SetCondition(c100213060.tosscon2)
+	c:RegisterEffect(e9)
+	local ea=e2:Clone()
+	ea:SetCode(EVENT_DAMAGE)
+	ea:SetCondition(c100213060.damcon1)
+	c:RegisterEffect(ea)
+	local eb=e3:Clone()
+	eb:SetCode(EVENT_DAMAGE)
+	eb:SetCondition(c100213060.damcon2)
+	c:RegisterEffect(eb)
 end
-function c100213060.checkop(e,tp,eg,ep,ev,re,r,rp)
-	local ex1,g1,gc1,dp1,dv1=Duel.GetOperationInfo(0,CATEGORY_DICE)
-	local ex2,g2,gc2,dp2,dv2=Duel.GetOperationInfo(0,CATEGORY_COIN)
-	if ex1 then
-		if dp1==PLAYER_ALL then
-			c100213060[0]=c100213060[0]+dv1
-			c100213060[1]=c100213060[1]+dv1
-		else
-			c100213060[dp1]=c100213060[dp1]+dv1
-		end
-	end
-	if ex2 then
-		if dp2==PLAYER_ALL then
-			c100213060[0]=c100213060[0]+dv2
-			c100213060[1]=c100213060[1]+dv2
-		else
-			c100213060[dp2]=c100213060[dp2]+dv2
-		end
-	end
-	Duel.RaiseSingleEvent(e:GetHandler(),EVENT_CUSTOM+100213060,re,r,rp,0,0)
+function c100213060.spfilter(c,tp)
+	return c:GetSummonPlayer()==tp
 end
-function c100213060.checkop2(e,tp,eg,ep,ev,re,r,rp)
-	if ep==PLAYER_ALL then
-		c100213060[0]=c100213060[0]+ev
-		c100213060[1]=c100213060[1]+ev
-	else
-		c100213060[ep]=c100213060[ep]+ev
-	end
-	Duel.RaiseSingleEvent(e:GetHandler(),EVENT_CUSTOM+100213060,re,r,rp,0,0)
+function c100213060.spcon1(e,tp,eg,ep,ev,re,r,rp)
+	return eg:GetCount()==5 and eg:IsExists(c100213060.spfilter,1,nil,tp) and eg:GetClassCount(Card.GetLevel)==5
 end
-function c100213060.checkop3(e,tp,eg,ep,ev,re,r,rp)
+function c100213060.spcon2(e,tp,eg,ep,ev,re,r,rp)
+	return eg:GetCount()==5 and eg:IsExists(c100213060.spfilter,1,nil,1-tp) and eg:GetClassCount(Card.GetLevel)==5
+end
+function c100213060.drop1(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_CARD,0,100213060)
+	Duel.Draw(tp,2,REASON_EFFECT)
+end
+function c100213060.drop2(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_CARD,0,100213060)
+	Duel.Draw(1-tp,2,REASON_EFFECT)
+end
+function c100213060.btcon1(e,tp,eg,ep,ev,re,r,rp)
 	local a=Duel.GetAttacker()
 	local d=Duel.GetAttackTarget()
-	a:RegisterFlagEffect(100213060,RESET_EVENT+0x1fe0000,0,1)
-	if d then d:RegisterFlagEffect(100213060,RESET_EVENT+0x1fe0000,0,1) end
+	if a:IsControler(1-tp) then a,d=d,a end
+	if a then
+		a:RegisterFlagEffect(100213060,RESET_EVENT+0x1fe0000,0,1)
+		return a:GetFlagEffect(100213060)==5
+	else return false end
 end
-function c100213060.clear(e,tp,eg,ep,ev,re,r,rp)
-	c100213060[0]=0
-	c100213060[1]=0
-end
---Utility to avoid repeating the same lines of code 5 times
-function c100213060.draw_op(e,p,flag)
-	Duel.Hint(HINT_CARD,0,e:GetHandler():GetCode())
-	Duel.Draw(p,2,REASON_EFFECT)
-	e:GetHandler():RegisterFlagEffect(flag+p,RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END,0,1)
-end
-function c100213060.spop(e,tp,eg,ep,ev,re,r,rp)
-	if not eg then return end
-	for p=0,1 do
-		if e:GetHandler():GetFlagEffect(100213060+p)==0 then
-			if eg:Filter(Card.IsControler,nil,p):GetClassCount(Card.GetLevel)==5 then
-				c100213060.draw_op(e,p,100213060)
-			end
-		end
-	end
-end
-function c100213060.batop(e,tp,eg,ep,ev,re,r,rp)
+function c100213060.btcon2(e,tp,eg,ep,ev,re,r,rp)
 	local a=Duel.GetAttacker()
 	local d=Duel.GetAttackTarget()
-	local g=nil
-	if d then g=Group.FromCards(a,d) else g=Group.FromCards(a) end
-	local tc=g:GetFirst()
-	while tc do
-		local p=tc:GetControler()
-		if tc:GetFlagEffect(100213060)==5 and e:GetHandler():GetFlagEffect(100213160+p)==0 then
-			c100213060.draw_op(e,p,100213160)
-		end
-		tc=g:GetNext()
-	end
+	if a:IsControler(tp) then a,d=d,a end
+	if a then
+		a:RegisterFlagEffect(100213060,RESET_EVENT+0x1fe0000,0,1)
+		return a:GetFlagEffect(100213060)==5
+	else return false end
 end
-function c100213060.chainop(e,tp,eg,ep,ev,re,r,rp)
-	local ch=Duel.GetCurrentChain()
-	if ch>=5 and e:GetHandler():GetFlagEffect(100213260+rp)==0 then
-		c100213060.draw_op(e,rp,100213260)
-	end
+function c100213060.chcon1(e,tp,eg,ep,ev,re,r,rp)
+	return rp==tp and Duel.GetCurrentChain()==5 and e:GetHandler():GetFlagEffect(1)>0
 end
-function c100213060.gamop(e,tp,eg,ep,ev,re,r,rp)
-	for p=0,1 do
-		if e:GetHandler():GetFlagEffect(100213360+p)==0 and c100213060[p]==5 then
-			c100213060.draw_op(e,p,100213360)
-		end
-	end
+function c100213060.chcon2(e,tp,eg,ep,ev,re,r,rp)
+	return rp==1-tp and Duel.GetCurrentChain()==5 and e:GetHandler():GetFlagEffect(1)>0
 end
-function c100213060.damop(e,tp,eg,ep,ev,re,r,rp)
-	for p=0,1 do
-		if (ep==p or ep==PLAYER_ALL) and Duel.GetLP(p)<=500
-			and e:GetHandler():GetFlagEffect(100213460+p)==0 then
-			c100213060.draw_op(e,p,100213460)
-		end
-	end
+function c100213060.tosscon1(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetTossedCoinCount(tp)+Duel.GetTossedDiceCount(tp)>=5 and e:GetHandler():GetFlagEffect(1)>0
+end
+function c100213060.tosscon2(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetTossedCoinCount(1-tp)+Duel.GetTossedDiceCount(1-tp)>=5 and e:GetHandler():GetFlagEffect(1)>0
+end
+function c100213060.damcon1(e,tp,eg,ep,ev,re,r,rp)
+	return ep==tp and Duel.GetLP(tp)<=500
+end
+function c100213060.damcon2(e,tp,eg,ep,ev,re,r,rp)
+	return ep==1-tp and Duel.GetLP(1-tp)<=500
 end
