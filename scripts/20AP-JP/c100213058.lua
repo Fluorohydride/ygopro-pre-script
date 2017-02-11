5b3c0a4bbae9db925ffb24952557a57c7d14f23e
nekrozar <unyuho.buster@gmail.com>
2017-02-11 09:50:53 +0900

    fix
    http://www.db.yugioh-card.com/yugiohdb/faq_search.action?ope=4&cid=13005
    
    http://www.db.yugioh-card.com/yugiohdb/faq_search.action?ope=5&fid=20607&keyword=&tag=-1

--- scripts/20AP-JP/c100213058.lua
+++ scripts/20AP-JP/c100213058.lua
@@ -11,75 +11,93 @@ function c100213058.initial_effect(c)
 	e1:SetTarget(c100213058.target)
 	e1:SetOperation(c100213058.activate)
 	c:RegisterEffect(e1)
-	--flag drawn
+	--public
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 	e2:SetCode(EVENT_DRAW)
 	e2:SetRange(LOCATION_SZONE)
 	e2:SetOperation(c100213058.drop)
 	c:RegisterEffect(e2)
-	--cannot summon
+	local g=Group.CreateGroup()
+	g:KeepAlive()
+	e2:SetLabelObject(g)
 	local e3=Effect.CreateEffect(c)
 	e3:SetType(EFFECT_TYPE_FIELD)
-	e3:SetCode(EFFECT_CANNOT_SUMMON)
-	e3:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e3:SetCode(EFFECT_PUBLIC)
 	e3:SetRange(LOCATION_SZONE)
-	e3:SetTargetRange(1,0)
-	e3:SetCondition(c100213058.scon1)
+	e3:SetTargetRange(LOCATION_HAND,LOCATION_HAND)
+	e3:SetCondition(c100213058.pubcon)
+	e3:SetTarget(c100213058.pubtg)
+	e3:SetLabelObject(g)
 	c:RegisterEffect(e3)
-	local e4=e3:Clone()
-	e4:SetCode(EFFECT_CANNOT_MSET)
+	--cannot summon
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_FIELD)
+	e4:SetCode(EFFECT_CANNOT_SUMMON)
+	e4:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e4:SetRange(LOCATION_SZONE)
+	e4:SetTargetRange(1,0)
+	e4:SetCondition(c100213058.scon1)
+	e4:SetLabelObject(g)
 	c:RegisterEffect(e4)
-	local e5=e3:Clone()
-	e5:SetTargetRange(0,1)
-	e5:SetCondition(c100213058.scon2)
+	local e5=e4:Clone()
+	e5:SetCode(EFFECT_CANNOT_MSET)
 	c:RegisterEffect(e5)
-	local e6=e5:Clone()
-	e6:SetCode(EFFECT_CANNOT_MSET)
+	local e6=e4:Clone()
+	e6:SetTargetRange(0,1)
+	e6:SetCondition(c100213058.scon2)
 	c:RegisterEffect(e6)
+	local e7=e6:Clone()
+	e7:SetCode(EFFECT_CANNOT_MSET)
+	c:RegisterEffect(e7)
 end
 function c100213058.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.GetLP(tp)>1000 and Duel.IsPlayerCanDraw(tp,1)
-		and Duel.GetLP(1-tp)>1000 and Duel.IsPlayerCanDraw(1-tp,1) end
+	if chk==0 then return Duel.GetLP(tp)>=1000 and Duel.IsPlayerCanDraw(tp,1)
+		and Duel.GetLP(1-tp)>=1000 and Duel.IsPlayerCanDraw(1-tp,1) end
 	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,PLAYER_ALL,1)
 end
 function c100213058.activate(e,tp,eg,ep,ev,re,r,rp)
 	if not e:GetHandler():IsRelateToEffect(e) then return end
-	local lp0=Duel.GetLP(0)
+	local lp0=Duel.GetLP(tp)
 	if lp0>1000 then
-		Duel.SetLP(0,lp0-1000)
-		Duel.Draw(0,1,REASON_EFFECT)
+		Duel.SetLP(tp,lp0-1000)
+		Duel.Draw(tp,1,REASON_EFFECT)
 	end
-	local lp1=Duel.GetLP(1)
+	local lp1=Duel.GetLP(1-tp)
 	if lp1>1000 then
-		Duel.SetLP(1,lp1-1000)
-		Duel.Draw(1,1,REASON_EFFECT)
+		Duel.SetLP(1-tp,lp1-1000)
+		Duel.Draw(1-tp,1,REASON_EFFECT)
 	end
 end
 function c100213058.drop(e,tp,eg,ep,ev,re,r,rp)
 	if not eg then return end
 	local c=e:GetHandler()
+	local pg=e:GetLabelObject()
+	if c:GetFlagEffect(100213058)==0 then
+		c:RegisterFlagEffect(100213058,RESET_EVENT+0x1ff0000,0,1)
+		pg:Clear()
+	end
 	local tc=eg:GetFirst()
 	while tc do
-		local e1=Effect.CreateEffect(c)
-		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetCode(EFFECT_PUBLIC)
-		e1:SetReset(RESET_EVENT+0x1fe0000)
-		tc:RegisterEffect(e1)
-		if tc:IsType(TYPE_SPELL) then
-			tc:RegisterFlagEffect(100213058,RESET_EVENT+0x1fe0000,0,1)
-		end
+		pg:AddCard(tc)
+		tc:RegisterFlagEffect(100213158,RESET_EVENT+0x1fe0000,0,1)
 		tc=eg:GetNext()
 	end
 end
-function c100213058.sfilter(c)
-	return c:IsPublic() and c:GetFlagEffect(100213058)>0
+function c100213058.pubcon(e)
+	return e:GetHandler():GetFlagEffect(100213058)~=0
+end
+function c100213058.pubtg(e,c)
+	return e:GetLabelObject():IsContains(c) and c:GetFlagEffect(100213158)~=0
+end
+function c100213058.sfilter(c,pg)
+	return c:IsPublic() and pg:IsContains(c) and c:GetFlagEffect(100213158)>0 and c:IsType(TYPE_SPELL)
 end
 function c100213058.scon1(e)
 	local tp=e:GetHandlerPlayer()
-	return Duel.IsExistingMatchingCard(c100213058.sfilter,tp,LOCATION_HAND,0,1,nil)
+	return Duel.IsExistingMatchingCard(c100213058.sfilter,tp,LOCATION_HAND,0,1,nil,e:GetLabelObject())
 end
 function c100213058.scon2(e)
 	local tp=e:GetHandlerPlayer()
-	return Duel.IsExistingMatchingCard(c100213058.sfilter,tp,0,LOCATION_HAND,1,nil)
+	return Duel.IsExistingMatchingCard(c100213058.sfilter,tp,0,LOCATION_HAND,1,nil,e:GetLabelObject())
 end
