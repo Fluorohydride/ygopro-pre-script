2a79def5a34cc73146431272cb936e52bb0d34e7
EerieCode <eeriecode@gmail.com>
2017-02-18 12:53:20 +0100

    Add COTD-JP001 --サイバース・ウィザード

---
+++ scripts/COTD-JP/c101001001.lua
@@ -0,0 +1,59 @@
+--サイバース・ウィザード
+--Cybers Wizard
+--Scripted by Eerie Code
+function c101001001.initial_effect(c)
+	--change pos
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_POSITION)
+	e1:SetDescription(aux.Stringid(101001001,0))
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetCountLimit(1)
+	e1:SetTarget(c101001001.postg)
+	e1:SetOperation(c101001001.posop)
+	c:RegisterEffect(e1)
+end
+function c101001001.postg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsPosition(POS_FACEUP_ATTACK) end
+	if chk==0 then return Duel.IsExistingTarget(Card.IsPosition,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil,POS_FACEUP_ATTACK) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_POSCHANGE)
+	local g=Duel.SelectTarget(tp,Card.IsPosition,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil,POS_FACEUP_ATTACK)
+	Duel.SetOperationInfo(0,CATEGORY_POSITION,g,1,0,0)
+end
+function c101001001.posop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) then
+		Duel.ChangePosition(tc,POS_FACEUP_DEFENSE)
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_FIELD)
+		e1:SetCode(EFFECT_MUST_ATTACK_MONSTER)
+		e1:SetTargetRange(LOCATION_MZONE,0)
+		e1:SetReset(RESET_PHASE+PHASE_END)
+		Duel.RegisterEffect(e1,tp)
+		local e2=Effect.CreateEffect(c)
+		e2:SetType(EFFECT_TYPE_FIELD)
+		e2:SetCode(EFFECT_MUST_BE_ATTACKED)
+		e2:SetTargetRange(0,LOCATION_MZONE)
+		e2:SetTarget(c101001001.attg)
+		e2:SetLabelObject(tc)
+		e2:SetValue(1)
+		e2:SetReset(RESET_PHASE+PHASE_END)
+		Duel.RegisterEffect(e2,tp)
+		--pierce
+		local e3=Effect.CreateEffect(c)
+		e3:SetType(EFFECT_TYPE_FIELD)
+		e3:SetCode(EFFECT_PIERCE)
+		e3:SetTargetRange(LOCATION_MZONE,0)
+		e3:SetTarget(c101001001.target)
+		e3:SetReset(RESET_PHASE+PHASE_END)
+		Duel.RegisterEffect(e3,tp)
+	end
+end
+function c101001001.attg(e,c)
+	return c==e:GetLabelObject()
+end
+function c101001001.target(e,c)
+	return c:IsRace(RACE_CYBERS)
+end
