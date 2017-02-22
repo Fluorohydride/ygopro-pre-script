12e72cea0a7918b7ed4389d810b28d0e50c8aa42
dest <dest.dn@gmail.com>
2017-02-22 19:32:01 +0100

    fix

--- scripts/VJMP-JP/c100200129.lua
+++ scripts/VJMP-JP/c100200129.lua
@@ -53,13 +53,9 @@ function c100200129.drop(e,tp,eg,ep,ev,re,r,rp)
 	if not e:GetHandler():IsRelateToEffect(e) then return end
 	Duel.Draw(tp,1,REASON_EFFECT)
 end
-function c100200129.damfilter(c)
-	return c:IsControler(tp) and c:IsSetCard(0xad)
-		and bit.band(c:GetSummonType(),SUMMON_TYPE_FUSION)==SUMMON_TYPE_FUSION
-end
 function c100200129.damtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(c100200129.damfilter,tp,LOCATION_GRAVE,0,1,nil) end
-	local val=Duel.GetMatchingGroupCount(c100200129.damfilter,tp,LOCATION_MZONE,0,nil)*200
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsSetCard,tp,LOCATION_GRAVE,0,1,nil,0xad) end
+	local val=Duel.GetMatchingGroupCount(Card.IsSetCard,tp,LOCATION_GRAVE,0,nil,0xad)*200
 	Duel.SetTargetPlayer(1-tp)
 	Duel.SetTargetParam(val)
 	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,val)
