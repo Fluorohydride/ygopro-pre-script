3fcb9fe7652bef06234052f3ca7a190e66c8ec13
dest <dest.dn@gmail.com>
2017-02-22 18:28:43 +0100

    add VJMP-JP 128,129

---
+++ scripts/VJMP-JP/c100200128.lua
@@ -0,0 +1,58 @@
+--EMカード・ガードナー
+--Performapal Card Gardna
+--Script by dest
+function c100200128.initial_effect(c)
+	--pendulum summon
+	aux.EnablePendulumAttribute(c)
+	--other defup
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(100200128,0))
+	e1:SetCategory(CATEGORY_DEFCHANGE)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_PZONE)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetCountLimit(1)
+	e1:SetTarget(c100200128.target)
+	e1:SetOperation(c100200128.operation)
+	c:RegisterEffect(e1)
+	--own defup
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCode(EFFECT_UPDATE_DEFENSE)
+	e2:SetValue(c100200128.defval)
+	c:RegisterEffect(e2)
+end
+function c100200128.filter(c,def)
+	return c:IsPosition(POS_FACEUP_DEFENSE) and c:GetDefense()~=def
+end
+function c100200128.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	local g=Duel.GetMatchingGroup(Card.IsPosition,tp,LOCATION_MZONE,0,nil,POS_FACEUP_DEFENSE)
+	local def=g:GetSum(Card.GetBaseDefense)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and c100200128.filter(chkc,def) end
+	if chk==0 then return Duel.IsExistingTarget(c100200128.filter,tp,LOCATION_MZONE,0,1,nil,def) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	local g=Duel.SelectTarget(tp,c100200128.filter,tp,LOCATION_MZONE,0,1,1,nil,def)
+end
+function c100200128.operation(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tc=Duel.GetFirstTarget()
+	if c:IsRelateToEffect(e) and tc:IsRelateToEffect(e) and tc:IsFaceup() then
+		local g=Duel.GetMatchingGroup(Card.IsPosition,tp,LOCATION_MZONE,0,nil,POS_FACEUP_DEFENSE)
+		local def=g:GetSum(Card.GetBaseDefense)
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_SET_DEFENSE_FINAL)
+		e1:SetReset(RESET_EVENT+0x1fe0000)
+		e1:SetValue(def)
+		tc:RegisterEffect(e1)
+	end
+end
+function c100200128.deffilter(c)
+	return c:GetBaseDefense()>=0 and c:IsSetCard(0x9f) and c:IsPosition(POS_FACEUP_DEFENSE)
+end
+function c100200128.defval(e,c)
+	local g=Duel.GetMatchingGroup(c100200128.deffilter,c:GetControler(),LOCATION_MZONE,0,c)
+	return g:GetSum(Card.GetBaseDefense)
+end
---
+++ scripts/VJMP-JP/c100200129.lua
@@ -0,0 +1,70 @@
+--エッジインプ・コットン・イーター
+--Edge Imp Cotton Eater
+--Script by dest
+function c100200129.initial_effect(c)
+	--pendulum summon
+	aux.EnablePendulumAttribute(c)
+	--atk up
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetRange(LOCATION_PZONE)
+	e1:SetCode(EFFECT_UPDATE_ATTACK)
+	e1:SetTargetRange(LOCATION_MZONE,0)
+	e1:SetTarget(aux.TargetBoolFunction(Card.IsType,TYPE_FUSION))
+	e1:SetValue(300)
+	c:RegisterEffect(e1)
+	--draw
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(100200129,0))
+	e2:SetCategory(CATEGORY_DRAW)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e2:SetRange(LOCATION_PZONE)
+	e2:SetProperty(EFFECT_FLAG_DELAY)
+	e2:SetCountLimit(1)
+	e2:SetCondition(c100200129.drcon)
+	e2:SetTarget(c100200129.drtg)
+	e2:SetOperation(c100200129.drop)
+	c:RegisterEffect(e2)
+	--damage
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(100200129,1))
+	e3:SetCategory(CATEGORY_DAMAGE)
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e3:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_DELAY)
+	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e3:SetCountLimit(1,100200129)
+	e3:SetTarget(c100200129.damtg)
+	e3:SetOperation(c100200129.damop)
+	c:RegisterEffect(e3)
+end
+function c100200129.drfilter(c,tp)
+	return c:IsControler(tp) and c:IsSetCard(0xad)
+		and bit.band(c:GetSummonType(),SUMMON_TYPE_FUSION)==SUMMON_TYPE_FUSION
+end
+function c100200129.drcon(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(c100200129.drfilter,1,nil,tp)
+end
+function c100200129.drtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsPlayerCanDraw(tp,1) end
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
+end
+function c100200129.drop(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetHandler():IsRelateToEffect(e) then return end
+	Duel.Draw(tp,1,REASON_EFFECT)
+end
+function c100200129.damfilter(c)
+	return c:IsControler(tp) and c:IsSetCard(0xad)
+		and bit.band(c:GetSummonType(),SUMMON_TYPE_FUSION)==SUMMON_TYPE_FUSION
+end
+function c100200129.damtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c100200129.damfilter,tp,LOCATION_GRAVE,0,1,nil) end
+	local val=Duel.GetMatchingGroupCount(c100200129.damfilter,tp,LOCATION_MZONE,0,nil)*200
+	Duel.SetTargetPlayer(1-tp)
+	Duel.SetTargetParam(val)
+	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,val)
+end
+function c100200129.damop(e,tp,eg,ep,ev,re,r,rp)
+	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+	Duel.Damage(p,d,REASON_EFFECT)
+end
