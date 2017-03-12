45e0fa39d462826f2ed5840663a7e199605e3baa
EerieCode <eeriecode@gmail.com>
2017-03-12 10:27:49 +0100

    Add COTD-JP038 --覇王眷竜スターヴ・ヴェノム

---
+++ scripts/COTD-JP/c101001038.lua
@@ -0,0 +1,92 @@
+--覇王眷竜スターヴ・ヴェノム
+--Supreme King Servant Dragon Starving Venom
+--Scripted by Eerie Code
+function c101001038.initial_effect(c)
+	--fusion material
+	c:EnableReviveLimit()
+	aux.AddFusionProcFunRep(c,c101001038.ffilter,2,false)
+	--spsummon condition
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e1:SetCode(EFFECT_SPSUMMON_CONDITION)
+	e1:SetValue(c101001038.splimit)
+	c:RegisterEffect(e1)
+	--special summon rule
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_SPSUMMON_PROC)
+	e2:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+	e2:SetRange(LOCATION_EXTRA)
+	e2:SetCondition(c101001038.spcon)
+	e2:SetOperation(c101001038.spop)
+	c:RegisterEffect(e2)
+	--copy effect
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(101001038,0))
+	e3:SetType(EFFECT_TYPE_IGNITION)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e3:SetCountLimit(1)
+	e3:SetCost(c101001038.copycost)
+	e3:SetTarget(c101001038.copytg)
+	e3:SetOperation(c101001038.copyop)
+	c:RegisterEffect(e3)
+end
+function c101001038.ffilter(c)
+	return c:IsFusionAttribute(ATTRIBUTE_DARK) and c:IsFusionType(TYPE_PENDULUM)
+end
+function c101001038.splimit(e,se,sp,st)
+	return bit.band(st,SUMMON_TYPE_FUSION)==SUMMON_TYPE_FUSION
+end
+function c101001038.spfilter(c,fc)
+	return c101001038.ffilter(c) and c:IsCanBeFusionMaterial(fc)
+end
+function c101001038.spcon(e,c)
+	if c==nil then return true end
+	local tp=c:GetControler()
+	return Duel.GetLocationCount(tp,LOCATION_MZONE)>-2
+		and Duel.CheckReleaseGroup(tp,c101001038.spfilter,2,nil,c)
+end
+function c101001038.spop(e,tp,eg,ep,ev,re,r,rp,c)
+	local g=Duel.SelectReleaseGroup(tp,c101001038.spfilter,2,2,nil,c)
+	c:SetMaterial(g)
+	Duel.Release(g,REASON_COST+REASON_FUSION+REASON_MATERIAL)
+end
+function c101001038.copycost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():GetFlagEffect(41209827)==0 end
+	e:GetHandler():RegisterFlagEffect(41209827,RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END,0,1)
+end
+function c101001038.copyfilter(c)
+	return c:IsFaceup() and c:IsType(TYPE_EFFECT) and not c:IsType(TYPE_TOKEN)
+end
+function c101001038.copytg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	local c=e:GetHandler()
+	if chkc then return chkc:IsLocation(LOCATION_MZONE+LOCATION_GRAVE) and c101001038.copyfilter(chkc) and chkc~=c end
+	if chk==0 then return Duel.IsExistingTarget(c101001038.copyfilter,tp,LOCATION_MZONE+LOCATION_GRAVE,LOCATION_MZONE+LOCATION_GRAVE,1,c) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	Duel.SelectTarget(tp,c101001038.copyfilter,tp,LOCATION_MZONE+LOCATION_GRAVE,LOCATION_MZONE+LOCATION_GRAVE,1,1,c)
+end
+function c101001038.copyop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tc=Duel.GetFirstTarget()
+	if tc and c:IsRelateToEffect(e) and c:IsFaceup() and tc:IsRelateToEffect(e) and tc:IsFaceup() and not tc:IsType(TYPE_TOKEN) then
+		local code=tc:GetOriginalCodeRule()
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e1:SetCode(EFFECT_CHANGE_CODE)
+		e1:SetValue(code)
+		e1:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END)
+		c:RegisterEffect(e1)
+		if not tc:IsType(TYPE_TRAPMONSTER) then
+			c:CopyEffect(code,RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END,1)
+		end
+		local e2=Effect.CreateEffect(c)
+		e2:SetType(EFFECT_TYPE_FIELD)
+		e2:SetCode(EFFECT_PIERCE)
+		e2:SetTargetRange(LOCATION_MZONE,0)
+		e2:SetReset(RESET_PHASE+PHASE_END)
+		Duel.RegisterEffect(e2,tp)
+	end
+end
