bf49bd80d18509e1fc716de225eb3dd3c86b67e9
EerieCode <eeriecode@gmail.com>
2017-03-21 20:32:31 +0100

    fix

--- scripts/CP17-JP/c100217013.lua
+++ scripts/CP17-JP/c100217013.lua
@@ -76,7 +76,7 @@ function c100217013.spop(e,tp,eg,ep,ev,re,r,rp)
 	ft=math.min(ft,Duel.GetLocationCount(tp,LOCATION_MZONE))
 	if ft<=0 then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectMatchingCard(tp,c23681456.filter,tp,LOCATION_DECK,0,1,ft,nil,e,tp)
+	local g=Duel.SelectMatchingCard(tp,c100217013.spfilter,tp,LOCATION_DECK,0,1,ft,nil,e,tp)
 	if g:GetCount()>0 then
 		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
 	end
