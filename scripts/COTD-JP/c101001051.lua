814c9a212b5f4c78c0d0fc2388c8d3c0a02e823d
nekrozar <nekrozar.s@gmail.com>
2017-03-25 19:29:39 +0900

    add script ST17, COTD

---
+++ scripts/COTD-JP/c101001051.lua
@@ -0,0 +1,8 @@
+--電影の騎士ガイアセイバー
+--Gaiasaber, the Video Knight
+--Script by nekrozar
+function c101001051.initial_effect(c)
+	--link summon
+	aux.AddLinkProcedure(c,nil,2)
+	c:EnableReviveLimit()
+end
