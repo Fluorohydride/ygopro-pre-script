e4c19027338ff7579b264881d2e5e0d4dc33fc48
nekrozar <nekrozar.s@gmail.com>
2017-03-25 18:57:25 +0900

    change ID

--- scripts/ST17-JP/c61583217.lua
+++ scripts/ST17-JP/c61583217.lua
@@ -1,7 +1,7 @@
 --サイバネット・ユニバース
 --Cybenet Universe
 --Script by nekrozar
-function c100317021.initial_effect(c)
+function c61583217.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
@@ -18,37 +18,37 @@ function c100317021.initial_effect(c)
 	c:RegisterEffect(e2)
 	--todeck
 	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(100317021,0))
+	e3:SetDescription(aux.Stringid(61583217,0))
 	e3:SetCategory(CATEGORY_TODECK)
 	e3:SetType(EFFECT_TYPE_IGNITION)
 	e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e3:SetRange(LOCATION_FZONE)
 	e3:SetCountLimit(1)
-	e3:SetTarget(c100317021.tdtg)
-	e3:SetOperation(c100317021.tdop)
+	e3:SetTarget(c61583217.tdtg)
+	e3:SetOperation(c61583217.tdop)
 	c:RegisterEffect(e3)
 	--tograve
 	local e4=Effect.CreateEffect(c)
-	e4:SetDescription(aux.Stringid(100317021,1))
+	e4:SetDescription(aux.Stringid(61583217,1))
 	e4:SetCategory(CATEGORY_TOGRAVE)
 	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
 	e4:SetCode(EVENT_DESTROYED)
-	e4:SetCondition(c100317021.tgcon)
-	e4:SetTarget(c100317021.tgtg)
-	e4:SetOperation(c100317021.tgop)
+	e4:SetCondition(c61583217.tgcon)
+	e4:SetTarget(c61583217.tgtg)
+	e4:SetOperation(c61583217.tgop)
 	c:RegisterEffect(e4)
 end
-function c100317021.tdfilter(c)
+function c61583217.tdfilter(c)
 	return c:IsType(TYPE_MONSTER) and c:IsAbleToDeck()
 end
-function c100317021.tdtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and c100317021.tdfilter(chkc) end
-	if chk==0 then return Duel.IsExistingTarget(c100317021.tdfilter,tp,LOCATION_GRAVE,LOCATION_GRAVE,1,nil) end
+function c61583217.tdtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and c61583217.tdfilter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(c61583217.tdfilter,tp,LOCATION_GRAVE,LOCATION_GRAVE,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
-	local g=Duel.SelectTarget(tp,c100317021.tdfilter,tp,LOCATION_GRAVE,LOCATION_GRAVE,1,1,nil)
+	local g=Duel.SelectTarget(tp,c61583217.tdfilter,tp,LOCATION_GRAVE,LOCATION_GRAVE,1,1,nil)
 	Duel.SetOperationInfo(0,CATEGORY_TODECK,g,1,0,0)
 end
-function c100317021.tdop(e,tp,eg,ep,ev,re,r,rp)
+function c61583217.tdop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if not c:IsRelateToEffect(e) then return end
 	local tc=Duel.GetFirstTarget()
@@ -56,19 +56,19 @@ function c100317021.tdop(e,tp,eg,ep,ev,re,r,rp)
 		Duel.SendtoDeck(tc,nil,2,REASON_EFFECT)
 	end
 end
-function c100317021.tgcon(e,tp,eg,ep,ev,re,r,rp)
+function c61583217.tgcon(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	return c:IsReason(REASON_EFFECT) and c:IsPreviousLocation(LOCATION_ONFIELD)
 end
-function c100317021.tgfilter(c)
+function c61583217.tgfilter(c)
 	return c:GetSequence()>=5
 end
-function c100317021.tgtg(e,tp,eg,ep,ev,re,r,rp,chk)
+function c61583217.tgtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
-	local g=Duel.GetMatchingGroup(c100317021.tgfilter,tp,LOCATION_MZONE,LOCATION_MZONE,nil)
+	local g=Duel.GetMatchingGroup(c61583217.tgfilter,tp,LOCATION_MZONE,LOCATION_MZONE,nil)
 	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,g,g:GetCount(),0,0)
 end
-function c100317021.tgop(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.GetMatchingGroup(c100317021.tgfilter,tp,LOCATION_MZONE,LOCATION_MZONE,nil)
+function c61583217.tgop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(c61583217.tgfilter,tp,LOCATION_MZONE,LOCATION_MZONE,nil)
 	Duel.SendtoGrave(g,REASON_EFFECT)
 end
