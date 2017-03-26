59e75e562756308b4b4a391e099f82787b1a2a68
dest <dest.dn@gmail.com>
2017-03-26 04:43:01 +0200

    fix

--- scripts/CP17-JP/c100217008.lua
+++ scripts/CP17-JP/c100217008.lua
@@ -99,7 +99,7 @@ function c100217008.xyzop(e,tp,eg,ep,ev,re,r,rp)
 end
 function c100217008.descost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return e:GetHandler():CheckRemoveOverlayCard(tp,1,REASON_COST) end
-	e:GetHandler():RemoveOverlayCost(tp,1,1,REASON_COST)
+	e:GetHandler():RemoveOverlayCard(tp,1,1,REASON_COST)
 end
 function c100217008.descfilter(c)
 	return c:IsFaceup() and c:IsType(TYPE_PENDULUM)
