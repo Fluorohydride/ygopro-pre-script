95e27e1a764e03f7c89816c6ab2b7f6323600bad
mercury233 <me@mercury233.me>
2017-04-03 00:42:52 +0800

    fix Salvagent Driver

--- scripts/COTD-JP/c101001005.lua
+++ scripts/COTD-JP/c101001005.lua
@@ -67,7 +67,7 @@ end
 function c101001005.spop(e,tp,eg,ep,ev,re,r,rp,chk)
 	local tc=Duel.GetFirstTarget()
 	if tc:IsRelateToEffect(e) and Duel.SpecialSummonStep(tc,0,tp,tp,false,false,POS_FACEUP) then
-			local e1=Effect.CreateEffect(c)
+			local e1=Effect.CreateEffect(e:GetHandler())
 			e1:SetType(EFFECT_TYPE_SINGLE)
 			e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
 			e1:SetCode(EFFECT_CANNOT_DIRECT_ATTACK)
