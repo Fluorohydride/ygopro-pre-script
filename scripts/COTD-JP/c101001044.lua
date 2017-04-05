bffd8aa17ab2e7512e8cbdc9ba11089fd7688a67
mercury233 <me@mercury233.me>
2017-04-06 00:45:36 +0800

    fix Trickster Holy Angel

--- scripts/COTD-JP/c101001044.lua
+++ scripts/COTD-JP/c101001044.lua
@@ -62,7 +62,7 @@ function c101001044.atkop(e,tp,eg,ep,ev,re,r,rp)
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_UPDATE_ATTACK)
 		e1:SetValue(ev)
-		e1:SetReset(RESET_EVENT+0x1ff0000)
+		e1:SetReset(RESET_EVENT+0x1ff0000+RESET_PHASE+PHASE_END)
 		c:RegisterEffect(e1)
 	end
 end
