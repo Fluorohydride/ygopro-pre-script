d6da06a8cf0d60b4add904b258f469a6a972d891
EerieCode <eeriecode@gmail.com>
2017-04-06 06:23:56 +0200

    fix setcodes - Stargrail Aurum

--- scripts/COTD-JP/c101001049.lua
+++ scripts/COTD-JP/c101001049.lua
@@ -3,7 +3,7 @@
 --Script by mercury233
 function c101001049.initial_effect(c)
 	--link summon
-	aux.AddLinkProcedure(c,aux.FilterBoolFunction(Card.IsSetCard,0x1fd),2,2)
+	aux.AddLinkProcedure(c,aux.FilterBoolFunction(Card.IsSetCard,0x11fd),2,2)
 	c:EnableReviveLimit()
 	--atkup
 	local e1=Effect.CreateEffect(c)
@@ -38,10 +38,10 @@ function c101001049.initial_effect(c)
 	c:RegisterEffect(e3)
 end
 function c101001049.atkval(e,c)
-	return Duel.GetMatchingGroupCount(Card.IsSetCard,c:GetControler(),LOCATION_GRAVE,0,nil,0x11fd)*5000
+	return Duel.GetMatchingGroupCount(Card.IsSetCard,c:GetControler(),LOCATION_GRAVE,0,nil,0x21fd)*5000
 end
 function c101001049.cfilter(c,g)
-	return c:IsSetCard(0x1fd) and g:IsContains(c)
+	return c:IsSetCard(0x11fd) and g:IsContains(c)
 end
 function c101001049.spcost1(e,tp,eg,ep,ev,re,r,rp,chk)
 	local lg=e:GetHandler():GetLinkedGroup()
@@ -75,7 +75,7 @@ function c101001049.spcon2(e,tp,eg,ep,ev,re,r,rp)
 	return e:GetHandler():IsPreviousLocation(LOCATION_ONFIELD)
 end
 function c101001049.spfilter2(c,e,tp)
-	return c:IsSetCard(0x1fd) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+	return c:IsSetCard(0x11fd) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function c101001049.sptg2(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
