e05bf0f91a78036c994c6f91e567c22c2d0aa0a7
EerieCode <eeriecode@gmail.com>
2017-04-14 13:51:54 +0200

    fix

--- scripts/COTD-JP/c101001070.lua
+++ scripts/COTD-JP/c101001070.lua
@@ -41,21 +41,45 @@ end
 function c101001070.activate(e,tp,eg,ep,ev,re,r,rp)
 	local dg=Duel.GetMatchingGroup(c101001070.desfilter,tp,LOCATION_MZONE,0,nil)
 	if Duel.Destroy(dg,REASON_EFFECT)==0 then return end
-	local sg=Duel.GetMatchingGroup(aux.NecrovalleyFilter(c101001070.spfilter),tp,LOCATION_HAND+LOCATION_DECK+LOCATION_GRAVE+LOCATION_EXTRA,0,nil,e,tp)
+	local sg=Duel.GetMatchingGroup(aux.NecroValleyFilter(c101001070.spfilter),tp,LOCATION_HAND+LOCATION_DECK+LOCATION_GRAVE+LOCATION_EXTRA,0,nil,e,tp)
 	local lc=Duel.GetLocationCount(tp,LOCATION_MZONE)
 	local ct=4
 	if Duel.IsPlayerAffectedByEffect(tp,59822133) then ct=1 end
 	if sg:GetCount()==0 or lc==0 then return end
+	local exlc=lc
+	if Duel.IsPlayerAffectedByEffect(tp,29724053) then
+		exlc=math.min(exlc,c29724053[tp])
+	end
+	if exlc<=0 then
+		sg:Remove(Card.IsLocation,nil,LOCATION_EXTRA)
+	end
 	local sel=Group.CreateGroup()
+	local exsel=Group.CreateGroup()
 	repeat
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-		local s1=sg:Select(tp,1,1,nil)
-		sel:Merge(s1)
-		sg:Remove(Card.IsCode,nil,s1:GetFirst():GetCode())
+		local s1=sg:Select(tp,1,1,nil):GetFirst()
+		sg:Remove(Card.IsCode,nil,s1:GetCode())
+		if s1:IsLocation(LOCATION_EXTRA) then
+			exsel:AddCard(s1)
+			exlc=exlc-1
+			if exlc==0 then
+				sg:Remove(Card.IsLocation,nil,LOCATION_EXTRA)
+			end
+		else sel:AddCard(s1) end
 		ct=ct-1
-		lc=lc-1
+		lc=lc-1  
 	until ct==0 or sg:GetCount()==0 or lc==0 or not Duel.SelectYesNo(tp,aux.Stringid(101001070,0))
-	Duel.SpecialSummon(sel,0,tp,tp,true,false,POS_FACEUP)
+	local tc=exsel:GetFirst()
+	while tc do
+		Duel.SpecialSummonStep(tc,0,tp,tp,true,false,POS_FACEUP)
+		tc=exsel:GetNext()
+	end
+	tc=sel:GetFirst()
+	while tc do
+		Duel.SpecialSummonStep(tc,0,tp,tp,true,false,POS_FACEUP)
+		tc=sel:GetNext()
+	end
+	Duel.SpecialSummonComplete()
 end
 function c101001070.matcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return e:GetHandler():IsAbleToRemoveAsCost() end
@@ -70,7 +94,7 @@ end
 function c101001070.mattg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_MZONE) and c101001070.xyzfilter(chkc) end
 	if chk==0 then return Duel.IsExistingTarget(c101001070.xyzfilter,tp,LOCATION_MZONE,0,1,nil)
-		and Duel.IsExistingMatchingCard(c101001070.matfilter,tp,LOCATION_GRAVE+LOCATION_EXTRA,0,1,nil) end
+		and Duel.IsExistingMatchingCard(c101001070.matfilter,tp,LOCATION_GRAVE+LOCATION_EXTRA,0,2,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
 	Duel.SelectTarget(tp,c101001070.xyzfilter,tp,LOCATION_MZONE,0,1,1,nil)
 end
@@ -78,7 +102,7 @@ function c101001070.matop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
 	if tc:IsFaceup() and tc:IsRelateToEffect(e) and not tc:IsImmuneToEffect(e) then
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
-		local g=Duel.SelectMatchingCard(tp,c101001070.matfilter,tp,LOCATION_GRAVE+LOCATION_EXTRA,0,1,1,nil)
+		local g=Duel.SelectMatchingCard(tp,c101001070.matfilter,tp,LOCATION_GRAVE+LOCATION_EXTRA,0,2,2,nil)
 		if g:GetCount()>0 then
 			Duel.Overlay(tc,g)
 		end
