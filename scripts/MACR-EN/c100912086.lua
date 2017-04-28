0f128a52b417e9b813933d826da3b512e74ee5f1
mercury233 <me@mercury233.me>
2017-04-28 16:39:51 +0800

    fix

--- scripts/MACR-EN/c100912086.lua
+++ scripts/MACR-EN/c100912086.lua
@@ -2,6 +2,12 @@
 --Scripted by Eerie Code
 function c100912086.initial_effect(c)
 	c:EnableReviveLimit()
+	--splimit
+	local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_SINGLE)
+	e0:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e0:SetCode(EFFECT_SPSUMMON_CONDITION)
+	c:RegisterEffect(e0)
 	--special summon
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
