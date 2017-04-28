3a0103f0a740c451d5fb65c727be8822e35102e7
nekrozar <nekrozar.s@gmail.com>
2017-04-28 19:56:13 +0900

    change ID

--- scripts/MG05-JP/c84569886.lua
+++ scripts/MG05-JP/c84569886.lua
@@ -1,41 +1,41 @@
 --DDD超死偉王パープリッシュ・ヘル・アーマゲドン
 --D/D/D Superdoom King Purplish Armageddon
 --Scripted by Eerie Code
-function c100219002.initial_effect(c)
+function c84569886.initial_effect(c)
 	c:EnableReviveLimit()
 	aux.EnablePendulumAttribute(c,false)
 	aux.AddFusionProcFunRep(c,aux.FilterBoolFunction(Card.IsFusionSetCard,0x10af),2,true)
 	--atk
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(100219002,0))
+	e1:SetDescription(aux.Stringid(84569886,0))
 	e1:SetCategory(CATEGORY_ATKCHANGE)
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
 	e1:SetCode(EVENT_BATTLE_CONFIRM)
 	e1:SetRange(LOCATION_PZONE)
 	e1:SetCountLimit(1)
-	e1:SetCondition(c100219002.atkcon1)
-	e1:SetOperation(c100219002.atkop1)
+	e1:SetCondition(c84569886.atkcon1)
+	e1:SetOperation(c84569886.atkop1)
 	c:RegisterEffect(e1)
 	--destroy
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(100219002,1))
+	e2:SetDescription(aux.Stringid(84569886,1))
 	e2:SetCategory(CATEGORY_DESTROY+CATEGORY_DAMAGE)
 	e2:SetType(EFFECT_TYPE_IGNITION)
 	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
-	e2:SetCountLimit(1,100219002)
+	e2:SetCountLimit(1,84569886)
 	e2:SetRange(LOCATION_MZONE)
-	e2:SetTarget(c100219002.destg)
-	e2:SetOperation(c100219002.desop)
+	e2:SetTarget(c84569886.destg)
+	e2:SetOperation(c84569886.desop)
 	c:RegisterEffect(e2)
 	--reset atk
 	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(100219002,2))
+	e3:SetDescription(aux.Stringid(84569886,2))
 	e3:SetCategory(CATEGORY_ATKCHANGE)
 	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
 	e3:SetCode(EVENT_BATTLE_CONFIRM)
 	e3:SetRange(LOCATION_MZONE)
-	e3:SetCondition(c100219002.atkcon2)
-	e3:SetOperation(c100219002.atkop2)
+	e3:SetCondition(c84569886.atkcon2)
+	e3:SetOperation(c84569886.atkop2)
 	c:RegisterEffect(e3)
 	--pendulum
 	local e6=Effect.CreateEffect(c)
@@ -43,18 +43,18 @@ function c100219002.initial_effect(c)
 	e6:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e6:SetCode(EVENT_DESTROYED)
 	e6:SetProperty(EFFECT_FLAG_DELAY)
-	e6:SetCondition(c100219002.pencon)
-	e6:SetTarget(c100219002.pentg)
-	e6:SetOperation(c100219002.penop)
+	e6:SetCondition(c84569886.pencon)
+	e6:SetTarget(c84569886.pentg)
+	e6:SetOperation(c84569886.penop)
 	c:RegisterEffect(e6)
 end
-function c100219002.atkcon1(e,tp,eg,ep,ev,re,r,rp)
+function c84569886.atkcon1(e,tp,eg,ep,ev,re,r,rp)
 	local a=Duel.GetAttacker()
 	local d=a:GetBattleTarget()
 	return a:IsFaceup() and a:IsRelateToBattle() and a:IsSetCard(0x10af) and a:IsType(TYPE_FUSION)
 		and d and d:IsFaceup() and d:IsRelateToBattle() and d:GetAttack()>0 and a:GetControler()~=d:GetControler()
 end
-function c100219002.atkop1(e,tp,ep,ev,re,r,rp)
+function c84569886.atkop1(e,tp,ep,ev,re,r,rp)
 	local a=Duel.GetAttacker()
 	local d=a:GetBattleTarget()
 	if e:GetHandler():IsRelateToEffect(e)
@@ -67,7 +67,7 @@ function c100219002.atkop1(e,tp,ep,ev,re,r,rp)
 		a:RegisterEffect(e1)
 	end
 end
-function c100219002.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+function c84569886.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) and chkc:IsPosition(POS_FACEUP_ATTACK) end
 	if chk==0 then return Duel.IsExistingTarget(Card.IsPosition,tp,0,LOCATION_MZONE,1,nil,POS_FACEUP_ATTACK) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
@@ -77,7 +77,7 @@ function c100219002.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
 	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,atk)
 end
-function c100219002.desop(e,tp,eg,ep,ev,re,r,rp)
+function c84569886.desop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
 	if tc:IsRelateToEffect(e) then
 		local atk=math.floor(tc:GetBaseAttack()/2)
@@ -87,13 +87,13 @@ function c100219002.desop(e,tp,eg,ep,ev,re,r,rp)
 		end
 	end
 end
-function c100219002.atkcon2(e,tp,eg,ep,ev,re,r,rp)
+function c84569886.atkcon2(e,tp,eg,ep,ev,re,r,rp)
 	local a=Duel.GetAttacker()
 	local d=a:GetBattleTarget()
 	if a:IsControler(tp) then a,d=d,a end
 	return a and a:IsControler(1-tp) and a:GetAttack()~=a:GetBaseAttack()
 end
-function c100219002.atkop2(e,tp,eg,ep,ev,re,r,rp)
+function c84569886.atkop2(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetAttacker()
 	if tc:IsControler(tp) then tc=tc:GetBattleTarget() end
 	if tc and tc:IsFaceup() and tc:GetAttack()~=tc:GetBaseAttack() then
@@ -105,14 +105,14 @@ function c100219002.atkop2(e,tp,eg,ep,ev,re,r,rp)
 		tc:RegisterEffect(e1)
 	end
 end
-function c100219002.pencon(e,tp,eg,ep,ev,re,r,rp)
+function c84569886.pencon(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	return c:IsPreviousLocation(LOCATION_MZONE) and c:IsFaceup()
 end
-function c100219002.pentg(e,tp,eg,ep,ev,re,r,rp,chk)
+function c84569886.pentg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.CheckLocation(tp,LOCATION_PZONE,0) or Duel.CheckLocation(tp,LOCATION_PZONE,1) end
 end
-function c100219002.penop(e,tp,eg,ep,ev,re,r,rp)
+function c84569886.penop(e,tp,eg,ep,ev,re,r,rp)
 	if not Duel.CheckLocation(tp,LOCATION_PZONE,0) and not Duel.CheckLocation(tp,LOCATION_PZONE,1) then return false end
 	local c=e:GetHandler()
 	if c:IsRelateToEffect(e) then
