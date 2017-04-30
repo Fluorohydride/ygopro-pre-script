ad253d91bb663c564b66a59ec806a414dae0a82a
mercury233 <me@mercury233.me>
2017-04-30 18:06:01 +0800

    change id

--- scripts/MACR-EN/c74640994.lua
+++ scripts/MACR-EN/c74640994.lua
@@ -1,50 +1,50 @@
 --Subterror Final Battle
 --Scripted by Eerie Code
-function c100912085.initial_effect(c)
+function c74640994.initial_effect(c)
 	--activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
-	e1:SetTarget(c100912085.target)
-	e1:SetOperation(c100912085.activate)
+	e1:SetTarget(c74640994.target)
+	e1:SetOperation(c74640994.activate)
 	c:RegisterEffect(e1)
 end
-function c100912085.filter(c)
+function c74640994.filter(c)
 	return c:IsFaceup() and c:IsSetCard(0xed)
 end
-function c100912085.fufilter(c)
+function c74640994.fufilter(c)
 	return c:IsFacedown() and c:IsSetCard(0xed)
 end
-function c100912085.fdfilter(c)
-	return c100912085.filter(c) and c:IsCanTurnSet()
+function c74640994.fdfilter(c)
+	return c74640994.filter(c) and c:IsCanTurnSet()
 end
-function c100912085.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	local b1=Duel.IsExistingMatchingCard(c100912085.fufilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil)
-	local b2=Duel.IsExistingMatchingCard(c100912085.fdfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil)
-	local b3=Duel.IsExistingMatchingCard(c100912085.filter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil)
-	local b4=Duel.GetFlagEffect(tp,100912085)==0
+function c74640994.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	local b1=Duel.IsExistingMatchingCard(c74640994.fufilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil)
+	local b2=Duel.IsExistingMatchingCard(c74640994.fdfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil)
+	local b3=Duel.IsExistingMatchingCard(c74640994.filter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil)
+	local b4=Duel.GetFlagEffect(tp,74640994)==0
 	if chk==0 then return b1 or b2 or b3 or b4 end
 	local off=1
 	local ops={}
 	local opval={}
 	if b1 then
-		ops[off]=aux.Stringid(100912085,0)
+		ops[off]=aux.Stringid(74640994,0)
 		opval[off-1]=1
 		off=off+1
 	end
 	if b2 then
-		ops[off]=aux.Stringid(100912085,1)
+		ops[off]=aux.Stringid(74640994,1)
 		opval[off-1]=2
 		off=off+1
 	end
 	if b3 then
-		ops[off]=aux.Stringid(100912085,2)
+		ops[off]=aux.Stringid(74640994,2)
 		opval[off-1]=3
 		off=off+1
 	end
 	if b4 then
-		ops[off]=aux.Stringid(100912085,3)
+		ops[off]=aux.Stringid(74640994,3)
 		opval[off-1]=4
 		off=off+1
 	end
@@ -58,12 +58,12 @@ function c100912085.target(e,tp,eg,ep,ev,re,r,rp,chk)
 		e:SetCategory(CATEGORY_ATKCHANGE+CATEGORY_DEFCHANGE)
 	end
 end
-function c100912085.activate(e,tp,eg,ep,ev,re,r,rp)
+function c74640994.activate(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local sel=e:GetLabel()
 	if sel==1 then
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_POSCHANGE)
-		local g=Duel.SelectMatchingCard(tp,c100912085.fufilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
+		local g=Duel.SelectMatchingCard(tp,c74640994.fufilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
 		if g:GetCount()>0 then
 			Duel.HintSelection(g)
 			local tc=g:GetFirst()
@@ -72,14 +72,14 @@ function c100912085.activate(e,tp,eg,ep,ev,re,r,rp)
 		end
 	elseif sel==2 then
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_POSCHANGE)
-		local g=Duel.SelectMatchingCard(tp,c100912085.fdfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
+		local g=Duel.SelectMatchingCard(tp,c74640994.fdfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
 		if g:GetCount()>0 then
 			Duel.HintSelection(g)
 			Duel.ChangePosition(g,POS_FACEDOWN_DEFENSE)
 		end
 	elseif sel==3 then
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
-		local g=Duel.SelectMatchingCard(tp,c100912085.filter,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
+		local g=Duel.SelectMatchingCard(tp,c74640994.filter,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
 		if g:GetCount()>0 then
 			Duel.HintSelection(g)
 			local tc=g:GetFirst()
@@ -98,10 +98,10 @@ function c100912085.activate(e,tp,eg,ep,ev,re,r,rp)
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_FIELD)
 		e1:SetCode(EFFECT_CANNOT_DISEFFECT)
-		e1:SetValue(c100912085.effectfilter)
+		e1:SetValue(c74640994.effectfilter)
 		e1:SetReset(RESET_PHASE+PHASE_END)
 		Duel.RegisterEffect(e1,tp)
-		Duel.RegisterFlagEffect(tp,100912085,RESET_PHASE+PHASE_END,0,1)
+		Duel.RegisterFlagEffect(tp,74640994,RESET_PHASE+PHASE_END,0,1)
 	end
 	if c:IsRelateToEffect(e) and c:IsCanTurnSet() and e:IsHasType(EFFECT_TYPE_ACTIVATE) then
 		Duel.BreakEffect()
@@ -110,7 +110,7 @@ function c100912085.activate(e,tp,eg,ep,ev,re,r,rp)
 		Duel.RaiseEvent(c,EVENT_SSET,e,REASON_EFFECT,tp,tp,0)
 	end
 end
-function c100912085.effectfilter(e,ct)
+function c74640994.effectfilter(e,ct)
 	local p=e:GetHandlerPlayer()
 	local te,tp,loc=Duel.GetChainInfo(ct,CHAININFO_TRIGGERING_EFFECT,CHAININFO_TRIGGERING_PLAYER,CHAININFO_TRIGGERING_LOCATION)
 	local tc=te:GetHandler()
