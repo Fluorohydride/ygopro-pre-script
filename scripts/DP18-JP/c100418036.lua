0b59971ff245d3cf1838229780b4659e6110e963
EerieCode <eeriecode@gmail.com>
2017-05-10 18:15:48 +0200

    Add DP18-JP036 --ウォーター・ドラゴン－クラスター

---
+++ scripts/DP18-JP/c100418036.lua
@@ -0,0 +1,81 @@
+--ウォーター・ドラゴン－クラスター
+--Water Dragon Cluster
+--Scripted by Eerie Code
+function c100418036.initial_effect(c)
+	c:EnableReviveLimit()
+	--cannot special summon
+	local e1=Effect.CreateEffect(c)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_SPSUMMON_CONDITION)
+	c:RegisterEffect(e1)
+	--atk change
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(100418036,0))
+	e2:SetCategory(CATEGORY_ATKCHANGE)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e2:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_DAMAGE_STEP)
+	e2:SetTarget(c100418036.atktg)
+	e2:SetOperation(c100418036.atkop)
+	c:RegisterEffect(e2)
+	--spsummon
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(100418036,1))
+	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e3:SetType(EFFECT_TYPE_QUICK_O)
+	e3:SetCode(EVENT_FREE_CHAIN)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCost(c100418036.spcost)
+	e3:SetTarget(c100418036.sptg)
+	e3:SetOperation(c100418036.spop)
+	c:RegisterEffect(e3)
+end
+function c100418036.atkfilter(c)
+	return aux.nzatk(c) and c:IsType(TYPE_EFFECT)
+end
+function c100418036.atktg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c100418036.atkfilter,tp,0,LOCATION_MZONE,1,nil) end
+end
+function c100418036.atkop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(c100418036.atkfilter,tp,0,LOCATION_MZONE,nil)
+	local tc=g:GetFirst()
+	while tc do
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_SET_ATTACK_FINAL)
+		e1:SetValue(0)
+		e1:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END)
+		tc:RegisterEffect(e1)
+		local e2=Effect.CreateEffect(e:GetHandler())
+		e2:SetType(EFFECT_TYPE_SINGLE)
+		e2:SetCode(EFFECT_CANNOT_ACTIVATE)
+		e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+		e2:SetRange(LOCATION_MZONE)
+		e2:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END)
+		tc:RegisterEffect(e2)
+		tc=g:GetNext()
+	end
+end
+function c100418036.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsReleasable() end
+	Duel.Release(e:GetHandler(),REASON_COST)
+end
+function c100418036.spfilter(c,e,tp)
+	return c:IsCode(85066822) and c:IsCanBeSpecialSummoned(e,0,tp,true,false,POS_FACEUP_DEFENSE)
+end
+function c100418036.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>1
+		and Duel.IsExistingMatchingCard(c100418036.spfilter,tp,LOCATION_HAND+LOCATION_DECK,0,2,nil,e,tp)
+		and not Duel.IsPlayerAffectedByEffect(tp,59822133) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,2,tp,LOCATION_HAND+LOCATION_DECK)
+end
+function c100418036.spop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<2 then return end
+	if Duel.IsPlayerAffectedByEffect(tp,59822133) then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,c100418036.spfilter,tp,LOCATION_HAND+LOCATION_DECK,0,2,2,nil,e,tp)
+	if g:GetCount()==2 then
+		Duel.SpecialSummon(g,0,tp,tp,true,false,POS_FACEUP_DEFENSE)
+	end
+end
