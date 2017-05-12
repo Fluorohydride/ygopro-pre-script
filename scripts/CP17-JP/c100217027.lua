e8cb5fe89db844fcf608330f273b33612457190b
EerieCode <eeriecode@gmail.com>
2017-05-12 14:08:52 +0200

    Add CP17-JP027 --剣闘獣総監エーディトル

---
+++ scripts/CP17-JP/c100217027.lua
@@ -0,0 +1,123 @@
+--剣闘獣総監エーディトル
+--Gladiator Beast Tamer Editor
+--Scripted by Eerie Code
+function c100217027.initial_effect(c)
+	c:EnableReviveLimit()
+	aux.AddFusionProcFunRep(c,c100217027.matfilter,2,true)
+	--spsummon condition
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e1:SetCode(EFFECT_SPSUMMON_CONDITION)
+	e1:SetValue(c100217027.splimit)
+	c:RegisterEffect(e1)
+	--special summon rule
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(100217027,1))
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_SPSUMMON_PROC)
+	e2:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+	e2:SetRange(LOCATION_EXTRA)
+	e2:SetCondition(c100217027.sprcon)
+	e2:SetOperation(c100217027.sprop)
+	c:RegisterEffect(e2)
+	--cannot be fusion material
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_SINGLE)
+	e3:SetCode(EFFECT_CANNOT_BE_FUSION_MATERIAL)
+	e3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e3:SetValue(1)
+	c:RegisterEffect(e3)
+	--special summon (extra)
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(100217027,3))
+	e4:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e4:SetType(EFFECT_TYPE_IGNITION)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetCountLimit(1)
+	e4:SetTarget(c100217027.esptg)
+	e4:SetOperation(c100217027.espop)
+	c:RegisterEffect(e4)
+	--special summon
+	local e6=Effect.CreateEffect(c)
+	e6:SetDescription(aux.Stringid(100217026,4))
+	e6:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e6:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e6:SetCode(EVENT_PHASE+PHASE_BATTLE)
+	e6:SetRange(LOCATION_MZONE)
+	e6:SetCost(c100217027.spcost)
+	e6:SetTarget(c100217027.sptg)
+	e6:SetOperation(c100217027.spop)
+	c:RegisterEffect(e6)
+end
+function c100217027.matfilter(c)
+	return c:IsLevelAbove(5) and c:IsFusionSetCard(0x19)
+end
+function c100217027.splimit(e,se,sp,st)
+	return e:GetHandler():GetLocation()~=LOCATION_EXTRA
+end
+function c100217027.fspfilter(c)
+	return c100217027.matfilter(c) and c:IsAbleToDeckOrExtraAsCost() and c:IsCanBeFusionMaterial()
+end
+function c100217027.sprcon(e,c)
+	if c==nil then return true end 
+	local tp=c:GetControler()
+	return Duel.GetLocationCount(tp,LOCATION_MZONE)>-2
+		and Duel.IsExistingMatchingCard(c100217027.fspfilter,tp,LOCATION_MZONE,0,2,nil,tp)
+end
+function c100217027.sprop(e,tp,eg,ep,ev,re,r,rp,c)
+	Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(100217027,2))
+	local g=Duel.SelectMatchingCard(tp,c100217027.fspfilter,tp,LOCATION_MZONE,0,2,2,nil)
+	local tc=g:GetFirst()
+	while tc do
+		if not tc:IsFaceup() then Duel.ConfirmCards(1-tp,tc) end
+		tc=g:GetNext()
+	end
+	Duel.SendtoDeck(g,nil,2,REASON_COST)
+end
+function c100217027.espfilter(c,e,tp)
+	return c:IsSetCard(0x19) and c:IsType(TYPE_FUSION) and not c:IsCode(100217027)
+		and c:IsCanBeSpecialSummoned(e,122,tp,true,false)
+end
+function c100217027.esptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCountFromEx(tp)>0
+		and Duel.IsExistingMatchingCard(c100217027.espfilter,tp,LOCATION_EXTRA,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
+end
+function c100217027.espop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCountFromEx(tp)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,c100217027.espfilter,tp,LOCATION_EXTRA,0,1,1,nil,e,tp)
+	if g:GetCount()>0 then
+		Duel.SpecialSummon(g,122,tp,tp,true,false,POS_FACEUP)
+	end
+end
+function c100217027.spcfilter(c)
+	return c:IsFaceup() and c:IsSetCard(0x19) and c:GetBattledGroupCount()>0
+		and c:IsAbleToDeckOrExtraAsCost()
+end
+function c100217027.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c100217027.spcfilter,tp,LOCATION_MZONE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
+	local g=Duel.SelectMatchingCard(tp,c100217027.spcfilter,tp,LOCATION_MZONE,0,1,1,nil)
+	Duel.ConfirmCards(1-tp,g)
+	Duel.SendtoDeck(g,nil,2,REASON_COST)
+end
+function c100217027.spfilter(c,e,tp)
+	return c:IsSetCard(0x19) and c:IsCanBeSpecialSummoned(e,122,tp,false,false)
+end
+function c100217027.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>-1
+		and Duel.IsExistingMatchingCard(c100217027.spfilter,tp,LOCATION_DECK,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
+end
+function c100217027.spop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<1 then return end
+	local g=Duel.SelectMatchingCard(tp,c100217027.spfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
+	local tc=g:GetFirst()
+	if tc then
+		Duel.SpecialSummonStep(tc,122,tp,tp,false,false,POS_FACEUP)
+		tc:RegisterFlagEffect(tc:GetOriginalCode(),RESET_EVENT+0x1ff0000,0,0)
+		Duel.SpecialSummonComplete()
+	end
+end
