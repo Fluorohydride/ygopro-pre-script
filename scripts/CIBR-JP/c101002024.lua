dc961c141a2f9687a26ba82b4c2823f4699b37d6
EerieCode <eeriecode@gmail.com>
2017-05-17 14:33:45 +0200

    Ass CIBR-JP024 --メタファイズ・ダイダロス

---
+++ scripts/CIBR-JP/c101002024.lua
@@ -0,0 +1,69 @@
+--メタファイズ・ダイダロス
+--Metaphys Daedalus
+--Scripted by Eerie Code
+function c101002024.initial_effect(c)
+	--banish
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(101002024,0))
+	e1:SetCategory(CATEGORY_REMOVE)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetProperty(EFFECT_FLAG_DELAY)
+	e1:SetCondition(c101002024.rmcon)
+	e1:SetTarget(c101002024.rmtg)
+	e1:SetOperation(c101002024.rmop)
+	c:RegisterEffect(e1)
+	--banish from deck
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(101002024,1))
+	e2:SetCategory(CATEGORY_REMOVE)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_PHASE+PHASE_STANDBY)
+	e2:SetCountLimit(1)
+	e2:SetRange(LOCATION_REMOVED)
+	e2:SetCondition(c101002024.rmcon2)
+	e2:SetCost(c101002024.rmcost2)
+	e2:SetTarget(c101002024.rmtg2)
+	e2:SetOperation(c101002024.rmop2)
+	c:RegisterEffect(e2)
+end
+function c101002024.rmcon(e,tp,eg,ep,ev,re,r,rp)
+	if not re then return false end
+	local rc=re:GetHandler()
+	return re and re:IsActiveType(TYPE_MONSTER) and (c:IsCode(89189982,36898537) or c:IsSetCard(0x202))
+end
+function c101002024.rmfilter(c)
+	return bit.band(c:GetSummonType(),SUMMON_TYPE_SPECIAL)==SUMMON_TYPE_SPECIAL and c:IsAbleToRemove()
+end
+function c101002024.rmtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local g=Duel.GetMatchingGroup(c101002024.rmfilter,tp,LOCATION_MZONE,LOCATION_MZONE,e:GetHandler())
+	if chk==0 then return g:GetCount()>0 end
+	Duel.SetOperationInfo(0,CATEGORY_REMOVE,g,g:GetCount(),0,0)
+end
+function c101002024.rmop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(c101002024.rmfilter,tp,LOCATION_MZONE,LOCATION_MZONE,e:GetHandler())
+	if g:GetCount()>0 then
+		Duel.Remove(g,POS_FACEUP,REASON_EFFECT)
+	end
+end
+function c101002024.rmcon2(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetTurnCount()==e:GetHandler():GetTurnID()+1
+end
+function c101002024.rmcost2(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsAbleToDeckAsCost() end
+	Duel.SendtoDeck(e:GetHandler(),tp,2,REASON_COST)
+end
+function c101002024.rmfilter2(c)
+	return (c:IsCode(89189982,36898537) or c:IsSetCard(0x202)) and not c:IsCode(101002024) and c:IsAbleToRemove()
+end
+function c101002024.rmtg2(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c101002024.rmfilter2,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_REMOVE,nil,1,tp,LOCATION_DECK)
+end
+function c101002024.rmop2(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local g=Duel.SelectMatchingCard(tp,c101002024.rmfilter2,tp,LOCATION_DECK,0,1,1,nil)
+	if g:GetCount()>0 then
+		Duel.Remove(g,POS_FACEUP,REASON_EFFECT)
+	end
+end
