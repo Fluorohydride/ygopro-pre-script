8264105484a0aaf85293c258ebcf7f2591c5dcbd
EerieCode <eeriecode@gmail.com>
2017-05-17 15:34:23 +0200

    rename

--- scripts/DP18-JP/c100418022.lua
+++ scripts/DP18-JP/c100418022.lua
@@ -1,90 +1,90 @@
 --サイバー・ダーク・カノン
 --Cyberdark Cannon
 --Scripted by sahim
-function c100418101.initial_effect(c)
+function c100418022.initial_effect(c)
 	--draw
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(100418101,0))
+	e1:SetDescription(aux.Stringid(100418022,0))
 	e1:SetCategory(CATEGORY_DRAW)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
 	e1:SetCode(EVENT_TO_GRAVE)
-	e1:SetCondition(c100418101.con)
-	e1:SetTarget(c100418101.tg)
-	e1:SetOperation(c100418101.op)
+	e1:SetCondition(c100418022.con)
+	e1:SetTarget(c100418022.tg)
+	e1:SetOperation(c100418022.op)
 	c:RegisterEffect(e1)
 	--search
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(100418101,1))
+	e2:SetDescription(aux.Stringid(100418022,1))
 	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
 	e2:SetType(EFFECT_TYPE_IGNITION)
 	e2:SetRange(LOCATION_HAND)
-	e2:SetCountLimit(1,100418101)
-	e2:SetCost(c100418101.cost)
-	e2:SetTarget(c100418101.target)
-	e2:SetOperation(c100418101.operation)
+	e2:SetCountLimit(1,100418022)
+	e2:SetCost(c100418022.cost)
+	e2:SetTarget(c100418022.target)
+	e2:SetOperation(c100418022.operation)
 	c:RegisterEffect(e2)
 	--send to grave
 	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(100418101,2))
+	e3:SetDescription(aux.Stringid(100418022,2))
 	e3:SetCategory(CATEGORY_TOGRAVE)
 	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
 	e3:SetCode(EVENT_PRE_DAMAGE_CALCULATE)
 	e3:SetRange(LOCATION_SZONE)
-	e3:SetCountLimit(1,100418101+100)
-	e3:SetCondition(c100418101.gycon)
-	e3:SetTarget(c100418101.gytg)
-	e3:SetOperation(c100418101.gyop)
+	e3:SetCountLimit(1,100418022+100)
+	e3:SetCondition(c100418022.gycon)
+	e3:SetTarget(c100418022.gytg)
+	e3:SetOperation(c100418022.gyop)
 	c:RegisterEffect(e3)
 end
-function c100418101.con(e,tp,eg,ep,ev,re,r,rp)
+function c100418022.con(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	return c:GetPreviousLocation()==LOCATION_SZONE and not c:IsReason(REASON_LOST_TARGET)
 end
-function c100418101.tg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+function c100418022.tg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chk==0 then return Duel.IsPlayerCanDraw(tp,1) end
 	Duel.SetTargetPlayer(tp)
 	Duel.SetTargetParam(1)
 	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
 end
-function c100418101.op(e,tp,eg,ep,ev,re,r,rp)
+function c100418022.op(e,tp,eg,ep,ev,re,r,rp)
 	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
 	Duel.Draw(p,d,REASON_EFFECT)
 end
-function c100418101.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+function c100418022.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
 	if chk==0 then return c:IsAbleToGraveAsCost() and c:IsDiscardable() end
 	Duel.SendtoGrave(c,REASON_COST+REASON_DISCARD)
 end
-function c100418101.filter(c)
+function c100418022.filter(c)
 	return (c:IsSetCard(0x4093) or c:IsCode(40418351,77625948,41230939,3019642))
 		and c:IsRace(RACE_MACHINE) and c:IsAbleToHand()
 end
-function c100418101.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chk==0 then return Duel.IsExistingMatchingCard(c100418101.filter,tp,LOCATION_DECK,0,1,nil) end
+function c100418022.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chk==0 then return Duel.IsExistingMatchingCard(c100418022.filter,tp,LOCATION_DECK,0,1,nil) end
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
 end
-function c100418101.operation(e,tp,eg,ep,ev,re,r,rp,chk)
-	local tg=Duel.SelectMatchingCard(tp,c100418101.filter,tp,LOCATION_DECK,0,1,1,nil)
+function c100418022.operation(e,tp,eg,ep,ev,re,r,rp,chk)
+	local tg=Duel.SelectMatchingCard(tp,c100418022.filter,tp,LOCATION_DECK,0,1,1,nil)
 	if tg:GetCount()>0 then
 		Duel.SendtoHand(tg,nil,REASON_EFFECT)
 		Duel.ConfirmCards(1-tp,tg)
 	end
 end
-function c100418101.tgfilter(c)
+function c100418022.tgfilter(c)
 	return c:IsType(TYPE_MONSTER) and c:IsAbleToGrave()
 end
-function c100418101.gycon(e,tp,eg,ep,ev,re,r,rp)
+function c100418022.gycon(e,tp,eg,ep,ev,re,r,rp)
 	local ec=e:GetHandler():GetEquipTarget()
 	return ec and (ec==Duel.GetAttacker() or ec==Duel.GetAttackTarget())
 end
-function c100418101.gytg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(c100418101.tgfilter,tp,LOCATION_DECK,0,1,nil) end
+function c100418022.gytg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c100418022.tgfilter,tp,LOCATION_DECK,0,1,nil) end
 	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_DECK)
 end
-function c100418101.gyop(e,tp,eg,ep,ev,re,r,rp)
+function c100418022.gyop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
-	local g=Duel.SelectMatchingCard(tp,c100418101.tgfilter,tp,LOCATION_DECK,0,1,1,nil)
+	local g=Duel.SelectMatchingCard(tp,c100418022.tgfilter,tp,LOCATION_DECK,0,1,1,nil)
 	if g:GetCount()>0 then
 		Duel.SendtoGrave(g,REASON_EFFECT)
 	end
