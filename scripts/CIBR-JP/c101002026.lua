3bbadf4bd327e60671dc0aedb77b73b4092458b6
EerieCode <eeriecode@gmail.com>
2017-05-17 16:30:08 +0200

    Add CIBR-JP026 --メタファイズ・タイラント・ドラゴン

---
+++ scripts/CIBR-JP/c101002026.lua
@@ -0,0 +1,110 @@
+--メタファイズ・タイラント・ドラゴン
+--Metaphys Tyrant Dragon
+--Scripted by Eerie Code
+function c101002026.initial_effect(c)
+	--summon success
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetCondition(c101002026.regcon)
+	e1:SetOperation(c101002026.regop)
+	c:RegisterEffect(e1)
+	--special summon
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(101002026,0))
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_PHASE+PHASE_STANDBY)
+	e2:SetCountLimit(1)
+	e2:SetRange(LOCATION_REMOVED)
+	e2:SetCondition(c101002026.spcon)
+	e2:SetCost(c101002026.spcost)
+	e2:SetTarget(c101002026.sptg)
+	e2:SetOperation(c101002026.spop)
+	c:RegisterEffect(e2)
+end
+function c101002026.regcon(e,tp,eg,ep,ev,re,r,rp)
+	if not re then return false end
+	local rc=re:GetHandler()
+	return re and re:IsActiveType(TYPE_MONSTER) and (c:IsCode(89189982,36898537) or c:IsSetCard(0x202))
+end
+function c101002026.regop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_IMMUNE_EFFECT)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetValue(c101002026.efilter)
+	e1:SetReset(RESET_EVENT+0x1fe0000)
+	c:RegisterEffect(e1)
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_DAMAGE_STEP_END)
+	e2:SetCondition(c101002026.atkcon)
+	e2:SetTarget(c101002026.atktg)
+	e2:SetOperation(c101002026.atkop)
+	c:RegisterEffect(e2)
+end
+function c101002026.efilter(e,re)
+	return re:IsActiveType(TYPE_TRAP)
+end
+function c101002026.atkcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler()==Duel.GetAttacker() and Duel.GetAttackTarget() and e:GetHandler():IsRelateToBattle()
+end
+function c101002026.atktg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsChainAttackable(0,true) end	
+end
+function c101002026.atkop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not c:IsRelateToBattle() then return end
+	Duel.ChainAttack()
+end
+function c101002026.spcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetTurnCount()==e:GetHandler():GetTurnID()+1
+end
+function c101002026.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsAbleToDeckAsCost() end
+	Duel.SendtoDeck(e:GetHandler(),tp,2,REASON_COST)
+end
+function c101002026.spfilter(c,e,tp)
+	return (c:IsCode(89189982,36898537) or c:IsSetCard(0x202))
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function c101002026.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(c101002026.spfilter,tp,LOCATION_DECK,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
+end
+function c101002026.spop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<1 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local tc=Duel.SelectMatchingCard(tp,c101002026.spfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp):GetFirst()
+	if tc and Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)~=0 then
+		tc:RegisterFlagEffect(101002026,RESET_EVENT+0x1fe0000,0,1)
+		local e2=Effect.CreateEffect(e:GetHandler())
+		e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		e2:SetCode(EVENT_PHASE+PHASE_END)
+		e2:SetCountLimit(1)
+		e2:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
+		e2:SetLabel(Duel.GetTurnCount()+1)
+		e2:SetLabelObject(tc)
+		e2:SetCondition(c101002026.descon)
+		e2:SetOperation(c101002026.desop)
+		Duel.RegisterEffect(e2,tp)
+	end
+end
+function c101002026.descon(e,tp,eg,ep,ev,re,r,rp)
+	local tc=e:GetLabelObject()
+	if tc:GetFlagEffect(101002026)~=0 then
+		return Duel.GetTurnCount()==e:GetLabel()
+	else
+		e:Reset()
+		return false
+	end
+end
+function c101002026.desop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=e:GetLabelObject()
+	Duel.Remove(tc,POS_FACEUP,REASON_EFFECT)
+end
