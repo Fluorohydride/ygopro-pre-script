e87c8b93d7cf003aa5c234c64cec790e11ede675
EerieCode <eeriecode@gmail.com>
2017-05-17 18:54:17 +0200

    Add CIBR-JP055 --スクイブ・ドロー

---
+++ scripts/CIBR-JP/c101002055.lua
@@ -0,0 +1,35 @@
+--スクイブ・ドロー
+--Squib Draw
+function c101002055.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_DESTROY)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCountLimit(1,101002055+EFFECT_COUNT_CODE_OATH)
+	e1:SetTarget(c101002055.target)
+	e1:SetOperation(c101002055.activate)
+	c:RegisterEffect(e1)
+end
+function c101002055.ddfilter(c)
+	return c:IsFaceup() and c:IsSetCard(0x201)
+end
+function c101002055.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and c101002055.ddfilter(chkc) end
+	if chk==0 then return Duel.IsPlayerCanDraw(tp,2)
+		and Duel.IsExistingTarget(c101002055.ddfilter,tp,LOCATION_MZONE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local g=Duel.SelectTarget(tp,c101002055.ddfilter,tp,LOCATION_MZONE,0,1,1,nil)
+	Duel.SetTargetPlayer(tp)
+	Duel.SetTargetParam(1)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,2)
+end
+function c101002055.activate(e,tp,eg,ep,ev,re,r,rp)
+	local g,p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+	g=g:Filter(Card.IsRelateToEffect,nil,e)
+	if g:GetCount()>0 and Duel.Destroy(g,REASON_EFFECT)>0 then
+		Duel.Draw(p,d,REASON_EFFECT)
+	end
+end
