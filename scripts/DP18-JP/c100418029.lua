bfbb210beb4c1d2a0aeca4668e889ea2939ed341
EerieCode <eeriecode@gmail.com>
2017-05-17 13:09:51 +0200

    Add DP18-JP029 --ミキサーロイド

---
+++ scripts/DP18-JP/c100418029.lua
@@ -0,0 +1,136 @@
+--ミキサーロイド
+--Mixeroid
+--Scripted by Eerie Code
+function c100418029.initial_effect(c)
+	--special summon
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(100418029,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1,100418029)
+	e1:SetCost(c100418029.cost)
+	e1:SetTarget(c100418029.target)
+	e1:SetOperation(c100418029.operation)
+	c:RegisterEffect(e1)
+	--special summon
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(100418029,1))
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetCountLimit(1,100418029+100)
+	e2:SetRange(LOCATION_GRAVE)
+	e2:SetCost(c100418029.spcost)
+	e2:SetTarget(c100418029.sptg)
+	e2:SetOperation(c100418029.spop)
+	c:RegisterEffect(e2)
+end
+function c100418029.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.CheckReleaseGroup(tp,Card.IsRace,nil,RACE_MACHINE) end
+	local g=Duel.SelectReleaseGroup(tp,Card.IsRace,1,1,nil,RACE_MACHINE)
+	Duel.Release(g,REASON_COST)
+end
+function c100418029.filter(c,e,tp)
+	return c:IsSetCard(0x16) and not c:IsAttribute(ATTRIBUTE_WIND) and c:IsCanBeSpecialSummoned(e,0,tp,true,true)
+end
+function c100418029.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>-1
+		and Duel.IsExistingMatchingCard(c100418029.filter,tp,LOCATION_DECK,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
+end
+function c100418029.operation(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,c100418029.filter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
+	if g:GetCount()~=0 then
+		Duel.SpecialSummon(g,0,tp,tp,true,true,POS_FACEUP)
+		g:GetFirst():CompleteProcedure()
+	end
+end
+function c100418029.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	e:SetLabel(100)
+	return true
+end
+function c100418029.cfilter(c)
+	return c:IsRace(RACE_MACHINE) and c:IsAbleToRemoveAsCost()
+end
+function c100418029.spfilter(c,e,tp,lv)
+	return c:IsSetCard(0x16) and c:IsType(TYPE_FUSION) and c:IsLevelBelow(lv) and c:IsCanBeSpecialSummoned(e,0,tp,true,false)
+end
+function c100418029.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then
+		if e:GetLabel()~=100 then return false end
+		e:SetLabel(0)
+		local cg=Duel.GetMatchingGroup(c100418029.cfilter,tp,LOCATION_GRAVE,0,nil)
+		return c:IsAbleToRemoveAsCost()
+			and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+			and Duel.IsExistingMatchingCard(c100418029.spfilter,tp,LOCATION_EXTRA,0,1,nil,e,tp,cg:GetCount())
+	end
+	Duel.PayLPCost(tp,math.floor(Duel.GetLP(tp)/2))
+	local cg=Duel.GetMatchingGroup(c100418029.cfilter,tp,LOCATION_GRAVE,0,nil)
+	local tg=Duel.GetMatchingGroup(c100418029.spfilter,tp,LOCATION_EXTRA,0,nil,e,tp,cg:GetCount())
+	local lvt={}
+	local tc=tg:GetFirst()
+	while tc do
+		local tlv=0
+		tlv=tlv+tc:GetLevel()
+		lvt[tlv]=tlv
+		tc=tg:GetNext()
+	end
+	local pc=1
+	for i=1,12 do
+		if lvt[i] then lvt[i]=nil lvt[pc]=i pc=pc+1 end
+	end
+	lvt[pc]=nil
+	Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(100418029,2))
+	local lv=Duel.AnnounceNumber(tp,table.unpack(lvt))
+	local rg1=Group.CreateGroup()
+	if lv>1 then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+		local rg2=cg:Select(tp,lv-1,lv-1,c)
+		rg1:Merge(rg2)
+	end
+	rg1:AddCard(c)
+	Duel.Remove(rg1,POS_FACEUP,REASON_COST)
+	e:SetLabel(lv)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
+end
+function c100418029.sfilter(c,e,tp,lv)
+	return c:IsSetCard(0x16) and c:IsType(TYPE_FUSION) and c:GetLevel()==lv and c:IsCanBeSpecialSummoned(e,0,tp,true,false)
+end
+function c100418029.spop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	local lv=e:GetLabel()
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,c100418029.sfilter,tp,LOCATION_EXTRA,0,1,1,nil,e,tp,lv)
+	local tc=g:GetFirst()
+	if tc and Duel.SpecialSummon(tc,0,tp,tp,true,false,POS_FACEUP)~=0 then
+		local fid=e:GetHandler():GetFieldID()
+		tc:RegisterFlagEffect(100418029,RESET_EVENT+0x1fe0000,0,1,fid)
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		e1:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
+		e1:SetCode(EVENT_PHASE+PHASE_END)
+		e1:SetCountLimit(1)
+		e1:SetLabel(fid)
+		e1:SetLabelObject(tc)
+		e1:SetCondition(c100418029.descon)
+		e1:SetOperation(c100418029.desop)
+		e1:SetReset(RESET_PHASE+PHASE_END)
+		Duel.RegisterEffect(e1,tp)
+	end
+end
+function c100418029.descon(e,tp,eg,ep,ev,re,r,rp)
+	local tc=e:GetLabelObject()
+	if tc:GetFlagEffectLabel(100418029)==e:GetLabel() then
+		return true
+	else
+		e:Reset()
+		return false
+	end
+end
+function c100418029.desop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=e:GetLabelObject()
+	Duel.Destroy(tc,REASON_EFFECT)
+end
