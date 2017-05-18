47c4febbfba080ba6566ab2c056898b26a3b8e4a
EerieCode <eeriecode@gmail.com>
2017-05-18 09:47:30 +0200

    fix

--- scripts/CIBR-JP/c101002008.lua
+++ scripts/CIBR-JP/c101002008.lua
@@ -1,30 +1,30 @@
 --スニッフィング・ドラゴン
 --Sniffing Dragon
 --Scripted by Eerie Code
-function c101002007.initial_effect(c)
+function c101002008.initial_effect(c)
 	--search
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(101002007,0))
+	e1:SetDescription(aux.Stringid(101002008,0))
 	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e1:SetCode(EVENT_SUMMON_SUCCESS)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
-	e1:SetTarget(c101002007.target)
-	e1:SetOperation(c101002007.operation)
+	e1:SetTarget(c101002008.target)
+	e1:SetOperation(c101002008.operation)
 	c:RegisterEffect(e1)
 	local e2=e1:Clone()
 	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
 	c:RegisterEffect(e2)
 end
-function c101002007.filter(c)
-	return c:IsCode(101002007) and c:IsAbleToHand()
+function c101002008.filter(c)
+	return c:IsCode(101002008) and c:IsAbleToHand()
 end
-function c101002007.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(c101002007.filter,tp,LOCATION_DECK,0,1,nil) end
+function c101002008.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c101002008.filter,tp,LOCATION_DECK,0,1,nil) end
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
 end
-function c101002007.operation(e,tp,eg,ep,ev,re,r,rp)
-	local tc=Duel.GetFirstMatchingCard(c101002007.filter,tp,LOCATION_DECK,0,nil)
+function c101002008.operation(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstMatchingCard(c101002008.filter,tp,LOCATION_DECK,0,nil)
 	if tc then
 		Duel.SendtoHand(tc,nil,REASON_EFFECT)
 	end
