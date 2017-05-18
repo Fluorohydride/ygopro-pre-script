f4c792709a6f19c8cef6825df627e89958fc5eb7
EerieCode <eeriecode@gmail.com>
2017-05-18 21:26:32 +0200

    SD32-JP004 --バックリンカー

---
+++ scripts/SD32-JP/c100332004.lua
@@ -0,0 +1,56 @@
+--バックリンカー
+--Backlinker
+function c100332004.initial_effect(c)
+	--special summon
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_SPSUMMON_PROC)
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCondition(c100332004.spcon)
+	c:RegisterEffect(e1)
+	--destroy
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(100332004,0))
+	e2:SetCategory(CATEGORY_TODECK)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCost(c100332004.tdcost)
+	e2:SetTarget(c100332004.tdtg)
+	e2:SetOperation(c100332004.tdop)
+	c:RegisterEffect(e2)
+end
+function c100332004.filter(c)
+	return c:IsFaceup() and c:GetSequence()>=5
+end
+function c100332004.spcon(e,c)
+	if c==nil then return true end
+	local tp=c:GetControler()
+	return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(c100332004.filter,tp,0,LOCATION_MZONE,1,nil)
+		and not Duel.IsExistingMatchingCard(c100332004.filter,tp,LOCATION_MZONE,0,1,nil)
+end
+function c100332004.tdcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsReleasable() end
+	Duel.Release(e:GetHandler(),REASON_COST)
+end
+function c100332004.tdtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c100332004.filter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
+	local g=Duel.GetMatchingGroup(c100332004.filter,tp,LOCATION_MZONE,LOCATION_MZONE,nil)
+	Duel.SetOperationInfo(0,CATEGORY_TODECK,g,g:GetCount(),0,0)
+end
+function c100332004.tdop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(c100332004.tdfilter,tp,LOCATION_MZONE,LOCATION_MZONE,e:GetHandler())
+	Duel.SendtoDeck(g,nil,2,REASON_EFFECT)
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+	e1:SetReset(RESET_PHASE+PHASE_END)
+	e1:SetTargetRange(1,0)
+	e1:SetTarget(c100332004.splimit)
+	Duel.RegisterEffect(e1,tp)
+end
+function c100332004.splimit(e,c,sump,sumtype,sumpos,targetp,se)
+	return c:IsLocation(LOCATION_EXTRA)
+end
