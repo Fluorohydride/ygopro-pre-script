64f98611d133a48d9aad6cf3b91b9ca62fc8cc71
EerieCode <eeriecode@gmail.com>
2017-05-18 09:47:53 +0200

    CIBR-JP007 --ゲートウェイ・ドラゴン

---
+++ scripts/CIBR-JP/c101002007.lua
@@ -0,0 +1,49 @@
+--ゲートウェイ・ドラゴン
+--Gateway Dragon
+function c101002007.initial_effect(c)
+	--special summon
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_SPSUMMON_PROC)
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCountLimit(1,101002007)
+	e1:SetCondition(c101002007.spcon)
+	c:RegisterEffect(e1)
+	--special summon
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(101002007,0))
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1)
+	e2:SetTarget(c101002007.sptg)
+	e2:SetOperation(c101002007.spop)
+	c:RegisterEffect(e2)
+end
+function c101002007.spfilter(c)
+	return c:IsFaceup() and c:IsType(TYPE_LINK)
+end
+function c101002007.spcon(e,c)
+	if c==nil then return true end
+	local tp=c:GetControler()
+	return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(c101002007.spfilter,tp,0,LOCATION_MZONE,1,nil)
+end
+function c101002007.filter(c,e,tp)
+	return c:IsRace(RACE_DRAGON)  and c:IsAttribute(ATTRIBUTE_DARK) and c:IsLevelBelow(4) 
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function c101002007.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(c101002007.filter,tp,LOCATION_HAND,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
+end
+function c101002007.spop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,c101002007.filter,tp,LOCATION_HAND,0,1,1,nil,e,tp)
+	if g:GetCount()>0 then
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
