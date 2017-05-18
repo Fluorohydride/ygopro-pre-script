4faa3395ccc653862600533fe179b8edefe30326
EerieCode <eeriecode@gmail.com>
2017-05-18 12:15:47 +0200

    SD32-JP023 --サイバネット・バックドア

---
+++ scripts/SD32-JP/c100332023.lua
@@ -0,0 +1,64 @@
+--サイバネット・バックドア
+--Cybnet Backdoor
+--Scripted by Eerie Code
+function c100332023.initial_effect(c)
+	--activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_REMOVE+CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetCountLimit(1,100332023+EFFECT_COUNT_CODE_OATH)
+	e1:SetTarget(c100332023.target)
+	e1:SetOperation(c100332023.activate)
+	c:RegisterEffect(e1)
+end
+function c100332023.rmfilter(c,tp)
+	return c:IsFaceup() and c:IsRace(RACE_CYBERS) and c:IsAbleToRemove() and c:GetBaseAttack()>0 
+		and Duel.IsExistingMatchingCard(c100332023.thfilter,tp,LOCATION_DECK,0,1,nil,c)
+end
+function c100332023.thfilter(c,rc)
+	return c:IsRace(RACE_CYBERS) and c:GetAttack()<rc:GetBaseAttack() and c:IsAbleToHand()
+end
+function c100332023.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and c100332023.rmfilter(chkc,tp) end
+	if chk==0 then return Duel.IsExistingTarget(c100332023.rmfilter,tp,LOCATION_MZONE,0,1,nil,tp) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local g=Duel.SelectTarget(tp,c100332023.rmfilter,tp,LOCATION_MZONE,0,1,1,nil,tp)
+	Duel.SetOperationInfo(0,CATEGORY_REMOVE,g,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function c100332023.activate(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) and Duel.Remove(tc,POS_FACEUP,REASON_EFFECT+REASON_TEMPORARY)>0 then
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		e1:SetCode(EVENT_PHASE+PHASE_STANDBY)
+		e1:SetReset(RESET_PHASE+PHASE_STANDBY+RESET_SELF_TURN)
+		e1:SetLabelObject(tc)
+		e1:SetCountLimit(1)
+		e1:SetCondition(c100332023.retcon)
+		e1:SetOperation(c100332023.retop)
+		Duel.RegisterEffect(e1,tp)
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+		local g=Duel.SelectMatchingCard(tp,c100332023.thfilter,tp,LOCATION_DECK,0,1,1,nil,tc)
+		if g:GetCount()>0 then
+			Duel.SendtoHand(g,nil,REASON_EFFECT)
+			Duel.ConfirmCards(1-tp,g)
+		end
+	end
+end
+function c100332023.retcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetTurnPlayer()==tp
+end
+function c100332023.retop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=e:GetLabelObject()
+	if Duel.ReturnToField(tc) then
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_DIRECT_ATTACK)
+		e1:SetValue(1)
+		e1:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END)
+		tc:RegisterEffect(e1)
+	end
+end
