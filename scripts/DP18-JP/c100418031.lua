107c42ce9c1bdd4a9da87b99080b6c7807c84636
EerieCode <eeriecode@gmail.com>
2017-05-18 15:25:45 +0200

    DP18-JP031 --メガロイド都市
    Credits to sahim

---
+++ scripts/DP18-JP/c100418031.lua
@@ -0,0 +1,116 @@
+--メガロイド都市
+--Megaroid City
+--Scripted by sahim
+function c100418031.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e1)
+	--destroy
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(100418031,0))
+	e2:SetCategory(CATEGORY_DESTROY+CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_FZONE)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetCountLimit(1,100418031)
+	e2:SetCost(c100418031.descost)
+	e2:SetTarget(c100418031.destg)
+	e2:SetOperation(c100418031.desop)
+	c:RegisterEffect(e2)
+	--Increase ATK
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(100418031,1))
+	e3:SetCategory(CATEGORY_ATKCHANGE+CATEGORY_DEFCHANGE)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e3:SetRange(LOCATION_FZONE)
+	e3:SetCode(EVENT_PRE_DAMAGE_CALCULATE)
+	e3:SetCountLimit(1,100418031+100)
+	e3:SetCondition(c100418031.atkcon)
+	e3:SetCost(c100418031.atkcost)
+	e3:SetOperation(c100418031.atkop)
+	c:RegisterEffect(e3)	
+	Duel.AddCustomActivityCounter(100418031,ACTIVITY_SPSUMMON,c100418031.counterfilter)
+end
+function c100418031.counterfilter(c)
+	return c:GetSummonLocation()~=LOCATION_EXTRA or c:IsType(TYPE_FUSION)
+end
+function c100418031.splimit(e,c,sump,sumtype,sumpos,targetp,se)
+	return not c:IsType(TYPE_FUSION) and c:IsLocation(LOCATION_EXTRA)
+end
+function c100418031.descost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetCustomActivityCount(100418031,tp,ACTIVITY_SPSUMMON)==0 end
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_OATH)
+	e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+	e1:SetTargetRange(1,0)
+	e1:SetTarget(c100418031.splimit)
+	e1:SetReset(RESET_PHASE+PHASE_END)
+	Duel.RegisterEffect(e1,tp)
+end
+function c100418031.desfilter(c)
+	return c:IsFaceup()
+end
+function c100418031.thfilter(c)
+	return c:IsSetCard(0x16) and c:IsAbleToHand()
+end
+function c100418031.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsOnField() and chkc:IsControler(tp) and c100418031.desfilter(chkc) and chkc~=e:GetHandler() end
+	if chk==0 then return Duel.IsExistingTarget(c100418031.desfilter,tp,LOCATION_ONFIELD,0,1,e:GetHandler())
+		and Duel.IsExistingMatchingCard(c100418031.thfilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local g=Duel.SelectTarget(tp,c100418031.desfilter,tp,LOCATION_ONFIELD,0,1,1,e:GetHandler())
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function c100418031.desop(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetHandler():IsRelateToEffect(e) then return end
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) and Duel.Destroy(tc,REASON_EFFECT)~=0 then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+		local g=Duel.SelectMatchingCard(tp,c100418031.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+		if g:GetCount()>0 then
+			Duel.SendtoHand(g,nil,REASON_EFFECT)
+			Duel.ConfirmCards(1-tp,g)
+		end
+	end
+end
+function c100418031.atkcon(e,tp,eg,ep,ev,re,r,rp)
+	local a=Duel.GetAttacker()
+	if not a:IsControler(tp) then
+		a=Duel.GetAttackTarget()
+	end
+	return a and a:IsSetCard(0x16)
+end
+function c100418031.atkfilter(c)
+	return c:IsType(TYPE_MONSTER) and c:IsSetCard(0x16) and c:IsAbleToGrave()
+end
+function c100418031.atkcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c100418031.atkfilter,tp,LOCATION_DECK,0,1,nil)
+		and Duel.GetCustomActivityCount(100418031,tp,ACTIVITY_SPSUMMON)==0 end
+	local g=Duel.SelectMatchingCard(tp,c100418031.atkfilter,tp,LOCATION_DECK,0,1,1,nil)
+	Duel.SendtoGrave(g,REASON_EFFECT)
+	
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_OATH)
+	e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+	e1:SetTargetRange(1,0)
+	e1:SetTarget(c100418031.splimit)
+	e1:SetReset(RESET_PHASE+PHASE_END)
+	Duel.RegisterEffect(e1,tp)
+end
+function c100418031.atkop(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetHandler():IsRelateToEffect(e) then return end
+	local tc=Duel.GetAttacker()
+	if tc:IsControler(1-tp) then tc=Duel.GetAttackTarget() end
+	if tc:IsRelateToBattle() then
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_SWAP_BASE_AD)
+		e1:SetReset(RESET_PHASE+PHASE_DAMAGE)
+		tc:RegisterEffect(e1)
+	end
+end
