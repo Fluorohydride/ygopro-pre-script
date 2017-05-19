085ab5727f2e67721a3b051c9ecc72040cb9fa31
EerieCode <eeriecode@gmail.com>
2017-05-19 21:46:36 +0200

    CIBR-JP042 --ヴァレルロード・ドラゴン
    Credits to edo9300

---
+++ scripts/CIBR-JP/c101002042.lua
@@ -0,0 +1,117 @@
+--ヴァレルロード・ドラゴン
+--Varrel Load Dragon
+--Scripted by edo9300
+function c101002042.initial_effect(c)
+	--link summon
+	aux.AddLinkProcedure(c,aux.FilterBoolFunction(Card.IsType,TYPE_EFFECT),3)
+	c:EnableReviveLimit()
+	--cannot be target
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
+	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetValue(c101002042.efilter1)
+	c:RegisterEffect(e2)
+	--swap
+	local e3=Effect.CreateEffect(c)
+	e3:SetCategory(CATEGORY_ATKCHANGE+CATEGORY_DEFCHANGE)
+	e3:SetType(EFFECT_TYPE_QUICK_O)
+	e3:SetCode(EVENT_FREE_CHAIN)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DAMAGE_STEP)
+	e3:SetHintTiming(TIMING_DAMAGE_STEP)
+	e3:SetCountLimit(1)
+	e3:SetCondition(c101002042.atkcon)
+	e3:SetTarget(c101002042.atktg)
+	e3:SetOperation(c101002042.atkop)
+	c:RegisterEffect(e3)
+	--control
+	local e4=Effect.CreateEffect(c)
+	e4:SetCategory(CATEGORY_CONTROL)
+	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e4:SetCode(EVENT_BATTLE_START)
+	e4:SetCondition(c101002042.ctcon)
+	e4:SetTarget(c101002042.cttg)
+	e4:SetOperation(c101002042.ctop)
+	c:RegisterEffect(e4)
+end
+function c101002042.efilter1(e,re,rp)
+	return re:IsActiveType(TYPE_MONSTER)
+end
+function c101002042.atkcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetCurrentPhase()~=PHASE_DAMAGE or not Duel.IsDamageCalculated()
+end
+function c101002042.atktg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsFaceup() end
+	if chk==0 then return Duel.IsExistingTarget(Card.IsFaceup,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	Duel.SelectTarget(tp,Card.IsFaceup,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
+	Duel.SetChainLimit(c101002042.chlimit)
+end
+function c101002042.chlimit(e,ep,tp)
+	return tp==ep
+end
+function c101002042.atkop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	local c=e:GetHandler()
+	if tc:IsFaceup() and tc:IsRelateToEffect(e) then
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e1:SetReset(RESET_EVENT+0x1fe0000)
+		e1:SetValue(-500)
+		tc:RegisterEffect(e1)
+		local e2=e1:Clone()
+		e2:SetCode(EFFECT_UPDATE_DEFENSE)
+		tc:RegisterEffect(e2)
+	end
+end
+function c101002042.ctcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local zone=c:GetFreeLinkedZone()
+	if c:GetSequence()>4 then zone=bit.band(zone,0xfff) end
+	return zone~=0
+end
+function c101002042.cttg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local tc=Duel.GetAttackTarget()
+	if chk==0 then return  Duel.GetAttacker()==e:GetHandler() and tc and tc:IsControlerCanBeChanged() end
+	Duel.SetOperationInfo(0,CATEGORY_CONTROL,tc,1,0,0)
+end
+function c101002042.ctop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetAttackTarget()
+	local c=e:GetHandler()
+	if tc then
+		local zone=c:GetFreeLinkedZone()
+		if c:GetSequence()>4 then zone=bit.band(zone,0xfff) end
+		local flag=bit.bxor(zone,0xff)
+		local s=Duel.SelectDisableField(tp,1,LOCATION_MZONE,0,flag)
+		if Duel.GetControl(tc,tp)~=0 then
+			local seq=0
+			if s==1 then seq=0
+			elseif s==2 then seq=1
+			elseif s==4 then seq=2
+			elseif s==8 then seq=3
+			else seq=4 end
+			Duel.MoveSequence(tc,seq)
+			local e1=Effect.CreateEffect(c)
+			e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+			e1:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
+			e1:SetRange(LOCATION_MZONE)
+			e1:SetCode(EVENT_PHASE+PHASE_END)
+			e1:SetCondition(c101002042.descon)
+			e1:SetOperation(c101002042.desop)
+			e1:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END,2)
+			e1:SetCountLimit(1)
+			e1:SetLabel(Duel.GetTurnCount())
+			tc:RegisterEffect(e1,true)
+		end
+	end
+end
+function c101002042.descon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetTurnCount()~=e:GetLabel()
+end
+function c101002042.desop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.SendtoGrave(e:GetHandler(),REASON_EFFECT)
+end
