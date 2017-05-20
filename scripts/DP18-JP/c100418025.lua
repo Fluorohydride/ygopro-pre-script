83cb7b238481b9ee46ff4a7f95a4dc8d202852e2
dest <dest.dn@gmail.com>
2017-05-20 23:32:28 +0200

    fix

--- scripts/DP18-JP/c100418025.lua
+++ scripts/DP18-JP/c100418025.lua
@@ -65,6 +65,7 @@ function c100418025.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,0,0)
 end
 function c100418025.operation(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetHandler():IsRelateToEffect(e) then return end
 	local tc=Duel.GetFirstTarget()
 	if tc and tc:IsRelateToEffect(e) and Duel.SendtoHand(tc,nil,REASON_EFFECT)>0 then
 		local g=Duel.GetMatchingGroup(c100418025.filter2,tp,LOCATION_HAND,0,nil)
