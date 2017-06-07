2479f9f4a5f0650cd08ae7e1840f5675866a0ea3
mercury233 <me@mercury233.me>
2017-06-07 22:18:27 +0800

    add CIBR-JP045 Gouki Thunder Ogre (not fully implemented)

---
+++ scripts/CIBR-JP/c101002045.lua
@@ -0,0 +1,81 @@
+--剛鬼サンダー・オーガ
+--Gouki Thunder Ogre
+--Script by mercury233
+--not fully implemented
+function c101002045.initial_effect(c)
+	--link summon
+	aux.AddLinkProcedure(c,aux.FilterBoolFunction(Card.IsSetCard,0xfc),2)
+	c:EnableReviveLimit()
+	--extra summon
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_EXTRA_SUMMON_COUNT)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetTargetRange(LOCATION_HAND,LOCATION_HAND)
+	e1:SetTarget(c101002045.estg)
+	c:RegisterEffect(e1)
+	--special summon
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCode(EVENT_LEAVE_FIELD_P)
+	e2:SetOperation(c101002045.regop)
+	c:RegisterEffect(e2)
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(101002045,1))
+	e3:SetCategory(CATEGORY_ATKCHANGE)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e3:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
+	e3:SetCode(EVENT_DESTROYED)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetLabelObject(e2)
+	e3:SetCondition(c101002045.atkcon)
+	e3:SetOperation(c101002045.atkop)
+	c:RegisterEffect(e3)
+end
+function c101002045.estg(e,tc)
+	local c=e:GetHandler()
+	if c:GetSequence()<5 then return false end
+	local tp=Duel.GetTurnPlayer()
+	local p=c:GetControler()
+	local ct=1
+	if p==tp then ct=2 end
+	local mi,ma=tc:GetTributeRequirement()
+	local lct1=c:GetLinkedGroup():Filter(Card.IsControler,nil,tp):GetCount()
+	if ma==0 then
+		return ct-lct1>0
+	else
+		local lct2=c:GetLinkedGroup():Filter(Card.IsControler,nil,tp):Filter(Card.IsReleasable,nil):GetCount()
+		local g=Duel.GetTributeGroup(tc)
+		g:RemoveCard(c)
+		return g:GetCount()>=mi and ct-lct1+lct2>0
+	end
+end
+function c101002045.regop(e,tp,eg,ep,ev,re,r,rp)
+	local g=e:GetHandler():GetLinkedGroup()
+	if not g then return end
+	local lg=g:Clone()
+	lg:KeepAlive()
+	e:SetLabelObject(lg)
+end
+function c101002045.cfilter(c,g)
+	return c:IsReason(REASON_BATTLE+REASON_EFFECT) and g:IsContains(c)
+end
+function c101002045.atkcon(e,tp,eg,ep,ev,re,r,rp)
+	local lg=e:GetLabelObject():GetLabelObject()
+	if not lg then return false end
+	return eg:IsExists(c101002045.cfilter,1,nil,lg)
+end
+function c101002045.atkop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsFaceup() and c:IsRelateToEffect(e) then
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetProperty(EFFECT_FLAG_COPY_INHERIT)
+		e1:SetValue(400)
+		e1:SetReset(RESET_EVENT+0x1ff0000)
+		c:RegisterEffect(e1)
+	end
+end
