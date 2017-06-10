de1f1a9505ace30a87022821da83dc4d9e72645f
EerieCode <eeriecode@gmail.com>
2017-06-10 08:45:15 +0200

    fix

--- scripts/CIBR-JP/c101002061.lua
+++ scripts/CIBR-JP/c101002061.lua
@@ -4,7 +4,7 @@
 function c101002061.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOKEN)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetCountLimit(1,101002061+EFFECT_COUNT_CODE_OATH)
@@ -14,13 +14,13 @@ function c101002061.initial_effect(c)
 end
 function c101002061.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsPlayerCanSpecialSummonMonster(tp,101002161,0,0x4011,2000,2000,4,RACE_CYBERS,ATTRIBUTE_LIGHT) end
+		and Duel.IsPlayerCanSpecialSummonMonster(tp,101002161,0,0x4011,2000,2000,4,RACE_CYBERS,ATTRIBUTE_LIGHT,POS_FACEUP_DEFENSE) end
 	Duel.SetOperationInfo(0,CATEGORY_TOKEN,nil,1,0,0)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,0,0)
 end
 function c101002061.activate(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0
-		or not Duel.IsPlayerCanSpecialSummonMonster(tp,101002161,0,0x4011,0,0,1,RACE_CYBERS,ATTRIBUTE_LIGHT) then return end
+		or not Duel.IsPlayerCanSpecialSummonMonster(tp,101002161,0,0x4011,2000,2000,4,RACE_CYBERS,ATTRIBUTE_LIGHT,POS_FACEUP_DEFENSE) then return end
 	local token=Duel.CreateToken(tp,101002161)
 	Duel.SpecialSummon(token,0,tp,tp,false,false,POS_FACEUP_DEFENSE)
 end
