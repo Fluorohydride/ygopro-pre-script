c3a3d0cd42a299919c43eaefca1cc786a436e77e
EerieCode <eeriecode@gmail.com>
2017-06-14 18:54:52 +0200

    CIBR-JP071 --オルターガイスト・プロトコル

---
+++ scripts/CIBR-JP/c101002071.lua
@@ -0,0 +1,76 @@
+--オルターガイスト・プロトコル
+--Altergeist Protocol
+--Scripted by Eerie Code
+function c101002071.initial_effect(c)
+	--activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e1)
+	--negate
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(20155904,0))
+	e2:SetCategory(CATEGORY_NEGATE)
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetCode(EVENT_CHAINING)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetCondition(c101002071.discon)
+	e2:SetCost(c101002071.discost)
+	e2:SetTarget(c101002071.distg)
+	e2:SetOperation(c101002071.disop)
+	c:RegisterEffect(e2)
+	--cannot disable
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetCode(EFFECT_CANNOT_DISABLE)
+	e3:SetRange(LOCATION_SZONE)
+	e3:SetTargetRange(LOCATION_ONFIELD,0)
+	e3:SetProperty(EFFECT_FLAG_SET_AVAILABLE)
+	e3:SetTarget(c101002071.distarget)
+	c:RegisterEffect(e3)
+	--inactivatable
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_FIELD)
+	e4:SetCode(EFFECT_CANNOT_INACTIVATE)
+	e4:SetRange(LOCATION_SZONE)
+	e4:SetValue(c101002071.effectfilter)
+	c:RegisterEffect(e4)
+	local e5=Effect.CreateEffect(c)
+	e5:SetType(EFFECT_TYPE_FIELD)
+	e5:SetCode(EFFECT_CANNOT_DISEFFECT)
+	e5:SetRange(LOCATION_SZONE)
+	e5:SetValue(c101002071.effectfilter)
+	c:RegisterEffect(e5)
+end
+function c101002071.discon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return ep==1-tp	and re:IsActiveType(TYPE_MONSTER) and Duel.IsChainNegatable(ev)
+end
+function c101002071.discfilter(c)
+	return c:IsFaceup() and c:IsSetCard(0x205) and c:IsAbleToGraveAsCost()
+end
+function c101002071.discost(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return Duel.IsExistingMatchingCard(c101002071.discfilter,tp,LOCATION_ONFIELD,0,1,c)
+		and Duel.GetFlagEffect(tp,101002071)==0 end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local g=Duel.SelectMatchingCard(tp,c101002071.discfilter,tp,LOCATION_ONFIELD,0,1,1,c)
+	Duel.SendtoGrave(g,REASON_COST)
+	Duel.RegisterFlagEffect(tp,101002071,RESET_PHASE+PHASE_END,0,1)
+end
+function c101002071.distg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_DISABLE,eg,1,0,0)
+end
+function c101002071.disop(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetHandler():IsRelateToEffect(e) then return end
+	Duel.NegateActivation(ev)
+end
+function c101002071.effectfilter(e,ct)
+	local p=e:GetHandler():GetControler()
+	local te,tp=Duel.GetChainInfo(ct,CHAININFO_TRIGGERING_EFFECT,CHAININFO_TRIGGERING_PLAYER)
+	return p==tp and te:GetHandler():IsSetCard(0x205)
+end
+function c101002071.distarget(e,c)
+	return c:IsSetCard(0x205)
+end
