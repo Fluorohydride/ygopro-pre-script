d243629a8545fd3e60ae78f26a71e7eb34f52381
EerieCode <eeriecode@gmail.com>
2017-06-14 22:16:28 +0200

    CIBR-JP070 --オルターガイスト・カモフラージュ

---
+++ scripts/CIBR-JP/c101002070.lua
@@ -0,0 +1,90 @@
+--オルターガイスト・カモフラージュ
+--Altergeist Camouflage
+--Scripted by Eerie Code
+function c101002070.initial_effect(c)
+	--activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_EQUIP)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetTarget(c101002070.target)
+	e1:SetOperation(c101002070.activate)
+	c:RegisterEffect(e1)
+	--destroy replace
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e2:SetCode(EFFECT_DESTROY_REPLACE)
+	e2:SetRange(LOCATION_GRAVE)
+	e2:SetTarget(c101002070.reptg)
+	e2:SetValue(c101002070.repval)
+	e2:SetOperation(c101002070.repop)
+	c:RegisterEffect(e2)
+end
+function c101002070.filter(c)
+	return c:IsFaceup() and c:IsSetCard(0x205)
+end
+function c101002070.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and c101002070.filter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(c101002070.filter,tp,LOCATION_MZONE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)
+	Duel.SelectTarget(tp,c101002070.filter,tp,LOCATION_MZONE,0,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_EQUIP,e:GetHandler(),1,0,0)
+end
+function c101002070.activate(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not c:IsLocation(LOCATION_SZONE) then return end
+	local tc=Duel.GetFirstTarget()
+	if c:IsRelateToEffect(e) and tc:IsRelateToEffect(e) and tc:IsFaceup() then
+		Duel.Equip(tp,c,tc)
+		c:CancelToGrave()
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_EQUIP_LIMIT)
+		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e1:SetValue(c101002070.eqlimit)
+		e1:SetReset(RESET_EVENT+0x1fe0000)
+		c:RegisterEffect(e1)
+		local e2=Effect.CreateEffect(c)
+		e2:SetType(EFFECT_TYPE_EQUIP)
+		e2:SetCode(EFFECT_IGNORE_BATTLE_TARGET)
+		e2:SetReset(RESET_EVENT+0x1fe0000)
+		c:RegisterEffect(e2)
+		--negate
+		local e4=Effect.CreateEffect(c)
+		e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		e4:SetCode(EVENT_CHAIN_SOLVING)
+		e4:SetRange(LOCATION_SZONE)
+		e4:SetLabelObject(tc)
+		e4:SetCondition(c101002070.negcon)
+		e4:SetOperation(c101002070.negop)
+		e4:SetReset(RESET_EVENT+0x1fe0000)
+		c:RegisterEffect(e4)
+	end
+end
+function c101002070.eqlimit(e,c)
+	return c:GetControler()==e:GetHandlerPlayer() or e:GetHandler():GetEquipTarget()==c
+end
+function c101002070.negcon(e,tp,eg,ep,ev,re,r,rp)
+	if not re:IsHasProperty(EFFECT_FLAG_CARD_TARGET) then return false end
+	local g,l=Duel.GetChainInfo(ev,CHAININFO_TARGET_CARDS,CHAININFO_TRIGGERING_LOCATION)
+	return rp~=tp and l==LOCATION_MZONE and re:IsActiveType(TYPE_MONSTER)
+		and g and g:IsContains(e:GetLabelObject()) and Duel.IsChainDisablable(ev) 
+end
+function c101002070.negop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.NegateEffect(ev)
+end
+function c101002070.repfilter(c,tp)
+	return c:IsFaceup() and c:IsRace(RACE_DRAGON) and c:IsLocation(LOCATION_MZONE)
+		and c:IsControler(tp) and c:IsReason(REASON_EFFECT+REASON_BATTLE)
+end
+function c101002070.reptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsAbleToRemove() and eg:IsExists(c101002070.repfilter,1,nil,tp) end
+	return Duel.SelectYesNo(tp,aux.Stringid(101002070,0))
+end
+function c101002070.repval(e,c)
+	return c101002070.repfilter(c,e:GetHandlerPlayer())
+end
+function c101002070.repop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Remove(e:GetHandler(),POS_FACEUP,REASON_EFFECT)
+end
