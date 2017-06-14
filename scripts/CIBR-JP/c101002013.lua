0bb104aae39cb4852b3fd2af8c7a9542b1d2e902
EerieCode <eeriecode@gmail.com>
2017-06-14 16:09:12 +0200

    CIBR-JP013 --オルターガイスト・シルキタス

---
+++ scripts/CIBR-JP/c101002013.lua
@@ -0,0 +1,71 @@
+--オルターガイスト・シルキタス
+--Altergeist Sylkitas
+--Scripted by Eerie Code
+function c101002013.initial_effect(c)
+	--to hand
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_TOHAND)
+	e1:SetDescription(aux.Stringid(101002013,0))
+	e1:SetType(EFFECT_TYPE_QUICK_O)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1,101002013)
+	e1:SetCost(c101002013.rthcost)
+	e1:SetTarget(c101002013.rthtg)
+	e1:SetOperation(c101002013.rthop)
+	c:RegisterEffect(e1)
+	--to hand
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(101002013,1))
+	e2:SetCategory(CATEGORY_TOHAND)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
+	e2:SetCode(EVENT_TO_GRAVE)
+	e2:SetCondition(c101002013.thcon)
+	e2:SetTarget(c101002013.thtg)
+	e2:SetOperation(c101002013.thop)
+	c:RegisterEffect(e2)
+end
+function c101002013.rthcfilter(c)
+	return c:IsFaceup() and c:IsSetCard(0x205) and c:IsAbleToHandAsCost()
+end
+function c101002013.rthcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return Duel.IsExistingMatchingCard(c101002013.rthcfilter,tp,LOCATION_ONFIELD,0,1,c) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RTOHAND)
+	local g=Duel.SelectMatchingCard(tp,c101002013.rthcfilter,tp,LOCATION_ONFIELD,0,1,1,c)
+	Duel.SendtoHand(g,nil,REASON_COST)
+end
+function c101002013.rthtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsControler(1-tp) and chkc:IsLocation(LOCATION_ONFIELD) and chkc:IsAbleToHand() end
+	if chk==0 then return Duel.IsExistingTarget(Card.IsAbleToHand,tp,0,LOCATION_ONFIELD,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RTOHAND)
+	local g=Duel.SelectTarget(tp,Card.IsAbleToHand,tp,0,LOCATION_ONFIELD,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,0,0)
+end
+function c101002013.rthop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) then
+		Duel.SendtoHand(tc,nil,REASON_EFFECT)
+	end
+end
+function c101002013.thcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsPreviousLocation(LOCATION_ONFIELD)
+end
+function c101002013.thfilter(c)
+	return c:IsSetCard(0x205) and c:IsType(TYPE_TRAP) and c:IsAbleToHand()
+end
+function c101002013.thtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and c101002013.thfilter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(c101002013.thfilter,tp,LOCATION_GRAVE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local sg=Duel.SelectTarget(tp,c101002013.thfilter,tp,LOCATION_GRAVE,0,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,sg,sg:GetCount(),0,0)
+end
+function c101002013.thop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) then
+		Duel.SendtoHand(tc,nil,REASON_EFFECT)
+	end
+end
