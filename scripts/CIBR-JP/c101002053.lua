e880a2446a70807a74523b1f3174c08dc210c449
EerieCode <eeriecode@gmail.com>
2017-06-17 09:08:57 +0200

    Fixed "Security Block"

--- scripts/CIBR-JP/c101002053.lua
+++ scripts/CIBR-JP/c101002053.lua
@@ -15,10 +15,10 @@ function c101002053.filter(c)
 	return c:IsFaceup() and c:IsRace(RACE_CYBERS)
 end
 function c101002053.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_MZONE) and c101002053.filter(chkc) end
-	if chk==0 then return Duel.IsExistingTarget(c101002053.filter,tp,LOCATION_MZONE,0,1,nil) end
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and c101002053.filter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(c101002053.filter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
-	Duel.SelectTarget(tp,c101002053.filter,tp,LOCATION_MZONE,0,1,1,nil)
+	Duel.SelectTarget(tp,c101002053.filter,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
 end
 function c101002053.activate(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
