dffb4d805db71939ed549afde09291b74e544ea0
EerieCode <eeriecode@gmail.com>
2017-06-17 12:15:25 +0200

    DBSW-JP016 --魔弾の射手 カスパール

---
+++ scripts/DBSW-JP/c100419016.lua
@@ -0,0 +1,60 @@
+--魔弾の射手 カスパール
+--Magibullet Shooter Caspar
+--Scripted by Eerie Code
+function c100419016.initial_effect(c)
+	--activate from hand
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_QP_ACT_IN_NTPHAND)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetTarget(aux.TargetBoolFunction(Card.IsSetCard,0x206))
+	e1:SetTargetRange(LOCATION_HAND,0)
+	c:RegisterEffect(e1)
+	local e2=e1:Clone()
+	e2:SetCode(EFFECT_TRAP_ACT_IN_HAND)
+	c:RegisterEffect(e2)
+	--search
+	local e3=Effect.CreateEffect(c)
+	e3:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e3:SetType(EFFECT_TYPE_QUICK_O)
+	e3:SetCode(EVENT_CHAINING)
+	e3:SetProperty(EFFECT_FLAG_DELAY)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCountLimit(1,100419016)
+	e3:SetCondition(c100419016.thcon)
+	e3:SetTarget(c100419016.thtg)
+	e3:SetOperation(c100419016.thop)
+	c:RegisterEffect(e3)
+end
+function c100419016.thcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local rc=re:GetHandler()
+	if not c:IsStatus(STATUS_BATTLE_DESTROYED) and re:IsHasType(EFFECT_TYPE_ACTIVATE) and rc:IsSetCard(0x206) then 
+		local ms=c:GetSequence()
+		local rs=rc:GetSequence()
+		e:SetLabelObject(rc)
+		if rc:IsControler(tp) then
+			return ms==rs
+		else
+			return ms==(4-rs)
+		end
+	else
+		return false 
+	end
+end
+function c100419016.thfilter(c,rc)
+	return c:IsSetCard(0x206) and not c:IsCode(rc:GetCode()) and c:IsAbleToHand()
+end
+function c100419016.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local rc=re:GetHandler()
+	if chk==0 then return rc and Duel.IsExistingMatchingCard(c100419016.thfilter,tp,LOCATION_DECK,0,1,nil,rc) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function c100419016.thop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,c100419016.thfilter,tp,LOCATION_DECK,0,1,1,nil,e:GetLabelObject())
+	if g:GetCount()>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end
