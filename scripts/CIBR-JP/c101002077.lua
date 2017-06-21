a27ca201f5216fb8d78a2fabbe9881c2a8fe357b
mercury233 <me@mercury233.me>
2017-06-21 22:35:45 +0800

    fix

--- scripts/CIBR-JP/c101002077.lua
+++ scripts/CIBR-JP/c101002077.lua
@@ -6,7 +6,9 @@ function c101002077.initial_effect(c)
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_REMOVE)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
-	e1:SetCode(EVENT_PHASE+PHASE_BATTLE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetHintTiming(TIMING_BATTLE_END)
+	e1:SetCondition(c101002077.condition)
 	e1:SetTarget(c101002077.target)
 	e1:SetOperation(c101002077.activate)
 	c:RegisterEffect(e1)
@@ -17,20 +19,22 @@ function c101002077.initial_effect(c)
 	e2:SetCondition(c101002077.handcon)
 	c:RegisterEffect(e2)
 end
+function c101002077.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetCurrentPhase()==PHASE_BATTLE
+end
 function c101002077.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	local g=Duel.GetMatchingGroup(Card.IsAbleToRemove,tp,0,LOCATION_ONFIELD,nil,POS_FACEDOWN)
-	local gc=Duel.GetFieldGroup(tp,0,LOCATION_ONFIELD)-Duel.GetFieldGroupCount(tp,LOCATION_ONFIELD,0)
-	if chk==0 then return g:GetCount()>0 and gc>0 and gc<=g:GetCount() end
+	local g=Duel.GetFieldGroup(tp,0,LOCATION_ONFIELD)
+	local gc=g:GetCount()-Duel.GetFieldGroupCount(tp,LOCATION_ONFIELD,0)
+	if chk==0 then return gc>0 end
 	Duel.SetOperationInfo(0,CATEGORY_REMOVE,g,gc,0,0)
 end
 function c101002077.activate(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.GetMatchingGroup(Card.IsAbleToRemove,tp,0,LOCATION_ONFIELD,nil,POS_FACEDOWN)
-	local gc=Duel.GetFieldGroup(tp,0,LOCATION_ONFIELD)-Duel.GetFieldGroupCount(tp,LOCATION_ONFIELD,0)
-	if g:GetCount()>0 and gc>0 and gc<=g:GetCount() then
+	local g=Duel.GetFieldGroup(tp,0,LOCATION_ONFIELD)
+	local gc=g:GetCount()-Duel.GetFieldGroupCount(tp,LOCATION_ONFIELD,0)
+	if gc>0 then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
 		local sg=g:Select(1-tp,gc,gc,nil)
-		if sg:GetCount()>0 then
-			Duel.Remove(sg,POS_FACEDOWN,REASON_EFFECT)
-		end
+		Duel.Remove(sg,POS_FACEDOWN,REASON_RULE)
 	end
 end
 function c101002077.handcon(e)
