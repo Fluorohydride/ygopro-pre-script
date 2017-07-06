7d8110fe2e5ffb240765ae21a69b57bd1703c95a
EerieCode <eeriecode@gmail.com>
2017-07-06 10:36:01 +0200

    CIBR-JP066 --燃え竹光

---
+++ scripts/CIBR-JP/c101002066.lua
@@ -0,0 +1,47 @@
+--燃え竹光
+--Burning Bamboo Sword 
+function c101002066.initial_effect(c)
+	--activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e1)
+	--set
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
+	e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e2:SetCode(EVENT_CHAINING)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetOperation(aux.chainreg)
+	c:RegisterEffect(e2)
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(101002066,0))
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e3:SetCode(EVENT_CHAIN_SOLVING)
+	e3:SetProperty(EFFECT_FLAG_DELAY)
+	e3:SetRange(LOCATION_SZONE)
+	e3:SetCondition(c101002066.skipcon)
+	e3:SetOperation(c101002066.skipop)
+	c:RegisterEffect(e3)
+end
+function c101002066.skipcon(e,tp,eg,ep,ev,re,r,rp)
+	return rp==tp and re:IsHasType(EFFECT_TYPE_ACTIVATE) and re:IsActiveType(TYPE_SPELL+TYPE_TRAP)
+		and re:GetHandler():IsSetCard(0x60) and e:GetHandler():GetFlagEffect(1)>0
+end
+function c101002066.skipop(e,tp,eg,ep,ev,re,r,rp)
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_SKIP_MP1)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetTargetRange(0,1)
+	if Duel.GetTurnPlayer()~=tp and (Duel.GetCurrentPhase()>=PHASE_BATTLE_START and Duel.GetCurrentPhase()<=PHASE_BATTLE) then
+		e1:SetLabel(Duel.GetTurnCount())
+		e1:SetReset(RESET_PHASE+PHASE_BATTLE+RESET_OPPO_TURN,2)
+	else
+		e1:SetReset(RESET_PHASE+PHASE_BATTLE+RESET_OPPO_TURN,1)
+	end
+	Duel.RegisterEffect(e1,tp)
+end
+function c101002066.skipcon(e)
+	return Duel.GetTurnCount()~=e:GetLabel()
+end
