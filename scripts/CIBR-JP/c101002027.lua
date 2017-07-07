d15f3f41c27613cf450f2964cbb9982a5022f8e3
EerieCode <eeriecode@gmail.com>
2017-07-07 07:33:42 +0200

    fix

--- scripts/CIBR-JP/c101002027.lua
+++ scripts/CIBR-JP/c101002027.lua
@@ -70,11 +70,11 @@ function c101002027.spfilter(c,e,tp)
 	return c:IsFaceup() and (c:IsCode(89189982,36898537) or c:IsSetCard(0x202)) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function c101002027.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_REMOVED) and chkc:IsControler(tp) and c11260714.spfilter(chkc,e,tp) end
+	if chkc then return chkc:IsLocation(LOCATION_REMOVED) and chkc:IsControler(tp) and c101002027.spfilter(chkc,e,tp) end
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingTarget(c11260714.spfilter,tp,LOCATION_REMOVED,0,1,nil,e,tp) end
+		and Duel.IsExistingTarget(c101002027.spfilter,tp,LOCATION_REMOVED,0,1,nil,e,tp) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectTarget(tp,c11260714.spfilter,tp,LOCATION_REMOVED,0,1,1,nil,e,tp)
+	local g=Duel.SelectTarget(tp,c101002027.spfilter,tp,LOCATION_REMOVED,0,1,1,nil,e,tp)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
 end
 function c101002027.spop(e,tp,eg,ep,ev,re,r,rp)
