ee33a092dc3dd600a4795e3302b4f18725e8354e
EerieCode <eeriecode@gmail.com>
2017-07-13 09:57:01 +0200

    DBSW-JP017 --魔弾の射手 ドクトル

---
+++ scripts/DBSW-JP/c100419017.lua
@@ -0,0 +1,57 @@
+--魔弾の射手 ドクトル
+--Magibullet Shooter Doctor
+--Scripted by Eerie Code
+function c100419017.initial_effect(c)
+	--activate from hand
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_QP_ACT_IN_NTPHAND)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetTarget(aux.TargetBoolFunction(Card.IsSetCard,0x206))
+	e1:SetTargetRange(LOCATION_HAND,0)
+	c:RegisterEffect(e1)
+	local e2=e1:Clone()
+	e2:SetCode(EFFECT_TRAP_ACT_IN_HAND)
+	c:RegisterEffect(e2)
+	--to hand
+	local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e0:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e0:SetCode(EVENT_CHAINING)
+	e0:SetRange(LOCATION_MZONE)
+	e0:SetOperation(aux.chainreg)
+	c:RegisterEffect(e0)
+	local e3=Effect.CreateEffect(c)
+	e3:SetCategory(CATEGORY_TOHAND)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e3:SetCode(EVENT_CHAIN_SOLVING)
+	e3:SetProperty(EFFECT_FLAG_DELAY)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCountLimit(1,100419017)
+	e3:SetCondition(c100419017.thcon)
+	e3:SetTarget(c100419017.thtg)
+	e3:SetOperation(c100419017.thop)
+	c:RegisterEffect(e3)
+end
+function c100419017.thcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local rc=re:GetHandler()
+	if not re:IsHasType(EFFECT_TYPE_ACTIVATE) or c:GetFlagEffect(1)<=0 then return false end
+	return aux.checksamecolumn(c,rc)
+end
+function c100419017.thfilter(c,rc)
+	return c:IsSetCard(0x206) and not c:IsCode(rc:GetCode()) and c:IsAbleToHand()
+end
+function c100419017.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local rc=re:GetHandler()
+	if chk==0 then return rc and Duel.IsExistingMatchingCard(c100419017.thfilter,tp,LOCATION_GRAVE,0,1,nil,rc) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_GRAVE)
+end
+function c100419017.thop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,c100419017.thfilter,tp,LOCATION_GRAVE,0,1,1,nil,e:GetLabelObject())
+	if g:GetCount()>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end
