c94b074bc606f57cdc3af958c7bc88c1478e35bf
mercury233 <me@mercury233.me>
2017-07-15 13:02:05 +0800

    fix

--- scripts/CIBR-JP/c101002066.lua
+++ scripts/CIBR-JP/c101002066.lua
@@ -33,18 +33,9 @@ function c101002066.skipop(e,tp,eg,ep,ev,re,r,rp)
 	if not e:GetHandler():IsRelateToEffect(e) then return end
 	local e1=Effect.CreateEffect(e:GetHandler())
 	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetCode(EFFECT_SKIP_MP1)
+	e1:SetCode(EFFECT_SKIP_M1)
 	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
 	e1:SetTargetRange(0,1)
-	e1:SetCondition(c101002066.skipcn)
-	if Duel.GetTurnPlayer()~=tp and (Duel.GetCurrentPhase()>=PHASE_BATTLE_START and Duel.GetCurrentPhase()<=PHASE_BATTLE) then
-		e1:SetLabel(Duel.GetTurnCount())
-		e1:SetReset(RESET_PHASE+PHASE_BATTLE+RESET_OPPO_TURN,2)
-	else
-		e1:SetReset(RESET_PHASE+PHASE_BATTLE+RESET_OPPO_TURN,1)
-	end
+	e1:SetReset(RESET_PHASE+PHASE_BATTLE+RESET_OPPO_TURN)
 	Duel.RegisterEffect(e1,tp)
-end
-function c101002066.skipcn(e)
-	return Duel.GetTurnCount()~=e:GetLabel()
 end
