51c65739e355bdf26c0be4439556fe31b0dbd058
EerieCode <eeriecode@gmail.com>
2017-07-17 22:14:14 +0200

    COTD-EN086 --F.A. Sonic Meister

---
+++ scripts/COTD-EN/c101001086.lua
@@ -0,0 +1,70 @@
+--F.A. Sonic Meister
+--Scripted by Eerie Code
+function c101001086.initial_effect(c)
+	--atk up
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_UPDATE_ATTACK)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetValue(c101001086.atkval)
+	c:RegisterEffect(e1)
+	--indes
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+	e2:SetValue(c101001086.indval)
+	c:RegisterEffect(e2)
+	--lv up
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(101001086,0))
+	e3:SetCategory(CATEGORY_LVCHANGE)
+	e3:SetCode(EVENT_CHAINING)
+	e3:SetType(EFFECT_TYPE_QUICK_O)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DAMAGE_CAL)
+	e3:SetCondition(c101001086.lvcon)
+	e3:SetOperation(c101001086.lvop)
+	c:RegisterEffect(e3)
+	--attack twice
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_SINGLE)
+	e4:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetCode(EFFECT_EXTRA_ATTACK_MONSTER)
+	e4:SetCondition(c101001086.excon)
+	e4:SetValue(1)
+	c:RegisterEffect(e4)
+end
+function c101001086.atkval(e,c)
+	return c:GetLevel()*300
+end
+function c101001086.indval(e,c)
+	local lv=e:GetHandler():GetLevel()
+	if c:GetRank()>0 then
+		return c:GetOriginalRank()<lv
+	elseif c:GetLevel()>0 then
+		return c:GetOriginalLevel()<lv
+	else return false end
+end
+function c101001086.lvcon(e,tp,eg,ep,ev,re,r,rp)
+	return not e:GetHandler():IsStatus(STATUS_BATTLE_DESTROYED)
+		and re:IsActiveType(TYPE_SPELL+TYPE_TRAP)
+		and re:GetHandler():IsSetCard(0x20a)
+end
+function c101001086.lvop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) and c:IsFaceup() then
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_LEVEL)
+		e1:SetValue(1)
+		e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+		e1:SetRange(LOCATION_MZONE)
+		e1:SetReset(RESET_EVENT+0x1fe0000)
+		c:RegisterEffect(e1)
+	end
+end
+function c101001086.excon(e)
+	return e:GetHandler():IsLevelAbove(7)
+end
