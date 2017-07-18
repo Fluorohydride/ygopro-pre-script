7982fc82de5dad54d070f6ccc30cfd97ff765018
EerieCode <eeriecode@gmail.com>
2017-07-18 07:32:23 +0200

    COTD-EN089 --F.A. Downforce

---
+++ scripts/COTD-EN/c101001089.lua
@@ -0,0 +1,47 @@
+--F.A. Downforce
+--Scripted by Eerie Code
+function c101001089.initial_effect(c)
+	--lv up
+	local e1=Effect.CreateEffect(c)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCountLimit(1,101001089)
+	e1:SetHintTiming(0x1c0)
+	e1:SetTarget(c101001089.target)
+	e1:SetOperation(c101001089.operation)
+	c:RegisterEffect(e1)
+	--grave
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_GRAVE)
+	e2:SetCountLimit(1,101001089+100)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetCondition(aux.exccon)
+	e2:SetCost(aux.bfgcost)
+	e2:SetTarget(c101001089.target)
+	e2:SetOperation(c101001089.operation)
+	c:RegisterEffect(e2)
+end
+function c101001089.filter(c)
+	return c:IsFaceup() and c:IsSetCard(0x20a) and c:IsLevelAbove(1)
+end
+function c101001089.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and c101001089.filter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(c101001089.filter,tp,LOCATION_MZONE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	Duel.SelectTarget(tp,c101001089.filter,tp,LOCATION_MZONE,0,1,1,nil)
+end
+function c101001089.operation(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tc=Duel.GetFirstTarget()
+	if tc:IsFaceup() and tc:IsRelateToEffect(e) then
+		local e1=Effect.CreateEffect(c)
+		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_LEVEL)
+		e1:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END)
+		e1:SetValue(2)
+		tc:RegisterEffect(e1)
+	end
+end
