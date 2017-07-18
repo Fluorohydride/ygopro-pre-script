06a15b05bd42565eacc216b4e1f22b2c2cad428c
mercury233 <me@mercury233.me>
2017-07-18 12:48:57 +0800

    fix

--- scripts/DBSW-JP/c100419038.lua
+++ scripts/DBSW-JP/c100419038.lua
@@ -50,9 +50,9 @@ function c100419038.datg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chk==0 then return gc and gc:IsFaceup() and gc:IsLocation(LOCATION_SZONE)
 		and not gc:IsDisabled() and c100419038.efffilter(c,gc:GetSequence(),true)
 		and Duel.IsExistingTarget(Card.IsFaceup,tp,LOCATION_MZONE,LOCATION_MZONE,1,c) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RTOHAND)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
 	local g=Duel.SelectTarget(tp,Card.IsFaceup,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,c)
-	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_ATKCHANGE,g,1,0,0)
 end
 function c100419038.daop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
