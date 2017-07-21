9b5d5284af94d5d96bf312eaad449fe89d060ef4
EerieCode <eeriecode@gmail.com>
2017-07-21 12:08:11 +0200

    DBSW-JP0?? --雷天気ターメル

---
+++ scripts/DBSW-JP/c100419100.lua
@@ -0,0 +1,79 @@
+--雷天気ターメル
+--Thunder Weathery Turmer
+--Scripted by Eerie Code
+function c100419100.initial_effect(c)
+	--place
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(100419100,0))
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_SUMMON_SUCCESS)
+	e1:SetCountLimit(1,100419100)
+	e1:SetCost(c100419100.tfcost)
+	e1:SetOperation(c100419100.tfop)
+	c:RegisterEffect(e1)
+	--spsummon
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e2:SetCode(EVENT_REMOVE)
+	e2:SetOperation(c100419100.spreg)
+	c:RegisterEffect(e2)
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(100419100,1))
+	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e3:SetRange(LOCATION_REMOVED)
+	e3:SetCode(EVENT_PHASE+PHASE_STANDBY)
+	e3:SetCondition(c100419100.spcon)
+	e3:SetTarget(c100419100.sptg)
+	e3:SetOperation(c100419100.spop)
+	e3:SetLabelObject(e2)
+	c:RegisterEffect(e3)
+end
+function c100419100.cfilter(c)
+	return c:IsFaceup() and c:IsType(TYPE_CONTINUOUS) and c:IsAbleToGraveAsCost()
+end
+function c100419100.tfcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_SZONE)>-1
+		and Duel.IsExistingMatchingCard(c100419100.cfilter,tp,LOCATION_SZONE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local g=Duel.SelectMatchingCard(tp,c100419100.cfilter,tp,LOCATION_SZONE,0,1,1,nil)
+	Duel.SendtoGrave(g,REASON_COST)
+end
+function c100419100.tffilter(c,tp)
+	return c:IsType(TYPE_SPELL+TYPE_TRAP) and c:IsSetCard(0x207) and not c:IsForbidden() and c:CheckUniqueOnField(tp)
+end
+function c100419100.tftg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_SZONE)>-1
+		and Duel.IsExistingMatchingCard(c100419100.tffilter,tp,LOCATION_DECK,0,1,nil,tp) end
+end
+function c100419100.tfop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_SZONE)<=0 then return end
+	local tc=Duel.SelectMatchingCard(tp,c100419100.tffilter,tp,LOCATION_DECK,0,1,1,nil,tp):GetFirst()
+	if tc then
+		Duel.MoveToField(tc,tp,tp,LOCATION_SZONE,POS_FACEUP,true)
+	end
+end
+function c100419100.spreg(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local rc=re:GetHandler()
+	if c:IsReason(REASON_COST) and rc:IsSetCard(0x207) then
+		e:SetLabel(Duel.GetTurnCount()+1)
+		c:RegisterFlagEffect(100419100,RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END,0,2)
+	end
+end
+function c100419100.spcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetLabelObject():GetLabel()==Duel.GetTurnCount() and e:GetHandler():GetFlagEffect(100419100)>0
+end
+function c100419100.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
+	e:GetHandler():ResetFlagEffect(100419100)
+end
+function c100419100.spop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) then
+		Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
