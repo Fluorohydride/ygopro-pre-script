9e83e02c724fe9e2988aba3b96f597c0fc62db59
dest <dest.dn@gmail.com>
2017-07-21 12:15:53 +0200

    fix

--- scripts/COTD-EN/c101001085.lua
+++ scripts/COTD-EN/c101001085.lua
@@ -28,7 +28,7 @@ function c101001085.activate(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<1 then return end
 	local tc=Duel.GetFirstTarget()
 	if tc:IsFaceup() and tc:IsRelateToEffect(e) and Duel.Release(tc,REASON_EFFECT)>0 then
-		local token=Duel.CreateToken(tp,101001085,0x209,0,0,tc:GetOriginalLevel(),RACE_ZOMBIE,ATTRIBUTE_DARK)
+		local token=Duel.CreateToken(tp,101001085+100,0x209,0,0,tc:GetOriginalLevel(),RACE_ZOMBIE,ATTRIBUTE_DARK)
 		Duel.SpecialSummonStep(token,0,tp,tp,false,false,POS_FACEUP)
 		local e1=Effect.CreateEffect(e:GetHandler())
 		e1:SetType(EFFECT_TYPE_FIELD)
