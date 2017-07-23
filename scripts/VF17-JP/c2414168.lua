350b9baea78c4705f878229b438d5dae72f8fd7b
mercury233 <me@mercury233.me>
2017-07-24 00:14:27 +0800

    change id

--- scripts/VF17-JP/c2414168.lua
+++ scripts/VF17-JP/c2414168.lua
@@ -1,39 +1,39 @@
 --インタラプト・レジスタンス
 --Interrupt Resistance
 --Script by nekrozar
-function c100230001.initial_effect(c)
+function c2414168.initial_effect(c)
 	--special summon
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(100230001,0))
+	e1:SetDescription(aux.Stringid(2414168,0))
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
 	e1:SetCode(EVENT_BATTLE_DAMAGE)
 	e1:SetRange(LOCATION_HAND)
-	e1:SetCondition(c100230001.spcon)
-	e1:SetTarget(c100230001.sptg)
-	e1:SetOperation(c100230001.spop)
+	e1:SetCondition(c2414168.spcon)
+	e1:SetTarget(c2414168.sptg)
+	e1:SetOperation(c2414168.spop)
 	c:RegisterEffect(e1)
 	--def
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(100230001,1))
+	e2:SetDescription(aux.Stringid(2414168,1))
 	e2:SetCategory(CATEGORY_DEFCHANGE)
 	e2:SetType(EFFECT_TYPE_TRIGGER_O+EFFECT_TYPE_FIELD)
 	e2:SetCode(EVENT_PRE_DAMAGE_CALCULATE)
 	e2:SetRange(LOCATION_MZONE)
 	e2:SetCountLimit(1)
-	e2:SetCondition(c100230001.defcon)
-	e2:SetOperation(c100230001.defop)
+	e2:SetCondition(c2414168.defcon)
+	e2:SetOperation(c2414168.defop)
 	c:RegisterEffect(e2)
 end
-function c100230001.spcon(e,tp,eg,ep,ev,re,r,rp)
+function c2414168.spcon(e,tp,eg,ep,ev,re,r,rp)
 	return ep==tp
 end
-function c100230001.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+function c2414168.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
 		and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
 end
-function c100230001.spop(e,tp,eg,ep,ev,re,r,rp)
+function c2414168.spop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if c:IsRelateToEffect(e) and Duel.SpecialSummonStep(c,0,tp,tp,false,false,POS_FACEUP) then
 		local e1=Effect.CreateEffect(c)
@@ -45,7 +45,7 @@ function c100230001.spop(e,tp,eg,ep,ev,re,r,rp)
 		Duel.SpecialSummonComplete()
 	end
 end
-function c100230001.defcon(e,tp,eg,ep,ev,re,r,rp)
+function c2414168.defcon(e,tp,eg,ep,ev,re,r,rp)
 	local a=Duel.GetAttacker()
 	local d=a:GetBattleTarget()
 	e:SetLabelObject(d)
