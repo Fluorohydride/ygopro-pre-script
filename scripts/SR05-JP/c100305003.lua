cae60e73b3dab2ac0e4717350f8f0d71ec4e2663
EerieCode <eeriecode@gmail.com>
2017-07-28 07:19:12 +0200

    SR05-JP003 --力天使ヴァルキリア

---
+++ scripts/SR05-JP/c100305003.lua
@@ -0,0 +1,59 @@
+--力天使ヴァルキリア
+--Power Angel Valkyria
+--Scripted by Eerie Code
+function c100305003.initial_effect(c)
+	--counter
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetCode(EVENT_CHAINING)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetOperation(c100305003.chop1)
+	c:RegisterEffect(e1)
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e2:SetCode(EVENT_CHAIN_NEGATED)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetOperation(c100305003.chop2)
+	c:RegisterEffect(e2)
+	e1:SetLabelObject(e2)
+	--special summon
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(100305003,0))
+	e3:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e3:SetCode(EVENT_CHAIN_END)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCountLimit(1,100305003)
+	e3:SetCondition(c100305003.thcon)
+	e3:SetTarget(c100305003.thtg)
+	e3:SetOperation(c100305003.thop)
+	e3:SetLabelObject(e2)
+	c:RegisterEffect(e3)
+end
+function c100305003.chop1(e,tp,eg,ep,ev,re,r,rp)
+	e:GetLabelObject():SetLabel(0)
+end
+function c100305003.chop2(e,tp,eg,ep,ev,re,r,rp)
+	local de,dp=Duel.GetChainInfo(ev,CHAININFO_DISABLE_REASON,CHAININFO_DISABLE_PLAYER)
+	if dp==tp then
+		e:SetLabel(1)
+	end
+end
+function c100305003.thcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetLabelObject():GetLabel()~=0
+end
+function c100305003.thfilter(c)
+	return c:IsRace(RACE_FAIRY) and c:IsAttribute(ATTRIBUTE_LIGHT) and c:IsAbleToHand()
+end
+function c100305003.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function c100305003.thop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,c100305003.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if g:GetCount()>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end
