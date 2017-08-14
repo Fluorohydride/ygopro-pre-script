5422ff188a7ad802170eb1cddb9ba0b5c9b0ae29
EerieCode <eeriecode@gmail.com>
2017-08-14 11:35:35 +0200

    SR05-JP031 --輪廻のパーシアス

---
+++ scripts/SR05-JP/c100305031.lua
@@ -0,0 +1,60 @@
+--輪廻のパーシアス
+--Reincarnating Parshath
+--Scripted by Eerie Code
+function c100305031.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_NEGATE+CATEGORY_DESTROY+CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_CHAINING)
+	e1:SetCondition(c100305031.condition)
+	e1:SetCost(c100305031.cost)
+	e1:SetTarget(c100305031.target)
+	e1:SetOperation(c100305031.activate)
+	c:RegisterEffect(e1)
+end
+function c100305031.condition(e,tp,eg,ep,ev,re,r,rp)
+	return (re:IsActiveType(TYPE_MONSTER) or re:IsHasType(EFFECT_TYPE_ACTIVATE)) and Duel.IsChainNegatable(ev)
+end
+function c100305031.cfilter(c)
+	return c:IsType(TYPE_COUNTER) and not c:IsPublic()
+end
+function c100305031.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c100305031.cfilter,tp,LOCATION_HAND,0,1,e:GetHandler())
+		and Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,1,e:GetHandler())
+		and Duel.CheckLPCost(tp,1000) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CONFIRM)
+	local cg=Duel.SelectMatchingCard(tp,c100305031.cfilter,tp,LOCATION_HAND,0,1,1,nil)
+	Duel.ConfirmCards(1-tp,cg)
+	Duel.DiscardHand(tp,Card.IsDiscardable,1,1,REASON_COST+REASON_DISCARD)
+	Duel.PayLPCost(tp,1000)
+end
+function c100305031.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
+	if re:GetHandler():IsAbleToDeck() and re:GetHandler():IsRelateToEffect(re) then
+		Duel.SetOperationInfo(0,CATEGORY_TODECK,eg,1,0,0)
+		Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK+LOCATION_EXTRA)
+	end
+end
+function c100305031.spfilter(c,e,tp)
+	return c:IsSetCard(0x208) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function c100305031.activate(e,tp,eg,ep,ev,re,r,rp)
+	if not Duel.NegateActivation(ev) then return end
+	if re:GetHandler():IsRelateToEffect(re) and Duel.SendtoDeck(eg,nil,2,REASON_EFFECT)~=0 then
+		local l1=Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		local l2=Duel.GetLocationCountFromEx(tp)>0
+		if not l1 and not l2 then return end
+		local loc=0
+		if l1 then loc=bit.bor(loc,LOCATION_DECK) end
+		if l2 then loc=bit.bor(loc,LOCATION_EXTRA) end
+		local g=Duel.GetMatchingGroup(c100305031.spfilter,tp,loc,0,nil,e,tp)
+		if g:GetCount()>0 and Duel.SelectYesNo(tp,aux.Stringid(100305031,0)) then
+			Duel.BreakEffect()
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+			local sg=g:Select(tp,1,1,nil)
+			Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP)
+		end
+	end
+end
