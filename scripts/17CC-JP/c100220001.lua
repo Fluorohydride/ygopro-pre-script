7fab31d543e4d6604d4f45df007f9d9e8f7bf927
EerieCode <eeriecode@gmail.com>
2017-08-28 10:28:05 +0200

    17CC-JP001 --コード・トーカー

---
+++ scripts/17CC-JP/c100220001.lua
@@ -0,0 +1,38 @@
+--コード・トーカー
+--Code Talker
+--Scripted by Eerie Code
+function c100220001.initial_effect(c)
+	--link summon
+	aux.AddLinkProcedure(c,aux.FilterBoolFunction(Card.IsLinkType,TYPE_EFFECT),2)
+	c:EnableReviveLimit()
+	--atk
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_UPDATE_ATTACK)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetValue(c100220001.atkval)
+	c:RegisterEffect(e1)
+	--indes
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+	e2:SetCondition(c100220001.incon)
+	e2:SetValue(1)
+	c:RegisterEffect(e2)
+	local e3=e2:Clone()
+	e3:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+	e3:SetValue(c100220001.inval)
+	c:RegisterEffect(e3)
+end
+function c100220001.atkval(e,c)
+	return c:GetLinkedGroupCount()*500
+end
+function c100220001.incon(e)
+	return c:GetLinkedGroupCount()>0
+end
+function c100220001.inval(e,re,r,rp)
+	return rp~=e:GetHandlerPlayer()
+end
