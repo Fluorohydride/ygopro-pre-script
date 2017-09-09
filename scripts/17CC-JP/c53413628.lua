a2670b07c7533932e11b4140d71e1946a88b489e
nekrozar <nekrozar.s@gmail.com>
2017-09-09 14:15:56 +0900

    change ID

--- scripts/17CC-JP/c53413628.lua
+++ scripts/17CC-JP/c53413628.lua
@@ -1,7 +1,7 @@
 --コード・トーカー
 --Code Talker
 --Scripted by Eerie Code
-function c100220001.initial_effect(c)
+function c53413628.initial_effect(c)
 	--link summon
 	aux.AddLinkProcedure(c,aux.FilterBoolFunction(Card.IsLinkType,TYPE_EFFECT),2,2)
 	c:EnableReviveLimit()
@@ -11,7 +11,7 @@ function c100220001.initial_effect(c)
 	e1:SetCode(EFFECT_UPDATE_ATTACK)
 	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
 	e1:SetRange(LOCATION_MZONE)
-	e1:SetValue(c100220001.atkval)
+	e1:SetValue(c53413628.atkval)
 	c:RegisterEffect(e1)
 	--indes
 	local e2=Effect.CreateEffect(c)
@@ -19,20 +19,20 @@ function c100220001.initial_effect(c)
 	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
 	e2:SetRange(LOCATION_MZONE)
 	e2:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
-	e2:SetCondition(c100220001.incon)
+	e2:SetCondition(c53413628.incon)
 	e2:SetValue(1)
 	c:RegisterEffect(e2)
 	local e3=e2:Clone()
 	e3:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
-	e3:SetValue(c100220001.inval)
+	e3:SetValue(c53413628.inval)
 	c:RegisterEffect(e3)
 end
-function c100220001.atkval(e,c)
+function c53413628.atkval(e,c)
 	return c:GetLinkedGroupCount()*500
 end
-function c100220001.incon(e)
+function c53413628.incon(e)
 	return e:GetHandler():GetLinkedGroupCount()>0
 end
-function c100220001.inval(e,re,r,rp)
+function c53413628.inval(e,re,r,rp)
 	return rp~=e:GetHandlerPlayer()
 end
--- scripts/EP17-JP/c1322368.lua
+++ scripts/EP17-JP/c1322368.lua
@@ -1,7 +1,7 @@
 --SPYRAL－ザ・ダブルヘリックス
 --SPYRAL - The Double Helix
 --Script by nekrozar
-function c100420018.initial_effect(c)
+function c1322368.initial_effect(c)
 	--link summon
 	c:EnableReviveLimit()
 	aux.AddLinkProcedure(c,aux.FilterBoolFunction(Card.IsSetCard,0xee),2,2)
@@ -15,27 +15,27 @@ function c100420018.initial_effect(c)
 	c:RegisterEffect(e1)
 	--special summon
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(100420018,0))
+	e2:SetDescription(aux.Stringid(1322368,0))
 	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e2:SetType(EFFECT_TYPE_IGNITION)
 	e2:SetRange(LOCATION_MZONE)
-	e2:SetCountLimit(1,100420018)
-	e2:SetTarget(c100420018.sptg)
-	e2:SetOperation(c100420018.spop)
+	e2:SetCountLimit(1,1322368)
+	e2:SetTarget(c1322368.sptg)
+	e2:SetOperation(c1322368.spop)
 	c:RegisterEffect(e2)
 end
-function c100420018.spfilter(c,e,tp,zone)
+function c1322368.spfilter(c,e,tp,zone)
 	return c:IsSetCard(0xee) and c:IsType(TYPE_MONSTER) and (c:IsAbleToHand() or (zone~=0 and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP,tp,zone)))
 end
-function c100420018.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+function c1322368.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local zone=e:GetHandler():GetLinkedZone()
 	if chk==0 then return Duel.GetFieldGroupCount(tp,0,LOCATION_DECK)>0
-		and Duel.IsExistingMatchingCard(c100420018.spfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,nil,e,tp,zone) end
+		and Duel.IsExistingMatchingCard(c1322368.spfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,nil,e,tp,zone) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CARDTYPE)
 	e:SetLabel(Duel.SelectOption(tp,70,71,72))
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK+LOCATION_GRAVE)
 end
-function c100420018.spop(e,tp,eg,ep,ev,re,r,rp)
+function c1322368.spop(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetFieldGroupCount(tp,0,LOCATION_DECK)==0 then return end
 	Duel.ConfirmDecktop(1-tp,1)
 	local g=Duel.GetDecktopGroup(1-tp,1)
@@ -44,11 +44,11 @@ function c100420018.spop(e,tp,eg,ep,ev,re,r,rp)
 	if (opt==0 and tc:IsType(TYPE_MONSTER)) or (opt==1 and tc:IsType(TYPE_SPELL)) or (opt==2 and tc:IsType(TYPE_TRAP)) then
 		local zone=e:GetHandler():GetLinkedZone()
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-		local sg=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(c100420018.spfilter),tp,LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil,e,tp,zone)
+		local sg=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(c1322368.spfilter),tp,LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil,e,tp,zone)
 		local sc=sg:GetFirst()
 		if sc then
 			if zone~=0 and sc:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP,tp,zone)
-				and (not sc:IsAbleToHand() or Duel.SelectYesNo(tp,aux.Stringid(100420018,1))) then
+				and (not sc:IsAbleToHand() or Duel.SelectYesNo(tp,aux.Stringid(1322368,1))) then
 				Duel.SpecialSummon(sc,0,tp,tp,false,false,POS_FACEUP,zone)
 			else
 				Duel.SendtoHand(sc,nil,REASON_EFFECT)
