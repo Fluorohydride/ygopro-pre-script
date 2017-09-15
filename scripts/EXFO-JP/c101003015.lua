9f3b902cceb5a176a1574f0f28049c9570b35abe
EerieCode <eeriecode@gmail.com>
2017-09-15 19:26:02 +0200

    EXFO-JP015 --翠嵐の機界騎士

---
+++ scripts/EXFO-JP/c101003015.lua
@@ -0,0 +1,77 @@
+--翠嵐の機界騎士
+--Mekk-Knight of the Green Heights
+--Scripted by Eerie Code
+function c101003015.initial_effect(c)
+	--special summon
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_SPSUMMON_PROC)
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCountLimit(1,101003015)
+	e1:SetCondition(c101003015.spcon)
+	e1:SetValue(c101003015.spval)
+	c:RegisterEffect(e1)
+	--to hand
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(101003015,0))
+	e2:SetCategory(CATEGORY_TOHAND)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_ATTACK_ANNOUNCE)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetCondition(c101003015.thcon)
+	e2:SetTarget(c101003015.thtg)
+	e2:SetOperation(c101003015.thop)
+	c:RegisterEffect(e2)
+end
+function c101003015.cfilter(c,tp,seq)
+	local s=c:GetSequence()
+	if c:IsLocation(LOCATION_SZONE) and s==5 then return false end
+	if c:IsControler(tp) then
+		return s==seq or (seq==1 and s==5) or (seq==3 and s==6)		
+	else
+		return s==4-seq or (seq==1 and s==6) or (seq==3 and s==5)
+	end
+end
+function c101003015.spcon(e,c)
+	if c==nil then return true end
+	local tp=c:GetControler()
+	local zone=0
+	for i=0,4 do
+		if Duel.GetMatchingGroupCount(c101003015.cfilter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,nil,tp,i)>=2 then
+			zone=zone+math.pow(2,i)
+		end
+	end
+	return Duel.GetLocationCount(tp,LOCATION_MZONE,tp,LOCATION_REASON_TOFIELD,zone)>0
+end
+function c101003015.spval(e,c)
+	local zone=0
+	for i=0,4 do
+		if Duel.GetMatchingGroupCount(c101003015.cfilter,c:GetControler(),LOCATION_ONFIELD,LOCATION_ONFIELD,nil,tp,i)>=2 then
+			zone=zone+math.pow(2,i)
+		end
+	end
+	return 0,zone
+end
+function c101003015.thcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tc=c:GetBattleTarget()
+	return tc and c:GetColumnGroup():IsContains(tc)
+end
+function c101003015.thfilter(c)
+	return c:IsSetCard(0x20c) and c:IsType(TYPE_MONSTER) and c:IsAbleToHand()
+end
+function c101003015.thtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and c101003015.thfilter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(c101003015.thfilter,tp,LOCATION_GRAVE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectTarget(tp,c101003015.thfilter,tp,LOCATION_GRAVE,0,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,0,0)
+end
+function c101003015.thop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) then
+		Duel.SendtoHand(tc,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,tc)
+	end
+end
