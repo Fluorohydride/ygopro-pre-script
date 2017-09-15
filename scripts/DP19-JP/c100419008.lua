b126f4652df7a48a223af8c963588b4765cad60f
EerieCode <eeriecode@gmail.com>
2017-09-15 11:16:39 +0200

    DP19-JP008 --究極変異態・インセクト女王

---
+++ scripts/DP19-JP/c100419008.lua
@@ -0,0 +1,98 @@
+--究極変異態・インセクト女王
+--Ultimately Mutated Insect Queen
+--Scripted by Eerie Code
+function c100419008.initial_effect(c)
+	c:EnableReviveLimit()
+	--cannot special summon
+	local e1=Effect.CreateEffect(c)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_SPSUMMON_CONDITION)
+	e1:SetValue(c100419008.splimit)
+	c:RegisterEffect(e1)
+	--indes
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetTargetRange(LOCATION_MZONE,0)
+	e2:SetCondition(c100419008.indcon)
+	e2:SetTarget(aux.TargetBoolFunction(Card.IsRace,RACE_INSECT))
+	e2:SetValue(c100419008.indval)
+	c:RegisterEffect(e2)
+	--cannot be target
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
+	e3:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
+	e3:SetRange(LOCATION_SZONE)
+	e3:SetTargetRange(LOCATION_MZONE,0)
+	e3:SetCondition(c100419008.indcon)
+	e3:SetTarget(aux.TargetBoolFunction(Card.IsRace,RACE_INSECT))
+	e3:SetValue(aux.tgoval)
+	c:RegisterEffect(e3)
+	--chain attack
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(100419008,0))
+	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e4:SetCode(EVENT_DAMAGE_STEP_END)
+	e4:SetCondition(c100419008.atcon)
+	e4:SetCost(c100419008.atcost)
+	e4:SetOperation(c100419008.atop)
+	c:RegisterEffect(e4)
+	--spsummon
+	local e5=Effect.CreateEffect(c)
+	e5:SetDescription(aux.Stringid(91512835,0))
+	e5:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOKEN)
+	e5:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e5:SetRange(LOCATION_MZONE)
+	e5:SetCode(EVENT_PHASE+PHASE_END)
+	e5:SetCountLimit(1)
+	e5:SetTarget(c100419008.sptg)
+	e5:SetOperation(c100419008.spop)
+	c:RegisterEffect(e4)
+end
+function c100419008.splimit(e,se,sp,st)
+	return se
+end
+function c100419008.indfilter(c)
+	return c:IsFaceup() and c:IsRace(RACE_INSECT)
+end
+function c100419008.indcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsExistingMatchingCard(c100419008.indfilter,tp,LOCATION_MZONE,0,1,e:GetHandler())
+end
+function c100419008.indval(e,re,rp)
+	return rp~=e:GetHandlerPlayer()
+end
+function c100419008.atcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return Duel.GetAttacker()==c and c:IsChainAttackable(0,true)
+end
+function c100419008.atcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.CheckReleaseGroup(tp,nil,1,e:GetHandler()) nd
+	local g=Duel.SelectReleaseGroup(tp,nil,1,1,e:GetHandler())
+	Duel.Release(g,REASON_COST)
+end
+function c100419008.atop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not c:IsRelateToBattle() then return end
+	Duel.ChainAttack()
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_CANNOT_DIRECT_ATTACK)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e1:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_BATTLE+PHASE_DAMAGE_CAL)
+	c:RegisterEffect(e1)
+end
+function c100419008.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsPlayerCanSpecialSummonMonster(tp,91512836,0,0x4011,100,100,1,RACE_INSECT,ATTRIBUTE_EARTH) end
+	Duel.SetOperationInfo(0,CATEGORY_TOKEN,nil,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,0,0)
+end
+function c100419008.spop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	if not Duel.IsPlayerCanSpecialSummonMonster(tp,91512836,0,0x4011,100,100,1,RACE_INSECT,ATTRIBUTE_EARTH) then return end
+	local token=Duel.CreateToken(tp,91512836)
+	Duel.SpecialSummon(token,0,tp,tp,false,false,POS_FACEUP_ATTACK)
+end
