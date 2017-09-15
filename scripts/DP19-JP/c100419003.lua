7b667bd95d82c3f80c7bc7298b2755bbe771564f
EerieCode <eeriecode@gmail.com>
2017-09-15 20:41:53 +0200

    DP19-JP003 --ミレニアム・アイズ・サクリファイス

---
+++ scripts/DP19-JP/c100419003.lua
@@ -0,0 +1,119 @@
+--ミレニアム・アイズ・サクリファイス
+-- Millennium-Eyes Restrict
+function c100419003.initial_effect(c)
+	--fusion material
+	c:EnableReviveLimit()
+	aux.AddFusionProcCodeFun(c,64631466,aux.FilterBoolFunction(Card.IsFusionType,TYPE_EFFECT),1,true,true)
+	--Equip
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(100419003,0))
+	e1:SetCategory(CATEGORY_EQUIP)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DAMAGE_CAL)
+	e1:SetType(EFFECT_TYPE_QUICK_O)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCode(EVENT_CHAINING)
+	e1:SetCondition(c100419003.eqcon)
+	e1:SetTarget(c100419003.eqtg)
+	e1:SetOperation(c100419003.eqop)
+	c:RegisterEffect(e1)	
+	--atk
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetCode(EFFECT_UPDATE_ATTACK)
+	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetValue(c100419003.atkval)
+	c:RegisterEffect(e2)
+	-- def
+	local e3=e2:Clone()
+	e3:SetCode(EFFECT_UPDATE_DEFENSE)
+	e3:SetValue(c100419003.defval)
+	c:RegisterEffect(e3)	
+	--disable
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_FIELD)
+	e4:SetCode(EFFECT_DISABLE)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetTargetRange(0,LOCATION_MZONE)
+	e4:SetTarget(c100419003.distg)
+	c:RegisterEffect(e4)
+	--atk limit
+	local e5=Effect.CreateEffect(c)
+	e5:SetType(EFFECT_TYPE_FIELD)
+	e5:SetCode(EFFECT_CANNOT_ATTACK)
+	e5:SetRange(LOCATION_MZONE)
+	e5:SetTargetRange(0,LOCATION_MZONE)
+	e5:SetTarget(c100419003.distg)
+	c:RegisterEffect(e5)
+end
+function c100419003.CanEquipMonster(c)
+	return true
+end
+function c100419003.eqcon(e,tp,eg,ep,ev,re,r,rp,chk)
+	local ph=Duel.GetCurrentPhase()
+	return not e:GetHandler():IsStatus(STATUS_BATTLE_DESTROYED) and ep~=tp
+		and re:IsActiveType(TYPE_MONSTER)
+end
+function c100419003.eqtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) and chkc:IsAbleToChangeControler() end
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_SZONE)>0
+		and Duel.IsExistingTarget(Card.IsAbleToChangeControler,tp,0,LOCATION_MZONE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)
+	local g=Duel.SelectTarget(tp,Card.IsAbleToChangeControler,tp,0,LOCATION_MZONE,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_EQUIP,g,1,0,0)
+end
+function c100419003.eqlimit(e,c)
+	return e:GetOwner()==c
+end
+function c100419003.EquipMonster(c,tp,tc)
+	if not Duel.Equip(tp,tc,c,false) then return end
+	--Add Equip limit
+	tc:RegisterFlagEffect(100419003,RESET_EVENT+0x1fe0000,0,0)
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_OWNER_RELATE)
+	e1:SetCode(EFFECT_EQUIP_LIMIT)
+	e1:SetReset(RESET_EVENT+0x1fe0000)
+	e1:SetValue(c100419003.eqlimit)
+	tc:RegisterEffect(e1)		
+end
+function c100419003.eqop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) and tc:IsType(TYPE_MONSTER) and tc:IsControler(1-tp) then
+		if c:IsFaceup() and c:IsRelateToEffect(e) then
+			c100419003.EquipMonster(c,tp,tc)
+		else Duel.SendtoGrave(tc,REASON_EFFECT) end
+	end
+end	
+function c100419003.atkval(e,c)
+	local atk=0
+	local g=c:GetEquipGroup()
+	local tc=g:GetFirst()
+	while tc do
+		if tc:GetFlagEffect(100419003)~=0 and tc:IsFaceup() and tc:GetAttack()>=0 then
+			atk=atk+tc:GetAttack()
+		end
+		tc=g:GetNext()
+	end
+	return atk
+end
+function c100419003.defval(e,c)
+	local atk=0
+	local g=c:GetEquipGroup()
+	local tc=g:GetFirst()
+	while tc do
+		if tc:GetFlagEffect(100419003)~=0 and tc:IsFaceup() and tc:GetDefense()>=0 then
+			atk=atk+tc:GetDefense()
+		end
+		tc=g:GetNext()
+	end
+	return atk
+end
+function c100419003.disfilter(c,code)
+	return c:IsFaceup() and c:GetFlagEffect(100419003)~=0 and c:IsCode(code)
+end
+function c100419003.distg(e,c)
+	local g=e:GetHandler():GetEquipGroup():Filter(c100419003.disfilter,nil,c:GetCode())
+	return c:IsFaceUp() and g:GetCount()>0
+end
