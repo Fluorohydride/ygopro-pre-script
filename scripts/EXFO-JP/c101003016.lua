15ce1bb63e6d17057c21d3b6e18f8afa8d7b76b6
EerieCode <eeriecode@gmail.com>
2017-09-15 19:36:05 +0200

    EXFO-JP016 --燈影の機界騎士

---
+++ scripts/EXFO-JP/c101003016.lua
@@ -0,0 +1,79 @@
+--燈影の機界騎士
+--Mekk-Knight of the Flickering Flame
+--Scripted by Eerie Code
+function c101003016.initial_effect(c)
+	--special summon
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_SPSUMMON_PROC)
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCountLimit(1,101003016)
+	e1:SetCondition(c101003016.hspcon)
+	e1:SetValue(c101003016.hspval)
+	c:RegisterEffect(e1)
+	--spsummon
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(34325937,0))
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_LEAVE_FIELD)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_DAMAGE_STEP)
+	e2:SetCondition(c101003016.spcon)
+	e2:SetTarget(c101003016.sptg)
+	e2:SetOperation(c101003016.spop)
+	c:RegisterEffect(e2)
+end
+function c101003016.cfilter(c,tp,seq)
+	local s=c:GetSequence()
+	if c:IsLocation(LOCATION_SZONE) and s==5 then return false end
+	if c:IsControler(tp) then
+		return s==seq or (seq==1 and s==5) or (seq==3 and s==6)		
+	else
+		return s==4-seq or (seq==1 and s==6) or (seq==3 and s==5)
+	end
+end
+function c101003016.hspcon(e,c)
+	if c==nil then return true end
+	local tp=c:GetControler()
+	local zone=0
+	for i=0,4 do
+		if Duel.GetMatchingGroupCount(c101003016.cfilter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,nil,tp,i)>=2 then
+			zone=zone+math.pow(2,i)
+		end
+	end
+	return Duel.GetLocationCount(tp,LOCATION_MZONE,tp,LOCATION_REASON_TOFIELD,zone)>0
+end
+function c101003016.hspval(e,c)
+	local zone=0
+	for i=0,4 do
+		if Duel.GetMatchingGroupCount(c101003016.cfilter,c:GetControler(),LOCATION_ONFIELD,LOCATION_ONFIELD,nil,tp,i)>=2 then
+			zone=zone+math.pow(2,i)
+		end
+	end
+	return 0,zone
+end
+function c101003016.spcfilter(c,tp,seq)
+	local s=c:GetPreviousSequence()
+	return c:GetPreviousControler()~=tp and (s==4-seq or (seq==1 and s==6) or (seq==3 and s==5))
+end
+function c101003016.spcon(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(c101003016.spcfilter,1,nil,tp,e:GetHandler():GetSequence())
+end
+function c101003016.filter(c,e,tp)
+	return c:IsSetCard(0x20c) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function c101003016.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(c101003016.filter,tp,LOCATION_HAND,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
+end
+function c101003016.spop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,c101003016.filter,tp,LOCATION_HAND,0,1,1,nil,e,tp)
+	if g:GetCount()>0 then
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
