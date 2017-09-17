45806deb8eb8e26ac0a71ab92ef7886e70d9ec88
EerieCode <eeriecode@gmail.com>
2017-09-17 06:59:09 +0200

    ID update for DP19

--- scripts/DP19-JP/c100407003.lua
+++ scripts/DP19-JP/c100407003.lua
@@ -1,21 +1,21 @@
 --ミレニアム・アイズ・サクリファイス
 --Millennium-Eyes Restrict
-function c100419003.initial_effect(c)
+function c100407003.initial_effect(c)
 	--fusion material
 	c:EnableReviveLimit()
 	aux.AddFusionProcCodeFun(c,64631466,aux.FilterBoolFunction(Card.IsFusionType,TYPE_EFFECT),1,true,true)
 	--Equip
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(100419003,0))
+	e1:SetDescription(aux.Stringid(100407003,0))
 	e1:SetCategory(CATEGORY_EQUIP)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetType(EFFECT_TYPE_QUICK_O)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetCode(EVENT_CHAINING)
 	e1:SetCountLimit(1)
-	e1:SetCondition(c100419003.eqcon)
-	e1:SetTarget(c100419003.eqtg)
-	e1:SetOperation(c100419003.eqop)
+	e1:SetCondition(c100407003.eqcon)
+	e1:SetTarget(c100407003.eqtg)
+	e1:SetOperation(c100407003.eqop)
 	c:RegisterEffect(e1)
 	--atk
 	local e2=Effect.CreateEffect(c)
@@ -23,12 +23,12 @@ function c100419003.initial_effect(c)
 	e2:SetCode(EFFECT_UPDATE_ATTACK)
 	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
 	e2:SetRange(LOCATION_MZONE)
-	e2:SetValue(c100419003.atkval)
+	e2:SetValue(c100407003.atkval)
 	c:RegisterEffect(e2)
 	-- def
 	local e3=e2:Clone()
 	e3:SetCode(EFFECT_UPDATE_DEFENSE)
-	e3:SetValue(c100419003.defval)
+	e3:SetValue(c100407003.defval)
 	c:RegisterEffect(e3)
 	--disable
 	local e4=Effect.CreateEffect(c)
@@ -36,7 +36,7 @@ function c100419003.initial_effect(c)
 	e4:SetCode(EFFECT_DISABLE)
 	e4:SetRange(LOCATION_MZONE)
 	e4:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
-	e4:SetTarget(c100419003.distg)
+	e4:SetTarget(c100407003.distg)
 	c:RegisterEffect(e4)
 	--atk limit
 	local e5=Effect.CreateEffect(c)
@@ -44,78 +44,78 @@ function c100419003.initial_effect(c)
 	e5:SetCode(EFFECT_CANNOT_ATTACK)
 	e5:SetRange(LOCATION_MZONE)
 	e5:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
-	e5:SetTarget(c100419003.distg)
+	e5:SetTarget(c100407003.distg)
 	c:RegisterEffect(e5)
 end
-function c100419003.CanEquipMonster(c)
+function c100407003.CanEquipMonster(c)
 	return true
 end
-function c100419003.eqcon(e,tp,eg,ep,ev,re,r,rp,chk)
+function c100407003.eqcon(e,tp,eg,ep,ev,re,r,rp,chk)
 	return ep~=tp and re:IsActiveType(TYPE_MONSTER)
 end
-function c100419003.eqfilter(c)
+function c100407003.eqfilter(c)
 	return (c:IsFaceup() or c:IsLocation(LOCATION_GRAVE)) and c:IsType(TYPE_EFFECT) and c:IsAbleToChangeControler()
 end
-function c100419003.eqtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_MZONE+LOCATION_GRAVE) and chkc:IsControler(1-tp) and c100419003.eqfilter(chkc) end
+function c100407003.eqtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE+LOCATION_GRAVE) and chkc:IsControler(1-tp) and c100407003.eqfilter(chkc) end
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_SZONE)>0
-		and Duel.IsExistingTarget(c100419003.eqfilter,tp,0,LOCATION_MZONE+LOCATION_GRAVE,1,nil) end
+		and Duel.IsExistingTarget(c100407003.eqfilter,tp,0,LOCATION_MZONE+LOCATION_GRAVE,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)
-	local g=Duel.SelectTarget(tp,c100419003.eqfilter,tp,0,LOCATION_MZONE+LOCATION_GRAVE,1,1,nil)
+	local g=Duel.SelectTarget(tp,c100407003.eqfilter,tp,0,LOCATION_MZONE+LOCATION_GRAVE,1,1,nil)
 	Duel.SetOperationInfo(0,CATEGORY_EQUIP,g,1,0,0)
 end
-function c100419003.eqlimit(e,c)
+function c100407003.eqlimit(e,c)
 	return e:GetOwner()==c
 end
-function c100419003.EquipMonster(c,tp,tc)
+function c100407003.EquipMonster(c,tp,tc)
 	if not Duel.Equip(tp,tc,c,false) then return end
 	--Add Equip limit
-	tc:RegisterFlagEffect(100419003,RESET_EVENT+0x1fe0000,0,0)
+	tc:RegisterFlagEffect(100407003,RESET_EVENT+0x1fe0000,0,0)
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetProperty(EFFECT_FLAG_OWNER_RELATE)
 	e1:SetCode(EFFECT_EQUIP_LIMIT)
 	e1:SetReset(RESET_EVENT+0x1fe0000)
-	e1:SetValue(c100419003.eqlimit)
+	e1:SetValue(c100407003.eqlimit)
 	tc:RegisterEffect(e1)
 end
-function c100419003.eqop(e,tp,eg,ep,ev,re,r,rp)
+function c100407003.eqop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local tc=Duel.GetFirstTarget()
 	if tc:IsFaceup() and tc:IsRelateToEffect(e) and tc:IsType(TYPE_MONSTER) and tc:IsControler(1-tp) then
 		if c:IsFaceup() and c:IsRelateToEffect(e) then
-			c100419003.EquipMonster(c,tp,tc)
+			c100407003.EquipMonster(c,tp,tc)
 		else Duel.SendtoGrave(tc,REASON_EFFECT) end
 	end
 end
-function c100419003.atkval(e,c)
+function c100407003.atkval(e,c)
 	local atk=0
 	local g=c:GetEquipGroup()
 	local tc=g:GetFirst()
 	while tc do
-		if tc:GetFlagEffect(100419003)~=0 and tc:IsFaceup() and tc:GetAttack()>=0 then
+		if tc:GetFlagEffect(100407003)~=0 and tc:IsFaceup() and tc:GetAttack()>=0 then
 			atk=atk+tc:GetAttack()
 		end
 		tc=g:GetNext()
 	end
 	return atk
 end
-function c100419003.defval(e,c)
+function c100407003.defval(e,c)
 	local atk=0
 	local g=c:GetEquipGroup()
 	local tc=g:GetFirst()
 	while tc do
-		if tc:GetFlagEffect(100419003)~=0 and tc:IsFaceup() and tc:GetDefense()>=0 then
+		if tc:GetFlagEffect(100407003)~=0 and tc:IsFaceup() and tc:GetDefense()>=0 then
 			atk=atk+tc:GetDefense()
 		end
 		tc=g:GetNext()
 	end
 	return atk
 end
-function c100419003.disfilter(c)
-	return c:IsFaceup() and c:GetFlagEffect(100419003)~=0
+function c100407003.disfilter(c)
+	return c:IsFaceup() and c:GetFlagEffect(100407003)~=0
 end
-function c100419003.distg(e,c)
-	local g=e:GetHandler():GetEquipGroup():Filter(c100419003.disfilter,nil)
+function c100407003.distg(e,c)
+	local g=e:GetHandler():GetEquipGroup():Filter(c100407003.disfilter,nil)
 	return c:IsFaceup() and g:IsExists(Card.IsCode,1,nil,c:GetCode())
 end
--- scripts/DP19-JP/c100407014.lua
+++ scripts/DP19-JP/c100407014.lua
@@ -1,80 +1,80 @@
 --BM-4 ボムスパイダー
 --BM-4 Bomb Spider
 --Scripted by Eerie Code
-function c100419014.initial_effect(c)
+function c100407014.initial_effect(c)
 	--Destroy
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(100419014,0))
+	e1:SetDescription(aux.Stringid(100407014,0))
 	e1:SetCategory(CATEGORY_DESTROY)
 	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetCountLimit(1)
-	e1:SetTarget(c100419014.destg)
-	e1:SetOperation(c100419014.desop)
+	e1:SetTarget(c100407014.destg)
+	e1:SetOperation(c100407014.desop)
 	c:RegisterEffect(e1)
 	--damage
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(100419014,0))
+	e2:SetDescription(aux.Stringid(100407014,0))
 	e2:SetCategory(CATEGORY_DAMAGE)
 	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
 	e2:SetCode(EVENT_BATTLE_DESTROYED)
 	e2:SetRange(LOCATION_MZONE)
-	e2:SetCountLimit(1,100419014)
-	e2:SetCondition(c100419014.damcon1)
-	e2:SetTarget(c100419014.damtg)
-	e2:SetOperation(c100419014.damop)
+	e2:SetCountLimit(1,100407014)
+	e2:SetCondition(c100407014.damcon1)
+	e2:SetTarget(c100407014.damtg)
+	e2:SetOperation(c100407014.damop)
 	c:RegisterEffect(e2)
 	local e3=e2:Clone()
 	e3:SetCode(EVENT_TO_GRAVE)
-	e3:SetCondition(c100419014.damcon2)
+	e3:SetCondition(c100407014.damcon2)
 	c:RegisterEffect(e3)
 end
-function c100419014.desfilter(c)
+function c100407014.desfilter(c)
 	return c:IsFaceup() and c:IsRace(RACE_MACHINE) and c:IsAttribute(ATTRIBUTE_DARK)
 end
-function c100419014.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+function c100407014.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return false end
-	if chk==0 then return Duel.IsExistingTarget(c100419014.desfilter,tp,LOCATION_MZONE,0,1,nil)
+	if chk==0 then return Duel.IsExistingTarget(c100407014.desfilter,tp,LOCATION_MZONE,0,1,nil)
 		and Duel.IsExistingTarget(Card.IsFaceup,tp,0,LOCATION_ONFIELD,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
-	local g1=Duel.SelectTarget(tp,c100419014.desfilter,tp,LOCATION_MZONE,0,1,1,nil)
+	local g1=Duel.SelectTarget(tp,c100407014.desfilter,tp,LOCATION_MZONE,0,1,1,nil)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
 	local g2=Duel.SelectTarget(tp,Card.IsFaceup,tp,0,LOCATION_ONFIELD,1,1,nil)
 	g1:Merge(g2)
 	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g1,2,0,0)
 end
-function c100419014.desop(e,tp,eg,ep,ev,re,r,rp)
+function c100407014.desop(e,tp,eg,ep,ev,re,r,rp)
 	local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS)
 	local tg=g:Filter(Card.IsRelateToEffect,nil,e)
 	if tg:GetCount()>0 then
 		Duel.Destroy(tg,REASON_EFFECT)
 	end
 end
-function c100419014.damcon1(e,tp,eg,ep,ev,re,r,rp)
+function c100407014.damcon1(e,tp,eg,ep,ev,re,r,rp)
 	local tc=eg:GetFirst()
 	local bc=tc:GetBattleTarget()
 	return tc:GetPreviousControler()~=tp and tc:IsLocation(LOCATION_GRAVE)
 		and bc:IsControler(tp) and bc:GetOriginalAttribute()==ATTRIBUTE_DARK and bc:GetOriginalRace()==RACE_MACHINE
 end
-function c100419014.damfilter1(c,tp)
+function c100407014.damfilter1(c,tp)
 	return c:IsReason(REASON_EFFECT) and c:IsReason(REASON_DESTROY) and c:IsLocation(LOCATION_GRAVE) and c:GetPreviousControler()~=tp
 end
-function c100419014.damcon2(e,tp,eg,ep,ev,re,r,rp)
+function c100407014.damcon2(e,tp,eg,ep,ev,re,r,rp)
 	local rc=re:GetHandler()
 	return rc:IsControler(tp) and rc:GetOriginalAttribute()==ATTRIBUTE_DARK
 		and rc:GetOriginalRace()==RACE_MACHINE
-		and eg:IsExists(c100419014.damfilter1,1,nil,tp)
+		and eg:IsExists(c100407014.damfilter1,1,nil,tp)
 end
-function c100419014.damtg(e,tp,eg,ep,ev,re,r,rp,chk)
+function c100407014.damtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
 	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,0)
 end
-function c100419014.damfilter2(c,tp)
+function c100407014.damfilter2(c,tp)
 	return c:IsReason(REASON_BATTLE+REASON_EFFECT) and c:IsReason(REASON_DESTROY) and c:IsLocation(LOCATION_GRAVE) and c:GetPreviousControler()~=tp
 end
-function c100419014.damop(e,tp,eg,ep,ev,re,r,rp)
-	local g=eg:Filter(c100419014.damfilter2,nil,tp)
+function c100407014.damop(e,tp,eg,ep,ev,re,r,rp)
+	local g=eg:Filter(c100407014.damfilter2,nil,tp)
 	if g:GetCount()>0 then
 		if g:GetCount()>1 then
 			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
