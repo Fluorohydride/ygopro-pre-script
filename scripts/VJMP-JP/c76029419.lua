fe2e3965985ba9c7c5f98a17c0ebfbedd59c8674
nekrozar <nekrozar.s@gmail.com>
2017-09-22 07:40:59 +0900

    change ID

--- scripts/VJMP-JP/c76029419.lua
+++ scripts/VJMP-JP/c76029419.lua
@@ -1,26 +1,26 @@
 --DDD超視王ゼロ・マクスウェル
 --D/D/D Supersight King Zero Maxwell
 --Scripted by Eerie Code
-function c100200135.initial_effect(c)
+function c76029419.initial_effect(c)
 	--pendulum summon
 	aux.EnablePendulumAttribute(c)
 	--defdown
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(100200135,0))
+	e1:SetDescription(aux.Stringid(76029419,0))
 	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetRange(LOCATION_PZONE)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
-	e1:SetCountLimit(1,100200135)
-	e1:SetTarget(c100200135.deftg)
-	e1:SetOperation(c100200135.defop)
+	e1:SetCountLimit(1,76029419)
+	e1:SetTarget(c76029419.deftg)
+	e1:SetOperation(c76029419.defop)
 	c:RegisterEffect(e1)
 	--defdown
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(100200135,1))
+	e2:SetDescription(aux.Stringid(76029419,1))
 	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e2:SetCode(EVENT_BATTLE_CONFIRM)
-	e2:SetCondition(c100200135.defcon2)
-	e2:SetOperation(c100200135.defop2)
+	e2:SetCondition(c76029419.defcon2)
+	e2:SetOperation(c76029419.defop2)
 	c:RegisterEffect(e2)
 	--pierce
 	local e3=Effect.CreateEffect(c)
@@ -34,13 +34,13 @@ function c100200135.initial_effect(c)
 	e4:SetValue(1)
 	c:RegisterEffect(e4)
 end
-function c100200135.deftg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+function c76029419.deftg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_MZONE) and aux.nzdef(chkc) end
 	if chk==0 then return Duel.IsExistingTarget(aux.nzdef,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
 	Duel.SelectTarget(tp,aux.nzdef,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
 end
-function c100200135.defop(e,tp,eg,ep,ev,re,r,rp)
+function c76029419.defop(e,tp,eg,ep,ev,re,r,rp)
 	if not e:GetHandler():IsRelateToEffect(e) then return end
 	local tc=Duel.GetFirstTarget()
 	if tc:IsFaceup() and tc:IsRelateToEffect(e) then
@@ -52,12 +52,12 @@ function c100200135.defop(e,tp,eg,ep,ev,re,r,rp)
 		tc:RegisterEffect(e1)
 	end
 end
-function c100200135.defcon2(e,tp,eg,ep,ev,re,r,rp)
+function c76029419.defcon2(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetAttackTarget()
 	e:SetLabelObject(tc)
 	return Duel.GetAttacker()==e:GetHandler() and tc and tc:IsPosition(POS_FACEUP_DEFENSE) and tc:GetDefense()>0
 end
-function c100200135.defop2(e,tp,eg,ep,ev,re,r,rp)
+function c76029419.defop2(e,tp,eg,ep,ev,re,r,rp)
 	local tc=e:GetLabelObject()
 	if tc:IsFaceup() and tc:IsRelateToBattle() then
 		local e1=Effect.CreateEffect(e:GetHandler())
