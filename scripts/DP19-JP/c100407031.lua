3d2083d02d4baf19d285860f9ba602495cbc5a62
nekrozar <nekrozar.s@gmail.com>
2017-09-27 16:03:02 +0900

    fix

--- scripts/DP19-JP/c100407031.lua
+++ scripts/DP19-JP/c100407031.lua
@@ -7,6 +7,7 @@ function c100407031.initial_effect(c)
 	--multi attack
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
 	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
 	e1:SetCondition(c100407031.condition)
 	e1:SetOperation(c100407031.operation)
@@ -29,14 +30,14 @@ function c100407031.initial_effect(c)
 	c:RegisterEffect(e3)
 	--special summon
 	local e4=Effect.CreateEffect(c)
-	e4:SetDescription(aux.Stringid(4591250,0))
+	e4:SetDescription(aux.Stringid(100407031,0))
 	e4:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e4:SetCode(EVENT_LEAVE_FIELD)
 	e4:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
-	e4:SetCondition(c100407031.espcon)
-	e4:SetTarget(c100407031.esptg)
-	e4:SetOperation(c100407031.espop)
+	e4:SetCondition(c100407031.spcon)
+	e4:SetTarget(c100407031.sptg)
+	e4:SetOperation(c100407031.spop)
 	c:RegisterEffect(e4)
 end
 function c100407031.valcheck(e,c)
@@ -50,9 +51,11 @@ function c100407031.operation(e,tp,eg,ep,ev,re,r,rp)
 	local ct=e:GetLabel()
 	if ct>=2 then
 		local e1=Effect.CreateEffect(c)
+		e1:SetDescription(aux.Stringid(100407031,1))
 		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetProperty(EFFECT_FLAG_CLIENT_HINT)
 		e1:SetCode(EFFECT_EXTRA_ATTACK)
-		e1:SetValue(ct-1)		
+		e1:SetValue(ct-1)
 		e1:SetReset(RESET_EVENT+0x1fe0000)
 		c:RegisterEffect(e1)
 	end
@@ -63,23 +66,23 @@ end
 function c100407031.actcon(e)
 	return Duel.GetAttacker()==e:GetHandler()
 end
-function c100407031.espcon(e,tp,eg,ep,ev,re,r,rp)
+function c100407031.spcon(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	return c:IsSummonType(SUMMON_TYPE_FUSION)	and c:GetReasonPlayer()~=tp 
-		and c:IsReason(REASON_EFFECT)	and c:IsPreviousPosition(POS_FACEUP)
+	return c:IsSummonType(SUMMON_TYPE_FUSION) and c:GetReasonPlayer()~=tp
+		and c:IsReason(REASON_EFFECT) and c:IsPreviousPosition(POS_FACEUP)
 end
-function c100407031.espfilter(c,e,tp)
+function c100407031.spfilter(c,e,tp)
 	return c:IsCode(12652643) and c:IsCanBeSpecialSummoned(e,0,tp,true,false)
 end
-function c100407031.esptg(e,tp,eg,ep,ev,re,r,rp,chk)
+function c100407031.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCountFromEx(tp)>0
-		and Duel.IsExistingMatchingCard(c100407031.espfilter,tp,LOCATION_EXTRA,0,1,nil,e,tp) end
+		and Duel.IsExistingMatchingCard(c100407031.spfilter,tp,LOCATION_EXTRA,0,1,nil,e,tp) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
 end
-function c100407031.espop(e,tp,eg,ep,ev,re,r,rp)
+function c100407031.spop(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCountFromEx(tp)<=0 then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectMatchingCard(tp,c100407031.espfilter,tp,LOCATION_EXTRA,0,1,1,nil,e,tp)
+	local g=Duel.SelectMatchingCard(tp,c100407031.spfilter,tp,LOCATION_EXTRA,0,1,1,nil,e,tp)
 	if g:GetCount()>0 then
 		Duel.SpecialSummon(g,0,tp,tp,true,false,POS_FACEUP)
 	end
