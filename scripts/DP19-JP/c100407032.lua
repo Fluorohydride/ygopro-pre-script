24c04ccbbaa12619b7e60d86609c70e55fc7e41b
edo9300 <edoardo762@gmail.com>
2017-09-30 14:12:39 +0200

    Updated Ancient Gear Fusion to use Auxiliary.FCheckAdditional (#143)
    * Updated Ancient Gear Fusion to use Auxiliary.FCheckAdditional
    
    * Update c100407032.lua
    
    * Update c100407032.lua
    
    * Last

--- scripts/DP19-JP/c100407032.lua
+++ scripts/DP19-JP/c100407032.lua
@@ -11,38 +11,42 @@ function c100407032.initial_effect(c)
 	e1:SetOperation(c100407032.activate)
 	c:RegisterEffect(e1)
 end
+function c100407032.fcheck(tp,sg,fc,mg)
+	if sg:IsExists(Card.IsLocation,1,nil,LOCATION_DECK) then
+		return sg:IsExists(c100407032.filterchk,1,nil) end
+	return true
+ end
+function c100407032.filterchk(c)
+	return c:IsFaceup() and c:IsCode(83104731,95735217) and c:IsOnField()
+end
 function c100407032.filter0(c)
 	return c:IsType(TYPE_MONSTER) and c:IsCanBeFusionMaterial() and c:IsAbleToGrave()
 end
 function c100407032.filter1(c,e)
 	return not c:IsImmuneToEffect(e)
 end
-function c100407032.filter2(c,e,tp,m,f,chkf,agc)
+function c100407032.filter2(c,e,tp,m,f,chkf)
 	return c:IsType(TYPE_FUSION) and c:IsSetCard(0x7) and (not f or f(c))
-		and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_FUSION,tp,false,false) and c:CheckFusionMaterial(m,agc,chkf)
-end
-function c100407032.agfilter(c,e,tp,m,m2,f,chkf)
-	if c:IsFacedown() or not c:IsCode(83104731,95735217) or not m:IsContains(c) then return false end
-	local g=m:Clone()
-	g:Merge(m2)
-	g:RemoveCard(c)
-	return Duel.IsExistingMatchingCard(c100407032.filter2,tp,LOCATION_EXTRA,0,1,nil,e,tp,g,f,chkf,c)
+		and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_FUSION,tp,false,false) and c:CheckFusionMaterial(m,nil,chkf)
 end
 function c100407032.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then
 		local chkf=tp
 		local mg=Duel.GetFusionMaterial(tp)
-		local res=Duel.IsExistingMatchingCard(c100407032.filter2,tp,LOCATION_EXTRA,0,1,nil,e,tp,mg,nil,chkf,nil)
-		if res then return true end
 		local mg2=Duel.GetMatchingGroup(c100407032.filter0,tp,LOCATION_DECK,0,nil)
-		res=Duel.IsExistingMatchingCard(c100407032.agfilter,tp,LOCATION_MZONE,0,1,nil,e,tp,mg,mg2,nil,chkf)
+		if mg2:GetCount()>0 then
+			mg:Merge(mg2)
+			aux.FCheckAdditional=c100407032.fcheck
+		end
+		local res=Duel.IsExistingMatchingCard(c100407032.filter2,tp,LOCATION_EXTRA,0,1,nil,e,tp,mg,nil,chkf)
+		aux.FCheckAdditional=nil
 		if not res then
 			local ce=Duel.GetChainMaterial(tp)
 			if ce~=nil then
 				local fgroup=ce:GetTarget()
 				local mg3=fgroup(ce,e,tp)
 				local mf=ce:GetValue()
-				res=Duel.IsExistingMatchingCard(c100407032.filter2,tp,LOCATION_EXTRA,0,1,nil,e,tp,mg3,mf,chkf,nil)
+				res=Duel.IsExistingMatchingCard(c100407032.filter2,tp,LOCATION_EXTRA,0,1,nil,e,tp,mg3,mf,chkf)
 			end
 		end
 		return res
@@ -51,57 +55,39 @@ end
 function c100407032.activate(e,tp,eg,ep,ev,re,r,rp)
 	local chkf=tp
 	local mg1=Duel.GetFusionMaterial(tp):Filter(c100407032.filter1,nil,e)
-	local sg1=Duel.GetMatchingGroup(c100407032.filter2,tp,LOCATION_EXTRA,0,nil,e,tp,mg1,nil,chkf,nil)
 	local mg2=Duel.GetMatchingGroup(c100407032.filter0,tp,LOCATION_DECK,0,nil)
-	local sg2=Duel.GetMatchingGroup(c100407032.agfilter,tp,LOCATION_MZONE,0,nil,e,tp,mg1,mg2,nil,chkf)
-	local mg3=nil
-	local sg3=nil
+	if mg2:GetCount()>0 then
+		mg1:Merge(mg2)
+		aux.FCheckAdditional=c100407032.fcheck
+	end
+	local sg1=Duel.GetMatchingGroup(c100407032.filter2,tp,LOCATION_EXTRA,0,nil,e,tp,mg1,nil,chkf)
+	aux.FCheckAdditional=nil
+	local mg2=nil
+	local sg2=nil
 	local ce=Duel.GetChainMaterial(tp)
 	if ce~=nil then
 		local fgroup=ce:GetTarget()
-		mg3=fgroup(ce,e,tp)
+		mg2=fgroup(ce,e,tp)
 		local mf=ce:GetValue()
-		sg3=Duel.GetMatchingGroup(c100407032.filter2,tp,LOCATION_EXTRA,0,nil,e,tp,mg3,mf,chkf)
+		sg2=Duel.GetMatchingGroup(c100407032.filter2,tp,LOCATION_EXTRA,0,nil,e,tp,mg2,mf,chkf)
 	end
-	if sg2:GetCount()>0 and (sg1:GetCount()==0 or Duel.SelectYesNo(tp,aux.Stringid(100407032,0)) then
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FMATERIAL)
-		local agc=sg2:Select(tp,1,1,nil):GetFirst()
-		mg1:Merge(mg2)
-		mg1:RemoveCard(agc)
-		local sg0=Duel.GetMatchingGroup(c100407032.filter2,tp,LOCATION_EXTRA,0,nil,e,tp,mg1,nil,chkf,agc)
-		local sg=sg0:Clone()
-		if sg3 then sg:Merge(sg3) end
-		local tg=sg:Select(tp,1,1,nil)
-		local tc=tg:GetFirst()
-		if sg0:IsContains(tc) and (sg3==nil or not sg3:IsContains(tc) or not Duel.SelectYesNo(tp,ce:GetDescription())) then
-			local mat1=Duel.SelectFusionMaterial(tp,tc,mg1,agc,chkf)
-			tc:SetMaterial(mat1)
-			Duel.SendtoGrave(mat1,REASON_EFFECT+REASON_MATERIAL+REASON_FUSION)
-			Duel.BreakEffect()
-			Duel.SpecialSummon(tc,SUMMON_TYPE_FUSION,tp,tp,false,false,POS_FACEUP)
-		else
-			local mat2=Duel.SelectFusionMaterial(tp,tc,mg2,agc,chkf)
-			local fop=ce:GetOperation()
-			fop(ce,e,tp,tc,mat2)
-		end
-		tc:CompleteProcedure()
+	local sg=sg1:Clone()
+	if sg2 then sg:Merge(sg2) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local tg=sg:Select(tp,1,1,nil)
+	local tc=tg:GetFirst()
+	if sg1:IsContains(tc) and (sg2==nil or not sg2:IsContains(tc) or not Duel.SelectYesNo(tp,ce:GetDescription())) then
+		aux.FCheckAdditional=c100407032.fcheck
+		local mat1=Duel.SelectFusionMaterial(tp,tc,mg1,nil,chkf)
+		aux.FCheckAdditional=nil
+		tc:SetMaterial(mat1)
+		Duel.SendtoGrave(mat1,REASON_EFFECT+REASON_MATERIAL+REASON_FUSION)
+		Duel.BreakEffect()
+		Duel.SpecialSummon(tc,SUMMON_TYPE_FUSION,tp,tp,false,false,POS_FACEUP)
 	else
-		local sg=sg1:Clone()
-		if sg3 then sg:Merge(sg3) end
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-		local tg=sg:Select(tp,1,1,nil)
-		local tc=tg:GetFirst()
-		if sg1:IsContains(tc) and (sg3==nil or not sg3:IsContains(tc) or not Duel.SelectYesNo(tp,ce:GetDescription())) then
-			local mat1=Duel.SelectFusionMaterial(tp,tc,mg1,nil,chkf)
-			tc:SetMaterial(mat1)
-			Duel.SendtoGrave(mat1,REASON_EFFECT+REASON_MATERIAL+REASON_FUSION)
-			Duel.BreakEffect()
-			Duel.SpecialSummon(tc,SUMMON_TYPE_FUSION,tp,tp,false,false,POS_FACEUP)
-		else
-			local mat2=Duel.SelectFusionMaterial(tp,tc,mg3,nil,chkf)
-			local fop=ce:GetOperation()
-			fop(ce,e,tp,tc,mat2)
-		end
-		tc:CompleteProcedure()
+		local mat2=Duel.SelectFusionMaterial(tp,tc,mg2,nil,chkf)
+		local fop=ce:GetOperation()
+		fop(ce,e,tp,tc,mat2)
 	end
+	tc:CompleteProcedure()
 end
