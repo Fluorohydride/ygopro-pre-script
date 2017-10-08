07bda4da51b5ff4d34f7f83372a80cbec8323de5
EerieCode <eeriecode@gmail.com>
2017-10-08 09:24:02 +0200

    CIBR-EN084 --Vendread Nights

---
+++ scripts/CIBR-EN/c101002084.lua
@@ -0,0 +1,76 @@
+--Vendread Nights
+--Scripted by Eerie Code
+function c101002084.initial_effect(c)
+	--activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e1)
+	--search
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(101002084,0))
+	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_FZONE)
+	e2:SetCountLimit(1,101002084)
+	e2:SetCost(c101002084.thcost)
+	e2:SetTarget(c101002084.thtg)
+	e2:SetOperation(c101002084.thop)
+	c:RegisterEffect(e2)
+	--chain attack
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(101002084,1))
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e3:SetRange(LOCATION_FZONE)
+	e3:SetCode(EVENT_BATTLE_DESTROYING)
+	e3:SetCondition(c101002084.atcon)
+	e3:SetCost(c101002084.atcost)
+	e3:SetOperation(c101002084.atop)
+	c:RegisterEffect(e3)
+end
+function c101002084.thcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,1,e:GetHandler()) end
+	Duel.DiscardHand(tp,Card.IsDiscardable,1,1,REASON_COST+REASON_DISCARD)
+end
+function c101002084.thfilter(c)
+	return c:IsSetCard(0x106) and c:IsType(TYPE_MONSTER) and c:IsAbleToHand()
+end
+function c101002084.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c101002084.thfilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function c101002084.thop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,c101002084.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if g:GetCount()>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end
+function c101002084.atcon(e,tp,eg,ep,ev,re,r,rp)
+	local ec=eg:GetFirst()
+	local bc=ec:GetBattleTarget()
+	e:SetLabelObject(ec)
+	return bc and bc:IsType(TYPE_MONSTER) and ec:IsChainAttackable(2,true) and ec:IsStatus(STATUS_OPPO_BATTLE)
+end
+function c101002084.atcfilter(c)
+	return c:IsType(TYPE_MONSTER) and c:IsSetCard(0x106) and c:IsAbleToRemoveAsCost()
+end
+function c101002084.atcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c101002084.atcfilter,tp,LOCATION_GRAVE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local g=Duel.SelectMatchingCard(tp,c101002084.atcfilter,tp,LOCATION_GRAVE,0,1,1,nil)
+	Duel.Remove(g,POS_FACEUP,REASON_COST)
+end
+function c101002084.atop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local ec=e:GetLabelObject()
+	if not ec or not ec:IsRelateToBattle() then return end
+	Duel.ChainAttack()
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_CANNOT_DIRECT_ATTACK)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e1:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_BATTLE)
+	ec:RegisterEffect(e1)
+end
