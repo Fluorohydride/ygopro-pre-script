381c85a61934b4225c1f85fe06c16a9a8fda5955
EerieCode <eeriecode@gmail.com>
2017-10-10 18:41:56 +0200

    CIBR-EN081 --Samurai Destroyer

---
+++ scripts/CIBR-EN/c101002081.lua
@@ -0,0 +1,89 @@
+--Samurai Destroyer
+--Scripted by Eerie Code
+function c101002081.initial_effect(c)
+	c:EnableReviveLimit()
+	aux.AddSynchroProcedure(c,nil,aux.NonTuner(),1)
+	--actlimit
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetCode(EFFECT_CANNOT_ACTIVATE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetTargetRange(0,1)
+	e1:SetValue(c101002081.aclimit)
+	e1:SetCondition(c101002081.con)
+	c:RegisterEffect(e1)
+	--disable
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e2:SetCode(EVENT_ATTACK_ANNOUNCE)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCondition(c101002081.discon)
+	e2:SetOperation(c101002081.disop)
+	c:RegisterEffect(e2)
+	local e3=e2:Clone()
+	e3:SetCode(EVENT_BE_BATTLE_TARGET)
+	c:RegisterEffect(e3)
+	--special summon
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(101002081,0))
+	e4:SetCategory(CATEGORY_DAMAGE)
+	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e4:SetCode(EVENT_LEAVE_FIELD)
+	e4:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
+	e4:SetCondition(c101002081.spcon)
+	e4:SetTarget(c101002081.sptg)
+	e4:SetOperation(c101002081.spop)
+	c:RegisterEffect(e4)
+end
+function c101002081.aclimit(e,re,tp)
+	return not re:GetHandler():IsImmuneToEffect(e)
+end
+function c101002081.con(e)
+	local c=e:GetHandler()
+	return (Duel.GetAttacker()==c and c:GetBattleTarget()) or c==Duel.GetAttackTarget()
+end
+function c101002081.disop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tc=Duel.GetAttackTarget()
+	if not tc then return end
+	if tc:IsControler(tp) then tc=Duel.GetAttacker() end
+	c:CreateRelation(tc,RESET_EVENT+0x1fe0000)
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_DISABLE)
+	e1:SetCondition(c101002081.discon2)
+	e1:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_BATTLE)
+	tc:RegisterEffect(e1)
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetCode(EFFECT_DISABLE_EFFECT)
+	e2:SetCondition(c101002081.discon2)
+	e2:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_BATTLE)
+	tc:RegisterEffect(e2)
+end
+function c101002081.discon2(e)
+	return e:GetOwner():IsRelateToCard(e:GetHandler())
+end
+function c101002081.spcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return c:GetReasonPlayer()~=tp and c:IsReason(REASON_EFFECT) and c:IsPreviousPosition(POS_FACEUP)
+		and c:GetPreviousControler()==c:GetOwner()
+end
+function c101002081.filter(c,e,tp)
+	return c:IsRace(RACE_MACHINE) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function c101002081.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_GRAVE) and c101002081.filter(chkc,e,tp) end
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingTarget(c101002081.filter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectTarget(tp,c101002081.filter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
+end
+function c101002081.spop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) then
+		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
