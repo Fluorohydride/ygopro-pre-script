20445ebc90f51a337e42e6b630af55e1dec05aa5
EerieCode <eeriecode@gmail.com>
2017-11-14 14:30:04 +0100

    LVP1-JP036 --魔導原典 クロウリー

---
+++ scripts/LVP1-JP/c100223036.lua
@@ -0,0 +1,70 @@
+--魔導原典 クロウリー
+--Crowley the Origin of Prophecy
+--Scripted by Eerie Code
+function c100223036.initial_effect(c)
+	c:EnableReviveLimit()
+	aux.AddLinkProcedure(c,aux.FilterBoolFunction(Card.IsRace,RACE_SPELLCASTER),2,2)
+	--to hand
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(100223036,0))
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e1:SetType(EFFECT_TYPE_TRIGGER_O+EFFECT_TYPE_SINGLE)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetCountLimit(1,100223036)
+	e1:SetCondition(c100223036.thcon)
+	e1:SetTarget(c100223036.thtg)
+	e1:SetOperation(c100223036.thop)
+	c:RegisterEffect(e1)
+	--decrease tribute
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(100223036,1))
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_SUMMON_PROC)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetTargetRange(LOCATION_HAND,0)
+	e2:SetCountLimit(1)
+	e2:SetCondition(c100223036.ntcon)
+	e2:SetTarget(c100223036.nttg)
+	c:RegisterEffect(e2)
+end
+function c100223036.thcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsSummonType(SUMMON_TYPE_LINK)
+end
+function c100223036.thfilter(c)
+	return c:IsSetCard(0x106e) and c:IsAbleToHand()
+end
+function c100223036.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then
+		local dg=Duel.GetMatchingGroup(c100223036.thfilter,tp,LOCATION_DECK,0,nil)
+		return dg:GetClassCount(Card.GetCode)>=3
+	end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function c100223036.thop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(c100223036.thfilter,tp,LOCATION_DECK,0,nil)
+	if g:GetClassCount(Card.GetCode)>=3 then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CONFIRM)
+		local sg1=g:Select(tp,1,1,nil)
+		g:Remove(Card.IsCode,nil,sg1:GetFirst():GetCode())
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CONFIRM)
+		local sg2=g:Select(tp,1,1,nil)
+		g:Remove(Card.IsCode,nil,sg2:GetFirst():GetCode())
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CONFIRM)
+		local sg3=g:Select(tp,1,1,nil)
+		sg1:Merge(sg2)
+		sg1:Merge(sg3)
+		Duel.ConfirmCards(1-tp,sg1)
+		Duel.Hint(HINT_SELECTMSG,1-tp,HINTMSG_ATOHAND)
+		local cg=sg1:Select(1-tp,1,1,nil)
+		local tc=cg:GetFirst()
+		Duel.SendtoHand(tc,nil,REASON_EFFECT)
+		Duel.ShuffleDeck(tp)
+	end
+end
+function c100223036.ntcon(e,c,minc)
+	if c==nil then return true end
+	return minc==0 and Duel.GetLocationCount(c:GetControler(),LOCATION_MZONE)>0
+end
+function c100223036.nttg(e,c)
+	return c:IsLevelAbove(5) and c:IsRace(RACE_SPELLCASTER)
+end
