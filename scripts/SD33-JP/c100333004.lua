dccdd7f1667e1c8b8f84941368e4675b87c48141
EerieCode <eeriecode@gmail.com>
2017-11-14 14:35:52 +0100

    SD33-JP004 --フレイム・バッファロー

---
+++ scripts/SD33-JP/c100333004.lua
@@ -0,0 +1,33 @@
+--フレイム・バッファロー
+--Flame Buffalo
+function c100333004.initial_effect(c)
+	--destroy
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(100333004,0))
+	e1:SetCategory(CATEGORY_DESTROY)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e1:SetCode(EVENT_LEAVE_FIELD)
+	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
+	e1:SetCountLimit(1,100333004)
+	e1:SetCondition(c100333004.condition)
+	e1:SetTarget(c100333004.target)
+	e1:SetOperation(c100333004.operation)
+	c:RegisterEffect(e1)
+end
+function c100333004.condition(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsPreviousPosition(POS_FACEUP) and not e:GetHandler():IsLocation(LOCATION_DECK)
+end
+function c100333004.tgfilter(c)
+	return c:IsType(TYPE_MONSTER) and c:IsRace(RACE_CYBERSE) and c:IsDiscardable()
+end
+function c100333004.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c100333004.tgfilter,tp,LOCATION_HAND,0,1,nil)
+		and Duel.IsPlayerCanDraw(tp,2) end
+	Duel.SetOperationInfo(0,CATEGORY_HANDES,nil,0,tp,1)
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,2)
+end
+function c100333004.operation(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.DiscardHand(tp,c100333004.tgfilter,1,1,REASON_EFFECT+REASON_DISCARD)~=0 then
+		Duel.Draw(tp,2,REASON_EFFECT)
+	end
+end
