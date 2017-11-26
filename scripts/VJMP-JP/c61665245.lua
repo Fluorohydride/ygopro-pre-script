ae9c5f8ff668f9393e486f5c5324c19da9ef8270
mercury233 <me@mercury233.me>
2017-11-26 18:06:04 +0800

    change id

--- scripts/VJMP-JP/c61665245.lua
+++ scripts/VJMP-JP/c61665245.lua
@@ -1,79 +1,79 @@
 --サモン・ソーサレス
 --Summon Sorceress
 --Script by nekrozar
-function c100200140.initial_effect(c)
+function c61665245.initial_effect(c)
 	--link summon
-	aux.AddLinkProcedure(c,aux.NOT(aux.FilterBoolFunction(Card.IsLinkType,TYPE_TOKEN)),2,99,c100200140.lcheck)
+	aux.AddLinkProcedure(c,aux.NOT(aux.FilterBoolFunction(Card.IsLinkType,TYPE_TOKEN)),2,99,c61665245.lcheck)
 	c:EnableReviveLimit()
 	--special summon
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(100200140,0))
+	e1:SetDescription(aux.Stringid(61665245,0))
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
 	e1:SetProperty(EFFECT_FLAG_DELAY)
-	e1:SetCondition(c100200140.spcon1)
-	e1:SetTarget(c100200140.sptg1)
-	e1:SetOperation(c100200140.spop1)
+	e1:SetCondition(c61665245.spcon1)
+	e1:SetTarget(c61665245.sptg1)
+	e1:SetOperation(c61665245.spop1)
 	c:RegisterEffect(e1)
 	--special summon
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(100200140,1))
+	e2:SetDescription(aux.Stringid(61665245,1))
 	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e2:SetType(EFFECT_TYPE_IGNITION)
 	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e2:SetRange(LOCATION_MZONE)
-	e2:SetCountLimit(1,100200140)
-	e2:SetTarget(c100200140.sptg2)
-	e2:SetOperation(c100200140.spop2)
+	e2:SetCountLimit(1,61665245)
+	e2:SetTarget(c61665245.sptg2)
+	e2:SetOperation(c61665245.spop2)
 	c:RegisterEffect(e2)
 end
-function c100200140.lcheck(g)
+function c61665245.lcheck(g)
 	return g:GetClassCount(Card.GetRace)==1
 end
-function c100200140.spcon1(e,tp,eg,ep,ev,re,r,rp)
+function c61665245.spcon1(e,tp,eg,ep,ev,re,r,rp)
 	return e:GetHandler():IsSummonType(SUMMON_TYPE_LINK)
 end
-function c100200140.spfilter1(c,e,tp,zone)
+function c61665245.spfilter1(c,e,tp,zone)
 	return c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP_DEFENSE,1-tp,zone)
 end
-function c100200140.sptg1(e,tp,eg,ep,ev,re,r,rp,chk)
+function c61665245.sptg1(e,tp,eg,ep,ev,re,r,rp,chk)
 	local zone=e:GetHandler():GetLinkedZone(1-tp)
 	if chk==0 then return Duel.GetLocationCount(1-tp,LOCATION_MZONE,tp,LOCATION_REASON_TOFIELD,zone)>0
-		and Duel.IsExistingMatchingCard(c100200140.spfilter1,tp,LOCATION_HAND,0,1,nil,e,tp,zone) end
+		and Duel.IsExistingMatchingCard(c61665245.spfilter1,tp,LOCATION_HAND,0,1,nil,e,tp,zone) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
 end
-function c100200140.spop1(e,tp,eg,ep,ev,re,r,rp)
+function c61665245.spop1(e,tp,eg,ep,ev,re,r,rp)
 	local zone=e:GetHandler():GetLinkedZone(1-tp)
 	if Duel.GetLocationCount(1-tp,LOCATION_MZONE,tp,LOCATION_REASON_TOFIELD,zone)<=0 then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectMatchingCard(tp,c100200140.spfilter1,tp,LOCATION_HAND,0,1,1,nil,e,tp,zone)
+	local g=Duel.SelectMatchingCard(tp,c61665245.spfilter1,tp,LOCATION_HAND,0,1,1,nil,e,tp,zone)
 	if g:GetCount()>0 then
 		Duel.SpecialSummon(g,0,tp,1-tp,false,false,POS_FACEUP_DEFENSE,zone)
 	end
 end
-function c100200140.spfilter2(c,e,tp,lg)
-	return c:IsFaceup() and lg:IsContains(c) and Duel.IsExistingMatchingCard(c100200140.spfilter3,tp,LOCATION_DECK,0,1,nil,e,tp,c:GetRace())
+function c61665245.spfilter2(c,e,tp,lg)
+	return c:IsFaceup() and lg:IsContains(c) and Duel.IsExistingMatchingCard(c61665245.spfilter3,tp,LOCATION_DECK,0,1,nil,e,tp,c:GetRace())
 end
-function c100200140.spfilter3(c,e,tp,rac)
+function c61665245.spfilter3(c,e,tp,rac)
 	return c:IsRace(rac)
 		and (c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP_DEFENSE) or c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP_DEFENSE,1-tp))
 end
-function c100200140.sptg2(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+function c61665245.sptg2(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	local c=e:GetHandler()
 	local zone={}
 	zone[0]=c:GetLinkedZone(0)
 	zone[1]=c:GetLinkedZone(1)
 	local lg=c:GetLinkedGroup()
-	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and c100200140.spfilter2(chkc,e,tp,lg) end
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and c61665245.spfilter2(chkc,e,tp,lg) end
 	if chk==0 then return (Duel.GetLocationCount(tp,LOCATION_MZONE,tp,LOCATION_REASON_TOFIELD,zone[tp])>0
 		or Duel.GetLocationCount(1-tp,LOCATION_MZONE,tp,LOCATION_REASON_TOFIELD,zone[1-tp])>0)
-		and Duel.IsExistingTarget(c100200140.spfilter2,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil,e,tp,lg) end
+		and Duel.IsExistingTarget(c61665245.spfilter2,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil,e,tp,lg) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
-	Duel.SelectTarget(tp,c100200140.spfilter2,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil,e,tp,lg)
+	Duel.SelectTarget(tp,c61665245.spfilter2,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil,e,tp,lg)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
 end
-function c100200140.spop2(e,tp,eg,ep,ev,re,r,rp)
+function c61665245.spop2(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local tc=Duel.GetFirstTarget()
 	if not c:IsRelateToEffect(e) or not tc:IsRelateToEffect(e) or tc:IsFacedown() then return end
@@ -83,12 +83,12 @@ function c100200140.spop2(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(tp,LOCATION_MZONE,tp,LOCATION_REASON_TOFIELD,zone[tp])<=0
 		and Duel.GetLocationCount(1-tp,LOCATION_MZONE,tp,LOCATION_REASON_TOFIELD,zone[1-tp])<=0 then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectMatchingCard(tp,c100200140.spfilter3,tp,LOCATION_DECK,0,1,1,nil,e,tp,tc:GetRace())
+	local g=Duel.SelectMatchingCard(tp,c61665245.spfilter3,tp,LOCATION_DECK,0,1,1,nil,e,tp,tc:GetRace())
 	local tc=g:GetFirst()
 	if tc then
 		local sump=tp
 		if tc:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP_DEFENSE,1-tp,zone[1-tp])
-			and (not tc:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP_DEFENSE,tp,zone[tp]) or Duel.SelectYesNo(tp,aux.Stringid(100200140,2))) then
+			and (not tc:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP_DEFENSE,tp,zone[tp]) or Duel.SelectYesNo(tp,aux.Stringid(61665245,2))) then
 			sump=1-tp
 		end
 		if Duel.SpecialSummon(tc,0,tp,sump,false,false,POS_FACEUP_DEFENSE,zone[sump])~=0 then
