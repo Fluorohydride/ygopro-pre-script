b097f40ca72f375e75a0661f4cbd871dcb64f8e4
nekrozar <nekrozar.s@gmail.com>
2017-11-30 09:11:09 +0900

    fix

--- scripts/PP20-JP/c100224014.lua
+++ scripts/PP20-JP/c100224014.lua
@@ -4,12 +4,12 @@
 function c100224014.initial_effect(c)
 	--special summon
 	local e1=Effect.CreateEffect(c)
-  e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetRange(LOCATION_GRAVE+LOCATION_HAND)
-  e1:SetCountLimit(1,100224014)
+	e1:SetCountLimit(1,100224014)
 	e1:SetCost(c100224014.spcost)
-  e1:SetTarget(c100224014.sptg)
+	e1:SetTarget(c100224014.sptg)
 	e1:SetOperation(c100224014.spop)
 	c:RegisterEffect(e1)
 	--pierce
@@ -22,17 +22,38 @@ function c100224014.initial_effect(c)
 	local e3=Effect.CreateEffect(c)
 	e3:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOKEN)
 	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e3:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
+	e3:SetProperty(EFFECT_FLAG_DELAY)
 	e3:SetCode(EVENT_TO_GRAVE)
 	e3:SetCondition(c100224014.condition)
 	e3:SetTarget(c100224014.target)
 	e3:SetOperation(c100224014.operation)
 	c:RegisterEffect(e3)
 end
+function c100224014.rfilter(c,tp)
+	return c:IsControler(tp) or c:IsFaceup()
+end
+function c100224014.mzfilter(c,tp)
+	return c:IsControler(tp) and c:GetSequence()<5
+end
 function c100224014.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
-  if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>-2
-		and Duel.CheckReleaseGroup(tp,nil,2,nil) end
-	local g=Duel.SelectReleaseGroup(tp,nil,2,2,nil)
+	local rg=Duel.GetReleaseGroup(tp):Filter(c100224014.rfilter,nil,tp)
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	local ct=-ft+1
+	if chk==0 then return ft>-2 and rg:GetCount()>1 and (ft>0 or rg:IsExists(c100224014.mzfilter,ct,nil,tp)) end
+	local g=nil
+	if ft>0 then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
+		g=rg:Select(tp,2,2,nil)
+	elseif ft==0 then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
+		g=rg:FilterSelect(tp,c100224014.mzfilter,1,1,nil,tp)
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
+		local g2=rg:Select(tp,1,1,g:GetFirst())
+		g:Merge(g2)
+	else
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
+		g=rg:FilterSelect(tp,c100224014.mzfilter,2,2,nil,tp)
+	end
 	Duel.Release(g,REASON_COST)
 end
 function c100224014.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
@@ -41,12 +62,12 @@ function c100224014.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 end
 function c100224014.spop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	if c:IsRelateToEffect(e) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 then
+	if c:IsRelateToEffect(e) then
 		Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
 	end
 end
 function c100224014.pcon(e)
-	return e:GetHandler():GetPreviousLocation()==LOCATION_GRAVE
+	return e:GetHandler():GetSummonLocation()==LOCATION_GRAVE
 end
 function c100224014.condition(e,tp,eg,ep,ev,re,r,rp)
 	return e:GetHandler():IsReason(REASON_BATTLE+REASON_EFFECT)
@@ -59,9 +80,7 @@ function c100224014.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,2,0,0)
 end
 function c100224014.operation(e,tp,eg,ep,ev,re,r,rp)
-	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
-	if ft>1 and Duel.IsPlayerAffectedByEffect(tp,59822133) then ft=1 end
-	if ft<2 then return end
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=1 or Duel.IsPlayerAffectedByEffect(tp,59822133) end
 	if not Duel.IsPlayerCanSpecialSummonMonster(tp,100224014+100,0,0x4011,500,500,2,RACE_ZOMBIE,ATTRIBUTE_FIRE) then return end
 	for i=1,2 do
 		local token=Duel.CreateToken(tp,100224014+100)
