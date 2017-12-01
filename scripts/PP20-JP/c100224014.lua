a24bcfebd2522b885d3cfbaba2c043830dbf9b83
nekrozar <nekrozar.s@gmail.com>
2017-12-01 09:27:00 +0900

    update
    use Duel.GetMZoneCount.

--- scripts/PP20-JP/c100224014.lua
+++ scripts/PP20-JP/c100224014.lua
@@ -29,30 +29,24 @@ function c100224014.initial_effect(c)
 	e3:SetOperation(c100224014.operation)
 	c:RegisterEffect(e3)
 end
-function c100224014.rfilter(c,tp)
-	return c:IsControler(tp) or c:IsFaceup()
-end
-function c100224014.mzfilter(c,tp)
-	return c:IsControler(tp) and c:GetSequence()<5
+function c100224014.fselect(c,tp,rg,sg)
+	sg:AddCard(c)
+	if sg:GetCount()<2 then
+		res=rg:IsExists(c100224014.fselect,1,sg,tp,rg,sg)
+	else
+		res=Duel.GetMZoneCount(tp,sg)>0
+	end
+	sg:RemoveCard(c)
+	return res
 end
 function c100224014.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
-	local rg=Duel.GetReleaseGroup(tp):Filter(c100224014.rfilter,nil,tp)
-	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
-	local ct=-ft+1
-	if chk==0 then return ft>-2 and rg:GetCount()>1 and (ft>0 or rg:IsExists(c100224014.mzfilter,ct,nil,tp)) end
-	local g=nil
-	if ft>0 then
+	local rg=Duel.GetReleaseGroup(tp)
+	local g=Group.CreateGroup()
+	if chk==0 then return rg:IsExists(c100224014.fselect,1,nil,tp,rg,g) end
+	while g:GetCount()<2 do
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
-		g=rg:Select(tp,2,2,nil)
-	elseif ft==0 then
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
-		g=rg:FilterSelect(tp,c100224014.mzfilter,1,1,nil,tp)
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
-		local g2=rg:Select(tp,1,1,g:GetFirst())
-		g:Merge(g2)
-	else
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
-		g=rg:FilterSelect(tp,c100224014.mzfilter,2,2,nil,tp)
+		local sg=rg:FilterSelect(tp,c100224014.fselect,1,1,g,tp,rg,g)
+		g:Merge(sg)
 	end
 	Duel.Release(g,REASON_COST)
 end
