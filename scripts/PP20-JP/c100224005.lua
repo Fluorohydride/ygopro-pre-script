d4c3c9d3e24278a7e7db8ef02917db53f00fa62c
EerieCode <eeriecode@gmail.com>
2017-12-11 11:38:12 +0100

    PP20-JP005 --No.5 亡霊竜デス・キマイラ・ドラゴン
    Credits to ahtelel

---
+++ scripts/PP20-JP/c100224005.lua
@@ -0,0 +1,82 @@
+--No.5 亡霊竜デス・キマイラ・ドラゴン
+--Number 5: Doom Chimera Dragon
+--Scripted by ahtelel
+function c100224005.initial_effect(c)
+	--xyz summon
+	aux.AddXyzProcedure(c,nil,5,2,nil,nil,99)
+	c:EnableReviveLimit()
+	--atk
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCode(EFFECT_UPDATE_ATTACK)
+	e1:SetValue(c100224005.atkval)
+	c:RegisterEffect(e1)
+	--attack all
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetCode(EFFECT_ATTACK_ALL)
+	e2:SetCondition(c100224005.allcon)
+	e2:SetValue(1)
+	c:RegisterEffect(e2)
+	--attach
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(100224005,0))
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e3:SetCode(EVENT_PHASE+PHASE_BATTLE)
+	e3:SetCountLimit(1,100224005)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCondition(c100224005.con)
+	e3:SetTarget(c100224005.mttg)
+	e3:SetOperation(c100224005.mtop)
+	c:RegisterEffect(e3)
+	--return 
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(100224005,1))
+	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e4:SetCode(EVENT_PHASE+PHASE_BATTLE)
+	e4:SetCountLimit(1,100224005)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetCondition(c100224005.con)
+	e4:SetTarget(c100224005.rettg)
+	e4:SetOperation(c100224005.retop)
+	c:RegisterEffect(e4)
+end
+function c100224005.atkval(e,c)
+	return c:GetOverlayCount()*1000
+end
+function c100224005.allcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsType(TYPE_XYZ) and e:GetHandler():GetOverlayCount()>0
+end
+function c100224005.con(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():GetBattledGroupCount()>0
+end
+function c100224005.mtfilter(c)
+	return c:IsType(TYPE_MONSTER)
+end
+function c100224005.mttg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c100224005.mtfilter,tp,LOCATION_GRAVE,0,1,nil) end
+end
+function c100224005.mtop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not c:IsRelateToEffect(e) or c:IsFacedown() then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_XMATERIAL)
+	local g=Duel.SelectMatchingCard(tp,c100224005.mtfilter,tp,LOCATION_GRAVE,0,1,1,nil)
+	if g:GetCount()>0 then
+		Duel.Overlay(c,g)
+	end
+end
+function c100224005.rettg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chkc then return chkc:GetLocation()==LOCATION_GRAVE and chkc:GetControler()==1-tp and chkc:IsAbleToDeck() end
+	if chk==0 then return Duel.IsExistingTarget(Card.IsAbleToDeck,1-tp,LOCATION_GRAVE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
+	local sg=Duel.SelectTarget(tp,Card.IsAbleToDeck,1-tp,LOCATION_GRAVE,0,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_TODECK,sg,1,0,0)
+end
+function c100224005.retop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc and tc:IsRelateToEffect(e) then
+		Duel.SendtoDeck(tc,nil,0,REASON_EFFECT)
+	end
+end
