d0bdf73121d8440564170cff0043e6c4a7897787
EerieCode <eeriecode@gmail.com>
2017-12-11 09:00:38 +0100

    LVB1-JP017 --タクティカル・エクスチェンバー

---
+++ scripts/LVB1-JP/c100225017.lua
@@ -0,0 +1,43 @@
+--タクティカル・エクスチェンバー
+--Tactical Exchamber
+function c100225017.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_DESTROY+CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCountLimit(1,100225017+EFFECT_COUNT_CODE_OATH)
+	e1:SetTarget(c100225017.target)
+	e1:SetOperation(c100225017.activate)
+	c:RegisterEffect(e1)
+end
+function c100225017.desfilter1(c,ft)
+	return c:IsFaceup() and (ft>0 or (c:IsLocation(LOCATION_MZONE) and c:GetSequence()<5)) 
+	and Duel.IsExistingMatchingCard(c100225017.spfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,nil,e,tp,c:GetOriginalCode())
+end
+function c100225017.spfilter(c,e,tp,code)
+	return c:IsSetCard(0x102) and c:IsCanBeSpecialSummoned(e,0,tp,false,false) and c:GetOriginalCode()~=code
+end
+function c100225017.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	local c=e:GetHandler()
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	if chkc then return chkc:IsOnField() and chkc:IsControler(tp) and chkc~=c and c100225017.desfilter1(chkc,ft) end
+	if chk==0 then
+		return ft>-1 and Duel.IsExistingTarget(c100225017.desfilter1,tp,LOCATION_ONFIELD,0,1,c,ft)
+	end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local g=Duel.SelectTarget(tp,c100225017.desfilter1,tp,LOCATION_ONFIELD,0,1,1,c,ft)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK+LOCATION_GRAVE)
+end
+function c100225017.activate(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc and tc:IsRelateToEffect(e) and Duel.Destroy(tc,REASON_EFFECT)~=0 and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+		local g=Duel.SelectMatchingCard(tp,c100225017.spfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil,e,tp,tc:GetOriginalCode())
+		if g:GetCount()>0 then
+			Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+		end
+	end
+end
