d9eec7fa3670b88c2c53369a4aa77958c58cb236
EerieCode <eeriecode@gmail.com>
2017-12-11 10:07:44 +0100

    PP20-JP003 --ジャンク・コネクター

---
+++ scripts/PP20-JP/c100224003.lua
@@ -0,0 +1,78 @@
+--ジャンク・コネクター
+--Junk Connector
+--Scripted by Eerie Code
+function c100224003.initial_effect(c)
+	c:EnableReviveLimit()
+	aux.AddLinkProcedure(c,aux.FilterBoolFunctionEx(Card.IsRace,RACE_WARRIOR+RACE_MACHINE,2,2,c100224003.lcheck)
+	--synchro effect
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(100224003,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_QUICK_O)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetHintTiming(0,TIMING_BATTLE_START+TIMING_BATTLE_END)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCondition(c100224003.sccon)
+	e1:SetTarget(c100224003.sctg)
+	e1:SetOperation(c100224003.scop)
+	c:RegisterEffect(e1)
+	--spsummon
+	local e2=Effect.CreateEffect(c)
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
+	e2:SetCode(EVENT_TO_GRAVE)
+	e2:SetCondition(c100224003.spcon)
+	e2:SetTarget(c100224003.sptg)
+	e2:SetOperation(c100224003.spop)
+	c:RegisterEffect(e2)
+end
+function c100224003.lcheck(g,lc)
+	return g:IsExists(Card.IsType,1,nil,TYPE_TUNER)
+end
+function c100224003.sccon(e,tp,eg,ep,ev,re,r,rp)
+	local ph=Duel.GetCurrentPhase()
+	return ph==PHASE_MAIN1 or (ph>=PHASE_BATTLE_START and ph<=PHASE_BATTLE) or ph==PHASE_MAIN2
+end
+function c100224003.scfilter(c,mg)
+	return c:IsSynchroSummonable(nil,mg)
+end
+function c100224003.sctg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then
+		local mg=e:GetHandler():GetLinkedGroup()
+		return Duel.IsExistingMatchingCard(c100224003.scfilter,tp,LOCATION_EXTRA,0,1,nil,mg)
+	end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
+end
+function c100224003.scop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not c:IsRelateToEffect(e) then return end
+	local mg=c:GetLinkedGroup()
+	local g=Duel.GetMatchingGroup(c100224003.scfilter,tp,LOCATION_EXTRA,0,nil,mg)
+	if g:GetCount()>0 then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+		local sg=g:Select(tp,1,1,nil)
+		Duel.SynchroSummon(tp,sg:GetFirst(),nil,mg)
+	end
+end
+function c100224003.spcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return c:IsReason(REASON_DESTROY) and c:IsReason(REASON_BATTLE+REASON_EFFECT) and c:GetReasonPlayer()~=tp
+end
+function c100224003.spfilter(c,e,tp)
+	return c:IsSetCard(0x43) and c:IsType(TYPE_SYNCHRO) and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_SYNCHRO,tp,false,false)
+end
+function c100224003.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCountFromEx(tp)>0
+		and Duel.IsExistingMatchingCard(c100224003.spfilter,tp,LOCATION_EXTRA,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
+end
+function c100224003.spop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCountFromEx(tp)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,c100224003.spfilter,tp,LOCATION_EXTRA,0,1,1,nil,e,tp)
+	if g:GetCount()>0 then
+		Duel.SpecialSummon(g,SUMMON_TYPE_SYNCHRO,tp,tp,false,false,POS_FACEUP)
+		g:GetFirst():CompleteProcedure()
+	end
+end
