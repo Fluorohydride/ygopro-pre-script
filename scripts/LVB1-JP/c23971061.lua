f49d2245554e5016afebaaadf73add51082ff4c2
Khriskh <chrisqs16@hotmail.com>
2017-12-22 22:04:37 -0500

    change ID LVB1 (#157)

--- scripts/LVB1-JP/c23971061.lua
+++ scripts/LVB1-JP/c23971061.lua
@@ -1,7 +1,7 @@
 --ダブルバイト・ドラゴン
 --Double Byte Dragon
 --Script by nekrozar
-function c100225001.initial_effect(c)
+function c23971061.initial_effect(c)
 	--link summon
 	c:EnableReviveLimit()
 	aux.AddLinkProcedure(c,aux.FilterBoolFunction(Card.IsLinkType,TYPE_LINK),2,2)
@@ -10,8 +10,8 @@ function c100225001.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
 	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
 	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
-	e1:SetCondition(c100225001.atkcon)
-	e1:SetOperation(c100225001.atkop)
+	e1:SetCondition(c23971061.atkcon)
+	e1:SetOperation(c23971061.atkop)
 	c:RegisterEffect(e1)
 	--immune
 	local e2=Effect.CreateEffect(c)
@@ -19,19 +19,19 @@ function c100225001.initial_effect(c)
 	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
 	e2:SetRange(LOCATION_MZONE)
 	e2:SetCode(EFFECT_IMMUNE_EFFECT)
-	e2:SetValue(c100225001.efilter)
+	e2:SetValue(c23971061.efilter)
 	c:RegisterEffect(e2)
 	--indes
 	local e3=Effect.CreateEffect(c)
 	e3:SetType(EFFECT_TYPE_SINGLE)
 	e3:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
-	e3:SetValue(c100225001.indval)
+	e3:SetValue(c23971061.indval)
 	c:RegisterEffect(e3)
 end
-function c100225001.atkcon(e,tp,eg,ep,ev,re,r,rp)
+function c23971061.atkcon(e,tp,eg,ep,ev,re,r,rp)
 	return e:GetHandler():IsSummonType(SUMMON_TYPE_LINK)
 end
-function c100225001.atkop(e,tp,eg,ep,ev,re,r,rp)
+function c23971061.atkop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local g=c:GetMaterial()
 	local atk=0
@@ -48,9 +48,9 @@ function c100225001.atkop(e,tp,eg,ep,ev,re,r,rp)
 	e1:SetReset(RESET_EVENT+0x1ff0000)
 	c:RegisterEffect(e1)
 end
-function c100225001.efilter(e,te)
+function c23971061.efilter(e,te)
 	return not te:IsType(TYPE_LINK)
 end
-function c100225001.indval(e,c)
+function c23971061.indval(e,c)
 	return not c:IsType(TYPE_LINK)
 end
