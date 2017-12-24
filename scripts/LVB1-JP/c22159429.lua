0296145e1af4236ad7a887856db063761d670255
Mercury233 <me@mercury233.me>
2017-12-24 16:27:10 +0800

    fix

--- scripts/LVB1-JP/c22159429.lua
+++ scripts/LVB1-JP/c22159429.lua
@@ -85,15 +85,18 @@ function c22159429.spcon(e,tp,eg,ep,ev,re,r,rp)
 	local cet=e:GetHandler():GetEquipTarget()
 	return ep~=tp and ((eg and eg:GetFirst() == cet) or (re and re:GetHandler() == cet))
 end
+function c22159429.spfilter2(c,e,tp)
+	return c:IsSetCard(0xfb) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
 function c22159429.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingMatchingCard(c22159429.spfilter,tp,LOCATION_HAND,0,1,nil,e,tp) end
+		and Duel.IsExistingMatchingCard(c22159429.spfilter2,tp,LOCATION_HAND,0,1,nil,e,tp) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
 end
 function c22159429.spop(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectMatchingCard(tp,c22159429.spfilter,tp,LOCATION_HAND,0,1,1,nil,e,tp)
+	local g=Duel.SelectMatchingCard(tp,c22159429.spfilter2,tp,LOCATION_HAND,0,1,1,nil,e,tp)
 	if g:GetCount()>0 then
 		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
 	end
