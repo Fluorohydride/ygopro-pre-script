27ccc231c670f1745e4f96e1fef40aa7fc4a0ea6
EerieCode <eeriecode@gmail.com>
2018-02-13 14:06:26 +0100

    DBDS-JP018 --空牙団の参謀 シール

---
+++ scripts/DBDS-JP/c100408018.lua
@@ -0,0 +1,66 @@
+--空牙団の参謀 シール
+--
+--Scripted by Eerie Code
+function c100408018.initial_effect(c)
+	--special summon
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(100408018,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetCountLimit(1,100408018)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetTarget(c100408018.sptg)
+	e1:SetOperation(c100408018.spop)
+	c:RegisterEffect(e1)
+	--recover from gy
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(100408018,1))
+	e2:SetCategory(CATEGORY_TOHAND)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DELAY)
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1,100408018+100)
+	e2:SetCondition(c100408018.thcon)
+	e2:SetTarget(c100408018.thtg)
+	e2:SetOperation(c100408018.thop)
+	c:RegisterEffect(e2)
+end
+function c100408018.spfilter(c,e,tp)
+	return c:IsSetCard(0x214) and not c:IsCode(100408018) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function c100408018.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(c100408018.spfilter,tp,LOCATION_HAND,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
+end
+function c100408018.spop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,c100408018.spfilter,tp,LOCATION_HAND,0,1,1,nil,e,tp)
+	if g:GetCount()>0 then
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
+function c100408018.cfilter(c,tp)
+	return c:IsFaceup() and c:IsSetCard(0x214) and c:IsControler(tp)
+end
+function c100408018.thcon(e,tp,eg,ep,ev,re,r,rp)
+	return not eg:IsContains(e:GetHandler()) and eg:IsExists(c100408018.cfilter,1,nil,tp)
+end
+function c100408018.thfilter(c)
+	return c:IsSetCard(0x214) and c:IsType(TYPE_MONSTER) and c:IsAbleToHand()
+end
+function c100408018.thtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and c88204302.thfilter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(c100408018.thfilter,tp,LOCATION_GRAVE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local sg=Duel.SelectTarget(tp,c100408018.thfilter,tp,LOCATION_GRAVE,0,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,sg,sg:GetCount(),0,0)
+end
+function c100408018.thop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) then
+		Duel.SendtoHand(tc,nil,REASON_EFFECT)
+	end
+end
