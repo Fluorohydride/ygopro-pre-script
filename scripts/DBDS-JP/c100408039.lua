0b34cca846f60afe4e77472f903021532c327a9a
EerieCode <eeriecode@gmail.com>
2018-02-14 21:51:05 +0100

    DBDS-JP039 --閃刀空域－エリアゼロ

---
+++ scripts/DBDS-JP/c100408039.lua
@@ -0,0 +1,85 @@
+--閃刀空域－エリアゼロ
+--Brandish Airspace Area Zero
+--Scripted by Eerie Code
+function c100408039.initial_effect(c)
+	--activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e1)
+	--excavate
+	local e2=Effect.CreateEffect(c)
+	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH+CATEGORY_TOGRAVE)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetRange(LOCATION_FZONE)
+	e2:SetCountLimit(1,100408039)
+	e2:SetTarget(c100408039.thtg)
+	e2:SetOperation(c100408039.thop)
+	c:RegisterEffect(e2)
+	--spsummon
+	local e3=Effect.CreateEffect(c)
+	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e3:SetCode(EVENT_TO_GRAVE)
+	e3:SetProperty(EFFECT_FLAG_DELAY)
+	e3:SetCountLimit(1,100408039+100)
+	e3:SetCondition(c100408039.spcon)
+	e3:SetTarget(c100408039.sptg)
+	e3:SetOperation(c100408039.spop)
+	c:RegisterEffect(e3)
+end
+function c100408039.thtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	local c=e:GetHandler()
+	if chkc then return chkc:IsOnField() and chkc:IsControler(tp) and chkc~=c end
+	if chk==0 then return Duel.IsExistingTarget(nil,tp,LOCATION_ONFIELD,0,1,c)
+		and Duel.GetFieldGroupCount(tp,LOCATION_DECK,0)>=3 end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local g=Duel.SelectTarget(tp,nil,tp,LOCATION_ONFIELD,0,1,1,c)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,0,LOCATION_DECK)
+	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,g,1,0,0)
+end
+function c100408039.thop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if not tc:IsRelateToEffect(e) then return end
+	Duel.ConfirmDecktop(tp,3)
+	local g=Duel.GetDecktopGroup(tp,3)
+	if g:GetCount()>0 then
+		if g:IsExists(Card.IsSetCard,1,nil,0x215) then
+			if Duel.SelectYesNo(tp,aux.Stringid(100408039,1)) then
+				Duel.Hint(HINT_SELECTMSG,p,HINTMSG_ATOHAND)
+				local sg=g:FilterSelect(tp,Card.IsSetCard,1,1,nil,0x215)
+				if sg:GetFirst():IsAbleToHand() then
+					Duel.SendtoHand(sg,nil,REASON_EFFECT)
+					Duel.ConfirmCards(1-tp,sg)
+					Duel.ShuffleHand(tp)
+				else
+					Duel.SendtoGrave(sg,REASON_RULE)
+				end
+			end
+			Duel.SendtoGrave(tc,REASON_EFFECT)
+		end
+		Duel.ShuffleDeck(tp)
+	end
+end
+function c100408039.spcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return c:IsReason(REASON_EFFECT) and c:IsPreviousLocation(LOCATION_FZONE)
+end
+function c100408039.spfilter(c,e,tp)
+	return c:IsSetCard(0x1215) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function c100408039.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(c100408039.spfilter,tp,LOCATION_DECK,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
+end
+function c100408039.spop(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetHandler():IsRelateToEffect(e) then return end
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,c100408039.spfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
+	if g:GetCount()>0 then
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
