82be9f878c5c6992931b0e756307c7696a8f118c
EerieCode <eeriecode@gmail.com>
2018-02-14 21:11:28 +0100

    Added new DBDS-JP cards
    030 --閃刀起動－エンゲージ
    031 --閃刀術式－アフターバーナー
    
    Credits to ahtelel

---
+++ scripts/DBDS-JP/c100408030.lua
@@ -0,0 +1,44 @@
+--閃刀起動－エンゲージ
+--Brandish Start-Up Engage
+--Scripted by ahtelel
+function c100408030.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCondition(c100408030.condition)
+	e1:SetTarget(c100408030.target)
+	e1:SetOperation(c100408030.activate)
+	c:RegisterEffect(e1)
+end
+function c100408030.cfilter(c)
+	return c:GetSequence()<5
+end
+function c100408030.condition(e,tp,eg,ep,ev,re,r,rp)
+	return not Duel.IsExistingMatchingCard(c100408030.cfilter,tp,LOCATION_MZONE,0,1,nil)
+end
+function c100408030.filter(c)
+	return c:IsSetCard(0x215) and c:IsAbleToHand() and not c:IsCode(100408030)
+end
+function c100408030.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c100408030.filter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+	if Duel.GetMatchingGroupCount(Card.IsType,tp,LOCATION_GRAVE,0,nil,TYPE_SPELL)>=3 then
+		e:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH+CATEGORY_DRAW)
+	end
+end
+function c100408030.activate(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,c100408030.filter,tp,LOCATION_DECK,0,1,1,nil)
+	if g:GetCount()>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+		if Duel.IsPlayerCanDraw(tp,1) 
+			and Duel.GetMatchingGroupCount(Card.IsType,tp,LOCATION_GRAVE,0,nil,TYPE_SPELL)>=3 
+			and Duel.SelectYesNo(tp,aux.Stringid(100408030,0)) then
+			Duel.BreakEffect()
+			Duel.Draw(tp,1,REASON_EFFECT)
+		end
+	end
+end
