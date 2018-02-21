eb53909f2327b12fe2dfac502fdea078e67a41a4
nekrozar <nekrozar.s@gmail.com>
2018-02-21 20:30:21 +0900

    add script DBDS

---
+++ scripts/DBDS-JP/c100408008.lua
@@ -0,0 +1,89 @@
+--ヴァンパイア・デザイア
+
+--Script by nekrozar
+function c100408008.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCountLimit(1,100408008+EFFECT_COUNT_CODE_OATH)
+	e1:SetTarget(c100408008.target)
+	e1:SetOperation(c100408008.activate)
+	c:RegisterEffect(e1)
+end
+function c100408008.tgfilter1(c,tp)
+	local lv=c:GetLevel()
+	return lv>0 and c:IsFaceup()
+		and Duel.IsExistingMatchingCard(c100408008.tgfilter2,tp,LOCATION_DECK,0,1,nil,lv)
+end
+function c100408008.tgfilter2(c,lv)
+	return c:IsSetCard(0x8e) and not c:IsLevel(lv) and c:IsAbleToGrave()
+end
+function c100408008.spfilter1(c,tp)
+	return c:IsAbleToGrave() and Duel.GetMZoneCount(tp,c)>0
+end
+function c100408008.spfilter2(c,e,tp)
+	return c:IsSetCard(0x8e) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function c100408008.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then
+		if e:GetLabel()==0 then
+			return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and c100408008.tgfilter1(chkc,tp)
+		else
+			return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and c100408008.spfilter2(chkc,e,tp)
+		end
+	end
+	local b1=Duel.IsExistingTarget(c100408008.tgfilter1,tp,LOCATION_MZONE,0,1,nil,tp)
+	local b2=Duel.IsExistingMatchingCard(c100408008.spfilter1,tp,LOCATION_MZONE,0,1,nil,tp)
+		and Duel.IsExistingTarget(c100408008.spfilter2,tp,LOCATION_GRAVE,0,1,nil,e,tp)
+	if chk==0 then return b1 or b2 end
+	local op=0
+	if b1 and b2 then
+		op=Duel.SelectOption(tp,aux.Stringid(100408008,0),aux.Stringid(100408008,1))
+	elseif b1 then
+		op=Duel.SelectOption(tp,aux.Stringid(100408008,0))
+	else
+		op=Duel.SelectOption(tp,aux.Stringid(100408008,1))+1
+	end
+	e:SetLabel(op)
+	if op==0 then
+		e:SetCategory(CATEGORY_TOGRAVE)
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+		Duel.SelectTarget(tp,c100408008.tgfilter1,tp,LOCATION_MZONE,0,1,1,nil,tp)
+		Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_DECK)
+	else
+		e:SetCategory(CATEGORY_TOGRAVE+CATEGORY_SPECIAL_SUMMON)
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+		local g=Duel.SelectTarget(tp,c100408008.spfilter2,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
+		Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_MZONE)
+		Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
+	end
+end
+function c100408008.activate(e,tp,eg,ep,ev,re,r,rp)
+	if e:GetLabel()==0 then
+		local tc=Duel.GetFirstTarget()
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+		local g=Duel.SelectMatchingCard(tp,c100408008.tgfilter2,tp,LOCATION_DECK,0,1,1,nil,tc:GetLevel())
+		if g:GetCount()>0 and Duel.SendtoGrave(g,REASON_EFFECT)~=0 and g:GetFirst():IsLocation(LOCATION_GRAVE)
+			and tc:IsRelateToEffect(e) and tc:IsFaceup() then
+				local lv=g:GetFirst():GetLevel()
+				local e1=Effect.CreateEffect(e:GetHandler())
+				e1:SetType(EFFECT_TYPE_SINGLE)
+				e1:SetCode(EFFECT_CHANGE_LEVEL)
+				e1:SetValue(lv)
+				e1:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END)
+				tc:RegisterEffect(e1)
+			end
+		end
+	else
+		local tc=Duel.GetFirstTarget()
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+		local tg=Duel.SelectMatchingCard(tp,c100408008.spfilter1,tp,LOCATION_MZONE,0,1,1,nil,tp)
+		if g:GetCount()>0 and Duel.SendtoGrave(g,REASON_EFFECT)~=0 and g:GetFirst():IsLocation(LOCATION_GRAVE)
+			and tc:IsRelateToEffect(e) then
+				Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
+			end
+		end
+	end
+end
---
+++ scripts/DBDS-JP/c100408011.lua
@@ -0,0 +1,39 @@
+--ヴァンパイアの支配
+
+--Script by nekrozar
+function c100408011.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_NEGATE+CATEGORY_DESTROY+CATEGORY_RECOVER)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_CHAINING)
+	e1:SetCountLimit(1,100408011+EFFECT_COUNT_CODE_OATH)
+	e1:SetCondition(c100408011.condition)
+	e1:SetTarget(c100408011.target)
+	e1:SetOperation(c100408011.activate)
+	c:RegisterEffect(e1)
+end
+function c100408011.cfilter(c)
+	return c:IsFaceup() and c:IsSetCard(0x8e)
+end
+function c100408011.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsExistingMatchingCard(c100408011.cfilter,tp,LOCATION_MZONE,0,1,nil)
+		and (re:IsActiveType(TYPE_MONSTER) or re:IsHasType(EFFECT_TYPE_ACTIVATE)) and Duel.IsChainNegatable(ev)
+end
+function c100408011.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
+	if re:GetHandler():IsDestructable() and re:GetHandler():IsRelateToEffect(re) then
+		Duel.SetOperationInfo(0,CATEGORY_DESTROY,eg,1,0,0)
+		if re:GetHandler():IsType(TYPE_MONSTER) then
+			Duel.SetOperationInfo(0,CATEGORY_RECOVER,nil,0,tp,re:GetHandler():GetBaseAttack())
+		end
+	end
+end
+function c100408011.activate(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.NegateActivation(ev) and re:GetHandler():IsRelateToEffect(re)
+		and Duel.Destroy(eg,REASON_EFFECT)~=0 and re:GetHandler():IsType(TYPE_MONSTER) and re:GetHandler():GetBaseAttack()>0 then
+		Duel.BreakEffect()
+		Duel.Recover(tp,re:GetHandler():GetBaseAttack(),REASON_EFFECT)
+	end
+end
