5984537a6ad595db8512e7edbc76245cb17b33b9
EerieCode <eeriecode@gmail.com>
2018-03-07 11:18:46 +0100

    ST18-JP041 --トランスコード・トーカー

---
+++ scripts/ST18-JP/c100318041.lua
@@ -0,0 +1,77 @@
+--トランスコード・トーカー
+--Transcode Talker
+--Scripted by Eerie Code
+function c100318041.initial_effect(c)
+	c:EnableReviveLimit()
+	aux.AddLinkProcedure(c,aux.FilterBoolFunction(Card.IsLinkType,TYPE_EFFECT),2)
+	--atk gain
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_UPDATE_ATTACK)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+	e1:SetCondition(c100318041.atkcon)
+	e1:SetTarget(c100318041.atktg)
+	e1:SetValue(500)
+	c:RegisterEffect(e1)
+	--untarget
+	local e2=e1:Clone()
+	e2:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
+	e2:SetValue(aux.tgoval)
+	c:RegisterEffect(e2)
+	--spsummon
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(100318041,0))
+	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e3:SetType(EFFECT_TYPE_IGNITION)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e3:SetCountLimit(1,100318041)
+	e3:SetCost(c100318041.spcost)
+	e3:SetTarget(c100318041.sptg)
+	e3:SetOperation(c100318041.spop)
+	c:RegisterEffect(e3)
+	Duel.AddCustomActivityCounter(100318041,ACTIVITY_SPSUMMON,c100318041.counterfilter)
+end
+function c100318041.counterfilter(c)
+	return not c:IsRace(RACE_CYBERSE)
+end
+function c100318041.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetCustomActivityCount(100318041,tp,ACTIVITY_SPSUMMON)==0 end
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_OATH)
+	e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+	e1:SetTargetRange(1,0)
+	e1:SetTarget(c100318041.splimit)
+	e1:SetReset(RESET_PHASE+PHASE_END)
+	Duel.RegisterEffect(e1,tp)
+end
+function c100318041.splimit(e,c,sump,sumtype,sumpos,targetp,se)
+	return c:IsRace(RACE_CYBERSE)
+end
+function c100318041.atkcon(e)
+	return e:GetHandler():GetMutualLinkedGroupCount()>0
+end
+function c100318041.atktg(e,c)
+	local g=e:GetHandler():GetMutualLinkedGroup()
+	return c==e:GetHandler() or g:IsContains(c)
+end
+function c100318041.filter(c,e,tp,zone)
+	return c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP,tp,zone)
+end
+function c100318041.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	local zone=e:GetHandler():GetLinkedZone(tp)&0x1f
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and c100318041.filter(chkc,e,tp,zone) end
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingTarget(c100318041.filter,tp,LOCATION_GRAVE,0,1,nil,e,tp,zone) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectTarget(tp,c100318041.filter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp,zone)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
+end
+function c100318041.spop(e,tp,eg,ep,ev,re,r,rp)
+	local zone=e:GetHandler():GetLinkedZone(tp)&0x1f
+	if tc:IsRelateToEffect(e) and zone~=0 then
+		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP,zone)
+	end
+end
