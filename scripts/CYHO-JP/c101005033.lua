6ee49d4eb292e91dff327439a9a256da0bae3c64
EerieCode <eeriecode@gmail.com>
2018-03-12 07:23:26 +0100

    CYHO-JP033 --ドラグニティナイト-アスカロン

---
+++ scripts/CYHO-JP/c101005033.lua
@@ -0,0 +1,78 @@
+--ドラグニティナイト-アスカロン`
+--Dragunity Knight - Ascalon
+--Scripted by ahtelel
+function c101005033.initial_effect(c)
+    --synchro summon
+    aux.AddSynchroProcedure(c,aux.FilterBoolFunction(Card.IsSetCard,0x29),aux.NonTuner(nil),1)
+    c:EnableReviveLimit()
+    --banish
+    local e1=Effect.CreateEffect(c)
+    e1:SetDescription(aux.Stringid(101005033,0))
+    e1:SetCategory(CATEGORY_REMOVE)
+    e1:SetType(EFFECT_TYPE_IGNITION)
+    e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+    e1:SetRange(LOCATION_MZONE)
+    e1:SetCost(c101005033.rmcost)
+    e1:SetTarget(c101005033.rmtg)
+    e1:SetOperation(c101005033.rmop)
+    c:RegisterEffect(e1)
+    --special summon
+    local e2=Effect.CreateEffect(c)
+    e2:SetDescription(aux.Stringid(101005033,1))
+    e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+    e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+    e2:SetProperty(EFFECT_FLAG_DELAY)
+    e2:SetCountLimit(1,101005033)
+    e2:SetCode(EVENT_DESTROYED)
+    e2:SetCondition(c101005033.spcon)
+    e2:SetTarget(c101005033.sptg)
+    e2:SetOperation(c101005033.spop)
+    c:RegisterEffect(e2)
+end
+function c101005033.cfilter(c)
+    return c:IsSetCard(0x29) and c:IsType(TYPE_MONSTER) and c:IsAbleToRemoveAsCost()
+end
+function c101005033.rmcost(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.IsExistingMatchingCard(c101005033.cfilter,tp,LOCATION_GRAVE,0,1,nil) end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+    local g=Duel.SelectMatchingCard(tp,c101005033.cfilter,tp,LLOCATION_GRAVE,0,1,1,nil)
+    Duel.Remove(g,POS_FACEUP,REASON_COST)
+end
+function c101005033.rmtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+    if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) end
+    if chk==0 then return Duel.IsExistingTarget(aux.TRUE,tp,0,LOCATION_MZONE,1,nil) end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+    local g=Duel.SelectTarget(tp,aux.TRUE,tp,0,LOCATION_MZONE,1,1,nil)
+    Duel.SetOperationInfo(0,CATEGORY_REMOVE,g,1,0,0)
+end
+function c101005033.rmop(e,tp,eg,ep,ev,re,r,rp)
+    local tc=Duel.GetFirstTarget()
+    if tc:IsRelateToEffect(e) then
+        Duel.Remove(tc,POS_FACEUP,REASON_EFFECT)
+    end
+end
+function c101005033.spfilter(c,e,tp)
+    return c:IsSetCard(0x29) and c:IsAttackBelow(3000) and c:IsType(TYPE_SYNCHRO)
+        and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_SYNCHRO,tp,false,false)
+end
+function c101005033.spcon(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    return rp==1-tp and c:GetPreviousControler()==tp and c:IsPreviousLocation(LOCATION_MZONE)
+        and c:IsSummonType(SUMMON_TYPE_SYNCHRO)
+end
+function c101005033.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then
+        local pg=aux.GetMustBeMaterialGroup(tp,Group.CreateGroup(),tp,nil,nil,REASON_SYNCHRO)
+        return pg:GetCount()<=0 and Duel.GetLocationCountFromEx(tp)>0
+            and Duel.IsExistingMatchingCard(c101005033.spfilter,tp,LOCATION_EXTRA,0,1,nil,e,tp) end
+    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
+end
+function c101005033.spop(e,tp,eg,ep,ev,re,r,rp)
+    local pg=aux.GetMustBeMaterialGroup(tp,Group.CreateGroup(),tp,nil,nil,REASON_SYNCHRO)
+    if Duel.GetLocationCountFromEx(tp)<=0 or pg:GetCount()>0 then return end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+    local tc=Duel.SelectMatchingCard(tp,c101005033.spfilter,tp,LOCATION_EXTRA,0,1,1,nil,e,tp):GetFirst()
+    if tc and Duel.SpecialSummon(tc,SUMMON_TYPE_SYNCHRO,tp,tp,false,false,POS_FACEUP)>0 then
+        tc:CompleteProcedure()
+    end
+end
