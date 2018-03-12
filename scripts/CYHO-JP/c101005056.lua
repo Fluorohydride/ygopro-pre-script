1a80d4255ba67c54ba0d71e86ef17ad1c1976132
EerieCode <eeriecode@gmail.com>
2018-03-12 12:54:52 +0100

    CYHO-JP056 --エンドレス・オブ・ザ・ワールド

---
+++ scripts/CYHO-JP/c101005056.lua
@@ -0,0 +1,89 @@
+--エンドレス・オブ・ザ・ワールド
+--Endless of the World
+function c101005056.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetTarget(c101005056.target)
+	e1:SetOperation(c101005056.activate)
+	c:RegisterEffect(e1)
+	--salvage
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(101005056,0))
+	e2:SetCategory(CATEGORY_TOHAND)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_GRAVE)
+	e2:SetCondition(c101005056.thcon)
+	e2:SetCost(c101005056.thcost)
+	e2:SetTarget(c101005056.thtg)
+	e2:SetOperation(c101005056.thop)
+	c:RegisterEffect(e2)
+end
+function c101005056.cfilter(c,e,tp,m)
+	if bit.band(c:GetType(),0x81)~=0x81 or not c:IsCode(46427957,72426662)
+		or not c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_RITUAL,tp,false,true) then return false end
+	local mg=m:Filter(Card.IsCanBeRitualMaterial,c,c)
+	return mg:CheckWithSumGreater(Card.GetRitualLevel,c:GetLevel(),c)
+end
+function c101005056.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then
+		if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return false end
+		local mg1=Duel.GetRitualMaterial(tp)
+		mg1:Remove(Card.IsLocation,nil,LOCATION_HAND)
+		return Duel.IsExistingMatchingCard(c101005056.cfilter,tp,LOCATION_HAND,0,1,nil,e,tp,mg1)
+	end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
+end
+function c101005056.activate(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	local mg1=Duel.GetRitualMaterial(tp)
+	mg1:Remove(Card.IsLocation,nil,LOCATION_HAND)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local tg=Duel.SelectMatchingCard(tp,c101005056.cfilter,tp,LOCATION_HAND,0,1,1,nil,e,tp,mg1)
+	local tc=tg:GetFirst()
+	if tc then
+		local mg=mg1:Filter(Card.IsCanBeRitualMaterial,tc,tc)
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
+		local mat=mg:SelectWithSumGreater(tp,Card.GetRitualLevel,tc:GetLevel(),tc)
+		tc:SetMaterial(mat)
+		Duel.ReleaseRitualMaterial(mat)
+		Duel.SpecialSummon(tc,SUMMON_TYPE_RITUAL,tp,tp,false,true,POS_FACEUP)
+		tc:CompleteProcedure()
+	end
+end
+function c101005056.thcon(e,tp,eg,ep,ev,re,r,rp)
+	return aux.exccon(e)
+end
+function c101005056.thcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsAbleToDeckAsCost() end
+	Duel.SendtoDeck(e:GetHandler(),nil,2,REASON_COST)
+end
+function c101005056.filter(c)
+	return c:IsCode(8198712) and c:IsAbleToHand()
+end
+function c101005056.filter2(c)
+	return c:IsCode(46427957,72426662) and c:IsAbleToHand()
+end
+function c101005056.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c101005056.filter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function c101005056.thop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,c101005056.filter,tp,LOCATION_DECK,0,1,1,nil)
+	if g:GetCount()>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+		local mg=Duel.GetMatchingGroup(aux.NecroValleyFilter(c101005056.filter2),tp,LOCATION_GRAVE,0,nil)
+		if mg:GetCount()>0 and Duel.SelectYesNo(tp,aux.Stringid(101005056,1)) then
+			Duel.BreakEffect()
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+			local sg=mg:Select(tp,1,1,nil)
+			Duel.SendtoHand(sg,nil,REASON_EFFECT)
+			Duel.ConfirmCards(1-tp,sg)
+		end
+	end
+end
