13725a4ef9ee24ea18f21f612998cd7313aa4e70
EerieCode <eeriecode@gmail.com>
2018-03-14 12:11:47 +0100

    ST18-JP002 --テクスチェンジャー

---
+++ scripts/ST18-JP/c100318002.lua
@@ -0,0 +1,35 @@
+--テクスチェンジャー
+--Texchangure
+--Scripted by Eerie Code
+function c100318002.initial_effect(c)
+	--negate attack
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e1:SetCode(EVENT_BE_BATTLE_TARGET)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetCondition(c100318002.condition)
+	e1:SetTarget(c100318002.target)
+	e1:SetOperation(c100318002.operation)
+	c:RegisterEffect(c)
+end
+function c100318002.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetAttackTarget() and Duel.GetAttackTarget():IsControler(tp)
+end
+function c100318002.filter(c,e,tp)
+	return c:IsRace(RACE_CYBERSE) and c:IsType(TYPE_NORMAL) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function c100318002.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(c100318002.filter,tp,LOCATION_HAND+LOCATION_DECK+LOCATION_GRAVE,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND+LOCATION_DECK+LOCATION_GRAVE)
+end
+function c100318002.operation(e,tp,eg,ep,ev,re,r,rp)
+	if not Duel.NegateAttack() or Duel.GetLocationCount(tp,LOCATION_MZONE)<1 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(c100318002.filter),tp,LOCATION_HAND+LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil,e,tp)
+	if g:GetCount()>0 then
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
