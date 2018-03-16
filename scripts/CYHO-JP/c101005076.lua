caf0dd08b9532e77040f45b390185ab677b2c55b
EerieCode <eeriecode@gmail.com>
2018-03-16 15:09:07 +0100

    CYHO-JP076 --メタファイズ・アセンション

---
+++ scripts/CYHO-JP/c101005076.lua
@@ -0,0 +1,69 @@
+--メタファイズ・アセンション
+--Metaphys Ascension
+--Scripted by Eerie Code
+function c101005076.initial_effect(c)
+	--draw
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_DRAW+CATEGORY_REMOVE)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetCountLimit(1,101005076)
+	e1:SetCost(c101005076.cost)
+	e1:SetTarget(c101005076.target)
+	e1:SetOperation(c101005076.activate)
+	c:RegisterEffect(e1)
+	--search
+	local e2=Effect.CreateEffect(c)
+	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_REMOVE)
+	e2:SetProperty(EFFECT_FLAG_DELAY)
+	e2:SetCountLimit(1,101005076)
+	e2:SetTarget(c101005076.thtg)
+	e2:SetOperation(c101005076.thop)
+	c:RegisterEffect(e2)
+end
+function c101005076.cfilter(c)
+	return c:IsSetCard(0x105) and c:IsDiscardable()
+end
+function c101005076.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c101005076.cfilter,tp,LOCATION_HAND,0,1,nil) end
+	Duel.DiscardHand(tp,c101005076.cfilter,1,1,REASON_COST+REASON_DISCARD)
+end
+function c101005076.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsPlayerCanDraw(tp,1) end
+	Duel.SetTargetPlayer(tp)
+	Duel.SetTargetParam(1)
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
+end
+function c101005076.filter(c)
+	return c:IsType(TYPE_MONSTER) and c:IsSetCard(0x105) and c:IsAbleToRemove()
+end
+function c101005076.activate(e,tp,eg,ep,ev,re,r,rp)
+	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+	if Duel.Draw(p,d,REASON_EFFECT)~=0 then
+		local g=Duel.GetMatchingGroup(c101005076.filter,tp,LOCATION_DECK,0,nil)
+		if g:GetCount()>0 and Duel.SelectYesNo(tp,aux.Stringid(101005076,0)) then
+			Duel.BreakEffect()
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+			local sg=g:Select(tp,1,1,nil)
+			Duel.Remove(sg,POS_FACEUP,REASON_EFFECT)
+		end
+	end
+end
+function c101005076.thfilter(c)
+	return c:IsSetCard(0x105) and not c:IsCode(101005076) and c:IsAbleToHand()
+end
+function c101005076.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c101005076.thfilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function c101005076.thop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,c101005076.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if g:GetCount()>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end
