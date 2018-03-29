81f385075f0c58bed6700d6f6c6e401dd153384c
EerieCode <eeriecode@gmail.com>
2018-03-29 17:22:09 +0200

    CYHO-JP034 --ヴァレルソード・ドラゴン

---
+++ scripts/CYHO-JP/c101005034.lua
@@ -0,0 +1,97 @@
+--ヴァレルソード・ドラゴン
+--BorrelSword Dragon
+function c101005034.initial_effect(c)
+	--link summon
+	aux.AddLinkProcedure(c,aux.FilterBoolFunctionEx(Card.IsType,TYPE_EFFECT),3)
+	c:EnableReviveLimit()
+	--battle indes
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+	e1:SetCountLimit(1)
+	c:RegisterEffect(e1)
+	--position
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(101005034,0))
+	e2:SetCategory(CATEGORY_POSITION)
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1)
+	e2:SetHintTiming(0,0x1e0)
+	e2:SetTarget(c101005034.postg)
+	e2:SetOperation(c101005034.posop)
+	c:RegisterEffect(e2)
+	--atk change
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(101005034,1))
+	e3:SetCategory(CATEGORY_ATKCHANGE)
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e3:SetCode(EVENT_ATTACK_ANNOUNCE)
+	e3:SetCountLimit(1)
+	e3:SetCondition(c101005034.atkcon)
+	e3:SetTarget(c101005034.atktg)
+	e3:SetOperation(c101005034.atkop)
+	c:RegisterEffect(e3)
+end
+function c101005034.postg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsControler(1-tp) and chkc:IsLocation(LOCATION_MZONE) and chkc:IsAttackPos() end
+	if chk==0 then return Duel.IsExistingTarget(Card.IsAttackPos,tp,0,LOCATION_MZONE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_POSCHANGE)
+	local g=Duel.SelectTarget(tp,Card.IsAttackPos,tp,0,LOCATION_MZONE,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_POSITION,g,g:GetCount(),0,0)
+	Duel.SetChainLimit(c101005034.chlimit)
+end
+function c101005034.chlimit(e,ep,tp)
+	return tp==ep
+end
+function c101005034.posop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tc=Duel.GetFirstTarget()
+	if tc:IsFaceup() and tc:IsRelateToEffect(e) then
+		Duel.ChangePosition(tc,POS_FACEUP_DEFENSE)
+	end
+	if c:IsRelateToEffect(e) then
+		local e2=Effect.CreateEffect(c)
+		e2:SetType(EFFECT_TYPE_SINGLE)
+		e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e2:SetCode(EFFECT_EXTRA_ATTACK)
+		e2:SetValue(1)
+		e2:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END)
+		c:RegisterEffect(e2)
+	end
+end
+function c101005034.atcon(e,tp,eg,ep,ev,re,r,rp)
+	local tc=e:GetHandler():GetBattleTarget()
+	return tc and tc:IsFaceup() 
+end
+function c101005034.atktg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chk==0 then return true end
+	e:GetHandler():GetBattleTarget():CreateEffectRelation(e)
+end
+function c101005034.atkop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tc=c:GetBattleTarget()
+	if not c:IsRelateToEffect(e) or c:IsFacedown() or tc:IsFacedown() or not tc:IsRelateToEffect(e) then return end
+	if c:IsRelateToEffect(e) and c:IsFaceup() then
+		local atk=tc:GetAttack()
+		local e2=Effect.CreateEffect(c)
+		e2:SetType(EFFECT_TYPE_SINGLE)
+		e2:SetCode(EFFECT_UPDATE_ATTACK)
+		e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e2:SetReset(RESET_EVENT+0x1fe0000)
+		e2:SetValue(math.ceil(atk/2))
+		c:RegisterEffect(e2)	
+		if tc:IsFaceup() and tc:IsRelateToEffect(e) and not tc:IsImmuneToEffect(e) then
+			local e1=Effect.CreateEffect(c)
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetCode(EFFECT_SET_ATTACK_FINAL)
+			e1:SetReset(RESET_EVENT+0x1fe0000)
+			e1:SetValue(math.ceil(atk/2))
+			tc:RegisterEffect(e1)
+		end
+	end
+end
