871fd0ba768ac556a65dafc568705cac29fea593
Khriskh <chrisqs16@hotmail.com>
2018-04-10 10:27:29 -0500

    Passcode VJUMP ST18 (#187)

--- scripts/ST18-JP/c46947713.lua
+++ scripts/ST18-JP/c46947713.lua
@@ -1,7 +1,7 @@
 --トランスコード・トーカー
 --Transcode Talker
 --Scripted by Eerie Code
-function c100318041.initial_effect(c)
+function c46947713.initial_effect(c)
 	--link summon
 	c:EnableReviveLimit()
 	aux.AddLinkProcedure(c,aux.FilterBoolFunction(Card.IsLinkType,TYPE_EFFECT),2)
@@ -11,8 +11,8 @@ function c100318041.initial_effect(c)
 	e1:SetCode(EFFECT_UPDATE_ATTACK)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
-	e1:SetCondition(c100318041.atkcon)
-	e1:SetTarget(c100318041.atktg)
+	e1:SetCondition(c46947713.atkcon)
+	e1:SetTarget(c46947713.atktg)
 	e1:SetValue(500)
 	c:RegisterEffect(e1)
 	--untarget
@@ -22,56 +22,56 @@ function c100318041.initial_effect(c)
 	c:RegisterEffect(e2)
 	--spsummon
 	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(100318041,0))
+	e3:SetDescription(aux.Stringid(46947713,0))
 	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e3:SetType(EFFECT_TYPE_IGNITION)
 	e3:SetRange(LOCATION_MZONE)
 	e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
-	e3:SetCountLimit(1,100318041)
-	e3:SetCost(c100318041.spcost)
-	e3:SetTarget(c100318041.sptg)
-	e3:SetOperation(c100318041.spop)
+	e3:SetCountLimit(1,46947713)
+	e3:SetCost(c46947713.spcost)
+	e3:SetTarget(c46947713.sptg)
+	e3:SetOperation(c46947713.spop)
 	c:RegisterEffect(e3)
-	Duel.AddCustomActivityCounter(100318041,ACTIVITY_SPSUMMON,c100318041.counterfilter)
+	Duel.AddCustomActivityCounter(46947713,ACTIVITY_SPSUMMON,c46947713.counterfilter)
 end
-function c100318041.counterfilter(c)
+function c46947713.counterfilter(c)
 	return c:IsRace(RACE_CYBERSE)
 end
-function c100318041.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.GetCustomActivityCount(100318041,tp,ACTIVITY_SPSUMMON)==0 end
+function c46947713.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetCustomActivityCount(46947713,tp,ACTIVITY_SPSUMMON)==0 end
 	local e1=Effect.CreateEffect(e:GetHandler())
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_OATH)
 	e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
 	e1:SetTargetRange(1,0)
-	e1:SetTarget(c100318041.splimit)
+	e1:SetTarget(c46947713.splimit)
 	e1:SetReset(RESET_PHASE+PHASE_END)
 	Duel.RegisterEffect(e1,tp)
 end
-function c100318041.splimit(e,c,sump,sumtype,sumpos,targetp,se)
+function c46947713.splimit(e,c,sump,sumtype,sumpos,targetp,se)
 	return not c:IsRace(RACE_CYBERSE)
 end
-function c100318041.atkcon(e)
+function c46947713.atkcon(e)
 	return e:GetHandler():GetMutualLinkedGroupCount()>0
 end
-function c100318041.atktg(e,c)
+function c46947713.atktg(e,c)
 	local g=e:GetHandler():GetMutualLinkedGroup()
 	return c==e:GetHandler() or g:IsContains(c)
 end
-function c100318041.filter(c,e,tp,zone)
-	return c:IsRace(RACE_CYBERSE) and c:IsLinkBelow(3) and c:IsType(TYPE_LINK) and not c:IsCode(100318041)
+function c46947713.filter(c,e,tp,zone)
+	return c:IsRace(RACE_CYBERSE) and c:IsLinkBelow(3) and c:IsType(TYPE_LINK) and not c:IsCode(46947713)
 		and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP,tp,zone)
 end
-function c100318041.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+function c46947713.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	local zone=bit.band(e:GetHandler():GetLinkedZone(tp),0x1f)
-	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and c100318041.filter(chkc,e,tp,zone) end
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and c46947713.filter(chkc,e,tp,zone) end
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingTarget(c100318041.filter,tp,LOCATION_GRAVE,0,1,nil,e,tp,zone) end
+		and Duel.IsExistingTarget(c46947713.filter,tp,LOCATION_GRAVE,0,1,nil,e,tp,zone) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectTarget(tp,c100318041.filter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp,zone)
+	local g=Duel.SelectTarget(tp,c46947713.filter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp,zone)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
 end
-function c100318041.spop(e,tp,eg,ep,ev,re,r,rp)
+function c46947713.spop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
 	local zone=bit.band(e:GetHandler():GetLinkedZone(tp),0x1f)
 	if tc:IsRelateToEffect(e) and zone~=0 then
