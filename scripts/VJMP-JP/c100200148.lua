1c643b16aa7d66d0d71876aec9791857b0b8c098
nekrozar <nekrozar.s@gmail.com>
2018-05-08 18:07:48 +0900

    fix

--- scripts/VJMP-JP/c100200148.lua
+++ scripts/VJMP-JP/c100200148.lua
@@ -1,72 +1,72 @@
---闇鋼龍ダークネスメタル
+--闇鋼龍 ダークネスメタル
 --Darkness Metal, the Dark Steel Dragon
 --Scripted by Eerie Code
 function c100200148.initial_effect(c)
-    c:EnableReviveLimit()
-    aux.AddLinkProcedure(c,aux.FilterBoolFunctionEx(Card.IsType,TYPE_EFFECT),2,nil,c100200148.spcheck)
-    --spsummon
-    local e1=Effect.CreateEffect(c)
-    e1:SetDescription(aux.Stringid(100200148,0))
-    e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
-    e1:SetType(EFFECT_TYPE_IGNITION)
-    e1:SetRange(LOCATION_MZONE)
-    e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
-    e1:SetCountLimit(1,100200148)
-    e1:SetTarget(c100200148.sptg)
-    e1:SetOperation(c100200148.spop)
-    c:RegisterEffect(e1)
+	--link summon
+	c:EnableReviveLimit()
+	aux.AddLinkProcedure(c,aux.FilterBoolFunction(Card.IsLinkType,TYPE_EFFECT),2,nil,c100200148.spcheck)
+	--spsummon
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(100200148,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetCountLimit(1,100200148)
+	e1:SetTarget(c100200148.sptg)
+	e1:SetOperation(c100200148.spop)
+	c:RegisterEffect(e1)
 end
-function c100200148.spcheck(g,lc,tp)
-    return g:GetClassCount(Card.GetRace,lc,SUMMON_TYPE_LINK,tp)==1
-        and g:GetClassCount(Card.GetAttribute,lc,SUMMON_TYPE_LINK,tp)==1
+function c100200148.spcheck(g,lc)
+	return g:GetClassCount(Card.GetLinkRace)==1
+		and g:GetClassCount(Card.Attribute)==1
 end
 function c100200148.filter(c,e,tp,zone)
-    return (c:IsFaceup() or not c:IsLocation(LOCATION_REMOVED))
-        and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP_DEFENSE,tp,zone)
+	return (c:IsFaceup() or not c:IsLocation(LOCATION_REMOVED)) and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP_DEFENSE,tp,zone)
 end
 function c100200148.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-    local zone=e:GetHandler():GetLinkedZone(tp)&0x1f
-    if chkc then return chkc:IsLocation(LOCATION_GRAVE+LOCATION_REMOVED) and chkc:IsControler(tp) and c100200148.filter(chkc,e,tp,zone) end
-    if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-        and Duel.IsExistingTarget(c100200148.filter,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,1,nil,e,tp,zone) end
-    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-    local g=Duel.SelectTarget(tp,c100200148.filter,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,1,1,nil,e,tp,zone)
-    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
+	local zone=bit.band(e:GetHandler():GetLinkedZone(tp),0x1f)
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE+LOCATION_REMOVED) and chkc:IsControler(tp) and c100200148.filter(chkc,e,tp,zone) end
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingTarget(c100200148.filter,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,1,nil,e,tp,zone) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectTarget(tp,c100200148.filter,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,1,1,nil,e,tp,zone)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
 end
 function c100200148.spop(e,tp,eg,ep,ev,re,r,rp)
-    local c=e:GetHandler()
-    local tc=Duel.GetFirstTarget()
-    local zone=c:GetLinkedZone(tp)&0x1f
-    if tc and tc:IsRelateToEffect(e) and zone~=0 
-        and Duel.SpecialSummonStep(tc,0,tp,tp,false,false,POS_FACEUP_DEFENSE,zone) then
-        local e1=Effect.CreateEffect(c)
-        e1:SetType(EFFECT_TYPE_SINGLE)
-        e1:SetCode(EFFECT_DISABLE)
-        e1:SetReset(RESET_EVENT+0xfe0000)
-        tc:RegisterEffect(e1)
-        local e2=Effect.CreateEffect(c)
-        e2:SetType(EFFECT_TYPE_SINGLE)
-        e2:SetCode(EFFECT_DISABLE_EFFECT)
-        e2:SetReset(RESET_EVENT+0xfe0000)
-        tc:RegisterEffect(e2)
-        local e3=Effect.CreateEffect(c)
-        e3:SetType(EFFECT_TYPE_SINGLE)
-        e3:SetCode(EFFECT_LEAVE_FIELD_REDIRECT)
-        e3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-        e3:SetReset(RESET_EVENT+0x47e0000)
-        e3:SetValue(LOCATION_DECKBOT)
-        tc:RegisterEffect(e3)
-        local e4=Effect.CreateEffect(c)
-	    e4:SetType(EFFECT_TYPE_FIELD)
-	    e4:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_OATH)
-	    e4:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
-	    e4:SetTargetRange(1,0)
-	    e4:SetTarget(c100200148.splimit)
-	    e4:SetReset(RESET_PHASE+PHASE_END)
-	    Duel.RegisterEffect(e4,tp)
-    end
-    Duel.SpecialSummonComplete()
+	local c=e:GetHandler()
+	local tc=Duel.GetFirstTarget()
+	local zone=bit.band(c:GetLinkedZone(tp),0x1f)
+	if tc and tc:IsRelateToEffect(e) and zone~=0
+		and Duel.SpecialSummonStep(tc,0,tp,tp,false,false,POS_FACEUP_DEFENSE,zone) then
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_DISABLE)
+		e1:SetReset(RESET_EVENT+0xfe0000)
+		tc:RegisterEffect(e1)
+		local e2=Effect.CreateEffect(c)
+		e2:SetType(EFFECT_TYPE_SINGLE)
+		e2:SetCode(EFFECT_DISABLE_EFFECT)
+		e2:SetReset(RESET_EVENT+0xfe0000)
+		tc:RegisterEffect(e2)
+		local e3=Effect.CreateEffect(c)
+		e3:SetType(EFFECT_TYPE_SINGLE)
+		e3:SetCode(EFFECT_LEAVE_FIELD_REDIRECT)
+		e3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e3:SetReset(RESET_EVENT+0x47e0000)
+		e3:SetValue(LOCATION_DECKBOT)
+		tc:RegisterEffect(e3)
+	end
+	Duel.SpecialSummonComplete()
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_FIELD)
+	e4:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_OATH)
+	e4:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+	e4:SetTargetRange(1,0)
+	e4:SetTarget(c100200148.splimit)
+	e4:SetReset(RESET_PHASE+PHASE_END)
+	Duel.RegisterEffect(e4,tp)
 end
 function c100200148.splimit(e,c,sump,sumtype,sumpos,targetp,se)
-	return not c:IsType(TYPE_LINK)
+	return c:IsType(TYPE_LINK)
 end
