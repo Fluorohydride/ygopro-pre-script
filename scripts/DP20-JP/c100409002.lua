1f0bc1d080d10083ff08d3d034f02a676e98433a
dest <dest.dn@gmail.com>
2018-05-14 20:07:18 +0200

    add scripts DP20
    02 Blue-Eyes Solid Dragon
    03 Bingo Machine GO! GO!
    added card code list to old spell/trap cards that list Blue-Eyes White
    Dragon or Blue-Eyes Ultimate Dragon

---
+++ scripts/DP20-JP/c100409002.lua
@@ -0,0 +1,84 @@
+--ブルーアイズ・ソリッド・ドラゴン
+--Blue-Eyes Solid Dragon
+--Script by dest
+function c100409002.initial_effect(c)
+	--negate
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(100409002,0))
+	e1:SetCategory(CATEGORY_DISABLE)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_SUMMON_SUCCESS)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DELAY)
+	e1:SetCountLimit(1,100409002)
+	e1:SetTarget(c100409002.negtg)
+	e1:SetOperation(c100409002.negop)
+	c:RegisterEffect(e1)
+	local e2=e1:Clone()
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	c:RegisterEffect(e2)
+	--spsummon
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(100409002,1))
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetCode(EVENT_CHAINING)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1,100409002+100)
+	e2:SetCondition(c100409002.spcon)
+	e2:SetTarget(c100409002.sptg)
+	e2:SetOperation(c100409002.spop)
+	c:RegisterEffect(e2)
+end
+function c100409002.negfilter(c)
+	return c:IsType(TYPE_MONSTER) and aux.disfilter1(c)
+end
+function c100409002.negtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsOnField() and c100409002.negfilter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(c100409002.negfilter,tp,0,LOCATION_MZONE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	local g=Duel.SelectTarget(tp,c100409002.negfilter,tp,0,LOCATION_MZONE,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_DISABLE,g,1,0,0)
+end
+function c100409002.negop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tc=Duel.GetFirstTarget()
+	if ((tc:IsFaceup() and not tc:IsDisabled()) or tc:IsType(TYPE_TRAPMONSTER)) and tc:IsRelateToEffect(e) then
+		Duel.NegateRelatedChain(tc,RESET_TURN_SET)
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e1:SetCode(EFFECT_DISABLE)
+		e1:SetReset(RESET_EVENT+0x1fe0000)
+		tc:RegisterEffect(e1)
+		local e2=e1:Clone()
+		e2:SetCode(EFFECT_DISABLE_EFFECT)
+		e2:SetValue(RESET_TURN_SET)
+		tc:RegisterEffect(e2)
+		if tc:IsType(TYPE_TRAPMONSTER) then
+			local e3=e1:Clone()
+			e3:SetCode(EFFECT_DISABLE_TRAPMONSTER)
+			tc:RegisterEffect(e3)
+		end
+	end
+end
+function c100409002.spcon(e,tp,eg,ep,ev,re,r,rp)
+	return rp~=tp
+end
+function c100409002.spfilter(c,e,tp)
+	return c:IsCode(89631139) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function c100409002.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>=0 and e:GetHandler():IsAbleToDeck()
+		and Duel.IsExistingMatchingCard(c100409002.spfilter,tp,LOCATION_DECK,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
+end
+function c100409002.spop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<0 then return end
+	if Duel.SendtoDeck(e:GetHandler(),nil,2,REASON_EFFECT)~=0 then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+		local g=Duel.SelectMatchingCard(tp,c100409002.spfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
+		if g:GetCount()>0 then
+			Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+		end
+	end
+end
