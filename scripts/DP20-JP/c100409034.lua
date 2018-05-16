9155c9a385ca280f9553d4a073513c4cb0d65127
dest <dest.dn@gmail.com>
2018-05-16 14:57:50 +0200

    fix

--- scripts/DP20-JP/c100409034.lua
+++ scripts/DP20-JP/c100409034.lua
@@ -5,10 +5,10 @@ function c100409034.initial_effect(c)
 	--xyz summon
 	aux.AddXyzProcedure(c,nil,4,2)
 	c:EnableReviveLimit()
-	--atkup
+	--special summon (hand)
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(100409034,0))
-	e1:SetCategory(CATEGORY_ATKCHANGE)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
 	e1:SetProperty(EFFECT_FLAG_DELAY)
@@ -37,7 +37,7 @@ function c100409034.initial_effect(c)
 	e3:SetTarget(aux.TargetBoolFunction(Card.IsAttackAbove,2000))
 	e3:SetValue(aux.tgoval)
 	c:RegisterEffect(e3)
-	--special summon
+	--special summon (gy/banished)
 	local e4=Effect.CreateEffect(c)
 	e4:SetDescription(aux.Stringid(100409034,1))
 	e4:SetCategory(CATEGORY_SPECIAL_SUMMON)
@@ -85,11 +85,11 @@ function c100409034.spfilter2(c,e,tp)
 	return (c:IsLocation(LOCATION_GRAVE) or c:IsFaceup()) and c:IsCode(93717133) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function c100409034.sptg2(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_GRAVE) and c100409034.spfilter2(chkc,e,tp) end
+	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_GRAVE+LOCATION_REMOVED) and c100409034.spfilter2(chkc,e,tp) end
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingTarget(c100409034.spfilter2,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
+		and Duel.IsExistingTarget(c100409034.spfilter2,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,1,nil,e,tp) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectTarget(tp,c100409034.spfilter2,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
+	local g=Duel.SelectTarget(tp,c100409034.spfilter2,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,1,1,nil,e,tp)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
 end
 function c100409034.spop2(e,tp,eg,ep,ev,re,r,rp)
