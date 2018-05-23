97d3f9231fe58bfc9263523c146664a3550536fd
dest <dest.dn@gmail.com>
2018-05-23 22:52:02 +0200

    add DP20 27 Black Bird Close

---
+++ scripts/DP20-JP/c100409027.lua
@@ -0,0 +1,60 @@
+--ブラック・バード・クローズ
+--Black Bird Close
+--Script by dest
+function c100409027.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_NEGATE+CATEGORY_DESTROY)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_CHAINING)
+	e1:SetCondition(c100409027.condition)
+	e1:SetCost(c100409027.cost)
+	e1:SetTarget(c100409027.target)
+	e1:SetOperation(c100409027.activate)
+	c:RegisterEffect(e1)
+	--act in hand
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetCode(EFFECT_TRAP_ACT_IN_HAND)
+	e2:SetCondition(c100409027.handcon)
+	c:RegisterEffect(e2)
+end
+function c100409027.condition(e,tp,eg,ep,ev,re,r,rp)
+	local loc=Duel.GetChainInfo(ev,CHAININFO_TRIGGERING_LOCATION)
+	return ep~=tp and loc==LOCATION_MZONE and re:IsActiveType(TYPE_MONSTER) and Duel.IsChainNegatable(ev)
+end
+function c100409027.discfilter(c)
+	return c:IsFaceup() and c:IsSetCard(0x33) and c:IsAbleToGraveAsCost()
+end
+function c100409027.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c100409027.discfilter,tp,LOCATION_MZONE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local g=Duel.SelectMatchingCard(tp,c100409027.discfilter,tp,LOCATION_MZONE,0,1,1,nil)
+	Duel.SendtoGrave(g,REASON_COST)
+end
+function c100409027.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
+	if re:GetHandler():IsDestructable() and re:GetHandler():IsRelateToEffect(re) then
+		Duel.SetOperationInfo(0,CATEGORY_DESTROY,eg,1,0,0)
+	end
+end
+function c100409027.sfilter(c,e,tp)
+	return c:IsCode(9012916) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function c100409027.activate(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.NegateActivation(ev) and re:GetHandler():IsRelateToEffect(re)
+		and Duel.Destroy(eg,REASON_EFFECT)~=0 then
+		local sc=Duel.GetFirstMatchingCard(c100409027.sfilter,tp,LOCATION_EXTRA,0,nil,e,tp)
+		if sc and Duel.GetLocationCountFromEx(tp)>0 and Duel.SelectYesNo(tp,aux.Stringid(100409027,0)) then
+			Duel.BreakEffect()
+			Duel.SpecialSummon(sc,0,tp,tp,false,false,POS_FACEUP)
+		end
+	end
+end
+function c100409027.cfilter(c)
+	return c:IsFaceup() and ((c:IsSetCard(0x33) and c:IsType(TYPE_SYNCHRO)) or c:IsCode(9012916))
+end
+function c100409027.handcon(e)
+	return Duel.IsExistingMatchingCard(c100409027.filter,e:GetHandlerPlayer(),LOCATION_MZONE,0,1,nil)
+end
