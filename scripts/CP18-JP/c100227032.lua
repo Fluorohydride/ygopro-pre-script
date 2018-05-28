29822772fefff05031e7c1f04c75056a3806b3bb
mercury233 <me@mercury233.me>
2018-05-28 13:54:52 +0800

    fix

--- scripts/CP18-JP/c100227032.lua
+++ scripts/CP18-JP/c100227032.lua
@@ -57,7 +57,7 @@ end
 function c100227032.xop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local tc=Duel.GetFirstTarget()
-	if c:IsRelateToEffect(e) and tc:IsRelateToEffect(e) then
+	if c:IsRelateToEffect(e) and tc:IsRelateToEffect(e) and not tc:IsImmuneToEffect(e) then
 		local mg=c:GetOverlayGroup()
 		if mg:GetCount()>0 then Duel.Overlay(tc,mg) end
 		Duel.Overlay(tc,Group.FromCards(c))
