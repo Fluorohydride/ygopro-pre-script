5daf0017532594ebedd02a25826d322a3b0ca31e
mercury233 <me@mercury233.me>
2018-05-29 17:06:34 +0800

    fix

--- scripts/DP20-JP/c100409027.lua
+++ scripts/DP20-JP/c100409027.lua
@@ -56,5 +56,5 @@ function c100409027.cfilter(c)
 	return c:IsFaceup() and ((c:IsSetCard(0x33) and c:IsType(TYPE_SYNCHRO)) or c:IsCode(9012916))
 end
 function c100409027.handcon(e)
-	return Duel.IsExistingMatchingCard(c100409027.filter,e:GetHandlerPlayer(),LOCATION_MZONE,0,1,nil)
+	return Duel.IsExistingMatchingCard(c100409027.cfilter,e:GetHandlerPlayer(),LOCATION_MZONE,0,1,nil)
 end
