1596aa45953ddbebc432af7b8698c6348b014acf
EerieCode <eeriecode@gmail.com>
2018-06-07 10:36:59 +0200

    DP20-JP048 --魔界劇場「ファンタスティックシアター」

---
+++ scripts/DP20-JP/c100409048.lua
@@ -0,0 +1,97 @@
+--魔界劇場「ファンタスティックシアター」
+--Abyss Playhouse – Fantastic Theater
+--Scripted by ahtelel
+function c100409048.initial_effect(c)
+	--activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e1)
+	--to hand
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(100409048,0))
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_FZONE)
+	e2:SetCountLimit(1,100409048)
+	e2:SetCost(c100409048.thcost)
+	e2:SetTarget(c100409048.thtg)
+	e2:SetOperation(c100409048.thop)
+	c:RegisterEffect(e2)
+	--change effect
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e3:SetCode(EVENT_CHAINING)
+	e3:SetRange(LOCATION_FZONE)
+	e3:SetCountLimit(1)
+	e3:SetCondition(c100409048.chcon1)
+	e3:SetOperation(c100409048.chop1)
+	c:RegisterEffect(e3)
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e4:SetCode(EVENT_CHAIN_ACTIVATING)
+	e4:SetRange(LOCATION_FZONE)
+	e4:SetCountLimit(1,100409148)
+	e4:SetCondition(c100409048.chcon2)
+	e4:SetOperation(c100409048.chop2)
+	c:RegisterEffect(e4)
+end
+function c100409048.cfilter(c)
+	return c:IsSetCard(0x10ec) and c:IsType(TYPE_PENDULUM) and not c:IsPublic()
+end
+function c100409048.cfilter2(c)
+	return c:IsSetCard(0x20ec) and c:IsType(TYPE_SPELL) and not c:IsPublic()
+		and Duel.IsExistingMatchingCard(c100409048.cfilter3,tp,LOCATION_DECK,0,1,nil,e,tp,c:GetCode())		
+end
+function c100409048.cfilter3(c)
+	return c:IsSetCard(0x20ec) and c:IsType(TYPE_SPELL) and not c:IsCode(code) and c:IsAbleToHand()
+end
+function c100409048.thcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c100409048.cfilter,tp,LOCATION_HAND,0,1,nil)
+		and Duel.IsExistingMatchingCard(c100409048.cfilter2,tp,LOCATION_HAND,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CONFIRM)
+	local g1=Duel.SelectMatchingCard(tp,c100409048.cfilter,tp,LOCATION_HAND,0,1,1,nil)
+	local g2=Duel.SelectMatchingCard(tp,c100409048.cfilter2,tp,LOCATION_HAND,0,1,1,nil)
+	g1:AddCard(g2)
+	Duel.ConfirmCards(1-tp,g1)
+	Duel.ShuffleHand(tp)
+end
+function c100409048.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c100409048.cfilter3,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function c100409048.thop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,c100409048.cfilter3,tp,LOCATION_DECK,0,1,1,nil)
+	if g:GetCount()>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end
+function c100409048.chcon1(e,tp,eg,ep,ev,re,r,rp)
+	return ep~=tp and re:GetHandler():GetType()==TYPE_MONSTER and re:IsHasType(EFFECT_TYPE_ACTIVATE)
+		and Duel.IsExistingMatchingCard(c100409048.confilter,tp,LOCATION_MZONE,0,1,nil)
+end
+function c100409048.chop1(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():RegisterFlagEffect(100409048,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,0,1)
+end
+function c100409048.confilter(c)
+	return c:IsFaceup() and c:IsSetCard(0x10ec) and c:IsSummonType(SUMMON_TYPE_PENDULUM)
+end
+function c100409048.chcon2(e,tp,eg,ep,ev,re,r,rp)
+	return ev==1 and e:GetHandler():GetFlagEffect(100409048)>0
+		and Duel.IsExistingMatchingCard(c100409048.confilter,tp,LOCATION_MZONE,0,1,nil)
+end
+function c100409048.chop2(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.ChangeChainOperation(1,c100409048.repop)
+end
+function c100409048.desfilter(c)
+	return c:IsFacedown() and c:IsType(TYPE_SPELL+TYPE_TRAP)
+end
+function c100409048.repop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local g=Duel.SelectMatchingCard(tp,c100409048.desfilter,tp,0,LOCATION_ONFIELD,1,1,nil)
+	if g:GetCount()>0 then
+		Duel.HintSelection(g)
+		Duel.Destroy(g,REASON_EFFECT)
+	end
+end
