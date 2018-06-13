a3a052d30795fdbda6b0332b9ee2ee037cc692a9
dest <dest.dn@gmail.com>
2018-06-13 15:30:14 +0200

    add Scripts SD34
    01 Latency
    02 Swap Cleric

---
+++ scripts/SD34-JP/c100334002.lua
@@ -0,0 +1,36 @@
+--スワップリースト
+--Swap Cleric
+--Script by dest
+function c100334002.initial_effect(c)
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(100334002,0))
+	e1:SetCategory(CATEGORY_DRAW+CATEGORY_ATKCHANGE)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetProperty(EFFECT_FLAG_DELAY)
+	e1:SetCode(EVENT_BE_MATERIAL)
+	e1:SetCountLimit(1,100334002)
+	e1:SetCondition(c100334002.drcon)
+	e1:SetTarget(c100334002.drtg)
+	e1:SetOperation(c100334002.drop)
+	c:RegisterEffect(e1)
+end
+function c100334002.drcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsLocation(LOCATION_GRAVE) and r==REASON_LINK
+end
+function c100334002.drtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsPlayerCanDraw(tp,1) and e:GetHandler():GetReasonCard():IsAttackAbove(500) end
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
+end
+function c100334002.drop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local rc=c:GetReasonCard()
+	if rc:GetAttack()<500 or rc:IsImmuneToEffect(e) or not rc:IsRelateToEffect(e) then return end
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_UPDATE_ATTACK)
+	e1:SetValue(-500)
+	e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+	rc:RegisterEffect(e1)
+	Duel.BreakEffect()
+	Duel.Draw(tp,1,REASON_EFFECT)
+end
