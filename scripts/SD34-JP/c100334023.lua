5d63ddcfeb2b3a7e34672c072d20b15b1a909260
dest <dest.dn@gmail.com>
2018-06-13 18:15:06 +0200

    add Scripts SD34
    23 Cynet Optimize
    24 Cynet Codec
    32 Cynet Conflict

---
+++ scripts/SD34-JP/c100334023.lua
@@ -0,0 +1,68 @@
+--サイバネット・オプティマイズ
+--Cynet Optimize
+--Script by dest
+function c100334023.initial_effect(c)
+	--activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e1)
+	--summon
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(100334023,0))
+	e2:SetCategory(CATEGORY_SUMMON)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetCountLimit(1,100334023)
+	e2:SetTarget(c100334023.sumtg)
+	e2:SetOperation(c100334023.sumop)
+	c:RegisterEffect(e2)
+	--actlimit
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e3:SetCode(EFFECT_CANNOT_ACTIVATE)
+	e3:SetRange(LOCATION_SZONE)
+	e3:SetTargetRange(0,1)
+	e3:SetCondition(c100334023.actcon)
+	e3:SetValue(c100334023.aclimit)
+	c:RegisterEffect(e3)
+end
+function c100334023.sumfilter(c)
+	return c:IsRace(RACE_CYBERSE) and c:IsSummonable(true,nil)
+end
+function c100334023.sumtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c100334023.sumfilter,tp,LOCATION_HAND,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_SUMMON,nil,1,0,0)
+end
+function c100334023.sumop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SUMMON)
+	local g=Duel.SelectMatchingCard(tp,c100334023.sumfilter,tp,LOCATION_HAND,0,1,1,nil)
+	local tc=g:GetFirst()
+	if tc then
+		Duel.Summon(tp,tc,true,nil)
+	end
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetTargetRange(1,0)
+	e1:SetTarget(c100334023.splimit)
+	e1:SetReset(RESET_PHASE+PHASE_END)
+	Duel.RegisterEffect(e1,tp)
+end
+function c100334023.splimit(e,c)
+	return not c:IsRace(RACE_CYBERSE) and c:IsLocation(LOCATION_EXTRA)
+end
+function c100334023.cfilter(c,tp)
+	return c:IsFaceup() and c:IsSetCard(0x101) and c:IsControler(tp)
+end
+function c100334023.actcon(e)
+	local tp=e:GetHandlerPlayer()
+	local a=Duel.GetAttacker()
+	local d=Duel.GetAttackTarget()
+	return (a and c100334023.cfilter(a,tp)) or (d and c100334023.cfilter(d,tp))
+end
+function c100334023.aclimit(e,re,tp)
+	return not re:GetHandler():IsImmuneToEffect(e)
+end
