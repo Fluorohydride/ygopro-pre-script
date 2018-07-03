dd04b6fe89eb9cd4493673422b44cc494de99dd5
destdev <dest.dn@gmail.com>
2018-07-03 22:08:21 +0200

    add scripts CYHO
    82 Danger! Bigfoot!
    83 Danger! Nessie!
    84 Danger! Chupacabra!
    85 Danger!? Jackalope?
    86 Realm of Danger!
    87 Danger! Zone

---
+++ scripts/CYHO-EN/c101005083.lua
@@ -0,0 +1,78 @@
+--Danger! Nessie!
+--Script by dest
+function c101005083.initial_effect(c)
+	--special summon
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(101005083,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_HANDES+CATEGORY_DRAW)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCost(c101005083.spcost)
+	e1:SetTarget(c101005083.sptg)
+	e1:SetOperation(c101005083.spop)
+	c:RegisterEffect(e1)
+	--search
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(101005083,1))
+	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
+	e2:SetCode(EVENT_TO_GRAVE)
+	e2:SetCountLimit(1,101005083)
+	e2:SetCondition(c101005083.thcon)
+	e2:SetTarget(c101005083.thtg)
+	e2:SetOperation(c101005083.thop)
+	c:RegisterEffect(e2)
+	local e3=e2:Clone()
+	e3:SetCode(EVENT_REMOVE)
+	c:RegisterEffect(e3)
+end
+function c101005083.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return not e:GetHandler():IsPublic() end
+end
+function c101005083.spfilter(c,e,tp)
+	return c:IsCode(101005083) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function c101005083.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsPlayerCanDraw(tp,1) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,1,nil)
+		and Duel.IsExistingMatchingCard(c101005083.spfilter,tp,LOCATION_HAND,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_HANDES,nil,0,tp,1)
+end
+function c101005083.spop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local g=Duel.GetFieldGroup(tp,LOCATION_HAND,0)
+	local tc=g:RandomSelect(1-tp,1):GetFirst()
+	if Duel.SendtoGrave(tc,REASON_DISCARD+REASON_EFFECT)~=0 and not tc:IsCode(101005083)
+		and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 then
+		local spg=Duel.GetMatchingGroup(c101005083.spfilter,tp,LOCATION_HAND,0,nil,e,tp)
+		if spg:GetCount()<=0 then return end
+		local sg=spg:GetFirst()
+		if spg:GetCount()~=1 then
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+			sg=spg:Select(tp,1,1,nil)
+		end
+		Duel.BreakEffect()
+		if Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP)~=0 then
+			Duel.Draw(tp,1,REASON_EFFECT)
+		end
+	end
+end
+function c101005083.thcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsPreviousLocation(LOCATION_HAND) and bit.band(r,REASON_DISCARD)~=0
+end
+function c101005083.thfilter(c)
+	return c:IsSetCard(0x223) and not c:IsCode(101005083) and c:IsAbleToHand()
+end
+function c101005083.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c101005083.thfilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function c101005083.tgop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,c101005083.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if g:GetCount()>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end
---
+++ scripts/CYHO-EN/c101005085.lua
@@ -0,0 +1,79 @@
+--Danger!? Jackalope?
+--Script by dest
+function c101005085.initial_effect(c)
+	--special summon (self)
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(101005085,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_HANDES+CATEGORY_DRAW)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCost(c101005085.spcost)
+	e1:SetTarget(c101005085.sptg)
+	e1:SetOperation(c101005085.spop)
+	c:RegisterEffect(e1)
+	--special summon (deck)
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(101005085,1))
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
+	e2:SetCode(EVENT_TO_GRAVE)
+	e2:SetCountLimit(1,101005085)
+	e2:SetCondition(c101005085.spcon)
+	e2:SetTarget(c101005085.sptg2)
+	e2:SetOperation(c101005085.spop2)
+	c:RegisterEffect(e2)
+	local e3=e2:Clone()
+	e3:SetCode(EVENT_REMOVE)
+	c:RegisterEffect(e3)
+end
+function c101005085.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return not e:GetHandler():IsPublic() end
+end
+function c101005085.spfilter(c,e,tp)
+	return c:IsCode(101005085) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function c101005085.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsPlayerCanDraw(tp,1) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,1,nil)
+		and Duel.IsExistingMatchingCard(c101005085.spfilter,tp,LOCATION_HAND,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_HANDES,nil,0,tp,1)
+end
+function c101005085.spop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local g=Duel.GetFieldGroup(tp,LOCATION_HAND,0)
+	local tc=g:RandomSelect(1-tp,1):GetFirst()
+	if Duel.SendtoGrave(tc,REASON_DISCARD+REASON_EFFECT)~=0 and not tc:IsCode(101005085)
+		and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 then
+		local spg=Duel.GetMatchingGroup(c101005085.spfilter,tp,LOCATION_HAND,0,nil,e,tp)
+		if spg:GetCount()<=0 then return end
+		local sg=spg:GetFirst()
+		if spg:GetCount()~=1 then
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+			sg=spg:Select(tp,1,1,nil)
+		end
+		Duel.BreakEffect()
+		if Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP)~=0 then
+			Duel.Draw(tp,1,REASON_EFFECT)
+		end
+	end
+end
+function c101005085.spcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsPreviousLocation(LOCATION_HAND) and bit.band(r,REASON_DISCARD)~=0
+end
+function c101005085.spfilter2(c,e,tp)
+	return c:IsSetCard(0x223) and not c:IsCode(101005085) and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP_DEFENSE)
+end
+function c101005085.sptg2(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(c101005085.spfilter2,tp,LOCATION_DECK,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
+end
+function c101005085.spop2(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,c101005085.spfilter2,tp,LOCATION_DECK,0,1,1,nil,e,tp)
+	if g:GetCount()>0 then
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP_DEFENSE)
+	end
+end
