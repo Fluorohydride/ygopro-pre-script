7511cfe43cce68813145aa7b3e5ca2eeb073f8ed
mercury233 <me@mercury233.me>
2018-07-08 19:04:58 +0800

    fix

--- scripts/SOFU-JP/c101006060.lua
+++ scripts/SOFU-JP/c101006060.lua
@@ -4,7 +4,7 @@
 function c101006060.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_FUSION_SUMMON)
+	e1:SetCategory(CATEGORY_TODECK+CATEGORY_SPECIAL_SUMMON+CATEGORY_FUSION_SUMMON)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetCountLimit(1,101006060)
@@ -50,6 +50,7 @@ function c101006060.target(e,tp,eg,ep,ev,re,r,rp,chk)
 		return res
 	end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
+	Duel.SetOperationInfo(0,CATEGORY_TODECK,nil,1,tp,LOCATION_ONFIELD+LOCATION_GRAVE+LOCATION_REMOVED)
 end
 function c101006060.activate(e,tp,eg,ep,ev,re,r,rp)
 	local chkf=tp
