7bf2fbb5aa95c9fd25de4e8809703022eaa66416
destdev <dest.dn@gmail.com>
2018-07-13 10:04:51 +0200

    add scripts SOFU
    12 Gravekeeper's Priest
    13 Gravekeeper's Spiritualist
    55 Royal Throne of Necrovalley
    68 Sanctuary of Necrovalley

---
+++ scripts/SOFU-JP/c101006068.lua
@@ -0,0 +1,90 @@
+--ネクロバレーの神殿
+--Sanctuary of Necrovalley
+--Script by dest
+function c101006068.initial_effect(c)
+	--activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e1)
+	--atk down
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_UPDATE_ATTACK)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetTargetRange(0,LOCATION_MZONE)
+	e2:SetCondition(c101006068.atkcon)
+	e2:SetValue(500)
+	c:RegisterEffect(e2)
+	--activate Necrovalley
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(101006068,0))
+	e3:SetType(EFFECT_TYPE_QUICK_O)
+	e3:SetRange(LOCATION_SZONE)
+	e3:SetCode(EVENT_FREE_CHAIN)
+	e3:SetCountLimit(1)
+	e3:SetCondition(c101006068.accon)
+	e3:SetOperation(c101006068.acop)
+	c:RegisterEffect(e3)
+	--set
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(101006068,1))
+	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e4:SetCode(EVENT_TO_GRAVE)
+	e4:SetProperty(EFFECT_FLAG_DELAY)
+	e4:SetCondition(c8608979.setcon)
+	e4:SetTarget(c8608979.settg)
+	e4:SetOperation(c8608979.setop)
+	c:RegisterEffect(e4)
+end
+function c101006068.cfilter(c)
+	return c:IsFaceup() and c:IsSetCard(0x2e)
+end
+function c101006068.atkcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsExistingMatchingCard(c101006068.cfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil)
+		and Duel.IsEnvironment(47355498)
+end
+function c101006068.accon(e,tp,eg,ep,ev,re,r,rp)
+	return (Duel.GetCurrentPhase()==PHASE_MAIN1 or Duel.GetCurrentPhase()==PHASE_MAIN2)
+		and not Duel.IsExistingMatchingCard(aux.TRUE,tp,LOCATION_FZONE,0,1,nil)
+end
+function c101006068.filter(c)
+	return c:IsCode(47355498) and c:GetActivateEffect() and c:GetActivateEffect():IsActivatable(tp,true,true)
+end
+function c101006068.acop(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetHandler():IsRelateToEffect(e) then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOFIELD)
+	local tc=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(c101006068.filter),tp,LOCATION_HAND+LOCATION_GRAVE,0,1,1,nil):GetFirst()
+	if tc then
+		local fc=Duel.GetFieldCard(tp,LOCATION_SZONE,5)
+		if fc then
+			Duel.SendtoGrave(fc,REASON_RULE)
+			Duel.BreakEffect()
+		end
+		Duel.MoveToField(tc,tp,tp,LOCATION_SZONE,POS_FACEUP,true)
+		local te=tc:GetActivateEffect()
+		te:UseCountLimit(tp,1,true)
+		local tep=tc:GetControler()
+		local cost=te:GetCost()
+		if cost then cost(te,tep,eg,ep,ev,re,r,rp,1) end
+		Duel.RaiseEvent(tc,4179255,te,0,tp,tp,Duel.GetCurrentChain())
+	end
+end
+function c101006068.setcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return rp==1-tp and c:IsReason(REASON_DESTROY+REASON_EFFECT) and c:GetPreviousControler()==tp
+end
+function c101006068.setfilter(c)
+	return c:IsSetCard(0x2e) and not c:IsCode(101006068) and c:IsSSetable()
+end
+function c101006068.settg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c101006068.setfilter,tp,LOCATION_DECK,0,1,nil) end
+end
+function c101006068.setop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SET)
+	local tc=Duel.SelectMatchingCard(tp,c101006068.setfilter,tp,LOCATION_DECK,0,1,1,nil):GetFirst()
+	if tc then
+		Duel.SSet(tp,tc)
+		Duel.ConfirmCards(1-tp,tc)
+	end
+end
