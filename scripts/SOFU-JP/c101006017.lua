83154de6c21525fd9796958f1f78f866d5e0ab62
mercury233 <me@mercury233.me>
2018-07-14 21:52:43 +0800

    fix

--- scripts/SOFU-JP/c101006017.lua
+++ scripts/SOFU-JP/c101006017.lua
@@ -59,11 +59,11 @@ function c101006017.spfilter2(c,e,tp)
 	return c:IsFaceup() and c:IsSetCard(0x225) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function c101006017.sptg2(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and c101006017.spfilter2(chkc,e,tp) end
+	if chkc then return chkc:IsLocation(LOCATION_REMOVED) and chkc:IsControler(tp) and c101006017.spfilter2(chkc,e,tp) end
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingTarget(c101006017.spfilter2,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
+		and Duel.IsExistingTarget(c101006017.spfilter2,tp,LOCATION_REMOVED,0,1,nil,e,tp) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectTarget(tp,c101006017.spfilter2,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
+	local g=Duel.SelectTarget(tp,c101006017.spfilter2,tp,LOCATION_REMOVED,0,1,1,nil,e,tp)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
 end
 function c101006017.spop2(e,tp,eg,ep,ev,re,r,rp)
