558dcff76e77a68191cdef5ebf6ce47f91da2f3e
EerieCode <eeriecode@gmail.com>
2018-07-16 14:28:35 +0200

    SR07-JP023 --アンデット・ネクロナイズ

---
+++ scripts/SR07-JP/c100307023.lua
@@ -0,0 +1,70 @@
+--アンデット・ネクロナイズ
+--Zombie Necronize
+--Scripted by Eerie Code
+function c100307023.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_CONTROL)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCountLimit(1,100307023)
+	e1:SetCondition(c100307023.condition)
+	e1:SetTarget(c100307023.target)
+	e1:SetOperation(c100307023.activate)
+	c:RegisterEffect(e1)
+	--set
+	local e2=Effect.CreateEffect(c)
+	e2:SetCategory(CATEGORY_TODECK)
+	e2:SetDescription(aux.Stringid(35498188,0))
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_GRAVE)
+	e2:SetCountLimit(1,100307023)
+	e2:SetTarget(c100307023.settg)
+	e2:SetOperation(c100307023.setop)
+	c:RegisterEffect(e2)
+end
+function c100307023.cfilter(c)
+	return c:IsFaceup() and c:IsLevelAbove(5) and c:IsRace(RACE_ZOMBIE)
+end
+function c100307023.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsExistingMatchingCard(c100307023.cfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil)
+end
+function c100307023.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) and chkc:IsControlerCanBeChanged() end
+	if chk==0 then return Duel.IsExistingTarget(Card.IsControlerCanBeChanged,tp,0,LOCATION_MZONE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CONTROL)
+	local g=Duel.SelectTarget(tp,Card.IsControlerCanBeChanged,tp,0,LOCATION_MZONE,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_CONTROL,g,1,0,0)
+end
+function c100307023.activate(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) then
+		Duel.GetControl(tc,tp,PHASE_END,1)
+	end
+end
+function c100307023.setfilter(c)
+	return c:IsFaceup() and c:IsRace(RACE_ZOMBIE) and c:IsAbleToDeck()
+end
+function c100307023.settg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsSSetable()
+		and Duel.IsExistingMatchingCard(c100307023.setfilter,tp,LOCATION_REMOVED,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_LEAVE_GRAVE,e:GetHandler(),1,0,0)
+end
+function c100307023.setop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
+	local g=Duel.SelectMatchingCard(tp,c100307023.setfilter,tp,LOCATION_REMOVED,0,1,1,nil)
+	if #g>0 and Duel.SendtoDeck(g,nil,2,REASON_EFFECT)>0
+		and c:IsRelateToEffect(e) and c:IsSSetable() then
+		Duel.SSet(tp,c)
+		Duel.ConfirmCards(1-tp,c)
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_LEAVE_FIELD_REDIRECT)
+		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e1:SetReset(RESET_EVENT+0x47e0000)
+		e1:SetValue(LOCATION_REMOVED)
+		c:RegisterEffect(e1)
+	end
+end
