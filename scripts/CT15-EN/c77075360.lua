02d7e90e00ca028a0d09f40bf6d217e50b8a92fb
mercury233 <me@mercury233.me>
2018-09-08 19:04:39 +0800

    change id

--- scripts/CT15-EN/c77075360.lua
+++ scripts/CT15-EN/c77075360.lua
@@ -1,92 +1,92 @@
 --Junk Speeder
 --Scripted by Cybercatman and AlphaKretin
-function c100244002.initial_effect(c)
+function c77075360.initial_effect(c)
 	--synchro summon
 	aux.AddSynchroProcedure(c,aux.FilterBoolFunction(Card.IsSetCard,0x1017),aux.NonTuner(nil),1)
 	c:EnableReviveLimit()
 	--special summon
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(100244002,0))
+	e1:SetDescription(aux.Stringid(77075360,0))
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
-	e1:SetCountLimit(1,100244002)
-	e1:SetCost(c100244002.spcost)
-	e1:SetCondition(c100244002.spcon)
-	e1:SetTarget(c100244002.sptg)
-	e1:SetOperation(c100244002.spop)
+	e1:SetCountLimit(1,77075360)
+	e1:SetCost(c77075360.spcost)
+	e1:SetCondition(c77075360.spcon)
+	e1:SetTarget(c77075360.sptg)
+	e1:SetOperation(c77075360.spop)
 	c:RegisterEffect(e1)
 	--atk up
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(100244002,0))
+	e2:SetDescription(aux.Stringid(77075360,0))
 	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
 	e2:SetCode(EVENT_ATTACK_ANNOUNCE)
 	e2:SetRange(LOCATION_MZONE)
-	e2:SetCountLimit(1,100244002+100)
-	e2:SetCondition(c100244002.atkcon)
-	e2:SetOperation(c100244002.atkop)
+	e2:SetCountLimit(1,77075360+100)
+	e2:SetCondition(c77075360.atkcon)
+	e2:SetOperation(c77075360.atkop)
 	c:RegisterEffect(e2,false,1)
 	local e3=Effect.CreateEffect(c)
 	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
 	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
 	e3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-	e3:SetCondition(c100244002.spcon)
-	e3:SetOperation(c100244002.regop)
+	e3:SetCondition(c77075360.spcon)
+	e3:SetOperation(c77075360.regop)
 	c:RegisterEffect(e3)
-	Duel.AddCustomActivityCounter(100244002,ACTIVITY_SPSUMMON,c100244002.counterfilter)
+	Duel.AddCustomActivityCounter(77075360,ACTIVITY_SPSUMMON,c77075360.counterfilter)
 end
-c100244002.material_setcode=0x1017
-function c100244002.counterfilter(c)
+c77075360.material_setcode=0x1017
+function c77075360.counterfilter(c)
 	return c:GetSummonLocation()~=LOCATION_EXTRA or c:IsType(TYPE_SYNCHRO)
 end
-function c100244002.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.GetCustomActivityCount(100244002,tp,ACTIVITY_SPSUMMON)==0 end
+function c77075360.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetCustomActivityCount(77075360,tp,ACTIVITY_SPSUMMON)==0 end
 	local e1=Effect.CreateEffect(e:GetHandler())
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_OATH)
 	e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
 	e1:SetTargetRange(1,0)
-	e1:SetTarget(c100244002.splimit)
+	e1:SetTarget(c77075360.splimit)
 	e1:SetReset(RESET_PHASE+PHASE_END)
 	Duel.RegisterEffect(e1,tp)
 end
-function c100244002.splimit(e,c,sump,sumtype,sumpos,targetp,se)
+function c77075360.splimit(e,c,sump,sumtype,sumpos,targetp,se)
 	return not c:IsType(TYPE_SYNCHRO) and c:IsLocation(LOCATION_EXTRA)
 end
-function c100244002.spcon(e,tp,eg,ep,ev,re,r,rp)
+function c77075360.spcon(e,tp,eg,ep,ev,re,r,rp)
 	return e:GetHandler():IsSummonType(SUMMON_TYPE_SYNCHRO)
 end
-function c100244002.filter(c,e,tp)
+function c77075360.filter(c,e,tp)
 	return c:IsSetCard(0x1017) and c:IsType(TYPE_TUNER) and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP_DEFENSE)
 end
-function c100244002.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+function c77075360.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then
 		local ct=Duel.GetLocationCount(tp,LOCATION_MZONE,tp,LOCATION_REASON_TOFIELD)
-		return ct>0 and Duel.IsExistingMatchingCard(c100244002.filter,tp,LOCATION_DECK,0,1,nil,e,tp)
+		return ct>0 and Duel.IsExistingMatchingCard(c77075360.filter,tp,LOCATION_DECK,0,1,nil,e,tp)
 	end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
 end
-function c100244002.rescon(sg,e,tp,mg)
+function c77075360.rescon(sg,e,tp,mg)
 	return aux.ChkfMMZ(#sg)(sg,e,tp,mg) and sg:GetClassCount(Card.GetLevel)==#sg
 end
-function c100244002.spop(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.GetMatchingGroup(c100244002.filter,tp,LOCATION_DECK,0,nil,e,tp)
+function c77075360.spop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(c77075360.filter,tp,LOCATION_DECK,0,nil,e,tp)
 	local ct=math.min(Duel.GetLocationCount(tp,LOCATION_MZONE),g:GetClassCount(Card.GetLevel))
 	if ct<=0 then return end
 	if Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) then ct=1 end
-	local sg=aux.SelectUnselectGroup(g,e,tp,ct,ct,c100244002.rescon,1,tp,HINTMSG_SPSUMMON)
+	local sg=aux.SelectUnselectGroup(g,e,tp,ct,ct,c77075360.rescon,1,tp,HINTMSG_SPSUMMON)
 	if #sg>0 then
 		Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP_DEFENSE)
 	end
 end
-function c100244002.regop(e,tp,eg,ep,ev,re,r,rp)
-	e:GetHandler():RegisterFlagEffect(100244002,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,0,1)
+function c77075360.regop(e,tp,eg,ep,ev,re,r,rp)
+	e:GetHandler():RegisterFlagEffect(77075360,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,0,1)
 end
-function c100244002.atkcon(e,tp,eg,ep,ev,re,r,rp)
+function c77075360.atkcon(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	return c:GetFlagEffect(100244002)~=0 and (c==Duel.GetAttacker() and Duel.GetAttackTarget()~=nil) or c==Duel.GetAttackTarget()
+	return c:GetFlagEffect(77075360)~=0 and (c==Duel.GetAttacker() and Duel.GetAttackTarget()~=nil) or c==Duel.GetAttackTarget()
 end
-function c100244002.atkop(e,tp,eg,ep,ev,re,r,rp)
+function c77075360.atkop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if c:IsRelateToEffect(e) and c:IsFaceup() then
 		local e1=Effect.CreateEffect(c)
