6ce66fff2261ccfbe3012ac24cfd8469636415de
mercury233 <me@mercury233.me>
2018-10-08 21:51:45 +0800

    fix

--- scripts/LEHD-EN/c100234002.lua
+++ scripts/LEHD-EN/c100234002.lua
@@ -1,5 +1,5 @@
 --Gullveig of the Nordic Ascendant
---Scripted by Eerie Code
+--Scripted by Eerie Code, mod by mercury233
 function c100234002.initial_effect(c)
 	c:EnableReviveLimit()
 	aux.AddLinkProcedure(c,c100234002.matfilter,1,1)
@@ -41,28 +41,48 @@ end
 function c100234002.spcon(e,tp,eg,ep,ev,re,r,rp)
 	return e:GetHandler():IsSummonType(SUMMON_TYPE_LINK)
 end
+function c100234002.rmfilter(c,tp)
+	return Duel.GetMZoneCount(tp,c)>0
+end
 function c100234002.spfilter(c,e,tp)
 	return c:IsSetCard(0x42) and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP_DEFENSE)
 end
-function c100234002.spcheck(sg,e,tp,mg)
-	return Duel.GetMZoneCount(tp,sg,tp,LOCATION_REASON_TOFIELD)>=#sg and Duel.IsExistingMatchingCard(c100234002.spfilter,tp,LOCATION_DECK,0,#sg,nil,e,tp)
-end
 function c100234002.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local g=Duel.GetMatchingGroup(Card.IsAbleToRemove,tp,LOCATION_HAND+LOCATION_ONFIELD,0,nil)
-	if chk==0 then return aux.SelectUnselectGroup(g,e,tp,1,3,c100234002.spcheck,0) end
+	if chk==0 then return Duel.IsExistingMatchingCard(c100234002.rmfilter,tp,LOCATION_HAND+LOCATION_ONFIELD,0,1,nil,tp)
+		and Duel.IsExistingMatchingCard(c100234002.spfilter,tp,LOCATION_DECK,0,1,nil,e,tp) end
 	Duel.SetOperationInfo(0,CATEGORY_REMOVE,g,1,0,0)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
 end
+function c100234002.fselect(c,tp,rg,sg,e)
+	sg:AddCard(c)
+	local res=c100234002.fgoal(tp,sg,e) or rg:IsExists(c100234002.fselect,1,sg,tp,rg,sg,e)
+	sg:RemoveCard(c)
+	return res
+end
+function c100234002.fgoal(tp,sg,e)
+	local ct=#sg
+	return ct>0 and ct<=3 and Duel.GetMZoneCount(tp,sg)>=ct
+		and Duel.IsExistingMatchingCard(c100234002.spfilter,tp,LOCATION_DECK,0,ct,nil,e,tp)
+end
 function c100234002.spop(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.GetMatchingGroup(Card.IsAbleToRemove,tp,LOCATION_HAND+LOCATION_ONFIELD,0,nil)
-	local ct=3
-	if Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) then ct=1 end
-	local rg=aux.SelectUnselectGroup(g,e,tp,1,ct,c100234002.spcheck,1,tp,HINTMSG_REMOVE)
-	if #rg>0 and Duel.Remove(rg,POS_FACEUP,REASON_EFFECT)~=0 then
-		ct=math.min(#(Duel.GetOperatedGroup()),Duel.GetLocationCount(tp,LOCATION_MZONE))
+	local rg=Duel.GetMatchingGroup(Card.IsAbleToRemove,tp,LOCATION_HAND+LOCATION_ONFIELD,0,nil)
+	local sg=Group.CreateGroup()
+	local min=1
+	for i=0,2 do
+		local cg=rg:Filter(c100234002.fselect,sg,tp,rg,sg,e)
+		if #cg==0 then break end
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+		local g=cg:Select(tp,min,1,nil)
+		if #g==0 then break end
+		sg:Merge(g)
+		min=0
+	end
+	if #sg>0 and Duel.Remove(sg,POS_FACEUP,REASON_EFFECT)~=0 then
+		local ct=#(Duel.GetOperatedGroup())
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-		local sg=Duel.SelectMatchingCard(tp,c100234002.spfilter,tp,LOCATION_DECK,0,ct,ct,nil,e,tp)
-		Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP_DEFENSE)
+		local tg=Duel.SelectMatchingCard(tp,c100234002.spfilter,tp,LOCATION_DECK,0,ct,ct,nil,e,tp)
+		Duel.SpecialSummon(tg,0,tp,tp,false,false,POS_FACEUP_DEFENSE)
 	end
 	local e1=Effect.CreateEffect(e:GetHandler())
 	e1:SetType(EFFECT_TYPE_FIELD)
