98959463dac3ff88ddb7767b1a7fcf99e023c9ab
nekrozar <nekrozar.s@gmail.com>
2018-10-08 11:47:18 +0900

    add script SAST

---
+++ scripts/SAST-JP/c101007011.lua
@@ -0,0 +1,43 @@
+--TG タンク・ラーヴァ
+--T.G. Tank Larva
+--Script by nekrozar
+function c101007011.initial_effect(c)
+	--nontuner
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCode(EFFECT_NONTUNER)
+	e1:SetValue(c101007011.tnval)
+	c:RegisterEffect(e1)
+	--token
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(101007011,0))
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOKEN)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_BE_MATERIAL)
+	e2:SetCountLimit(1,101007011)
+	e2:SetCondition(c101007011.tkcon)
+	e2:SetTarget(c101007011.tktg)
+	e2:SetOperation(c101007011.tkop)
+	c:RegisterEffect(e2)
+end
+function c101007011.tnval(e,c)
+	return c:IsSetCard(0x27)
+end
+function c101007011.tkcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsLocation(LOCATION_GRAVE) and r==REASON_SYNCHRO
+		and e:GetHandler():GetReasonCard():IsSetCard(0x27)
+end
+function c101007011.tktg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsPlayerCanSpecialSummonMonster(tp,101007111,0x27,0x4011,0,0,1,RACE_MACHINE,ATTRIBUTE_EARTH) end
+	Duel.SetOperationInfo(0,CATEGORY_TOKEN,nil,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,0,0)
+end
+function c101007011.tkop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	if not Duel.IsPlayerCanSpecialSummonMonster(tp,101007111,0x27,0x4011,0,0,1,RACE_MACHINE,ATTRIBUTE_EARTH) then return end
+	local token=Duel.CreateToken(tp,101007111)
+	Duel.SpecialSummon(token,0,tp,tp,false,false,POS_FACEUP)
+end
---
+++ scripts/SAST-JP/c101007028.lua
@@ -0,0 +1,45 @@
+--炎天禍サンバーン
+--Sunburn the Sweltering Solar Calamity
+--Script by nekrozar
+function c101007028.initial_effect(c)
+	--special summon
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(101007028,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_DAMAGE)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
+	e1:SetCode(EVENT_DESTROYED)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCountLimit(1,101007028)
+	e1:SetCondition(c101007028.spcon)
+	e1:SetTarget(c101007028.sptg)
+	e1:SetOperation(c101007028.spop)
+	c:RegisterEffect(e1)
+end
+function c101007028.cfilter(c,tp)
+	return c:GetPreviousControler()==tp and c:IsPreviousLocation(LOCATION_MZONE)
+		and c:IsPreviousPosition(POS_FACEUP) and bit.band(c:GetPreviousAttributeOnField(),ATTRIBUTE_FIRE)~=0
+		and (c:IsReason(REASON_BATTLE) or c:IsReason(REASON_EFFECT) and c:GetReasonPlayer()==1-tp)
+end
+function c101007028.spcon(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(c101007028.cfilter,1,nil,tp)
+end
+function c101007028.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
+end
+function c101007028.damfilter(c,tp)
+	return c:IsLocation(LOCATION_GRAVE) and c:IsControler(tp) and c:GetAttack()>0 and c101007028.cfilter(c,tp)
+end
+function c101007028.spop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not c:IsRelateToEffect(e) then return end
+	local g=eg:IsExists(c101007028.damfilter,1,nil,tp)
+	if Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)>0
+		and g:GetCount()>0 and Duel.SelectYesNo(tp,aux.Stringid(101007028,1)) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SELF)
+		local sg=g:Select(tp,1,1,nil)
+		Duel.Damage(1-tp,math.ceil(sg:GetFirst():GetAttack()/2),REASON_EFFECT)
+	end
+end
