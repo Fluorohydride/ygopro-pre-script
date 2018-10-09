0e582c8381a77a881d8f19c2b3aba267112d50d8
dammy63 <mbfaih@0mel.com>
2018-10-09 13:45:20 +0900

    fix (#210)

--- scripts/SAST-JP/c101007023.lua
+++ scripts/SAST-JP/c101007023.lua
@@ -21,7 +21,6 @@ function c101007023.initial_effect(c)
 	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
 	e2:SetProperty(EFFECT_FLAG_DELAY)
 	e2:SetCountLimit(1,101007023+100)
-	e2:SetCost(c101007023.spcost2)
 	e2:SetTarget(c101007023.sptg2)
 	e2:SetOperation(c101007023.spop2)
 	c:RegisterEffect(e2)
@@ -35,10 +34,6 @@ function c101007023.initial_effect(c)
 	e3:SetTarget(c101007023.rettg)
 	e3:SetOperation(c101007023.retop)
 	c:RegisterEffect(e3)
-	Duel.AddCustomActivityCounter(101007023,ACTIVITY_SPSUMMON,c101007023.counterfilter)
-end
-function c101007023.counterfilter(c)
-	return c:IsSetCard(0x71)
 end
 function c101007023.spcon1(e,tp,eg,ep,ev,re,r,rp)
 	return not Duel.IsExistingMatchingCard(Card.IsType,tp,LOCATION_GRAVE,0,1,nil,TYPE_MONSTER)
@@ -54,32 +49,18 @@ function c101007023.spop1(e,tp,eg,ep,ev,re,r,rp)
 		Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
 	end
 end
-function c101007023.spcost2(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.GetCustomActivityCount(101007023,tp,ACTIVITY_SPSUMMON)==0 end
-	local e1=Effect.CreateEffect(e:GetHandler())
-	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-	e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
-	e1:SetReset(RESET_PHASE+PHASE_END)
-	e1:SetTargetRange(1,0)
-	e1:SetTarget(c101007023.splimit)
-	Duel.RegisterEffect(e1,tp)
-end
-function c101007023.splimit(e,c,sump,sumtype,sumpos,targetp,se)
-	return not c:IsSetCard(0x71)
-end
 function c101007023.spfilter2(c,e,tp)
 	return c:IsSetCard(0x71) and not c:IsCode(101007023) and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP)
 end
 function c101007023.sptg2(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingMatchingCard(c101007023.spfilter2,tp,LOCATION_DECK,0,1,nil,e,tp) end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
+		and Duel.IsExistingMatchingCard(c101007023.spfilter2,tp,LOCATION_DECK+LOCATION_HAND,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK+LOCATION_HAND)
 end
 function c101007023.spop2(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectMatchingCard(tp,c101007023.spfilter2,tp,LOCATION_DECK,0,1,1,nil,e,tp)
+	local g=Duel.SelectMatchingCard(tp,c101007023.spfilter2,tp,LOCATION_DECK+LOCATION_HAND,0,1,1,nil,e,tp)
 	if g:GetCount()>0 and Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)>0 then
 		local tc=g:GetFirst()
 		local e1=Effect.CreateEffect(e:GetHandler())
@@ -89,6 +70,17 @@ function c101007023.spop2(e,tp,eg,ep,ev,re,r,rp)
 		e1:SetValue(-1)
 		tc:RegisterEffect(e1)
 	end
+	local e2=Effect.CreateEffect(e:GetHandler())
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e2:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+	e2:SetReset(RESET_PHASE+PHASE_END)
+	e2:SetTargetRange(1,0)
+	e2:SetTarget(c101007023.splimit)
+	Duel.RegisterEffect(e2,tp)
+end
+function c101007023.splimit(e,c,sump,sumtype,sumpos,targetp,se)
+	return not c:IsSetCard(0x71)
 end
 function c101007023.retcon(e,tp,eg,ep,ev,re,r,rp)
 	return e:GetHandler():IsReason(REASON_DESTROY) and e:GetHandler():GetReasonPlayer()==1-tp
