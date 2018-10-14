add19a656cdbe880085695b65896343b0f543a51
mercury233 <me@mercury233.me>
2018-10-14 20:12:35 +0800

    fix

--- scripts/SAST-JP/c101007021.lua
+++ scripts/SAST-JP/c101007021.lua
@@ -45,7 +45,7 @@ function c101007021.atktg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chk==0 then return Duel.IsExistingTarget(c101007021.tgfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil)
 		 and Duel.IsExistingMatchingCard(c101007021.filter,tp,LOCATION_DECK+LOCATION_HAND,0,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
-	Duel.SelectTarget(tp,c101007021.tgfilter,tp,LOCATION_MZONE,0,1,1,nil)
+	Duel.SelectTarget(tp,c101007021.tgfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
 end
 function c101007021.atkop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
