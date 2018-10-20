5b6dd27c41f1fa7be614e2679088cfa69a6e3446
Przemysław Adamczyk <przemyslaw.adamczyk@microsoft.wsei.edu.pl>
2018-10-20 13:30:57 +0200

    fix Gilgamesh

--- scripts/LVP2-JP/c100235066.lua
+++ scripts/LVP2-JP/c100235066.lua
@@ -56,7 +56,7 @@ function c100235066.setop(e,tp,eg,ep,ev,re,r,rp)
 	local tc1=g1:GetFirst()
 	if not tc1 then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOFIELD)
-	local g2=Duel.SelectMatchingCard(tp,c100235066.setfilter2,tp,LOCATION_DECK,0,1,1,nil,tc:GetCode())
+	local g2=Duel.SelectMatchingCard(tp,c100235066.setfilter2,tp,LOCATION_DECK,0,1,1,nil,tc1:GetCode())
 	local tc2=g2:GetFirst()
 	Duel.MoveToField(tc1,tp,tp,LOCATION_SZONE,POS_FACEUP,true)
 	Duel.MoveToField(tc2,tp,tp,LOCATION_SZONE,POS_FACEUP,true)
@@ -87,6 +87,6 @@ function c100235066.spop(e,tp,eg,ep,ev,re,r,rp)
 	if loc==0 then return end
 	local g=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(c100235066.spfilter),tp,loc,0,1,1,nil,e,tp)
 	if g:GetCount()>0 then
-		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP_DEFENSE)
 	end
 end
