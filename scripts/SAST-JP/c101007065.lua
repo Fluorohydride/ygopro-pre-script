09db3c28f0db8df08c56ea4c9aab3d3bcac5afbe
mercury233 <me@mercury233.me>
2018-10-20 17:33:04 +0800

    fix

--- scripts/SAST-JP/c101007065.lua
+++ scripts/SAST-JP/c101007065.lua
@@ -27,7 +27,7 @@ function c101007065.filter(c,e,tp,m,ft)
 	if bit.band(c:GetType(),0x81)~=0x81
 		or not c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_RITUAL,tp,false,true) then return false end
 	local mg=m:Filter(Card.IsCanBeRitualMaterial,c,c)
-	if c:IsCode(21105106) then return c:ritual_custom_condition(mg,ft) end
+	if c:IsCode(21105106) then return c:ritual_custom_condition(mg,ft,true) end
 	if ft>0 then
 		return mg:CheckWithSumGreater(Card.GetRitualLevel,c:GetLevel(),c)
 	else
@@ -57,19 +57,25 @@ function c101007065.activate(e,tp,eg,ep,ev,re,r,rp)
 	if tc then
 		mg=mg:Filter(Card.IsCanBeRitualMaterial,tc,tc)
 		local mat=nil
-		if ft>0 then
-			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
-			mat=mg:SelectWithSumGreater(tp,Card.GetRitualLevel,tc:GetLevel(),tc)
+		if tc:IsCode(21105106) then
+			tc:ritual_custom_operation(mg,true)
+			local mat=tc:GetMaterial()
+			Duel.ReleaseRitualMaterial(mat)
 		else
-			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
-			mat=mg:FilterSelect(tp,c101007065.filterF,1,1,nil,tp,mg,tc)
-			Duel.SetSelectedCard(mat)
-			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
-			local mat2=mg:SelectWithSumGreater(tp,Card.GetRitualLevel,tc:GetLevel(),tc)
-			mat:Merge(mat2)
+			if ft>0 then
+				Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
+				mat=mg:SelectWithSumGreater(tp,Card.GetRitualLevel,tc:GetLevel(),tc)
+			else
+				Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
+				mat=mg:FilterSelect(tp,c101007065.filterF,1,1,nil,tp,mg,tc)
+				Duel.SetSelectedCard(mat)
+				Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
+				local mat2=mg:SelectWithSumGreater(tp,Card.GetRitualLevel,tc:GetLevel(),tc)
+				mat:Merge(mat2)
+			end
+			tc:SetMaterial(mat)
+			Duel.ReleaseRitualMaterial(mat)
 		end
-		tc:SetMaterial(mat)
-		Duel.ReleaseRitualMaterial(mat)
 		Duel.BreakEffect()
 		Duel.SpecialSummon(tc,SUMMON_TYPE_RITUAL,tp,tp,false,true,POS_FACEUP)
 		tc:CompleteProcedure()
