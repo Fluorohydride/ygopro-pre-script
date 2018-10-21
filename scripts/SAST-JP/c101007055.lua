9baa23a14623b77d67a7862c8b21ec30753f9993
mercury233 <me@mercury233.me>
2018-10-21 23:31:32 +0800

    temp

--- scripts/SAST-JP/c101007055.lua
+++ scripts/SAST-JP/c101007055.lua
@@ -46,7 +46,7 @@ function c101007055.atkop(e,tp,eg,ep,ev,re,r,rp)
 		local e1=Effect.CreateEffect(e:GetHandler())
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_CANNOT_ATTACK)
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END+RESET_OPPO_TURN)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,2)
 		tc:RegisterEffect(e1)
 	end
 end
