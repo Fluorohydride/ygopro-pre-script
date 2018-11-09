11a5dc84f1be5a6f37963e36e4b0bc6e90dc8b2f
Przemysław Adamczyk <przemyslaw.adamczyk@microsoft.wsei.edu.pl>
2018-11-09 20:00:25 +0100

    fixed lunalights

--- scripts/DP11-JP/c100411047.lua
+++ scripts/DP11-JP/c100411047.lua
@@ -1,11 +1,12 @@
 --月光黄鼬
-
---Script by nekrozar
+--Lunalight Yellow Marten
+--scripted by Logical Nonsense
+--edited by Szefo09
 function c100411047.initial_effect(c)
-	--special summon
+	--Special summon, ignition effect
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(100411047,0))
-	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SPECIAL_SUMMON)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetRange(LOCATION_HAND+LOCATION_GRAVE)
@@ -13,36 +14,38 @@ function c100411047.initial_effect(c)
 	e1:SetTarget(c100411047.sptg)
 	e1:SetOperation(c100411047.spop)
 	c:RegisterEffect(e1)
-	--tohand
+	--Search spell/trap, optional trigger effect
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(100411047,1))
 	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
 	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e2:SetProperty(EFFECT_FLAG_DELAY)
 	e2:SetCode(EVENT_TO_GRAVE)
-	e2:SetCountLimit(1,100411147)
+	e2:SetCountLimit(1,100411047+100)
 	e2:SetCondition(c100411047.thcon)
 	e2:SetTarget(c100411047.thtg)
 	e2:SetOperation(c100411047.thop)
 	c:RegisterEffect(e2)
 end
+	--Check for Lunalight card to return to hand
 function c100411047.thfilter1(c,tp)
 	return c:IsFaceup() and c:IsSetCard(0xdf) and not c:IsCode(100411047) and c:IsAbleToHand() and Duel.GetMZoneCount(tp,c)
 end
+	--Activation legality
 function c100411047.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsOnField() and chkc:IsControler(tp) and c100411047.thfilter1(chkc,tp) end
-	if chk==0 then return e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP_DEFENSE)
+	if chkc then return chkc:IsControler(tp) and c100411047.thfilter1(chkc,tp) end
+	if chk==0 then return e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false)
 		and Duel.IsExistingTarget(c100411047.thfilter1,tp,LOCATION_ONFIELD,0,1,nil,tp) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RTOHAND)
 	local g=Duel.SelectTarget(tp,c100411047.thfilter1,tp,LOCATION_ONFIELD,0,1,1,nil,tp)
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,0,0)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
 end
+	--Performing the effect of special summoning from hand or GY by returning a "Lunalight" monster, banish if it leaves
 function c100411047.spop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local tc=Duel.GetFirstTarget()
-	if tc:IsRelateToEffect(e) and Duel.SendtoHand(tc,nil,REASON_EFFECT)~=0 and tc:IsLocation(LOCATION_HAND) and c:IsRelateToEffect(e) then
-		Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP_DEFENSE)
+	if tc:IsRelateToEffect(e) and Duel.SendtoHand(tc,nil,REASON_EFFECT)~=0 and tc:IsLocation(LOCATION_HAND) and c:IsRelateToEffect(e) and Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP_DEFENSE)~=0 then
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_LEAVE_FIELD_REDIRECT)
@@ -52,19 +55,22 @@ function c100411047.spop(e,tp,eg,ep,ev,re,r,rp)
 		c:RegisterEffect(e1,true)
 	end
 end
+	--If sent to the GY by card effect
 function c100411047.thcon(e,tp,eg,ep,ev,re,r,rp)
 	return e:GetHandler():IsReason(REASON_EFFECT)
 end
-function c100411047.thfilter2(c)
+	--Check for "Lunalight" spell/trap
+function c100411047.srchfilter(c)
 	return c:IsSetCard(0xdf) and c:IsType(TYPE_SPELL+TYPE_TRAP) and c:IsAbleToHand()
 end
 function c100411047.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(c100411047.thfilter2,tp,LOCATION_DECK,0,1,nil) end
+	if chk==0 then return Duel.IsExistingMatchingCard(c100411047.srchfilter,tp,LOCATION_DECK,0,1,nil) end
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
 end
+	--Performing the effect of adding a "Lunalight" spell/trap from deck to hand
 function c100411047.thop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-	local g=Duel.SelectMatchingCard(tp,c100411047.thfilter2,tp,LOCATION_DECK,0,1,1,nil)
+	local g=Duel.SelectMatchingCard(tp,c100411047.srchfilter,tp,LOCATION_DECK,0,1,1,nil)
 	if g:GetCount()>0 then
 		Duel.SendtoHand(g,nil,REASON_EFFECT)
 		Duel.ConfirmCards(1-tp,g)
--- scripts/DP11-JP/c100411049.lua
+++ scripts/DP11-JP/c100411049.lua
@@ -1,6 +1,8 @@
 --月光小夜曲舞踊
+--Lunalight Serenade Dance
+--Logical Nonsense
+--edited by Szefo09
 
---Script by nekrozar
 function c100411049.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
@@ -8,91 +10,104 @@ function c100411049.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	c:RegisterEffect(e1)
-	--special summon
+	--ATK gain and token
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(100411049,1))
-	e2:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOKEN+CATEGORY_ATKCHANGE)
-	e2:SetType(EFFECT_TYPE_ACTIVATE)
+	e2:SetCategory(CATEGORY_ATKCHANGE+CATEGORY_SPECIAL_SUMMON)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetType(EFFECT_TYPE_TRIGGER_O)
 	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
-	e2:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DELAY)
+	e2:SetCondition(c100411049.tkcon)
 	e2:SetTarget(c100411049.tktg)
 	e2:SetOperation(c100411049.tkop)
 	c:RegisterEffect(e2)
 	local e3=e2:Clone()
-	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e3:SetType(EFFECT_TYPE_TRIGGER_O+EFFECT_TYPE_FIELD)
 	e3:SetRange(LOCATION_SZONE)
 	c:RegisterEffect(e3)
-	--special summon
+	--Special summon from deck, quick effect
 	local e4=Effect.CreateEffect(c)
 	e4:SetDescription(aux.Stringid(100411049,2))
-	e4:SetCategory(CATEGORY_TOGRAVE+CATEGORY_SPECIAL_SUMMON)
+	e4:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e4:SetType(EFFECT_TYPE_QUICK_O)
-	e4:SetRange(LOCATION_GRAVE)
 	e4:SetCode(EVENT_FREE_CHAIN)
+	e4:SetRange(LOCATION_GRAVE)
 	e4:SetCountLimit(1,100411049)
-	e4:SetCondition(c100411049.spcon)
 	e4:SetCost(aux.bfgcost)
+	e4:SetCondition(c100411049.spcon)
 	e4:SetTarget(c100411049.sptg)
 	e4:SetOperation(c100411049.spop)
 	c:RegisterEffect(e4)
 end
-function c100411049.atkfilter(c,e,tp)
-	return c:IsFaceup() and c:IsType(TYPE_FUSION) and c:IsSummonType(SUMMON_TYPE_FUSION) and c:IsControler(tp) and c:IsCanBeEffectTarget(e)
+	--If fusion monster(s) were fusion summoned to your field
+function c100411049.cfilter(c,tp)
+	return c:IsFaceup() and c:IsType(TYPE_FUSION) and c:IsControler(tp) and c:IsSummonType(SUMMON_TYPE_FUSION)
 end
+	--If it ever happened
+function c100411049.tkcon(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(c100411049.cfilter,1,nil,tp)
+end
+	--Activation legality
 function c100411049.tktg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return eg:IsContains(chkc) and c100411049.atkfilter(chkc,e,tp) end
-	if chk==0 then return eg:IsExists(c100411049.atkfilter,1,nil,e,tp)
-		and Duel.GetLocationCount(1-tp,LOCATION_MZONE)>0
-		and Duel.IsPlayerCanSpecialSummonMonster(tp,100411149,0xdf,0x4011,0,0,1,RACE_BEASTWARRIOR,ATTRIBUTE_DARK,POS_FACEUP,1-tp) end
-	if eg:GetCount()==1 then
-		Duel.SetTargetCard(eg)
-	else
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
-		local g=eg:FilterSelect(tp,c100411049.atkfilter,1,1,nil,e,tp)
-		Duel.SetTargetCard(g)
+	if chkc then return eg:IsContains(chkc) and c100411049.cfilter(chkc,e,1-tp) end
+	if chk==0 then return eg:IsExists(c100411049.cfilter,1,nil,e,1-tp) 
+		and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 
+		and Duel.IsPlayerCanSpecialSummonMonster(tp,100411049+100,0xdf,0x4011,2000,2000,1,RACE_BEASTWARRIOR,ATTRIBUTE_DARK,POS_FACEUP,1-tp) 
 	end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+	local g=eg:FilterSelect(tp,c100411049.cfilter,1,1,nil,e,1-tp)
+	Duel.SetTargetCard(g)
+	Duel.SetOperationInfo(0,CATEGORY_TOKEN,nil,1,0,0)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,0,0)
-	Duel.SetOperationInfo(0,CATEGORY_TOKEN,nil,1,tp,0)
 end
+	--Performing the effect of ATK gain and token special summoned to opponent's field
 function c100411049.tkop(e,tp,eg,ep,ev,re,r,rp)
-	if not e:GetHandler():IsRelateToEffect(e) then return end
-	if Duel.GetLocationCount(1-tp,LOCATION_MZONE)>0
-		and Duel.IsPlayerCanSpecialSummonMonster(tp,100411149,0xdf,0x4011,0,0,1,RACE_BEASTWARRIOR,ATTRIBUTE_DARK,POS_FACEUP,1-tp) then
-		local token=Duel.CreateToken(tp,100411149)
-		Duel.SpecialSummon(token,0,tp,1-tp,false,false,POS_FACEUP)
-	end
 	local tc=Duel.GetFirstTarget()
-	if tc:IsRelateToEffect(e) and tc:IsFaceup() then
-		local ct=Duel.GetFieldGroupCount(tp,0,LOCATION_MZONE)
+	if tc:IsFaceup() and tc:IsRelateToEffect(e) then
 		local e1=Effect.CreateEffect(e:GetHandler())
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_UPDATE_ATTACK)
-		e1:SetValue(ct*500)
+		e1:SetValue(c100411049.value)
 		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
 		tc:RegisterEffect(e1)
 	end
+	if Duel.GetLocationCount(1-tp,LOCATION_MZONE)>0
+		and Duel.IsPlayerCanSpecialSummonMonster(tp,100411049+100,0xdf,0x4011,2000,2000,1,RACE_BEASTWARRIOR,ATTRIBUTE_DARK,POS_FACEUP,1-tp) then
+		local token=Duel.CreateToken(tp,100411049+100)
+		Duel.SpecialSummon(token,0,tp,1-tp,false,false,POS_FACEUP)
+	end
 end
+
+function c100411049.value(e,c)
+	return Duel.GetFieldGroupCount(c:GetControler(),0,LOCATION_MZONE)*500
+end
+	--Check if current phase is main phase
 function c100411049.spcon(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.GetTurnPlayer()==tp and (Duel.GetCurrentPhase()==PHASE_MAIN1 or Duel.GetCurrentPhase()==PHASE_MAIN2)
+	local tn=Duel.GetTurnPlayer()
+	local ph=Duel.GetCurrentPhase()
+	return tn==tp and (ph==PHASE_MAIN1 or ph==PHASE_MAIN2)
 end
-function c100411049.spfilter(c,e,tp)
+	--Check for "Lunalight" monster
+function c100411049.spfilter(c,e,tp,zone)
 	return c:IsSetCard(0xdf) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
+	--Activation legality
 function c100411049.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingMatchingCard(Card.IsAbleToGrave,tp,LOCATION_HAND,0,1,nil)
-		and Duel.IsExistingMatchingCard(c100411049.spfilter,tp,LOCATION_DECK,0,1,nil,e,tp) end
+	if chk==0 then return Duel.IsExistingMatchingCard(c100411049.spfilter,tp,LOCATION_DECK,0,1,nil,e,tp) 
+		and Duel.IsExistingMatchingCard(Card.IsAbleToGrave,tp,LOCATION_HAND,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_HAND)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
 end
+	--Performing the effect of sending a card from hand to GY, and then special summmoning 1 "Lunalight" monster from deck
 function c100411049.spop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
 	local g=Duel.SelectMatchingCard(tp,Card.IsAbleToGrave,tp,LOCATION_HAND,0,1,1,nil)
-	if g:GetCount()>0 and Duel.SendtoGrave(g,REASON_EFFECT)~=0 and g:GetFirst():IsLocation(LOCATION_GRAVE) then
-		if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	if g:GetCount()>0 and Duel.SendtoGrave(g,REASON_EFFECT)~=0
+		and g:GetFirst():IsLocation(LOCATION_GRAVE) then
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-		local g=Duel.SelectMatchingCard(tp,c100411049.spfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
-		if g:GetCount()>0 then
-			Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+		local sg=Duel.SelectMatchingCard(tp,c100411049.spfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
+		if zone~=0 and sg:GetCount()>0 then
+			Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP)
 		end
 	end
 end
