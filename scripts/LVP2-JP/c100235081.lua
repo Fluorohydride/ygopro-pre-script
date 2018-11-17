aaeab707f199e2ae1f271570ad60b0a500b0d530
nekrozar <nekrozar.s@gmail.com>
2018-11-17 10:33:39 +0900

    fix

--- scripts/LVP2-JP/c100235081.lua
+++ scripts/LVP2-JP/c100235081.lua
@@ -30,12 +30,12 @@ function c100235081.sumfilter(c)
 	return c:IsAttribute(ATTRIBUTE_WIND) and c:IsSummonable(true,nil)
 end
 function c100235081.sumtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(c100235081.sumfilter,tp,LOCATION_HAND,0,1,nil) end
+	if chk==0 then return Duel.IsExistingMatchingCard(c100235081.sumfilter,tp,LOCATION_HAND+LOCATION_MZONE,0,1,nil) end
 	Duel.SetOperationInfo(0,CATEGORY_SUMMON,nil,1,0,0)
 end
 function c100235081.sumop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SUMMON)
-	local g=Duel.SelectMatchingCard(tp,c100235081.sumfilter,tp,LOCATION_HAND,0,1,1,nil)
+	local g=Duel.SelectMatchingCard(tp,c100235081.sumfilter,tp,LOCATION_HAND+LOCATION_MZONE,0,1,1,nil)
 	local tc=g:GetFirst()
 	if tc then
 		Duel.Summon(tp,tc,true,nil)
