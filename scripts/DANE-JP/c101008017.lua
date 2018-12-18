abf7a3be7ba3e276f1b87c0b534cb1542a3db77d
mercury233 <me@mercury233.me>
2018-12-18 20:04:37 +0800

    add script DANE

---
+++ scripts/DANE-JP/c101008017.lua
@@ -0,0 +1,71 @@
+--夢幻転星イドリース
+--
+--Script by mercury233
+function c101008017.initial_effect(c)
+	--special summon
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(101008017,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_QUICK_O)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCountLimit(1,101008017)
+	e1:SetCondition(c101008017.spcon)
+	e1:SetTarget(c101008017.sptg)
+	e1:SetOperation(c101008017.spop)
+	c:RegisterEffect(e1)
+	--to grave
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(101008017,1))
+	e2:SetCategory(CATEGORY_TOGRAVE)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e2:SetCondition(c101008017.descon)
+	e2:SetTarget(c101008017.destg)
+	e2:SetOperation(c101008017.desop)
+	c:RegisterEffect(e2)
+	--indes
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetTargetRange(LOCATION_MZONE,0)
+	e3:SetTarget(c101008017.indtg)
+	e3:SetValue(1)
+	c:RegisterEffect(e3)
+end
+function c101008017.cfilter(c)
+	return c:IsFaceup() and c:IsType(TYPE_LINK)
+end
+function c101008017.spcon(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(c101008017.cfilter,tp,LOCATION_MZONE,LOCATION_MZONE,nil)
+	return #g>0 and g:GetSum(Card.GetLink)>=8
+end
+function c101008017.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
+end
+function c101008017.spop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) then
+		Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
+function c101008017.descon(e,tp,eg,ep,ev,re,r,rp)
+	local g1=Duel.GetMatchingGroup(c101008017.cfilter,tp,LOCATION_MZONE,0,nil)
+	local g2=Duel.GetMatchingGroup(c101008017.cfilter,tp,0,LOCATION_MZONE,nil)
+	return #g2>#g1
+end
+function c101008017.destg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local g=Duel.GetMatchingGroup(c101008017.cfilter,tp,LOCATION_MZONE,LOCATION_MZONE,nil)
+	if chk==0 then return #g>0 end
+	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,g,#g,0,0)
+end
+function c101008017.desop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(c101008017.cfilter,tp,LOCATION_MZONE,LOCATION_MZONE,nil)
+	Duel.SendtoGrave(g,REASON_EFFECT)
+end
+function c101008017.indtg(e,c)
+	return c:IsLevel(9)
+end
