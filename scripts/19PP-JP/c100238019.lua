07151a64a3975b8a8e04689d215a35df32d4b729
Nanahira <78877@qq.com>
2018-12-25 21:48:51 +0800

    c100238019 by Djeeta

---
+++ scripts/19PP-JP/c100238019.lua
@@ -0,0 +1,75 @@
+--灵魂彷徨的墓地
+--Script by Djeeta
+function c100238019.initial_effect(c)
+    --Activate
+    local e1=Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_ACTIVATE)
+    e1:SetCode(EVENT_FREE_CHAIN)
+    e1:SetHintTiming(0,TIMING_END_PHASE)
+    c:RegisterEffect(e1)  
+    --hitama token
+    local e2=Effect.CreateEffect(c)
+    e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_QUICK_F)
+    e2:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOKEN)
+    e2:SetCode(EVENT_BATTLE_DESTROYED)
+    e2:SetRange(LOCATION_SZONE)
+    e2:SetCondition(c100238019.spcon1)
+    e2:SetTarget(c100238019.sptg1)
+    e2:SetOperation(c100238019.spop1)
+    c:RegisterEffect(e2)
+    local e3=Effect.CreateEffect(c)
+    e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_QUICK_F)
+    e3:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOKEN)
+    e3:SetCode(EVENT_TO_GRAVE)
+    e3:SetRange(LOCATION_SZONE)
+    e3:SetCountLimit(1,100238019)
+    e3:SetCondition(c100238019.spcon2)
+    e3:SetTarget(c100238019.sptg2)
+    e3:SetOperation(c100238019.spop2)
+    c:RegisterEffect(e3)
+end
+function c100238019.cfilter(c,tp)
+    return c:IsReason(REASON_BATTLE) and c:IsLocation(LOCATION_GRAVE) and c:GetPreviousControler()==tp
+end
+function c100238019.spcon1(e,tp,eg,ep,ev,re,r,rp)
+    return eg:IsExists(c100238019.cfilter,1,nil,tp)
+end
+function c100238019.sptg1(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+        and Duel.IsPlayerCanSpecialSummonMonster(tp,100238119,0,0x4011,100,100,1,RACE_PYRO,ATTRIBUTE_FIRE) end
+    Duel.SetOperationInfo(0,CATEGORY_TOKEN,nil,1,0,0)
+    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,0,0)
+end
+function c100238019.spop1(e,tp,eg,ep,ev,re,r,rp)
+    if not e:GetHandler():IsRelateToEffect(e) or Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+    if Duel.IsPlayerCanSpecialSummonMonster(tp,100238119,0,0x4011,100,100,1,RACE_PYRO,ATTRIBUTE_FIRE) then
+        local token=Duel.CreateToken(tp,100238119)
+        Duel.SpecialSummon(token,0,tp,tp,false,false,POS_FACEUP)
+    end
+end
+function c100238019.cfilter1(c,tp)
+    return c:IsReason(REASON_EFFECT) and c:IsLocation(LOCATION_GRAVE) and (c:GetPreviousLocation(LOCATION_HAND) or c:GetPreviousLocation(LOCATION_ONFIELD)) and c:GetPreviousControler()==tp and c:GetReasonPlayer()==1-tp
+end
+function c100238019.spcon2(e,tp,eg,ep,ev,re,r,rp)
+    return eg:IsExists(c100238019.cfilter1,1,nil,tp)
+end
+function c100238019.sptg2(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+        and Duel.IsPlayerCanSpecialSummonMonster(tp,100238119,0,0x4011,100,100,1,RACE_PYRO,ATTRIBUTE_FIRE) end
+    local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+    if Duel.IsPlayerAffectedByEffect(tp,59822133) then ft=1 end
+    Duel.SetOperationInfo(0,CATEGORY_TOKEN,nil,ft,0,0)
+    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,ft,0,0)
+end
+function c100238019.spop2(e,tp,eg,ep,ev,re,r,rp)
+    local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+    if not e:GetHandler():IsRelateToEffect(e) or ft<=0 or not Duel.IsPlayerCanSpecialSummonMonster(tp,100238119,0,0x4011,100,100,1,RACE_PYRO,ATTRIBUTE_FIRE) then return end
+    if Duel.IsPlayerAffectedByEffect(tp,59822133) then ft=1 end
+    local fid=e:GetHandler():GetFieldID()
+    local g=Group.CreateGroup()
+    for i=1,ft do
+        local token=Duel.CreateToken(tp,100238119)
+        Duel.SpecialSummonStep(token,0,tp,tp,false,false,POS_FACEUP)
+    end
+    Duel.SpecialSummonComplete()
+end
