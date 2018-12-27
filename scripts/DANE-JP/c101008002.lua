d87a6d3e5142e06228a7693b9fd2fcd62dd6553d
mercury233 <me@mercury233.me>
2018-12-27 19:04:13 +0800

    add script DANE

---
+++ scripts/DANE-JP/c101008002.lua
@@ -0,0 +1,66 @@
+--グリッド・スィーパー
+--
+--Script by mercury233
+function c101008002.initial_effect(c)
+	--special summon
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(101008002,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCountLimit(1,101008002)
+	e1:SetCondition(c101008002.spcon)
+	e1:SetTarget(c101008002.sptg)
+	e1:SetOperation(c101008002.spop)
+	c:RegisterEffect(e1)
+	--destroy
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(101008002,1))
+	e2:SetCategory(CATEGORY_DESTROY)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_GRAVE)
+	e2:SetCountLimit(1,101008002+100)
+	e2:SetCost(c101008002.descost)
+	e2:SetTarget(c101008002.destg)
+	e2:SetOperation(c101008002.desop)
+	c:RegisterEffect(e2)
+end
+function c101008002.spcon(e,tp,eg,ep,ev,re,r,rp)
+	local f1=Duel.GetFieldCard(0,LOCATION_SZONE,5)
+	local f2=Duel.GetFieldCard(1,LOCATION_SZONE,5)
+	return (f1~=nil and f1:IsFaceup()) or (f2~=nil and f2:IsFaceup())
+end
+function c101008002.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
+end
+function c101008002.spop(e,tp,eg,ep,ev,re,r,rp)
+	if e:GetHandler():IsRelateToEffect(e) then
+		Duel.SpecialSummon(e:GetHandler(),0,tp,tp,false,false,POS_FACEUP)
+	end
+end
+function c101008002.cfilter(c)
+	return c:IsType(TYPE_LINK) and c:IsAbleToRemoveAsCost()
+end
+function c101008002.descost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsAbleToRemoveAsCost()
+		and Duel.IsExistingMatchingCard(c101008002.cfilter,tp,LOCATION_MZONE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local g=Duel.SelectMatchingCard(tp,c101008002.cfilter,tp,LOCATION_MZONE,0,1,1,nil)
+	g:AddCard(e:GetHandler())
+	Duel.Remove(g,POS_FACEUP,REASON_COST)
+end
+function c101008002.destg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(nil,tp,0,LOCATION_ONFIELD,1,nil) end
+	local g=Duel.GetMatchingGroup(nil,tp,0,LOCATION_ONFIELD,nil)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
+end
+function c101008002.desop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local g=Duel.SelectMatchingCard(tp,nil,tp,0,LOCATION_ONFIELD,1,1,nil)
+	if #g>0 then
+		Duel.HintSelection(g)
+		Duel.Destroy(g,REASON_EFFECT)
+	end
+end
