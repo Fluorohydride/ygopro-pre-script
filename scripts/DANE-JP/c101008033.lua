21e903c56dbc2882fb2dc37f46fad9f999cc2457
mercury233 <me@mercury233.me>
2018-12-27 22:54:56 +0800

    add script DANE

---
+++ scripts/DANE-JP/c101008033.lua
@@ -0,0 +1,59 @@
+--オルターガイスト・ドラッグウィリオン
+--
+--Script by mercury233
+function c101008033.initial_effect(c)
+	--synchro summon
+	aux.AddSynchroProcedure(c,nil,aux.NonTuner(nil),1)
+	c:EnableReviveLimit()
+	--disable attack
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(101008033,0))
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCode(EVENT_ATTACK_ANNOUNCE)
+	e1:SetCountLimit(1,101008033)
+	e1:SetCondition(c101008033.atkcon)
+	e1:SetCost(c101008033.atkcost)
+	e1:SetOperation(c101008033.atkop)
+	c:RegisterEffect(e1)
+	--spsummon
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(101008033,1))
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetProperty(EFFECT_FLAG_DELAY)
+	e2:SetCode(EVENT_TO_GRAVE)
+	e2:SetCountLimit(1,101008033+100)
+	e2:SetCondition(c101008033.spcon)
+	e2:SetTarget(c101008033.sptg)
+	e2:SetOperation(c101008033.spop)
+	c:RegisterEffect(e2)
+end
+function c101008033.atkcon(e,tp,eg,ep,ev,re,r,rp)
+	local tc=eg:GetFirst()
+	return tc:IsControler(1-tp) and tc:IsSummonType(SUMMON_TYPE_SPECIAL)
+end
+function c101008033.cfilter(c)
+	return c:IsFaceup() and c:IsSetCard(0x103) and c:IsAbleToHandAsCost()
+end
+function c101008033.atkcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c101008033.cfilter,tp,LOCATION_ONFIELD,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RTOHAND)
+	local g=Duel.SelectMatchingCard(tp,c101008033.cfilter,tp,LOCATION_ONFIELD,0,1,1,nil)
+	Duel.SendtoHand(g,nil,REASON_COST)
+end
+function c101008033.atkop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.NegateAttack()
+end
+function c101008033.spcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsReason(REASON_RELEASE)
+end
+function c101008033.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
+end
+function c101008033.spop(e,tp,eg,ep,ev,re,r,rp)
+	if e:GetHandler():IsRelateToEffect(e) then
+		Duel.SpecialSummon(e:GetHandler(),1,tp,tp,false,false,POS_FACEUP)
+	end
+end
