a826a5685e30a45cf04407169b9d7150c91dbc53
mercury233 <me@mercury233.me>
2018-12-28 14:01:39 +0800

    add script DANE

---
+++ scripts/DANE-JP/c101008040.lua
@@ -0,0 +1,113 @@
+--サイバース・リマインダー
+--
+--Script by mercury233
+function c101008040.initial_effect(c)
+	--xyz summon
+	aux.AddXyzProcedure(c,nil,3,2)
+	c:EnableReviveLimit()
+	--to hand
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(101008040,0))
+	e1:SetCategory(CATEGORY_TOHAND)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1,101008040)
+	e1:SetCost(c101008040.thcost)
+	e1:SetTarget(c101008040.thtg)
+	e1:SetOperation(c101008040.thop)
+	c:RegisterEffect(e1)
+	--special summon
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(101008040,0))
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetProperty(EFFECT_FLAG_DELAY)
+	e2:SetCode(EVENT_DESTROYED)
+	e2:SetCountLimit(1,101008040+100)
+	e2:SetCost(c101008040.cost)
+	e2:SetCondition(c101008040.spcon)
+	e2:SetTarget(c101008040.sptg)
+	e2:SetOperation(c101008040.spop)
+	c:RegisterEffect(e2)
+	Duel.AddCustomActivityCounter(101008040,ACTIVITY_SPSUMMON,c101008040.counterfilter)
+end
+function c101008040.counterfilter(c)
+	return c:GetSummonLocation()~=LOCATION_EXTRA or c:IsRace(RACE_CYBERSE)
+end
+function c101008040.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetCustomActivityCount(101008040,tp,ACTIVITY_SPSUMMON)==0 end
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_OATH)
+	e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+	e1:SetTargetRange(1,0)
+	e1:SetTarget(c101008040.splimit)
+	e1:SetReset(RESET_PHASE+PHASE_END)
+	Duel.RegisterEffect(e1,tp)
+end
+function c101008040.splimit(e,c,sump,sumtype,sumpos,targetp,se)
+	return not c:IsType(RACE_CYBERSE) and c:IsLocation(LOCATION_EXTRA)
+end
+function c101008040.thcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():CheckRemoveOverlayCard(tp,1,REASON_COST)
+		and c101008040.cost(e,tp,eg,ep,ev,re,r,rp,0) end
+	e:GetHandler():RemoveOverlayCard(tp,1,1,REASON_COST)
+	c101008040.cost(e,tp,eg,ep,ev,re,r,rp,1)
+end
+function c101008040.thfilter(c)
+	return c:IsSetCard(0x118) and c:IsType(TYPE_SPELL+TYPE_TRAP) and c:IsAbleToHand()
+end
+function c101008040.thtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and c101008040.thfilter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(c101008040.thfilter,tp,LOCATION_GRAVE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectTarget(tp,c101008040.thfilter,tp,LOCATION_GRAVE,0,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,0,0)
+end
+function c101008040.thop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) then
+		Duel.SendtoHand(tc,nil,REASON_EFFECT)
+	end
+end
+function c101008040.spcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return (c:IsReason(REASON_BATTLE) or (c:IsReason(REASON_EFFECT) and c:GetReasonPlayer()==1-tp))
+		and c:IsPreviousLocation(LOCATION_MZONE) and c:IsSummonType(SUMMON_TYPE_XYZ)
+end
+function c101008040.spfilter(c,e,tp)
+	return c:IsRace(RACE_CYBERSE) and c:IsType(TYPE_MONSTER) and c:IsLevel(4) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function c101008040.spcheck(g)
+	return g:GetClassCount(Card.GetCode)==#g
+end
+function c101008040.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then
+		if Duel.GetLocationCount(tp,LOCATION_MZONE)<2 or Duel.IsPlayerAffectedByEffect(tp,59822133) then return false end
+		local g=Duel.GetMatchingGroup(c101008040.spfilter,tp,LOCATION_DECK,0,nil,e,tp)
+		return g:CheckSubGroup(c101008040.spcheck,2,2)
+	end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,2,tp,LOCATION_DECK)
+end
+function c101008040.spop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<2 or Duel.IsPlayerAffectedByEffect(tp,59822133) then return end
+	local g=Duel.GetMatchingGroup(c101008040.spfilter,tp,LOCATION_DECK,0,nil,e,tp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local sg=g:SelectSubGroup(tp,c101008040.spcheck,false,2,2)
+	if sg then
+		local tc=sg:GetFirst()
+		while tc do
+			Duel.SpecialSummonStep(tc,0,tp,tp,false,false,POS_FACEUP)
+			local e1=Effect.CreateEffect(e:GetHandler())
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetCode(EFFECT_DISABLE)
+			e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+			tc:RegisterEffect(e1)
+			local e2=e1:Clone()
+			e2:SetCode(EFFECT_DISABLE_EFFECT)
+			tc:RegisterEffect(e2)
+			tc=sg:GetNext()
+		end
+		Duel.SpecialSummonComplete()
+	end
+end
