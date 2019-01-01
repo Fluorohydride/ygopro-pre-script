bd16348c4a2bad1d554f3cad91dba5ebfa9de28d
mercury233 <me@mercury233.me>
2019-01-01 20:58:36 +0800

    add script DANE

---
+++ scripts/DANE-JP/c101008027.lua
@@ -0,0 +1,84 @@
+--魔獣皇帝ガーゼット
+--
+--Scripy by mercury233
+function c101008027.initial_effect(c)
+	c:EnableReviveLimit()
+	--spsummon condition
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e1:SetCode(EFFECT_SPSUMMON_CONDITION)
+	c:RegisterEffect(e1)
+	--special summon
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_SPSUMMON_PROC)
+	e2:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+	e2:SetRange(LOCATION_HAND)
+	e2:SetCondition(c101008027.spcon)
+	e2:SetOperation(c101008027.spop)
+	c:RegisterEffect(e2)
+	--activate limit
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e3:SetCode(EFFECT_CANNOT_ACTIVATE)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetTargetRange(0,1)
+	e3:SetCondition(c101008027.condition)
+	e3:SetValue(c101008027.aclimit)
+	c:RegisterEffect(e3)
+end
+function c101008027.mzfilter(c,tp)
+	return c:IsControler(tp) and c:GetSequence()<5
+end
+function c101008027.spcon(e,c)
+	if c==nil then return true end
+	local tp=c:GetControler()
+	local rg=Duel.GetReleaseGroup(tp)
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	local ct=-ft+1
+	return ft>-3 and rg:GetCount()>2 and (ft>0 or rg:IsExists(c101008027.mzfilter,ct,nil,tp))
+end
+function c101008027.spop(e,tp,eg,ep,ev,re,r,rp,c)
+	local rg=Duel.GetReleaseGroup(tp)
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	local g=nil
+	if ft>0 then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
+		g=rg:Select(tp,3,3,nil)
+	elseif ft>-2 then
+		local ct=-ft+1
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
+		g=rg:FilterSelect(tp,c101008027.mzfilter,ct,ct,nil,tp)
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
+		local g2=rg:Select(tp,3-ct,3-ct,g)
+		g:Merge(g2)
+	else
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
+		g=rg:FilterSelect(tp,c101008027.mzfilter,3,3,nil,tp)
+	end
+	Duel.Release(g,REASON_COST)
+	local atk=0
+	local tc=g:GetFirst()
+	while tc do
+		local batk=tc:GetTextAttack()
+		if batk>0 then
+			atk=atk+batk
+		end
+		tc=g:GetNext()
+	end
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_SET_ATTACK)
+	e1:SetValue(atk)
+	e1:SetReset(RESET_EVENT+0xff0000)
+	c:RegisterEffect(e1)
+end
+function c101008027.condition(e)
+	local ph=Duel.GetCurrentPhase()
+	return ph>=PHASE_BATTLE_START and ph<=PHASE_BATTLE
+end
+function c101008027.aclimit(e,re,tp)
+	return not re:GetHandler():IsImmuneToEffect(e)
+end
