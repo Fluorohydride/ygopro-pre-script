368442739f46d91af1d0fd81fb3739e0f0c89776
Nanahira <78877@qq.com>
2019-01-11 19:53:02 +0800

    --ロー・キューピット -- --By Tanaka Kotoha

---
+++ scripts/DANE-JP/c101008028.lua
@@ -0,0 +1,38 @@
+--ロー・キューピット
+--
+--By Tanaka Kotoha
+function c101008028.initial_effect(c)
+	--indes
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+	e1:SetValue(c101008028.indval)
+	c:RegisterEffect(e1)
+	--atkup
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCategory(CATEGORY_LVCHANGE)
+	e2:SetCode(EVENT_PHASE+PHASE_STANDBY)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1)
+	e2:SetCondition(c101008028.atkcon)
+	e2:SetOperation(c101008028.atkop)
+	c:RegisterEffect(e2)
+end
+function c101008028.indval(e,c)
+	return not c:IsLevelAbove(e:GetHandler():GetLevel())
+end
+function c101008028.atkcon(e,tp,eg,ep,ev,re,r,rp)
+	return tp==Duel.GetTurnPlayer()
+end
+function c101008028.atkop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not c:IsRelateToEffect(e) or c:IsFacedown() then return end
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_UPDATE_LEVEL)
+	e1:SetProperty(EFFECT_FLAG_COPY_INHERIT)
+	e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_DISABLE)
+	e1:SetValue(1)
+	c:RegisterEffect(e1)
+end
