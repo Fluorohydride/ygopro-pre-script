1acad4ff571edd8034839b0cb88334538154538e
Mercury233 <me@mercury233.me>
2019-01-11 22:30:07 +0800

    update

--- scripts/DANE-JP/c101008067.lua
+++ scripts/DANE-JP/c101008067.lua
@@ -25,12 +25,12 @@ function c101008067.filter2(c)
 end
 function c101008067.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return false end
-	if chk==0 then return Duel.IsExistingTarget(c101008067.filter2,tp,0,LOCATION_MZONE,1,nil)
-		and Duel.IsExistingTarget(c101008067.filter1,tp,LOCATION_MZONE,0,1,nil) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CONTROL)
-	local g2=Duel.SelectTarget(tp,c101008067.filter2,tp,0,LOCATION_MZONE,1,1,nil)
+	if chk==0 then return Duel.IsExistingTarget(c101008067.filter1,tp,LOCATION_MZONE,0,1,nil)
+		and Duel.IsExistingTarget(c101008067.filter2,tp,0,LOCATION_MZONE,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CONTROL)
 	local g1=Duel.SelectTarget(tp,c101008067.filter1,tp,LOCATION_MZONE,0,1,1,nil)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CONTROL)
+	local g2=Duel.SelectTarget(tp,c101008067.filter2,tp,0,LOCATION_MZONE,1,1,nil)
 	g1:Merge(g2)
 	Duel.SetOperationInfo(0,CATEGORY_CONTROL,g1,2,0,0)
 end
