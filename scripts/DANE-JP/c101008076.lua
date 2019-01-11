af8689c1375c1fd8e50fb9aa972d26ffdfccba98
Nanahira <78877@qq.com>
2019-01-11 10:01:21 -0600

    add script DANE (#242)

---
+++ scripts/DANE-JP/c101008076.lua
@@ -0,0 +1,45 @@
+--星遺物の対焉
+--
+--Scripted by Djeeta
+function c101008076.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_TODECK)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_ATTACK_ANNOUNCE)
+	e1:SetCondition(c101008076.condition)
+	e1:SetTarget(c101008076.target)
+	e1:SetOperation(c101008076.activate)
+	c:RegisterEffect(e1)    
+end
+function c101008076.condition(e,tp,eg,ep,ev,re,r,rp)
+	local a=Duel.GetAttacker()
+	local d=Duel.GetAttackTarget()
+	return a and d and a:IsFaceup() and a:IsType(TYPE_LINK) and d:IsFaceup() and d:IsType(TYPE_LINK)
+end
+function c101008076.tdfilter(c)
+	return c:IsType(TYPE_MONSTER) and c:IsAbleToDeck()
+end
+function c101008076.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	local g=Duel.GetMatchingGroup(c101008076.tdfilter,tp,LOCATION_ONFIELD+LOCATION_GRAVE,LOCATION_ONFIELD+LOCATION_GRAVE,nil)
+	if chk==0 then return g:GetCount()>0 end
+	Duel.SetOperationInfo(0,CATEGORY_TODECK,g,g:GetCount(),0,0)
+end
+function c101008076.activate(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local g=Duel.GetMatchingGroup(c101008076.tdfilter,tp,LOCATION_ONFIELD+LOCATION_GRAVE,LOCATION_ONFIELD+LOCATION_GRAVE,nil)
+	if g:GetCount()>0 then
+		Duel.SendtoDeck(g,nil,2,REASON_EFFECT)
+	end
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_OATH)
+	e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+	e1:SetTargetRange(1,1)
+	e1:SetTarget(c101008076.splimit)
+	e1:SetReset(RESET_PHASE+PHASE_END)
+	Duel.RegisterEffect(e1,tp)
+end
+function c101008076.splimit(e,c,tp,sumtp,sumpos)
+	return bit.band(sumtp,SUMMON_TYPE_LINK)==SUMMON_TYPE_LINK
+end
