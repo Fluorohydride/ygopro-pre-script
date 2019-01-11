7fd9010eba72ebdbe9db43d6a0e2f740e80b89e7
Nanahira <78877@qq.com>
2019-01-11 19:31:30 +0800

    --モルトシュラーク -- --By Tanaka Kotoha

---
+++ scripts/DANE-JP/c101008065.lua
@@ -0,0 +1,82 @@
+--モルトシュラーク
+--
+--By Tanaka Kotoha
+function c101008065.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_EQUIP)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetTarget(c101008065.target)
+	e1:SetOperation(c101008065.operation)
+	c:RegisterEffect(e1)
+	--Equip limit
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e2:SetCode(EFFECT_EQUIP_LIMIT)
+	e2:SetValue(c101008065.eqlimit)
+	c:RegisterEffect(e2)
+	--immune
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_EQUIP)
+	e3:SetCode(EFFECT_IMMUNE_EFFECT)
+	e3:SetValue(c101008065.efilter)
+	c:RegisterEffect(e3)
+	--atk
+	local e4=Effect.CreateEffect(c)
+	e4:SetCategory(CATEGORY_ATKCHANGE+CATEGORY_DEFCHANGE)
+	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e4:SetCode(EVENT_PRE_DAMAGE_CALCULATE)
+	e4:SetRange(LOCATION_SZONE)
+	e4:SetCondition(c101008065.atkcon)
+	e4:SetOperation(c101008065.atkop)
+	c:RegisterEffect(e4)
+end
+function c101008065.eqlimit(e,c)
+	return c:IsControler(e:GetHandlerPlayer()) and c:IsSummonType(SUMMON_TYPE_NORMAL)
+end
+function c101008065.filter(c)
+	return c:IsFaceup() and c:IsSummonType(SUMMON_TYPE_NORMAL)
+end
+function c101008065.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and c101008065.filter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(c101008065.filter,tp,LOCATION_MZONE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)
+	Duel.SelectTarget(tp,c101008065.filter,tp,LOCATION_MZONE,0,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_EQUIP,e:GetHandler(),1,0,0)
+end
+function c101008065.operation(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tc=Duel.GetFirstTarget()
+	if c:IsRelateToEffect(e) and tc:IsRelateToEffect(e) and tc:IsFaceup() then
+		Duel.Equip(tp,c,tc)
+	end
+end
+function c101008065.efilter(e,te)
+	return te:GetOwnerPlayer()~=e:GetHandlerPlayer() and te:GetActivateLocation()==LOCATION_MZONE and bit.band(te:GetHandler():GetSummonType(),SUMMON_TYPE_SPECIAL)==SUMMON_TYPE_SPECIAL 
+end
+function c101008065.atkcon(e,tp,eg,ep,ev,re,r,rp)
+	local ec=e:GetHandler():GetEquipTarget()
+	if ec~=Duel.GetAttacker() and ec~=Duel.GetAttackTarget() then return false end
+	local tc=ec:GetBattleTarget()
+	return tc and tc:IsFaceup() and bit.band(tc:GetSummonType(),SUMMON_TYPE_SPECIAL)==SUMMON_TYPE_SPECIAL
+end
+function c101008065.atkop(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetHandler():IsRelateToEffect(e) then return end
+	local ec=e:GetHandler():GetEquipTarget()
+	local tc=e:GetHandler():GetEquipTarget():GetBattleTarget()
+	if ec and tc and ec:IsFaceup() and tc:IsFaceup() then
+		local val=math.max(ec:GetBaseAttack(),0)
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetValue(-val)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_DAMAGE_CAL)
+		tc:RegisterEffect(e1)
+		local e2=e1:Clone()
+		e2:SetCode(EFFECT_UPDATE_DEFENSE)
+		tc:RegisterEffect(e2)
+	end
+end
