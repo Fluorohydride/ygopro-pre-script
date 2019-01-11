68d36eb7359600278dedc815eb659a36f10607c0
Mercury233 <me@mercury233.me>
2019-01-12 00:20:13 +0800

    update

--- scripts/DANE-JP/c101008063.lua
+++ scripts/DANE-JP/c101008063.lua
@@ -26,15 +26,24 @@ function c101008063.initial_effect(c)
 	e3:SetCondition(c101008063.damcon)
 	e3:SetOperation(c101008063.damop)
 	c:RegisterEffect(e3)
-	--indes
+	--self destroy
 	local e4=Effect.CreateEffect(c)
-	e4:SetType(EFFECT_TYPE_SINGLE)
-	e4:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
-	e4:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e4:SetCode(EVENT_PHASE+PHASE_END)
 	e4:SetRange(LOCATION_SZONE)
-	e4:SetCondition(c101008063.indcon)
-	e4:SetValue(1)
+	e4:SetCountLimit(1)
+	e4:SetCondition(c101008063.descon)
+	e4:SetOperation(c101008063.desop)
 	c:RegisterEffect(e4)
+	--indes
+	local e5=Effect.CreateEffect(c)
+	e5:SetType(EFFECT_TYPE_SINGLE)
+	e5:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+	e5:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e5:SetRange(LOCATION_SZONE)
+	e5:SetCondition(c101008063.indcon)
+	e5:SetValue(1)
+	c:RegisterEffect(e5)
 end
 function c101008063.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
@@ -60,37 +69,16 @@ function c101008063.damcon(e,tp,eg,ep,ev,re,r,rp)
 end
 function c101008063.damop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
+	local p=e:GetLabelObject():GetLabel()
 	Duel.Hint(HINT_CARD,0,101008063)
-	Duel.SetLP(e:GetLabelObject():GetLabel(),math.ceil(Duel.GetLP(e:GetLabelObject():GetLabel())/2))
-	--to grave
-	local e1=Effect.CreateEffect(e:GetHandler())
-	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e1:SetCode(EVENT_PHASE+PHASE_END)
-	e1:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
-	e1:SetReset(RESET_PHASE+PHASE_END)
-	e1:SetCountLimit(1)
-	e1:SetLabel(fid)
-	e1:SetLabelObject(g)
-	e1:SetCondition(c101008063.descon)
-	e1:SetOperation(c101008063.desop)
-	Duel.RegisterEffect(e1,tp)
-end
-function c101008063.desfilter(c,fid)
-	return c:GetFlagEffectLabel(101008063)==fid
+	Duel.SetLP(p,math.ceil(Duel.GetLP(p)/2))
+	c:RegisterFlagEffect(101008063+100,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,0,1)
 end
 function c101008063.descon(e,tp,eg,ep,ev,re,r,rp)
-	local g=e:GetLabelObject()
-	if not g:IsExists(c101008063.desfilter,1,nil,e:GetLabel()) then
-		g:DeleteGroup()
-		e:Reset()
-		return false
-	else return true end
+	return e:GetHandler():GetFlagEffect(101008063+100)~=0
 end
 function c101008063.desop(e,tp,eg,ep,ev,re,r,rp)
-	local g=e:GetLabelObject()
-	local tg=g:Filter(c101008063.desfilter,nil,e:GetLabel())
-	g:DeleteGroup()
-	Duel.SendtoGrave(tg,REASON_EFFECT)
+	Duel.SendtoGrave(e:GetHandler(),REASON_EFFECT)
 end
 function c101008063.indcon(e)
 	return Duel.GetFieldGroupCount(e:GetOwnerPlayer(),0,LOCATION_MZONE)>0
