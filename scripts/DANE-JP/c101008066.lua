8752947904e91e84a452e363028d2f4aa9dcc89e
Mercury233 <me@mercury233.me>
2019-01-11 22:43:14 +0800

    update

--- scripts/DANE-JP/c101008066.lua
+++ scripts/DANE-JP/c101008066.lua
@@ -2,7 +2,7 @@
 --
 --Scripted By-ღ Viola
 function c101008066.initial_effect(c)
---
+	--activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
@@ -14,52 +14,44 @@ function c101008066.initial_effect(c)
 	e1:SetTarget(c101008066.target)
 	e1:SetOperation(c101008066.activate)
 	c:RegisterEffect(e1)
---
 end
---
 function c101008066.cfilter(c,e,tp)
-	local rc=c:GetOriginalRace()
-	local att=c:GetOriginalAttribute()
+	local race=c:GetOriginalRace()
+	local attr=c:GetOriginalAttribute()
 	return bit.band(c:GetOriginalType(),TYPE_MONSTER)~=0 and c:IsReleasable()
-		and Duel.IsExistingMatchingCard(c101008066.spfilter,tp,0,LOCATION_GRAVE,1,nil,rc,att,e,tp)
+		and Duel.GetMZoneCount(tp,c,tp)>0
+		and Duel.IsExistingMatchingCard(c101008066.spfilter,tp,0,LOCATION_GRAVE,1,nil,race,attr,e,tp)
 end
-function c101008066.spfilter(c,rc,att,e,tp)
-	return c:IsRace(rc) and c:IsAttribute(att)
+function c101008066.spfilter(c,race,attr,e,tp)
+	return c:IsRace(race) and c:IsAttribute(attr)
 		and c:IsCanBeEffectTarget(e)
 		and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP,tp)
 end
---
 function c101008066.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	e:SetLabel(100)
 	if chk==0 then return Duel.IsExistingMatchingCard(c101008066.cfilter,tp,LOCATION_MZONE,0,1,nil,e,tp) end
---
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
 	local sg=Duel.SelectMatchingCard(tp,c101008066.cfilter,tp,LOCATION_MZONE,0,1,1,nil,e,tp)
 	e:SetLabelObject(sg:GetFirst())
 	Duel.Release(sg,REASON_COST)
---
 end
---
 function c101008066.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
---
-	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(1-tp) and c101008066.spfilter(chkc,e:GetLabelObject():GetOriginalRace(),e:GetLabelObject():GetOriginalAttribute(),e,tp) end
+	local rc=e:GetLabelObject()
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(1-tp)
+		and c101008066.spfilter(chkc,rc:GetOriginalRace(),rc:GetOriginalAttribute(),e,tp) end
 	if chk==0 then
 		if e:GetLabel()~=100 then return false end
 		e:SetLabel(0)
-		return Duel.GetMZoneCount(tp,e:GetLabelObject(),tp)>0
+		return true
 	end
 	e:SetLabel(0)
---
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local sg=Duel.SelectTarget(tp,c101008066.spfilter,tp,0,LOCATION_GRAVE,1,1,nil,e:GetLabelObject():GetOriginalRace(),e:GetLabelObject():GetOriginalAttribute(),e,tp)
---
+	local sg=Duel.SelectTarget(tp,c101008066.spfilter,tp,0,LOCATION_GRAVE,1,1,nil,rc:GetOriginalRace(),rc:GetOriginalAttribute(),e,tp)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,sg,1,0,0)
 end
---
 function c101008066.activate(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetMZoneCount(tp)<1 then return end
 	local tc=Duel.GetFirstTarget()
 	if not tc:IsRelateToEffect(e) then return end
 	Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
 end
---
