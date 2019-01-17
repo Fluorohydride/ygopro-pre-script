d8305e6b3b9674b9adaec75dc7986c07f827f1ea
purerosefallen <78877@qq.com>
2019-01-17 17:02:26 +0800

    new

---
+++ scripts/____-JP/c100200158.lua
@@ -0,0 +1,98 @@
+--HSRカイドレイク
+--
+--Scripted By-FW空鸽
+function c100200158.initial_effect(c)
+	--synchro summon
+	aux.AddSynchroProcedure(c,c100200158.sfilter,aux.NonTuner(nil),1)
+	c:EnableReviveLimit()
+	--link success
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(100200158,0))
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetCountLimit(1,100200158)
+	e1:SetCondition(c100200158.con)
+	e1:SetTarget(c100200158.target)
+	e1:SetOperation(c100200158.operation)
+	c:RegisterEffect(e1)
+	--spsummon
+	local e2=Effect.CreateEffect(c)
+	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e2:SetDescription(aux.Stringid(100200158,1))
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_TO_GRAVE)
+	e2:SetCountLimit(1,100200158+100)
+	e2:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_DAMAGE_STEP)
+	e2:SetCondition(c100200158.thcon)
+	e2:SetTarget(c100200158.thtg)
+	e2:SetOperation(c100200158.thop)
+	c:RegisterEffect(e2)
+end
+function c100200158.sfilter(c)
+	return c:IsRace(RACE_MACHINE) and c:IsAttribute(ATTRIBUTE_WIND) and c:IsType(TYPE_TUNER)
+end
+function c100200158.con(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsSummonType(SUMMON_TYPE_SYNCHRO)
+end
+function c100200158.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(aux.TRUE,tp,0,LOCATION_ONFIELD,1,nil) end
+end
+function c100200158.negfilter(c)
+	return c:IsFaceup() and not c:IsDisabled() 
+end
+function c100200158.operation(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local sg=Duel.GetMatchingGroup(aux.TRUE,tp,0,LOCATION_ONFIELD,nil)
+	local pg=Duel.GetMatchingGroup(c100200158.negfilter,tp,0,LOCATION_ONFIELD,nil)
+	local op=0
+	if pg:GetCount()>0 then
+		op=Duel.SelectOption(tp,aux.Stringid(100200158,2),aux.Stringid(100200158,3))
+	else
+		op=Duel.SelectOption(tp,aux.Stringid(100200158,2))
+	end
+	if op==0 then
+		Duel.Destroy(sg,REASON_EFFECT)
+	else
+		local nc=pg:GetFirst()
+		while nc do
+			local e1=Effect.CreateEffect(c)
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetCode(EFFECT_DISABLE)
+			e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+			nc:RegisterEffect(e1)
+			local e2=Effect.CreateEffect(c)
+			e2:SetType(EFFECT_TYPE_SINGLE)
+			e2:SetCode(EFFECT_DISABLE_EFFECT)
+			e2:SetReset(RESET_EVENT+RESETS_STANDARD)
+			nc:RegisterEffect(e2)
+			if nc:IsType(TYPE_TRAPMONSTER) then
+				local e3=Effect.CreateEffect(c)
+				e3:SetType(EFFECT_TYPE_SINGLE)
+				e3:SetCode(EFFECT_DISABLE_TRAPMONSTER)
+				e3:SetReset(RESET_EVENT+RESETS_STANDARD)
+				nc:RegisterEffect(e3)
+			end
+			nc=pg:GetNext()
+		end
+	end
+end
+function c100200158.filter(c)
+	return c:IsSetCard(0x2016) and c:IsType(TYPE_MONSTER) and c:IsAbleToHand()
+end
+function c100200158.thcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return c:IsPreviousLocation(LOCATION_MZONE) and c:IsSummonType(SUMMON_TYPE_SYNCHRO)
+		and rp==1-tp and c:GetPreviousControler()==tp
+end
+function c100200158.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c100200158.filter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function c100200158.thop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,c100200158.filter,tp,LOCATION_DECK,0,1,1,nil)
+	if g:GetCount()>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end
