02dadeb896f56342c5d665220b7f1eb83fa259bb
mercury233 <me@mercury233.me>
2019-02-04 14:42:53 +0800

    update

--- scripts/SAST-EN/c101007090.lua
+++ scripts/SAST-EN/c101007090.lua
@@ -15,79 +15,69 @@ function c101007090.initial_effect(c)
 	c:RegisterEffect(e1)
 end
 function c101007090.condition(e,tp,eg,ep,ev,re,r,rp)
-	for _,te in ipairs({Duel.GetPlayerEffect(tp,EFFECT_LPCOST_CHANGE)}) do
-		local val=te:GetValue()
-		if val(te,e,tp,1000)~=1000 then return false end
-	end
-	return true
+	return not Duel.IsPlayerAffectedByEffect(tp,EFFECT_LPCOST_CHANGE)
 end
-function c101007090.filtervalkyrie(c,e,tp)
-	return c:IsType(TYPE_MONSTER) and c:IsSetCard(0x122) and c:IsCanBeSpecialSummoned(e,0,tp,false,false) and c:IsCanBeEffectTarget(e)
+function c101007090.spfilter(c,e,tp)
+	return c:IsSetCard(0x122) and c:IsCanBeSpecialSummoned(e,0,tp,false,false) and c:IsCanBeEffectTarget(e)
 end
 function c101007090.cost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk == 0 then
-		e:SetLabel(10)
+	if chk==0 then
+		e:SetLabel(100)
 		return true
 	end
 end
+function c101007090.check(g)
+	return g:GetClassCount(Card.GetCode)==#g
+end
 function c101007090.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	local g = Duel.GetMatchingGroup(c101007090.filtervalkyrie,tp,LOCATION_GRAVE,0,nil,e,tp)
-	local graveCount = g:GetClassCount(Card.GetCode)
-	local mZoneCount = Duel.GetLocationCount(tp,LOCATION_MZONE)
+	local g=Duel.GetMatchingGroup(c101007090.spfilter,tp,LOCATION_GRAVE,0,nil,e,tp)
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
 	if chkc then return false end
-	if chk == 0 then
-		if e:GetLabel() ~= 10 then return false end
-		return mZoneCount > 0 and g:GetCount() > 0 and Duel.CheckLPCost(tp,1000)
+	if chk==0 then
+		if e:GetLabel()~=100 then return false end
+		e:SetLabel(0)
+		return ft>0 and g:GetCount()>0 and Duel.CheckLPCost(tp,1000)
 	end
-	local n = graveCount
-	if graveCount > mZoneCount then
-		n = mZoneCount
+	e:SetLabel(0)
+	local ct=math.min(g:GetClassCount(Card.GetCode),ft)
+	if Duel.IsPlayerAffectedByEffect(tp,59822133) then
+		ct=1
 	end
-	if (Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT)) then
-		n = 1
+	local pay_list = {}
+	for p=1,ct do
+		if Duel.CheckLPCost(tp,1000*p) then table.insert(pay_list,p) end
 	end
 	Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(101007090,0))
-	local pay_list = {}
-	for p = 1, n do
-		if Duel.CheckLPCost(tp,1000*p) then table.insert(pay_list, p) end
-	end
-	local pay = Duel.AnnounceNumber(tp,table.unpack(pay_list))
+	local pay=Duel.AnnounceNumber(tp,table.unpack(pay_list))
 	Duel.PayLPCost(tp,pay*1000)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local spg = Group.CreateGroup()
-	for p2 = 1, n do
-		if g:GetCount() == 0 then
-			break
-		end
-		local add = g:Select(tp,1,1,nil):GetFirst()
-		spg:AddCard(add)
-		g = g:Remove(Card.IsCode,nil,add:GetCode())
-	end
-	Duel.SetTargetCard(spg)
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,spg:GetCount(),spg,tp,0)
+	local sg=g:SelectSubGroup(tp,c101007090.check,false,pay,pay)
+	Duel.SetTargetCard(sg)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,sg,sg:GetCount(),0,0)
 end
-function c101007090.filter2(c,e,sp)
-	return c:IsAttackBelow(2000) and c:IsCanBeSpecialSummoned(e,0,sp,false,false)
+function c101007090.spfilter2(c,e,tp)
+	return c:IsAttackBelow(2000) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function c101007090.operation(e,tp,eg,ep,ev,re,r,rp)
-	local g1, ft1 = Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS):Filter(Card.IsRelateToEffect,nil,e), Duel.GetLocationCount(tp,LOCATION_MZONE)
-	if ft1 <= 0 then return end
-	if Duel.IsPlayerAffectedByEffect(tp,59822133) then ft1 = 1 end
-	local sg = g1
-	if g1:GetCount() > ft1 then
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	if ft<=0 then return end
+	local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS)
+	local sg=g:Filter(Card.IsRelateToEffect,nil,e)
+	if sg:GetCount()>1 and Duel.IsPlayerAffectedByEffect(tp,59822133) then return end
+	if sg:GetCount()>ft then
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-		sg = g1:Select(tp,ft1,ft1,nil)
+		sg=sg:Select(tp,ft,ft,nil)
 	end
-	local count = Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP)
-	if count <= 0 then return end
-	local g2 = Duel.GetMatchingGroup(c101007090.filter2,tp,0,LOCATION_GRAVE,nil,e,1-tp)
-	local ft2 = Duel.GetLocationCount(1-tp,LOCATION_MZONE)
-	if ft2 > count then ft2 = count end
-	if Duel.IsPlayerAffectedByEffect(1-tp,59822133) then ft2 = 1 end
-	if g2:GetCount() > 0 and ft2 > 0 and Duel.SelectYesNo(1-tp,aux.Stringid(101007090,1)) then
+	local ct=Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP)
+	if ct<=0 then return end
+	local g2=Duel.GetMatchingGroup(c101007090.spfilter2,tp,0,LOCATION_GRAVE,nil,e,1-tp)
+	local ct2=math.min(Duel.GetLocationCount(1-tp,LOCATION_MZONE),ct)
+	if g2:GetCount()>0 and ct2>0
+		and Duel.SelectYesNo(1-tp,aux.Stringid(101007090,1)) then
+		if Duel.IsPlayerAffectedByEffect(1-tp,59822133) then ct2=1 end
 		Duel.BreakEffect()
 		Duel.Hint(HINT_SELECTMSG,1-tp,HINTMSG_SPSUMMON)
-		sg = g2:Select(1-tp,1,ft2,nil)
+		sg=g2:Select(1-tp,1,ct2,nil)
 		Duel.SpecialSummon(sg,0,1-tp,1-tp,false,false,POS_FACEUP)
 	end
 end
--- scripts/SAST-EN/c101007091.lua
+++ scripts/SAST-EN/c101007091.lua
@@ -24,8 +24,23 @@ function c101007091.initial_effect(c)
 	e3:SetCode(EVENT_REMOVE)
 	c:RegisterEffect(e3)
 end
-function c101007091.valkyriefilter(c)
+function c101007091.cfilter(c)
 	return c:IsFaceup() and c:IsSetCard(0x122)
+end
+function c101007091.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsAbleToRemove() and chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(1-tp) end
+	if chk==0 then return Duel.IsExistingMatchingCard(c101007091.cfilter,tp,LOCATION_MZONE,0,1,nil)
+		and Duel.IsExistingTarget(Card.IsAbleToRemove,tp,0,LOCATION_GRAVE,1,nil) end
+	local ct=Duel.GetMatchingGroupCount(c101007091.cfilter,tp,LOCATION_MZONE,0,nil)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local g=Duel.SelectTarget(tp,Card.IsAbleToRemove,tp,0,LOCATION_GRAVE,1,ct,nil)
+	Duel.SetOperationInfo(0,CATEGORY_REMOVE,g,g:GetCount(),0,0)
+end
+function c101007091.activate(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS):Filter(Card.IsRelateToEffect,nil,e)
+	if g:GetCount()>0 then
+		Duel.Remove(g,POS_FACEUP,REASON_EFFECT)
+	end
 end
 function c101007091.drcon(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
@@ -33,29 +48,13 @@ function c101007091.drcon(e,tp,eg,ep,ev,re,r,rp)
 		and c:IsPreviousLocation(LOCATION_ONFIELD) and c:IsPreviousPosition(POS_FACEDOWN)
 end
 function c101007091.drtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	local ct=Duel.GetMatchingGroupCount(c101007091.valkyriefilter,tp,LOCATION_MZONE,0,nil)
+	local ct=Duel.GetMatchingGroupCount(c101007091.cfilter,tp,LOCATION_MZONE,0,nil)
 	if chk==0 then return Duel.IsPlayerCanDraw(tp,ct+1) end
 	Duel.SetTargetPlayer(tp)
 	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,ct+1)
 end
 function c101007091.drop(e,tp,eg,ep,ev,re,r,rp)
 	local p=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER)
-	local ct=Duel.GetMatchingGroupCount(c101007091.valkyriefilter,tp,LOCATION_MZONE,0,nil)
+	local ct=Duel.GetMatchingGroupCount(c101007091.cfilter,p,LOCATION_MZONE,0,nil)
 	Duel.Draw(p,ct+1,REASON_EFFECT)
-end
-function c101007091.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsAbleToRemove() and chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(1-tp) end
-	if chk==0 then return Duel.IsExistingMatchingCard(c101007091.valkyriefilter,tp,LOCATION_MZONE,0,1,nil)
-		and Duel.IsExistingTarget(Card.IsAbleToRemove,tp,0,LOCATION_GRAVE,1,nil) end
-	local ct=Duel.GetMatchingGroupCount(c101007091.valkyriefilter,tp,LOCATION_MZONE,0,nil)
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
-	local g=Duel.SelectTarget(tp,Card.IsAbleToRemove,tp,0,LOCATION_GRAVE,1,ct,nil)
-	Duel.SetOperationInfo(0,CATEGORY_REMOVE,g,g:GetCount(),0,0)
-end
-function c101007091.activate(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS)
-	g = g:Filter(Card.IsRelateToEffect,nil,e)
-	if g:GetCount() > 0 then
-		Duel.Remove(g,POS_FACEUP,REASON_EFFECT)
-	end
 end
