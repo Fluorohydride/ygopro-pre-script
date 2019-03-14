331472e6393a6458d2c629204349606bd2953899
mercury233 <me@mercury233.me>
2019-03-14 23:02:33 +0800

    change id

--- scripts/SR08-JP/c66104644.lua
+++ scripts/SR08-JP/c66104644.lua
@@ -1,11 +1,11 @@
 --マギステル・オブ・エンディミオン
 --
 --Script by JoyJ and mercury233
-function c100308003.initial_effect(c)
+function c66104644.initial_effect(c)
 	--Pendulum Summon
 	c:EnableCounterPermit(0x1,LOCATION_PZONE+LOCATION_MZONE)
 	aux.EnablePendulumAttribute(c)
-	c:SetSPSummonOnce(100308003)
+	c:SetSPSummonOnce(66104644)
 	--Add Counter
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
@@ -18,85 +18,85 @@ function c100308003.initial_effect(c)
 	e2:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
 	e2:SetCode(EVENT_CHAIN_SOLVED)
 	e2:SetRange(LOCATION_PZONE)
-	e2:SetOperation(c100308003.counterop)
+	e2:SetOperation(c66104644.counterop)
 	c:RegisterEffect(e2)
 	--Special Summon
 	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(100308003,0))
+	e3:SetDescription(aux.Stringid(66104644,0))
 	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e3:SetType(EFFECT_TYPE_IGNITION)
 	e3:SetRange(LOCATION_PZONE)
-	e3:SetCost(c100308003.spcost)
-	e3:SetTarget(c100308003.sptg)
-	e3:SetOperation(c100308003.spop)
+	e3:SetCost(c66104644.spcost)
+	e3:SetTarget(c66104644.sptg)
+	e3:SetOperation(c66104644.spop)
 	c:RegisterEffect(e3)
 	--add counter
 	local e4=Effect.CreateEffect(c)
-	e4:SetDescription(aux.Stringid(100308003,1))
+	e4:SetDescription(aux.Stringid(66104644,1))
 	e4:SetCategory(CATEGORY_COUNTER)
 	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e4:SetCode(EVENT_ATTACK_ANNOUNCE)
-	e4:SetTarget(c100308003.countertg2)
-	e4:SetOperation(c100308003.counterop2)
+	e4:SetTarget(c66104644.countertg2)
+	e4:SetOperation(c66104644.counterop2)
 	c:RegisterEffect(e4)
 	--special summon
 	local e5=Effect.CreateEffect(c)
-	e5:SetDescription(aux.Stringid(100308003,2))
+	e5:SetDescription(aux.Stringid(66104644,2))
 	e5:SetType(EFFECT_TYPE_QUICK_O)
 	e5:SetCode(EVENT_FREE_CHAIN)
 	e5:SetRange(LOCATION_MZONE)
 	e5:SetCountLimit(1)
 	e5:SetHintTiming(0,TIMING_END_PHASE)
-	e5:SetCondition(c100308003.spcon2)
-	e5:SetCost(c100308003.spcost2)
-	e5:SetTarget(c100308003.sptg2)
-	e5:SetOperation(c100308003.spop2)
+	e5:SetCondition(c66104644.spcon2)
+	e5:SetCost(c66104644.spcost2)
+	e5:SetTarget(c66104644.sptg2)
+	e5:SetOperation(c66104644.spop2)
 	c:RegisterEffect(e5)
 	--pendulum set
 	local e6=Effect.CreateEffect(c)
-	e6:SetDescription(aux.Stringid(100308003,1))
+	e6:SetDescription(aux.Stringid(66104644,1))
 	e6:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e6:SetProperty(EFFECT_FLAG_DELAY)
 	e6:SetCode(EVENT_DESTROYED)
-	e6:SetCondition(c100308003.pencon)
-	e6:SetTarget(c100308003.pentg)
-	e6:SetOperation(c100308003.penop)
+	e6:SetCondition(c66104644.pencon)
+	e6:SetTarget(c66104644.pentg)
+	e6:SetOperation(c66104644.penop)
 	c:RegisterEffect(e6)
 	local e7=Effect.CreateEffect(c)
 	e7:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
 	e7:SetCode(EVENT_LEAVE_FIELD_P)
 	e7:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-	e7:SetOperation(c100308003.regop)
+	e7:SetOperation(c66104644.regop)
 	e7:SetLabelObject(e6)
 	c:RegisterEffect(e7)
 end
-function c100308003.counterop(e,tp,eg,ep,ev,re,r,rp)
+function c66104644.counterop(e,tp,eg,ep,ev,re,r,rp)
 	if re:IsHasType(EFFECT_TYPE_ACTIVATE) and re:IsActiveType(TYPE_SPELL) and e:GetHandler():GetFlagEffect(1)>0 then
 		e:GetHandler():AddCounter(0x1,1)
 	end
 end
-function c100308003.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+function c66104644.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return e:GetHandler():IsCanRemoveCounter(tp,0x1,3,REASON_COST) end
 	e:GetHandler():RemoveCounter(tp,0x1,3,REASON_COST)
 end
-function c100308003.spfilter(c,e,tp)
+function c66104644.spfilter(c,e,tp)
 	return c:IsFaceup() and c:IsCanAddCounter(0x1,1,false,LOCATION_MZONE) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
-function c100308003.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+function c66104644.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
 	if chk==0 then return Duel.GetLocationCountFromEx(tp)>=1 and Duel.GetLocationCount(tp,LOCATION_MZONE)>=1
 		and Duel.GetUsableMZoneCount(tp)>=2 and not Duel.IsPlayerAffectedByEffect(tp,59822133)
 		and c:IsCanBeSpecialSummoned(e,0,tp,false,false) and c:IsCanAddCounter(0x1,1,false,LOCATION_MZONE)
-		and Duel.IsExistingMatchingCard(c100308003.spfilter,tp,LOCATION_EXTRA,0,1,nil,e,tp) end
+		and Duel.IsExistingMatchingCard(c66104644.spfilter,tp,LOCATION_EXTRA,0,1,nil,e,tp) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,2,tp,LOCATION_EXTRA+LOCATION_PZONE)
 end
-function c100308003.spop(e,tp,eg,ep,ev,re,r,rp)
+function c66104644.spop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if not c:IsRelateToEffect(e) then return end
 	if Duel.GetLocationCountFromEx(tp)<1 or Duel.GetLocationCount(tp,LOCATION_MZONE)<1
 		or Duel.GetUsableMZoneCount(tp)<1 or Duel.IsPlayerAffectedByEffect(tp,59822133) then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectMatchingCard(tp,c100308003.spfilter,tp,LOCATION_EXTRA,0,1,1,nil,e,tp)
+	local g=Duel.SelectMatchingCard(tp,c66104644.spfilter,tp,LOCATION_EXTRA,0,1,1,nil,e,tp)
 	if g:GetCount()>0 then
 		Duel.SpecialSummonStep(g:GetFirst(),0,tp,tp,false,false,POS_FACEUP)
 		Duel.SpecialSummonStep(c,0,tp,tp,false,false,POS_FACEUP)
@@ -107,43 +107,43 @@ function c100308003.spop(e,tp,eg,ep,ev,re,r,rp)
 		end
 	end
 end
-function c100308003.countertg2(e,tp,eg,ep,ev,re,r,rp,chk)
+function c66104644.countertg2(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return e:GetHandler():IsCanAddCounter(0x1,1) end
 	Duel.SetOperationInfo(0,CATEGORY_COUNTER,nil,1,0,0)
 end
-function c100308003.counterop2(e,tp,eg,ep,ev,re,r,rp)
+function c66104644.counterop2(e,tp,eg,ep,ev,re,r,rp)
 	e:GetHandler():AddCounter(0x1,1)
 end
-function c100308003.spcon2(e,tp,eg,ep,ev,re,r,rp)
+function c66104644.spcon2(e,tp,eg,ep,ev,re,r,rp)
 	return Duel.GetTurnPlayer()==1-tp
 end
-function c100308003.spcost2(e,tp,eg,ep,ev,re,r,rp,chk)
+function c66104644.spcost2(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsCanRemoveCounter(tp,1,0,0x1,3,REASON_COST) end
 	Duel.RemoveCounter(tp,1,0,0x1,3,REASON_COST)
 end
-function c100308003.spfilter2(c,e,tp)
+function c66104644.spfilter2(c,e,tp)
 	return c:IsCanAddCounter(0x1,1,false,LOCATION_MZONE) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
-function c100308003.sptg2(e,tp,eg,ep,ev,re,r,rp,chk)
+function c66104644.sptg2(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingMatchingCard(c100308003.spfilter2,tp,LOCATION_DECK,0,1,nil,e,tp) end
+		and Duel.IsExistingMatchingCard(c66104644.spfilter2,tp,LOCATION_DECK,0,1,nil,e,tp) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
 end
-function c100308003.spop2(e,tp,eg,ep,ev,re,r,rp)
+function c66104644.spop2(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local tc=Duel.SelectMatchingCard(tp,c100308003.spfilter2,tp,LOCATION_DECK,0,1,1,nil,e,tp):GetFirst()
+	local tc=Duel.SelectMatchingCard(tp,c66104644.spfilter2,tp,LOCATION_DECK,0,1,1,nil,e,tp):GetFirst()
 	if tc then
 		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
 	end
 end
-function c100308003.pencon(e,tp,eg,ep,ev,re,r,rp)
+function c66104644.pencon(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	return c:IsPreviousLocation(LOCATION_MZONE) and c:IsFaceup()
 end
-function c100308003.pentg(e,tp,eg,ep,ev,re,r,rp,chk)
+function c66104644.pentg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.CheckLocation(tp,LOCATION_PZONE,0) or Duel.CheckLocation(tp,LOCATION_PZONE,1) end
 end
-function c100308003.penop(e,tp,eg,ep,ev,re,r,rp)
+function c66104644.penop(e,tp,eg,ep,ev,re,r,rp)
 	if not Duel.CheckLocation(tp,LOCATION_PZONE,0) and not Duel.CheckLocation(tp,LOCATION_PZONE,1) then return end
 	local c=e:GetHandler()
 	local ct=e:GetLabel()
@@ -152,6 +152,6 @@ function c100308003.penop(e,tp,eg,ep,ev,re,r,rp)
 		c:AddCounter(0x1,ct)
 	end
 end
-function c100308003.regop(e,tp,eg,ep,ev,re,r,rp)
+function c66104644.regop(e,tp,eg,ep,ev,re,r,rp)
 	e:GetLabelObject():SetLabel(e:GetHandler():GetCounter(0x1))
 end
--- scripts/SR08-JP/c92559258.lua
+++ scripts/SR08-JP/c92559258.lua
@@ -1,11 +1,11 @@
 --サーヴァント・オブ・エンディミオン
 --
 --Script by JoyJ and mercury233
-function c100308004.initial_effect(c)
+function c92559258.initial_effect(c)
 	--Pendulum Summon
 	c:EnableCounterPermit(0x1,LOCATION_PZONE+LOCATION_MZONE)
 	aux.EnablePendulumAttribute(c)
-	c:SetSPSummonOnce(100308004)
+	c:SetSPSummonOnce(92559258)
 	--Add Counter
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
@@ -18,80 +18,80 @@ function c100308004.initial_effect(c)
 	e2:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
 	e2:SetCode(EVENT_CHAIN_SOLVED)
 	e2:SetRange(LOCATION_PZONE)
-	e2:SetOperation(c100308004.counterop)
+	e2:SetOperation(c92559258.counterop)
 	c:RegisterEffect(e2)
 	--Special Summon
 	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(100308004,0))
+	e3:SetDescription(aux.Stringid(92559258,0))
 	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e3:SetType(EFFECT_TYPE_IGNITION)
 	e3:SetRange(LOCATION_PZONE)
-	e3:SetCost(c100308004.spcost)
-	e3:SetTarget(c100308004.sptg)
-	e3:SetOperation(c100308004.spop)
+	e3:SetCost(c92559258.spcost)
+	e3:SetTarget(c92559258.sptg)
+	e3:SetOperation(c92559258.spop)
 	c:RegisterEffect(e3)
 	--direct attack
 	local e4=Effect.CreateEffect(c)
 	e4:SetType(EFFECT_TYPE_SINGLE)
 	e4:SetCode(EFFECT_DIRECT_ATTACK)
-	e4:SetCondition(c100308004.dacon)
+	e4:SetCondition(c92559258.dacon)
 	c:RegisterEffect(e4)
 	--add counter
 	local e5=Effect.CreateEffect(c)
-	e5:SetDescription(aux.Stringid(100308003,2))
+	e5:SetDescription(aux.Stringid(66104644,2))
 	e5:SetType(EFFECT_TYPE_QUICK_O)
 	e5:SetCode(EVENT_FREE_CHAIN)
 	e5:SetRange(LOCATION_MZONE)
 	e5:SetCountLimit(1)
 	e5:SetHintTiming(0,TIMING_END_PHASE)
-	e5:SetCondition(c100308004.countercon2)
-	e5:SetCost(c100308004.countercost2)
-	e5:SetTarget(c100308004.countertg2)
-	e5:SetOperation(c100308004.counterop2)
+	e5:SetCondition(c92559258.countercon2)
+	e5:SetCost(c92559258.countercost2)
+	e5:SetTarget(c92559258.countertg2)
+	e5:SetOperation(c92559258.counterop2)
 	c:RegisterEffect(e5)
 	--pendulum set
 	local e6=Effect.CreateEffect(c)
-	e6:SetDescription(aux.Stringid(100308004,1))
+	e6:SetDescription(aux.Stringid(92559258,1))
 	e6:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e6:SetProperty(EFFECT_FLAG_DELAY)
 	e6:SetCode(EVENT_DESTROYED)
-	e6:SetCondition(c100308004.pencon)
-	e6:SetTarget(c100308004.pentg)
-	e6:SetOperation(c100308004.penop)
+	e6:SetCondition(c92559258.pencon)
+	e6:SetTarget(c92559258.pentg)
+	e6:SetOperation(c92559258.penop)
 	c:RegisterEffect(e6)
 	local e7=Effect.CreateEffect(c)
 	e7:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
 	e7:SetCode(EVENT_LEAVE_FIELD_P)
 	e7:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-	e7:SetOperation(c100308004.regop)
+	e7:SetOperation(c92559258.regop)
 	e7:SetLabelObject(e6)
 	c:RegisterEffect(e7)
 end
-function c100308004.counterop(e,tp,eg,ep,ev,re,r,rp)
+function c92559258.counterop(e,tp,eg,ep,ev,re,r,rp)
 	if re:IsHasType(EFFECT_TYPE_ACTIVATE) and re:IsActiveType(TYPE_SPELL) and e:GetHandler():GetFlagEffect(1)>0 then
 		e:GetHandler():AddCounter(0x1,1)
 	end
 end
-function c100308004.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+function c92559258.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return e:GetHandler():IsCanRemoveCounter(tp,0x1,3,REASON_COST) end
 	e:GetHandler():RemoveCounter(tp,0x1,3,REASON_COST)
 end
-function c100308004.spfilter(c,e,tp)
+function c92559258.spfilter(c,e,tp)
 	return c:IsAttackAbove(1000) and c:IsCanAddCounter(0x1,1,false,LOCATION_MZONE) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
-function c100308004.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+function c92559258.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>=2 and not Duel.IsPlayerAffectedByEffect(tp,59822133)
 		and c:IsCanBeSpecialSummoned(e,0,tp,false,false) and c:IsCanAddCounter(0x1,1,false,LOCATION_MZONE)
-		and Duel.IsExistingMatchingCard(c100308004.spfilter,tp,LOCATION_DECK,0,1,nil,e,tp) end
+		and Duel.IsExistingMatchingCard(c92559258.spfilter,tp,LOCATION_DECK,0,1,nil,e,tp) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,2,tp,LOCATION_DECK+LOCATION_PZONE)
 end
-function c100308004.spop(e,tp,eg,ep,ev,re,r,rp)
+function c92559258.spop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if not c:IsRelateToEffect(e) then return end
 	if Duel.IsPlayerAffectedByEffect(tp,59822133) then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectMatchingCard(tp,c100308004.spfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
+	local g=Duel.SelectMatchingCard(tp,c92559258.spfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
 	if g:GetCount()>0 then
 		g:AddCard(c)
 		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
@@ -100,17 +100,17 @@ function c100308004.spop(e,tp,eg,ep,ev,re,r,rp)
 		end
 	end
 end
-function c100308004.dacon(e)
+function c92559258.dacon(e)
 	return e:GetHandler():GetCounter(0x1)>0
 end
-function c100308004.countercon2(e,tp,eg,ep,ev,re,r,rp)
+function c92559258.countercon2(e,tp,eg,ep,ev,re,r,rp)
 	return Duel.GetTurnPlayer()==1-tp
 end
-function c100308004.countercost2(e,tp,eg,ep,ev,re,r,rp,chk)
+function c92559258.countercost2(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,1,e:GetHandler()) end
 	Duel.DiscardHand(tp,Card.IsDiscardable,1,1,REASON_COST+REASON_DISCARD)
 end
-function c100308004.countertg2(e,tp,eg,ep,ev,re,r,rp,chk)
+function c92559258.countertg2(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
 	if chk==0 then return c:IsCanAddCounter(0x1,1)
 		and Duel.IsExistingMatchingCard(Card.IsCanAddCounter,tp,LOCATION_ONFIELD,0,1,c,0x1,1) end
@@ -118,7 +118,7 @@ function c100308004.countertg2(e,tp,eg,ep,ev,re,r,rp,chk)
 	g:AddCard(c)
 	Duel.SetOperationInfo(0,CATEGORY_COUNTER,g,#g,0,0)
 end
-function c100308004.counterop2(e,tp,eg,ep,ev,re,r,rp)
+function c92559258.counterop2(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local g=Duel.GetMatchingGroup(Card.IsCanAddCounter,tp,LOCATION_ONFIELD,0,c,0x1,1)
 	if c:IsRelateToEffect(e) then g:AddCard(c) end
@@ -128,14 +128,14 @@ function c100308004.counterop2(e,tp,eg,ep,ev,re,r,rp)
 		end
 	end
 end
-function c100308004.pencon(e,tp,eg,ep,ev,re,r,rp)
+function c92559258.pencon(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	return c:IsPreviousLocation(LOCATION_MZONE) and c:IsFaceup()
 end
-function c100308004.pentg(e,tp,eg,ep,ev,re,r,rp,chk)
+function c92559258.pentg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.CheckLocation(tp,LOCATION_PZONE,0) or Duel.CheckLocation(tp,LOCATION_PZONE,1) end
 end
-function c100308004.penop(e,tp,eg,ep,ev,re,r,rp)
+function c92559258.penop(e,tp,eg,ep,ev,re,r,rp)
 	if not Duel.CheckLocation(tp,LOCATION_PZONE,0) and not Duel.CheckLocation(tp,LOCATION_PZONE,1) then return end
 	local c=e:GetHandler()
 	local ct=e:GetLabel()
@@ -144,6 +144,6 @@ function c100308004.penop(e,tp,eg,ep,ev,re,r,rp)
 		c:AddCounter(0x1,ct)
 	end
 end
-function c100308004.regop(e,tp,eg,ep,ev,re,r,rp)
+function c92559258.regop(e,tp,eg,ep,ev,re,r,rp)
 	e:GetLabelObject():SetLabel(e:GetHandler():GetCounter(0x1))
 end
