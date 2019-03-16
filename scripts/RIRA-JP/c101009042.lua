c0e7b63d3a34c2e5d9b01936d8429b0428dc3c32
purerosefallen <78877@qq.com>
2019-03-16 22:32:44 +0800

    lq

---
+++ scripts/RIRA-JP/c101009042.lua
@@ -0,0 +1,87 @@
+--海晶少女 石狗公
+function c101009042.initial_effect(c)
+    --link summon
+	aux.AddLinkProcedure(c,aux.FilterBoolFunction(Card.IsLinkAttribute,ATTRIBUTE_WATER),2)
+	c:EnableReviveLimit()
+	--special summon
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(101009042,0))
+	e1:SetCategory(CATEGORY_TOHAND)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1,101009042)
+	e1:SetTarget(c101009042.thtg)
+	e1:SetOperation(c101009042.thop)
+	c:RegisterEffect(e1)
+	--negate attack
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(101009042,0))
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_ATTACK_ANNOUNCE)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCost(c101009042.cost)
+	e2:SetCondition(c101009042.condition)
+	e2:SetOperation(c101009042.operation)
+	c:RegisterEffect(e2)
+end
+function c101009042.thfilter(c)
+	return c:IsSetCard(0x22b) and c:IsType(TYPE_MONSTER) and not c:IsCode(101009042) and c:IsAbleToHand()
+end
+function c101009042.thtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and c101009042.thfilter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(c101009042.thfilter,tp,LOCATION_GRAVE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectTarget(tp,c101009042.thfilter,tp,LOCATION_GRAVE,0,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,0,0)
+end
+function c101009042.thop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) then
+		Duel.SendtoHand(tc,nil,REASON_EFFECT)
+	end
+end
+function c101009042.cfilter(c)
+	return c:IsSetCard(0x22b) and c:IsType(TYPE_MONSTER) and c:IsAbleToGraveAsCost()
+end
+function c101009042.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c101009042.cfilter,tp,LOCATION_HAND,0,1,nil) end
+	Duel.DiscardHand(tp,c101009042.cfilter,1,1,REASON_COST)
+	e:GetHandler():RegisterFlagEffect(101009042,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,0,1)
+end
+function c101009042.condition(e,tp,eg,ep,ev,re,r,rp)
+	return eg:GetFirst():IsControler(1-tp)
+end
+function c101009042.operation(e,tp,eg,ep,ev,re,r,rp)
+	local a=Duel.GetAttacker()
+	local d=Duel.GetAttackTarget()
+	if a then
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+		e1:SetValue(1)
+		e1:SetReset(RESET_PHASE+PHASE_DAMAGE)
+		a:RegisterEffect(e1)
+		local e2=Effect.CreateEffect(e:GetHandler())
+		e2:SetType(EFFECT_TYPE_SINGLE)
+		e2:SetCode(EFFECT_AVOID_BATTLE_DAMAGE)
+		e2:SetValue(1)
+		e2:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_DAMAGE)
+		a:RegisterEffect(e2)
+	end
+	if d then
+		local e3=Effect.CreateEffect(e:GetHandler())
+		e3:SetType(EFFECT_TYPE_SINGLE)
+		e3:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+		e3:SetValue(1)
+		e3:SetReset(RESET_PHASE+PHASE_DAMAGE)
+		d:RegisterEffect(e3)
+		local e4=Effect.CreateEffect(e:GetHandler())
+		e4:SetType(EFFECT_TYPE_SINGLE)
+		e4:SetCode(EFFECT_AVOID_BATTLE_DAMAGE)
+		e4:SetValue(1)
+		e4:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_DAMAGE)
+		d:RegisterEffect(e4)
+	end
+end
