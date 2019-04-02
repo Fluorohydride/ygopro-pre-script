b85d01cb14f7f3b0b6113a380e73c1dbacb0bf93
mercury233 <me@mercury233.me>
2019-04-02 12:16:56 +0800

    add scripts RIRA by 222DIY

---
+++ scripts/RIRA-JP/c101009038.lua
@@ -0,0 +1,90 @@
+--フォーチュンレディ・エヴァリー
+--
+--Script by 夜落诗染
+function c101009038.initial_effect(c)
+	--synchro summon
+	aux.AddSynchroProcedure(c,nil,aux.NonTuner(Card.IsRace,RACE_SPELLCASTER),1)
+	c:EnableReviveLimit()
+	--atk,def
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCode(EFFECT_SET_ATTACK)
+	e1:SetValue(c101009038.value)
+	c:RegisterEffect(e1)
+	local e2=e1:Clone()
+	e2:SetCode(EFFECT_SET_DEFENSE)
+	c:RegisterEffect(e2)
+	--level up
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(101009038,0))
+	e3:SetCategory(CATEGORY_REMOVE)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCountLimit(1)
+	e3:SetCode(EVENT_PHASE+PHASE_STANDBY)
+	e3:SetCondition(c101009038.lvcon)
+	e3:SetOperation(c101009038.lvop)
+	c:RegisterEffect(e3)
+	--spsummon
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(101009038,1))
+	e4:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e4:SetCode(EVENT_PHASE+PHASE_END)
+	e4:SetRange(LOCATION_GRAVE)
+	e4:SetCountLimit(1)
+	e4:SetCondition(c101009038.spcon)
+	e4:SetCost(c101009038.spcost)
+	e4:SetTarget(c101009038.sptg)
+	e4:SetOperation(c101009038.spop)
+	c:RegisterEffect(e4)
+end
+function c101009038.value(e,c)
+	return c:GetLevel()*400
+end
+function c101009038.lvcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetTurnPlayer()==tp and e:GetHandler():IsLevelAbove(1) and e:GetHandler():IsLevelBelow(11)
+end
+function c101009038.rmfilter(c)
+	return c:IsAbleToRemove() and c:IsFaceup()
+end
+function c101009038.lvop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsFacedown() or not c:IsRelateToEffect(e) or c:IsLevelAbove(12) then return end
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_UPDATE_LEVEL)
+	e1:SetValue(1)
+	e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_DISABLE)
+	c:RegisterEffect(e1)
+	local sg=Duel.GetMatchingGroup(c101009038.rmfilter,tp,0,LOCATION_MZONE,nil)
+	if sg:GetCount()>0 and Duel.SelectYesNo(tp,aux.Stringid(101009038,2)) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+		local tg=sg:Select(tp,1,1,nil)
+		Duel.Remove(tg,POS_FACEUP,REASON_EFFECT)
+	end
+end
+function c101009038.spcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetTurnPlayer()~=tp
+end
+function c101009038.cfilter(c)
+	return c:IsAbleToRemoveAsCost() and c:IsRace(RACE_SPELLCASTER) and c:IsType(TYPE_MONSTER)
+end
+function c101009038.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c101009038.cfilter,tp,LOCATION_GRAVE,0,1,e:GetHandler()) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local tg=Duel.SelectMatchingCard(tp,c101009038.cfilter,tp,LOCATION_GRAVE,0,1,1,e:GetHandler())
+	Duel.Remove(tg,POS_FACEUP,REASON_COST)
+end
+function c101009038.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetMZoneCount(tp)>0 and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
+end
+function c101009038.spop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) and Duel.GetMZoneCount(tp)>0 then
+		Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
---
+++ scripts/RIRA-JP/c101009056.lua
@@ -0,0 +1,52 @@
+--フォーチュンレディ・コーリング
+--
+--Script by 夜落诗染
+function c101009056.initial_effect(c)
+	--spsummon
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCountLimit(1,101009056+EFFECT_COUNT_CODE_OATH)
+	e1:SetCondition(c101009056.spcon)
+	e1:SetTarget(c101009056.sptg)
+	e1:SetOperation(c101009056.spop)
+	c:RegisterEffect(e1)
+end
+function c101009056.cfilter(c)
+	return c:IsFaceup() and c:IsSetCard(0x31)
+end
+function c101009056.spcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsExistingMatchingCard(c101009056.cfilter,tp,LOCATION_MZONE,0,1,nil)
+end
+function c101009056.tfilter(c,e,tp)
+	return c:IsSetCard(0x31) and c:IsType(TYPE_MONSTER) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+		and not Duel.IsExistingMatchingCard(c101009056.bfilter,tp,LOCATION_ONFIELD,0,1,nil,c)
+end
+function c101009056.bfilter(c,tc)
+	return tc:IsCode(c:GetCode()) and c:IsFaceup()
+end
+function c101009056.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetMZoneCount(tp)>0 and Duel.IsExistingMatchingCard(c101009056.tfilter,tp,LOCATION_DECK,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
+end
+function c101009056.spop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetMZoneCount(tp)<1 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,c101009056.tfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
+	if g:GetCount()>0 then
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+	end
+	if not e:IsHasType(EFFECT_TYPE_ACTIVATE) then return end
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetTargetRange(1,0)
+	e1:SetTarget(c101009056.splimit)
+	e1:SetReset(RESET_PHASE+PHASE_END)
+	Duel.RegisterEffect(e1,tp)
+end
+function c101009056.splimit(e,c)
+	return not c:IsType(TYPE_SYNCHRO) and c:IsLocation(LOCATION_EXTRA)
+end
