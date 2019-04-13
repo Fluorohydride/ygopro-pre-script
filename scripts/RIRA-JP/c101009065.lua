98f21cbd17b2db2664ce20ef1d607857a6eb92b3
nekrozar <nekrozar.s@gmail.com>
2019-04-13 13:33:44 +0900

    add script RIRA

---
+++ scripts/RIRA-JP/c101009065.lua
@@ -0,0 +1,73 @@
+--遮攻カーテン
+
+--Script by nekrozar
+function c101009065.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e1)
+	--destroy replace
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e2:SetCode(EFFECT_DESTROY_REPLACE)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetTarget(c101009065.reptg1)
+	e2:SetValue(c101009065.repval)
+	e2:SetOperation(c101009065.repop1)
+	c:RegisterEffect(e2)
+	--destroy replace
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e3:SetCode(EFFECT_DESTROY_REPLACE)
+	e3:SetRange(LOCATION_GRAVE)
+	e3:SetTarget(c101009065.reptg2)
+	e3:SetValue(c101009065.repval)
+	e3:SetOperation(c101009065.repop2)
+	c:RegisterEffect(e3)
+end
+function c101009065.repfilter1(c,tp)
+	return c:IsLocation(LOCATION_ONFIELD) and c:IsControler(tp)
+		and (c:IsReason(REASON_BATTLE) or (c:IsReason(REASON_EFFECT) and c:GetReasonPlayer()==1-tp)) and not c:IsReason(REASON_REPLACE)
+end
+function c101009065.reptg1(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return eg:IsExists(c101009065.repfilter1,1,c,tp)
+		and c:IsDestructable(e) and not c:IsStatus(STATUS_DESTROY_CONFIRMED) end
+	if Duel.SelectEffectYesNo(tp,c,96) then
+		local sg=eg
+		if eg:FilterCount(c101009065.repfilter1,1,c,tp)>1 then
+			Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(101009065,0))
+			sg=eg:FilterSelect(tp,c101009065.repfilter1,1,1,c,tp)
+		end
+		e:SetLabelObject(sg)
+		return true
+	else return false end
+end
+function c101009065.repval(e,c)
+	local g=e:GetLabelObject()
+	return g:IsContains(c)
+end
+function c101009065.repop1(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Destroy(e:GetHandler(),REASON_EFFECT+REASON_REPLACE)
+end
+function c101009065.repfilter2(c,tp)
+	return c:IsLocation(LOCATION_ONFIELD) and c:IsControler(1-tp)
+		and c:IsReason(REASON_BATTLE+REASON_EFFECT) and not c:IsReason(REASON_REPLACE)
+end
+function c101009065.reptg2(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return eg:IsExists(c101009065.repfilter2,1,nil,tp) and c:IsAbleToRemove() end
+	if Duel.SelectEffectYesNo(tp,c,96) then
+		local sg=eg
+		if eg:FilterCount(c101009065.repfilter2,1,nil,tp)>1 then
+			Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(101009065,0))
+			sg=eg:FilterSelect(tp,c101009065.repfilter2,1,1,nil,tp)
+		end
+		e:SetLabelObject(sg)
+		return true
+	else return false end
+end
+function c101009065.repop2(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Remove(e:GetHandler(),POS_FACEUP,REASON_EFFECT)
+end
