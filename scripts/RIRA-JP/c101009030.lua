45af754a1da149b041c479e6f7d3e87ef6d28f21
nekrozar <nekrozar.s@gmail.com>
2019-04-13 11:42:32 +0900

    add script RIRA

---
+++ scripts/RIRA-JP/c101009030.lua
@@ -0,0 +1,46 @@
+--満天禍コルドー
+
+--Script by nekrozar
+function c101009030.initial_effect(c)
+	--special summon
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(101009030,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TODECK)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
+	e1:SetCode(EVENT_DESTROYED)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCountLimit(1,101009030)
+	e1:SetCondition(c101009030.spcon)
+	e1:SetTarget(c101009030.sptg)
+	e1:SetOperation(c101009030.spop)
+	c:RegisterEffect(e1)
+end
+function c101009030.cfilter(c,tp)
+	return c:GetPreviousControler()==tp and c:IsPreviousLocation(LOCATION_MZONE)
+		and c:IsPreviousPosition(POS_FACEUP) and bit.band(c:GetPreviousAttributeOnField(),ATTRIBUTE_WIND)~=0
+		and (c:IsReason(REASON_BATTLE) or c:IsReason(REASON_EFFECT) and c:GetReasonPlayer()==1-tp)
+end
+function c101009030.spcon(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(c101009030.cfilter,1,nil,tp)
+end
+function c101009030.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
+end
+function c101009030.tdfilter(c)
+	return c:IsType(TYPE_SPELL+TYPE_TRAP) and c:IsAbleToDeck()
+end
+function c101009030.spop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not c:IsRelateToEffect(e) then return end
+	local g=Duel.GetMatchingGroup(c101009030.tdfilter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,nil)
+	if Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)>0
+		and g:GetCount()>0 and Duel.SelectYesNo(tp,aux.Stringid(101009030,1)) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
+		local sg=g:Select(tp,1,1,nil)
+		Duel.HintSelection(sg)
+		Duel.SendtoDeck(sg,nil,0,REASON_EFFECT)
+	end
+end
---
+++ scripts/RIRA-JP/c101009036.lua
@@ -0,0 +1,66 @@
+--メギストリーの儀術師
+
+--Script by nekrozar
+function c101009036.initial_effect(c)
+	c:EnableCounterPermit(0x1)
+	--add counter
+	local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
+	e0:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e0:SetCode(EVENT_CHAINING)
+	e0:SetRange(LOCATION_MZONE)
+	e0:SetOperation(aux.chainreg)
+	c:RegisterEffect(e0)
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
+	e1:SetCode(EVENT_CHAIN_SOLVED)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetOperation(c101009036.acop)
+	c:RegisterEffect(e1)
+	--tohand
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(101009036,0))
+	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1,101009036)
+	e2:SetCost(c101009036.thcost)
+	e2:SetTarget(c101009036.thtg)
+	e2:SetOperation(c101009036.thop)
+	c:RegisterEffect(e2)
+end
+function c101009036.acop(e,tp,eg,ep,ev,re,r,rp)
+	if re:IsHasType(EFFECT_TYPE_ACTIVATE) and re:IsActiveType(TYPE_SPELL) and e:GetHandler():GetFlagEffect(1)>0 then
+		e:GetHandler():AddCounter(0x1,1)
+	end
+end
+function c101009036.thcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsCanRemoveCounter(tp,0x1,3,REASON_COST) end
+	e:GetHandler():RemoveCounter(tp,0x1,3,REASON_COST)
+end
+function c101009036.thfilter1(c,tp)
+	return c:IsFaceup() and c:IsType(TYPE_SPELL)
+		and Duel.IsExistingMatchingCard(c101009036.thfilter2,tp,LOCATION_DECK,0,1,nil,c:GetCode())
+end
+function c101009036.thfilter2(c,code)
+	return c:IsCode(code) and c:IsAbleToHand()
+end
+function c101009036.thtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_REMOVED) and c101009036.thfilter1(chkc,tp) end
+	if chk==0 then return Duel.IsExistingTarget(c101009036.thfilter1,tp,LOCATION_REMOVED,0,1,nil,tp) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+	Duel.SelectTarget(tp,c101009036.thfilter1,tp,LOCATION_REMOVED,0,1,1,nil,tp)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function c101009036.thop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+		local g=Duel.SelectMatchingCard(tp,c101009036.thfilter2,tp,LOCATION_DECK,0,1,1,nil,tc:GetCode())
+		if g:GetCount()>0 then
+			Duel.SendtoHand(g,nil,REASON_EFFECT)
+			Duel.ConfirmCards(1-tp,g)
+		end
+	end
+end
