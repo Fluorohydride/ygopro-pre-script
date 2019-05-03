9c3fb77eebc013fe380d9c71a3a123924a616649
purerosefallen <78877@qq.com>
2019-05-03 14:30:38 +0800

    move

---
+++ scripts/DANE-EN/c101008085.lua
@@ -0,0 +1,67 @@
+--You're in Danger!
+--
+--Script by Real_Scl
+function c101008085.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetHintTiming(0,TIMINGS_CHECK_MONSTER+TIMING_END_PHASE)
+	e1:SetCountLimit(1,101008085+EFFECT_COUNT_CODE_OATH)
+	e1:SetTarget(c101008085.target)
+	e1:SetOperation(c101008085.activate)
+	c:RegisterEffect(e1)	
+end
+function c101008085.spfilter(c,e,tp)
+	return c:IsSetCard(0x11e) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function c101008085.spfilter2(g)
+	return g:GetClassCount(Card.GetCode)==3
+end
+function c101008085.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then
+		local dg=Duel.GetMatchingGroup(c101008085.spfilter,tp,LOCATION_DECK,0,nil,e,tp)
+		return dg:GetClassCount(Card.GetCode)>=3 and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+	end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
+end
+function c101008085.activate(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(c101008085.spfilter,tp,LOCATION_DECK,0,nil,e,tp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 or g:GetClassCount(Card.GetCode)<3 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CONFIRM)
+	local sg=g:SelectSubGroup(tp,c101008085.spfilter2,false,3,3)
+	if #sg>0 then
+		Duel.ConfirmCards(1-tp,sg)
+		local tc=sg:RandomSelect(1-tp,1):GetFirst()
+		Duel.ConfirmCards(tp,tc)
+		if Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)>0 then
+			local fid=tc:GetFieldID()
+			tc:RegisterFlagEffect(101008085,RESET_EVENT+RESETS_STANDARD,0,1,fid)
+			local e1=Effect.CreateEffect(e:GetHandler())
+			e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+			e1:SetCode(EVENT_PHASE+PHASE_END)
+			e1:SetCountLimit(1)
+			e1:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
+			e1:SetLabel(fid)
+			e1:SetLabelObject(tc)
+			e1:SetCondition(c101008085.descon)
+			e1:SetOperation(c101008085.desop)
+			Duel.RegisterEffect(e1,tp)
+		end
+		Duel.ShuffleDeck(tp)
+	end
+end
+function c101008085.descon(e,tp,eg,ep,ev,re,r,rp)
+	local tc=e:GetLabelObject()
+	if tc:GetFlagEffectLabel(101008085)~=e:GetLabel() then
+		e:Reset()
+		return false
+	else
+		return true
+	end
+end
+function c101008085.desop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=e:GetLabelObject()
+	Duel.Destroy(tc,REASON_EFFECT)
+end
