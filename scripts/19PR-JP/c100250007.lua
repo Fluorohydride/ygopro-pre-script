2f9d04aebf36bd4e60b31f9517a9a4d2cbe8109a
mercury233 <me@mercury233.me>
2019-05-09 17:00:09 +0800

    fix

--- scripts/19PR-JP/c100250007.lua
+++ scripts/19PR-JP/c100250007.lua
@@ -9,15 +9,21 @@ function c100250007.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetRange(LOCATION_HAND)
 	e1:SetCountLimit(1,100250007)
+	e1:SetCost(c100250007.spcost)
 	e1:SetTarget(c100250007.sptg)
 	e1:SetOperation(c100250007.spop)
 	c:RegisterEffect(e1)	
+end
+function c100250007.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return not e:GetHandler():IsPublic() end
 end
 function c100250007.rmfilter(c)
 	return c:IsFacedown() and c:IsAbleToRemove()
 end
 function c100250007.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(c100250007.rmfilter,tp,LOCATION_EXTRA,0,1,nil) and Duel.IsPlayerCanDraw(tp,1) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	if chk==0 then return Duel.IsExistingMatchingCard(c100250007.rmfilter,tp,LOCATION_EXTRA,0,1,nil)
+		and Duel.IsPlayerCanDraw(tp,1) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
 	Duel.SetOperationInfo(0,CATEGORY_REMOVE,nil,1,tp,LOCATION_EXTRA)
 	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
@@ -26,12 +32,14 @@ function c100250007.spop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local g=Duel.GetMatchingGroup(c100250007.rmfilter,tp,LOCATION_EXTRA,0,nil)
 	if g:GetCount()==0 then return end
-	if not c:IsRelateToEffect(e) then return end
 	local tc=g:RandomSelect(tp,1):GetFirst()
-	if Duel.Remove(tc,POS_FACEUP,REASON_EFFECT)~=0 and tc:IsType(TYPE_LINK) then
-		Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
-	elseif not tc:IsType(TYPE_LINK) then
-		Duel.SendtoGrave(e:GetHandler(),REASON_DISCARD)
-		Duel.Draw(tp,1,REASON_EFFECT)
+	if Duel.Remove(tc,POS_FACEUP,REASON_EFFECT)~=0 and tc:IsLocation(LOCATION_REMOVED)
+		and c:IsRelateToEffect(e) then
+		if tc:IsType(TYPE_LINK) then
+			Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
+		else
+			Duel.SendtoGrave(e:GetHandler(),REASON_DISCARD)
+			Duel.Draw(tp,1,REASON_EFFECT)
+		end
 	end
 end
