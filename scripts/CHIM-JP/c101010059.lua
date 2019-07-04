4c03be9c37aa1dd9e073ed585cdecf1c7b78d601
mercury233 <me@mercury233.me>
2019-07-04 22:16:53 +0800

    fix アロマガーデニング

--- scripts/CHIM-JP/c101010059.lua
+++ scripts/CHIM-JP/c101010059.lua
@@ -59,12 +59,12 @@ end
 function c101010059.spfilter(c,e,tp)
 	return c:IsSetCard(0xc9) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
-function c101010059.sptg1(e,tp,eg,ep,ev,re,r,rp,chk)
+function c101010059.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
 		and Duel.IsExistingMatchingCard(c101010059.spfilter,tp,LOCATION_DECK,0,1,nil,e,tp) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
 end
-function c101010059.spop1(e,tp,eg,ep,ev,re,r,rp)
+function c101010059.spop(e,tp,eg,ep,ev,re,r,rp)
 	if not e:GetHandler():IsRelateToEffect(e) or Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 	local g=Duel.SelectMatchingCard(tp,c101010059.spfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
