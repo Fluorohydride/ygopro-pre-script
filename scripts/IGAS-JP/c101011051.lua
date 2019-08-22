ecc1b7f2a4836d858c97ad88b995595a685c9596
nekrozar <nekrozar.s@gmail.com>
2019-08-22 10:47:42 +0900

    add script IGAS, LVDS, SR09

---
+++ scripts/IGAS-JP/c101011051.lua
@@ -0,0 +1,78 @@
+--Ai打ち
+
+--Scripted by nekrozar
+function c101011051.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_ATKCHANGE+CATEGORY_DAMAGE)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_PRE_DAMAGE_CALCULATE)
+	e1:SetCountLimit(1,101011051+EFFECT_COUNT_CODE_OATH)
+	e1:SetCondition(c101011051.condition)
+	e1:SetOperation(c101011051.activate)
+	c:RegisterEffect(e1)
+	--destroy replace
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_DESTROY_REPLACE)
+	e2:SetRange(LOCATION_GRAVE)
+	e2:SetTarget(c101011051.reptg)
+	e2:SetValue(c101011051.repval)
+	e2:SetOperation(c101011051.repop)
+	c:RegisterEffect(e2)
+end
+function c101011051.condition(e,tp,eg,ep,ev,re,r,rp)
+	local a=Duel.GetAttacker()
+	local d=Duel.GetAttackTarget()
+	return a and d and a:IsFaceup() and d:IsFaceup() and a:GetAttack()~=d:GetAttack()
+end
+function c101011051.activate(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local a=Duel.GetAttacker()
+	local d=Duel.GetAttackTarget()
+	if a:IsControler(1-tp) then a,d=d,a end
+	if a:IsFaceup() and a:IsRelateToBattle() and d:IsFaceup() and d:IsRelateToBattle() then
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_SET_ATTACK_FINAL)
+		e1:SetValue(d:GetAttack())
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_DAMAGE_CAL)
+		a:RegisterEffect(e1)
+		local g=Group.FromCards(a,d)
+		g:KeepAlive()
+		local e2=Effect.CreateEffect(c)
+		e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		e2:SetCode(EVENT_BATTLED)
+		e2:SetLabelObject(g)
+		e2:SetOperation(c101011051.damop)
+		e2:SetReset(RESET_PHASE+PHASE_DAMAGE)
+		Duel.RegisterEffect(e2,tp)
+	end
+end
+function c101011051.damop(e,tp,eg,ep,ev,re,r,rp)
+	local g=e:GetLabelObject()
+	local tg=Filter(Card.IsStatus,nil,STATUS_BATTLE_DESTROYED)
+	local tc1=tg:Filter(Card.IsControler,nil,tp):GetFirst()
+	local tc2=tg:Filter(Card.IsControler,nil,1-tp):GetFirst()
+	if tc1 then
+		Duel.Damage(tp,tc1:GetBaseAttack(),REASON_EFFECT,true)
+	end
+	if tc2 then
+		Duel.Damage(1-tp,tc2:GetBaseAttack(),REASON_EFFECT,true)
+	end
+	Duel.RDComplete()
+	g:DeleteGroup()
+end
+function c101011051.repfilter(c,tp)
+	return c:IsFaceup() and c:IsControler(tp) and c:IsLocation(LOCATION_MZONE) c:IsSetCard(0x235) and c:IsReason(REASON_BATTLE) and not c:IsReason(REASON_REPLACE)
+end
+function c101011051.reptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return eg:IsExists(c101011051.repfilter,1,nil,tp) and e:GetHandler():IsAbleToRemove() end
+	return Duel.SelectEffectYesNo(tp,e:GetHandler(),96)
+end
+function c101011051.repval(e,c)
+	return c101011051.repfilter(c,e:GetHandlerPlayer())
+end
+function c101011051.repop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Remove(e:GetHandler(),POS_FACEUP,REASON_EFFECT+REASON_REPLACE)
+end
---
+++ scripts/LVDS-JP/c100254005.lua
@@ -0,0 +1,79 @@
+--トリックスター・フェス
+
+--Scripted by nekrozar
+function c100254005.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOKEN)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCountLimit(1,100254005+EFFECT_COUNT_CODE_OATH)
+	e1:SetCost(c100254005.cost)
+	e1:SetTarget(c100254005.target)
+	e1:SetOperation(c100254005.activate)
+	c:RegisterEffect(e1)
+	--destroy replace
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_DESTROY_REPLACE)
+	e2:SetRange(LOCATION_GRAVE)
+	e2:SetTarget(c100254005.reptg)
+	e2:SetValue(c100254005.repval)
+	e2:SetOperation(c100254005.repop)
+	c:RegisterEffect(e2)
+	Duel.AddCustomActivityCounter(100254005,ACTIVITY_SUMMON,c100254005.counterfilter)
+	Duel.AddCustomActivityCounter(100254005,ACTIVITY_SPSUMMON,c100254005.counterfilter)
+end
+function c100254005.counterfilter(c)
+	return c:IsSetCard(0xfb)
+end
+function c100254005.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetCustomActivityCount(100254005,tp,ACTIVITY_SUMMON)==0
+		and Duel.GetCustomActivityCount(100254005,tp,ACTIVITY_SPSUMMON)==0 end
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_OATH)
+	e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+	e1:SetReset(RESET_PHASE+PHASE_END)
+	e1:SetTargetRange(1,0)
+	e1:SetTarget(c100254005.sumlimit)
+	Duel.RegisterEffect(e1,tp)
+	local e2=e1:Clone()
+	e2:SetCode(EFFECT_CANNOT_SUMMON)
+	Duel.RegisterEffect(e2,tp)
+end
+function c100254005.sumlimit(e,c,sump,sumtype,sumpos,targetp,se)
+	return  c:IsSetCard(0xfb)
+end
+function c100254005.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return not Duel.IsPlayerAffectedByEffect(tp,59822133)
+		and Duel.GetLocationCount(tp,LOCATION_MZONE)>1
+		and Duel.IsPlayerCanSpecialSummonMonster(tp,100254105,0xfb,0x4011,0,0,1,RACE_FAIRY,ATTRIBUTE_LIGHT) end
+	Duel.SetOperationInfo(0,CATEGORY_TOKEN,nil,2,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,2,0,0)
+end
+function c100254005.activate(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.IsPlayerAffectedByEffect(tp,59822133) then return end
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)>1
+		and Duel.IsPlayerCanSpecialSummonMonster(tp,100254105,0xfb,0x4011,0,0,1,RACE_FAIRY,ATTRIBUTE_LIGHT) then
+		for i=1,2 do
+			local token=Duel.CreateToken(tp,100254105)
+			Duel.SpecialSummonStep(token,0,tp,tp,false,false,POS_FACEUP)
+		end
+		Duel.SpecialSummonComplete()
+	end
+end
+function c100254005.repfilter(c,tp)
+	return c:IsFaceup() and c:IsControler(tp) and c:IsLocation(LOCATION_MZONE) and c:GetSummonLocation()==LOCATION_EXTRA and c:IsSetCard(0xfb)
+		and c:IsReason(REASON_BATTLE+REASON_EFFECT) and not c:IsReason(REASON_REPLACE)
+end
+function c100254005.reptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return eg:IsExists(c100254005.repfilter,1,nil,tp) and e:GetHandler():IsAbleToRemove() end
+	return Duel.SelectEffectYesNo(tp,e:GetHandler(),96)
+end
+function c100254005.repval(e,c)
+	return c100254005.repfilter(c,e:GetHandlerPlayer())
+end
+function c100254005.repop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Remove(e:GetHandler(),POS_FACEUP,REASON_EFFECT+REASON_REPLACE)
+end
