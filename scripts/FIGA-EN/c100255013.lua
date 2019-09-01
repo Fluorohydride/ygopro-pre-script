ba8688be317cab0650a004f5f066d5b63f4f8ff7
mercury233 <me@mercury233.me>
2019-09-01 18:14:20 +0800

    fix 寿炎星 Panda

--- scripts/FIGA-EN/c100255013.lua
+++ scripts/FIGA-EN/c100255013.lua
@@ -39,7 +39,7 @@ end
 function c100255013.spop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if c:IsRelateToEffect(e) and Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)~=0 then
-		local g=Duel.GetMatchingGroup(aux.NecroValleyFilter(c100255013.spfilter),tp,LOCATION_GRAVE,0,nil)
+		local g=Duel.GetMatchingGroup(aux.NecroValleyFilter(c100255013.spfilter),tp,LOCATION_GRAVE,0,nil,e,tp)
 		if g:GetCount()>0 and Duel.SelectYesNo(tp,aux.Stringid(100255013,1)) then
 			Duel.BreakEffect()
 			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
