a7d7f07272c84666642dc4e0c0cf6ce59355a86f
mercury233 <me@mercury233.me>
2019-09-14 16:51:34 +0800

    add new scripts EP19 SJMP

---
+++ scripts/SJMP-JP/c100204008.lua
@@ -0,0 +1,73 @@
+--リングリボー
+--
+--Script by mercury233
+function c100204008.initial_effect(c)
+	--link summon
+	aux.AddLinkProcedure(c,c100204008.mfilter,1,1)
+	c:EnableReviveLimit()
+	--disable
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(100204008,0))
+	e1:SetCategory(CATEGORY_DISABLE+CATEGORY_REMOVE)
+	e1:SetType(EFFECT_TYPE_QUICK_O)
+	e1:SetCode(EVENT_CHAINING)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1,100204008)
+	e1:SetCondition(c100204008.discon)
+	e1:SetCost(c100204008.discost)
+	e1:SetTarget(c100204008.distg)
+	e1:SetOperation(c100204008.disop)
+	c:RegisterEffect(e1)
+	--special summon
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(100204008,1))
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetRange(LOCATION_GRAVE)
+	e2:SetCountLimit(1,100204008+100)
+	e2:SetCost(c100204008.spcost)
+	e2:SetTarget(c100204008.sptg)
+	e2:SetOperation(c100204008.spop)
+	c:RegisterEffect(e2)
+end
+function c100204008.mfilter(c)
+	return c:IsLevelBelow(4) and c:IsLinkRace(RACE_CYBERSE)
+end
+function c100204008.discon(e,tp,eg,ep,ev,re,r,rp)
+	return ep~=tp and re:IsActiveType(TYPE_TRAP) and re:IsHasType(EFFECT_TYPE_ACTIVATE) and Duel.IsChainNegatable(ev)
+end
+function c100204008.discost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsReleasable() end
+	Duel.Release(e:GetHandler(),REASON_COST)
+end
+function c100204008.distg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return aux.nbcon(tp,re) end
+	Duel.SetOperationInfo(0,CATEGORY_DISABLE,eg,1,0,0)
+	if re:GetHandler():IsRelateToEffect(re) then
+		Duel.SetOperationInfo(0,CATEGORY_REMOVE,eg,1,0,0)
+	end
+end
+function c100204008.disop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.NegateEffect(ev) and re:GetHandler():IsRelateToEffect(re) then
+		Duel.Remove(eg,POS_FACEUP,REASON_EFFECT)
+	end
+end
+function c100204008.cfilter(c,tp)
+	return c:IsSetCard(0x235) and c:GetSummonLocation()==LOCATION_EXTRA and Duel.GetMZoneCount(tp,c)>0
+end
+function c100204008.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.CheckReleaseGroup(tp,c100204008.cfilter,1,nil,tp) end
+	local g=Duel.SelectReleaseGroup(tp,c100204008.costfilter,1,1,nil,tp)
+	Duel.Release(g,REASON_COST)
+end
+function c100204008.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
+end
+function c100204008.spop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) then
+		Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
