074b3c02353533a864f948486e6d1cf44b348e0e
mercury233 <me@mercury233.me>
2019-09-23 16:38:12 +0800

    fix アースゴーレム＠イグニスター

--- scripts/IGAS-JP/c101011041.lua
+++ scripts/IGAS-JP/c101011041.lua
@@ -74,7 +74,7 @@ function c101011041.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 end
 function c101011041.spop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
-	if tc:IsRelateToEffect(e) and  then
+	if tc:IsRelateToEffect(e) then
 		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
 	end
 end
