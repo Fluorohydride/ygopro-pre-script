eecc21b852f78af3a7fb71f5d9ca96c49500782f
mercury233 <me@mercury233.me>
2019-09-24 09:21:17 +0800

    fix

--- scripts/DP23-JP/c100423002.lua
+++ scripts/DP23-JP/c100423002.lua
@@ -26,92 +26,74 @@ end
 function c100423002.cfilter(c)
 	return c:IsLevelAbove(6) and c:IsRace(RACE_SPELLCASTER) and c:IsAbleToGraveAsCost()
 end
-function c100423002.spfilter(c,e,tp)
-	return (c:IsCode(46986414) or c:IsCode(38033121)) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
-end
 function c100423002.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(c100423002.cfilter,tp,LOCATION_DECK,0,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
 	local g=Duel.SelectMatchingCard(tp,c100423002.cfilter,tp,LOCATION_DECK,0,1,1,nil)
-	if g:GetCount()>0 then
-		Duel.SendtoGrave(g,REASON_COST)
-	end
+	Duel.SendtoGrave(g,REASON_COST)
 end
 function c100423002.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if Duel.GetLocationCount(tp,LOCATION_MZONE)<0 then return false end
-	local b1=e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false)
-	local b2=e:GetHandler():IsAbleToGrave()
+	local c=e:GetHandler()
+	local b1=c:IsCanBeSpecialSummoned(e,0,tp,false,false) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+	local b2=c:IsAbleToGrave()
 	if chk==0 then return b1 or b2 end
 	local s=0
 	if b1 and not b2 then
-		e:SetLabel(0)
+		s=Duel.SelectOption(tp,aux.Stringid(100423002,0))
 	elseif not b1 and b2 then
-		e:SetLabel(1)
+		s=Duel.SelectOption(tp,aux.Stringid(100423002,1))+1
 	elseif b1 and b2 then
 		s=Duel.SelectOption(tp,aux.Stringid(100423002,0),aux.Stringid(100423002,1))
-		if s==0 then
-			e:SetLabel(0)
-		end
-		if s==1 then
-			e:SetLabel(1)
-		end
 	end
-	if e:GetLabel()==0 then
-		Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
-	end
-	if e:GetLabel()==1 then
-		e:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOGRAVE)
-		Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,e:GetHandler(),1,0,0)
+	e:SetLabel(s)
+	if s==0 then
+		e:SetCategory(CATEGORY_SPECIAL_SUMMON)
+		Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,0,0)
+	else
+		e:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_HANDES)
+		Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,c,1,0,0)
 		Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE)
 	end
+end
+function c100423002.spfilter(c,e,tp)
+	return c:IsCode(46986414,38033121) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function c100423002.spop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if e:GetLabel()==0 and c:IsRelateToEffect(e) then
 		Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
 	end
-	if e:GetLabel()==1 and c:IsRelateToEffect(e) and Duel.SendtoGrave(c,REASON_EFFECT)
-		and Duel.IsExistingMatchingCard(c100423002.spfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp)
+	if e:GetLabel()==1 and c:IsRelateToEffect(e)
+		and Duel.SendtoGrave(c,REASON_EFFECT)>0 and c:IsLocation(LOCATION_GRAVE)
+		and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(aux.NecroValleyFilter(c100423002.spfilter),tp,LOCATION_GRAVE,0,1,nil,e,tp)
 		and Duel.SelectYesNo(tp,aux.Stringid(100423002,2)) then
 		Duel.BreakEffect()
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 		local g=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(c100423002.spfilter),tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
-		if g:GetCount()>0 then
-			Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
-		end
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
 	end
 end
 function c100423002.drfilter(c)
 	return c:IsType(TYPE_SPELL+TYPE_TRAP) and c:IsAbleToGraveAsCost()
 end
-function c100423002.drfilter2(c)
-	return c:IsType(TYPE_SPELL+TYPE_TRAP) and c:IsAbleToGraveAsCost() and c:IsFaceup()
-end
 function c100423002.drcost(e,tp,eg,ep,ev,re,r,rp,chk)
-	local b1=Duel.IsExistingMatchingCard(c100423002.drfilter,tp,LOCATION_HAND+LOCATION_ONFIELD,0,1,nil)
-	or Duel.IsExistingMatchingCard(c100423002.drfilter2,tp,LOCATION_ONFIELD,0,1,nil)
-	local b2=Duel.IsPlayerCanDraw(tp,1)
-	local b3=Duel.IsPlayerCanDraw(tp,2)
-	local gc=0
-	if not b2 then return false end
-	if b2 and not b3 then gc=1 end
-	if b3 then gc=2 end
-	if chk==0 then return b1 and (b2 or b3) end
-	local g1=Duel.GetMatchingGroup(c100423002.drfilter,tp,LOCATION_HAND,0,nil)
-	local g2=Duel.GetMatchingGroup(c100423002.drfilter2,tp,LOCATION_ONFIELD,0,nil)
-	g1:Merge(g2)
+	if chk==0 then return Duel.IsExistingMatchingCard(c100423002.drfilter,tp,LOCATION_HAND+LOCATION_ONFIELD,0,1,nil) end
+	local gc=1
+	if Duel.IsPlayerCanDraw(tp,2) then gc=2 end
+	local g1=Duel.GetMatchingGroup(c100423002.drfilter,tp,LOCATION_HAND+LOCATION_ONFIELD,0,nil)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
 	local g=g1:Select(tp,1,gc,nil)
-	if g:GetCount()>0 then
-		e:SetLabel(Duel.SendtoGrave(g,REASON_COST))
-	end
+	e:SetLabel(Duel.SendtoGrave(g,REASON_COST))
 end
 function c100423002.drtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return true end
-	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,e:GetLabel())
+	if chk==0 then return Duel.IsPlayerCanDraw(tp,1) end
+	local ct=e:GetLabel()
+	Duel.SetTargetPlayer(tp)
+	Duel.SetTargetParam(ct)
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,ct)
 end
 function c100423002.drop(e,tp,eg,ep,ev,re,r,rp)
-	if e:GetLabel()>0 then
-		Duel.Draw(tp,e:GetLabel(),REASON_EFFECT)
-	end
+	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+	Duel.Draw(p,d,REASON_EFFECT)
 end
