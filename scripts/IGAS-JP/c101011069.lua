ca0bc8ac1455d0b87b617f9013b1d582bc314634
mallu11 <54900811+mallu11@users.noreply.github.com>
2019-09-26 13:45:36 +0800

    add Aiシャドー (#292)
    * Update c100200172.lua
    
    Fix the problem of the third effect that can't activate in the battle phase and damage step.
    
    * Create c100309002.lua
    
    * Delete c100309002.lua
    
    * add Aiシャドー
    
    * Update c101011069.lua
    
    * Update c101011069.lua
    
    * fix
    
    * fix

---
+++ scripts/IGAS-JP/c101011069.lua
@@ -0,0 +1,85 @@
+--Aiシャドー
+
+--Scripted by mallu11
+function c101011069.initial_effect(c)
+	--activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_CONTINUOUS_TARGET)
+	e1:SetCondition(c101011069.condition)
+	e1:SetTarget(c101011069.target)
+	e1:SetOperation(c101011069.activate)
+	c:RegisterEffect(e1)
+	--atk up
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_UPDATE_ATTACK)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+	e2:SetValue(800)
+	e2:SetTarget(aux.ctg)
+	c:RegisterEffect(e2)
+	--must attack
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetCode(EFFECT_MUST_ATTACK)
+	e3:SetRange(LOCATION_SZONE)
+	e3:SetTargetRange(0,LOCATION_MZONE)
+	c:RegisterEffect(e3)
+	--only attack monster
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_FIELD)
+	e4:SetCode(EFFECT_ONLY_ATTACK_MONSTER)
+	e4:SetRange(LOCATION_SZONE)
+	e4:SetTargetRange(0,LOCATION_MZONE)
+	e4:SetValue(aux.ctg)
+	c:RegisterEffect(e4)
+	--draw
+	local e5=Effect.CreateEffect(c)
+	e5:SetCategory(CATEGORY_DRAW)
+	e5:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e5:SetCode(EVENT_TO_GRAVE)
+	e5:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e5:SetCountLimit(1,101011069)
+	e5:SetCondition(c101011069.drcon)
+	e5:SetTarget(c101011069.drtg)
+	e5:SetOperation(c101011069.drop)
+	c:RegisterEffect(e5)
+	local e6=e5:Clone()
+	e6:SetCode(EVENT_REMOVE)
+	c:RegisterEffect(e6)
+end
+function c101011069.cfilter(c)
+	return c:IsFaceup() and c:IsSetCard(0x235)
+end
+function c101011069.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetCurrentPhase()~=PHASE_DAMAGE or not Duel.IsDamageCalculated()
+end
+function c101011069.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and c101011069.cfilter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(c101011069.cfilter,tp,LOCATION_MZONE,0,1,nil) end
+	Duel.SelectTarget(tp,c101011069.cfilter,tp,LOCATION_MZONE,0,1,1,nil)
+end
+function c101011069.activate(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tc=Duel.GetFirstTarget()
+	if c:IsRelateToEffect(e) and tc:IsRelateToEffect(e) then
+		c:SetCardTarget(tc)
+	end
+end
+function c101011069.drcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return c:IsPreviousPosition(POS_FACEUP) and c:IsPreviousLocation(LOCATION_SZONE) and c:GetPreviousControler()==tp
+		and c:IsReason(REASON_EFFECT) and rp==1-tp
+end
+function c101011069.drtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetTargetPlayer(tp)
+	Duel.SetTargetParam(1)
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
+end
+function c101011069.drop(e,tp,eg,ep,ev,re,r,rp)
+	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+	Duel.Draw(p,d,REASON_EFFECT)
+end
