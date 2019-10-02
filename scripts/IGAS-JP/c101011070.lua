dc96c62cd851e493574b5625ffdf04dc1566b5b5
mallu11 <54900811+mallu11@users.noreply.github.com>
2019-10-03 03:38:14 +0800

    add 戦華史略－長坂之雄

---
+++ scripts/IGAS-JP/c101011070.lua
@@ -0,0 +1,93 @@
+--戦華史略－長坂之雄
+
+--Scripted by mallu11
+function c101011070.initial_effect(c)
+    --activate
+    local e0=Effect.CreateEffect(c)
+    e0:SetType(EFFECT_TYPE_ACTIVATE)
+    e0:SetCode(EVENT_FREE_CHAIN)
+    c:RegisterEffect(e0)
+    --actlimit
+    local e1=Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_FIELD)
+    e1:SetCode(EFFECT_CANNOT_ACTIVATE)
+    e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+    e1:SetRange(LOCATION_SZONE)
+    e1:SetTargetRange(0,1)
+    e1:SetValue(c101011070.aclimit)
+    e1:SetCondition(c101011070.actcon)
+    c:RegisterEffect(e1)
+    --can not be attack target
+    local e2=Effect.CreateEffect(c)
+    e2:SetDescription(aux.Stringid(101011070,0))
+    e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+    e2:SetCode(EVENT_PHASE+PHASE_BATTLE_START)
+    e2:SetRange(LOCATION_SZONE)
+    e2:SetCountLimit(1,101011070)
+    e2:SetCondition(c101011070.atcon)
+    e2:SetCost(c101011070.atcost)
+    e2:SetOperation(c101011070.atop)
+    c:RegisterEffect(e2)
+    --spsummon
+    local e3=Effect.CreateEffect(c)
+    e3:SetDescription(aux.Stringid(101011070,1))
+    e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
+    e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+    e3:SetCode(EVENT_ATTACK_ANNOUNCE)
+    e3:SetRange(LOCATION_GRAVE)
+    e3:SetCountLimit(1,101011070)
+    e3:SetCondition(c101011070.spcon)
+    e3:SetCost(c101011070.spcost)
+    e3:SetTarget(c101011070.sptg)
+    e3:SetOperation(c101011070.spop)
+    c:RegisterEffect(e3)
+end
+function c101011070.aclimit(e,re,tp)
+    return re:IsHasType(EFFECT_TYPE_ACTIVATE)
+end
+function c101011070.actcon(e,tp,eg,ep,ev,re,r,rp)
+    local ac=Duel.GetAttacker()
+    local tc=Duel.GetAttackTarget()
+    if not ac then return false end
+    if ac:IsControler(tp) and ac:IsSetCard(0x237) then
+        return true
+    elseif tc and tc:IsControler(tp) and tc:IsSetCard(0x237) then
+        return true
+    else return false end
+end
+function c101011070.atcon(e,tp,eg,ep,ev,re,r,rp)
+    return Duel.GetTurnPlayer()==1-tp
+end
+function c101011070.atcost(e,tp,eg,ep,ev,re,r,rp,chk)
+    local c=e:GetHandler()
+    if chk==0 then return c:IsFaceup() and c:IsLocation(LOCATION_SZONE) and c:IsAbleToGraveAsCost() end
+    Duel.SendtoGrave(c,REASON_COST)
+end
+function c101011070.atop(e,tp,eg,ep,ev,re,r,rp)
+    local e1=Effect.CreateEffect(e:GetHandler())
+    e1:SetType(EFFECT_TYPE_FIELD)
+    e1:SetCode(effect_can)
+end
+function c101011070.spfilter(c,e,tp)
+    return c:IsSetCard(0x237) and c:IsType(TYPE_MONSTER) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function c101011070.spcon(e,tp,eg,ep,ev,re,r,rp)
+    return tp~=Duel.GetTurnPlayer()
+end
+function c101011070.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+    local c=e:GetHandler()
+    if chk==0 then return c:IsAbleToRemoveAsCost() end
+    Duel.Remove(c,POS_FACEUP,REASON_COST)
+end
+function c101011070.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.IsExistingMatchingCard(c101011070.spfilter,tp,LOCATION_DECK,0,1,nil,e,tp) end
+    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
+end
+function c101011070.spop(e,tp,eg,ep,ev,re,r,rp)
+    if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+    local g=Duel.SelectMatchingCard(tp,c101011070.spfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
+    if g:GetCount()>0 then
+        Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+    end
+end
