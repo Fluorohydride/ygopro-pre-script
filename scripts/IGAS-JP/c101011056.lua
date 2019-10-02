b598d0c5cc8ab6965e30389ca68b0f6e234bf1f7
mallu11 <54900811+mallu11@users.noreply.github.com>
2019-10-03 04:02:23 +0800

    Update c101011056.lua

--- scripts/IGAS-JP/c101011056.lua
+++ scripts/IGAS-JP/c101011056.lua
@@ -2,102 +2,102 @@
 
 --Scripted by mallu11
 function c101011056.initial_effect(c)
-    --activate
-    local e0=Effect.CreateEffect(c)
-    e0:SetType(EFFECT_TYPE_ACTIVATE)
-    e0:SetCode(EVENT_FREE_CHAIN)
-    c:RegisterEffect(e0)
-    --can not activate effect
-    local e1=Effect.CreateEffect(c)
-    e1:SetDescription(aux.Stringid(101011056,0))
-    e1:SetCategory(CATEGORY_ANNOUNCE)
-    e1:SetType(EFFECT_TYPE_IGNITION)
-    e1:SetRange(LOCATION_SZONE)
-    e1:SetCountLimit(1,101011056)
-    e1:SetCondition(c101011056.actcon)
-    e1:SetTarget(c101011056.acttg)
-    e1:SetOperation(c101011056.actop)
-    c:RegisterEffect(e1)
-    --atk up
-    local e2=Effect.CreateEffect(c)
-    e2:SetDescription(aux.Stringid(101011056,1))
-    e2:SetCategory(CATEGORY_ATKCHANGE)
-    e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-    e2:SetCode(EVENT_SPSUMMON_SUCCESS)
-    e2:SetProperty(EFFECT_FLAG_DELAY)
-    e2:SetRange(LOCATION_SZONE)
-    e2:SetCountLimit(1,101011156)
-    e2:SetCondition(c101011056.atkcon)
-    e2:SetTarget(c101011056.atktg)
-    e2:SetOperation(c101011056.atkop)
-    c:RegisterEffect(e2)
-    local e3=Effect.CreateEffect(c)
-    e3:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
-    e3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-    e3:SetCode(EVENT_CHAINING)
-    e3:SetRange(LOCATION_MZONE)
-    e3:SetOperation(aux.chainreg)
-    c:RegisterEffect(e3)
-    local e4=e2:Clone()
-    e4:SetCode(EVENT_CHAIN_SOLVING)
-    e4:SetCondition(c101011056.atkcon2)
-    c:RegisterEffect(e4)
+	--activate
+	local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_ACTIVATE)
+	e0:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e0)
+	--can not activate effect
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(101011056,0))
+	e1:SetCategory(CATEGORY_ANNOUNCE)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_SZONE)
+	e1:SetCountLimit(1,101011056)
+	e1:SetCondition(c101011056.actcon)
+	e1:SetTarget(c101011056.acttg)
+	e1:SetOperation(c101011056.actop)
+	c:RegisterEffect(e1)
+	--atk up
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(101011056,1))
+	e2:SetCategory(CATEGORY_ATKCHANGE)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e2:SetProperty(EFFECT_FLAG_DELAY)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetCountLimit(1,101011156)
+	e2:SetCondition(c101011056.atkcon)
+	e2:SetTarget(c101011056.atktg)
+	e2:SetOperation(c101011056.atkop)
+	c:RegisterEffect(e2)
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
+	e3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e3:SetCode(EVENT_CHAINING)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetOperation(aux.chainreg)
+	c:RegisterEffect(e3)
+	local e4=e2:Clone()
+	e4:SetCode(EVENT_CHAIN_SOLVING)
+	e4:SetCondition(c101011056.atkcon2)
+	c:RegisterEffect(e4)
 end
 function c101011056.cfilter(c)
-    return c:IsFaceup() and c:IsSetCard(0x237)
+	return c:IsFaceup() and c:IsSetCard(0x237)
 end
 function c101011056.actcon(e,tp,eg,ep,ev,re,r,rp)
-    local g=Duel.GetFieldGroup(tp,LOCATION_MZONE,0)
-    local sg=g:Filter(c101011056.cfilter,nil)
-    return sg and sg:GetClassCount(Card.GetAttribute)>=2
+	local g=Duel.GetFieldGroup(tp,LOCATION_MZONE,0)
+	local sg=g:Filter(c101011056.cfilter,nil)
+	return sg and sg:GetClassCount(Card.GetAttribute)>=2
 end
 function c101011056.acttg(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.IsExistingMatchingCard(aux.TRUE,tp,0,LOCATION_MZONE,1,nil) end
-    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATTRIBUTE)
-    local att=Duel.AnnounceAttribute(tp,1,0xffff)
-    Duel.SetOperationInfo(0,CATEGORY_ANNOUNCE,nil,0,tp,0)
-    e:SetLabel(att)
+	if chk==0 then return Duel.IsExistingMatchingCard(aux.TRUE,tp,0,LOCATION_MZONE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATTRIBUTE)
+	local att=Duel.AnnounceAttribute(tp,1,0xffff)
+	Duel.SetOperationInfo(0,CATEGORY_ANNOUNCE,nil,0,tp,0)
+	e:SetLabel(att)
 end
 function c101011056.actop(e,tp,eg,ep,ev,re,r,rp)
-    local g=Duel.GetMatchingGroup(aux.TRUE,tp,0,LOCATION_MZONE,nil)
-    local sg=g:Filter(Card.IsAttribute,nil,e:GetLabel())
-    if sg:GetCount()<=0 then return end
-    local tc=sg:GetFirst()
-    while tc do
-        local e1=Effect.CreateEffect(e:GetHandler())
-        e1:SetType(EFFECT_TYPE_SINGLE)
-        e1:SetCode(EFFECT_CANNOT_TRIGGER)
-        e1:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
-        e1:SetReset(RESET_PHASE+PHASE_END)
-        tc:RegisterEffect(e1)
-        tc=sg:GetNext()
-    end
+	local g=Duel.GetMatchingGroup(aux.TRUE,tp,0,LOCATION_MZONE,nil)
+	local sg=g:Filter(Card.IsAttribute,nil,e:GetLabel())
+	if sg:GetCount()<=0 then return end
+	local tc=sg:GetFirst()
+	while tc do
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_CANNOT_TRIGGER)
+		e1:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
+		e1:SetReset(RESET_PHASE+PHASE_END)
+		tc:RegisterEffect(e1)
+		tc=sg:GetNext()
+	end
 end
 function c101011056.atkfilter(c)
-    return c:IsFaceup() and c:IsSetCard(0x237)
+	return c:IsFaceup() and c:IsSetCard(0x237)
 end
 function c101011056.atkcon(e,tp,eg,ep,ev,re,r,rp)
-    return rp==1-tp
+	return rp==1-tp
 end
 function c101011056.atktg(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.IsExistingMatchingCard(c101011056.atkfilter,tp,LOCATION_MZONE,0,1,nil) end
+	if chk==0 then return Duel.IsExistingMatchingCard(c101011056.atkfilter,tp,LOCATION_MZONE,0,1,nil) end
 end
 function c101011056.atkop(e,tp,eg,ep,ev,re,r,rp)
-    local g=Duel.GetMatchingGroup(c101011056.atkfilter,tp,LOCATION_MZONE,0,nil)
-    local gc=g:GetCount()
-    if gc>0 then
-        local tc=g:GetFirst()
-        while tc do
-            local e1=Effect.CreateEffect(e:GetHandler())
-            e1:SetType(EFFECT_TYPE_SINGLE)
-            e1:SetCode(EFFECT_UPDATE_ATTACK)
-            e1:SetValue(gc*300)
-            e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
-            tc:RegisterEffect(e1)
-            tc=g:GetNext()
-        end
-    end
+	local g=Duel.GetMatchingGroup(c101011056.atkfilter,tp,LOCATION_MZONE,0,nil)
+	local gc=g:GetCount()
+	if gc>0 then
+		local tc=g:GetFirst()
+		while tc do
+			local e1=Effect.CreateEffect(e:GetHandler())
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetCode(EFFECT_UPDATE_ATTACK)
+			e1:SetValue(gc*300)
+			e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+			tc:RegisterEffect(e1)
+			tc=g:GetNext()
+		end
+	end
 end
 function c101011056.atkcon2(e,tp,eg,ep,ev,re,r,rp)
-    return re:IsActiveType(TYPE_MONSTER) and re:GetHandler():IsSetCard(0x237) and rp==tp and e:GetHandler():GetFlagEffect(1)>0
+	return re:IsActiveType(TYPE_MONSTER) and re:GetHandler():IsSetCard(0x237) and rp==tp and e:GetHandler():GetFlagEffect(1)>0
 end
