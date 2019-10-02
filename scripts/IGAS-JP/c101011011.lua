24536ffedef803c41e988f22482f8a0ca4ebc890
mallu11 <54900811+mallu11@users.noreply.github.com>
2019-10-03 03:34:34 +0800

    add 戦華の徳－劉玄

---
+++ scripts/IGAS-JP/c101011011.lua
@@ -0,0 +1,86 @@
+--戦華の徳－劉玄
+
+--Scripted by mallu11
+function c101011011.initial_effect(c)
+	--can not be attack target
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_CANNOT_SELECT_BATTLE_TARGET)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCondition(c101011011.atcon)
+	c:RegisterEffect(e1)
+	--spsummon
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(101011011,0))
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1,101011011)
+	e2:SetCondition(c101011011.spcon)
+	e2:SetCost(c101011011.spcost)
+	e2:SetTarget(c101011011.sptg)
+	e2:SetOperation(c101011011.spop)
+	c:RegisterEffect(e2)
+	--draw
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(101011011,1))
+	e3:SetCategory(CATEGORY_DRAW)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e3:SetCode(EVENT_ATTACK_ANNOUNCE)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCountLimit(1,101011111)
+	e3:SetCondition(c101011011.drcon)
+	e3:SetTarget(c101011011.drtg)
+	e3:SetOperation(c101011011.drop)
+	c:RegisterEffect(e3)
+end
+function c101011011.atfilter(c)
+	return c:IsFaceup() and c:IsSetCard(0x237)
+end
+function c101011011.atcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsExistingMatchingCard(c101011011.atfilter,e:GetHandlerPlayer(),LOCATION_MZONE,0,1,e:GetHandler())
+end
+function c101011011.spfilter(c,e,tp)
+	return c:IsSetCard(0x237) and c:IsType(TYPE_MONSTER) and not c:IsCode(101011011) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function c101011011.spcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0)<Duel.GetFieldGroupCount(1-tp,LOCATION_MZONE,0)
+end
+function c101011011.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsAbleToGraveAsCost,tp,LOCATION_HAND+LOCATION_ONFIELD,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local g=Duel.SelectMatchingCard(tp,Card.IsAbleToGraveAsCost,tp,LOCATION_HAND+LOCATION_ONFIELD,0,1,1,nil)
+	Duel.SendtoGrave(g,REASON_COST)
+end
+function c101011011.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.IsExistingMatchingCard(c101011011.spfilter,tp,LOCATION_DECK,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
+end
+function c101011011.spop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,c101011011.spfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
+	if g:GetCount()>0 then
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
+function c101011011.drcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local ac=Duel.GetAttacker()
+	local tc=Duel.GetAttackTarget()
+	if ac:IsControler(tp) and ac:IsSetCard(0x237) and ac~=c then
+		return true
+	elseif tc and tc:IsControler(tp) and tc:IsSetCard(0x237) and tc~=c then
+		return true
+	else return false end
+end
+function c101011011.drtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsPlayerCanDraw(tp,1) end
+	Duel.SetTargetPlayer(tp)
+	Duel.SetTargetParam(1)
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
+end
+function c101011011.drop(e,tp,eg,ep,ev,re,r,rp)
+	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+	Duel.Draw(p,d,REASON_EFFECT)
+end
