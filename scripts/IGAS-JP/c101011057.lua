b5ac40c39657c9d7c8791e8208fdf9c09fcc75d0
mallu11 <54900811+mallu11@users.noreply.github.com>
2019-10-04 17:28:29 +0800

    add メガリス・ポータル

---
+++ scripts/IGAS-JP/c101011057.lua
@@ -0,0 +1,63 @@
+--メガリス・ポータル
+
+--Scripted by mallu11
+function c101011057.initial_effect(c)
+	--activate
+	local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_ACTIVATE)
+	e0:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e0)
+	--indes
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_INDESTRUCTABLE_COUNT)
+	e1:SetRange(LOCATION_FZONE)
+	e1:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+	e1:SetTarget(c101011057.indtg)
+	e1:SetValue(c101011057.indct)
+	c:RegisterEffect(e1)
+	--to hand
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(101011057,0))
+	e2:SetCategory(CATEGORY_TOHAND)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DELAY)
+	e2:SetRange(LOCATION_FZONE)
+	e2:SetCountLimit(1,101011057)
+	e2:SetCondition(c101011057.thcon)
+	e2:SetTarget(c101011057.thtg)
+	e2:SetOperation(c101011057.thop)
+	c:RegisterEffect(e2)
+end
+function c101011057.indtg(e,c)
+	return c:IsSummonType(SUMMON_TYPE_RITUAL)
+end
+function c101011057.indct(e,re,r,rp)
+	if bit.band(r,REASON_BATTLE)~=0 then
+		return 1
+	else return 0 end
+end
+function c101011057.cfilter(c)
+	return c:IsFaceup() and c:IsSetCard(0x238)
+end
+function c101011057.thfilter(c)
+	return c:IsType(TYPE_RITUAL) and c:IsAbleToHand()
+end
+function c101011057.thcon(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(c101011057.cfilter,1,nil)
+end
+function c101011057.thtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and c101011057.thfilter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(c101011057.thfilter,tp,LOCATION_GRAVE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectTarget(tp,c101011057.thfilter,tp,LOCATION_GRAVE,0,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,0,0)
+end
+function c101011057.thop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) then
+		Duel.SendtoHand(tc,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,tc)
+	end
+end
