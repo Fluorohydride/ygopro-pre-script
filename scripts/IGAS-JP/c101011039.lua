d7cc6e8c045d6db419add0c4713838c3edc221a1
mallu11 <54900811+mallu11@users.noreply.github.com>
2019-10-04 19:32:34 +0800

    Update c101011039.lua

--- scripts/IGAS-JP/c101011039.lua
+++ scripts/IGAS-JP/c101011039.lua
@@ -4,16 +4,14 @@
 function c101011039.initial_effect(c)
 	c:EnableReviveLimit()
 	--ritual summon
-	local e1=Effect.CreateEffect(c)
+	local e1=aux.AddRitualProcGreater(c,c101011039.filter,nil,nil,c101011039.mfilter)
 	e1:SetDescription(aux.Stringid(101011039,0))
 	e1:SetCategory(CATEGORY_RELEASE+CATEGORY_SPECIAL_SUMMON)
+	e1:SetCode(0)
 	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetRange(LOCATION_HAND)
 	e1:SetCountLimit(1,101011039)
 	e1:SetCost(c101011039.rscost)
-	e1:SetTarget(c101011039.rstg)
-	e1:SetOperation(c101011039.rsop)
-	c:RegisterEffect(e1)
 	--destroy
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(101011039,1))
@@ -30,49 +28,12 @@ end
 function c101011039.filter(c)
 	return c:IsSetCard(0x238)
 end
+function c101011039.mfilter(c,e,tp)
+	return c~=e:GetHandler()
+end
 function c101011039.rscost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return e:GetHandler():IsDiscardable() end
 	Duel.SendtoGrave(e:GetHandler(),REASON_COST+REASON_DISCARD)
-end
-function c101011039.rstg(e,tp,eg,ep,ev,re,r,rp,chk)
-	local c=e:GetHandler()
-	if chk==0 then
-		local mg=Duel.GetRitualMaterial(tp)
-		if mg:IsContains(c) then
-			mg:RemoveCard(c)
-		end
-		local sg=nil
-		return Duel.IsExistingMatchingCard(aux.RitualUltimateFilter,tp,LOCATION_HAND,0,1,c,c101011039.filter,e,tp,mg,sg,Card.GetLevel,"Greater")
-	end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
-end
-function c101011039.rsop(e,tp,eg,ep,ev,re,r,rp)
-	local mg=Duel.GetRitualMaterial(tp)
-	local sg=nil
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local tg=Duel.SelectMatchingCard(tp,aux.RitualUltimateFilter,tp,LOCATION_HAND,0,1,1,nil,c101011039.filter,e,tp,mg,sg,Card.GetLevel,"Greater")
-	local tc=tg:GetFirst()
-	if tc then
-		mg=mg:Filter(Card.IsCanBeRitualMaterial,tc,tc)
-		if sg then
-			mg:Merge(sg)
-		end
-		if tc.mat_filter then
-			mg=mg:Filter(tc.mat_filter,tc,tp)
-		else
-			mg:RemoveCard(tc)
-		end
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
-		aux.GCheckAdditional=aux.RitualCheckAdditional(tc,tc:GetLevel(),"Greater")
-		local mat=mg:SelectSubGroup(tp,aux.RitualCheck,false,1,tc:GetLevel(),tp,tc,tc:GetLevel(),"Greater")
-		aux.GCheckAdditional=nil
-		if not mat or mat:GetCount()==0 then return end
-		tc:SetMaterial(mat)
-		Duel.ReleaseRitualMaterial(mat)
-		Duel.BreakEffect()
-		Duel.SpecialSummon(tc,SUMMON_TYPE_RITUAL,tp,tp,false,true,POS_FACEUP)
-		tc:CompleteProcedure()
-	end
 end
 function c101011039.descon(e,tp,eg,ep,ev,re,r,rp)
 	return e:GetHandler():IsSummonType(SUMMON_TYPE_RITUAL)
