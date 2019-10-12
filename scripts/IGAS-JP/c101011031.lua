70c23a51dbd56a50486259898a4c5537df569a61
mallu11 <54900811+mallu11@users.noreply.github.com>
2019-10-13 00:03:23 +0800

    add チューン・ナイト

---
+++ scripts/IGAS-JP/c101011031.lua
@@ -0,0 +1,137 @@
+--チューン・ナイト
+
+--Scripted by mallu11
+function c101011031.initial_effect(c)
+	--turner
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(101011031,0))
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetProperty(EFFECT_FLAG_DELAY)
+	e1:SetTarget(c101011031.tntg)
+	e1:SetOperation(c101011031.tnop)
+	c:RegisterEffect(e1)
+	--equip
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(101011031,1))
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetCategory(CATEGORY_EQUIP)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetTarget(c101011031.eqtg)
+	e2:SetOperation(c101011031.eqop)
+	c:RegisterEffect(e2)
+	--unequip
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(101011031,2))
+	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e3:SetType(EFFECT_TYPE_IGNITION)
+	e3:SetRange(LOCATION_SZONE)
+	e3:SetTarget(c101011031.sptg)
+	e3:SetOperation(c101011031.spop)
+	c:RegisterEffect(e3)
+	--destroy sub
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_EQUIP)
+	e4:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
+	e4:SetCode(EFFECT_DESTROY_SUBSTITUTE)
+	e4:SetValue(c101011031.repval)
+	c:RegisterEffect(e4)
+	--eqlimit
+	local e5=Effect.CreateEffect(c)
+	e5:SetType(EFFECT_TYPE_SINGLE)
+	e5:SetCode(EFFECT_EQUIP_LIMIT)
+	e5:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e5:SetValue(c101011031.eqlimit)
+	c:RegisterEffect(e5)
+end
+function c101011031.tntg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return c:IsFaceup() and c:IsLevelAbove(1) and not c:IsType(TYPE_TUNER) and c:IsRelateToEffect(e) end
+end
+function c101011031.tnop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsFaceup() and c:IsRelateToEffect(e) and c:IsLevelAbove(1) then
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_ADD_TYPE)
+		e1:SetValue(TYPE_TUNER)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		c:RegisterEffect(e1)
+	end
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e2:SetCode(EVENT_SPSUMMON)
+	e2:SetCondition(c101011031.regcon)
+	e2:SetOperation(c101011031.regop)
+	e2:SetReset(RESET_PHASE+PHASE_END)
+	Duel.RegisterEffect(e2,tp)
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e3:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+	e3:SetTargetRange(1,0)
+	e3:SetCondition(c101011031.spcon)
+	e3:SetValue(c101011031.splimit)
+	e3:SetReset(RESET_PHASE+PHASE_END)
+	Duel.RegisterEffect(e3,tp)
+end
+function c101011031.cfilter(c)
+	return c:GetSummonLocation()==LOCATION_EXTRA
+end
+function c101011031.regcon(e,tp,eg,ep,ev,re,r,rp)
+	return ep==tp and eg:IsExists(c101011031.cfilter,1,nil)
+end
+function c101011031.regop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.RegisterFlagEffect(tp,101011031,RESET_PHASE+PHASE_END,0,1)
+end
+function c101011031.spcon(e)
+	local tp=e:GetHandlerPlayer()
+	return Duel.GetFlagEffect(tp,101011031)>0
+end
+function c101011031.splimit(e,c)
+	return c:IsLocation(LOCATION_EXTRA)
+end
+function c101011031.filter(c)
+	local ct1,ct2=c:GetUnionCount()
+	return c:IsFaceup() and ct2==0
+end
+function c101011031.eqtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	local c=e:GetHandler()
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and c101011031.filter(chkc) end
+	if chk==0 then return e:GetHandler():GetFlagEffect(101011031)==0 and Duel.GetLocationCount(tp,LOCATION_SZONE)>0
+	and Duel.IsExistingTarget(c101011031.filter,tp,LOCATION_MZONE,0,1,c) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)
+	local g=Duel.SelectTarget(tp,c101011031.filter,tp,LOCATION_MZONE,0,1,1,c)
+	Duel.SetOperationInfo(0,CATEGORY_EQUIP,g,1,0,0)
+	c:RegisterFlagEffect(c101011031,RESET_EVENT+0x7e0000+RESET_PHASE+PHASE_END,0,1)
+end
+function c101011031.eqop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tc=Duel.GetFirstTarget()
+	if not c:IsRelateToEffect(e) or c:IsFacedown() then return end
+	if not tc:IsRelateToEffect(e) or not c101011031.filter(tc) then
+		Duel.SendtoGrave(c,REASON_EFFECT)
+		return
+	end
+	if not Duel.Equip(tp,c,tc,false) then return end
+	aux.SetUnionState(c)
+end
+function c101011031.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return c:GetFlagEffect(101011031)==0 and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+	and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,true,false) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,0,0)
+	c:RegisterFlagEffect(101011031,RESET_EVENT+0x7e0000+RESET_PHASE+PHASE_END,0,1)
+end
+function c101011031.spop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not c:IsRelateToEffect(e) then return end
+	Duel.SpecialSummon(c,0,tp,tp,true,false,POS_FACEUP)
+end
+function c101011031.repval(e,re,r,rp)
+	return bit.band(r,REASON_BATTLE)~=0 or bit.band(r,REASON_EFFECT)~=0
+end
+function c101011031.eqlimit(e,c)
+	return e:GetHandler():GetEquipTarget()==c
+end
