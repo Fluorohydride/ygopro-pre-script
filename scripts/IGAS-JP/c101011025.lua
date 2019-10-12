ff96b7b3ca28e9e4d3dceda71e7303a70f42167b
mallu11 <54900811+mallu11@users.noreply.github.com>
2019-10-12 23:25:45 +0800

    add 楽天禍カルクラグラ

---
+++ scripts/IGAS-JP/c101011025.lua
@@ -0,0 +1,45 @@
+--楽天禍カルクラグラ
+
+--Scripted by mallu11
+function c101011025.initial_effect(c)
+	--spsummon
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(101011025,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOGRAVE)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_DESTROYED)
+	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCountLimit(1,101011025)
+	e1:SetCondition(c101011025.spcon)
+	e1:SetTarget(c101011025.sptg)
+	e1:SetOperation(c101011025.spop)
+	c:RegisterEffect(e1)
+end
+function c101011025.cfilter(c,tp,rp)
+	return c:GetPreviousControler()==tp and c:IsPreviousLocation(LOCATION_MZONE)
+	and c:IsPreviousPosition(POS_FACEUP) and bit.band(c:GetPreviousAttributeOnField(),ATTRIBUTE_EARTH)~=0
+	and (c:IsReason(REASON_BATTLE) or c:IsReason(REASON_EFFECT) and c:GetReasonPlayer()==1-tp)
+end
+function c101011025.spcon(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(c101011025.cfilter,1,nil,tp)
+end
+function c101011025.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
+end
+function c101011025.tgfilter(c)
+	return c:IsType(TYPE_MONSTER) and c:IsAbleToGrave()
+end
+function c101011025.spop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not c:IsRelateToEffect(e) then return end
+	local g=Duel.GetMatchingGroup(c101011025.tgfilter,tp,LOCATION_DECK,0,nil)
+	if Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)~=0
+	and g:GetCount()>0 and Duel.SelectYesNo(tp,aux.Stringid(101011025,1)) then
+		Duel.BreakEffect()
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+		local sg=g:Select(tp,1,1,nil)
+		Duel.SendtoGrave(sg,REASON_EFFECT)
+	end
+end
