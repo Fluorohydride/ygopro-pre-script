06e3f06ab3f7f007b33af7d2faf48e436f69d01d
mallu11 <54900811+mallu11@users.noreply.github.com>
2019-10-13 17:08:40 +0800

    fix クロシープ

--- scripts/IGAS-JP/c101011047.lua
+++ scripts/IGAS-JP/c101011047.lua
@@ -6,6 +6,7 @@ function c101011047.initial_effect(c)
 	aux.AddLinkProcedure(c,nil,2,2,c101011047.lcheck)
 	--activate
 	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_DRAW+CATEGORY_HANDES+CATEGORY_SPECIAL_SUMMON+CATEGORY_ATKCHANGE)
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
 	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
 	e1:SetProperty(EFFECT_FLAG_DELAY)
@@ -37,19 +38,12 @@ function c101011047.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	local b4=Duel.IsExistingMatchingCard(Card.IsFaceup,tp,0,LOCATION_MZONE,1,nil) and lg:IsExists(Card.IsType,1,nil,TYPE_XYZ)
 	if chk==0 then return b1 or b2 or b3 or b4 end
 	if b1 then
-		local cat=e:GetCategory()
-		e:SetCategory(bit.bor(cat,CATEGORY_DRAW+CATEGORY_HANDES))
 		Duel.SetTargetPlayer(tp)
 		Duel.SetTargetParam(2)
 	end
-	if b2 then
-		local cat=e:GetCategory()
-		e:SetCategory(bit.bor(cat,CATEGORY_SPECIAL_SUMMON))
-	end
-	if b3 or b4 then
-		local cat=e:GetCategory()
-		e:SetCategory(bit.bor(cat,CATEGORY_ATKCHANGE))
-	end
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,2)
+	Duel.SetOperationInfo(0,CATEGORY_HANDES,nil,0,tp,2)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE)
 end
 function c101011047.activate(e,tp,eg,ep,ev,re,r,rp)
 	local lg=e:GetHandler():GetLinkedGroup()
