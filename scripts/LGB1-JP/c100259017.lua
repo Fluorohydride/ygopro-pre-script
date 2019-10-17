33a64185d972f8d7c01b0045178d00a2dd9a0d0d
mallu11 <54900811+mallu11@users.noreply.github.com>
2019-10-17 14:21:49 +0800

    add シューティング・スター・ドラゴン・TG－EX

---
+++ scripts/LGB1-JP/c100259017.lua
@@ -0,0 +1,100 @@
+--シューティング・スター・ドラゴン・TG－EX
+
+--Scripted by mallu11
+function c100259017.initial_effect(c)
+	c:EnableReviveLimit()
+	aux.AddSynchroProcedure(c,aux.FilterBoolFunction(Card.IsSynchroType,TYPE_SYNCHRO),aux.NonTuner(Card.IsSynchroType,TYPE_SYNCHRO),1)
+	--negate
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(100259017,0))
+	e1:SetCategory(CATEGORY_NEGATE+CATEGORY_DESTROY)
+	e1:SetType(EFFECT_TYPE_QUICK_O)
+	e1:SetCode(EVENT_CHAINING)
+	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DAMAGE_CAL)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCondition(c100259017.discon)
+	e1:SetCost(c100259017.discost)
+	e1:SetTarget(c100259017.distg)
+	e1:SetOperation(c100259017.disop)
+	c:RegisterEffect(e1)
+	--disable attack
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(100259017,1))
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_ATTACK_ANNOUNCE)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1,100259017)
+	e2:SetCondition(c100259017.atkcon)
+	e2:SetOperation(c100259017.atkop)
+	c:RegisterEffect(e2)
+	--spsummon
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(100259017,2))
+	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e3:SetType(EFFECT_TYPE_QUICK_O)
+	e3:SetCode(EVENT_FREE_CHAIN)
+	e3:SetRange(LOCATION_GRAVE)
+	e3:SetCountLimit(1,100259117)
+	e3:SetCondition(c100259017.spcon)
+	e3:SetCost(c100259017.spcost)
+	e3:SetTarget(c100259017.sptg)
+	e3:SetOperation(c100259017.spop)
+	c:RegisterEffect(e3)
+end
+function c100259017.tfilter(c,tp)
+	return c:IsLocation(LOCATION_MZONE) and c:IsControler(tp)
+end
+function c100259017.disfilter(c)
+	return c:IsType(TYPE_TUNER) and c:IsAbleToRemoveAsCost()
+end
+function c100259017.discon(e,tp,eg,ep,ev,re,r,rp)
+	if e:GetHandler():IsStatus(STATUS_BATTLE_DESTROYED) then return false end
+	if not re:IsHasProperty(EFFECT_FLAG_CARD_TARGET) then return false end
+	local tg=Duel.GetChainInfo(ev,CHAININFO_TARGET_CARDS)
+	return tg and tg:IsExists(c100259017.tfilter,1,nil,tp) and Duel.IsChainNegatable(ev)
+end
+function c100259017.discost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c100259017.disfilter,tp,LOCATION_GRAVE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local g=Duel.SelectMatchingCard(tp,c100259017.disfilter,tp,LOCATION_GRAVE,0,1,1,nil)
+	Duel.Remove(g,POS_FACEUP,REASON_COST)
+end
+function c100259017.distg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
+	if re:GetHandler():IsDestructable() and re:GetHandler():IsRelateToEffect(re) then
+		Duel.SetOperationInfo(0,CATEGORY_DESTROY,eg,1,0,0)
+	end
+end
+function c100259017.disop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.NegateActivation(ev) and re:GetHandler():IsRelateToEffect(re) then
+		Duel.Destroy(eg,REASON_EFFECT)
+	end
+end
+function c100259017.atkcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetAttacker():IsControler(1-tp)
+end
+function c100259017.atkop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.NegateAttack()
+end
+function c100259017.spfilter(c,tp)
+	return c:IsType(TYPE_SYNCHRO) and Duel.GetMZoneCount(tp,c)>0
+end
+function c100259017.spcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetTurnPlayer()==1-tp
+end
+function c100259017.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.CheckReleaseGroup(tp,c100259017.spfilter,2,nil,tp) end
+	local g=Duel.SelectReleaseGroup(tp,c100259017.spfilter,2,2,nil,tp)
+	Duel.Release(g,REASON_COST)
+end
+function c100259017.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
+end
+function c100259017.spop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) then
+		Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
