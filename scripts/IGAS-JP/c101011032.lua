21c18bfc46e4179d0dab9ed872e3627734182608
mallu11 <54900811+mallu11@users.noreply.github.com>
2019-10-29 01:21:33 +0800

    fix バトル・サバイバー

--- scripts/IGAS-JP/c101011032.lua
+++ scripts/IGAS-JP/c101011032.lua
@@ -21,7 +21,28 @@ function c101011032.initial_effect(c)
 	e2:SetCondition(c101011032.regcon)
 	e2:SetOperation(c101011032.regop)
 	c:RegisterEffect(e2)
-	e1:SetLabelObject(e2)
+	e2:SetLabelObject(e1)
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e3:SetCode(EVENT_CHANGE_POS)
+	e3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e3:SetCondition(c101011032.fdcon)
+	e3:SetOperation(c101011032.fdop)
+	c:RegisterEffect(e3)
+	e3:SetLabelObject(e1)
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e4:SetCode(EVENT_LEAVE_FIELD)
+	e4:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e4:SetOperation(c101011032.fdop)
+	c:RegisterEffect(e4)
+	e4:SetLabelObject(e1)
+end
+function c101011032.fdcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsPosition(POS_FACEDOWN) and e:GetHandler():IsLocation(LOCATION_MZONE)
+end
+function c101011032.fdop(e,tp,eg,ep,ev,re,r,rp)
+	e:GetLabelObject():SetLabelObject(nil)
 end
 function c101011032.regcon(e,tp,eg,ep,ev,re,r,rp)
 	return Duel.GetCurrentPhase()>=PHASE_BATTLE_START and Duel.GetCurrentPhase()<=PHASE_BATTLE
@@ -31,13 +52,13 @@ function c101011032.regop(e,tp,eg,ep,ev,re,r,rp)
 		local g=eg:Filter(Card.IsControler,nil,tp)
 		local ng=Group.CreateGroup()
 		ng:KeepAlive()
-		ng=e:GetLabelObject()
+		ng=e:e:GetLabelObject():GetLabelObject()
 		if ng then
 			ng:Merge(g)
-			e:SetLabelObject(ng)
+			e:GetLabelObject():SetLabelObject(ng)
 		else
 			g:KeepAlive()
-			e:SetLabelObject(g)
+			e:GetLabelObject():SetLabelObject(g)
 		end
 		local tc=g:GetFirst()
 		while tc do
@@ -50,12 +71,12 @@ function c101011032.thfilter(c)
 	return c:GetFlagEffect(101011032)~=0 and c:IsAbleToHand() and not c:IsCode(101011032)
 end
 function c101011032.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	local ng=e:GetLabelObject():GetLabelObject()
+	local ng=e:GetLabelObject()
 	if chk==0 then return ng and ng:IsExists(c101011032.thfilter,1,nil) end
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_GRAVE)
 end
 function c101011032.thop(e,tp,eg,ep,ev,re,r,rp)
-	local ng=e:GetLabelObject():GetLabelObject()
+	local ng=e:GetLabelObject()
 	if not ng then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
 	local g=ng:FilterSelect(tp,aux.NecroValleyFilter(c101011032.thfilter),1,1,nil)
