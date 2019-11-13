d7411d2bd79f4de65039ef47cb84cdf7ca781df8
mallu11 <54900811+mallu11@users.noreply.github.com>
2019-11-14 01:37:33 +0800

    add 幻奏の華樂聖ブルーム・ハーモニスト

---
+++ scripts/LVP3-JP/c100257066.lua
@@ -0,0 +1,89 @@
+--幻奏の華樂聖ブルーム・ハーモニスト
+
+--Scripted by mallu11
+function c100257066.initial_effect(c)
+	--link summon
+	c:EnableReviveLimit()
+	aux.AddLinkProcedure(c,aux.FilterBoolFunction(Card.IsLinkRace,RACE_FAIRY),2,2)
+	--spsummon
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(100257066,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetProperty(EFFECT_FLAG_DELAY)
+	e1:SetCountLimit(1,100257066)
+	e1:SetCondition(c100257066.spcon)
+	e1:SetCost(c100257066.spcost)
+	e1:SetTarget(c100257066.sptg)
+	e1:SetOperation(c100257066.spop)
+	c:RegisterEffect(e1)
+	--actlimit
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e2:SetCode(EFFECT_CANNOT_ACTIVATE)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetTargetRange(0,1)
+	e2:SetCondition(c100257066.actcon)
+	e2:SetValue(1)
+	c:RegisterEffect(e2)
+	Duel.AddCustomActivityCounter(100257066,ACTIVITY_SPSUMMON,c100257066.counterfilter)
+end
+function c100257066.cfilter(c)
+	return c:IsFaceup() and c:IsSetCard(0x9b)
+end
+function c100257066.actcon(e)
+	local a=Duel.GetAttacker()
+	return a and c100257066.cfilter(a) and e:GetHandler():GetLinkedGroup():IsContains(a)
+end
+function c100257066.counterfilter(c)
+	return c:IsSetCard(0x9b)
+end
+function c100257066.spcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsSummonType(SUMMON_TYPE_LINK)
+end
+function c100257066.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetCustomActivityCount(100257066,tp,ACTIVITY_SPSUMMON)==0
+		and Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,1,nil) end
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_OATH)
+	e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+	e1:SetReset(RESET_PHASE+PHASE_END)
+	e1:SetTargetRange(1,0)
+	e1:SetTarget(c100257066.splimit)
+	Duel.RegisterEffect(e1,tp)
+	Duel.DiscardHand(tp,Card.IsDiscardable,1,1,REASON_COST+REASON_DISCARD)
+end
+function c100257066.splimit(e,c)
+	return not c:IsSetCard(0x9b)
+end
+function c100257066.spfilter(c,e,tp,zone)
+	return c:IsSetCard(0x9b) and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP_DEFENSE,tp,zone)
+end
+function c100257066.fselect(g)
+	if g:GetClassCount(Card.GetLevel)>=2 then
+		Duel.SetSelectedCard(g)
+		return true
+	else return false end
+end
+function c100257066.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local zone=bit.band(e:GetHandler():GetLinkedZone(tp),0x1f)
+	local ct=Duel.GetLocationCount(tp,LOCATION_MZONE,tp,LOCATION_REASON_TOFIELD,zone)
+	local g=Duel.GetMatchingGroup(c100257066.spfilter,tp,LOCATION_DECK,0,nil,e,tp,zone)
+	if chk==0 then return not Duel.IsPlayerAffectedByEffect(tp,59822133) and ct>1 and g:GetClassCount(Card.GetLevel)>=2 end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,2,tp,LOCATION_DECK)
+end
+function c100257066.spop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local zone=bit.band(c:GetLinkedZone(tp),0x1f)
+	local ct=Duel.GetLocationCount(tp,LOCATION_MZONE,tp,LOCATION_REASON_TOFIELD,zone)
+	local g=Duel.GetMatchingGroup(c100257066.spfilter,tp,LOCATION_DECK,0,nil,e,tp,zone)
+	if c:IsRelateToEffect(e) and not Duel.IsPlayerAffectedByEffect(tp,59822133) and ct>1 and g:GetClassCount(Card.GetLevel)>=2 then
+		local sg=g:SelectSubGroup(tp,c100257066.fselect,false,2,2)
+		if sg:GetCount()==2 then
+			Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP_DEFENSE,zone)
+		end
+	end
+end
