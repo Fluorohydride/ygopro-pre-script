bd0e1b631eedccba1d28500a32fdd2ddff1f8064
mallu11 <54900811+mallu11@users.noreply.github.com>
2019-11-17 21:45:01 +0800

    add LGB1 (#336)
    * add FNo.0 未来龍皇ホープ
    
    * add 覇王黒竜オッドアイズ・リベリオン・ドラゴン−オーバーロード
    
    * add デコード・トーカー・ヒートソウル

---
+++ scripts/LGB1-JP/c100259033.lua
@@ -0,0 +1,131 @@
+--覇王黒竜オッドアイズ・リベリオン・ドラゴン−オーバーロード
+
+--Scripted by mallu11
+function c100259033.initial_effect(c)
+	c:SetSPSummonOnce(100259033)
+	--xyz summon
+	aux.AddXyzProcedure(c,nil,7,2,c100259033.ovfilter,aux.Stringid(100259033,0))
+	c:EnableReviveLimit()
+	--pendulum summon
+	aux.EnablePendulumAttribute(c,false)
+	--spsummon
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(100259033,1))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_PZONE)
+	e1:SetCountLimit(1)
+	e1:SetTarget(c100259033.sptg)
+	e1:SetOperation(c100259033.spop)
+	c:RegisterEffect(e1)
+	--summon success
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e2:SetCondition(c100259033.regcon)
+	e2:SetOperation(c100259033.regop)
+	c:RegisterEffect(e2)
+	--material check
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_SINGLE)
+	e3:SetCode(EFFECT_MATERIAL_CHECK)
+	e3:SetValue(c100259033.valcheck)
+	e3:SetLabelObject(e2)
+	c:RegisterEffect(e3)
+	--extra attack
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_SINGLE)
+	e4:SetCode(EFFECT_EXTRA_ATTACK)
+	e4:SetValue(2)
+	e4:SetCondition(c100259033.effcon)
+	c:RegisterEffect(e4)
+	--pendulum
+	local e5=Effect.CreateEffect(c)
+	e5:SetDescription(aux.Stringid(100259033,2))
+	e5:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e5:SetCode(EVENT_DESTROYED)
+	e5:SetProperty(EFFECT_FLAG_DELAY)
+	e5:SetCondition(c100259033.pencon)
+	e5:SetTarget(c100259033.pentg)
+	e5:SetOperation(c100259033.penop)
+	c:RegisterEffect(e5)
+end
+c100259033.pendulum_level=7
+function c100259033.ovfilter(c)
+	return c:IsFaceup() and c:IsType(TYPE_XYZ) and c:IsSetCard(0x23b)
+end
+function c100259033.spfilter(c,e,tp,mc)
+	return (c:IsSetCard(0x23b) or c:IsSetCard(0x10db)) and mc:IsCanBeXyzMaterial(c)
+		and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_XYZ,tp,false,false) and not c:IsCode(100259033)
+end
+function c100259033.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return Duel.IsPlayerCanSpecialSummonCount(tp,2) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.GetLocationCountFromEx(tp)>0 and aux.MustMaterialCheck(nil,tp,EFFECT_MUST_BE_XMATERIAL)
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+		and Duel.IsExistingMatchingCard(c100259033.spfilter,tp,LOCATION_EXTRA,0,1,nil,e,tp,c) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,0,0)
+end
+function c100259033.spop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local mg=Group.FromCards(c)
+	if c:IsRelateToEffect(e) and Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)~=0 and Duel.GetLocationCountFromEx(tp)>0 then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+		local g=Duel.SelectMatchingCard(tp,c100259033.spfilter,tp,LOCATION_EXTRA,0,1,1,nil,e,tp,c)
+		local tc=g:GetFirst()
+		if tc then
+			Duel.BreakEffect()
+			tc:SetMaterial(mg)
+			Duel.Overlay(tc,mg)
+			if Duel.SpecialSummon(tc,SUMMON_TYPE_XYZ,tp,tp,false,false,POS_FACEUP)~=0 then
+				tc:CompleteProcedure()
+				if Duel.IsExistingMatchingCard(aux.TRUE,tp,LOCATION_PZONE,0,1,nil)
+					and Duel.SelectYesNo(tp,aux.Stringid(100259033,3)) then
+					Duel.BreakEffect()
+					local sg=Duel.SelectMatchingCard(tp,aux.TRUE,tp,LOCATION_PZONE,0,1,1,nil)
+					Duel.HintSelection(sg)
+					if not tc:IsImmuneToEffect(e) then
+						Duel.Overlay(tc,sg)
+					end
+				end
+			end
+		end
+	end
+end
+function c100259033.mfilter(c)
+	return c:IsType(TYPE_XYZ) and c:GetRank()==7
+end
+function c100259033.valcheck(e,c)
+	local g=c:GetMaterial()
+	if g:IsExists(c100259033.mfilter,1,nil) then
+		e:GetLabelObject():SetLabel(1)
+	else
+		e:GetLabelObject():SetLabel(0)
+	end
+end
+function c100259033.regcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsSummonType(SUMMON_TYPE_XYZ) and e:GetLabel()==1
+end
+function c100259033.regop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	c:RegisterFlagEffect(100259033,RESET_EVENT+RESETS_STANDARD,0,1)
+	c:RegisterFlagEffect(0,RESET_EVENT+RESETS_STANDARD,EFFECT_FLAG_CLIENT_HINT,1,0,aux.Stringid(100259033,4))
+end
+function c100259033.effcon(e)
+	return e:GetHandler():GetFlagEffect(100259033)>0
+end
+function c100259033.pencon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return c:IsPreviousLocation(LOCATION_MZONE) and c:IsFaceup()
+end
+function c100259033.pentg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.CheckLocation(tp,LOCATION_PZONE,0) or Duel.CheckLocation(tp,LOCATION_PZONE,1) end
+end
+function c100259033.penop(e,tp,eg,ep,ev,re,r,rp)
+	if not Duel.CheckLocation(tp,LOCATION_PZONE,0) and not Duel.CheckLocation(tp,LOCATION_PZONE,1) then return false end
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) then
+		Duel.MoveToField(c,tp,tp,LOCATION_SZONE,POS_FACEUP,true)
+	end
+end
