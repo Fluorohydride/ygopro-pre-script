98b79d41ff6d39c0d5f58e216aaef5d3fa015bb2
mallu11 <54900811+mallu11@users.noreply.github.com>
2019-11-17 21:45:59 +0800

    add 20PP-JP (#340)

---
+++ scripts/20PP-JP/c100229001.lua
@@ -0,0 +1,113 @@
+--钢鋼鉄の魔導騎士－ギルティギア・フリード
+
+--Scripted by mallu11
+function c100229001.initial_effect(c)
+	--fusion summon
+	c:EnableReviveLimit()
+	aux.AddFusionProcFunRep(c,c100229001.ffilter,2,true)
+	--disable
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(100229001,0))
+	e1:SetCategory(CATEGORY_DISABLE+CATEGORY_DESTROY)
+	e1:SetType(EFFECT_TYPE_QUICK_O)
+	e1:SetCode(EVENT_CHAINING)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetCondition(c100229001.discon)
+	e1:SetTarget(c100229001.distg)
+	e1:SetOperation(c100229001.disop)
+	c:RegisterEffect(e1)
+	--mat check
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetCode(EFFECT_MATERIAL_CHECK)
+	e2:SetValue(c100229001.matcheck)
+	c:RegisterEffect(e2)
+	--extra attack
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_SINGLE)
+	e3:SetCode(EFFECT_EXTRA_ATTACK)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetValue(1)
+	e3:SetLabelObject(e2)
+	e3:SetCondition(c100229001.xacon)
+	c:RegisterEffect(e3)
+	--atk up
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(100229001,1))
+	e4:SetCategory(CATEGORY_ATKCHANGE)
+	e4:SetType(EFFECT_TYPE_QUICK_O)
+	e4:SetCode(EVENT_PRE_DAMAGE_CALCULATE)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetCountLimit(1)
+	e4:SetCondition(c100229001.atkcon)
+	e4:SetCost(c100229001.atkcost)
+	e4:SetOperation(c100229001.atkop)
+	c:RegisterEffect(e4)
+end
+function c100229001.ffilter(c,fc,sub,mg,sg)
+	return c:IsRace(RACE_WARRIOR) and (not sg or not sg:IsExists(Card.IsFusionAttribute,1,c,c:GetFusionAttribute()))
+end
+function c100229001.discon(e,tp,eg,ep,ev,re,r,rp)
+	if e:GetHandler():IsStatus(STATUS_BATTLE_DESTROYED) then return false end
+	if not re:IsHasProperty(EFFECT_FLAG_CARD_TARGET) then return false end
+	local tg=Duel.GetChainInfo(ev,CHAININFO_TARGET_CARDS)
+	return tg and tg:IsContains(e:GetHandler()) and Duel.IsChainDisablable(ev)
+end
+function c100229001.distg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_DISABLE,eg,1,0,0)
+	local g=Duel.GetMatchingGroup(aux.TRUE,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,nil)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
+end
+function c100229001.disop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.NegateEffect(ev) then
+		local g=Duel.SelectMatchingCard(tp,aux.TRUE,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil)
+		if g:GetCount()>0 then
+			Duel.Destroy(g,REASON_EFFECT)
+		end
+	end
+end
+function c100229001.matcheck(e,c)
+	local g=c:GetMaterial()
+	local res=true
+	local tc=g:GetFirst()
+	while tc do
+		res=res and tc:IsLocation(LOCATION_MZONE)
+		tc=g:GetNext()
+	end
+	if res then
+		e:SetLabel(1)
+	else
+		e:SetLabel(0)
+	end
+end
+function c100229001.xacon(e)
+	local flag=e:GetLabelObject():GetLabel()
+	return e:GetHandler():IsSummonType(SUMMON_TYPE_FUSION) and flag==1
+end
+function c100229001.atkcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local bc=c:GetBattleTarget()
+	return bc and bc:IsControler(1-tp) and c:GetDefense()>0
+end
+function c100229001.atkfilter(c)
+	return c:IsType(TYPE_SPELL) and c:IsAbleToRemoveAsCost()
+end
+function c100229001.atkcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c100229001.atkfilter,tp,LOCATION_GRAVE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local g=Duel.SelectMatchingCard(tp,c100229001.atkfilter,tp,LOCATION_GRAVE,0,1,1,nil)
+	Duel.Remove(g,POS_FACEUP,REASON_COST)
+end
+function c100229001.atkop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToBattle() and c:IsFaceup() then
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_DISABLE+RESET_PHASE+PHASE_END)
+		e1:SetValue(c:GetDefense()/2)
+		c:RegisterEffect(e1)
+	end
+end
