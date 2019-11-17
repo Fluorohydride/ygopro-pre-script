3a36a3c099f57dda45f6327ccb9ad033a88d9fcf
mercury233 <me@mercury233.me>
2019-11-17 21:50:00 +0800

    rename

--- scripts/20PP-JP/c100260003.lua
+++ scripts/20PP-JP/c100260003.lua
@@ -1,32 +1,32 @@
 --オルターガイスト・メモリーガント
 
 --Scripted by mallu11
-function c100229003.initial_effect(c)
+function c100260003.initial_effect(c)
 	--link summon
 	c:EnableReviveLimit()
 	aux.AddLinkProcedure(c,aux.FilterBoolFunction(Card.IsLinkSetCard,0x103),2,4)
 	--atk up
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(100229003,0))
+	e1:SetDescription(aux.Stringid(100260003,0))
 	e1:SetCategory(CATEGORY_ATKCHANGE)
 	e1:SetType(EFFECT_TYPE_QUICK_O)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetRange(LOCATION_MZONE)
-	e1:SetCountLimit(1,100229003)
-	e1:SetCondition(c100229003.atkcon)
-	e1:SetCost(c100229003.atkcost)
-	e1:SetOperation(c100229003.atkop)
+	e1:SetCountLimit(1,100260003)
+	e1:SetCondition(c100260003.atkcon)
+	e1:SetCost(c100260003.atkcost)
+	e1:SetOperation(c100260003.atkop)
 	c:RegisterEffect(e1)
 	--destroy
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(100229003,1))
+	e2:SetDescription(aux.Stringid(100260003,1))
 	e2:SetCategory(CATEGORY_DESTROY)
 	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e2:SetCode(EVENT_BATTLE_DESTROYING)
-	e2:SetCountLimit(1,100229103)
+	e2:SetCountLimit(1,100260103)
 	e2:SetCondition(aux.bdcon)
-	e2:SetTarget(c100229003.destg)
-	e2:SetOperation(c100229003.desop)
+	e2:SetTarget(c100260003.destg)
+	e2:SetOperation(c100260003.desop)
 	c:RegisterEffect(e2)
 	--destroy replace
 	local e3=Effect.CreateEffect(c)
@@ -34,15 +34,15 @@ function c100229003.initial_effect(c)
 	e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
 	e3:SetCode(EFFECT_DESTROY_REPLACE)
 	e3:SetRange(LOCATION_MZONE)
-	e3:SetCountLimit(1,100229203)
-	e3:SetTarget(c100229003.desreptg)
-	e3:SetOperation(c100229003.desrepop)
+	e3:SetCountLimit(1,100260203)
+	e3:SetTarget(c100260003.desreptg)
+	e3:SetOperation(c100260003.desrepop)
 	c:RegisterEffect(e3)
 end
-function c100229003.atkcon(e,tp,eg,ep,ev,re,r,rp)
+function c100260003.atkcon(e,tp,eg,ep,ev,re,r,rp)
 	return Duel.GetCurrentPhase()>=PHASE_BATTLE_START and Duel.GetCurrentPhase()<=PHASE_BATTLE and (Duel.GetCurrentPhase()~=PHASE_DAMAGE or not Duel.IsDamageCalculated())
 end
-function c100229003.atkcost(e,tp,eg,ep,ev,re,r,rp,chk)
+function c100260003.atkcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
 	if chk==0 then return Duel.CheckReleaseGroup(tp,Card.IsAttackAbove,1,c,1) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
@@ -50,7 +50,7 @@ function c100229003.atkcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.Release(g,REASON_COST)
 	e:SetLabel(g:GetFirst():GetPreviousAttackOnField())
 end
-function c100229003.atkop(e,tp,eg,ep,ev,re,r,rp)
+function c100260003.atkop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if c:IsFaceup() and c:IsRelateToEffect(e) then
 		local e1=Effect.CreateEffect(c)
@@ -61,12 +61,12 @@ function c100229003.atkop(e,tp,eg,ep,ev,re,r,rp)
 		c:RegisterEffect(e1)
 	end
 end
-function c100229003.destg(e,tp,eg,ep,ev,re,r,rp,chk)
+function c100260003.destg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(aux.TRUE,tp,0,LOCATION_MZONE,1,nil) end
 	local g=Duel.GetMatchingGroup(aux.TRUE,tp,0,LOCATION_MZONE,nil)
 	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
 end
-function c100229003.desop(e,tp,eg,ep,ev,re,r,rp)
+function c100260003.desop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
 	local g=Duel.SelectMatchingCard(tp,aux.TRUE,tp,0,LOCATION_MZONE,1,1,nil)
@@ -77,22 +77,22 @@ function c100229003.desop(e,tp,eg,ep,ev,re,r,rp)
 		end
 	end
 end
-function c100229003.repfilter(c)
+function c100260003.repfilter(c)
 	return c:IsType(TYPE_MONSTER) and c:IsAbleToRemove()
 end
-function c100229003.desreptg(e,tp,eg,ep,ev,re,r,rp,chk)
+function c100260003.desreptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
 	if chk==0 then
-		return not c:IsReason(REASON_REPLACE) and Duel.IsExistingMatchingCard(c100229003.repfilter,tp,LOCATION_GRAVE,0,1,nil)
+		return not c:IsReason(REASON_REPLACE) and Duel.IsExistingMatchingCard(c100260003.repfilter,tp,LOCATION_GRAVE,0,1,nil)
 	end
 	if Duel.SelectEffectYesNo(tp,c,96) then
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESREPLACE)
-		local g=Duel.SelectMatchingCard(tp,c100229003.repfilter,tp,LOCATION_GRAVE,0,1,1,nil)
+		local g=Duel.SelectMatchingCard(tp,c100260003.repfilter,tp,LOCATION_GRAVE,0,1,1,nil)
 		e:SetLabelObject(g:GetFirst())
 		return true
 	else return false end
 end
-function c100229003.desrepop(e,tp,eg,ep,ev,re,r,rp)
+function c100260003.desrepop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=e:GetLabelObject()
 	Duel.Remove(tc,POS_FACEUP,REASON_EFFECT+REASON_REPLACE)
 end
--- scripts/20PP-JP/c100260011.lua
+++ scripts/20PP-JP/c100260011.lua
@@ -1,7 +1,7 @@
 --白薔薇の回廊
 
 --Scripted by mallu11
-function c100229011.initial_effect(c)
+function c100260011.initial_effect(c)
 	--activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
@@ -9,55 +9,55 @@ function c100229011.initial_effect(c)
 	c:RegisterEffect(e1)
 	--spsummon
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(100229011,0))
+	e2:SetDescription(aux.Stringid(100260011,0))
 	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e2:SetType(EFFECT_TYPE_IGNITION)
 	e2:SetRange(LOCATION_FZONE)
-	e2:SetCountLimit(1,100229011)
-	e2:SetCondition(c100229011.spcon)
-	e2:SetTarget(c100229011.sptg)
-	e2:SetOperation(c100229011.spop)
+	e2:SetCountLimit(1,100260011)
+	e2:SetCondition(c100260011.spcon)
+	e2:SetTarget(c100260011.sptg)
+	e2:SetOperation(c100260011.spop)
 	c:RegisterEffect(e2)
 	--confirm deck
 	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(100229011,1))
+	e3:SetDescription(aux.Stringid(100260011,1))
 	e3:SetCategory(CATEGORY_ATKCHANGE)
 	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
 	e3:SetCode(EVENT_PREDRAW)
 	e3:SetRange(LOCATION_FZONE)
-	e3:SetCondition(c100229011.cfcon)
-	e3:SetTarget(c100229011.cftg)
-	e3:SetOperation(c100229011.cfop)
+	e3:SetCondition(c100260011.cfcon)
+	e3:SetTarget(c100260011.cftg)
+	e3:SetOperation(c100260011.cfop)
 	c:RegisterEffect(e3)
 end
-function c100229011.spcon(e,tp,eg,ep,ev,re,r,rp)
+function c100260011.spcon(e,tp,eg,ep,ev,re,r,rp)
 	return Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0)==0
 end
-function c100229011.spfilter(c,e,tp)
+function c100260011.spfilter(c,e,tp)
 	return (c:IsSetCard(0x1123) or c:IsRace(RACE_PLANT)) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
-function c100229011.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+function c100260011.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingMatchingCard(c100229011.spfilter,tp,LOCATION_HAND,0,1,nil,e,tp) end
+		and Duel.IsExistingMatchingCard(c100260011.spfilter,tp,LOCATION_HAND,0,1,nil,e,tp) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
 end
-function c100229011.spop(e,tp,eg,ep,ev,re,r,rp)
+function c100260011.spop(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectMatchingCard(tp,c100229011.spfilter,tp,LOCATION_HAND,0,1,1,nil,e,tp)
+	local g=Duel.SelectMatchingCard(tp,c100260011.spfilter,tp,LOCATION_HAND,0,1,1,nil,e,tp)
 	if g:GetCount()>0 then
 		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
 	end
 end
-function c100229011.cfcon(e,tp,eg,ep,ev,re,r,rp)
+function c100260011.cfcon(e,tp,eg,ep,ev,re,r,rp)
 	return Duel.GetTurnPlayer()==tp and Duel.GetFieldGroupCount(tp,LOCATION_DECK,0)>0
 end
-function c100229011.cftg(e,tp,eg,ep,ev,re,r,rp,chk)
+function c100260011.cftg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CARDTYPE)
 	e:SetLabel(Duel.AnnounceType(tp))
 end
-function c100229011.cfop(e,tp,eg,ep,ev,re,r,rp)
+function c100260011.cfop(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetFieldGroupCount(tp,LOCATION_DECK,0)<=0 then return end
 	Duel.ConfirmDecktop(tp,1)
 	local g=Duel.GetDecktopGroup(tp,1)
@@ -70,10 +70,10 @@ function c100229011.cfop(e,tp,eg,ep,ev,re,r,rp)
 		e1:SetReset(RESET_PHASE+PHASE_END)
 		e1:SetValue(1000)
 		e1:SetTargetRange(LOCATION_MZONE,0)
-		e1:SetTarget(c100229011.atktg)
+		e1:SetTarget(c100260011.atktg)
 		Duel.RegisterEffect(e1,tp)
 	end
 end
-function c100229011.atktg(e,c)
+function c100260011.atktg(e,c)
 	return c:IsFaceup() and c:IsLevelAbove(7) and c:IsType(TYPE_SYNCHRO)
 end
