ab6b5bd05cffddb6b985abb6945496ba0d0bf4d7
mallu11 <54900811+mallu11@users.noreply.github.com>
2019-11-18 10:10:34 +0800

    fix LVP3 and LGB1 (#341)
    * fix 虚光の宣告者
    
    * fix 魔界劇団－ハイパー・ディレクター
    
    * fix FNo.0 未来龍皇ホープ

--- scripts/LVP3-JP/c100257021.lua
+++ scripts/LVP3-JP/c100257021.lua
@@ -66,25 +66,15 @@ end
 function c100257021.thfilter(c)
 	return c:IsType(TYPE_RITUAL) and c:IsAbleToHand()
 end
-function c100257021.fselect(g)
-	if g:GetClassCount(Card.GetLevel)==g:GetCount() then
-		Duel.SetSelectedCard(g)
-		return true
-	else return false end
-end
 function c100257021.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	local g=Duel.GetMatchingGroup(c100257021.thfilter,tp,LOCATION_GRAVE,0,nil)
-	if chk==0 then return g:GetClassCount(Card.GetCode)>=2 end
-	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,2,tp,LOCATION_GRAVE)
+	if chk==0 then return Duel.IsExistingMatchingCard(c100257021.thfilter,tp,LOCATION_GRAVE,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_GRAVE)
 end
 function c100257021.thop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
 	local g=Duel.GetMatchingGroup(aux.NecroValleyFilter(c100257021.thfilter),tp,LOCATION_GRAVE,0,nil)
-	if g:GetClassCount(Card.GetLevel)>=2 then
-		local sg=g:SelectSubGroup(tp,c100257021.fselect,false,2,2)
-		if g:GetCount()==2 then
-			Duel.SendtoHand(g,nil,REASON_EFFECT)
-			Duel.ConfirmCards(1-tp,g)
-		end
+	local sg=g:SelectSubGroup(tp,aux.dncheck,false,1,2)
+	if sg then
+		Duel.SendtoHand(sg,nil,REASON_EFFECT)
 	end
 end
