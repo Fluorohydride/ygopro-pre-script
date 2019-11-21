a01a8d7d86b62653da25e6fab297d0ad7b39ebba
nekrozar <nekrozar.s@gmail.com>
2019-11-21 09:31:38 +0900

    add script LVP3

---
+++ scripts/LVP3-JP/c100257056.lua
@@ -0,0 +1,138 @@
+--武神姫－アハシマ
+
+--Scripted by nekrozar
+function c100257056.initial_effect(c)
+	Duel.EnableGlobalFlag(GLOBALFLAG_DETACH_EVENT)
+	--link summon
+	aux.AddLinkProcedure(c,c100257056.mfilter,2,2,c100257056.lcheck)
+	c:EnableReviveLimit()
+	--cannot link material
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e1:SetCode(EFFECT_CANNOT_BE_LINK_MATERIAL)
+	e1:SetValue(1)
+	c:RegisterEffect(e1)
+	--xyz summon
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(100257056,0))
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetProperty(EFFECT_FLAG_DELAY)
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e2:SetCountLimit(1,100257056)
+	e2:SetCondition(c100257056.spcon)
+	e2:SetTarget(c100257056.sptg)
+	e2:SetOperation(c100257056.spop)
+	c:RegisterEffect(e2)
+	--destroy
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
+	e3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e3:SetCode(EVENT_CHAINING)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetOperation(aux.chainreg)
+	c:RegisterEffect(e3)
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(100257056,1))
+	e4:SetCategory(CATEGORY_DESTROY)
+	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e4:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
+	e4:SetCode(EVENT_CHAIN_SOLVING)
+	e4:SetCountLimit(1,100257156)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetCondition(c100257056.descon)
+	e4:SetTarget(c100257056.destg)
+	e4:SetOperation(c100257056.desop)
+	c:RegisterEffect(e4)
+	if not c100257056.global_check then
+		c100257056.global_check=true
+		c100257056[0]=nil
+		local ge1=Effect.CreateEffect(c)
+		ge1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		ge1:SetCode(EVENT_DETACH_MATERIAL)
+		ge1:SetOperation(c100257056.checkop)
+		Duel.RegisterEffect(ge1,0)
+	end
+end
+function c100257056.checkop(e,tp,eg,ep,ev,re,r,rp)
+	local cid=Duel.GetCurrentChain()
+	if cid>0 then
+		c100257056[0]=Duel.GetChainInfo(cid,CHAININFO_CHAIN_ID)
+	end
+end
+function c100257056.mfilter(c)
+	return c:IsLevelAbove(0)
+end
+function c100257056.lcheck(g,lc)
+	return g:GetClassCount(Card.GetLevel)==1
+end
+function c100257056.spcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsSummonType(SUMMON_TYPE_LINK)
+end
+function c100257056.spfilter(c,e,tp)
+	return c:IsLevelAbove(0) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function c100257056.fselect(g,tp)
+	return g:GetClassCount(Card.GetLocation)==g:GetCount()
+		and Duel.IsExistingMatchingCard(Card.IsXyzSummonable,tp,LOCATION_EXTRA,0,1,nil,g,2,2)
+end
+function c100257056.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local g=Duel.GetMatchingGroup(c100257056.spfilter,tp,LOCATION_HAND+LOCATION_GRAVE,0,nil,e,tp)
+	if chk==0 then return Duel.IsPlayerCanSpecialSummonCount(tp,2)
+		and not Duel.IsPlayerAffectedByEffect(tp,59822133)
+		and Duel.GetLocationCount(tp,LOCATION_MZONE)>1
+		and Duel.GetLocationCountFromEx(tp)>0
+		and g:CheckSubGroup(c100257056.fselect,2,2,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,2,tp,LOCATION_HAND+LOCATION_GRAVE)
+end
+function c100257056.spop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(aux.NecroValleyFilter(c100257056.spfilter),tp,LOCATION_HAND+LOCATION_GRAVE,0,nil,e,tp)
+	if Duel.IsPlayerAffectedByEffect(tp,59822133) or Duel.GetLocationCount(tp,LOCATION_MZONE)<=1 or g:GetCount()==0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local sg=g:SelectSubGroup(tp,c100257056.fselect,false,2,2)
+	if sg and sg:GetCount()==2 then
+		local tc1=sg:GetFirst()
+		local tc2=sg:GetNext()
+		Duel.SpecialSummonStep(tc1,0,tp,tp,false,false,POS_FACEUP)
+		Duel.SpecialSummonStep(tc2,0,tp,tp,false,false,POS_FACEUP)
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_DISABLE)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		tc1:RegisterEffect(e1)
+		local e2=e1:Clone()
+		tc2:RegisterEffect(e2)
+		local e3=Effect.CreateEffect(e:GetHandler())
+		e3:SetType(EFFECT_TYPE_SINGLE)
+		e3:SetCode(EFFECT_DISABLE_EFFECT)
+		e3:SetReset(RESET_EVENT+RESETS_STANDARD)
+		tc1:RegisterEffect(e3)
+		local e4=e3:Clone()
+		tc2:RegisterEffect(e4)
+		Duel.SpecialSummonComplete()
+		if Duel.GetLocationCountFromEx(tp,tp,g)<=0 then return end
+		local xyzg=Duel.GetMatchingGroup(Card.IsXyzSummonable,tp,LOCATION_EXTRA,0,nil,g,2,2)
+		if xyzg:GetCount()>0 then
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+			local xyz=xyzg:Select(tp,1,1,nil):GetFirst()
+			Duel.XyzSummon(tp,xyz,g)
+		end
+	end
+end
+function c100257056.descon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetChainInfo(ev,CHAININFO_CHAIN_ID)==c100257056[0] and re:IsActiveType(TYPE_XYZ) and e:GetHandler():GetFlagEffect(1)>0
+end
+function c100257056.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsControler(1-tp) and chkc:IsOnField() and chkc:IsType(TYPE_SPELL+TYPE_TRAP) end
+	if chk==0 then return Duel.IsExistingTarget(Card.IsType,tp,0,LOCATION_ONFIELD,1,nil,TYPE_SPELL+TYPE_TRAP) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local g=Duel.SelectTarget(tp,Card.IsType,tp,0,LOCATION_ONFIELD,1,1,nil,TYPE_SPELL+TYPE_TRAP)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
+end
+function c100257056.desop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) then
+		Duel.Destroy(tc,REASON_EFFECT)
+	end
+end
