9d042614270ceefbe2654d7de0f31036d6736bcb
nekrozar <nekrozar.s@gmail.com>
2019-12-18 21:12:21 +0900

    add script ETCO

---
+++ scripts/ETCO-JP/c101012055.lua
@@ -0,0 +1,72 @@
+--清冽の水霊使いエリア
+
+--Scripted by nekrozar
+function c101012055.initial_effect(c)
+	--link summon
+	aux.AddLinkProcedure(c,nil,2,2,c101012055.lcheck)
+	c:EnableReviveLimit()
+	--special summon
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(101012055,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1,101012055)
+	e1:SetTarget(c101012055.sptg)
+	e1:SetOperation(c101012055.spop)
+	c:RegisterEffect(e1)
+	--tohand
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(101012055,1))
+	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetProperty(EFFECT_FLAG_DELAY)
+	e2:SetCode(EVENT_DESTROYED)
+	e2:SetCountLimit(1,101012155)
+	e2:SetCondition(c101012055.thcon)
+	e2:SetTarget(c101012055.thtg)
+	e2:SetOperation(c101012055.thop)
+	c:RegisterEffect(e2)
+end
+function c101012055.lcheck(g)
+	return g:IsExists(Card.IsLinkAttribute,1,nil,ATTRIBUTE_WATER)
+end
+function c101012055.spfilter(c,e,tp,zone)
+	return c:IsAttribute(ATTRIBUTE_WATER) and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP,tp,zone)
+end
+function c101012055.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	local zone=bit.band(e:GetHandler():GetLinkedZone(tp),0x1f)
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(1-tp) and c101012055.spfilter(chkc,e,tp,zone) end
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingTarget(c101012055.spfilter,tp,0,LOCATION_GRAVE,1,nil,e,tp,zone) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectTarget(tp,c101012055.spfilter,tp,0,LOCATION_GRAVE,1,1,nil,e,tp,zone)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
+end
+function c101012055.spop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	local zone=bit.band(e:GetHandler():GetLinkedZone(tp),0x1f)
+	if tc:IsRelateToEffect(e) and zone~=0 then
+		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP,zone)
+	end
+end
+function c101012055.thcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return c:IsReason(REASON_DESTROY) and (c:IsReason(REASON_BATTLE) or (c:IsReason(REASON_EFFECT) and c:GetReasonPlayer()==1-tp)) and c:IsPreviousLocation(LOCATION_MZONE) and c:IsSummonType(SUMMON_TYPE_LINK)
+end
+function c101012055.thfilter(c)
+	return c:IsDefenseBelow(1500) and c:IsAttribute(ATTRIBUTE_WATER) and c:IsAbleToHand()
+end
+function c101012055.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c101012055.thfilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function c101012055.thop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,c101012055.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if g:GetCount()>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end
