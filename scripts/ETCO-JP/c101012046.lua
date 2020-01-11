d68fea3ac1d3c1be5c1e8ec883a4509c419055de
mercury233 <me@mercury233.me>
2020-01-11 21:40:42 +0800

    crlf

--- scripts/ETCO-JP/c101012046.lua
+++ scripts/ETCO-JP/c101012046.lua
@@ -50,7 +50,7 @@ end
 function c101012046.atkop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local tc=Duel.GetFirstTarget()
-	if c:IsFaceup() and c:IsRelateToEffect(e) and tc:IsRelateToEffect(e)  then
+	if c:IsFaceup() and c:IsRelateToEffect(e) and tc:IsRelateToEffect(e) then
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_UPDATE_ATTACK)
