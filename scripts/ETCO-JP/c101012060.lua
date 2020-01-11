fdb025ff12f739d04d7c03165ed74e91240a441b
mallu11 <54900811+mallu11@users.noreply.github.com>
2020-01-11 14:16:47 +0800

    fix

--- scripts/ETCO-JP/c101012060.lua
+++ scripts/ETCO-JP/c101012060.lua
@@ -19,7 +19,8 @@ function c101012060.filter(c,e,tp)
 end
 function c101012060.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_GRAVE+LOCATION_REMOVED) and chkc:IsControler(tp) and c101012060.filter(chkc,e,tp) end
-	if chk==0 then return Duel.IsExistingTarget(c101012060.filter,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,1,nil,e,tp) end
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingTarget(c101012060.filter,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,1,nil,e,tp) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 	local g=Duel.SelectTarget(tp,c101012060.filter,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,1,1,nil,e,tp)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
