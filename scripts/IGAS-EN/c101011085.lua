26ea667d1c797c3a6ca7b03e9395cf944b8c9a5d
mallu11 <54900811+mallu11@users.noreply.github.com>
2020-01-17 17:42:58 +0800

    Update c101011085.lua

--- scripts/IGAS-EN/c101011085.lua
+++ scripts/IGAS-EN/c101011085.lua
@@ -59,7 +59,7 @@ function c101011085.spop1(e,tp,eg,ep,ev,re,r,rp)
 	local sg=Duel.SelectMatchingCard(tp,c101011085.spfilter1,tp,LOCATION_EXTRA,0,1,1,nil,e,tp,attr)
 	local sc=sg:GetFirst()
 	if sc and Duel.SpecialSummon(sc,0,tp,tp,false,false,POS_FACEUP)~=0 and sc:IsFaceup() and c:IsRelateToEffect(e) and c:IsControler(tp) then
-		if not Duel.Equip(tp,c,sc) then return end
+		if not Duel.Equip(tp,c,sc,false) then return end
 		--equip limit
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
