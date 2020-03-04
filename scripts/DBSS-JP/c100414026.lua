101c11e632d6beb601fe9cb52553afd636630d0b
mallu11 <54900811+mallu11@users.noreply.github.com>
2020-03-04 12:12:38 +0800

    fix 六花の薄氷

--- scripts/DBSS-JP/c100414026.lua
+++ scripts/DBSS-JP/c100414026.lua
@@ -14,42 +14,30 @@ function c100414026.initial_effect(c)
 	e1:SetTarget(c100414026.target)
 	e1:SetOperation(c100414026.activate)
 	c:RegisterEffect(e1)
+	--cost
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(100414026,1))
+	e2:SetCategory(CATEGORY_CONTROL)
+	e2:SetType(EFFECT_TYPE_ACTIVATE)
+	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetHintTiming(0,TIMINGS_CHECK_MONSTER+TIMING_END_PHASE)
+	e2:SetCountLimit(1,100414026+EFFECT_COUNT_CODE_OATH)
+	e2:SetCost(c100414026.cost)
+	e2:SetTarget(c100414026.target2)
+	e2:SetOperation(c100414026.activate2)
+	c:RegisterEffect(e2)
 end
 function c100414026.filter(c,check)
 	return c:IsFaceup() and (c:IsType(TYPE_EFFECT) or bit.band(c:GetOriginalType(),TYPE_EFFECT)==TYPE_EFFECT)
 		and (check or c:IsControlerCanBeChanged())
 end
-function c100414026.cost(e,tp,eg,ep,ev,re,r,rp,chk)
-	e:SetLabel(100)
-	return true
-end
 function c100414026.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	local b1=Duel.IsExistingTarget(c100414026.filter,1-tp,LOCATION_MZONE,0,1,nil,true)
-	local b2=e:IsHasType(EFFECT_TYPE_ACTIVATE) and Duel.IsExistingTarget(c100414026.filter,1-tp,LOCATION_MZONE,0,1,nil)
-		and Duel.CheckReleaseGroup(tp,Card.IsRace,1,nil,RACE_PLANT)
-	if chk==0 then
-		if e:GetLabel()~=100 then return false end
-		e:SetLabel(0)
-		return b1 or b2
-	end
-	if b2 and (not b1 or Duel.SelectOption(tp,aux.Stringid(100414026,1),aux.Stringid(100414026,2))==1) then
-		e:SetCategory(CATEGORY_CONTROL)
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
-		local g=Duel.SelectReleaseGroup(tp,Card.IsRace,1,1,nil,RACE_PLANT)
-		Duel.Release(g,REASON_COST)
-		Duel.SetTargetParam(1)
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
-		local g=Duel.SelectTarget(tp,c100414026.filter,1-tp,LOCATION_MZONE,0,1,1,nil)
-		Duel.SetOperationInfo(0,CATEGORY_CONTROL,nil,1,0,0)
-	else
-		e:SetCategory(0)
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
-		local g=Duel.SelectTarget(tp,c100414026.filter,1-tp,LOCATION_MZONE,0,1,1,nil,true)
-	end
-	e:SetLabel(0)
+	if chk==0 then return Duel.IsExistingTarget(c100414026.filter,1-tp,LOCATION_MZONE,0,1,nil,true) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+	Duel.SelectTarget(tp,c100414026.filter,1-tp,LOCATION_MZONE,0,1,1,nil,true)
 end
 function c100414026.activate(e,tp,eg,ep,ev,re,r,rp)
-	local param=Duel.GetChainInfo(0,CHAININFO_TARGET_PARAM)
 	local tc=Duel.GetFirstTarget()
 	if tc:IsFaceup() and tc:IsRelateToEffect(e) then
 		local e1=Effect.CreateEffect(e:GetHandler())
@@ -57,7 +45,29 @@ function c100414026.activate(e,tp,eg,ep,ev,re,r,rp)
 		e1:SetCode(EFFECT_CANNOT_TRIGGER)
 		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
 		tc:RegisterEffect(e1)
-		if param==1 and e:IsHasType(EFFECT_TYPE_ACTIVATE) and tc:IsControlerCanBeChanged() then
+	end
+end
+function c100414026.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.CheckReleaseGroup(tp,Card.IsRace,1,nil,RACE_PLANT) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
+	local g=Duel.SelectReleaseGroup(tp,Card.IsRace,1,1,nil,RACE_PLANT)
+	Duel.Release(g,REASON_COST)
+end
+function c100414026.target2(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:IsHasType(EFFECT_TYPE_ACTIVATE) and Duel.IsExistingTarget(c100414026.filter,1-tp,LOCATION_MZONE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+	local g=Duel.SelectTarget(tp,c100414026.filter,1-tp,LOCATION_MZONE,0,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_CONTROL,g,1,0,0)
+end
+function c100414026.activate2(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsFaceup() and tc:IsRelateToEffect(e) then
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_CANNOT_TRIGGER)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		tc:RegisterEffect(e1)
+		if e:IsHasType(EFFECT_TYPE_ACTIVATE) and tc:IsControlerCanBeChanged() then
 			Duel.BreakEffect()
 			if Duel.GetControl(tc,tp,PHASE_END,1)~=0 then
 				local e2=Effect.CreateEffect(e:GetHandler())
