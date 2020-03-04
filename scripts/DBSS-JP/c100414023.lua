cbb2d5232d6cfefe8f30817ac093b6879954c02b
mallu11 <54900811+mallu11@users.noreply.github.com>
2020-03-04 11:00:53 +0800

    fix 六花絢爛

--- scripts/DBSS-JP/c100414023.lua
+++ scripts/DBSS-JP/c100414023.lua
@@ -19,14 +19,16 @@ function c100414023.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	return true
 end
 function c100414023.thfilter(c,check)
-	return c:IsType(TYPE_MONSTER) and c:IsSetCard(0x245) and c:IsAbleToHand() and (check or Duel.IsExistingMatchingCard(c100414023.thfilter2,tp,LOCATION_DECK,0,1,c,c:GetCode(),c:GetOriginalLevel()))
+	return c:IsType(TYPE_MONSTER) and c:IsSetCard(0x245) and c:IsAbleToHand()
+		and (check or Duel.IsExistingMatchingCard(c100414023.thfilter2,tp,LOCATION_DECK,0,1,c,c:GetCode(),c:GetOriginalLevel()))
 end
 function c100414023.thfilter2(c,code,lv)
 	return c:IsRace(RACE_PLANT) and not c:IsCode(code) and c:GetOriginalLevel()==lv and c:IsAbleToHand()
 end
 function c100414023.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	local b1=Duel.IsExistingMatchingCard(c100414023.thfilter,tp,LOCATION_DECK,0,1,nil,true)
-	local b2=Duel.IsExistingMatchingCard(c100414023.thfilter,tp,LOCATION_DECK,0,1,nil) and Duel.CheckReleaseGroup(tp,Card.IsRace,1,nil,RACE_PLANT)
+	local b2=e:IsHasType(EFFECT_TYPE_ACTIVATE) and Duel.IsExistingMatchingCard(c100414023.thfilter,tp,LOCATION_DECK,0,1,nil)
+		and Duel.CheckReleaseGroup(tp,Card.IsRace,1,nil,RACE_PLANT)
 	if chk==0 then
 		if e:GetLabel()~=100 then return false end
 		e:SetLabel(0)
@@ -45,18 +47,13 @@ function c100414023.target(e,tp,eg,ep,ev,re,r,rp,chk)
 end
 function c100414023.activate(e,tp,eg,ep,ev,re,r,rp)
 	local param=Duel.GetChainInfo(0,CHAININFO_TARGET_PARAM)
-	local check=nil
-	if param==1 then
-		check=false
-	else
-		check=true
-	end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
 	local g=Duel.SelectMatchingCard(tp,c100414023.thfilter,tp,LOCATION_DECK,0,1,1,nil,true)
 	local tc=g:GetFirst()
 	if tc and Duel.SendtoHand(tc,nil,REASON_EFFECT)~=0 then
 		Duel.ConfirmCards(1-tp,tc)
-		if not check and tc:IsLocation(LOCATION_HAND) and Duel.IsExistingMatchingCard(c100414023.thfilter2,tp,LOCATION_DECK,0,1,nil,tc:GetCode(),tc:GetOriginalLevel()) then
+		if param==1 and e:IsHasType(EFFECT_TYPE_ACTIVATE) and tc:IsLocation(LOCATION_HAND)
+			and Duel.IsExistingMatchingCard(c100414023.thfilter2,tp,LOCATION_DECK,0,1,nil,tc:GetCode(),tc:GetOriginalLevel()) then
 			Duel.BreakEffect()
 			local tg=Duel.SelectMatchingCard(tp,c100414023.thfilter2,tp,LOCATION_DECK,0,1,1,nil,tc:GetCode(),tc:GetOriginalLevel())
 			Duel.SendtoHand(tg,nil,REASON_EFFECT)
