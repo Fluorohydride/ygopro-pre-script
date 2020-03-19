3dfa1911ebc8bcfcc0b9cd3658757d1d6264d4a0
mallu11 <54900811+mallu11@users.noreply.github.com>
2020-03-20 00:17:41 +0800

    fix 焔聖騎士－モージ

--- scripts/ROTD-JP/c101101015.lua
+++ scripts/ROTD-JP/c101101015.lua
@@ -31,7 +31,8 @@ function c101101015.initial_effect(c)
 	c:RegisterEffect(e3)
 end
 function c101101015.cfilter(c)
-	return ((c:IsRace(RACE_WARRIOR) and c:IsAttribute(ATTRIBUTE_FIRE)) or c:IsSetCard(0x207a)) and c:IsAbleToDeckAsCost()
+	return (c:IsLocation(LOCATION_GRAVE) or c:IsFaceup())
+		and ((c:IsRace(RACE_WARRIOR) and c:IsAttribute(ATTRIBUTE_FIRE)) or c:IsSetCard(0x207a)) and c:IsAbleToDeckAsCost()
 end
 function c101101015.drcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(c101101015.cfilter,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,3,e:GetHandler()) end
@@ -57,24 +58,25 @@ function c101101015.eqtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_SZONE)>0
 		and Duel.IsExistingTarget(c101101015.eqfilter,tp,LOCATION_MZONE,0,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)
+	Duel.SetOperationInfo(0,CATEGORY_EQUIP,e:GetHandler(),1,0,0)
 	Duel.SelectTarget(tp,c101101015.eqfilter,tp,LOCATION_MZONE,0,1,1,nil)
 end
 function c101101015.eqop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local tc=Duel.GetFirstTarget()
 	if c:IsRelateToEffect(e) and tc:IsFaceup() and tc:IsControler(tp) and tc:IsRelateToEffect(e) then
-		Duel.Equip(tp,c,tc)
+		if not Duel.Equip(tp,c,tc) then return end
 		--equip limit
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_EQUIP_LIMIT)
 		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e1:SetLabelObject(tc)
 		e1:SetValue(c101101015.eqlimit)
 		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
 		c:RegisterEffect(e1)
 	end
 end
 function c101101015.eqlimit(e,c)
-	local tp=e:GetHandlerPlayer()
-	return c:IsControler(tp)
+	return c==e:GetLabelObject()
 end
