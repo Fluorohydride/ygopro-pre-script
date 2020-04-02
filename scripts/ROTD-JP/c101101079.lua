0149e8a8b0896d1b6634dffea817eb4faa25bbf0
mallu11 <54900811+mallu11@users.noreply.github.com>
2020-04-02 21:11:58 +0800

    add 天龍雪獄 (#429)

---
+++ scripts/ROTD-JP/c101101079.lua
@@ -0,0 +1,70 @@
+--天龍雪獄
+
+--Scripted by mallu11
+function c101101079.initial_effect(c)
+	--activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_REMOVE)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetHintTiming(0,TIMINGS_CHECK_MONSTER+TIMING_END_PHASE)
+	e1:SetCountLimit(1,101101079+EFFECT_COUNT_CODE_OATH)
+	e1:SetTarget(c101101079.target)
+	e1:SetOperation(c101101079.activate)
+	c:RegisterEffect(e1)
+end
+function c101101079.spfilter(c,e,tp)
+	return c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function c101101079.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(1-tp) and c101101079.spfilter(chkc,e,tp) end
+	if chk==0 then
+		return Duel.IsExistingTarget(c101101079.spfilter,tp,0,LOCATION_GRAVE,1,nil,e,tp)
+			and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+	end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectTarget(tp,c101101079.spfilter,tp,0,LOCATION_GRAVE,1,1,nil,e,tp)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_REMOVE,nil,0,PLAYER_ALL,LOCATION_MZONE)
+end
+function c101101079.rmfilter(c)
+	return c:IsFaceup() and c:IsAbleToRemove()
+end
+function c101101079.fselect(g)
+	return g:GetClassCount(Card.GetControler)==g:GetCount() and g:IsExists(c101101079.fcheck,1,nil,g)
+end
+function c101101079.fcheck(c,g)
+	return g:IsExists(Card.IsRace,1,c,c:GetRace())
+end
+function c101101079.activate(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tc=Duel.GetFirstTarget()
+	local res=false
+	if tc:IsRelateToEffect(e) then
+		res=Duel.SpecialSummonStep(tc,0,tp,tp,false,false,POS_FACEUP)
+		if res then
+			local e1=Effect.CreateEffect(c)
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetCode(EFFECT_DISABLE)
+			e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+			tc:RegisterEffect(e1)
+			local e2=Effect.CreateEffect(c)
+			e2:SetType(EFFECT_TYPE_SINGLE)
+			e2:SetCode(EFFECT_DISABLE_EFFECT)
+			e2:SetReset(RESET_EVENT+RESETS_STANDARD)
+			tc:RegisterEffect(e2)
+		end
+		Duel.SpecialSummonComplete()
+	end
+	if res then
+		local g=Duel.GetMatchingGroup(c101101079.rmfilter,tp,LOCATION_MZONE,LOCATION_MZONE,nil)
+		if g:CheckSubGroup(c101101079.fselect,2,2) and Duel.SelectYesNo(tp,aux.Stringid(101101079,0)) then
+			Duel.BreakEffect()
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+			local sg=g:SelectSubGroup(tp,c101101079.fselect,false,2,2)
+			Duel.HintSelection(sg)
+			Duel.Remove(sg,POS_FACEUP,REASON_EFFECT)
+		end
+	end
+end
