3320fee6e92a4002f2c1b2a96fc02f376b5a5ac5
mallu11 <54900811+mallu11@users.noreply.github.com>
2020-04-05 14:16:17 +0800

    add ROTD scripts (#430)

---
+++ scripts/ROTD-JP/c101101039.lua
@@ -0,0 +1,87 @@
+--デストーイ・クルーエル・ホエール
+
+--Scripted by mallu11
+function c101101039.initial_effect(c)
+	--fusion material
+	c:EnableReviveLimit()
+	aux.AddFusionProcFun2(c,aux.FilterBoolFunction(Card.IsFusionSetCard,0xc3),aux.FilterBoolFunction(Card.IsFusionSetCard,0xa9),true)
+	--destroy
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(101101039,0))
+	e1:SetCategory(CATEGORY_DESTROY)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetProperty(EFFECT_FLAG_DELAY)
+	e1:SetCondition(c101101039.descon)
+	e1:SetTarget(c101101039.destg)
+	e1:SetOperation(c101101039.desop)
+	c:RegisterEffect(e1)
+	--atk
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(101101039,1))
+	e2:SetCategory(CATEGORY_TOGRAVE+CATEGORY_ATKCHANGE)
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DAMAGE_STEP)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetHintTiming(0,TIMINGS_CHECK_MONSTER+TIMING_DAMAGE_STEP+TIMING_END_PHASE)
+	e2:SetCountLimit(1)
+	e2:SetCondition(aux.dscon)
+	e2:SetTarget(c101101039.atktg)
+	e2:SetOperation(c101101039.atkop)
+	c:RegisterEffect(e2)
+end
+function c101101039.descon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsSummonType(SUMMON_TYPE_FUSION)
+end
+function c101101039.destg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local g1=Duel.GetMatchingGroup(aux.TRUE,tp,LOCATION_ONFIELD,0,nil)
+	local g2=Duel.GetMatchingGroup(aux.TRUE,tp,0,LOCATION_ONFIELD,nil)
+	if chk==0 then return g1:GetCount()>0 and g2:GetCount()>0 end
+	g1:Merge(g2)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g1,2,0,0)
+end
+function c101101039.desop(e,tp,eg,ep,ev,re,r,rp)
+	local g1=Duel.GetMatchingGroup(aux.TRUE,tp,LOCATION_ONFIELD,0,nil)
+	local g2=Duel.GetMatchingGroup(aux.TRUE,tp,0,LOCATION_ONFIELD,nil)
+	if g1:GetCount()>0 and g2:GetCount()>0 then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+		local dg1=g1:Select(tp,1,1,nil)
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+		local dg2=g2:Select(tp,1,1,nil)
+		dg1:Merge(dg2)
+		if dg1:GetCount()==2 then
+			Duel.HintSelection(dg1)
+			Duel.Destroy(dg1,REASON_EFFECT)
+		end
+	end
+end
+function c101101039.atkfilter(c)
+	return c:IsFaceup() and c:IsType(TYPE_FUSION) and c:GetBaseAttack()>0
+end
+function c101101039.tgfilter(c)
+	return c:IsSetCard(0xad) and not c:IsCode(101101039) and c:IsAbleToGrave()
+end
+function c101101039.atktg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and c101101039.atkfilter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(c101101039.atkfilter,tp,LOCATION_MZONE,0,1,nil) and Duel.IsExistingMatchingCard(c101101039.tgfilter,tp,LOCATION_DECK+LOCATION_EXTRA,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+	Duel.SelectTarget(tp,c101101039.atkfilter,tp,LOCATION_MZONE,0,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_DECK+LOCATION_EXTRA)
+end
+function c101101039.atkop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local g=Duel.SelectMatchingCard(tp,c101101039.tgfilter,tp,LOCATION_DECK+LOCATION_EXTRA,0,1,1,nil)
+	local gc=g:GetFirst()
+	if gc and Duel.SendtoGrave(gc,REASON_EFFECT)~=0 and gc:IsLocation(LOCATION_GRAVE) and tc:IsRelateToEffect(e) and tc:IsFaceup() then
+		local atk=tc:GetBaseAttack()
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		e1:SetValue(math.ceil(atk/2))
+		tc:RegisterEffect(e1)
+	end
+end
