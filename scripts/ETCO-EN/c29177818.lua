07b44431f5db0f201b500df374131f0f2d43ada8
Khriskh <chrisqs16@hotmail.com>
2020-04-29 21:42:37 -0500

    ETCO-EN official passcode

--- scripts/ETCO-EN/c29177818.lua
+++ scripts/ETCO-EN/c29177818.lua
@@ -1,60 +1,60 @@
 --Rose Girl
 --
 --Scripted by: XGlitchy30
-function c101012081.initial_effect(c)
+function c29177818.initial_effect(c)
 	--spsummon
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(101012081,0))
+	e1:SetDescription(aux.Stringid(29177818,0))
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
 	e1:SetCode(EVENT_TO_GRAVE)
 	e1:SetProperty(EFFECT_FLAG_DELAY)
 	e1:SetRange(LOCATION_HAND)
-	e1:SetCountLimit(1,101012081)
-	e1:SetCondition(c101012081.spcon)
-	e1:SetTarget(c101012081.sptg)
-	e1:SetOperation(c101012081.spop)
+	e1:SetCountLimit(1,29177818)
+	e1:SetCondition(c29177818.spcon)
+	e1:SetTarget(c29177818.sptg)
+	e1:SetOperation(c29177818.spop)
 	c:RegisterEffect(e1)
 	--tohand
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(101012081,1))
+	e2:SetDescription(aux.Stringid(29177818,1))
 	e2:SetCategory(CATEGORY_TOHAND)
 	e2:SetType(EFFECT_TYPE_IGNITION)
 	e2:SetRange(LOCATION_GRAVE)
-	e2:SetCountLimit(1,101012081)
-	e2:SetCondition(c101012081.thcon)
-	e2:SetTarget(c101012081.thtg)
-	e2:SetOperation(c101012081.thop)
+	e2:SetCountLimit(1,29177818)
+	e2:SetCondition(c29177818.thcon)
+	e2:SetTarget(c29177818.thtg)
+	e2:SetOperation(c29177818.thop)
 	c:RegisterEffect(e2)
 end
-function c101012081.cfilter(c,tp)
+function c29177818.cfilter(c,tp)
 	return c:IsPreviousLocation(LOCATION_MZONE) and c:GetPreviousControler()==tp and c:IsPreviousPosition(POS_FACEUP)
 		and bit.band(c:GetPreviousRaceOnField(),RACE_PLANT)>0
 end
-function c101012081.spcon(e,tp,eg,ep,ev,re,r,rp)
-	return eg:IsExists(c101012081.cfilter,1,nil,tp)
+function c29177818.spcon(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(c29177818.cfilter,1,nil,tp)
 end
-function c101012081.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+function c29177818.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
 		and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
 end
-function c101012081.spop(e,tp,eg,ep,ev,re,r,rp)
+function c29177818.spop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if not c:IsRelateToEffect(e) then return end
 	Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
 end
-function c101012081.filter(c)
+function c29177818.filter(c)
 	return c:IsFaceup() and c:IsRace(RACE_PLANT)
 end
-function c101012081.thcon(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.IsExistingMatchingCard(c101012081.filter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil)
+function c29177818.thcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsExistingMatchingCard(c29177818.filter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil)
 end
-function c101012081.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+function c29177818.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return e:GetHandler():IsAbleToHand() end
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,e:GetHandler(),1,0,0)
 end
-function c101012081.thop(e,tp,eg,ep,ev,re,r,rp)
+function c29177818.thop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if c:IsRelateToEffect(e) then
 		Duel.SendtoHand(c,nil,REASON_EFFECT)
