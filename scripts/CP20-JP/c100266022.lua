9abd286a177c62916ec03ec155856a62df97d4d1
mallu11 <54900811+mallu11@users.noreply.github.com>
2020-05-15 17:45:45 +0800

    fix No.1 ゲート・オブ・ヌメロン－エーカム

--- scripts/CP20-JP/c100266022.lua
+++ scripts/CP20-JP/c100266022.lua
@@ -2,50 +2,54 @@
 --Number 1: Numeron Gate Ekam
 --Scripted by TOP
 function c100266022.initial_effect(c)
-    --xyz summon
-    aux.AddXyzProcedure(c,nil,1,3)
-    c:EnableReviveLimit()
-    --battle indestructable
-    local e1=Effect.CreateEffect(c)
-    e1:SetType(EFFECT_TYPE_SINGLE)
-    e1:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
-    e1:SetValue(1)
-    c:RegisterEffect(e1)
-    --atkup
-    local e2=Effect.CreateEffect(c)
-    e2:SetDescription(aux.Stringid(100266022,0))
-    e2:SetCategory(CATEGORY_ATKCHANGE)
-    e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-    e2:SetCode(EVENT_DAMAGE_STEP_END)
-    e2:SetRange(LOCATION_MZONE)
-    e2:SetCost(c100266022.atkcost)
-    e2:SetCondition(c100266022.atkcon)
-    e2:SetOperation(c100266022.atkop)
-    c:RegisterEffect(e2)
+	--xyz summon
+	aux.AddXyzProcedure(c,nil,1,3)
+	c:EnableReviveLimit()
+	--battle indestructable
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+	e1:SetValue(1)
+	c:RegisterEffect(e1)
+	--atkup
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(100266022,0))
+	e2:SetCategory(CATEGORY_ATKCHANGE)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_DAMAGE_STEP_END)
+	e2:SetCost(c100266022.atkcost)
+	e2:SetCondition(c100266022.atkcon)
+	e2:SetTarget(c100266022.atktg)
+	e2:SetOperation(c100266022.atkop)
+	c:RegisterEffect(e2)
 end
 c100266022.xyz_number=1
 function c100266022.atkcost(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return e:GetHandler():CheckRemoveOverlayCard(tp,1,REASON_COST) end
-    e:GetHandler():RemoveOverlayCard(tp,1,1,REASON_COST)
+	if chk==0 then return e:GetHandler():CheckRemoveOverlayCard(tp,1,REASON_COST) end
+	e:GetHandler():RemoveOverlayCard(tp,1,1,REASON_COST)
 end
 function c100266022.atkcon(e,tp,eg,ep,ev,re,r,rp)
-    local c=e:GetHandler()
-    local bc=c:GetBattleTarget()
-    return bc and bc:IsControler(1-tp)
+	local c=e:GetHandler()
+	local bc=c:GetBattleTarget()
+	return bc and bc:IsControler(1-tp)
 end
 function c100266022.atkfilter(c)
-    return c:IsFaceup() and c:IsSetCard(0x24a)
+	return c:IsFaceup() and c:IsSetCard(0x24a)
+end
+function c100266022.atktg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c100266022.atkfilter,tp,LOCATION_MZONE,0,1,nil) end
 end
 function c100266022.atkop(e,tp,eg,ep,ev,re,r,rp)
-    local tg=Duel.GetMatchingGroup(c100266022.atkfilter,tp,LOCATION_MZONE,0,nil)
-            local tc=tg:GetFirst()
-            while tc do
-                local e1=Effect.CreateEffect(e:GetHandler())
-                e1:SetType(EFFECT_TYPE_SINGLE)
-                e1:SetCode(EFFECT_SET_ATTACK_FINAL)
-                e1:SetValue(tc:GetAttack()*2)
-                e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
-                tc:RegisterEffect(e1)
-                tc=tg:GetNext()
-            end
+	local g=Duel.GetMatchingGroup(c100266022.atkfilter,tp,LOCATION_MZONE,0,nil)
+	local tc=g:GetFirst()
+	while tc do
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_SET_ATTACK_FINAL)
+		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e1:SetValue(tc:GetAttack()*2)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		tc:RegisterEffect(e1)
+		tc=g:GetNext()
+	end
 end
