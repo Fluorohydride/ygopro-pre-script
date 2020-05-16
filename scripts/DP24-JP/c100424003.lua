a5a099f9102f692e2f4dc7a1dc494ec66e77e8c0
shadowfox87 <aiyush87@gmail.com>
2020-05-16 10:31:48 -0400

    fix string (#455)

--- scripts/DP24-JP/c100424003.lua
+++ scripts/DP24-JP/c100424003.lua
@@ -29,7 +29,7 @@ function c100424003.initial_effect(c)
 	c:RegisterEffect(e2)
 	--to hand
 	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(100424003,0))
+	e3:SetDescription(aux.Stringid(100424003,2))
 	e3:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
 	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e3:SetCode(EVENT_TO_GRAVE)
