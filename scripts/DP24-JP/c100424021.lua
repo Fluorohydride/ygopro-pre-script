27068abaf556a5de272f605acfca9af896d94976
mallu11 <54900811+mallu11@users.noreply.github.com>
2020-05-31 14:03:20 +0800

    fix 機皇統制
    https://www.db.yugioh-card.com/yugiohdb/faq_search.action?ope=5&fid=23020&keyword=&tag=-1
    
    Question
    自分のモンスターゾーンに「機皇帝ワイゼル∞－S・アブソープション」と「機皇帝スキエル∞」が表側表示で存在しています。
    
    この状況で、「機皇帝ワイゼル∞－S・アブソープション」を対象として「機皇統制」の『①』の効果を発動した際に、相手がチェーンして「月の書」を発動し、対象の「機皇帝ワイゼル∞－S・アブソープション」が裏側守備表示になっている場合、処理はどうなりますか？
    Answer
    質問の状況のように、「機皇統制」の『①』の効果の対象としたモンスターが効果処理時に裏側守備表示になっている場合、「機皇統制」の『そのモンスターの攻撃力は、自分フィールドの「機皇」モンスターの元々の攻撃力を合計した数値になり』の処理が適用されませんので、『ターン終了時までそのモンスターの戦闘で発生する相手への戦闘ダメージは０になる』処理も適用されません。

--- scripts/DP24-JP/c100424021.lua
+++ scripts/DP24-JP/c100424021.lua
@@ -42,20 +42,18 @@ end
 function c100424021.activate(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local tc=Duel.GetFirstTarget()
-	if tc:IsRelateToEffect(e) then
-		if tc:IsFaceup() then
-			local atk=0
-			local g=Duel.GetMatchingGroup(c100424021.atkfilter,tp,LOCATION_MZONE,0,nil)
-			if g:GetCount()>0 then atk=g:GetSum(Card.GetBaseAttack) end
-			--atk
-			local e1=Effect.CreateEffect(c)
-			e1:SetType(EFFECT_TYPE_SINGLE)
-			e1:SetCode(EFFECT_SET_ATTACK_FINAL)
-			e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-			e1:SetValue(atk)
-			e1:SetReset(RESET_EVENT+RESETS_STANDARD)
-			tc:RegisterEffect(e1)
-		end
+	if tc:IsRelateToEffect(e) and tc:IsFaceup() then
+		local atk=0
+		local g=Duel.GetMatchingGroup(c100424021.atkfilter,tp,LOCATION_MZONE,0,nil)
+		if g:GetCount()>0 then atk=g:GetSum(Card.GetBaseAttack) end
+		--atk
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_SET_ATTACK_FINAL)
+		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e1:SetValue(atk)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		tc:RegisterEffect(e1)
 		--damage 0
 		local e2=Effect.CreateEffect(c)
 		e2:SetType(EFFECT_TYPE_SINGLE)
