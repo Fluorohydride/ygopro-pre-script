aaed65814e77aac21b7bd6b4e4087604e3d447ee
Mercury233 <me@mercury233.me>
2020-06-10 09:05:06 +0800

    add scripts by JustFish (#470)

---
+++ scripts/CP20-JP/c100266017.lua
@@ -0,0 +1,60 @@
+--アフター・グロー
+--Afterglow
+--Script by JustFish
+function c100266017.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_REMOVE+CATEGORY_TODECK)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCountLimit(1,100266017+EFFECT_COUNT_CODE_DUEL+EFFECT_COUNT_CODE_OATH)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetTarget(c100266017.target)
+	e1:SetOperation(c100266017.operation)
+	c:RegisterEffect(e1)
+end
+function c100266017.rmfilter(c)
+	return c:IsCode(100266017) and c:IsAbleToRemove() and (c:IsFaceup() or not c:IsOnField())
+end
+function c100266017.tdfilter(c)
+	return c:IsCode(100266017) and c:IsAbleToDeck() and c:IsFaceup()
+end
+function c100266017.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	local g=Duel.GetMatchingGroup(c100266017.rmfilter,tp,LOCATION_HAND+LOCATION_ONFIELD+LOCATION_GRAVE+LOCATION_DECK,0,nil)
+	g:AddCard(e:GetHandler())
+	if chk==0 then return g:GetCount()>0 end
+	Duel.SetOperationInfo(0,CATEGORY_REMOVE,g,g:GetCount(),tp,LOCATION_HAND+LOCATION_ONFIELD+LOCATION_GRAVE+LOCATION_DECK)
+	Duel.SetOperationInfo(0,CATEGORY_TODECK,nil,1,tp,LOCATION_REMOVED)
+end
+function c100266017.operation(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetHandler():IsRelateToEffect(e) then return end
+	local g=Duel.GetMatchingGroup(c100266017.rmfilter,tp,LOCATION_HAND+LOCATION_ONFIELD+LOCATION_GRAVE+LOCATION_DECK,0,nil)
+	if Duel.Remove(g,POS_FACEUP,REASON_EFFECT)>0 then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
+		local tg=Duel.SelectMatchingCard(tp,c100266017.tdfilter,tp,LOCATION_REMOVED,0,1,1,nil)
+		if tg:GetCount()>0 then
+			Duel.SendtoDeck(tg,nil,2,REASON_EFFECT)
+		end
+	end
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetProperty(EFFECT_FLAG_DELAY)
+	e1:SetCode(EVENT_DRAW)
+	e1:SetCondition(c100266017.damcon)
+	e1:SetOperation(c100266017.damop)
+	e1:SetReset(RESET_PHASE+PHASE_DRAW+RESET_SELF_TURN)
+	Duel.RegisterEffect(e1,tp)
+end
+function c100266017.damcon(e,tp,eg,ep,ev,re,r,rp)
+	return ep==e:GetOwnerPlayer() and r==REASON_RULE
+end
+function c100266017.damop(e,tp,eg,ep,ev,re,r,rp)
+	local hg=eg:Filter(Card.IsLocation,nil,LOCATION_HAND)
+	if hg:GetCount()==0 then return end
+	Duel.ConfirmCards(1-ep,hg)
+	local dg=hg:Filter(Card.IsCode,nil,100266017)
+	if dg:GetCount()>0 then
+		Duel.Hint(HINT_CARD,0,100266017)
+		Duel.Damage(1-ep,4000,REASON_EFFECT)
+	end
+	Duel.ShuffleHand(ep)
+end
---
+++ scripts/TOCH-EN/c100268010.lua
@@ -0,0 +1,75 @@
+--Eternal Chaos
+--Script by JustFish
+function c100268010.initial_effect(c)
+	--activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_TOGRAVE)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetHintTiming(0,TIMINGS_CHECK_MONSTER+TIMING_END_PHASE)
+	e1:SetCountLimit(1,100268010+EFFECT_COUNT_CODE_OATH)
+	e1:SetTarget(c100268010.target)
+	e1:SetOperation(c100268010.activate)
+	c:RegisterEffect(e1)
+end
+function c100268010.tfilter(c,tp)
+	return c:IsFaceup() and Duel.IsExistingMatchingCard(c100268010.tgfilter,tp,LOCATION_DECK,0,1,c,tp,c:GetAttack())
+end
+function c100268010.tgfilter(c,tp,atk)
+	return c:IsAttackBelow(atk) and c:IsAbleToGrave() and c:IsAttribute(ATTRIBUTE_LIGHT+ATTRIBUTE_DARK)
+		and Duel.IsExistingMatchingCard(c100268010.tgfilter1,tp,LOCATION_DECK,0,1,c,atk-c:GetAttack(),c:GetAttribute())
+end
+function c100268010.tgfilter1(c,atk,att)
+	return c:IsAttackBelow(atk) and c:IsAbleToGrave() and c:IsAttribute(ATTRIBUTE_LIGHT+ATTRIBUTE_DARK) and not c:IsAttribute(att)
+end
+function c100268010.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) and c100268010.desfilter(chkc,tp) end
+	if chk==0 then return Duel.IsExistingTarget(c100268010.tfilter,tp,0,LOCATION_MZONE,1,nil,tp) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+	Duel.SelectTarget(tp,c100268010.tfilter,tp,0,LOCATION_MZONE,1,1,nil,tp)
+	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,2,tp,LOCATION_DECK)
+end
+function c100268010.activate(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsFaceup() and tc:IsRelateToEffect(e) then
+		local atk=tc:GetAttack()
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+		local g=Duel.SelectMatchingCard(tp,c100268010.tgfilter,tp,LOCATION_DECK,0,1,1,nil,tp,atk)
+		local gc=g:GetFirst()
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+		local g1=Duel.SelectMatchingCard(tp,c100268010.tgfilter1,tp,LOCATION_DECK,0,1,1,gc,atk-gc:GetAttack(),gc:GetAttribute())
+		g:Merge(g1)
+		if g:GetCount()>1 then
+			Duel.SendtoGrave(g,REASON_EFFECT)
+		end
+	end
+	if e:IsHasType(EFFECT_TYPE_ACTIVATE) then
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_FIELD)
+		e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+		e1:SetCode(EFFECT_CANNOT_ACTIVATE)
+		e1:SetTargetRange(1,0)
+		e1:SetCondition(c100268010.actcon)
+		e1:SetValue(c100268010.actlimit)
+		e1:SetReset(RESET_PHASE+PHASE_END)
+		Duel.RegisterEffect(e1,tp)
+		local e2=Effect.CreateEffect(e:GetHandler())
+		e2:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
+		e2:SetCode(EVENT_CHAINING)
+		e2:SetOperation(c100268010.aclimit1)
+		e2:SetReset(RESET_PHASE+PHASE_END)
+		Duel.RegisterEffect(e2,tp)
+	end
+end
+function c100268010.actcon(e)
+	return Duel.GetFlagEffect(e:GetHandlerPlayer(),100268010)~=0
+end
+function c100268010.actlimit(e,re,tp)
+	return re:IsActiveType(TYPE_MONSTER) and re:GetActivateLocation()==LOCATION_GRAVE
+end
+function c100268010.aclimit1(e,tp,eg,ep,ev,re,r,rp)
+	local tp=e:GetHandlerPlayer()
+	if ep~=tp or not re:IsActiveType(TYPE_MONSTER) or not re:GetActivateLocation()==LOCATION_GRAVE then return end
+	Duel.RegisterFlagEffect(tp,100268010,RESET_PHASE+PHASE_END,0,1)
+end
