6ea3a172d131cc307c566e6b6dc95131177c933d
mercury233 <me@mercury233.me>
2020-06-11 05:59:04 +0800

    add scripts PHRA by JoyJ

---
+++ scripts/PHRA-JP/c101102041.lua
@@ -0,0 +1,93 @@
+--Arc Rebellion Xyz Dragon
+function c101102041.initial_effect(c)
+	--xyz summon
+	aux.AddXyzProcedure(c,nil,5,3)
+	c:EnableReviveLimit()
+	--indes
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+	e1:SetCondition(c101102041.indcon)
+	e1:SetValue(1)
+	c:RegisterEffect(e1)
+	--negate
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(101102041,0))
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetCategory(CATEGORY_ATKCHANGE)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1,101102041)
+	e2:SetCost(c101102041.cost)
+	e2:SetTarget(c101102041.target)
+	e2:SetOperation(c101102041.operation)
+	c:RegisterEffect(e2)
+end
+function c101102041.indcon(e)
+	return e:GetHandler():IsSummonType(SUMMON_TYPE_XYZ)
+end
+function c101102041.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():CheckRemoveOverlayCard(tp,1,REASON_COST) end
+	e:GetHandler():RemoveOverlayCard(tp,1,1,REASON_COST)
+end
+function c101102041.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetFieldGroup(tp,LOCATION_MZONE,LOCATION_MZONE):GetSum(Card.GetBaseAttack)>0 end
+end
+function c101102041.mgfilter(c)
+	return c:IsType(TYPE_XYZ) and c:IsAttribute(ATTRIBUTE_DARK)
+end
+function c101102041.tgfilter(c)
+	return c:IsFaceup() and c:IsType(TYPE_EFFECT)
+end
+function c101102041.operation(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local atk=Duel.GetFieldGroup(tp,LOCATION_MZONE,LOCATION_MZONE):GetSum(Card.GetBaseAttack)
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_UPDATE_ATTACK)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+	e1:SetValue(atk)
+	c:RegisterEffect(e1)
+	local mg=c:GetOverlayGroup()
+	if not mg:IsExists(c101102041.mgfilter,1,nil) then return end
+	local exc=aux.ExceptThisCard(e)
+	local g=Duel.GetMatchingGroup(c101102041.tgfilter,tp,LOCATION_MZONE,LOCATION_MZONE,exc)
+	for tc in aux.Next(g) do
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_DISABLE)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		tc:RegisterEffect(e1)
+		local e2=Effect.CreateEffect(c)
+		e2:SetType(EFFECT_TYPE_SINGLE)
+		e2:SetCode(EFFECT_DISABLE_EFFECT)
+		e2:SetReset(RESET_EVENT+RESETS_STANDARD)
+		tc:RegisterEffect(e2)
+	end
+	local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_FIELD)
+	e0:SetCode(EFFECT_CANNOT_ATTACK)
+	e0:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
+	e0:SetTargetRange(LOCATION_MZONE,0)
+	e0:SetTarget(c101102041.ftarget)
+	e0:SetLabel(c:GetFieldID())
+	e0:SetReset(RESET_PHASE+PHASE_END)
+	Duel.RegisterEffect(e0,tp)
+end
+
+function c101102041.ftarget(e,c)
+	return e:GetLabel()~=c:GetFieldID()
+end
+
+
+
+
+
+
+
+
+
+
+
