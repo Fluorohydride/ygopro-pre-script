c89c8e47cc43cb2cd9b7c4bda7622f2c8506a462
mercury233 <me@mercury233.me>
2020-06-11 14:35:45 +0800

    add script CP20 by JoyJ

---
+++ scripts/CP20-JP/c100266015.lua
@@ -0,0 +1,76 @@
+--Psychic Wave
+function c100266015.initial_effect(c)
+	aux.AddCodeList(c,77585513)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_DAMAGE)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCondition(c100266015.condition)
+	e1:SetCost(c100266015.cost)
+	e1:SetTarget(c100266015.target)
+	e1:SetOperation(c100266015.activate)
+	c:RegisterEffect(e1)
+	--tohand
+	local e2=Effect.CreateEffect(c)
+	e2:SetCategory(CATEGORY_TOGRAVE+CATEGORY_TOHAND)
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetRange(LOCATION_GRAVE)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetCondition(c100266015.thcon)
+	e2:SetCost(aux.bfgcost)
+	e2:SetTarget(c100266015.thtg)
+	e2:SetOperation(c100266015.thop)
+	c:RegisterEffect(e2)
+end
+function c100266015.cfilter(c)
+	return c:IsRace(RACE_MACHINE) and c:IsFaceup()
+end
+function c100266015.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsExistingMatchingCard(c100266015.cfilter,tp,LOCATION_MZONE,0,1,nil)
+end
+function c100266015.costfilter(c)
+	return c:IsCode(77585513) and c:IsAbleToGraveAsCost()
+end
+function c100266015.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return Duel.IsExistingMatchingCard(c100266015.costfilter,tp,LOCATION_HAND+LOCATION_DECK,0,1,nil) end
+	local g=Duel.SelectMatchingCard(tp,c100266015.filter,tp,LOCATION_HAND+LOCATION_DECK,0,1,1,nil)
+	Duel.SendtoGrave(g,REASON_COST)
+end
+function c100266015.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetTargetPlayer(1-tp)
+	Duel.SetTargetParam(600)
+	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,600)
+end
+function c100266015.activate(e,tp,eg,ep,ev,re,r,rp)
+	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+	Duel.Damage(p,d,REASON_EFFECT)
+end
+function c100266015.thcon(e,tp,eg,ep,ev,re,r,rp)
+	return aux.exccon(e) and Duel.GetTurnPlayer()==tp and (Duel.GetCurrentPhase()==PHASE_MAIN1 or Duel.GetCurrentPhase()==PHASE_MAIN2)
+end
+function c100266015.dfilter(c)
+	return c:IsSetCard(0xbc) and c:IsAbleToGrave()
+end
+function c100266015.thfilter(c)
+	return c:IsType(TYPE_MONSTER) and c:IsRace(RACE_MACHINE) and c:IsAbleToHand()
+end
+function c100266015.thtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_GRAVE) and c100266015.thfilter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(c100266015.thfilter,tp,LOCATION_GRAVE,0,1,nil)
+		and Duel.IsExistingMatchingCard(c100266015.dfilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectTarget(tp,c100266015.thfilter,tp,LOCATION_GRAVE,0,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_DECK)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,0,0)
+end
+function c100266015.thop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	local g=Duel.SelectMatchingCard(tp,c100266015.dfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if g and Duel.SendtoGrave(g,REASON_EFFECT) and tc:IsRelateToEffect(e) then
+		Duel.SendtoHand(tc,nil,REASON_EFFECT)
+	end
+end
