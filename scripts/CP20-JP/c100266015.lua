bf0739470840bfb7e04a200042d06de432e33b53
mercury233 <me@mercury233.me>
2020-06-11 19:30:29 +0800

    fix

--- scripts/CP20-JP/c100266015.lua
+++ scripts/CP20-JP/c100266015.lua
@@ -16,11 +16,11 @@ function c100266015.initial_effect(c)
 	--tohand
 	local e2=Effect.CreateEffect(c)
 	e2:SetCategory(CATEGORY_TOGRAVE+CATEGORY_TOHAND)
-	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetType(EFFECT_TYPE_IGNITION)
 	e2:SetRange(LOCATION_GRAVE)
 	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
-	e2:SetCode(EVENT_FREE_CHAIN)
-	e2:SetCondition(c100266015.thcon)
+	e2:SetCountLimit(1,100266015)
+	e2:SetCondition(aux.exccon)
 	e2:SetCost(aux.bfgcost)
 	e2:SetTarget(c100266015.thtg)
 	e2:SetOperation(c100266015.thop)
@@ -51,9 +51,6 @@ function c100266015.activate(e,tp,eg,ep,ev,re,r,rp)
 	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
 	Duel.Damage(p,d,REASON_EFFECT)
 end
-function c100266015.thcon(e,tp,eg,ep,ev,re,r,rp)
-	return aux.exccon(e) and Duel.GetTurnPlayer()==tp and (Duel.GetCurrentPhase()==PHASE_MAIN1 or Duel.GetCurrentPhase()==PHASE_MAIN2)
-end
 function c100266015.dfilter(c)
 	return c:IsSetCard(0xbc) and c:IsAbleToGrave()
 end
@@ -71,8 +68,10 @@ function c100266015.thtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 end
 function c100266015.thop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
 	local g=Duel.SelectMatchingCard(tp,c100266015.dfilter,tp,LOCATION_DECK,0,1,1,nil)
-	if g and Duel.SendtoGrave(g,REASON_EFFECT) and tc:IsRelateToEffect(e) then
+	if #g>0 and Duel.SendtoGrave(g,REASON_EFFECT)>0 and g:GetFirst():IsLocation(LOCATION_GRAVE)
+		and tc:IsRelateToEffect(e) then
 		Duel.SendtoHand(tc,nil,REASON_EFFECT)
 	end
 end
