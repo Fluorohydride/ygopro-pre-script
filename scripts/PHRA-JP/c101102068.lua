1fcb9e31a766f4811902af83fac4814b34d14a3b
mercury233 <me@mercury233.me>
2020-06-12 20:01:15 +0800

    fix Raiders' Unbreakable Mind

--- scripts/PHRA-JP/c101102068.lua
+++ scripts/PHRA-JP/c101102068.lua
@@ -35,19 +35,19 @@ end
 function c101102068.descfilter2(c)
 	return c:IsType(TYPE_XYZ) and c:IsAttribute(ATTRIBUTE_DARK)
 end
-function c101102068.descfilter(c,lg)
-	return c:IsSummonType(SUMMON_TYPE_XYZ)
+function c101102068.descfilter(c,tp)
+	return c:IsSummonType(SUMMON_TYPE_XYZ) and c:GetSummonPlayer()==tp
 		and c:GetMaterial():IsExists(c101102068.descfilter2,1,nil)
 end
 function c101102068.descon(e,tp,eg,ep,ev,re,r,rp)
-	return eg:IsExists(c101102068.descfilter,1,nil,lg)
+	return eg:IsExists(c101102068.descfilter,1,nil,tp)
 end
 function c101102068.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsOnField() end
 	if chk==0 then return Duel.IsExistingTarget(aux.TRUE,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
 	local g=Duel.SelectTarget(tp,aux.TRUE,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil)
-	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,PLAYER_ALL,LOCATION_ONFIELD)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
 end
 function c101102068.desop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
