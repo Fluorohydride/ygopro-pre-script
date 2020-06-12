7e3e260af8c2f1e0cd925a0d61a4fdfa54f8c663
mercury233 <me@mercury233.me>
2020-06-12 14:00:48 +0800

    fix U.A. Playing Manager

--- scripts/PHRA-JP/c101102019.lua
+++ scripts/PHRA-JP/c101102019.lua
@@ -5,7 +5,7 @@ function c101102019.initial_effect(c)
 	--spsummon
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(101102019,0))
-	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_DRAW)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
 	e1:SetProperty(EFFECT_FLAG_DELAY)
 	e1:SetCode(EVENT_SUMMON_SUCCESS)
