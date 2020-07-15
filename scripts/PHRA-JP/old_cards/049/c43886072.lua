504d4278e66b7c2d22cc212af74e2c951a1e9eb1
mercury233 <me@mercury233.me>
2020-07-15 08:21:04 +0800

    update

---
+++ scripts/PHRA-JP/old_cards/049/c43886072.lua
@@ -0,0 +1,100 @@
+--プランキッズ・バウワウ
+function c43886072.initial_effect(c)
+	--link summon
+	c:EnableReviveLimit()
+	aux.AddLinkProcedure(c,aux.FilterBoolFunction(Card.IsLinkSetCard,0x120),2,2)
+	--atk gain
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_UPDATE_ATTACK)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+	e1:SetTarget(c43886072.atktg)
+	e1:SetValue(1000)
+	c:RegisterEffect(e1)
+	--to hand
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(43886072,0))
+	e2:SetCategory(CATEGORY_TOHAND)
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1,43886072)
+	e2:SetHintTiming(0,TIMING_END_PHASE)
+	e2:SetCondition(c43886072.thcon)
+	e2:SetCost(c43886072.thcost)
+	e2:SetTarget(c43886072.thtg)
+	e2:SetOperation(c43886072.thop)
+	c:RegisterEffect(e2)
+end
+function c43886072.atktg(e,c)
+	return e:GetHandler():GetLinkedGroup():IsContains(c) and c:IsSetCard(0x120)
+end
+function c43886072.thcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetTurnPlayer()==1-tp
+end
+function c43886072.excostfilter(c,tp)
+	return (c:IsFaceup() or c:IsLocation(LOCATION_GRAVE)) and c:IsAbleToRemoveAsCost() and c:IsHasEffect(101102049,tp)
+end
+function c43886072.costfilter(c,tp,g)
+	local tg=g:Clone()
+	tg:RemoveCard(c)
+	return tg:GetClassCount(Card.GetCode)>=2
+end
+function c43886072.thcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	e:SetLabel(0)
+	local g=Duel.GetMatchingGroup(c43886072.excostfilter,tp,LOCATION_MZONE+LOCATION_GRAVE,0,nil,tp)
+	local tg=Duel.GetMatchingGroup(c43886072.thfilter,tp,LOCATION_GRAVE,0,nil,e)
+	if e:GetHandler():IsReleasable() then g:AddCard(e:GetHandler()) end
+	if chk==0 then
+		e:SetLabel(100)
+		return g:IsExists(c43886072.costfilter,1,nil,tp,tg)
+	end
+	local cg=g:Filter(c43886072.costfilter,nil,tp,tg)
+	local tc
+	if #cg>1 then
+		Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(101102049,0))
+		tc=cg:Select(tp,1,1,nil):GetFirst()
+	else
+		tc=cg:GetFirst()
+	end
+	local te=tc:IsHasEffect(101102049,tp)
+	if te then
+		te:UseCountLimit(tp)
+		Duel.Remove(tc,POS_FACEUP,REASON_COST+REASON_REPLACE)
+	else
+		Duel.Release(tc,REASON_COST)
+	end
+end
+function c43886072.thfilter(c,e)
+	return c:IsSetCard(0x120) and not c:IsType(TYPE_LINK)
+		and c:IsCanBeEffectTarget(e) and c:IsAbleToHand()
+end
+function c43886072.thtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return false end
+	if chk==0 then return e:GetLabel()==100 end
+	e:SetLabel(0)
+	local g=Duel.GetMatchingGroup(c43886072.thfilter,tp,LOCATION_GRAVE,0,nil,e)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local sg=g:SelectSubGroup(tp,aux.dncheck,false,2,2)
+	Duel.SetTargetCard(sg)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,sg,#sg,0,0)
+end
+function c43886072.thop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS):Filter(Card.IsRelateToEffect,nil,e)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+	end
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+	e1:SetTargetRange(LOCATION_MZONE,0)
+	e1:SetReset(RESET_PHASE+PHASE_END)
+	e1:SetTarget(c43886072.indtg)
+	e1:SetValue(aux.indoval)
+	Duel.RegisterEffect(e1,tp)
+end
+function c43886072.indtg(e,c)
+	return c:IsSetCard(0x120)
+end
--- scripts/PHRA-JP/old_cards/049/c44509529.lua
+++ scripts/PHRA-JP/old_cards/049/c44509529.lua
@@ -1,71 +1,59 @@
---プランキッズ・ロケット
-function c18514525.initial_effect(c)
+--プランキッズ・ウェザー
+function c44509529.initial_effect(c)
 	--fusion material
 	c:EnableReviveLimit()
 	aux.AddFusionProcFunRep(c,aux.FilterBoolFunction(Card.IsFusionSetCard,0x120),2,true)
-	--direct attack
+	--actlimit
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(18514525,0))
-	e1:SetCategory(CATEGORY_ATKCHANGE)
-	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
-	e1:SetProperty(EFFECT_FLAG_DELAY)
-	e1:SetCountLimit(1,18514525)
-	e1:SetCondition(c18514525.atkcon)
-	e1:SetOperation(c18514525.atkop)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetCode(EFFECT_CANNOT_ACTIVATE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetTargetRange(0,1)
+	e1:SetValue(1)
+	e1:SetCondition(c44509529.actcon)
 	c:RegisterEffect(e1)
 	--spsummon
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(18514525,1))
+	e2:SetDescription(aux.Stringid(44509529,0))
 	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_QUICK_O)
 	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
-	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetCode(EVENT_FREE_CHAIN)
 	e2:SetRange(LOCATION_MZONE)
-	e2:SetCountLimit(1,18514526)
-	e2:SetCost(c18514525.spcost)
-	e2:SetTarget(c18514525.sptg)
-	e2:SetOperation(c18514525.spop)
+	e2:SetCountLimit(1,44509529)
+	e2:SetHintTiming(0,TIMING_BATTLE_START+TIMING_END_PHASE)
+	e2:SetCondition(c44509529.spcon)
+	e2:SetCost(c44509529.spcost)
+	e2:SetTarget(c44509529.sptg)
+	e2:SetOperation(c44509529.spop)
 	c:RegisterEffect(e2)
 end
-function c18514525.atkcon(e)
-	return e:GetHandler():IsSummonType(SUMMON_TYPE_FUSION)
+function c44509529.actcon(e)
+	local a=Duel.GetAttacker()
+	return a and a:IsControler(e:GetHandlerPlayer()) and a:IsSetCard(0x120)
 end
-function c18514525.atkop(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	local atk=c:GetAttack()
-	if not c:IsRelateToEffect(e) or atk<1000 then return end
-	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_SINGLE)
-	e1:SetCode(EFFECT_UPDATE_ATTACK)
-	e1:SetValue(-1000)
-	e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
-	c:RegisterEffect(e1)
-	if not c:IsHasEffect(EFFECT_REVERSE_UPDATE) then
-		local e2=Effect.CreateEffect(c)
-		e2:SetType(EFFECT_TYPE_SINGLE)
-		e2:SetCode(EFFECT_DIRECT_ATTACK)
-		e2:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
-		c:RegisterEffect(e2)
-	end
+function c44509529.spcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetTurnPlayer()~=tp
 end
-function c18514525.excostfilter(c,tp)
+function c44509529.excostfilter(c,tp)
 	return (c:IsFaceup() or c:IsLocation(LOCATION_GRAVE)) and c:IsAbleToRemoveAsCost() and c:IsHasEffect(101102049,tp)
 end
-function c18514525.costfilter(c,tp,g)
+function c44509529.costfilter(c,tp,g)
 	local tg=g:Clone()
 	tg:RemoveCard(c)
 	return Duel.GetMZoneCount(tp,c)>1 and tg:GetClassCount(Card.GetCode)>=2
 end
-function c18514525.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+function c44509529.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	e:SetLabel(0)
-	local g=Duel.GetMatchingGroup(c18514525.excostfilter,tp,LOCATION_MZONE+LOCATION_GRAVE,0,nil,tp)
-	local tg=Duel.GetMatchingGroup(c18514525.spfilter,tp,LOCATION_GRAVE,0,nil,e,tp)
+	local g=Duel.GetMatchingGroup(c44509529.excostfilter,tp,LOCATION_MZONE+LOCATION_GRAVE,0,nil,tp)
+	local tg=Duel.GetMatchingGroup(c44509529.spfilter,tp,LOCATION_GRAVE,0,nil,e,tp)
 	if e:GetHandler():IsReleasable() then g:AddCard(e:GetHandler()) end
 	if chk==0 then
 		e:SetLabel(100)
-		return not Duel.IsPlayerAffectedByEffect(tp,59822133) and g:IsExists(c18514525.costfilter,1,nil,tp,tg)
+		return not Duel.IsPlayerAffectedByEffect(tp,59822133) and g:IsExists(c44509529.costfilter,1,nil,tp,tg)
 	end
-	local cg=g:Filter(c18514525.costfilter,nil,tp,tg)
+	local cg=g:Filter(c44509529.costfilter,nil,tp,tg)
 	local tc
 	if #cg>1 then
 		Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(101102049,0))
@@ -81,21 +69,21 @@ function c18514525.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
 		Duel.Release(tc,REASON_COST)
 	end
 end
-function c18514525.spfilter(c,e,tp)
+function c44509529.spfilter(c,e,tp)
 	return c:IsSetCard(0x120) and not c:IsType(TYPE_FUSION)
 		and c:IsCanBeEffectTarget(e) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
-function c18514525.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+function c44509529.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return false end
 	if chk==0 then return e:GetLabel()==100 end
 	e:SetLabel(0)
-	local g=Duel.GetMatchingGroup(c18514525.spfilter,tp,LOCATION_GRAVE,0,nil,e,tp)
+	local g=Duel.GetMatchingGroup(c44509529.spfilter,tp,LOCATION_GRAVE,0,nil,e,tp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 	local g1=g:SelectSubGroup(tp,aux.dncheck,false,2,2)
 	Duel.SetTargetCard(g1)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g1,2,0,0)
 end
-function c18514525.spop(e,tp,eg,ep,ev,re,r,rp)
+function c44509529.spop(e,tp,eg,ep,ev,re,r,rp)
 	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
 	local tg=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS)
 	local g=tg:Filter(Card.IsRelateToEffect,nil,e)
@@ -109,7 +97,8 @@ function c18514525.spop(e,tp,eg,ep,ev,re,r,rp)
 		if Duel.SpecialSummonStep(tc,0,tp,tp,false,false,POS_FACEUP) then
 			local e1=Effect.CreateEffect(e:GetHandler())
 			e1:SetType(EFFECT_TYPE_SINGLE)
-			e1:SetCode(EFFECT_CANNOT_ATTACK)
+			e1:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+			e1:SetValue(1)
 			e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
 			tc:RegisterEffect(e1)
 		end
