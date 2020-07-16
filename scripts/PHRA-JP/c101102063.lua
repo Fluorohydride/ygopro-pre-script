c526ea73d428510def323c6e48668bae60d53407
mercury233 <me@mercury233.me>
2020-07-16 10:44:34 +0800

    add scripts PHRA

---
+++ scripts/PHRA-JP/c101102063.lua
@@ -0,0 +1,35 @@
+--暗黒界の援軍
+--
+--Script by mercury233
+function c101102063.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_HANDES)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCountLimit(1,101102063+EFFECT_COUNT_CODE_OATH)
+	e1:SetTarget(c101102063.target)
+	e1:SetOperation(c101102063.activate)
+	c:RegisterEffect(e1)
+end
+function c101102063.filter(c,e,tp)
+	return c:IsLevelBelow(4) and c:IsRace(RACE_FIEND) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function c101102063.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_GRAVE) and c101102063.filter(chkc,e,tp) end
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingTarget(c101102063.filter,tp,LOCATION_GRAVE,0,1,nil,e,tp)
+		and Duel.GetMatchingGroupCount(Card.IsRace,tp,LOCATION_HAND,0,nil,RACE_FIEND)>0 end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectTarget(tp,c101102063.filter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_HANDES,nil,1,tp,1)
+end
+function c101102063.activate(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) and Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)>0 then
+		Duel.BreakEffect()
+		Duel.DiscardHand(tp,Card.IsRace,1,1,REASON_EFFECT+REASON_DISCARD,nil,RACE_FIEND)
+	end
+end
