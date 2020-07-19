3d16858b79372252c1dd20dc53619d78652aa63b
mallu11 <54900811+mallu11@users.noreply.github.com>
2020-07-19 16:38:44 +0800

    fix 電脳堺悟－老々

--- scripts/PHRA-JP/c101102011.lua
+++ scripts/PHRA-JP/c101102011.lua
@@ -5,7 +5,7 @@ function c101102011.initial_effect(c)
 	--spsummon
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(101102011,0))
-	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOGRAVE+CATEGORY_DECKDES)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOGRAVE+CATEGORY_GRAVE_ACTION)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetRange(LOCATION_HAND)
@@ -22,7 +22,7 @@ function c101102011.tgfilter(c,type1)
 	return not c:IsType(type1) and c:IsSetCard(0x24e) and c:IsAbleToGrave()
 end
 function c101102011.spfilter(c,e,tp,code1)
-	return c:IsSetCard(0x24e) and not c:IsCode(code1) and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP)
+	return c:IsSetCard(0x24e) and not c:IsCode(code1) and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP_DEFENSE)
 end
 function c101102011.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsOnField() and chkc:IsControler(tp) and c101102011.tfilter(chkc,tp) end
@@ -32,7 +32,6 @@ function c101102011.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
 	local g=Duel.SelectTarget(tp,c101102011.tfilter,tp,LOCATION_ONFIELD,0,1,1,nil,tp)
 	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_DECK)
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
 end
 function c101102011.spop(e,tp,eg,ep,ev,re,r,rp)
@@ -42,33 +41,28 @@ function c101102011.spop(e,tp,eg,ep,ev,re,r,rp)
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
 		local g=Duel.SelectMatchingCard(tp,c101102011.tgfilter,tp,LOCATION_DECK,0,1,1,nil,type1)
 		local tgc=g:GetFirst()
-		local code1=tgc:GetCode()
-		if tgc then
-			Duel.SendtoGrave(tgc,REASON_EFFECT)
-		end
-		if c:IsRelateToEffect(e) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 then
-			if Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)~=0 then
-				local sg=Duel.GetMatchingGroup(aux.NecroValleyFilter(c101102011.spfilter),tp,LOCATION_GRAVE,0,nil,e,tp,code1)
-				if #sg>0 and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.SelectYesNo(tp,aux.Stringid(101102011,1)) then
-					Duel.BreakEffect()
-					Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-					local g=sg:Select(tp,1,1,nil)
-					local tc=g:GetFirst()
-					if tc then
-						Duel.SpecialSummonStep(tc,0,tp,tp,false,false,POS_FACEUP)
-						local e1=Effect.CreateEffect(c)
-						e1:SetType(EFFECT_TYPE_SINGLE)
-						e1:SetCode(EFFECT_DISABLE)
-						e1:SetReset(RESET_EVENT+RESETS_STANDARD)
-						tc:RegisterEffect(e1)
-						local e2=Effect.CreateEffect(c)
-						e2:SetType(EFFECT_TYPE_SINGLE)
-						e2:SetCode(EFFECT_DISABLE_EFFECT)
-						e2:SetReset(RESET_EVENT+RESETS_STANDARD)
-						tc:RegisterEffect(e2)
-						Duel.SpecialSummonComplete()
-					end
-				end
+		if tgc and Duel.SendtoGrave(tgc,REASON_EFFECT)~=0 and tgc:IsLocation(LOCATION_GRAVE) and c:IsRelateToEffect(e)
+			and Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)~=0 then
+			local code1=tgc:GetCode()
+			local sg=Duel.GetMatchingGroup(aux.NecroValleyFilter(c101102011.spfilter),tp,LOCATION_GRAVE,0,nil,e,tp,code1)
+			if #sg>0 and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.SelectYesNo(tp,aux.Stringid(101102011,1)) then
+				Duel.BreakEffect()
+				Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+				local sc=sg:Select(tp,1,1,nil):GetFirst()
+				Duel.SpecialSummonStep(sc,0,tp,tp,false,false,POS_FACEUP_DEFENSE)
+				local e1=Effect.CreateEffect(c)
+				e1:SetType(EFFECT_TYPE_SINGLE)
+				e1:SetCode(EFFECT_DISABLE)
+				e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+				e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+				sc:RegisterEffect(e1)
+				local e2=Effect.CreateEffect(c)
+				e2:SetType(EFFECT_TYPE_SINGLE)
+				e2:SetCode(EFFECT_DISABLE_EFFECT)
+				e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+				e2:SetReset(RESET_EVENT+RESETS_STANDARD)
+				sc:RegisterEffect(e2)
+				Duel.SpecialSummonComplete()
 			end
 		end
 	end
