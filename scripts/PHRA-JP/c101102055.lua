84bd08f7114a5c062e70fbf37e86adb369715ee8
mallu11 <54900811+mallu11@users.noreply.github.com>
2020-07-19 17:30:34 +0800

    fix 電脳堺都－九竜

--- scripts/PHRA-JP/c101102055.lua
+++ scripts/PHRA-JP/c101102055.lua
@@ -26,7 +26,7 @@ function c101102055.exfilter1(c)
 	return c:IsFacedown() and c:IsType(TYPE_XYZ+TYPE_SYNCHRO+TYPE_FUSION)
 end
 function c101102055.exfilter2(c)
-	return c:IsFaceup() and c:IsType(TYPE_PENDULUM+TYPE_LINK)
+	return c:IsFaceup() and c:IsType(TYPE_PENDULUM) or c:IsFacedown() and c:IsType(TYPE_LINK)
 end
 function c101102055.fselect(g,ft1,ft2,ect,ft)
 	return aux.dncheck(g) and #g<=ft and #g<=ect
@@ -35,12 +35,11 @@ function c101102055.fselect(g,ft1,ft2,ect,ft)
 end
 function c101102055.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then
-		local ft=1
-		if e:GetHandler():IsLocation(LOCATION_SZONE) then ft=0 end
-		return Duel.GetLocationCount(tp,LOCATION_SZONE)>ft
-		and Duel.IsExistingMatchingCard(c101102055.tffilter,tp,LOCATION_DECK,0,1,nil,tp)
+		local ft=Duel.GetLocationCount(tp,LOCATION_SZONE)
+		if e:IsHasType(EFFECT_TYPE_ACTIVATE) and not e:GetHandler():IsLocation(LOCATION_SZONE) then ft=ft-1 end
+		return ft>0 and Duel.IsExistingMatchingCard(c101102055.tffilter,tp,LOCATION_DECK,0,1,nil,tp)
 	end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,0,tp,LOCATION_EXTRA)
 end
 function c101102055.activate(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(tp,LOCATION_SZONE)<=0 then return end
@@ -50,6 +49,7 @@ function c101102055.activate(e,tp,eg,ep,ev,re,r,rp)
 		if Duel.MoveToField(tc,tp,tp,LOCATION_SZONE,POS_FACEUP,true)~=0 then
 			local gc=Duel.GetMatchingGroupCount(c101102055.gtfilter,tp,LOCATION_ONFIELD,0,nil)
 			if gc>=2 and Duel.SelectYesNo(tp,aux.Stringid(101102055,1)) then
+				Duel.BreakEffect()
 				local e1=Effect.CreateEffect(e:GetHandler())
 				e1:SetType(EFFECT_TYPE_FIELD)
 				e1:SetCode(EFFECT_UPDATE_ATTACK)
