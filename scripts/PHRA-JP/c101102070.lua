9de7cc2333a8f143ea47b9ff20a985d59a8147b5
mercury233 <me@mercury233.me>
2020-07-23 08:26:24 +0800

    update 鉄獣の抗戦

--- scripts/PHRA-JP/c101102070.lua
+++ scripts/PHRA-JP/c101102070.lua
@@ -12,30 +12,46 @@ function c101102070.initial_effect(c)
 	e1:SetOperation(c101102070.activate)
 	c:RegisterEffect(e1)
 end
-function c101102070.spfilter1(c,e,tp)
-	return c:IsRace(RACE_BEAST+RACE_BEASTWARRIOR+RACE_WINDBEAST) and c:IsCanBeSpecialSummoned(e,0,tp,false,false) and (c:IsLocation(LOCATION_GRAVE) or c:IsFaceup())
+function c101102070.spfilter(c,e,tp)
+	return c:IsRace(RACE_BEAST+RACE_BEASTWARRIOR+RACE_WINDBEAST) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+		and (c:IsLocation(LOCATION_GRAVE) or c:IsFaceup())
 end
-function c101102070.fselect1(g,tp)
+function c101102070.fselect(g,tp)
 	return Duel.IsExistingMatchingCard(c101102070.lkfilter,tp,LOCATION_EXTRA,0,1,nil,g)
 end
 function c101102070.lkfilter(c,g)
 	return c:IsSetCard(0x24f) and c:IsLinkSummonable(g,nil,g:GetCount(),g:GetCount())
 end
+function c101102070.chkfilter(c,tp)
+	return c:IsType(TYPE_LINK) and c:IsSetCard(0x24f) and Duel.GetLocationCountFromEx(tp,tp,nil,c)>0
+end
 function c101102070.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
-	if ft>1 and Duel.IsPlayerAffectedByEffect(tp,59822133) then ft=1 end
-	local g=Duel.GetMatchingGroup(c101102070.spfilter1,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,nil,e,tp)
-	if chk==0 then return ft>0 and Duel.IsPlayerCanSpecialSummonCount(tp,2) and g:CheckSubGroup(c101102070.fselect1,1,ft,tp) end
+	if chk==0 then
+		if not Duel.IsPlayerCanSpecialSummonCount(tp,2) then return false end
+		local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+		if ft<=0 then return false end
+		if Duel.IsPlayerAffectedByEffect(tp,59822133) then ft=1 end
+		local cg=Duel.GetMatchingGroup(c101102070.chkfilter,tp,LOCATION_EXTRA,0,nil,tp)
+		if #cg==0 then return false end
+		local _,maxlink=cg:GetMaxGroup(Card.GetLink)
+		if maxlink>ft then maxlink=ft end
+		local g=Duel.GetMatchingGroup(c101102070.spfilter,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,nil,e,tp)
+		return g:CheckSubGroup(c101102070.fselect,1,maxlink,tp)
+	end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE+LOCATION_REMOVED)
 end
 
 function c101102070.activate(e,tp,eg,ep,ev,re,r,rp)
 	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
 	if ft>1 and Duel.IsPlayerAffectedByEffect(tp,59822133) then ft=1 end
-	local g=Duel.GetMatchingGroup(aux.NecroValleyFilter(c101102070.spfilter1),tp,LOCATION_GRAVE+LOCATION_REMOVED,0,nil,e,tp)
-	if ft>0 then
+	local g=Duel.GetMatchingGroup(aux.NecroValleyFilter(c101102070.spfilter),tp,LOCATION_GRAVE+LOCATION_REMOVED,0,nil,e,tp)
+	local cg=Duel.GetMatchingGroup(c101102070.chkfilter,tp,LOCATION_EXTRA,0,nil,tp)
+	local _,maxlink=cg:GetMaxGroup(Card.GetLink)
+	if ft>0 and maxlink then
+		if maxlink>ft then maxlink=ft end
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-		local sg=g:SelectSubGroup(tp,c101102070.fselect1,false,1,ft,tp)
+		local sg=g:SelectSubGroup(tp,c101102070.fselect,false,1,maxlink,tp)
+		if not sg then return end
 		local tc=sg:GetFirst()
 		while tc do
 			Duel.SpecialSummonStep(tc,0,tp,tp,false,false,POS_FACEUP)
@@ -55,7 +71,7 @@ function c101102070.activate(e,tp,eg,ep,ev,re,r,rp)
 		if og:GetCount()==sg:GetCount() and tg:GetCount()>0 then
 			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 			local rg=tg:Select(tp,1,1,nil)
-			Duel.LinkSummon(tp,rg:GetFirst(),og)
+			Duel.LinkSummon(tp,rg:GetFirst(),og,nil,#og,#og)
 		end
 	end
 end
