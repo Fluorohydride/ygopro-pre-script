1994d0a4d2629d78f5416ca5914e81aa8f898654
mallu11 <54900811+mallu11@users.noreply.github.com>
2020-08-11 18:32:19 +0800

    fix

--- scripts/DBGI-JP/c100415010.lua
+++ scripts/DBGI-JP/c100415010.lua
@@ -4,6 +4,7 @@
 function c100415010.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_EQUIP)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
