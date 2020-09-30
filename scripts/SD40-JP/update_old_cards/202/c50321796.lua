5efc22fdb802502c0cb7c3aa02e4e13b7c7c474a
Mercury233 <me@mercury233.me>
2020-09-30 22:15:44 +0800

    add scripts SD40 by mallu11 (#525)

---
+++ scripts/SD40-JP/update_old_cards/202/c50321796.lua
@@ -0,0 +1,59 @@
+--氷結界の龍 ブリューナク
+function c50321796.initial_effect(c)
+	--synchro summon
+	aux.AddSynchroProcedure(c,nil,aux.NonTuner(nil),1)
+	c:EnableReviveLimit()
+	--to hand
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(50321796,0))
+	e1:SetCategory(CATEGORY_TOHAND)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1,50321796)
+	e1:SetCost(c50321796.cost)
+	e1:SetTarget(c50321796.target)
+	e1:SetOperation(c50321796.operation)
+	c:RegisterEffect(e1)
+end
+function c50321796.costfilter(c,e,tp)
+	if c:IsLocation(LOCATION_HAND) then
+		return c:IsDiscardable() and c:IsAbleToGraveAsCost()
+	else
+		return e:GetHandler():IsSetCard(0x2f) and c:IsAbleToRemove() and c:IsHasEffect(100340202,tp)
+	end
+end
+function c50321796.fselect(g)
+	return g:FilterCount(Card.IsLocation,nil,LOCATION_GRAVE)<=1
+end
+function c50321796.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c50321796.costfilter,tp,LOCATION_HAND+LOCATION_GRAVE,0,1,nil,e,tp) end
+	local rt=Duel.GetTargetCount(Card.IsAbleToHand,tp,0,LOCATION_ONFIELD,nil)
+	local g=Duel.GetMatchingGroup(c50321796.costfilter,tp,LOCATION_HAND+LOCATION_GRAVE,0,nil,e,tp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DISCARD)
+	local cg=g:SelectSubGroup(tp,c50321796.fselect,false,1,rt)
+	e:SetLabel(cg:GetCount())
+	local tc=cg:Filter(Card.IsLocation,nil,LOCATION_GRAVE):GetFirst()
+	if tc then
+		local te=tc:IsHasEffect(100340202,tp)
+		te:UseCountLimit(tp)
+		Duel.Remove(tc,POS_FACEUP,REASON_EFFECT+REASON_REPLACE)
+		cg:RemoveCard(tc)
+	end
+	Duel.SendtoGrave(cg,REASON_COST+REASON_DISCARD)
+end
+function c50321796.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsOnField() and chkc:IsAbleToHand() end
+	if chk==0 then return Duel.IsExistingTarget(Card.IsAbleToHand,tp,0,LOCATION_ONFIELD,1,nil) end
+	local ct=e:GetLabel()
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RTOHAND)
+	local tg=Duel.SelectTarget(tp,Card.IsAbleToHand,tp,0,LOCATION_ONFIELD,ct,ct,nil)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,tg,ct,0,0)
+end
+function c50321796.operation(e,tp,eg,ep,ev,re,r,rp,chk)
+	local tg=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS)
+	local rg=tg:Filter(Card.IsRelateToEffect,nil,e)
+	if rg:GetCount()>0 then 
+		Duel.SendtoHand(rg,nil,REASON_EFFECT)
+	end
+end
---
+++ scripts/SD40-JP/update_old_cards/202/c73061465.lua
@@ -0,0 +1,48 @@
+--氷結界の封魔団
+function c73061465.initial_effect(c)
+	--act limit
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(73061465,0))
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetCost(c73061465.cost)
+	e1:SetOperation(c73061465.operation)
+	c:RegisterEffect(e1)
+end
+function c73061465.cfilter(c,e,tp)
+	if c:IsLocation(LOCATION_HAND) then
+		return c:IsSetCard(0x2f) and c:IsType(TYPE_MONSTER) and c:IsAbleToGraveAsCost()
+	else
+		return e:GetHandler():IsSetCard(0x2f) and c:IsAbleToRemove() and c:IsHasEffect(100340202,tp)
+	end
+end
+function c73061465.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c73061465.cfilter,tp,LOCATION_HAND+LOCATION_GRAVE,0,1,nil,e,tp) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local g=Duel.SelectMatchingCard(tp,c73061465.cfilter,tp,LOCATION_HAND+LOCATION_GRAVE,0,1,1,nil,e,tp)
+	local tc=g:GetFirst()
+	local te=tc:IsHasEffect(100340202,tp)
+	if te then
+		te:UseCountLimit(tp)
+		Duel.Remove(tc,POS_FACEUP,REASON_EFFECT+REASON_REPLACE)
+	else
+		Duel.SendtoGrave(tc,REASON_COST)
+	end
+end
+function c73061465.operation(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsFacedown() or not c:IsRelateToEffect(e) then return end
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_CANNOT_ACTIVATE)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetTargetRange(1,1)
+	e1:SetValue(c73061465.tgval)
+	e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_DISABLE+RESET_PHASE+PHASE_END,3)
+	c:RegisterEffect(e1)
+end
+function c73061465.tgval(e,re,rp)
+	return re:IsActiveType(TYPE_SPELL) and re:IsHasType(EFFECT_TYPE_ACTIVATE)
+end
