68f7f79d459131a567e18ed7657043b7c2bd9ee6
Xylen09 <69278311+Xylen09@users.noreply.github.com>
2020-10-13 11:00:44 +0800

    Create c101103049.lua

---
+++ scripts/BLVO-JP/c101103049.lua
@@ -0,0 +1,193 @@
+--ヘヴィメタルフォシス アマルガム
+
+--Scripted by Xylen09
+
+--Heavymetalphosis Amalgam
+
+function c101103049.initial_effect(c)
+
+	--(0) Link Summon	aux.AddLinkProcedure(c,aux.FilterBoolFunction(Card.IsSetCard,0xe1),2,2)
+
+	c:EnableReviveLimit()
+
+	--(1) Equip
+
+	local e1=Effect.CreateEffect(c)
+
+	e1:SetDescription(aux.Stringid(101103049,0))
+
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+
+	e1:SetCode(EVENT_DESTROYED)
+
+	e1:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
+
+	e1:SetCountLimit(1,101103049)
+
+	e1:SetCondition(c101103049.eqcon)
+
+	e1:SetTarget(c101103049.eqtg)
+
+	e1:SetOperation(c101103049.eqop)
+
+	c:RegisterEffect(e1)
+
+	--(2) Destroy and Special Summon
+
+	local e2=Effect.CreateEffect(c)
+
+	e2:SetDescription(aux.Stringid(101103049,1))
+
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+
+	e2:SetProperty(EFFECT_FLAG_DELAY)
+
+	e2:SetCode(EVENT_DESTROYED)
+
+	e2:SetCountLimit(1,101103049)
+
+	e2:SetCondition(c101103049.spcon)
+
+	e2:SetTarget(c101103049.sptg)
+
+	e2:SetOperation(c101103049.spop)
+
+	c:RegisterEffect(e2)
+
+end
+
+function c101103049.eqcon(e,tp,eg,ep,ev,re,r,rp)
+
+	local c=e:GetHandler()
+
+	return c:IsPreviousLocation(LOCATION_MZONE) and c:IsFaceup()
+
+end
+
+function c101103049.eqfilter(c)
+
+	return c:IsFaceup() and c:IsSetCard(0xe1)
+
+end
+
+function c101103049.eqtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and c101103049.filter(chkc) end
+
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_SZONE)>0
+
+		and Duel.IsExistingTarget(c101103049.filter,tp,LOCATION_MZONE,0,1,nil) end
+
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)
+
+	Duel.SelectTarget(tp,c101103049.filter,tp,LOCATION_MZONE,0,1,1,nil)
+
+end
+
+function c101103049.eqop(e,tp,eg,ep,ev,re,r,rp)
+
+	local c=e:GetHandler()
+
+	if not c:IsRelateToEffect(e) or c:IsFacedown() then return end
+
+	local tc=Duel.GetFirstTarget()
+
+	if Duel.GetLocationCount(tp,LOCATION_SZONE)<=0 or tc:GetControler()~=tp or tc:IsFacedown() or not tc:IsRelateToEffect(e) or not c:CheckUniqueOnField(tp) then
+
+		Duel.SendtoGrave(c,REASON_EFFECT)
+
+		return
+
+	end
+
+	Duel.Equip(tp,c,tc)
+
+	local e1=Effect.CreateEffect(c)
+
+	e1:SetType(EFFECT_TYPE_SINGLE)
+
+	e1:SetCode(EFFECT_EQUIP_LIMIT)
+
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+
+	e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+
+	e1:SetValue(c101103049.eqlimit)
+
+	e1:SetLabelObject(tc)
+
+	c:RegisterEffect(e1)
+
+	local e2=Effect.CreateEffect(c)
+
+	e2:SetType(EFFECT_TYPE_EQUIP)
+
+	e2:SetCode(EFFECT_UPDATE_ATTACK)
+
+	e2:SetValue(1000)
+
+	e2:SetReset(RESET_EVENT+RESETS_STANDARD)
+
+	c:RegisterEffect(e2)
+
+end
+
+function c101103049.eqlimit(e,c)
+
+	return c==e:GetLabelObject()
+
+end
+
+function c101103049.spcon(e,tp,eg,ep,ev,re,r,rp)
+
+	local c=e:GetHandler()
+
+	return c:IsPreviousLocation(LOCATION_MZONE) and c:IsFaceup()
+
+end
+
+function c101103049.desfilter(c)
+
+	return c:IsFaceup() and c:IsSetCard(0xe1) and c:IsType(TYPE_MONSTER)
+
+end
+
+function c101103049.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+
+	local c=e:GetHandler()
+
+	if chkc then return chkc:IsOnField() and chkc:IsControler(tp) and chkc:IsFaceup() and chkc~=c end
+
+	if chk==0 then return Duel.IsExistingTarget(Card.IsFaceup,tp,LOCATION_ONFIELD,0,1,c)
+
+		and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
+
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+
+	local g=Duel.SelectTarget(tp,Card.IsFaceup,tp,LOCATION_ONFIELD,0,1,1,c)
+
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
+
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
+
+end
+
+function c101103049.spop(e,tp,eg,ep,ev,re,r,rp)
+
+	local tc=Duel.GetFirstTarget()
+
+	if tc:IsRelateToEffect(e) and Duel.Destroy(tc,REASON_EFFECT)~=0 then
+
+		local c=e:GetHandler()
+
+		if c:IsRelateToEffect(e) then
+
+			Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
+
+		end
+
+	end
+
+end
