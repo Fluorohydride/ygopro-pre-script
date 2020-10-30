02a991f940f95576dd0b5cdaff64d4ad8f033a67
Mercury233 <me@mercury233.me>
2020-10-30 00:31:56 +0000

    fix

--- scripts/BLVO-JP/c101103036.lua
+++ scripts/BLVO-JP/c101103036.lua
@@ -1,10 +1,12 @@
---Ejeffers1239
+--おジャマ・ピンク
 --Ojama Pink
-
-local e1=Effect.CreateEffect(c)
+--Ejeffers1239
+function c101103036.initial_effect(c)
+	--draw
+	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(101103036,1))
-	e1:SetCategory(CATEGORY_HANDS+CATEGORY_DRAW)
-	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O+)
+	e1:SetCategory(CATEGORY_HANDES+CATEGORY_DRAW)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
 	e1:SetCode(EVENT_TO_GRAVE)
 	e1:SetCountLimit(1,101103036)
@@ -25,22 +27,23 @@ function c101103036.activate(e,tp,eg,ep,ev,re,r,rp)
     local h1=Duel.Draw(tp,1,REASON_EFFECT)
     local h2=Duel.Draw(1-tp,1,REASON_EFFECT)
     if h1>0 or h2>0 then Duel.BreakEffect() end
+    local groundcollapse=false
     if h1>0 then
         Duel.ShuffleHand(tp)
-        if not Duel.DiscardHand(tp,aux.TRUE,1,1,REASON_EFFECT+REASON_DISCARD)
+        Duel.DiscardHand(tp,aux.TRUE,1,1,REASON_EFFECT+REASON_DISCARD)
 		local dc=Duel.GetOperatedGroup():GetFirst()
-			if dc:IsSetCard(0xf) then groundcollapse=true end
+		if dc:IsSetCard(0xf) then groundcollapse=true end
     end
     if h2>0 then 
         Duel.ShuffleHand(1-tp)
-        Duel.DiscardHand(1-tp,aux.TRUE,1,1,REASON_EFFECT+REASON_DISCARD,)
+        Duel.DiscardHand(1-tp,aux.TRUE,1,1,REASON_EFFECT+REASON_DISCARD)
     end
 	if groundcollapse and Duel.GetLocationCount(1-tp,LOCATION_MZONE)>0
-	and Duel.SelectYesNo(tp,aux.Stringid(101103036,0)) then
+		and Duel.SelectYesNo(tp,aux.Stringid(101103036,0)) then
 		Duel.BreakEffect()
 		local zone=Duel.SelectDisableField(tp,1,0,LOCATION_MZONE,0)
 		if tp==1 then
-				zone=((zone&0xffff)<<16)|((zone>>16)&0xffff)
+			zone=((zone&0xffff)<<16)|((zone>>16)&0xffff)
 		end
 		local e1=Effect.CreateEffect(e:GetHandler())
 		e1:SetType(EFFECT_TYPE_FIELD)
