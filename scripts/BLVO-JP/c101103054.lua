d6280523e3b815c01e783864944f96668608d35e
mallu11 <54900811+mallu11@users.noreply.github.com>
2020-10-30 10:06:40 +0800

    add スプリガンズ (#540)

---
+++ scripts/BLVO-JP/c101103054.lua
@@ -0,0 +1,50 @@
+--スプリガンズ・ウォッチ
+
+--Scripted by mallu11
+function c101103054.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH+CATEGORY_TOGRAVE)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCountLimit(1,101103054+EFFECT_COUNT_CODE_OATH)
+	e1:SetTarget(c101103054.target)
+	e1:SetOperation(c101103054.activate)
+	c:RegisterEffect(e1)
+end
+function c101103054.filter(c)
+	return c:IsCode(101103055) and c:IsAbleToHand()
+end
+function c101103054.thfilter(c,tp)
+	return c:IsSetCard(0x257) and c:IsType(TYPE_MONSTER) and c:IsAbleToHand() and Duel.IsExistingMatchingCard(c101103054.tgfilter,tp,LOCATION_DECK,0,1,c)
+end
+function c101103054.tgfilter(c)
+	return c:IsSetCard(0x257) and c:IsType(TYPE_MONSTER) and c:IsAbleToGrave()
+end
+function c101103054.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	local b=Duel.IsEnvironment(101103055,tp,LOCATION_FZONE)
+	if chk==0 then return Duel.IsExistingMatchingCard(c101103054.filter,tp,LOCATION_DECK,0,1,nil) or b and Duel.IsExistingMatchingCard(c101103054.thfilter,tp,LOCATION_DECK,0,1,nil,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,0,tp,LOCATION_DECK)
+end
+function c101103054.activate(e,tp,eg,ep,ev,re,r,rp)
+	local b=Duel.IsEnvironment(101103055,tp,LOCATION_FZONE)
+	if b and Duel.IsExistingMatchingCard(c101103054.thfilter,tp,LOCATION_DECK,0,1,nil,tp) and (not Duel.IsExistingMatchingCard(c101103054.filter,tp,LOCATION_DECK,0,1,nil) or Duel.SelectYesNo(tp,aux.Stringid(101103054,0))) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+		local g=Duel.SelectMatchingCard(tp,c101103054.thfilter,tp,LOCATION_DECK,0,1,1,nil,tp)
+		local tc=g:GetFirst()
+		if Duel.SendtoHand(tc,nil,REASON_EFFECT)~=0 and tc:IsLocation(LOCATION_HAND) then
+			Duel.ConfirmCards(1-tp,tc)
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+			local tg=Duel.SelectMatchingCard(tp,c101103054.tgfilter,tp,LOCATION_DECK,0,1,1,nil)
+			Duel.SendtoGrave(tg,REASON_EFFECT)
+		end
+	else
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+		local g=Duel.SelectMatchingCard(tp,c101103054.filter,tp,LOCATION_DECK,0,1,1,nil)
+		if g:GetCount()>0 then
+			Duel.SendtoHand(g,nil,REASON_EFFECT)
+			Duel.ConfirmCards(1-tp,g)
+		end
+	end
+end
