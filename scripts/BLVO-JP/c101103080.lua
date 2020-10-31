210a94ea8838065f9422b465d929defb63db5012
mercury233 <me@mercury233.me>
2020-10-31 16:20:52 +0800

    format

--- scripts/BLVO-JP/c101103080.lua
+++ scripts/BLVO-JP/c101103080.lua
@@ -1,36 +1,36 @@
 --壱時砲固定式
 --Stationary One-o'-Clock Linear Accelerator
 --Scripted by Gecko-chan
-function c70916046.initial_effect(c)
+function c101103080.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_DISABLE)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetCountLimit(1,70916046)
-	e1:SetTarget(c70916046.target)
-	e1:SetOperation(c70916046.activate)
+	e1:SetCountLimit(1,101103080)
+	e1:SetTarget(c101103080.target)
+	e1:SetOperation(c101103080.activate)
 	c:RegisterEffect(e1)
 end
-function c70916046.filter(c)
+function c101103080.filter(c)
 	return c:IsFaceup() and c:IsType(TYPE_EFFECT) and c:IsLevelAbove(1)
 end
-function c70916046.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) and c70916046.filter(chkc) end
-	if chk==0 then return Duel.IsExistingMatchingCard(c70916046.filter,tp,0,LOCATION_MZONE,1,nil) end
+function c101103080.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) and c101103080.filter(chkc) end
+	if chk==0 then return Duel.IsExistingMatchingCard(c101103080.filter,tp,0,LOCATION_MZONE,1,nil) end
 	local t={}
 	local i=1
 	for i=1,6 do t[i]=i end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_NUMBER)
 	e:SetLabel(Duel.AnnounceNumber(tp,table.unpack(t)))
 end
-function c70916046.activate(e,tp,eg,ep,ev,re,r,rp)
+function c101103080.activate(e,tp,eg,ep,ev,re,r,rp)
 	--Set up the equation
 	local dnum=e:GetLabel()
 	local fnum=Duel.GetFieldGroupCount(tp,0,LOCATION_ONFIELD)
 	local gnum=Duel.GetFieldGroupCount(tp,LOCATION_GRAVE,0)
-	local dg=Duel.GetMatchingGroup(c70916046.filter,tp,0,LOCATION_ONFIELD,nil)
+	local dg=Duel.GetMatchingGroup(c101103080.filter,tp,0,LOCATION_ONFIELD,nil)
 	local mon=dg:Select(tp,1,1,nil)
 	local lnum=mon:GetFirst():GetLevel()
 	--The equation
