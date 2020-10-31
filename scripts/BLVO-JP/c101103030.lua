7ec29941340da355dbf9c13156bac6ca1068dbfb
mallu11 <54900811+mallu11@users.noreply.github.com>
2020-10-31 20:01:17 +0800

    fix 妖精弓士イングナル

--- scripts/BLVO-JP/c101103030.lua
+++ scripts/BLVO-JP/c101103030.lua
@@ -36,7 +36,7 @@ function c101103030.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and c101103030.filter(chkc,e,tp) end
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.IsExistingTarget(c101103030.filter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectTarget(tp,aux.NecroValleyFilter(c101103030.filter),tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
+	local g=Duel.SelectTarget(tp,c101103030.filter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
 end
 function c101103030.spop(e,tp,eg,ep,ev,re,r,rp)
