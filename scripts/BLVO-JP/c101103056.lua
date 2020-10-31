af80e7464664b81dc7f9bd1662273266f9f33a7e
mallu11 <54900811+mallu11@users.noreply.github.com>
2020-10-31 21:08:42 +0800

    fix 鉄獣の邂逅

--- scripts/BLVO-JP/c101103056.lua
+++ scripts/BLVO-JP/c101103056.lua
@@ -8,6 +8,7 @@ function c101103056.initial_effect(c)
 	e1:SetCategory(CATEGORY_ATKCHANGE)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetCountLimit(1,101103056)
 	e1:SetTarget(c101103056.target)
 	e1:SetOperation(c101103056.activate)
@@ -18,22 +19,26 @@ function c101103056.initial_effect(c)
 	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 	e2:SetCode(EFFECT_DESTROY_REPLACE)
 	e2:SetRange(LOCATION_GRAVE)
-	e2:SetCountLimit(1,101103057+100)
+	e2:SetCountLimit(1,101103056+100)
 	e2:SetValue(c101103056.replaceval)
 	e2:SetTarget(c101103056.replacetg)
 	e2:SetOperation(c101103056.replaceop)
 	c:RegisterEffect(e2)
 end
 function c101103056.atkfilter(c)
-	return c:IsRace(RACE_BEAST+RACE_BEASTWARRIOR+RACE_WINDBEAST) and c:IsType(TYPE_LINK) and c:IsLinkState()
+	return c:IsFaceup() and c:IsRace(RACE_BEAST+RACE_BEASTWARRIOR+RACE_WINDBEAST) and c:IsLinkState()
+end
+function c101103056.atkfilter2(c,e)
+	return c101103056.atkfilter(c) and c:IsCanBeEffectTarget(e)
 end
 function c101103056.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsController(tp) and c101103056.atkfilter(chkc) end
-	if chk==0 then return Duel.IsExistingTarget(c101103056.atkfilter,tp,LOCATION_MZONE,0,1,e:GetHandler()) end
-	local g=Duel.SelectTarget(tp,c101103056.atkfilter,tp,LOCATION_MZONE,0,1,99,e:GetHandler())
+	local g=Duel.GetMatchingGroup(c101103056.atkfilter2,tp,LOCATION_MZONE,0,nil,e)
+	if chk==0 then return #g>0 end
+	local ag=Duel.SelectTarget(tp,c101103056.atkfilter,tp,LOCATION_MZONE,0,1,#g,nil)
 end
 function c101103056.activate(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS):Filter(Card.IsRelateToEffect,nil,e)
+	local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS):Filter(Card.IsRelateToEffect,nil,e):Filter(Card.IsFaceup,nil)
 	local tc=g:GetFirst()
 	while tc do
 		local e1=Effect.CreateEffect(e:GetHandler())
@@ -46,8 +51,8 @@ function c101103056.activate(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 function c101103056.replaceft(c,tp)
-	return c:IsFaceup() and c:IsRace(RACE_BEAST+RACE_BEASTWARRIOR+RACE_WINDBEAST) and c:IsType(TYPE_LINK) and c:IsLinkState()
-		and c:IsOnField() and c:IsControler(tp) and c:IsReason(REASON_EFFECT+REASON_BATTLE) and not c:IsReason(REASON_REPLACE)
+	return c:IsFaceup() and c:IsRace(RACE_BEAST+RACE_BEASTWARRIOR+RACE_WINDBEAST) and c:IsLinkState()
+		and c:IsLocation(LOCATION_MZONE) and c:IsControler(tp) and c:IsReason(REASON_EFFECT+REASON_BATTLE) and not c:IsReason(REASON_REPLACE)
 end
 function c101103056.replacetg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return e:GetHandler():IsAbleToRemove() and eg:IsExists(c101103056.replaceft,1,nil,tp) end
@@ -57,5 +62,5 @@ function c101103056.replaceval(e,c)
 	return c101103056.replaceft(c,e:GetHandlerPlayer())
 end
 function c101103056.replaceop(e,tp,eg,ep,ev,re,r,rp)
-	Duel.Remove(e:GetHandler(),POS_FACEUP,REASON_EFFECT)
+	Duel.Remove(e:GetHandler(),POS_FACEUP,REASON_EFFECT+REASON_REPLACE)
 end
