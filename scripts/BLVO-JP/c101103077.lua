b7960c244ed13d13151b2690714e355d895f54fb
mercury233 <me@mercury233.me>
2020-11-02 21:54:33 +0800

    more

--- scripts/BLVO-JP/c101103077.lua
+++ scripts/BLVO-JP/c101103077.lua
@@ -2,7 +2,7 @@
 --
 --Script by Real_Scl
 function c101103077.initial_effect(c)
-	aux.AddCodeList(74665651,1050355)
+	aux.AddCodeList(c,74665651,1050355)
 	--Activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
