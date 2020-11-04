7d65a663bcd1611169c2ca9a17421438bba0829f
mallu11 <54900811+mallu11@users.noreply.github.com>
2020-11-04 21:03:50 +0800

    fix 鎧竜の聖騎士

--- scripts/BLVO-JP/c101103037.lua
+++ scripts/BLVO-JP/c101103037.lua
@@ -20,8 +20,8 @@ function c101103037.initial_effect(c)
 	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e2:SetType(EFFECT_TYPE_IGNITION)
 	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1,101103137)
 	e2:SetCost(c101103037.cost)
-	e2:SetCountLimit(1,101103037)
 	e2:SetTarget(c101103037.target)
 	e2:SetOperation(c101103037.activate)
 	c:RegisterEffect(e2)
@@ -34,7 +34,7 @@ end
 function c101103037.tdtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
 	local bc=c:GetBattleTarget()
-	if chk==0 then return bc:IsAbleToDeck() end
+	if chk==0 then return bc and bc:IsRelateToBattle() and bc:IsAbleToDeck() end
 	Duel.SetOperationInfo(0,CATEGORY_TODECK,bc,1,0,0)
 end
 function c101103037.tdop(e,tp,eg,ep,ev,re,r,rp)
@@ -48,10 +48,10 @@ function c101103037.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.Release(e:GetHandler(),REASON_COST)
 end
 function c101103037.filter(c,e,tp)
-	return c:IsRace(RACE_DRAGON) and c:GetLevel()>=5 and c:IsAttribute(ATTRIBUTE_WIND) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+	return c:IsRace(RACE_DRAGON) and c:IsLevelAbove(5) and c:IsAttribute(ATTRIBUTE_WIND) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function c101103037.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(c101103037.filter,tp,LOCATION_HAND+LOCATION_DECK,0,1,nil,e,tp) end
+	if chk==0 then return Duel.GetMZoneCount(tp,e:GetHandler())>0 and Duel.IsExistingMatchingCard(c101103037.filter,tp,LOCATION_HAND+LOCATION_DECK,0,1,nil,e,tp) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND+LOCATION_DECK)
 end
 function c101103037.activate(e,tp,eg,ep,ev,re,r,rp)
@@ -60,6 +60,5 @@ function c101103037.activate(e,tp,eg,ep,ev,re,r,rp)
 	local g=Duel.SelectMatchingCard(tp,c101103037.filter,tp,LOCATION_HAND+LOCATION_DECK,0,1,1,nil,e,tp)
 	if g:GetCount()>0 then
 		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
-		g:GetFirst():CompleteProcedure()
 	end
 end
