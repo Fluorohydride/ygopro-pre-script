8b20ce3eef967df10d0591299e1b569b41979c51
mallu11 <54900811+mallu11@users.noreply.github.com>
2020-11-04 19:41:26 +0800

    fix 電子光虫－レジストライダー

--- scripts/BLVO-JP/c101103023.lua
+++ scripts/BLVO-JP/c101103023.lua
@@ -12,7 +12,7 @@ function c101103023.initial_effect(c)
 	--スペシャル召喚と両方のモンスターは、そのレベルを増加させます
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(101103023,0))
-	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_LVCHANGE)
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
 	e1:SetRange(LOCATION_HAND)
 	e1:SetCode(EVENT_SUMMON_SUCCESS)
@@ -26,6 +26,8 @@ function c101103023.initial_effect(c)
 	e2:SetCategory(CATEGORY_POSITION)
 	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e2:SetProperty(EFFECT_FLAG_DELAY)
+	e2:SetCondition(c101103023.poscon)
 	e2:SetTarget(c101103023.postg)
 	e2:SetOperation(c101103023.posop)
 	c:RegisterEffect(e2)
@@ -46,24 +48,27 @@ end
 --スペシャル召喚
 function c101103023.spcon(e,tp,eg,ep,ev,re,r,rp)
 	local ec=eg:GetFirst()
-	return ep==tp and ec:GetLevel()==3 and ec:IsRace(RACE_INSECT)
+	return ep==tp and ec:IsLevel(3) and ec:IsRace(RACE_INSECT)
 end
 function c101103023.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,true,false) end
+		and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
 end
 function c101103023.spop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=eg:GetFirst()
 	local c=e:GetHandler()
 	if not c:IsRelateToEffect(e) then return end
-	if Duel.SpecialSummon(c,0,tp,tp,true,false,POS_FACEUP)~=0 and Duel.SelectYesNo(tp,aux.Stringid(101103023,2)) then
-		local g=Group.FromCards(c,tc):Filter(Card.IsFaceup,nil,nil)
-		local lv=Duel.AnnounceLevel(tp,5,7,6)
+	if Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)~=0 and Duel.SelectYesNo(tp,aux.Stringid(101103023,2)) then
+		local g=Group.FromCards(c)
+		if tc:IsRelateToEffect(e) then g:AddCard(tc) end
+		g=g:Filter(Card.IsFaceup,nil)
+		local lv=Duel.AnnounceNumber(tp,5,7)
 		for oc in aux.Next(g) do
 			local e1=Effect.CreateEffect(c)
 			e1:SetType(EFFECT_TYPE_SINGLE)
 			e1:SetCode(EFFECT_CHANGE_LEVEL)
+			e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
 			e1:SetValue(lv)
 			e1:SetReset(RESET_EVENT+RESETS_STANDARD)
 			oc:RegisterEffect(e1)
@@ -71,19 +76,23 @@ function c101103023.spop(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 --戦闘位置の変更
+function c101103023.poscon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsPreviousLocation(LOCATION_HAND)
+end
 function c101103023.posfilter(c)
-	return c:IsRace(RACE_INSECT) and c:IsFaceup()
+	return c:IsRace(RACE_INSECT) and c:IsFaceup() and c:IsCanChangePosition()
 end
 function c101103023.postg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(c101103023.spfilter,tp,LOCATION_MZONE,0,1,nil) end
-	Duel.SetOperationInfo(0,CATEGORY_POSITION,nil,1,0,0)
+	if chk==0 then return Duel.IsExistingMatchingCard(c101103023.posfilter,tp,LOCATION_MZONE,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_POSITION,nil,1,tp,LOCATION_MZONE)
 end
 function c101103023.posop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_POSCHANGE)
 	local g=Duel.SelectMatchingCard(tp,c101103023.posfilter,tp,LOCATION_MZONE,0,1,1,nil)
 	local tc=g:GetFirst()
 	if tc then
-		Duel.ChangePosition(tc,POS_FACEUP_DEFENSE,0,POS_FACEUP_ATTACK,0)
+		Duel.HintSelection(g)
+		Duel.ChangePosition(tc,POS_FACEUP_DEFENSE,POS_FACEDOWN_DEFENSE,POS_FACEUP_ATTACK,POS_FACEUP_ATTACK)
 	end
 end
 --ゲイン効果
@@ -93,15 +102,15 @@ end
 function c101103023.efop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local rc=c:GetReasonCard()
-	local e1=Effect.CreateEffect(c)
+	local e1=Effect.CreateEffect(rc)
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetCode(EFFECT_UPDATE_ATTACK)
 	e1:SetValue(1000)
-	e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_DISABLE)
-	rc:RegisterEffect(e1)
+	e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+	rc:RegisterEffect(e1,true)
 	local e2=e1:Clone()
 	e2:SetCode(EFFECT_UPDATE_DEFENSE)
-	rc:RegisterEffect(e2)
+	rc:RegisterEffect(e2,true)
 	if not rc:IsType(TYPE_EFFECT) then
 		local e3=Effect.CreateEffect(c)
 		e3:SetType(EFFECT_TYPE_SINGLE)
@@ -110,4 +119,5 @@ function c101103023.efop(e,tp,eg,ep,ev,re,r,rp)
 		e3:SetReset(RESET_EVENT+RESETS_STANDARD)
 		rc:RegisterEffect(e3,true)
 	end
+	rc:RegisterFlagEffect(0,RESET_EVENT+RESETS_STANDARD,EFFECT_FLAG_CLIENT_HINT,1,0,aux.Stringid(101103023,3))
 end
