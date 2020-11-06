db3bab77cb2ed2f083353985c7126cb1bdafe8bc
KohanaSonogami <73727154+KohanaSonogami@users.noreply.github.com>
2020-11-06 21:45:26 +0900

    format 2

--- scripts/BLVO-JP/c100200191.lua
+++ scripts/BLVO-JP/c100200191.lua
@@ -32,12 +32,12 @@ end
 function c100200191.spspfilter1(c,e,tp)
 	return c:IsSetCard(0x2f) and c:IsType(TYPE_MONSTER) and c:IsLevelAbove(3) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
-function c100200191.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+function c100200191.sptg1(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>-1
 		and Duel.IsExistingMatchingCard(c100200191.spfilter1,tp,LOCATION_HAND,0,1,nil,e,tp) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
 end
-function c100200191.spop(e,tp,eg,ep,ev,re,r,rp)
+function c100200191.spop1(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 	local g=Duel.SelectMatchingCard(tp,c100200191.spfilter1,tp,LOCATION_HAND,0,1,1,nil,e,tp)
