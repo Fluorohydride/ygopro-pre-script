dfc272ece98773548e53d3f5bff956ceb8b520e5
mercury233 <me@mercury233.me>
2020-11-06 09:40:53 +0800

    fix Myutant GB-88

--- scripts/PHRA-EN/c101102086.lua
+++ scripts/PHRA-EN/c101102086.lua
@@ -10,6 +10,7 @@ function c101102086.initial_effect(c)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetRange(LOCATION_HAND)
 	e1:SetCountLimit(1,101102086)
+	e1:SetHintTiming(0,TIMINGS_CHECK_MONSTER+TIMING_MAIN_END)
 	e1:SetCondition(c101102086.spcon)
 	e1:SetTarget(c101102086.sptg)
 	e1:SetOperation(c101102086.spop)
@@ -22,7 +23,8 @@ function c101102086.initial_effect(c)
 	e2:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
 	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
 	e2:SetCountLimit(1,101102086+100)
-	e2:SetTarget(c101102086.sp2cost)
+	e2:SetCondition(c101102086.sp2con)
+	e2:SetCost(c101102086.sp2cost)
 	e2:SetTarget(c101102086.sp2tg)
 	e2:SetOperation(c101102086.sp2op)
 	c:RegisterEffect(e2)
@@ -42,10 +44,13 @@ function c101102086.spop(e,tp,eg,ep,ev,re,r,rp)
 	if not c:IsRelateToEffect(e) then return end
 	Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
 end
+function c101102086.sp2con(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetTurnPlayer()~=tp
+end
 function c101102086.sp2costfilter(c)
 	return c:IsAbleToRemoveAsCost() and (c:IsFaceup() or c:IsLocation(LOCATION_HAND))
 end
-function c101102086.sp2cost(e,tp,eg,ep,ev,re,r,rp)
+function c101102086.sp2cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
 	if chk==0 then return c:IsReleasable()
 		and Duel.IsExistingMatchingCard(c101102086.sp2costfilter,tp,LOCATION_HAND+LOCATION_ONFIELD,0,1,c) end
@@ -64,9 +69,6 @@ function c101102086.sp2tg(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE+LOCATION_REMOVED)
 end
 function c101102086.sp2op(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	if not c:IsRelateToEffect(e) then return end
-	Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 	local tc=Duel.SelectMatchingCard(tp,c101102086.sp2tgfilter,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,1,1,nil,e,tp)
 	if tc then Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP) end
