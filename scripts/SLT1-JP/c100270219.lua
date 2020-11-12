312139e7694850ba0f121f364d9d56d4563c05a1
KohanaSonogami <73727154+KohanaSonogami@users.noreply.github.com>
2020-11-12 13:56:20 +0900

    c100270219.lua

--- scripts/SLT1-JP/c100270219.lua
+++ scripts/SLT1-JP/c100270219.lua
@@ -1,45 +1,45 @@
---ヘリオポリスのヒエラティックヘブンリードラゴンオーバーロード
+--ヒエラティックヘブンリードラゴンヘリオポリスの領主
 --LUA by Kohana Sonogami
 --
-function c1002702008.initial_effect(c)
+function c100270219.initial_effect(c)
 	c:EnableReviveLimit()
 	aux.AddXyzProcedure(c,nil,8,2)
 	--negate
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(1002702008,0))
+	e1:SetDescription(aux.Stringid(100270219,0))
 	e1:SetCategory(CATEGORY_NEGATE+CATEGORY_DESTROY)
 	e1:SetType(EFFECT_TYPE_QUICK_O)
 	e1:SetCode(EVENT_CHAINING)
 	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DAMAGE_CAL)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetCountLimit(1)
-	e1:SetCondition(c1002702008.discon)
-	e1:SetCost(c1002702008.discost)
-	e1:SetTarget(c1002702008.distg)
-	e1:SetOperation(c1002702008.disop)
+	e1:SetCondition(c100270219.discon)
+	e1:SetCost(c100270219.discost)
+	e1:SetTarget(c100270219.distg)
+	e1:SetOperation(c100270219.disop)
 	c:RegisterEffect(e1)
 end
-function c1002702008.tfilter(c,tp)
+function c100270219.tfilter(c,tp)
 	return c:IsLocation(LOCATION_ONFIELD+LOCATION_GRAVE+LOCATION_REMOVED) and c:IsControler(tp)
 end
-function c1002702008.discon(e,tp,eg,ep,ev,re,r,rp)
+function c100270219.discon(e,tp,eg,ep,ev,re,r,rp)
 	if rp==tp or e:GetHandler():IsStatus(STATUS_BATTLE_DESTROYED) then return false end
 	if not re:IsHasProperty(EFFECT_FLAG_CARD_TARGET) then return false end
 	local tg=Duel.GetChainInfo(ev,CHAININFO_TARGET_CARDS)
-	return tg and tg:IsExists(c1002702008.tfilter,1,nil,tp) and Duel.IsChainNegatable(ev)
+	return tg and tg:IsExists(c100270219.tfilter,1,nil,tp) and Duel.IsChainNegatable(ev)
 end
-function c1002702008.discost(e,tp,eg,ep,ev,re,r,rp,chk)
+function c100270219.discost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return e:GetHandler():CheckRemoveOverlayCard(tp,1,REASON_COST) end
 	e:GetHandler():RemoveOverlayCard(tp,1,1,REASON_COST)
 end
-function c1002702008.distg(e,tp,eg,ep,ev,re,r,rp,chk)
+function c100270219.distg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
 	Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
 	if re:GetHandler():IsDestructable() and re:GetHandler():IsRelateToEffect(re) then
 		Duel.SetOperationInfo(0,CATEGORY_DESTROY,eg,1,0,0)
 	end
 end
-function c1002702008.disop(e,tp,eg,ep,ev,re,r,rp)
+function c100270219.disop(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.NegateActivation(ev) and re:GetHandler():IsRelateToEffect(re) then
 		Duel.Destroy(eg,REASON_EFFECT)
 	end
