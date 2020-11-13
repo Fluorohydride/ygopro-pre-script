fff6fff2130bc291357d20627f3bb0470507e607
KohanaSonogami <73727154+KohanaSonogami@users.noreply.github.com>
2020-11-13 23:30:54 +0900

    format

--- scripts/SLT1-JP/c1002702043.lua
+++ scripts/SLT1-JP/c1002702043.lua
@@ -18,7 +18,7 @@ function c1002702043.initial_effect(c)
 	--Gains ATK
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(1002702043,0))
-	e2:SetCategory(CATEGORY_ATKCHANGE) 
+	e2:SetCategory(CATEGORY_ATKCHANGE)
 	e2:SetType(EFFECT_TYPE_IGNITION)
 	e2:SetRange(LOCATION_GRAVE)
 	e2:SetCountLimit(1,1002702043+100)
@@ -51,4 +51,23 @@ function c1002702043.spop(e,tp,eg,ep,ev,re,r,rp)
 		end
 	end
 end
-function c1002702043.atk
+function c1002702043.atkfilter(c)
+	return c:IsLevel(7) and c:IsAttribute(ATTRIBUTE_LIGHT) and c:IsRace(RACE_DRAGON)
+end
+function c1002702043.atktg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and c1002702043.atkfilter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(c1002702043.atkfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	Duel.SelectTarget(tp,c1002702043.atkfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
+end
+function c1002702043.atkop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsFaceup() and tc:IsRelateToEffect(e) then
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetValue(1000)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		tc:RegisterEffect(e1)
+	end
+end
