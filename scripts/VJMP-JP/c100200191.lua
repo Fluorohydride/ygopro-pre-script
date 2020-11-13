34c6114232e353d83667e23a7dcb5bf8f1482761
mercury233 <me@mercury233.me>
2020-11-13 09:29:39 +0800

    fix 氷結界の随身

--- scripts/VJMP-JP/c100200191.lua
+++ scripts/VJMP-JP/c100200191.lua
@@ -31,7 +31,7 @@ function c100200191.spcost1(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.Release(e:GetHandler(),REASON_COST)
 end
 function c100200191.spfilter1(c,e,tp)
-	return c:IsSetCard(0x2f) and c:IsType(TYPE_MONSTER) and c:IsLevelAbove(3) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+	return c:IsSetCard(0x2f) and c:IsType(TYPE_MONSTER) and c:IsLevelAbove(5) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function c100200191.sptg1(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>-1
