9e5655f76fafd797adee2b60acd7798a1f5a7770
KohanaSonogami <73727154+KohanaSonogami@users.noreply.github.com>
2020-11-14 18:49:11 +0900

    update "ep==1-tp"

--- scripts/SLT1-JP/c100270249.lua
+++ scripts/SLT1-JP/c100270249.lua
@@ -21,7 +21,7 @@ function c100270249.cfilter(c)
 		and c:IsAbleToHandAsCost()
 end
 function c100270249.condition(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.IsChainDisablable(ev)
+	return ep==1-tp and Duel.IsChainDisablable(ev)
 end
 function c100270249.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then
