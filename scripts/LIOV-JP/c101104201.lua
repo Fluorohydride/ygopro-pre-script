3c061712e3027e43ee03237b4789e34e18d397a2
Mercury233 <me@mercury233.me>
2020-11-17 02:35:40 +0000

    Update and rename c101103201.lua to scripts/LIOV-JP/c101104201.lua

--- scripts/LIOV-JP/c101104201.lua
+++ scripts/LIOV-JP/c101104201.lua
@@ -1,27 +1,26 @@
---ZWペガサス ツイン サーベル
+--ZW－天馬双翼剣
 --ZW - Pegasus Weapon Saber
 --LUA by Kohana Sonogami
---
-function c101103201.initial_effect(c)
-	c:SetUniqueOnField(1,0,101103201)
+function c101104201.initial_effect(c)
+	c:SetUniqueOnField(1,0,101104201)
 	--Special Summon
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetCode(EFFECT_SPSUMMON_PROC)
 	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
 	e1:SetRange(LOCATION_HAND)
-	e1:SetCondition(c101103201.spcon)
+	e1:SetCondition(c101104201.spcon)
 	c:RegisterEffect(e1)
 	--Equip
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(101103201,0))
+	e2:SetDescription(aux.Stringid(101104201,0))
 	e2:SetType(EFFECT_TYPE_IGNITION)
 	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e2:SetCategory(CATEGORY_EQUIP)
 	e2:SetRange(LOCATION_MZONE)
-	e2:SetCondition(c101103201.eqcon)
-	e2:SetTarget(c101103201.eqtg)
-	e2:SetOperation(c101103201.eqop)
+	e2:SetCondition(c101104201.eqcon)
+	e2:SetTarget(c101104201.eqtg)
+	e2:SetOperation(c101104201.eqop)
 	c:RegisterEffect(e2)
 	--Negate
 	local e3=Effect.CreateEffect(c)
@@ -29,35 +28,35 @@ function c101103201.initial_effect(c)
 	e3:SetCode(EVENT_CHAIN_SOLVING)
 	e3:SetRange(LOCATION_SZONE)
 	e3:SetCountLimit(1)
-	e3:SetCondition(c101103201.negcon)
-	e3:SetOperation(c101103201.negop)
+	e3:SetCondition(c101104201.negcon)
+	e3:SetOperation(c101104201.negop)
 	c:RegisterEffect(e3)
 end
-function c101103201.spcon(e,c)
+function c101104201.spcon(e,c)
 	if c==nil then return true end
 	local tp=c:GetControler()
 	return Duel.GetLP(tp)<=Duel.GetLP(1-tp)-2000
 		and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
 end
-function c101103201.eqcon(e,tp,eg,ep,ev,re,r,rp)
+function c101104201.eqcon(e,tp,eg,ep,ev,re,r,rp)
 	return e:GetHandler():CheckUniqueOnField(tp)
 end
-function c101103201.filter(c)
+function c101104201.filter(c)
 	return c:IsFaceup() and c:IsSetCard(0x107f)
 end
-function c101103201.eqtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and c101103201.filter(chkc) end
+function c101104201.eqtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and c101104201.filter(chkc) end
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_SZONE)>0
-		and Duel.IsExistingTarget(c101103201.filter,tp,LOCATION_MZONE,0,1,nil) end
+		and Duel.IsExistingTarget(c101104201.filter,tp,LOCATION_MZONE,0,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)
-	Duel.SelectTarget(tp,c101103201.filter,tp,LOCATION_MZONE,0,1,1,nil)
+	Duel.SelectTarget(tp,c101104201.filter,tp,LOCATION_MZONE,0,1,1,nil)
 end
-function c101103201.eqop(e,tp,eg,ep,ev,re,r,rp)
+function c101104201.eqop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if not c:IsRelateToEffect(e) then return end
 	if c:IsLocation(LOCATION_MZONE) and c:IsFacedown() then return end
 	local tc=Duel.GetFirstTarget()
-	if Duel.GetLocationCount(tp,LOCATION_SZONE)<=0 or tc:GetControler()~=tp or tc:IsFacedown() or not tc:IsRelateToEffect(e) or not c:CheckUniqueOnField(tp) then
+	if Duel.GetLocationCount(tp,LOCATION_SZONE)<=0 or tc:IsFacedown() or not tc:IsRelateToEffect(e) or not c:CheckUniqueOnField(tp) then
 		Duel.SendtoGrave(c,REASON_EFFECT)
 		return
 	end
@@ -67,7 +66,7 @@ function c101103201.eqop(e,tp,eg,ep,ev,re,r,rp)
 	e1:SetCode(EFFECT_EQUIP_LIMIT)
 	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
 	e1:SetReset(RESET_EVENT+RESETS_STANDARD)
-	e1:SetValue(c101103201.eqlimit)
+	e1:SetValue(c101104201.eqlimit)
 	e1:SetLabelObject(tc)
 	c:RegisterEffect(e1)
 	--Gains ATK
@@ -78,17 +77,16 @@ function c101103201.eqop(e,tp,eg,ep,ev,re,r,rp)
 	e2:SetReset(RESET_EVENT+RESETS_STANDARD)
 	c:RegisterEffect(e2)
 end
-function c101103201.eqlimit(e,c)
+function c101104201.eqlimit(e,c)
 	return c==e:GetLabelObject()
 end
-function c101103201.negcon(e,tp,eg,ep,ev,re,r,rp)
+function c101104201.negcon(e,tp,eg,ep,ev,re,r,rp)
 	return rp==1-tp and Duel.GetChainInfo(ev,CHAININFO_TRIGGERING_LOCATION)==LOCATION_MZONE
 		and re:IsActiveType(TYPE_MONSTER) and Duel.IsChainDisablable(ev)
 end
-function c101103201.negop(e,tp,eg,ep,ev,re,r,rp)
-	local rc=re:GetHandler()
-	if Duel.SelectYesNo(tp,aux.Stringid(101103201,1)) then
-		Duel.Hint(HINT_CARD,0,c101103201)
+function c101104201.negop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.SelectYesNo(tp,aux.Stringid(101104201,1)) then
+		Duel.Hint(HINT_CARD,0,101104201)
 		Duel.NegateEffect(ev)
 	end
 end
