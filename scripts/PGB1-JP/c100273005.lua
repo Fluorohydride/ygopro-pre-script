da892ccb0dbdbfd93bf2a672dde70e866403c973
KohanaSonogami <73727154+KohanaSonogami@users.noreply.github.com>
2020-11-17 09:48:15 +0900

    fix

--- scripts/PGB1-JP/c100273005.lua
+++ scripts/PGB1-JP/c100273005.lua
@@ -59,7 +59,8 @@ function c100273005.eqfilter(c)
 end
 function c100273005.eqtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) and c100273005.filter(chkc) end
-	if chk==0 then return Duel.IsExistingTarget(c100273005.filter,tp,0,LOCATION_MZONE,1,nil) end
+	if chk==0 then return Duel.IsExistingTarget(c100273005.filter,tp,0,LOCATION_MZONE,1,nil)
+		and Duel.IsExistingMatchingCard(c100273005.eqfilter,tp,LOCATION_MZONE,0,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)
 	local g=Duel.SelectTarget(tp,c100273005.filter,tp,0,LOCATION_MZONE,1,1,nil)
 	Duel.SetOperationInfo(0,CATEGORY_EQUIP,g,1,0,0)
