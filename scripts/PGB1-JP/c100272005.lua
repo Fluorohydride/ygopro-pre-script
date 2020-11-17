a74eef85151d3e57ac522e23387dc0949875a872
Mercury233 <me@mercury233.me>
2020-11-17 02:54:27 +0000

    Update and rename c100273005.lua to c100272005.lua

--- scripts/PGB1-JP/c100272005.lua
+++ scripts/PGB1-JP/c100272005.lua
@@ -1,81 +1,81 @@
---ゴールデンアイドル
+--黄金の邪教神
 --Golden Idol
 --LUA by Kohana Sonogami
---
-function c100273005.initial_effect(c)
+function c100272005.initial_effect(c)
 	--Change the Name
 	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(100272005,0))
 	e1:SetType(EFFECT_TYPE_QUICK_O)
 	e1:SetProperty(EFFECT_FLAG_DELAY)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetRange(LOCATION_MZONE)
-	e1:SetCountLimit(1,100273005)
-	e1:SetTarget(c100273005.rntg)
-	e1:SetOperation(c100273005.rnop)
+	e1:SetCountLimit(1,100272005)
+	e1:SetTarget(c100272005.rntg)
+	e1:SetOperation(c100272005.rnop)
 	c:RegisterEffect(e1)
 	--Equip
 	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(100272005,0))
 	e2:SetCategory(CATEGORY_EQUIP)
 	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e2:SetCode(EVENT_REMOVE)
-	e2:SetCountLimit(1,100273005+100)
-	e2:SetTarget(c100273005.eqtg)
-	e2:SetOperation(c100273005.eqop)
+	e2:SetCountLimit(1,100272005+100)
+	e2:SetTarget(c100272005.eqtg)
+	e2:SetOperation(c100272005.eqop)
 	c:RegisterEffect(e2)
-	local e2x=e2:Clone()
-	e2x:SetCode(EVENT_TO_GRAVE)
-	e2x:SetCondition(c100273005.eqcon)
-	c:RegisterEffect(e2x)
+	local e3=e2:Clone()
+	e3:SetCode(EVENT_TO_GRAVE)
+	e3:SetCondition(c100272005.eqcon)
+	c:RegisterEffect(e3)
 end
-function c100273005.rntg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.GetFieldGroupCount(tp,0,LOCATION_HAND)>0 end
-	Duel.SetOperationInfo(0,CATEGORY_REMOVE,nil,1,1-tp,LOCATION_HAND)
+function c100272005.rntg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetMatchingGroupCount(aux.NOT(Card.IsPublic),tp,0,LOCATION_HAND,nil)>0 end
 end
-function c100273005.rnop(e,tp,eg,ep,ev,re,r,rp)
+function c100272005.rnop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local g=Duel.GetMatchingGroup(aux.NOT(Card.IsPublic),tp,0,LOCATION_HAND,nil)
 	if g:GetCount()>0 then
 		Duel.ConfirmCards(tp,g)
 		Duel.ShuffleHand(1-tp)
-	end
-	if c:IsFaceup() then
-		local e1=Effect.CreateEffect(c)
-		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetCode(EFFECT_CHANGE_CODE)
-		e1:SetValue(27125110)
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
-		c:RegisterEffect(e1)
+		if c:IsFaceup() and c:IsRelateToEffect(e) then
+			local e1=Effect.CreateEffect(c)
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetCode(EFFECT_CHANGE_CODE)
+			e1:SetValue(27125110)
+			e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+			c:RegisterEffect(e1)
+		end
 	end
 end
-function c100273005.eqcon(e,tp,eg,ep,ev,re,r,rp)
+function c100272005.eqcon(e,tp,eg,ep,ev,re,r,rp)
 	return e:GetHandler():IsReason(REASON_EFFECT)
 end
-function c100273005.filter(c)
+function c100272005.filter(c)
 	return c:IsFaceup() and c:IsType(TYPE_EFFECT) and c:IsAbleToChangeControler()
 end
-function c100273005.eqfilter(c)
+function c100272005.eqfilter(c)
 	return c:IsFaceup() and c:IsSetCard(0x110) and c:IsType(TYPE_MONSTER) and not c:IsSummonableCard()
 end
-function c100273005.eqtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) and c100273005.filter(chkc) end
-	if chk==0 then return Duel.IsExistingTarget(c100273005.filter,tp,0,LOCATION_MZONE,1,nil)
-		and Duel.IsExistingMatchingCard(c100273005.eqfilter,tp,LOCATION_MZONE,0,1,nil) end
+function c100272005.eqtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) and c100272005.filter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(c100272005.filter,tp,0,LOCATION_MZONE,1,nil)
+		and Duel.IsExistingMatchingCard(c100272005.eqfilter,tp,LOCATION_MZONE,0,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)
-	local g=Duel.SelectTarget(tp,c100273005.filter,tp,0,LOCATION_MZONE,1,1,nil)
+	local g=Duel.SelectTarget(tp,c100272005.filter,tp,0,LOCATION_MZONE,1,1,nil)
 	Duel.SetOperationInfo(0,CATEGORY_EQUIP,g,1,0,0)
 end
-function c100273005.eqop(e,tp,eg,ep,ev,re,r,rp)
+function c100272005.eqop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if Duel.GetLocationCount(tp,LOCATION_SZONE)<=0 then return end
 	local tc1=Duel.GetFirstTarget()
 	if tc1:IsRelateToEffect(e) and tc1:IsAbleToChangeControler() then
 		local atk=tc1:GetTextAttack()
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)
-		local sg=Duel.SelectMatchingCard(tp,c100273005.eqfilter,tp,LOCATION_MZONE,0,1,1,nil)
+		local sg=Duel.SelectMatchingCard(tp,c100272005.eqfilter,tp,LOCATION_MZONE,0,1,1,nil)
 		if sg:GetCount()>0 then
 			local tc2=sg:GetFirst()
-			if tc1:IsFaceup() and tc1:IsRelateToEffect(e) and tc1:IsControler(1-tp) and tc2 then
+			if tc1:IsFaceup() and tc1:IsRelateToEffect(e) and tc2 then
 				Duel.Equip(tp,tc1,tc2,atk)
 				--Gains ATK
 				local e1=Effect.CreateEffect(tc1)
@@ -89,13 +89,14 @@ function c100273005.eqop(e,tp,eg,ep,ev,re,r,rp)
 				e2:SetType(EFFECT_TYPE_SINGLE)
 				e2:SetCode(EFFECT_EQUIP_LIMIT)
 				e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-				e2:SetValue(c100273005.eqlimit)
+				e2:SetValue(c100272005.eqlimit)
 				e2:SetReset(RESET_EVENT+RESETS_STANDARD)
+				e2:SetLabelObject(tc2)
 				tc1:RegisterEffect(e2)
 			end
 		end
 	end
 end
-function c100273005.eqlimit(e,c)
-	return c:GetControler()==e:GetHandlerPlayer()
+function c100272005.eqlimit(e,c)
+	return c==e:GetLabelObject()
 end
