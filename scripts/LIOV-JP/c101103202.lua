c73421d4ec146bfc3ed630353d085eeff0dfbc4d
Mercury233 <me@mercury233.me>
2020-11-17 02:28:57 +0000

    Update and rename c101103202.lua to scripts/LIOV-JP/c101103202.lua

--- scripts/LIOV-JP/c101103202.lua
+++ scripts/LIOV-JP/c101103202.lua
@@ -1,49 +1,47 @@
---ZS - アームズセージ
+--ZS－武装賢者
 --ZS - Arms Sage
 --LUA by Kohana Sonogami
---
-function c101103202.initial_effect(c)
+function c101104202.initial_effect(c)
 	--Special Summon
 	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(101104202,0))
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
 	e1:SetCode(EFFECT_SPSUMMON_PROC)
 	e1:SetRange(LOCATION_HAND)
-	e1:SetCondition(c101103202.sprcon)
+	e1:SetCondition(c101104202.sprcon)
 	c:RegisterEffect(e1)
 	--Gains Effect
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(101103202,0))
 	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
 	e2:SetCode(EVENT_BE_MATERIAL)
 	e2:SetProperty(EFFECT_FLAG_EVENT_PLAYER)
-	e2:SetCountLimit(1,101103202)
-	e2:SetCondition(c101103202.efcon)
-	e2:SetOperation(c101103202.efop)
+	e2:SetCountLimit(1,101104202)
+	e2:SetCondition(c101104202.efcon)
+	e2:SetOperation(c101104202.efop)
 	c:RegisterEffect(e2)
 end
-function c101103202.sprcon(e,c)
+function c101104202.sprcon(e,c)
 	if c==nil then return true end
-	local tp=c:GetControler()
-	return Duel.GetMatchingGroupCount(c101103202.cfilter,e:GetHandler():GetControler(),LOCATION_MZONE,0,nil)==1
+	local g=Duel.GetFieldGroup(c:GetControler(),LOCATION_MZONE,0)
+	local tc=g:GetFirst()
+	return #g==1 and tc:IsFaceup() and tc:IsLevel(4) and not tc:IsCode(101104202)
 end
-function c101103202.cfilter(c)
-	return c:IsFaceup() and c:IsLevel(4) and not c:IsCode(101103202)
-end
-function c101103202.efcon(e,tp,eg,ep,ev,re,r,rp)
+function c101104202.efcon(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	return r==REASON_XYZ and c:GetReasonCard():IsSetCard(0x107f)
 end
-function c101103202.efop(e,tp,eg,ep,ev,re,r,rp)
+function c101104202.efop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local rc=c:GetReasonCard()
 	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(101104202,1))
 	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
 	e1:SetType(EFFECT_TYPE_TRIGGER_O+EFFECT_TYPE_SINGLE)
 	e1:SetProperty(EFFECT_FLAG_DELAY)
 	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
-	e1:SetTarget(c101103202.thtg)
-	e1:SetOperation(c101103202.thop)
+	e1:SetTarget(c101104202.thtg)
+	e1:SetOperation(c101104202.thop)
 	e1:SetReset(RESET_EVENT+RESETS_STANDARD)
 	rc:RegisterEffect(e1,true)
 	if not rc:IsType(TYPE_EFFECT) then
@@ -55,16 +53,16 @@ function c101103202.efop(e,tp,eg,ep,ev,re,r,rp)
 		rc:RegisterEffect(e2,true)
 	end
 end
-function c101103202.thfilter(c)
-	return c:IsSetCard(0x7e) and c:IsAbleToHand()
+function c101104202.thfilter(c)
+	return c:IsSetCard(0x7e) and c:IsType(TYPE_MONSTER) and c:IsAbleToHand()
 end
-function c101103202.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(c101103202.thfilter,tp,LOCATION_DECK,0,1,nil) end
+function c101104202.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c101104202.thfilter,tp,LOCATION_DECK,0,1,nil) end
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
 end
-function c101103202.thop(e,tp,eg,ep,ev,re,r,rp)
+function c101104202.thop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-	local g=Duel.SelectMatchingCard(tp,c101103202.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+	local g=Duel.SelectMatchingCard(tp,c101104202.thfilter,tp,LOCATION_DECK,0,1,1,nil)
 	if g:GetCount()>0 then
 		Duel.SendtoHand(g,nil,REASON_EFFECT)
 		Duel.ConfirmCards(1-tp,g)
