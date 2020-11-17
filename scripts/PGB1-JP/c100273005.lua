da10ade6a4c0a1a3b0f2e19d0fffb2ecb5c9bdfc
KohanaSonogami <73727154+KohanaSonogami@users.noreply.github.com>
2020-11-17 09:42:31 +0900

    Re-Change

--- scripts/PGB1-JP/c100273005.lua
+++ scripts/PGB1-JP/c100273005.lua
@@ -23,10 +23,10 @@ function c100273005.initial_effect(c)
 	e2:SetTarget(c100273005.eqtg)
 	e2:SetOperation(c100273005.eqop)
 	c:RegisterEffect(e2)
-	local e3x=e2:Clone()
-	e3x:SetCode(EVENT_TO_GRAVE)
-	e3x:SetCondition(c100273005.eqcon)
-	c:RegisterEffect(e3x)
+	local e2x=e2:Clone()
+	e2x:SetCode(EVENT_TO_GRAVE)
+	e2x:SetCondition(c100273005.eqcon)
+	c:RegisterEffect(e2x)
 end
 function c100273005.rntg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetFieldGroupCount(tp,0,LOCATION_HAND)>0 end
@@ -55,28 +55,46 @@ function c100273005.filter(c)
 	return c:IsFaceup() and c:IsType(TYPE_EFFECT) and c:IsAbleToChangeControler()
 end
 function c100273005.eqfilter(c)
-	local m=_G["c"..c:GetCode()]
-	return c:IsFaceup() and ((c:IsSetCard(0x110) and c:IsType(TYPE_MONSTER)) or c:IsCode(64631466))
-		and m.can_equip_monster(c) and not c:IsSummonableCard()
+	return c:IsFaceup() and c:IsSetCard(0x110) and c:IsType(TYPE_MONSTER)
 end
 function c100273005.eqtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsOnField() and c100273005.filter(chkc) and chkc~=e:GetHandler() end
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_SZONE)>0
-		and Duel.IsExistingTarget(c100273005.filter,tp,0,LOCATION_MZONE,1,nil)
-		and Duel.IsExistingMatchingCard(c100273005.eqfilter,tp,LOCATION_MZONE,0,1,nil) end
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) and c100273005.filter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(c100273005.filter,tp,0,LOCATION_MZONE,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)
 	local g=Duel.SelectTarget(tp,c100273005.filter,tp,0,LOCATION_MZONE,1,1,nil)
 	Duel.SetOperationInfo(0,CATEGORY_EQUIP,g,1,0,0)
 end
 function c100273005.eqop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
+	if Duel.GetLocationCount(tp,LOCATION_SZONE)<=0 then return end
 	local tc1=Duel.GetFirstTarget()
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
-	local g=Duel.SelectMatchingCard(tp,c100273005.eqfilter,tp,LOCATION_MZONE,0,1,1,nil)
-	local tc2=g:GetFirst()
-	if not tc2 then return end
-	local m=_G["c"..tc2:GetCode()]
-	if tc1:IsFaceup() and tc1:IsRelateToEffect(e) and tc1:IsControler(1-tp) then
-		m.equip_monster(tc2,tp,tc1)
+	if tc1:IsRelateToEffect(e) and tc1:IsAbleToChangeControler() then
+		local atk=tc1:GetTextAttack()
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)
+		local sg=Duel.SelectMatchingCard(tp,c100273005.eqfilter,tp,LOCATION_MZONE,0,1,1,nil)
+		if sg:GetCount()>0 then
+			local tc2=sg:GetFirst()
+			if tc1:IsFaceup() and tc1:IsRelateToEffect(e) and tc1:IsControler(1-tp) and tc2 then
+				Duel.Equip(tp,tc1,tc2,atk)
+				--Gains ATK
+				local e1=Effect.CreateEffect(tc1)
+				e1:SetType(EFFECT_TYPE_EQUIP)
+				e1:SetCode(EFFECT_UPDATE_ATTACK)
+				e1:SetValue(atk)
+				e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+				tc1:RegisterEffect(e1)
+				--Equip Limit
+				local e2=Effect.CreateEffect(c)
+				e2:SetType(EFFECT_TYPE_SINGLE)
+				e2:SetCode(EFFECT_EQUIP_LIMIT)
+				e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+				e2:SetValue(c100273005.eqlimit)
+				e2:SetReset(RESET_EVENT+RESETS_STANDARD)
+				tc1:RegisterEffect(e2)
+			end
+		end
 	end
+end
+function c100273005.eqlimit(e,c)
+	return c:GetControler()==e:GetHandlerPlayer()
 end
