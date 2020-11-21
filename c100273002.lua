ee30db62ee51b0ac13e91f457efef47e2549d377
KohanaSonogami <73727154+KohanaSonogami@users.noreply.github.com>
2020-11-21 11:09:17 +0900

    Added 21PP-JP002 by Kohana Sonogami
    ID of token i put it in the same id little angel. because idk the ID of token.

---
+++ c100273002.lua
@@ -0,0 +1,81 @@
+--テルス小さな天使
+--Telus the Little Angel
+--LUA by Kohana Sonogami
+--
+function c100273002.initial_effect(c)
+	--Special Summon a Token
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(100273002,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOKEN)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetProperty(EFFECT_FLAG_DELAY)
+	e1:SetCode(EVENT_TO_GRAVE)
+	e1:SetCountLimit(1,100273002)
+	e1:SetCondition(c100273002.tkcon1)
+	e1:SetTarget(c100273002.tktg1)
+	e1:SetOperation(c100273002.tkop1)
+	c:RegisterEffect(e1)
+	--Special Summon a 2 Token
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(100273002,1))
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOKEN)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_GRAVE)
+	e2:SetCountLimit(1,100273002+100)
+	e2:SetCost(c100273002.tkcost2)
+	e2:SetCondition(c100273002.tkcon2)
+	e2:SetTarget(c100273002.tktg2)
+	e2:SetOperation(c100273002.tkop2)
+	c:RegisterEffect(e2)
+end
+function c100273002.tkcon1(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsPreviousLocation(LOCATION_MZONE)
+end
+function c89907227.tktg1(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+	and Duel.IsPlayerCanSpecialSummonMonster(tp,100273002,0,0x4011,0,0,1,RACE_FAIRY,ATTRIBUTE_LIGHT) end
+	Duel.SetOperationInfo(0,CATEGORY_TOKEN,nil,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,0,0)
+end
+function c89907227.tkop1(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	if Duel.IsPlayerCanSpecialSummonMonster(tp,100273002,0,0x4011,0,0,1,RACE_FAIRY,ATTRIBUTE_LIGHT) then
+		local token=Duel.CreateToken(tp,100273002)
+		Duel.SpecialSummon(token,0,tp,tp,false,false,POS_FACEUP_DEFENSE)
+	end
+end
+function c100273002.tkcsfilter(c)
+	return c:IsType(TYPE_SPELL) and c:IsAbleToRemoveAsCost()
+end
+function c100273002.tkcost2(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return Duel.IsExistingMatchingCard(c100273002.tkcsfilter,tp,LOCATION_HAND,0,1,c) and c:IsAbleToRemoveAsCost() end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local g=Duel.SelectMatchingCard(tp,c100273002.tkcsfilter,tp,LOCATION_HAND,0,1,1,c)
+	g:AddCard(c)
+	Duel.Remove(g,POS_FACEUP,REASON_COST)
+end
+function c100273002.ctkfilter(c)
+	return c:IsFaceup() and c:IsCode(100273002)
+end
+function c100273002.tkcon2(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetTurnPlayer()~=tp and not Duel.IsExistingMatchingCard(c100273002.ctkfilter,tp,LOCATION_MZONE,0,1,nil)
+end
+function c100273002.tktg2(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return not Duel.IsPlayerAffectedByEffect(tp,59822133)
+		and Duel.GetLocationCount(tp,LOCATION_MZONE)>1
+		and Duel.IsPlayerCanSpecialSummonMonster(tp,100273002,0x4011,0,0,1,RACE_FAIRY,ATTRIBUTE_LIGHT) end
+	Duel.SetOperationInfo(0,CATEGORY_TOKEN,nil,2,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,2,0,0)
+end
+function c100273002.tkop2(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.IsPlayerAffectedByEffect(tp,59822133) then return end
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)>1
+		and Duel.IsPlayerCanSpecialSummonMonster(tp,100273002,0x4011,0,0,1,RACE_FAIRY,ATTRIBUTE_LIGHT) then
+		for i=1,2 do
+			local token=Duel.CreateToken(tp,100273002)
+			Duel.SpecialSummonStep(token,0,tp,tp,false,false,POS_FACEUP)
+		end
+	end
+end
