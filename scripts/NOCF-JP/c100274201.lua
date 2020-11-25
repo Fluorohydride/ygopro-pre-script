d4ef7489849eeb4973c0ea8e6ada0b2e6fd36735
KohanaSonogami <73727154+KohanaSonogami@users.noreply.github.com>
2020-11-25 16:15:57 +0900

    Added "C1000 Numeronius" by Kohana Sonogami (#614)

---
+++ scripts/NOCF-JP/c100274201.lua
@@ -0,0 +1,107 @@
+--CNo.1000 夢幻虚神ヌメロニアス
+--Number C1000: Numeronius
+--Anime Retrain Version by Kohana Sonogami
+function c100274201.initial_effect(c)
+	--Xyz Summon
+	aux.AddXyzProcedure(c,nil,12,5)
+	c:EnableReviveLimit()
+	--Destroy 1 other monster
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(100274201,0))
+	e1:SetType(EFFECT_TYPE_QUICK_O)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCountLimit(1)
+	e1:SetCost(c100274201.descost1)
+	e1:SetTarget(c100274201.destg1)
+	e1:SetOperation(c100274201.desop1)
+	c:RegisterEffect(e1)
+	--Destroy all/Special Summon
+	local e2=Effect.CreateEffect(c)
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_DESTROY)
+	e2:SetDescription(aux.Stringid(100274201,1))
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e2:SetCode(EVENT_PHASE+PHASE_BATTLE)
+	e2:SetCountLimit(1)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetTarget(c100274201.destg2)
+	e2:SetOperation(c100274201.desop2)
+	c:RegisterEffect(e2)
+	--Xyz Summon
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(100274201,2))
+	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e3:SetProperty(EFFECT_FLAG_DELAY)
+	e3:SetCode(EVENT_TO_GRAVE)
+	e3:SetCondition(c100274201.spcon)
+	e3:SetTarget(c100274201.sptg)
+	e3:SetOperation(c100274201.spop)
+	c:RegisterEffect(e3)
+end
+c100274201.xyz_number=1000
+function c100274201.descost1(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():CheckRemoveOverlayCard(tp,1,REASON_COST) end
+	e:GetHandler():RemoveOverlayCard(tp,1,1,REASON_COST)
+end
+function c100274201.destg1(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(aux.TRUE,tp,LOCATION_MZONE,LOCATION_MZONE,1,e:GetHandler()) end
+	local g=Duel.GetMatchingGroup(aux.TRUE,tp,LOCATION_MZONE,LOCATION_MZONE,e:GetHandler())
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
+end
+function c100274201.desop1(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local g=Duel.SelectMatchingCard(tp,aux.TRUE,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,e:GetHandler())
+	if g:GetCount()>0 then
+		Duel.HintSelection(g)
+		Duel.Destroy(g,REASON_EFFECT)
+	end
+end
+function c100274201.destg2(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	local sg=Duel.GetMatchingGroup(nil,tp,LOCATION_MZONE,LOCATION_MZONE,e:GetHandler())
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,sg,sg:GetCount(),0,0)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,0,tp,LOCATION_GRAVE)
+end
+function c100274201.spfilter2(c,e,tp)
+	return c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP_DEFENSE)
+end
+function c100274201.desop2(e,tp,eg,ep,ev,re,r,rp)
+	local dg=Duel.GetMatchingGroup(nil,tp,LOCATION_MZONE,LOCATION_MZONE,aux.ExceptThisCard(e))
+	if Duel.Destroy(dg,REASON_EFFECT)>0 and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 then
+		local g=Duel.GetMatchingGroup(c100274201.spfilter2,tp,0,LOCATION_GRAVE,nil,e,tp)
+		if #g>0 and Duel.SelectYesNo(tp,aux.Stringid(100274201,3)) then
+			Duel.BreakEffect()
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+			local sg=g:Select(tp,1,1,nil)
+			Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP_DEFENSE)
+		end
+	end
+end
+function c100274201.spcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return c:GetOverlayGroup() and c:IsPreviousLocation(LOCATION_MZONE)
+		and rp==1-tp and c:IsReason(REASON_EFFECT) and c:GetPreviousControler()==tp
+end
+function c100274201.spfilter(c,e,tp)
+	return c:IsCode(100274202) and c:IsType(TYPE_XYZ) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+		and Duel.GetLocationCountFromEx(tp,tp,nil,c)>0
+end
+function c100274201.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c100274201.spfilter,tp,LOCATION_EXTRA,0,1,nil,e,tp)
+		and e:GetHandler():IsCanOverlay() end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
+	Duel.SetOperationInfo(0,CATEGORY_LEAVE_GRAVE,e:GetHandler(),1,0,0)
+end
+function c100274201.spop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,c100274201.spfilter,tp,LOCATION_EXTRA,0,1,1,nil,e,tp)
+	local tc=g:GetFirst()
+	if tc then
+		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
+		local c=e:GetHandler()
+		if c:IsRelateToEffect(e) then
+			Duel.Overlay(tc,Group.FromCards(c))
+		end
+	end
+end
