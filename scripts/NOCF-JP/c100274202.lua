b03e8fed4d6e429de6acf043270891112f6b0eee
Mercury233 <me@mercury233.me>
2020-11-25 07:33:41 +0000

    Update and rename c100274202.lua to scripts/NOCF-JP/c100274202.lua

--- scripts/NOCF-JP/c100274202.lua
+++ scripts/NOCF-JP/c100274202.lua
@@ -1,7 +1,6 @@
---番号Ci1000:ヌメロニウス・ヌメロニア
+--CiNo.1000 夢幻虚光神ヌメロニアス・ヌメロニア
 --Number Ci1000: Numeronius Numeronia
 --Anime Retrain Version by Kohana Sonogami
---
 function c100274202.initial_effect(c)
 	--Xyz Summon
 	aux.AddXyzProcedure(c,nil,13,5)
@@ -11,12 +10,12 @@ function c100274202.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
 	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
 	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
-	e1:SetCondition(c100274202.NumeroniusCondition)
-	e1:SetOperation(c100274202.NumeroniusOperation)
+	e1:SetCondition(c100274202.condition)
+	e1:SetOperation(c100274202.operation)
 	c:RegisterEffect(e1)
 	--Negate Attack/Gains LP
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(100274202,4))
+	e2:SetDescription(aux.Stringid(100274202,0))
 	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
 	e2:SetCode(EVENT_BE_BATTLE_TARGET)
 	e2:SetRange(LOCATION_MZONE)
@@ -26,32 +25,32 @@ function c100274202.initial_effect(c)
 	c:RegisterEffect(e2)
 end
 c100274202.xyz_number=1000
-function c100274202.NumeroniusCondition(e,tp,eg,ep,ev,re,r,rp)
+function c100274202.condition(e,tp,eg,ep,ev,re,r,rp)
 	return re and re:GetHandler():IsCode(100274201)
 end
-function c100274202.NumeroniusOperation(e,tp,eg,ep,ev,re,r,rp)
+function c100274202.operation(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	--100,000 ATK
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(100274202,1))
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetCode(EFFECT_UPDATE_ATTACK)
 	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetCondition(c100274202.atkcon)
 	e1:SetValue(100000)
+	e1:SetReset(RESET_EVENT+RESETS_STANDARD)
 	c:RegisterEffect(e1)
 	local e2=e1:Clone()
 	e2:SetCode(EFFECT_UPDATE_DEFENSE)
 	c:RegisterEffect(e2)
 	--Must attack this card
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(100274202,2))
 	e2:SetType(EFFECT_TYPE_FIELD)
 	e2:SetCode(EFFECT_MUST_ATTACK)
 	e2:SetRange(LOCATION_MZONE)
 	e2:SetTargetRange(0,LOCATION_MZONE)
 	e2:SetCondition(c100274202.effcon)
+	e2:SetReset(RESET_EVENT+RESETS_STANDARD)
 	c:RegisterEffect(e2)
 	local e3=e2:Clone()
 	e3:SetCode(EFFECT_MUST_ATTACK_MONSTER)
@@ -59,35 +58,33 @@ function c100274202.NumeroniusOperation(e,tp,eg,ep,ev,re,r,rp)
 	c:RegisterEffect(e3)
 	--Win Condition
 	local e4=Effect.CreateEffect(c)
-	e4:SetDescription(aux.Stringid(100274202,3))
 	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 	e4:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
 	e4:SetRange(LOCATION_MZONE)
 	e4:SetCode(EVENT_PHASE+PHASE_END)
 	e4:SetCondition(c100274202.wincon)
 	e4:SetOperation(c100274202.winop)
+	e4:SetReset(RESET_EVENT+RESETS_STANDARD)
 	c:RegisterEffect(e4)
 end
 function c100274202.atkcon(e)
-	return Duel.GetTurnPlayer()~=e:GetHandlerPlayer()
+	return Duel.GetTurnPlayer()==1-e:GetHandlerPlayer()
 end
 function c100274202.effcon(e)
-	return e:GetHandler():GetSummonType()==SUMMON_TYPE_SPECIAL and Duel.GetTurnCount()==e:GetHandler():GetTurnID()+1
+	return Duel.GetTurnCount()>=e:GetHandler():GetTurnID()+1
 end
 function c100274202.atklimit(e,c)
 	return c==e:GetHandler()
 end
 function c100274202.wincon(e,tp,eg,ep,ev,re,r,rp)
-	return not tp~=Duel.GetTurnPlayer() and e:GetHandler():GetAttackedGroupCount()==0 and c100274202.effcon(e)
+	return tp~=Duel.GetTurnPlayer() and e:GetHandler():GetBattledGroupCount()==0 and c100274202.effcon(e)
 end
 function c100274202.winop(e,tp,eg,ep,ev,re,r,rp)
 	local WIN_REASON_NUMERONIUS_NUMERONIA=0x21
 	Duel.Win(tp,WIN_REASON_NUMERONIUS_NUMERONIA)
 end
 function c100274202.nacon(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	local bt=eg:GetFirst()
-	return bt:GetControler()==c:GetControler()
+	return Duel.GetTurnPlayer()==1-tp
 end
 function c100274202.nacost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return e:GetHandler():CheckRemoveOverlayCard(tp,1,REASON_COST) end
@@ -95,6 +92,6 @@ function c100274202.nacost(e,tp,eg,ep,ev,re,r,rp,chk)
 end
 function c100274202.naop(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.NegateAttack() then
-		Duel.Recover(e:GetHandler():GetControler(),Duel.GetAttacker():GetAttack(),REASON_EFFECT)
+		Duel.Recover(tp,Duel.GetAttacker():GetAttack(),REASON_EFFECT)
 	end
 end
