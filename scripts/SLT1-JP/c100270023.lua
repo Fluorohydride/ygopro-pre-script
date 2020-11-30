e4b209664ac9ab0144bb756d01fcfede6c5c79c4
Mercury233 <me@mercury233.me>
2020-11-30 03:49:53 +0000

    Update and rename c100270023.lua to scripts/SLT1-JP/c100270023.lua

--- scripts/SLT1-JP/c100270023.lua
+++ scripts/SLT1-JP/c100270023.lua
@@ -1,13 +1,13 @@
---タイム泥棒アジャスター
---Chronodiver Adjuster/ Time Thief Adjuster
+--クロノダイバー・アジャスター
+--Time Thief Adjuster
 --LUA by Kohana Sonogami
---
 function c100270023.initial_effect(c)
 	--Special Summon this card
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(100270023,0))
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e1:SetProperty(EFFECT_FLAG_DELAY)
 	e1:SetRange(LOCATION_HAND)
 	e1:SetCode(EVENT_SUMMON_SUCCESS)
 	e1:SetCountLimit(1,100270023)
@@ -25,7 +25,7 @@ function c100270023.initial_effect(c)
 	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e3:SetProperty(EFFECT_FLAG_DELAY)
 	e3:SetCode(EVENT_SUMMON_SUCCESS)
-	e3:SetCountLimit(1,100270023+100)
+	e3:SetCountLimit(1,100270023)
 	e3:SetTarget(c100270023.tgtg)
 	e3:SetOperation(c100270023.tgop)
 	c:RegisterEffect(e3)
@@ -33,19 +33,22 @@ function c100270023.initial_effect(c)
 	e4:SetCode(EVENT_SPSUMMON_SUCCESS)
 	c:RegisterEffect(e4)
 end
+function c100270023.cfilter(c,tp)
+	return c:IsFaceup() and c:IsSetCard(0x126) and not c:IsCode(100270023) and c:GetSummonPlayer()==tp
+end
 function c100270023.spcon(e,tp,eg,ep,ev,re,r,rp)
-	local ec=eg:GetFirst()
-	return ep==tp and ec:IsSetCard(0x126) and not ec:IsCode(100270023)
+	return eg:IsExists(c100270023.cfilter,1,nil,tp)
 end
 function c100270023.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,true,false) end
+		and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
 end
 function c100270023.spop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
 	local c=e:GetHandler()
 	if c:IsRelateToEffect(e) then
-		Duel.SpecialSummon(c,0,tp,tp,true,false,POS_FACEUP)
+		Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
 	end
 end
 function c100270023.tgfilter(c)
