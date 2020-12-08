d0ddaa320739a30bdf8a6c9b89088726266e86bb
mallu11 <54900811+mallu11@users.noreply.github.com>
2020-12-09 01:31:34 +0800

    add 煉獄の釜

---
+++ scripts/21PP-JP/c100273021.lua
@@ -0,0 +1,52 @@
+--煉獄の釜
+
+--Scripted by mallu11
+function c100273021.initial_effect(c)
+	--activate
+	local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_ACTIVATE)
+	e0:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e0)
+	--destroy replace
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetCode(EFFECT_DESTROY_REPLACE)
+	e1:SetRange(LOCATION_SZONE)
+	e1:SetTarget(c100273021.desreptg)
+	e1:SetValue(c100273021.desrepval)
+	c:RegisterEffect(e1)
+	--to grave
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e2:SetCode(EVENT_ADJUST)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetOperation(c100273021.adjustop)
+	c:RegisterEffect(e2)
+end
+function c100273021.repfilter(c,tp)
+	return c:IsFaceup() and c:IsControler(tp) and c:IsLocation(LOCATION_MZONE)
+		and (c:IsSetCard(0xb) or c:IsRace(RACE_DRAGON) and c:IsAttribute(ATTRIBUTE_DARK) and c:IsLevel(8) and c:IsType(TYPE_SYNCHRO))
+		and c:IsReason(REASON_EFFECT) and not c:IsReason(REASON_REPLACE) and c:GetReasonPlayer()==tp
+end
+function c100273021.desfilter(c)
+	return c:IsSetCard(0xb) and c:IsAbleToRemove()
+end
+function c100273021.desreptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return eg:IsExists(c100273021.repfilter,1,nil,tp)
+		and Duel.IsExistingMatchingCard(aux.NecroValleyFilter(c100273021.desfilter),tp,LOCATION_GRAVE,0,1,nil) end
+	if Duel.SelectEffectYesNo(tp,c,96) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESREPLACE)
+		local g=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(c100273021.desfilter),tp,LOCATION_GRAVE,0,1,1,nil)
+		Duel.Remove(g,POS_FACEUP,REASON_EFFECT+REASON_REPLACE)
+		return true
+	else return false end
+end
+function c100273021.desrepval(e,c)
+	return c100273021.repfilter(c,e:GetHandlerPlayer())
+end
+function c100273021.adjustop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetFieldGroupCount(tp,LOCATION_HAND,0)==0 then return end
+	Duel.HintSelection(Group.FromCards(e:GetHandler()))
+	Duel.SendtoGrave(e:GetHandler(),REASON_EFFECT)
+end
