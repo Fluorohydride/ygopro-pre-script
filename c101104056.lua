182157333a7a95ebd965c4a0625cb5bc4e501025
KohanaSonogami <73727154+KohanaSonogami@users.noreply.github.com>
2020-12-09 22:42:32 +0900

    Create c101104056.lua

---
+++ c101104056.lua
@@ -0,0 +1,78 @@
+--アメージングタイムチケット
+--Amazing Time Ticket
+--LUA by Kohana Sonogami
+--
+function c101104056.initial_effect(c)
+	--Add 1 "Amazement" from your Deck to your hand
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(101104056,0))
+	e1:SetCategory(CATEGORY_NEGATE+CATEGORY_DESTROY)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCountLimit(1,101104056+EFFECT_COUNT_CODE_OATH)
+	e1:SetCost(c101104056.thcost)
+	e1:SetTarget(c101104056.thtg)
+	e1:SetOperation(c101104056.thop)
+	c:RegisterEffect(e1)
+	--Set 1 “∀ttraction" that can activate this turn
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(101104056,1))
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCountLimit(1,101104056+EFFECT_COUNT_CODE_OATH)
+	e1:SetCondition(c101104056.setcon)
+	e1:SetCost(c101104056.setcost)
+	e1:SetTarget(c101104056.settg)
+	e1:SetOperation(c101104056.setop)
+	c:RegisterEffect(e1)
+end
+function c101104056.thcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.CheckLPCost(tp,800) end
+	Duel.PayLPCost(tp,800)
+end
+function c101104056.thcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetTurnPlayer()==tp
+end
+function c101104056.thfilter(c)
+	return c:IsSetCard(0x25d) and c:IsAbleToHand()
+end
+function c101104056.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c101104056.thfilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function c101104056.thop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,c101104056.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if g:GetCount()>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end
+function c101104056.setfilter(c)
+	return c:IsSetCard(0x25a) and c:IsType(TYPE_SPELL+TYPE_TRAP) and c:IsSSetable()
+end
+function c101104056.setcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.CheckLPCost(tp,800) end
+	Duel.PayLPCost(tp,800)
+end
+function c101104056.setcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetTurnPlayer()~=tp
+end
+function c101104056.settg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c101104056.setfilter,tp,LOCATION_DECK,0,1,nil) end
+end
+function c101104056.setop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SET)
+	local g=Duel.SelectMatchingCard(tp,c101104056.setfilter,tp,LOCATION_DECK,0,1,1,nil)
+	local tc=g:GetFirst()
+	if tc and Duel.SSet(tp,tc)~=0 then
+		if tc:IsType(TYPE_SPELL+TYPE_TRAP) then
+			local e1=Effect.CreateEffect(e:GetHandler())
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetProperty(EFFECT_FLAG_SET_AVAILABLE)
+			e1:SetCode(EFFECT_QP_ACT_IN_SET_TURN)
+			e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+			tc:RegisterEffect(e1)
+		end
+	end
+end
