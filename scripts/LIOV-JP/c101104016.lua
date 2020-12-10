f70355c5899bb13902ccbc3724ce60bd818fbe86
mallu11 <54900811+mallu11@users.noreply.github.com>
2020-12-10 15:47:49 +0800

    add 蠱惑魔

---
+++ scripts/LIOV-JP/c101104016.lua
@@ -0,0 +1,82 @@
+--ジーナの蠱惑魔
+
+--Scripted by mallu11
+function c101104016.initial_effect(c)
+	--special summon
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(101104016,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCountLimit(1,101104016)
+	e1:SetCost(c101104016.spcost)
+	e1:SetTarget(c101104016.sptg)
+	e1:SetOperation(c101104016.spop)
+	c:RegisterEffect(e1)
+	--immune
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCode(EFFECT_IMMUNE_EFFECT)
+	e2:SetValue(c101104016.efilter)
+	c:RegisterEffect(e2)
+	--set
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(101104016,1))
+	e3:SetType(EFFECT_TYPE_IGNITION)
+	e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e3:SetRange(LOCATION_GRAVE)
+	e3:SetCountLimit(1,101104016)
+	e3:SetCondition(c101104016.setcon)
+	e3:SetCost(aux.bfgcost)
+	e3:SetTarget(c101104016.settg)
+	e3:SetOperation(c101104016.setop)
+	c:RegisterEffect(e3)
+end
+function c101104016.costfilter(c)
+	return c:IsFacedown() and c:IsType(TYPE_TRAP) and c:IsAbleToGraveAsCost()
+end
+function c101104016.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c101104016.costfilter,tp,LOCATION_SZONE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local g=Duel.SelectMatchingCard(tp,c101104016.costfilter,tp,LOCATION_SZONE,0,1,1,nil)
+	Duel.SendtoGrave(g,REASON_COST)
+end
+function c101104016.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and c:IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,0,0)
+end
+function c101104016.spop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) then
+		Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
+function c101104016.efilter(e,te)
+	local c=te:GetHandler()
+	return c:GetType()==TYPE_TRAP and c:IsSetCard(0x4c,0x89)
+end
+function c101104016.confilter(c)
+	return c:GetSequence()<5
+end
+function c101104016.setcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetMatchingGroupCount(c101104016.confilter,tp,LOCATION_SZONE,0,nil)==0
+end
+function c101104016.setfilter(c)
+	return c:GetType()==TYPE_TRAP and c:IsSetCard(0x4c,0x89) and c:IsSSetable()
+end
+function c101104016.settg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and c101104016.setfilter(chkc) end 
+	if chk==0 then return Duel.IsExistingTarget(c101104016.setfilter,tp,LOCATION_GRAVE,0,1,e:GetHandler()) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SET)
+	local g=Duel.SelectTarget(tp,c101104016.setfilter,tp,LOCATION_GRAVE,0,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_LEAVE_GRAVE,g,1,0,0)
+end
+function c101104016.setop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) then
+		Duel.SSet(tp,tc)
+	end
+end
