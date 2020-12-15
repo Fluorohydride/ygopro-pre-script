8112897665afa634c60a4cfb188e3cc4e25b743f
Mercury233 <me@mercury233.me>
2020-12-15 01:50:55 +0000

    Update c100272007.lua

--- scripts/PGB1-JP/c100272007.lua
+++ scripts/PGB1-JP/c100272007.lua
@@ -1,7 +1,6 @@
---ヨゲンシャ・ゾルガ
+--予見者ゾルガ
 --Zolga the Prophet
---Scripted by Kohana Sonogami
---
+--Scripted by Kohana Sonogami & mercury233
 function c100272007.initial_effect(c)
 	--special summon
 	local e1=Effect.CreateEffect(c)
@@ -16,17 +15,25 @@ function c100272007.initial_effect(c)
 	c:RegisterEffect(e1)
 	--damage
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(100272007,0))
-	e2:SetCategory(CATEGORY_RECOVER)
+	e2:SetDescription(aux.Stringid(100272007,1))
+	e2:SetCategory(CATEGORY_DESTROY+CATEGORY_DAMAGE)
 	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
 	e2:SetRange(LOCATION_GRAVE)
 	e2:SetCode(EVENT_ATTACK_ANNOUNCE)
-	e2:SetCost(aux.bfgcost)
 	e2:SetCountLimit(1,100272007+100)
 	e2:SetCondition(c100272007.dmcon)
+	e2:SetCost(aux.bfgcost)
 	e2:SetTarget(c100272007.dmtg)
 	e2:SetOperation(c100272007.dmop)
 	c:RegisterEffect(e2)
+	--effect reg
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e3:SetCode(EVENT_BE_MATERIAL)
+	e3:SetProperty(EFFECT_FLAG_EVENT_PLAYER)
+	e3:SetCondition(c100272007.regcon)
+	e3:SetOperation(c100272007.regop)
+	c:RegisterEffect(e3)
 end
 function c100272007.cfilter(c)
 	return c:IsFaceup() and c:IsAttribute(ATTRIBUTE_EARTH) and c:IsRace(RACE_FAIRY)
@@ -44,35 +51,52 @@ function c100272007.spop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if not c:IsRelateToEffect(e) then return end
 	if Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)~=0 then
-		local g=Duel.GetFieldGroup(tp,LOCATION_DECK,0)
-		local ct=1
-		if g:GetCount()>=5 then ct=Duel.AnnounceNumber(tp,1,2,3,4,5) end
-		Duel.ConfirmDecktop(tp,ct)
+		Duel.BreakEffect()
+		for p=tp,1-tp,1-tp-tp do
+			local g=Duel.GetFieldGroup(p,LOCATION_DECK,0)
+			local ct={}
+			for i=5,1,-1 do
+				if #g>=i then
+					table.insert(ct,i)
+				end
+			end
+			Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(100272007,(p==tp and 2 or 3)))
+			local ac=Duel.AnnounceNumber(tp,table.unpack(ct))
+			local sg=Duel.GetDecktopGroup(p,ac)
+			Duel.ConfirmCards(tp,sg)
+		end
 	end
 end
 function c100272007.dmcon(e,tp,eg,ev,ep,re,r,rp)
 	local c=e:GetHandler()
-	local tc=c:GetReasonCard()
 	local at=Duel.GetAttacker()
-	return e:GetHandler():IsReason(REASON_RELEASE) and not e:GetHandler():IsReason(REASON_EFFECT) and tc:IsType(TYPE_MONSTER) and at==tc
+	return at:GetFlagEffectLabel(100272007)==c:GetFieldID()
 end
 function c100272007.dmtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
 	local at=Duel.GetAttacker()
-	local tc=e:GetHandler():GetReasonCard()
-	if e:GetHandler():IsReason(REASON_RELEASE) and not e:GetHandler():IsReason(REASON_EFFECT) and tc:IsType(TYPE_MONSTER) and at==tc then
-		Duel.SetOperationInfo(0,CATEGORY_DESTROY,at,1,0,0)
-		Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,2000)
-	end
+	Duel.SetTargetCard(at)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,at,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,2000)
 end
 function c100272007.dmop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	local tc=e:GetHandler():GetReasonCard()
-	local at=Duel.GetAttacker()
-	if not tc:IsReason(REASON_SUMMON) or (not c:IsReason(REASON_RELEASE) or c:IsReason(REASON_EFFECT)) and not at==tc then return end
-	if at==tc then 
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) then 
 		if Duel.Destroy(tc,REASON_EFFECT)~=0 then
 			Duel.Damage(1-tp,2000,REASON_EFFECT)
 		end
+	end
+end
+function c100272007.regcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local rc=c:GetReasonCard()
+	return r==REASON_SUMMON and rc:IsFaceup()
+end
+function c100272007.regop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local rc=c:GetReasonCard()
+	if r==REASON_SUMMON and rc:IsFaceup() and c:IsLocation(LOCATION_GRAVE) then
+		rc:RegisterFlagEffect(100272007,RESET_EVENT+RESETS_STANDARD,EFFECT_FLAG_CLIENT_HINT,1,c:GetFieldID(),aux.Stringid(100272007,4))
 	end
 end
