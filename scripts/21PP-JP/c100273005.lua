1fd0c4089bac898c0b032dae934c58f91bfed348
Mercury233 <me@mercury233.me>
2020-12-17 01:04:43 +0000

    Update c100273005.lua

--- scripts/21PP-JP/c100273005.lua
+++ scripts/21PP-JP/c100273005.lua
@@ -1,12 +1,11 @@
 --フリント・クラッガー
 --Flint Cragger
 --Scripted by Kohana Sonogami
---
 function c100273005.initial_effect(c)
 	--tograve
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(100273005,0))
-	e1:SetCategory(CATEGORY_GRAVE)
+	e1:SetCategory(CATEGORY_TOGRAVE)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
 	e1:SetProperty(EFFECT_FLAG_DELAY)
@@ -17,12 +16,11 @@ function c100273005.initial_effect(c)
 	--return to gy
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(100273005,1))
-	e2:SetCategory(CATEGORY_TODECK)
-	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e2:SetProperty(EFFECT_FLAG_DELAY)
-	e2:SetCode(EVENT_TO_GRAVE)
+	e2:SetCategory(CATEGORY_DAMAGE)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_MZONE)
 	e2:SetCountLimit(1,100273005+100)
-	e2:SetCondition(c100273005.damcon)
+	e2:SetCost(c100273005.damcost)
 	e2:SetTarget(c100273005.damtg)
 	e2:SetOperation(c100273005.damop)
 	c:RegisterEffect(e2)
@@ -31,12 +29,13 @@ function c100273005.tgfilter(c)
 	return c:IsType(TYPE_FUSION) and c:IsSetCard(0x149) and c:IsAbleToGrave()
 end
 function c100273005.tgtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,1,e:GetHandler())
+	if chk==0 then return Duel.GetFieldGroupCount(tp,LOCATION_HAND,0)>0
 		and Duel.IsExistingMatchingCard(c100273005.tgfilter,tp,LOCATION_EXTRA,0,1,nil) end
-	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,e:GetHandler(),1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_HANDES,nil,0,tp,1)
+	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_EXTRA)
 end
 function c100273005.tgop(e,tp,eg,ep,ev,re,r,rp)
-	if Duel.DiscardHand(tp,Card.IsDiscardable,1,1,REASON_EFFECT+REASON_DISCARD)~=0 then
+	if Duel.DiscardHand(tp,nil,1,1,REASON_EFFECT+REASON_DISCARD)~=0 then
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
 		local g=Duel.SelectMatchingCard(tp,c100273005.tgfilter,tp,LOCATION_EXTRA,0,1,1,nil)
 		if g:GetCount()>0 then
@@ -44,31 +43,29 @@ function c100273005.tgop(e,tp,eg,ep,ev,re,r,rp)
 		end
 	end
 end
-function c100273005.damcon(e,tp,eg,ep,ev,re,r,rp)
-	return e:GetHandler():IsPreviousLocation(LOCATION_ONFIELD)
+function c100273005.damcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsAbleToGraveAsCost() end
+	Duel.SendtoGrave(e:GetHandler(),REASON_COST)
 end
 function c100273005.damtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
-	Duel.SetTargetPlayer(1-tp)
-	Duel.SetTargetParam(500)
 	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,500)
 end
-function c100273005.tgfilter1(c)
-	return (aux.IsCodeListed(c,59419719) or c:IsCode(59419719)) and c:IsAbleToDeck()
+function c100273005.rfilter(c)
+	return (aux.IsCodeListed(c,59419719) or c:IsCode(59419719)) and c:IsFaceup()
 end
 function c100273005.cfilter(c)
 	return c:IsCode(59419719)
 end
 function c100273005.damop(e,tp,eg,ep,ev,re,r,rp)
-	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
-	if Duel.Damage(p,d,REASON_EFFECT) and Duel.IsExistingMatchingCard(c100273005.cfilter,tp,LOCATION_GRAVE,0,1,nil)~=0 then
+	if Duel.Damage(1-tp,500,REASON_EFFECT)~=0
+		and Duel.IsExistingMatchingCard(c100273005.cfilter,tp,LOCATION_GRAVE,0,1,nil)
+		and Duel.IsExistingMatchingCard(c100273005.rfilter,tp,LOCATION_REMOVED,0,1,nil)
+		and Duel.SelectYesNo(tp,aux.Stringid(100273005,2)) then
+		Duel.BreakEffect()
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
-		local g=Duel.GetMatchingGroup(c100273005.tgfilter1,tp,LOCATION_REMOVED,0,nil)
-		if g:GetCount()>0 and Duel.SelectYesNo(tp,aux.Stringid(100273005,2)) then
-			Duel.BreakEffect()
-			local sg=g:Select(tp,1,1,nil)
-			Duel.HintSelection(sg)
-			Duel.SendtoGrave(sg,REASON_EFFECT+REASON_RETURN)
-		end
+		local sg=Duel.SelectMatchingCard(tp,c100273005.rfilter,tp,LOCATION_REMOVED,0,1,1,nil)
+		Duel.HintSelection(sg)
+		Duel.SendtoGrave(sg,REASON_EFFECT+REASON_RETURN)
 	end
 end
