fc7b46e6cb9709dce77c93922038aea9fc5ddcaf
KohanaSonogami <73727154+KohanaSonogami@users.noreply.github.com>
2020-12-20 13:38:54 +0900

    misread effect fix

--- scripts/LIOV-JP/c101104032.lua
+++ scripts/LIOV-JP/c101104032.lua
@@ -18,10 +18,12 @@ function c101104032.initial_effect(c)
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(101104032,1))
 	e2:SetCategory(CATEGORY_TOGRAVE)
-	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_CHAINING)
+	e2:SetProperty(EFFECT_FLAG_DELAY)
 	e2:SetRange(LOCATION_MZONE)
 	e2:SetCountLimit(1,101104032)
-	e2:SetCost(c101104032.tgcost)
+	e2:SetCondition(c101104032.tgcon)
 	e2:SetTarget(c101104032.tgtg)
 	e2:SetOperation(c101104032.tgop)
 	c:RegisterEffect(e2)
@@ -29,39 +31,34 @@ end
 function c101104032.splimit(e,c,tp,sumtp,sumpos)
 	return not c:IsSetCard(0x145)
 end
-function c101104032.tgfilter(c)
-	return c:IsAbleToGraveAsCost()
-end
-function c101104032.tgcost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(c101104032.tgfilter,tp,LOCATION_EXTRA,0,1,nil) end
-	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_EXTRA)
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
-	local g=Duel.SelectMatchingCard(tp,c101104032.tgfilter,tp,LOCATION_EXTRA,0,1,1,nil)
-	Duel.SendtoGrave(g,REASON_EFFECT)
+function c101104032.tgcon(e,tp,eg,ep,ev,re,r,rp)
+	return ep==1-tp
 end
 function c101104032.tgtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(c101104032.tgfilter,tp,LOCATION_EXTRA,0,1,nil) end
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsAbleToGrave,tp,LOCATION_EXTRA,0,1,nil)
+		and Duel.IsExistingMatchingCard(Card.IsAbleToGrave,tp,0,LOCATION_EXTRA,1,nil) end
 	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_EXTRA)
 end
 function c101104032.tgop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	local g=Duel.GetFieldGroup(tp,0,LOCATION_HAND)
-	if g:GetCount()>0 then
-		Duel.ConfirmCards(tp,g)
-		Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(101104032,1))
-		local tg=g:FilterSelect(tp,Card.IsType,1,1,nil,TYPE_MONSTER)
-		local tc=tg:GetFirst()
-		local atk=tc:GetAttack()//2
-		e:SetLabel(atk)
-		Duel.SendtoGrave(tc,REASON_EFFECT)
-		Duel.ShuffleHand(1-tp)
-		if c:IsFaceup() and c:IsRelateToEffect(e) then
-			local e1=Effect.CreateEffect(c)
-			e1:SetType(EFFECT_TYPE_SINGLE)
-			e1:SetCode(EFFECT_UPDATE_ATTACK)
-			e1:SetValue(e:GetLabel())
-			e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_DISABLE+RESET_PHASE+PHASE_END)
-			c:RegisterEffect(e1)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local g=Duel.SelectMatchingCard(tp,Card.IsAbleToGrave,tp,LOCATION_EXTRA,0,1,1,nil)
+	if g:GetCount()>0 and Duel.SendtoGrave(g,REASON_EFFECT) and g:GetFirst():IsLocation(LOCATION_GRAVE) then
+		local atk=g:GetFirst():GetTextAttack()
+		local rg=Duel.GetMatchingGroup(Card.IsAbleToGrave,tp,0,LOCATION_EXTRA,nil)
+		if #rg>0 then
+			Duel.ConfirmCards(tp,Duel.GetFieldGroup(tp,0,LOCATION_EXTRA))
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+			local sg=rg:Select(tp,1,1,nil)
+			Duel.SendtoGrave(sg,REASON_EFFECT)
+			Duel.ShuffleExtra(1-tp)
+			if sg:GetFirst():IsLocation(LOCATION_GRAVE) then e:SetLabel(sg:GetFirst():GetTextAttack()) end
 		end
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetValue((atk+e:GetLabel())/2)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_DISABLE+RESET_PHASE+PHASE_END)
+		c:RegisterEffect(e1)
 	end
 end
