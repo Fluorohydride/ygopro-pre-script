c4a1b390f0e18572b942c54a695f62c033215d30
Mercury233 <me@mercury233.me>
2020-12-24 01:58:50 +0000

    Update c101104044.lua

--- scripts/LIOV-JP/c101104044.lua
+++ scripts/LIOV-JP/c101104044.lua
@@ -1,7 +1,6 @@
---三旅団ベアブルーム固体暴行
---Tri-Brigade The Solid Assault
+--鉄獣戦線 塊撃のベアブルム
+--Tri-Brigade Bearbloom the Solid Assault
 --Scripted by Kohana Sonogami
---
 function c101104044.initial_effect(c)
 	c:EnableReviveLimit()
 	--link summon
@@ -12,6 +11,7 @@ function c101104044.initial_effect(c)
 	e1:SetCategory(CATEGORY_HANDES+CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetRange(LOCATION_MZONE)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetCountLimit(1,101104044)
 	e1:SetCost(c101104044.spcost)
 	e1:SetTarget(c101104044.sptg)
@@ -30,11 +30,11 @@ function c101104044.initial_effect(c)
 	c:RegisterEffect(e2)
 end
 function c101104044.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,1,e:GetHandler()) end
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,2,nil) end
 	Duel.DiscardHand(tp,Card.IsDiscardable,2,2,REASON_COST+REASON_DISCARD)
 end
 function c101104044.spfilter(c,e,tp)
-	return c:IsLevelBelow(4) and c:IsRace(RACE_BEAST+RACE_BEASTWARRIOR+RACE_WINDBEAST)
+	return c:IsFaceup() and c:IsLevelBelow(4) and c:IsRace(RACE_BEAST+RACE_BEASTWARRIOR+RACE_WINDBEAST)
 		and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function c101104044.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
@@ -57,28 +57,31 @@ end
 function c101104044.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(c101104044.thfilter,tp,LOCATION_DECK,0,1,nil) end
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
-	Duel.SetOperationInfo(0,CATEGORY_TODECK,nil,0,tp,1)
+	Duel.SetOperationInfo(0,CATEGORY_TODECK,nil,1,tp,LOCATION_HAND)
 end
 function c101104044.thop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
 	local g=Duel.SelectMatchingCard(tp,c101104044.thfilter,tp,LOCATION_DECK,0,1,1,nil)
-	if g:GetCount()>0 and Duel.SendtoHand(g,nil,REASON_EFFECT) and Duel.ConfirmCards(1-tp,g)~=0 then
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATODECK)
+	if g:GetCount()>0 and Duel.SendtoHand(g,nil,REASON_EFFECT)>0 then
+		Duel.ConfirmCards(1-tp,g)
+		Duel.ShuffleDeck(tp)
+		Duel.ShuffleHand(tp)
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
 		local sg=Duel.SelectMatchingCard(tp,Card.IsAbleToDeck,tp,LOCATION_HAND,0,1,1,nil)
 		if sg:GetCount()>0 then
 			Duel.BreakEffect()
 			Duel.SendtoDeck(sg,nil,1,REASON_EFFECT)
 		end
-		local e1=Effect.CreateEffect(c)
-		e1:SetType(EFFECT_TYPE_FIELD)
-		e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
-		e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-		e1:SetTargetRange(1,0)
-		e1:SetTarget(c101104044.splimit)
-		e1:SetReset(RESET_PHASE+PHASE_END)
-		Duel.RegisterEffect(e1,tp)
 	end
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetTargetRange(1,0)
+	e1:SetTarget(c101104044.splimit)
+	e1:SetReset(RESET_PHASE+PHASE_END)
+	Duel.RegisterEffect(e1,tp)
 end
 function c101104044.splimit(e,c)
 	return not c:IsSetCard(0x14d)
