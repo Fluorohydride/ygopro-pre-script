460eb34f47eb4315259f6bf758b4fe0cd3219e26
Mercury233 <me@mercury233.me>
2020-12-24 03:54:40 +0000

    Update c101104053.lua

--- scripts/LIOV-JP/c101104053.lua
+++ scripts/LIOV-JP/c101104053.lua
@@ -1,7 +1,6 @@
---ドグマティカ
+--凶導の福音
 --Dogmatikas
---Scripted by Kohana Sonogami
---
+--Scripted by Kohana Sonogami & mercury233
 function c101104053.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
@@ -19,39 +18,53 @@ end
 function c101104053.mfilter(c)
 	return c:GetLevel()>0 and c:IsType(TYPE_MONSTER) and c:IsAbleToGrave()
 end
+function c101104053.rfilter2(c,e,tp,m1)
+	if bit.band(c:GetType(),0x81)~=0x81 or not c:IsSetCard(0x145)
+		or not c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_RITUAL,tp,false,true) then return false end
+	local mg=m1:Filter(Card.IsCanBeRitualMaterial,c,c)
+	if c.mat_filter then
+		mg=mg:Filter(c.mat_filter,c,tp)
+	end
+	return mg:IsExists(Card.IsLevel,1,nil,c:GetLevel())
+end
 function c101104053.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then
-		local mg1=Duel.GetMatchingGroup(c101104053.mfilter,tp,LOCATION_EXTRA,0,nil)
-		local mg2=Duel.GetRitualMaterial(tp)
-		return Duel.IsExistingMatchingCard(aux.RitualUltimateFilter,tp,LOCATION_HAND,0,1,nil,c101104053.filter,e,tp,mg1,mg2,Card.GetLevel,"Equal")
+		local mg1=Duel.GetRitualMaterial(tp)
+		local mg2=Duel.GetMatchingGroup(c101104053.mfilter,tp,LOCATION_EXTRA,0,nil)
+		return Duel.IsExistingMatchingCard(c101104053.rfilter2,tp,LOCATION_HAND,0,1,nil,e,tp,mg2)
+			or Duel.IsExistingMatchingCard(aux.RitualUltimateFilter,tp,LOCATION_HAND,0,1,nil,c101104053.filter,e,tp,mg1,nil,Card.GetLevel,"Equal")
 	end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
 end
 function c101104053.activate(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	local mg1=Duel.GetMatchingGroup(c101104053.mfilter,tp,LOCATION_EXTRA,0,nil)
-	local mg2=Duel.GetRitualMaterial(tp)
+	local mg1=Duel.GetRitualMaterial(tp)
+	local mg2=Duel.GetMatchingGroup(c101104053.mfilter,tp,LOCATION_EXTRA,0,nil)
+	local g1=Duel.GetMatchingGroup(aux.RitualUltimateFilter,tp,LOCATION_HAND,0,nil,c101104053.filter,e,tp,mg1,nil,Card.GetLevel,"Equal")
+	local g2=Duel.GetMatchingGroup(c101104053.rfilter2,tp,LOCATION_HAND,0,nil,e,tp,mg2)
+	local g=g1+g2
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectMatchingCard(tp,aux.RitualUltimateFilter,tp,LOCATION_HAND,0,1,1,nil,c101104053.filter,e,tp,mg1,mg2,Card.GetLevel,"Equal")
-	local tc=g:GetFirst()
+	local tc=g:Select(tp,1,1,nil):GetFirst()
 	if tc then
 		local mg=mg1:Filter(Card.IsCanBeRitualMaterial,tc,tc)
-		mg:Merge(mg2)
 		if tc.mat_filter then
 			mg=mg:Filter(tc.mat_filter,tc,tp)
 		else
 			mg:RemoveCard(tc)
 		end
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
-		aux.GCheckAdditional=aux.RitualCheckAdditional(tc,tc:GetLevel(),"Equal")
-		local mat=mg:SelectSubGroup(tp,aux.RitualCheck,false,1,tc:GetLevel(),tp,tc,tc:GetLevel(),"Equal")
-		aux.GCheckAdditional=nil
-		if not mat or mat:GetCount()==0 then return end
-		tc:SetMaterial(mat)
-		local mat2=mat:Filter(Card.IsLocation,nil,LOCATION_EXTRA):Filter(Card.IsType,nil,TYPE_MONSTER)
-		mat:Sub(mat2)
-		Duel.ReleaseRitualMaterial(mat)
-		Duel.SendtoGrave(mat2,REASON_EFFECT+REASON_MATERIAL+REASON_RITUAL)
+		if g1:IsContains(tc) and (not g2:IsContains(tc) or not Duel.SelectYesNo(tp,aux.Stringid(101104053,0))) then
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
+			aux.GCheckAdditional=aux.RitualCheckAdditional(tc,tc:GetLevel(),"Equal")
+			local mat=mg:SelectSubGroup(tp,aux.RitualCheck,false,1,tc:GetLevel(),tp,tc,tc:GetLevel(),"Equal")
+			aux.GCheckAdditional=nil
+			tc:SetMaterial(mat)
+			Duel.ReleaseRitualMaterial(mat)
+		else
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+			local mat=mg2:FilterSelect(tp,Card.IsLevel,1,1,nil,tc:GetLevel())
+			tc:SetMaterial(mat)
+			Duel.SendtoGrave(mat,REASON_EFFECT+REASON_MATERIAL+REASON_RITUAL)
+		end
 		Duel.BreakEffect()
 		Duel.SpecialSummon(tc,SUMMON_TYPE_RITUAL,tp,tp,false,true,POS_FACEUP)
 		tc:CompleteProcedure()
