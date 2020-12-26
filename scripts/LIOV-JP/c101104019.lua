cdcef76f206fb1daa7c03b05913662f7d9211275
Mercury233 <me@mercury233.me>
2020-12-26 01:39:39 +0000

    Update c101104019.lua

--- scripts/LIOV-JP/c101104019.lua
+++ scripts/LIOV-JP/c101104019.lua
@@ -26,18 +26,18 @@ function c101104019.initial_effect(c)
 	e2:SetOperation(c101104019.spop2)
 	c:RegisterEffect(e2)
 end
-function c101104019.cfilter(c)
-	return (c:IsSetCard(0x25c) or c:IsCode(100270041)) and c:IsAbleToHand() 
+function c101104019.cfilter(c,tp)
+	return c:IsAbleToHandAsCost() and Duel.GetMZoneCount(tp,c)>0
+		and (c:IsSetCard(0x25c) and not c:IsCode(101104019) or c:IsRace(RACE_DRAGON) and c:IsLevel(7) and c:IsAttribute(ATTRIBUTE_LIGHT))
 end
 function c101104019.spcost1(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(c101104019.cfilter,tp,LOCATION_MZONE,0,1,nil) end
+	if chk==0 then return Duel.IsExistingMatchingCard(c101104019.cfilter,tp,LOCATION_MZONE,0,1,nil,tp) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RTOHAND)
-	local g=Duel.SelectMatchingCard(tp,c101104019.cfilter,tp,LOCATION_MZONE,0,1,1,nil)
-	Duel.Sendtohand(g,nil,REASON_COST)
+	local g=Duel.SelectMatchingCard(tp,c101104019.cfilter,tp,LOCATION_MZONE,0,1,1,nil,tp)
+	Duel.SendtoHand(g,nil,REASON_COST)
 end
 function c101104019.sptg1(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	if chk==0 then return e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
 end
 function c101104019.spop1(e,tp,eg,ep,ev,re,r,rp)
@@ -46,14 +46,14 @@ function c101104019.spop1(e,tp,eg,ep,ev,re,r,rp)
 		Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
 	end
 end
-function c101104019.spfilter(c,e,tp)
-	return c:IsRace(RACE_DRAGON) and c:IsLevel(7) and c:IsAttribute(ATTRIBUTE_LIGHT) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
-end
 function c101104019.spcon2(e,tp,eg,ep,ev,re,r,rp)
 	return Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0)==0
 end
+function c101104019.spfilter(c,e,tp)
+	return c:IsRace(RACE_DRAGON) and c:IsLevel(7) and c:IsAttribute(ATTRIBUTE_LIGHT) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
 function c101104019.sptg2(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>-1
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
 		and Duel.IsExistingMatchingCard(c101104019.spfilter,tp,LOCATION_HAND,0,1,nil,e,tp) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
 end
