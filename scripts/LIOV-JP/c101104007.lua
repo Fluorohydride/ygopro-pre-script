1fea13a872098b2886bdf5e0c9d456f2e422a77a
KohanaSonogami <73727154+KohanaSonogami@users.noreply.github.com>
2020-12-28 11:23:54 +0900

    Add last 2 Amazement and Ɐttraction Script (#648)

---
+++ scripts/LIOV-JP/c101104007.lua
@@ -0,0 +1,87 @@
+--驚楽園の大使 ＜Ｂｕｆｏ＞
+--Amazement Ambassador <Bufo>
+--Scripted by Kohana Sonogami
+function c101104007.initial_effect(c)
+	--equip
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(101104007,0))
+	e1:SetCategory(CATEGORY_EQUIP)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetCode(EVENT_SUMMON_SUCCESS)
+	e1:SetTarget(c101104007.eqtg1)
+	e1:SetOperation(c101104007.eqop1)
+	c:RegisterEffect(e1)
+	--equip change
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(101104007,1))
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetCountLimit(1,101104007)
+	e2:SetTarget(c101104007.eqtg2)
+	e2:SetOperation(c101104007.eqop2)
+	c:RegisterEffect(e2)
+end
+function c101104007.eqfilter(c)
+	return c:IsType(TYPE_TRAP) and c:IsSetCard(0x25f)
+end
+function c101104007.eqtg1(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return false end
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_SZONE)>0
+		and Duel.IsExistingTarget(c101104007.eqfilter,tp,LOCATION_GRAVE,0,1,nil)
+		and Duel.IsExistingTarget(Card.IsFaceup,tp,0,LOCATION_MZONE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)
+	local g1=Duel.SelectTarget(tp,c101104007.eqfilter,tp,LOCATION_GRAVE,0,1,1,nil)
+	e:SetLabelObject(g1:GetFirst())
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+	local g2=Duel.SelectTarget(tp,Card.IsFaceup,tp,0,LOCATION_MZONE,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_LEAVE_GRAVE,g1,1,0,0)
+end
+function c101104007.eqop1(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_SZONE)<=0 then return end
+	local ec=e:GetLabelObject()
+	local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS)
+	local tc=g:GetFirst()
+	if tc==ec then tc=g:GetNext() end
+	if ec:IsRelateToEffect(e) and tc:IsFaceup() and tc:IsRelateToEffect(e) then
+		Duel.Equip(tp,ec,tc)
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetProperty(EFFECT_FLAG_COPY_INHERIT+EFFECT_FLAG_OWNER_RELATE)
+		e1:SetCode(EFFECT_EQUIP_LIMIT)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		e1:SetValue(c101104007.eqlimit)
+		e1:SetLabelObject(tc)
+		ec:RegisterEffect(e1)
+	end
+end
+function c101104007.eqlimit(e,c)
+	return c==e:GetLabelObject()
+end
+function c101104007.eqfilter1(c,tp)
+	return c:IsSetCard(0x25f) and c:IsType(TYPE_TRAP) and c:GetEquipTarget()
+		and Duel.IsExistingTarget(c101104007.eqfilter2,tp,LOCATION_MZONE,LOCATION_MZONE,1,c:GetEquipTarget(),c,tp)
+end
+function c101104007.eqfilter2(c,ec,tp)
+	return c:IsFaceup() and (c:IsSetCard(0x25e) or not c:IsControler(tp)) and ec:CheckEquipTarget(c)
+end
+function c101104007.eqtg2(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return false end
+	if chk==0 then return Duel.IsExistingTarget(c101104007.eqfilter1,tp,LOCATION_SZONE,0,1,nil,tp) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+	local g1=Duel.SelectTarget(tp,c101104007.eqfilter1,tp,LOCATION_SZONE,0,1,1,nil,tp)
+	e:SetLabelObject(g1:GetFirst())
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)
+	local g2=Duel.SelectTarget(tp,c101104007.eqfilter2,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,tc:GetEquipTarget(),tc,tp)
+end
+function c101104007.eqop2(e,tp,eg,ep,ev,re,r,rp)
+	local ec=e:GetLabelObject()
+	local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS)
+	local tc=g:GetFirst()
+	if tc==ec then tc=g:GetNext() end
+	if ec:IsFaceup() and ec:IsRelateToEffect(e) and ec:CheckEquipTarget(tc) and tc:IsFaceup() and tc:IsRelateToEffect(e) then
+		Duel.Equip(tp,ec,tc)
+	end
+end
