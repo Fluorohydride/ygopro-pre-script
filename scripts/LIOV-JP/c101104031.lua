7a50b88519bafc6d6cc7220334b84924863b08ab
Mercury233 <me@mercury233.me>
2021-01-03 01:37:03 +0000

    Update c101104031.lua

--- scripts/LIOV-JP/c101104031.lua
+++ scripts/LIOV-JP/c101104031.lua
@@ -14,7 +14,7 @@ function c101104031.initial_effect(c)
 	c:RegisterEffect(e1)
 	--sp token
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(101104031,1))
+	e2:SetDescription(aux.Stringid(101104031,0))
 	e2:SetCategory(CATEGORY_COUNTER+CATEGORY_TOKEN)
 	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
 	e2:SetCode(EVENT_TO_GRAVE) 
@@ -26,13 +26,13 @@ function c101104031.initial_effect(c)
 	c:RegisterEffect(e2)
 end
 function c101104031.atkfilter(c)
-	return c:IsFaceup() and not c:IsType(TYPE_TUNER)
+	return c:IsFacedown() or c:IsFaceup() and not c:IsType(TYPE_TUNER)
 end
 function c101104031.atklm(e)
-	return Duel.IsExistingMatchingCard(c101104031.atkfilter,tp,LOCATION_MZONE,0,1,nil)
+	return Duel.IsExistingMatchingCard(c101104031.atkfilter,e:GetHandlerPlayer(),LOCATION_MZONE,0,1,nil)
 end
 function c101104031.cfilter(c,tp)
-	return c:IsPreviousLocation(LOCATION_MZONE) and c:GetPreviousControler()==1-tp and c:IsType(TYPE_MONSTER)
+	return c:IsPreviousLocation(LOCATION_MZONE) and c:IsControler(tp) and c:IsType(TYPE_MONSTER)
 end
 function c101104031.tkcon(e,tp,eg,ep,ev,re,r,rp)
 	return eg:IsExists(c101104031.cfilter,1,nil,1-tp)
@@ -46,12 +46,15 @@ function c101104031.tkfilter(c)
 end
 function c101104031.tkop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	if e:GetHandler():AddCounter(0x59,1)~=0
-		and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsPlayerCanSpecialSummonMonster(tp,101104100,0,0x4011,0,0,1,RACE_BEAST,ATTRIBUTE_EARTH) then
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	if e:GetHandler():AddCounter(0x59,1)~=0 and ft>0
+		and Duel.IsPlayerCanSpecialSummonMonster(tp,101104131,0,0x4011,0,0,1,RACE_BEAST,ATTRIBUTE_EARTH) then
+		Duel.BreakEffect()
 		local ct=c:GetCounter(0x59)
+		if Duel.IsPlayerAffectedByEffect(tp,59822133) then ft=1 end
+		if ct>ft then ct=ft end
 		while ct>0 do
-			local token=Duel.CreateToken(tp,101104100)
+			local token=Duel.CreateToken(tp,101104131)
 			local e1=Effect.CreateEffect(c)
 			e1:SetType(EFFECT_TYPE_SINGLE)
 			e1:SetCode(EFFECT_UPDATE_LEVEL)
@@ -70,8 +73,10 @@ function c101104031.tkop(e,tp,eg,ep,ev,re,r,rp)
 			e3:SetValue(c:GetCounter(0x59)*500)
 			e3:SetReset(RESET_EVENT+RESETS_STANDARD-RESET_TOFIELD)
 			token:RegisterEffect(e3)
-			Duel.SpecialSummon(token,0,tp,tp,false,false,POS_FACEUP)
+			Duel.SpecialSummonStep(token,0,tp,tp,false,false,POS_FACEUP)
 			ct=ct-1
+			if ct>0 and not Duel.SelectYesNo(tp,aux.Stringid(101104031,1)) then ct=0 end
 		end
+		Duel.SpecialSummonComplete()
 	end
 end
