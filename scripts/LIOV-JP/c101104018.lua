aa4c160d9a037ca2bc10276bcea2fbddd9c4cabb
Mercury233 <me@mercury233.me>
2021-01-04 00:45:36 +0000

    Update c101104018.lua

--- scripts/LIOV-JP/c101104018.lua
+++ scripts/LIOV-JP/c101104018.lua
@@ -1,4 +1,4 @@
---略奪者パトロールパイロット、レイヴンウィング
+--海造賊－黒翼の水先人
 --Ravenwing, the Plunder Patroll Pilot
 --Scripted by Kohana Sonogami
 function c101104018.initial_effect(c)
@@ -7,9 +7,9 @@ function c101104018.initial_effect(c)
 	e1:SetDescription(aux.Stringid(101104018,0))
 	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_IGNITION)
-	e1:SetCountLimit(1,101104018)
-	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetRange(LOCATION_HAND)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetCountLimit(1,101104018)
 	e1:SetTarget(c101104018.thtg)
 	e1:SetOperation(c101104018.thop)
 	c:RegisterEffect(e1)
@@ -17,7 +17,7 @@ function c101104018.initial_effect(c)
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(101104018,1))
 	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e2:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DELAY)
 	e2:SetCode(EVENT_TO_GRAVE)
 	e2:SetCountLimit(1,101104018+100)
 	e2:SetCondition(c101104018.spcon)
@@ -33,16 +33,15 @@ function c101104018.thtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
 		and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false)
 		and Duel.IsExistingTarget(c101104018.thfilter,tp,LOCATION_GRAVE,0,1,nil) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RTOHAND)
 	local g=Duel.SelectTarget(tp,c101104018.thfilter,tp,LOCATION_GRAVE,0,1,1,nil)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,0,0)
 end
 function c101104018.thop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	if not c:IsRelateToEffect(e) then return end
 	local tc=Duel.GetFirstTarget()
-	if Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)~=0 and tc:IsRelateToEffect(e) then
+	if c:IsRelateToEffect(e) and Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)~=0 and tc:IsRelateToEffect(e) then
 		Duel.SendtoHand(tc,nil,REASON_EFFECT)
 	end
 	local e1=Effect.CreateEffect(c)
@@ -57,12 +56,12 @@ end
 function c101104018.splimit(e,c,sump,sumtype,sumpos,targetp,se)
 	return not c:IsSetCard(0x13f)
 end
-function c101104018.spfilter(c,e,tp)
-	return c:IsSetCard(0x13f) and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP_DEFENSE)
-end
 function c101104018.spcon(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	return (c:IsPreviousLocation(LOCATION_MZONE) or c:IsPreviousLocation(LOCATION_HAND))
+	return c:IsPreviousLocation(LOCATION_HAND+LOCATION_MZONE)
+end
+function c101104018.spfilter(c,e,tp)
+	return c:IsSetCard(0x13f) and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP_DEFENSE)
 end
 function c101104018.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and c101104018.spfilter(chkc,e,tp) end
