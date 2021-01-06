c94580e2a1ac831d03a1002974c3d60a7088a509
Mercury233 <me@mercury233.me>
2021-01-06 01:03:06 +0000

    Update c101104079.lua

--- scripts/LIOV-JP/c101104079.lua
+++ scripts/LIOV-JP/c101104079.lua
@@ -14,21 +14,17 @@ function c101104079.initial_effect(c)
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(101104079,1))
 	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e2:SetCode(EVENT_DESTROYED)
-	e2:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
+	e2:SetCode(EVENT_TO_GRAVE)
+	e2:SetProperty(EFFECT_FLAG_DELAY)
 	e2:SetCountLimit(1,101104079)
 	e2:SetCondition(c101104079.setcon)
 	e2:SetTarget(c101104079.settg)
 	e2:SetOperation(c101104079.setop)
 	c:RegisterEffect(e2)
 end
-	--If a monster(s) battles, during damage calculation
 function c101104079.atcon(e,tp,eg,ep,ev,re,r,rp)
-	local ga=Duel.GetAttacker()
-	local gt=Duel.GetAttackTarget()
-	return ga and gt and ga:IsRelateToBattle() and gt:IsRelateToBattle() 
+	return Duel.GetBattleDamage(tp)>0
 end
-	--You take no damage from that battles
 function c101104079.atop(e,tp,eg,ep,ev,re,r,rp)
 	local e1=Effect.CreateEffect(e:GetHandler())
 	e1:SetType(EFFECT_TYPE_FIELD)
@@ -38,29 +34,25 @@ function c101104079.atop(e,tp,eg,ep,ev,re,r,rp)
 	e1:SetReset(RESET_PHASE+PHASE_DAMAGE)
 	Duel.RegisterEffect(e1,tp)
 end
-	--Check filter of Normal Trap Card
+function c101104079.setcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return bit.band(r,REASON_DESTROY+REASON_EFFECT)==REASON_DESTROY+REASON_EFFECT and rp==1-tp and c:GetPreviousControler()==tp
+		and c:IsPreviousLocation(LOCATION_ONFIELD) and c:IsPreviousPosition(POS_FACEDOWN)
+end
 function c101104079.setfilter(c)
 	return c:GetType()==TYPE_TRAP and not c:IsCode(101104079) and c:IsSSetable()
 end
-	--If this setdown card is destroyed by a card
-function c101104079.setcon(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	return rp==1-tp and c:IsReason(REASON_EFFECT) and c:GetPreviousControler()==tp and c:IsFacedown()
-end
-	--Check target of 2 Normal Trap Card
 function c101104079.settg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and c101104079.setfilter(chkc) end
 	if chk==0 then return Duel.IsExistingTarget(c101104079.setfilter,tp,LOCATION_GRAVE,0,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SET)
 	local g=Duel.SelectTarget(tp,c101104079.setfilter,tp,LOCATION_GRAVE,0,1,2,nil)
 end
-	--Set up to 2 Normal Traps to your field
 function c101104079.setop(e,tp,eg,ep,ev,re,r,rp)
 	local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS)
 	local tg=g:Filter(Card.IsRelateToEffect,nil,e)
-	local tc=tg:GetFirst()
+	Duel.SSet(tp,tg)
 	for tc in aux.Next(tg) do
-		Duel.SSet(tp,tg)
 		local e1=Effect.CreateEffect(e:GetHandler())
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_TRAP_ACT_IN_SET_TURN)
