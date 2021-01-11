fa8ee214e2a4c09fafeab43613d9acc0888da806
Mercury233 <me@mercury233.me>
2021-01-11 02:17:13 +0000

    Update c101104076.lua

--- scripts/LIOV-JP/c101104076.lua
+++ scripts/LIOV-JP/c101104076.lua
@@ -1,4 +1,4 @@
---果たしーAiー
+--果たし－Ai－
 --Mano A.I. Mano
 --Scripted by Kohana Sonogami
 function c101104076.initial_effect(c)
@@ -6,12 +6,14 @@ function c101104076.initial_effect(c)
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
+	e1:SetHintTiming(TIMING_DAMAGE_STEP)
 	c:RegisterEffect(e1)
 	--atk down
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_FIELD)
 	e2:SetCode(EFFECT_UPDATE_ATTACK)
-	e2:SetRange(LOCATION_MZONE)
+	e2:SetRange(LOCATION_SZONE)
 	e2:SetTargetRange(0,LOCATION_MZONE)
 	e2:SetValue(c101104076.atkval)
 	c:RegisterEffect(e2)
@@ -27,11 +29,11 @@ function c101104076.initial_effect(c)
 	c:RegisterEffect(e3)
 	--special summon
 	local e4=Effect.CreateEffect(c)
-	e4:SetDescription(aux.Stringid(id,0))
+	e4:SetDescription(aux.Stringid(101104076,0))
 	e4:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-	e4:SetCode(EVENT_DESTROYED)
-	e4:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
+	e4:SetCode(EVENT_BATTLE_DESTROYED)
+	e4:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e4:SetRange(LOCATION_SZONE)
 	e4:SetCountLimit(1,101104076)
 	e4:SetCondition(c101104076.spcon)
@@ -52,21 +54,20 @@ function c101104076.actcon(e)
 	return (a and c101104076.cfilter(a,tp)) or (d and c101104076.cfilter(d,tp))
 end
 function c101104076.cfilter(c,tp)
-	return c:IsSetCard(0x135) and c:IsReason(REASON_BATTLE)
-		and c:IsPreviousLocation(LOCATION_MZONE) and c:IsPreviousControler(tp)
+	return c:IsSetCard(0x135) and c:IsPreviousLocation(LOCATION_MZONE) and c:GetPreviousControler()==tp
 end
-function s.spcon(e,tp,eg,ep,ev,re,r,rp)
+function c101104076.spcon(e,tp,eg,ep,ev,re,r,rp)
 	return eg:IsExists(c101104076.cfilter,1,nil,tp)
 end
 function c101104076.spfilter(c,e,tp)
-	return c:GetAttack()==2300 and c:IsSetCard(0x135) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+	return c:GetAttack()==2300 and c:IsRace(RACE_CYBERSE) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function c101104076.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_GRAVE) and c101104076.spfilter(chkc,e,tp) and not eg:IsContains(chkc) end
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingTarget(c101104076.spfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
+		and Duel.IsExistingTarget(c101104076.spfilter,tp,LOCATION_GRAVE,0,1,eg,e,tp) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectTarget(tp,c101104076.spfilter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
+	local g=Duel.SelectTarget(tp,c101104076.spfilter,tp,LOCATION_GRAVE,0,1,1,eg,e,tp)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
 end
 function c101104076.spop(e,tp,eg,ep,ev,re,r,rp)
