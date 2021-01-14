ff2c68a29dc7d7d65bcea785b1b644c8073e21ef
コハナ ソノガミ <73727154+KohanaSonogami5967@users.noreply.github.com>
2021-01-14 21:20:17 +0800

    update

--- scripts/DBAG/c100416027.lua
+++ scripts/DBAG/c100416027.lua
@@ -72,7 +72,7 @@ end
 function c100416027.thop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
 	local g=Duel.SelectMatchingCard(tp,c100416027.thfilter,tp,LOCATION_DECK,0,1,1,nil)
-	if g:GetCount()>0 then
+	if #g>0 then
 		Duel.SendtoHand(g,nil,REASON_EFFECT)
 		Duel.ConfirmCards(1-tp,g)
 	end
