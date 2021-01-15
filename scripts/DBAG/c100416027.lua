6b37be1bf1bbe70d3b33f3038eb96b62cfb5716d
コハナ ソノガミ <73727154+KohanaSonogami5967@users.noreply.github.com>
2021-01-15 14:34:00 +0800

    update

--- scripts/DBAG/c100416027.lua
+++ scripts/DBAG/c100416027.lua
@@ -61,6 +61,17 @@ function c100416027.spop(e,tp,eg,ep,ev,re,r,rp)
 	if c:IsRelateToEffect(e) then
 		Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
 	end
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetTargetRange(1,0)
+	e1:SetTarget(c100416027.splimit)
+	e1:SetReset(RESET_PHASE+PHASE_END)
+	Duel.RegisterEffect(e1,tp)
+end
+function c100416027.splimit(e,c)
+	return not c:IsLevel(0)
 end
 function c100416027.thfilter(c)
 	return c:IsLevelBelow(0x261) and c:IsType(TYPE_MONSTER) and not c:IsCode(100416027) and c:IsAbleToHand()
