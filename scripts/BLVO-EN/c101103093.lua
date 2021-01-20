326a39890a8006870872fd0be2aeab00d4deeb88
コハナ ソノガミ <73727154+KohanaSonogami5967@users.noreply.github.com>
2021-01-21 07:31:05 +0800

    fix

--- scripts/BLVO-EN/c101103093.lua
+++ scripts/BLVO-EN/c101103093.lua
@@ -59,7 +59,7 @@ function c101103093.thop(e,tp,eg,ep,ev,re,r,rp)
 			e1:SetType(EFFECT_TYPE_SINGLE)
 			e1:SetCode(EFFECT_UPDATE_ATTACK)
 			e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-			e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+			e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_DISABLE+RESET_PHASE+PHASE_END,Duel.GetTurnPlayer()==tp and 2 or 1)
 			e1:SetValue(200)
 			tc:RegisterEffect(e1)
 		end
