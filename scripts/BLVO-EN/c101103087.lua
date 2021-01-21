c3d99b326ed81ffcc4e2964e7a56a2510aa755bd
mallu11 <54900811+mallu11@users.noreply.github.com>
2021-01-21 13:27:04 +0800

    Update c101103087.lua

--- scripts/BLVO-EN/c101103087.lua
+++ scripts/BLVO-EN/c101103087.lua
@@ -39,6 +39,7 @@ function c101103087.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
 end
 function c101103087.activate(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
 	local chkf=tp
 	local mg1=Duel.GetFusionMaterial(tp):Filter(aux.NOT(Card.IsImmuneToEffect),nil,e)
 	aux.FCheckAdditional=c101103087.fcheck
@@ -58,6 +59,7 @@ function c101103087.activate(e,tp,eg,ep,ev,re,r,rp)
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 		local tg=sg:Select(tp,1,1,nil)
 		local tc=tg:GetFirst()
+		local res=false
 		if sg1:IsContains(tc) and (sg2==nil or not sg2:IsContains(tc) or not Duel.SelectYesNo(tp,ce:GetDescription())) then
 			Auxiliary.FCheckAdditional=c101103087.fcheck
 			local mat1=Duel.SelectFusionMaterial(tp,tc,mg1,nil,chkf)
@@ -66,12 +68,33 @@ function c101103087.activate(e,tp,eg,ep,ev,re,r,rp)
 			Duel.SendtoGrave(mat1,REASON_EFFECT+REASON_MATERIAL+REASON_FUSION)
 			Duel.BreakEffect()
 			Duel.SpecialSummon(tc,SUMMON_TYPE_FUSION,tp,tp,false,false,POS_FACEUP)
+			res=true
 		else
 			local mat2=Duel.SelectFusionMaterial(tp,tc,mg2,nil,chkf)
 			local fop=ce:GetOperation()
 			fop(ce,e,tp,tc,mat2)
+			res=true
 		end
-		tc:CompleteProcedure()
+		if res then
+			tc:CompleteProcedure()
+			--cannot be destroyed
+			local e1=Effect.CreateEffect(c)
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+			e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+			e1:SetRange(LOCATION_MZONE)
+			e1:SetValue(1)
+			e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+			tc:RegisterEffect(e1,true)
+			--pierce
+			local e2=Effect.CreateEffect(c)
+			e2:SetDescription(aux.Stringid(101103087,0))
+			e2:SetType(EFFECT_TYPE_SINGLE)
+			e2:SetCode(EFFECT_PIERCE)
+			e2:SetProperty(EFFECT_FLAG_CLIENT_HINT)
+			e2:SetReset(RESET_EVENT+RESETS_STANDARD)
+			tc:RegisterEffect(e2,true)
+		end
 	end
 	aux.FCheckAdditional=nil
 end
