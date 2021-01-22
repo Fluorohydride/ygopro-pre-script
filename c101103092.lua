2f4ca935b6cb6bcdb7b061c91cec1a72abd4ba4f
MisterKay7 <42005481+MisterKay7@users.noreply.github.com>
2021-01-22 14:30:17 +0200

    Update c101103092.lua

--- c101103092.lua
+++ c101103092.lua
@@ -1,7 +1,6 @@
 --Smsm
 --Kay Aye
 --Underdog
-local s,id=GetID()
 function c101103092.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
@@ -13,8 +12,8 @@ function c101103092.initial_effect(c)
 	e2:SetType(EFFECT_TYPE_FIELD)
 	e2:SetRange(LOCATION_SZONE)
 	e2:SetTargetRange(0,LOCATION_MZONE)
-	e2:SetTarget(s.disable)
-	e2:SetCondition(s.discon)
+	e2:SetTarget(c101103092.disable)
+	e2:SetCondition(c101103092.discon)
 	e2:SetCode(EFFECT_DISABLE)
 	c:RegisterEffect(e2)
 	--disable 2
@@ -22,24 +21,24 @@ function c101103092.initial_effect(c)
 	e3:SetType(EFFECT_TYPE_FIELD)
 	e3:SetRange(LOCATION_SZONE)
 	e3:SetTargetRange(LOCATION_MZONE,0)
-	e3:SetTarget(s.disable)
-	e3:SetCondition(s.disconop)
+	e3:SetTarget(c101103092.disable)
+	e3:SetCondition(c101103092.disconop)
 	e3:SetCode(EFFECT_DISABLE)
 	c:RegisterEffect(e3)
 end
 
 
-function s.disable(e,c)
+function c101103092.disable(e,c)
 	return c:IsType(TYPE_EFFECT) or (c:GetOriginalType()&TYPE_EFFECT)==TYPE_EFFECT
 end
 
-function s.discon(e,tp,eg,ep,ev,re,r,rp,chk)
+function c101103092.discon(e,tp,eg,ep,ev,re,r,rp,chk)
 	
 	return Duel.GetTurnPlayer()== 0
 
 end
 
-function s.disconop(e,tp,eg,ep,ev,re,r,rp,chk)
+function c101103092.disconop(e,tp,eg,ep,ev,re,r,rp,chk)
 	
 	return Duel.GetTurnPlayer()== 1
 
