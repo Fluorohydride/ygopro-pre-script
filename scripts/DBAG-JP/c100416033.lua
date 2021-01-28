6fd9927543603a92e27e930c76f2352033a0cc71
mercury233 <me@mercury233.me>
2021-01-28 17:25:37 +0800

    update ベアルクティ－ポラリィ

--- scripts/DBAG-JP/c100416033.lua
+++ scripts/DBAG-JP/c100416033.lua
@@ -97,72 +97,48 @@ end
 function c100416033.rfilter(c,tp)
 	return c:IsLevelAbove(7) and (c:IsControler(tp) or c:IsFaceup())
 end
-function c100416033.rfilter2(c,e,tp)
-	return Duel.IsExistingMatchingCard(c100416033.cfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp,c)
-end
-function c100416033.cfilter(c,e,tp,mc)
-	if not (c:IsSetCard(0x261) and c:IsType(TYPE_MONSTER)) then return false end
-	local b1=c:IsAbleToHand()
-	local b2=c:IsCanBeSpecialSummoned(e,0,tp,false,false)
-	if not b1 and b2 then
-		return Duel.GetMZoneCount(tp,mc)>0
-	else
-		return b1 or b2
-	end
-end
 function c100416033.excostfilter(c,tp)
 	return c:IsAbleToRemove() and c:IsHasEffect(100416038,tp)
 end
-function c100416033.cfilter2(c,e,tp,ft)
-	return Duel.IsExistingMatchingCard(c100416033.thfilter,tp,LOCATION_GRAVE,0,1,c,e,tp,ft)
+function c100416033.costfilter(c,e,tp)
+	local check=Duel.GetMZoneCount(tp,c)>0
+	return Duel.IsExistingMatchingCard(c100416033.tgfilter,tp,LOCATION_GRAVE,0,1,c,e,tp,check)
+end
+function c100416033.tgfilter(c,e,tp,check)
+	return c:IsSetCard(0x261) and c:IsType(TYPE_MONSTER)
+		and (c:IsAbleToHand() or check and c:IsCanBeSpecialSummoned(e,0,tp,false,false))
 end
 function c100416033.thcost(e,tp,eg,ep,ev,re,r,rp,chk)
-	local g1=Duel.GetReleaseGroup(tp):Filter(c100416033.rfilter,nil,tp)
+	local g1=Duel.GetReleaseGroup(tp):Filter(c100416033.rfilter,e:GetHandler(),tp)
 	local g2=Duel.GetMatchingGroup(c100416033.excostfilter,tp,LOCATION_GRAVE,0,nil,tp)
-	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
-	local b1=g1:IsExists(c100416033.rfilter2,1,nil,e,tp)
-	local b2=g2:IsExists(c100416033.cfilter2,1,nil,e,tp,ft)
-	if chk==0 then return b1 or b2 end
-	local s=0
-	if b1 and not b2 then
-		s=0
-	elseif not b1 and b2 then
-		s=1
-	elseif b1 and b2 then
-		s=Duel.SelectOption(tp,aux.Stringid(100416033,2),aux.Stringid(100416033,3))
-	end
-	local tc
-	if s==0 then
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
-		tc=g1:FilterSelect(tp,c100416033.rfilter2,1,1,nil,e,tp):GetFirst()
-		Duel.Release(tc,REASON_COST)
-	else
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
-		tc=g2:FilterSelect(tp,c100416033.cfilter2,1,1,nil,e,tp,ft):GetFirst()
+	g1:Merge(g2)
+	if chk==0 then return g1:IsExists(c100416033.costfilter,1,nil,e,tp) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
+	tc=g1:FilterSelect(tp,c100416033.costfilter,1,1,nil,e,tp):GetFirst()
+	if tc:IsLocation(LOCATION_GRAVE) then
 		local te=tc:IsHasEffect(100416038,tp)
 		if te then
 			te:UseCountLimit(tp)
 			Duel.Remove(tc,POS_FACEUP,REASON_EFFECT+REASON_REPLACE)
 		end
+	else
+		Duel.Release(tc,REASON_COST)
 	end
 end
-function c100416033.thfilter(c,e,tp,ft)
-	return c:IsSetCard(0x261) and c:IsType(TYPE_MONSTER) and (c:IsAbleToHand() or c:IsCanBeSpecialSummoned(e,0,tp,false,false) and (not ft or ft>0))
-end
 function c100416033.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(c100416033.thfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
+	if chk==0 then return Duel.IsExistingMatchingCard(c100416033.tgfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp,true) end
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,0,tp,LOCATION_GRAVE)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,0,tp,LOCATION_GRAVE)
 end
 function c100416033.thop(e,tp,eg,ep,ev,re,r,rp)
+	local check=Duel.GetLocationCount(tp,LOCATION_MZONE)>0
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_OPERATECARD)
-	local g=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(c100416033.thfilter),tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
-	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	local g=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(c100416033.tgfilter),tp,LOCATION_GRAVE,0,1,1,nil,e,tp,check)
 	local tc=g:GetFirst()
 	if tc then
-		if tc:IsAbleToHand() and (not tc:IsCanBeSpecialSummoned(e,0,tp,false,false) or ft<=0 or Duel.SelectOption(tp,1190,1152)==0) then
+		if tc:IsAbleToHand() and (not tc:IsCanBeSpecialSummoned(e,0,tp,false,false) or not check
+			or Duel.SelectOption(tp,1190,1152)==0) then
 			Duel.SendtoHand(tc,nil,REASON_EFFECT)
-			Duel.ConfirmCards(1-tp,tc)
 		else
 			Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
 		end
