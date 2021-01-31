9029f5849358ffed07a90c033fca57846be4415e
mallu11 <54900811+mallu11@users.noreply.github.com>
2021-01-31 19:33:21 +0800

    fix A・Ɐ・MM

--- scripts/LIOV-JP/c101104073.lua
+++ scripts/LIOV-JP/c101104073.lua
@@ -68,7 +68,8 @@ function c101104073.filter(c,tp)
 end
 function c101104073.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_MZONE) and c101104073.filter(chkc,tp) end
-	if chk==0 then return Duel.IsExistingTarget(c101104073.filter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil,tp) end
+	if chk==0 then return e:IsHasType(EFFECT_TYPE_ACTIVATE)
+		and Duel.IsExistingTarget(c101104073.filter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil,tp) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)
 	local g=Duel.SelectTarget(tp,c101104073.filter,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil,tp)
 	Duel.SetOperationInfo(0,CATEGORY_EQUIP,e:GetHandler(),1,0,0)
