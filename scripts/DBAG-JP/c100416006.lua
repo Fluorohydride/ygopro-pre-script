7418ebcbe2f41c3e45b1f8f9bb488cb8418dcc55
mallu11 <54900811+mallu11@users.noreply.github.com>
2021-02-09 16:02:29 +0800

    add 溟界

---
+++ scripts/DBAG-JP/c100416006.lua
@@ -0,0 +1,72 @@
+--溟界の昏闇－アレート
+
+--Scripted by mallu11
+function c100416006.initial_effect(c)
+	--spsummon
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(100416006,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_GRAVE_ACTION)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_GRAVE)
+	e1:SetCountLimit(1,100416006)
+	e1:SetCost(c100416006.spcost)
+	e1:SetTarget(c100416006.sptg)
+	e1:SetOperation(c100416006.spop)
+	c:RegisterEffect(e1)
+	--return to grave
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(100416006,1))
+	e2:SetCategory(CATEGORY_TOGRAVE)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e2:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
+	e2:SetCountLimit(1,100416006+100)
+	e2:SetTarget(c100416006.rgtg)
+	e2:SetOperation(c100416006.rgop)
+	c:RegisterEffect(e2)
+end
+function c100416006.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	local g=Duel.GetReleaseGroup(tp)
+	if chk==0 then return g:CheckSubGroup(aux.mzctcheckrel,1,1,tp) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
+	local rg=g:SelectSubGroup(tp,aux.mzctcheckrel,false,1,1,tp)
+	Duel.Release(rg,REASON_COST)
+end
+function c100416006.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
+end
+function c100416006.thfilter(c,tp)
+	return c:IsType(TYPE_MONSTER) and c:IsAbleToHand(1-tp)
+end
+function c100416006.spop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) and Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)~=0 and Duel.IsExistingMatchingCard(aux.NecroValleyFilter(c100416006.thfilter),tp,0,LOCATION_GRAVE,1,nil,tp) and Duel.SelectYesNo(1-tp,aux.Stringid(100416006,2)) then
+		Duel.BreakEffect()
+		Duel.Hint(HINT_SELECTMSG,1-tp,HINTMSG_ATOHAND)
+		local g=Duel.SelectMatchingCard(1-tp,aux.NecroValleyFilter(c100416006.thfilter),tp,0,LOCATION_GRAVE,1,1,nil,tp)
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(tp,g)
+	end
+end
+function c100416006.rgfilter(c,e)
+	return c:IsFaceup() and c:IsType(TYPE_MONSTER) and c:IsCanBeEffectTarget(e)
+end
+function c100416006.fselect(g)
+	return g:IsExists(Card.IsRace,1,nil,RACE_REPTILE)
+end
+function c100416006.rgtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return false end
+	local g=Duel.GetMatchingGroup(c100416006.rgfilter,tp,LOCATION_REMOVED,0,nil,e)
+	if chk==0 then return g:CheckSubGroup(c100416006.fselect,2,2) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local sg=g:SelectSubGroup(tp,c100416006.fselect,false,2,2)
+	Duel.SetTargetCard(sg)
+	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,sg,2,0,0)
+end
+function c100416006.rgop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS):Filter(Card.IsRelateToEffect,nil,e)
+	if #g>0 then
+		Duel.SendtoGrave(g,REASON_EFFECT+REASON_RETURN)
+	end
+end
---
+++ scripts/DBAG-JP/c100416008.lua
@@ -0,0 +1,100 @@
+--溟界妃－アミュネシア
+
+--Scripted by mallu11
+function c100416008.initial_effect(c)
+	--spsummon
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(100416008,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_GRAVE)
+	e1:SetCountLimit(1,100416008)
+	e1:SetCost(c100416008.spcost)
+	e1:SetTarget(c100416008.sptg)
+	e1:SetOperation(c100416008.spop)
+	c:RegisterEffect(e1)
+	--to grave
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(100416008,1))
+	e2:SetCategory(CATEGORY_TOGRAVE)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e2:SetProperty(EFFECT_FLAG_DELAY)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1,100416008+100)
+	e2:SetCondition(c100416008.tgcon)
+	e2:SetTarget(c100416008.tgtg)
+	e2:SetOperation(c100416008.tgop)
+	c:RegisterEffect(e2)
+	--spsummon2
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(100416008,2))
+	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e3:SetCode(EVENT_TO_GRAVE)
+	e3:SetProperty(EFFECT_FLAG_DELAY)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCountLimit(1,100416008+200)
+	e3:SetCondition(c100416008.spcon2)
+	e3:SetTarget(c100416008.sptg2)
+	e3:SetOperation(c100416008.spop2)
+	c:RegisterEffect(e3)
+end
+function c100416008.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	local g=Duel.GetReleaseGroup(tp)
+	if chk==0 then return g:CheckSubGroup(aux.mzctcheckrel,2,2,tp) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
+	local rg=g:SelectSubGroup(tp,aux.mzctcheckrel,false,2,2,tp)
+	Duel.Release(rg,REASON_COST)
+end
+function c100416008.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
+end
+function c100416008.spop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) then
+		Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
+function c100416008.cfilter(c,tp)
+	return c:IsPreviousControler(1-tp) and c:IsPreviousLocation(LOCATION_GRAVE) and c:GetOriginalType()&TYPE_MONSTER~=0
+end
+function c100416008.tgcon(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(c100416008.cfilter,1,e:GetHandler(),tp)
+end
+function c100416008.tgtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local g=Duel.GetFieldGroup(tp,0,LOCATION_ONFIELD)
+	if chk==0 then return #g>0 end
+	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,g,1,0,0)
+end
+function c100416008.tgop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetFieldGroup(tp,0,LOCATION_ONFIELD)
+	if #g>0 then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+		local sg=g:Select(tp,1,1,nil)
+		Duel.HintSelection(sg)
+		Duel.SendtoGrave(sg,REASON_EFFECT)
+	end
+end
+function c100416008.cfilter2(c,tp)
+	return c:IsType(TYPE_MONSTER) and c:IsPreviousControler(1-tp) and c:IsPreviousLocation(LOCATION_HAND+LOCATION_DECK)
+end
+function c100416008.spcon2(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(c100416008.cfilter2,1,nil,tp)
+end
+function c100416008.spfilter(c,e,tp)
+	return not c:IsCode(100416008) and c:IsAttribute(ATTRIBUTE_LIGHT+ATTRIBUTE_DARK) and c:IsRace(RACE_REPTILE) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function c100416008.sptg2(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.IsExistingMatchingCard(c100416008.spfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE)
+end
+function c100416008.spop2(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(c100416008.spfilter),tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
+	if g:GetCount()>0 then
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
