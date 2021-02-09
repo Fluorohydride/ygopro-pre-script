5a84dc06c85a7383172648408903b29b17e5eacb
mallu11 <54900811+mallu11@users.noreply.github.com>
2021-02-09 21:59:05 +0800

    Update c100416008.lua

--- scripts/DBAG-JP/c100416008.lua
+++ scripts/DBAG-JP/c100416008.lua
@@ -45,6 +45,7 @@ function c100416008.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return g:CheckSubGroup(aux.mzctcheckrel,2,2,tp) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
 	local rg=g:SelectSubGroup(tp,aux.mzctcheckrel,false,2,2,tp)
+	aux.UseExtraReleaseCount(rg,tp)
 	Duel.Release(rg,REASON_COST)
 end
 function c100416008.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
