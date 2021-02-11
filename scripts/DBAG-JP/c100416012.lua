7fb1b8a9e35300647fa47ad705b328a2aa6d8352
XyleN5967 <73727154+XyleN5967@users.noreply.github.com>
2021-02-11 09:32:33 +0800

    add 冥界の席

---
+++ scripts/DBAG-JP/c100416012.lua
@@ -0,0 +1,77 @@
+--冥界の席
+
+--scripted by Xylen5967
+function c100416012.initial_effect(c)
+	--activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE) 
+	e1:SetCode(EVENT_FREE_CHAIN) 
+	c:RegisterEffect(e1)
+	--special summon
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(100416012,0))
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetCountLimit(1,100416012)
+	e2:SetCost(c100416012.spcost)
+	e2:SetTarget(c100416012.sptg)
+	e2:SetOperation(c100416012.spop)
+	c:RegisterEffect(e2)
+	--to grave
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(100416012,1))
+	e3:SetCategory(CATEGORY_TOGRAVE)
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e3:SetCode(EVENT_TO_GRAVE)
+	e3:SetProperty(EFFECT_FLAG_DELAY)
+	e3:SetCountLimit(1,100416012+100)
+	e3:SetCondition(c100416012.tgcon)
+	e3:SetTarget(c100416012.tgtg)
+	e3:SetOperation(c100416012.tgop)
+	c:RegisterEffect(e3)
+end
+function c100416012.rfilter(c,tp)
+	return c:IsRace(RACE_REPTILE) and (c:IsControler(tp) or c:IsFaceup()) and Duel.GetMZoneCount(tp,c)>0
+end
+function c100416012.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	if chk==0 then return Duel.CheckReleaseGroup(tp,c100416012.rfilter,1,nil,tp) end
+	local g=Duel.SelectReleaseGroup(tp,c100416012.rfilter,1,1,nil,tp)
+	Duel.Release(g,REASON_COST)
+end
+function c100416012.spfilter(c,e,tp)
+	return c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function c100416012.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_GRAVE) and c100416012.spfilter(chkc,e,tp) end
+	if chk==0 then return Duel.IsExistingTarget(c100416012.spfilter,tp,0,LOCATION_GRAVE,1,nil,e,tp) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectTarget(tp,c100416012.spfilter,tp,0,LOCATION_GRAVE,1,1,nil,e,tp)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
+end
+function c100416012.spop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) then
+		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
+function c100416012.tgcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return c:IsPreviousLocation(LOCATION_SZONE) and c:IsPreviousPosition(POS_FACEUP)
+end
+function c100416012.cfilter(c)
+	return not c:IsRace(RACE_REPTILE) and c:IsFaceup() and c:IsAbleToGrave() 
+end
+function c100416012.tgtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local g=Duel.GetMatchingGroup(c100416012.cfilter,tp,LOCATION_MZONE,0,nil)
+	if chk==0 then return #g>0 end
+	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,0,LOCATION_MZONE)
+end
+function c100416012.tgop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(c100416012.cfilter,tp,LOCATION_MZONE,0,nil)
+	if #g>0 then
+		Duel.SendtoGrave(g,REASON_EFFECT)
+	end
+end
