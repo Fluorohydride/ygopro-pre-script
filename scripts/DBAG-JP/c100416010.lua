58df6c20bda3fb5055114b7d31194d42e59a1568
mercury233 <me@mercury233.me>
2021-02-28 22:30:22 +0800

    fix 溟界の蛇睡蓮

--- scripts/DBAG-JP/c100416010.lua
+++ scripts/DBAG-JP/c100416010.lua
@@ -4,7 +4,7 @@
 function c100416010.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_TOGRAVE+CATEGORY_GRAVE_SPSUMMON)
+	e1:SetCategory(CATEGORY_TOGRAVE+CATEGORY_SPECIAL_SUMMON+CATEGORY_GRAVE_SPSUMMON)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetCountLimit(1,100416010)
