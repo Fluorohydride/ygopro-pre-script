fdd217b3e41abb6f250ebe28ee5196f48a67b05d
Mercury233 <me@mercury233.me>
2021-02-28 09:02:08 +0000

    Update c100416010.lua

--- scripts/DBAG-JP/c100416010.lua
+++ scripts/DBAG-JP/c100416010.lua
@@ -1,10 +1,10 @@
---溟界的蛇睡莲
+--溟界の蛇睡蓮
 
 --Scripted by Lighty_The_Light
 function c100416010.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_TOGRAVE)
+	e1:SetCategory(CATEGORY_TOGRAVE+CATEGORY_GRAVE_SPSUMMON)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetCountLimit(1,100416010)
@@ -19,31 +19,26 @@ function c100416010.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(c100416010.tgfilter,tp,LOCATION_DECK,0,1,nil) end
 	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_DECK)
 end
-function c100416010.spfilter1(c)
+function c100416010.cfilter(c)
 	return c:IsType(TYPE_MONSTER) and c:IsRace(RACE_REPTILE)
 end
-function c100416010.spfilter2(c,e,tp)
-	return c100416010.spfilter1(c) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+function c100416010.spfilter(c,e,tp)
+	return c100416010.cfilter(c) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function c100416010.activate(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
 	local g=Duel.SelectMatchingCard(tp,c100416010.tgfilter,tp,LOCATION_DECK,0,1,1,nil)
 	if g:GetCount()==0 then return end
 	if Duel.SendtoGrave(g,REASON_EFFECT)==0 then return end
-
-	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return false end
-
-	local g=Duel.GetMatchingGroup(c100416010.spfilter1,tp,LOCATION_GRAVE,0,nil)
-	local ct=g:GetClassCount(Card.GetCode)
-	if ct<5 then return end
-	
-	if Duel.SelectYesNo(tp,aux.Stringid(100416010,0)) then
-		local sg=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(c100416010.spfilter2),tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
-		local tc=sg:GetFirst()
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	local cg=Duel.GetMatchingGroup(c100416010.cfilter,tp,LOCATION_GRAVE,0,nil)
+	local ct=cg:GetClassCount(Card.GetCode)
+	if ct<5 then return end	
+	if Duel.IsExistingMatchingCard(aux.NecroValleyFilter(c100416010.spfilter),tp,LOCATION_GRAVE,0,1,nil,e,tp)
+		and Duel.SelectYesNo(tp,aux.Stringid(100416010,0)) then
 		Duel.BreakEffect()
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
+		local sg=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(c100416010.spfilter),tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
+		Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP)
 	end
 end
-
-
