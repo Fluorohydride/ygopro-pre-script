f8862d1e1b931cce689a2a10fc70fce26e74f746
Mercury233 <me@mercury233.me>
2021-02-28 08:40:23 +0000

    Update c101105207.lua

--- scripts/Dawn of Majesty/c101105207.lua
+++ scripts/Dawn of Majesty/c101105207.lua
@@ -14,47 +14,48 @@ function c101105207.initial_effect(c)
 	c:RegisterEffect(e1)
 end
 function c101105207.filter(c)
-	return (c:IsCode(44508094) or c:IsType(TYPE_SYNCHRO) and aux.IsCodeListed(c,44508094))
+	return c:IsFaceup() and (c:IsCode(44508094) or c:IsType(TYPE_SYNCHRO) and aux.IsCodeListed(c,44508094))
+end
+function c101105207.eqfilter(c)
+	return c:IsFaceup() and c:IsAbleToChangeControler()
 end
 function c101105207.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_MZONE) and c101105207.filter(chkc) end
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_SZONE)>0
-		and Duel.IsExistingTarget(c101105207.filter,tp,LOCATION_MZONE,0,1,nil)
-		and Duel.IsExistingMatchingCard(c101105207.eqfilter,tp,0,LOCATION_MZONE,1,nil) end
+	local b1=Duel.GetLocationCount(tp,LOCATION_SZONE)>0
+		and Duel.IsExistingMatchingCard(c101105207.eqfilter,tp,0,LOCATION_MZONE,1,nil)
+	local b2=aux.bpcon()
+	if chk==0 then return (b1 or b2)
+		and Duel.IsExistingTarget(c101105207.filter,tp,LOCATION_MZONE,0,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
 	local g=Duel.SelectTarget(tp,c101105207.filter,tp,LOCATION_MZONE,0,1,1,nil)
 	local op
-	local tc=g:GetFirst() 
-	if tc then op=Duel.SelectOption(tp,aux.Stringid(101105207,0),aux.Stringid(101105207,1),aux.Stringid(101105207,2)) 
-	elseif tc:IsFaceup() then
-		op=Duel.SelectOption(tp,aux.Stringid(101105207,0)) --Equip 1 face-up monster your opponent controls to that target.
-	elseif not tc:IsFaceup() then
-		op=Duel.SelectOption(tp,aux.Stringid(101105207,1))+1 --That target can attack directly this turn.
+	if b1 and b2 then
+		op=Duel.SelectOption(tp,aux.Stringid(101105207,0),aux.Stringid(101105207,1),aux.Stringid(101105207,2)) 
+	elseif b1 then
+		op=Duel.SelectOption(tp,aux.Stringid(101105207,0))
 	else
-		op=Duel.SelectOption(tp,aux.Stringid(101105207,2))+2 --inflict damage to your opponent equal to the destroyed monster’s original ATK.
+		op=Duel.SelectOption(tp,aux.Stringid(101105207,1),aux.Stringid(101105207,2))+1
 	end
 	e:SetLabel(op)
 	if op==0 then
 		e:SetCategory(CATEGORY_EQUIP)
 		Duel.SetOperationInfo(0,CATEGORY_EQUIP,g,1,0,0)
-	elseif op==1 then
-		e:SetCategory(0)
 	else
-		e:SetCategory(CATEGORY_DAMAGE)
+		e:SetCategory(0)
 	end
 end
 function c101105207.activate(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local op=e:GetLabel()
 	if op==0 then
-		equip(e,tp,eg,ep,ev,re,r,rp)
+		c101105207.equip(e,tp,eg,ep,ev,re,r,rp)
 	elseif op==1 then
-		directattack(e,tp,eg,ep,ev,re,r,rp)
+		c101105207.directattack(e,tp,eg,ep,ev,re,r,rp)
 	else
-		damage(e,tp,eg,ep,ev,re,r,rp)
+		c101105207.damage(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
-function equip(e,tp,eg,ep,ev,re,r,rp)
+function c101105207.equip(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local tc=Duel.GetFirstTarget()
 	if tc:IsRelateToEffect(e) and tc:IsFaceup() and Duel.GetLocationCount(tp,LOCATION_SZONE)>0 then
@@ -78,17 +79,18 @@ end
 function c101105207.eqlimit(e,c)
 	return c==e:GetLabelObject()
 end
-function directattack(e,tp,eg,ep,ev,re,r,rp)
+function c101105207.directattack(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local tc=Duel.GetFirstTarget()
-	if tc:IsRelateToEffect(e) then
+	if tc:IsRelateToEffect(e) and tc:IsFaceup() then
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_DIRECT_ATTACK)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
 		tc:RegisterEffect(e1)
 	end
 end
-function damage(e,tp,eg,ep,ev,re,r,rp)
+function c101105207.damage(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local tc=Duel.GetFirstTarget()
 	if tc:IsFacedown() or not tc:IsRelateToEffect(e) or tc:IsControler(1-tp) then return end
@@ -96,7 +98,7 @@ function damage(e,tp,eg,ep,ev,re,r,rp)
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 	e1:SetCode(EVENT_BATTLE_DESTROYING)
-	e1:SetReset(RESET_PHASE+RESETS_STANDARD)
+	e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
 	e1:SetLabelObject(tc)
 	e1:SetCondition(c101105207.damcon)
 	e1:SetOperation(c101105207.damop)
