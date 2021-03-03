fe53077068d460e56f9d50e0df741d8edd1f9de7
mallu11 <54900811+mallu11@users.noreply.github.com>
2021-03-03 20:36:18 +0800

    fix 光来する奇跡

--- scripts/DAMA-JP/c101105205.lua
+++ scripts/DAMA-JP/c101105205.lua
@@ -24,7 +24,7 @@ function c101105205.initial_effect(c)
 	local e3=Effect.CreateEffect(c)
 	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
 	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
-	e3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e3:SetProperty(EFFECT_FLAG_DELAY)
 	e3:SetRange(LOCATION_SZONE)
 	e3:SetCondition(c101105205.opcon)
 	e3:SetTarget(c101105205.optg)
@@ -32,11 +32,10 @@ function c101105205.initial_effect(c)
 	c:RegisterEffect(e3)
 end
 function c101105205.tdfilter(c)
-	return c:IsRace(RACE_DRAGON) and c:IsLevel(1) and c:IsAbleToDeck() 
+	return c:IsRace(RACE_DRAGON) and c:IsLevel(1) and (c:IsAbleToDeck() or c:IsLocation(LOCATION_DECK))
 end
 function c101105205.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(c101105205.tdfilter,tp,LOCATION_DECK+LOCATION_HAND,0,1,nil) end
-	Duel.SetOperationInfo(0,CATEGORY_TODECK,nil,1,tp,LOCATION_HAND+LOCATION_DECK)
 end
 function c101105205.activate(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
@@ -57,27 +56,24 @@ end
 function c101105205.tdlimit(e,c)
 	return (c:IsCode(44508094) or c:IsType(TYPE_SYNCHRO) and aux.AddCodeList(c,44508094)) and c:IsOnField()
 end
-function c101105205.cfilter(c,tp)
-	return c:IsFaceup() and c:IsType(TYPE_SYNCHRO) and c:IsControler(tp)
+function c101105205.cfilter(c)
+	return c:IsFaceup() and c:IsType(TYPE_SYNCHRO)
 end
 function c101105205.opcon(e,tp,eg,ep,ev,re,r,rp)
-	return eg:IsExists(c101105205.cfilter,1,nil,tp)
+	return eg:IsExists(c101105205.cfilter,1,nil)
+end
+function c101105205.spfilter(c,e,tp)
+	return c:IsType(TYPE_TUNER) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function c101105205.optg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
 	local b1=Duel.IsPlayerCanDraw(tp,1) and Duel.GetFlagEffect(tp,101105205)==0
-	local b2=Duel.IsExistingMatchingCard(c101105205.spfilter,tp,LOCATION_HAND,0,1,nil,e,tp) and Duel.GetFlagEffect(tp,101105205+100)==0
-	if chk==0 then
-		return c:RegisterFlagEffect(101105205,RESET_CHAIN,0,1) and (b1 or b2)
-	end
-	c:RegisterFlagEffect(101105205,RESET_CHAIN,0,1)
-end
-function c101105205.spfilter(c,e,tp)
-	return c:IsType(TYPE_TUNER) and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP_DEFENSE)
+	local b2=Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.IsExistingMatchingCard(c101105205.spfilter,tp,LOCATION_HAND,0,1,nil,e,tp) and Duel.GetFlagEffect(tp,101105205+100)==0
+	if chk==0 then return b1 or b2 end
 end
 function c101105205.opop(e,tp,eg,ep,ev,re,r,rp)
 	local b1=Duel.IsPlayerCanDraw(tp,1) and Duel.GetFlagEffect(tp,101105205)==0
-	local b2=Duel.IsExistingMatchingCard(c101105205.spfilter,tp,LOCATION_HAND,0,1,nil,e,tp) and Duel.GetFlagEffect(tp,101105205+100)==0
+	local b2=Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.IsExistingMatchingCard(c101105205.spfilter,tp,LOCATION_HAND,0,1,nil,e,tp) and Duel.GetFlagEffect(tp,101105205+100)==0
 	local op=0
 	if b1 and b2 then op=Duel.SelectOption(tp,aux.Stringid(101105205,1),aux.Stringid(101105205,2))
 	elseif b1 then op=Duel.SelectOption(tp,aux.Stringid(101105205,1)) --Draw 1 Card
@@ -87,10 +83,8 @@ function c101105205.opop(e,tp,eg,ep,ev,re,r,rp)
 		Duel.Draw(tp,1,REASON_EFFECT)
 		Duel.RegisterFlagEffect(tp,101105205,RESET_PHASE+PHASE_END,0,1)
 	else
-		if Duel.GetLocationCount(tp,LOCATION_MZONE)<1 then return end
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 		local g=Duel.SelectMatchingCard(tp,c101105205.spfilter,tp,LOCATION_HAND,0,1,1,nil,e,tp)
-		if #g==0 then return end
 		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
 		Duel.RegisterFlagEffect(tp,101105205+100,RESET_PHASE+PHASE_END,0,1)
 	end
