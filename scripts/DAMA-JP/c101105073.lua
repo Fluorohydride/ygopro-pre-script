1d8854ba80b5306066c3fdcebbe9fbd2bd8ed831
Mercury233 <me@mercury233.me>
2021-03-22 02:27:37 +0000

    Update c101105073.lua

--- scripts/DAMA-JP/c101105073.lua
+++ scripts/DAMA-JP/c101105073.lua
@@ -1,5 +1,5 @@
+--魔鍵錠－解－
 --Magikey Unlocked
-
 --scripted by XyleN5967
 function c101105073.initial_effect(c)
 	--Activate
@@ -15,12 +15,11 @@ function c101105073.initial_effect(c)
 	c:RegisterEffect(e1)
 end
 function c101105073.cfilter(c)
-	return (c:IsSetCard(0x266) and c:IsType(TYPE_RITUAL) or c:IsSetCard(0x18f) and c:IsType(TYPE_MONSTER) and c:IsSummonLocation(LOCATION_EXTRA)) 
+	return c:IsFaceup() and c:IsSetCard(0x266) and (c:IsType(TYPE_RITUAL) or c:IsSummonLocation(LOCATION_EXTRA))
 end
 function c101105073.condition(e,tp,eg,ep,ev,re,r,rp)
 	if not Duel.IsExistingMatchingCard(c101105073.cfilter,tp,LOCATION_MZONE,0,1,nil) then return false end
-	return not e:GetHandler():IsStatus(STATUS_BATTLE_DESTROYED)
-		and ep~=tp and re:IsActiveType(TYPE_SPELL+TYPE_TRAP) and Duel.IsChainNegatable(ev)
+	return ep~=tp and re:IsHasType(EFFECT_TYPE_ACTIVATE) and Duel.IsChainNegatable(ev)
 end
 function c101105073.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
@@ -38,19 +37,13 @@ function c101105073.activate(e,tp,eg,ep,ev,re,r,rp)
 		and Duel.SelectYesNo(tp,aux.Stringid(101105073,0)) then
 		Duel.BreakEffect()
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATTRIBUTE)
-		local rc=Duel.AnnounceAttribute(tp,1,0xffff)
-		e:SetLabel(rc)
-		local g=Duel.GetMatchingGroup(c101105073.attfilter,tp,0,LOCATION_MZONE,nil)
-		local tc=g:GetFirst()
-		while tc do
-			local e1=Effect.CreateEffect(c)
-			e1:SetType(EFFECT_TYPE_SINGLE)
-			e1:SetCode(EFFECT_CHANGE_ATTRIBUTE)
-			e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-			e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
-			e1:SetValue(e:GetLabel())
-			tc:RegisterEffect(e1)
-			tc=g:GetNext()
-		end
+		local attr=Duel.AnnounceAttribute(tp,1,ATTRIBUTE_ALL)
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_FIELD)
+		e1:SetCode(EFFECT_CHANGE_ATTRIBUTE)
+		e1:SetTargetRange(0,LOCATION_MZONE)
+		e1:SetReset(RESET_PHASE+PHASE_END)
+		e1:SetValue(attr)
+		Duel.RegisterEffect(e1,tp)
 	end
 end
