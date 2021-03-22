0df2db5a837d1923bd0d28ea47ba2833a96711e5
XyleN5967 <80439628+XyleN5967@users.noreply.github.com>
2021-03-22 12:30:27 +0800

    add Gizmek scripts from DAMA

---
+++ scripts/DAMA-JP/c101105060.lua
@@ -0,0 +1,87 @@
+--機巧一神使記紀图
+--
+--Script by XyleN5967
+function c101105060.initial_effect(c)
+	c:EnableCounterPermit(0x61,LOCATION_FZONE)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e1)
+	--to hand
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(101105060,0))
+	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_FZONE)
+	e2:SetCountLimit(1,101105060)
+	e2:SetTarget(c101105060.thtg)
+	e2:SetOperation(c101105060.thop)
+	c:RegisterEffect(e2)
+	--counter
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
+	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e3:SetRange(LOCATION_FZONE)
+	e3:SetCondition(c101105060.countercon)
+	e3:SetOperation(c101105060.counterop)
+	c:RegisterEffect(e3)
+	local e4=e3:Clone() 
+	e4:SetCode(EVENT_SUMMON_SUCCESS)
+	c:RegisterEffect(e4)
+	--act limit
+	local e5=Effect.CreateEffect(c)
+	e5:SetType(EFFECT_TYPE_FIELD)
+	e5:SetCode(EFFECT_CANNOT_ACTIVATE)
+	e5:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e5:SetRange(LOCATION_FZONE)
+	e5:SetTargetRange(1,1)
+	e5:SetValue(c101105060.actlimit)
+	e5:SetCondition(c101105060.actlimitcon)
+	c:RegisterEffect(e5)
+end
+function c101105060.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetFieldGroupCount(tp,LOCATION_DECK,0)>=3 end
+	Duel.SetTargetPlayer(tp)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,0,LOCATION_DECK)
+end
+function c101105060.thfilter(c)
+	return c:GetTextAttack()==c:GetTextDefense() and c:IsRace(RACE_MACHINE) and c:IsAbleToHand()
+end
+function c101105060.thop(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetHandler():IsRelateToEffect(e) then return end
+	if Duel.GetFieldGroupCount(tp,LOCATION_DECK,0)<3 then return end
+	Duel.ConfirmDecktop(tp,3)
+	local g=Duel.GetDecktopGroup(tp,3)
+	if g:GetCount()>0 then
+		Duel.DisableShuffleCheck()
+		if g:IsExists(c101105060.thfilter,1,nil) and Duel.SelectYesNo(p,aux.Stringid(101105060,1)) then
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+			local sg=g:FilterSelect(tp,c101105060.thfilter,1,1,nil)
+			Duel.SendtoHand(sg,nil,REASON_EFFECT)
+			Duel.ConfirmCards(1-tp,sg)
+			Duel.ShuffleHand(tp)
+			g:Sub(sg)
+		end
+		Duel.Remove(g,POS_FACEDOWN,REASON_EFFECT+REASON_REVEAL)
+	end
+end
+function c101105060.cfilter(c,tp)
+	return c:IsControler(tp) and c:GetTextAttack()==c:GetTextDefense() and c:IsRace(RACE_MACHINE) and c:IsFaceup()
+end
+function c101105060.countercon(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(c101105060.cfilter,1,nil,tp)
+end
+function c101105060.counterop(e,tp,eg,ep,ev,re,r,rp)
+	e:GetHandler():AddCounter(0x61,1)
+end
+function c101105060.actlimitcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():GetCounter(0x61)>=10
+end
+function c101105060.actlimit(e,re,tp)
+	local rc=re:GetHandler()
+	local loc=re:GetActivateLocation()
+	return loc==LOCATION_MZONE and re:IsActiveType(TYPE_MONSTER) and rc:GetTextAttack()~=rc:GetTextDefense()
+end
