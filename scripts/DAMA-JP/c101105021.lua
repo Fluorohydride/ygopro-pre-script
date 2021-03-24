cfcdadacaa9831199a107b16abba2489e71ebc55
XyleN5967 <80439628+XyleN5967@users.noreply.github.com>
2021-03-24 09:10:33 +0800

    add Alien scripts from DAMA

---
+++ scripts/DAMA-JP/c101105021.lua
@@ -0,0 +1,61 @@
+--エーリアン・バスター
+--
+--Script by XyleN5967
+function c101105021.initial_effect(c)
+	--add counter
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(101105021,0))
+	e1:SetCategory(CATEGORY_COUNTER)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
+	e1:SetCode(EVENT_TO_GRAVE)
+	e1:SetCountLimit(1,101105021)
+	e1:SetTarget(c101105021.cttg)
+	e1:SetOperation(c101105021.ctop)
+	c:RegisterEffect(e1)
+	--destroy with counter
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(101105021,1))
+	e2:SetCategory(CATEGORY_DESTROY)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_GRAVE)
+	e2:SetCountLimit(1,101105021+100)
+	e2:SetCondition(aux.exccon)
+	e2:SetCost(aux.bfgcost)
+	e2:SetTarget(c101105021.destg)
+	e2:SetOperation(c101105021.desop)
+	c:RegisterEffect(e2)
+end
+function c101105021.ctfilter(c) 
+	return c:IsFaceup() and c:IsType(TYPE_MONSTER)
+end
+function c101105021.cttg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsOnField() and c101105021.ctfilter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(c101105021.ctfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_COUNTER)
+	local g=Duel.SelectTarget(tp,c101105021.ctfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_COUNTER,nil,1,0,0)
+end
+function c101105021.ctop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tc=Duel.GetFirstTarget()
+	if tc and tc:IsRelateToEffect(e) then
+		tc:AddCounter(0x100e,2)
+	end
+end
+function c101105021.desfilter(c)
+	return c:GetCounter(0x100e)>0
+end
+function c101105021.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsOnField() and c101105021.desfilter(chkc) and chkc~=e:GetHandler() end
+	if chk==0 then return Duel.IsExistingTarget(c101105021.desfilter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local g=Duel.SelectTarget(tp,c101105021.desfilter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
+end
+function c101105021.desop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) then
+		Duel.Destroy(tc,REASON_EFFECT)
+	end
+end
