a11c3ba5faf9d33f567cedda54d42e97638c2762
Mercury233 <me@mercury233.me>
2021-03-27 06:57:45 +0000

    Update c101105074.lua

--- scripts/DAMA-JP/c101105074.lua
+++ scripts/DAMA-JP/c101105074.lua
@@ -16,7 +16,8 @@ function c101105074.initial_effect(c)
 	e2:SetDescription(aux.Stringid(101105074,1))
 	e2:SetCategory(CATEGORY_TODECK+CATEGORY_DRAW)
 	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
-	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetCode(EVENT_FREE_CHAIN)
 	e2:SetRange(LOCATION_GRAVE)
 	e2:SetCondition(aux.exccon)
 	e2:SetCost(aux.bfgcost)
@@ -49,18 +50,13 @@ function c101105074.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,0,LOCATION_DECK)
 end
 function c101105074.activate(e,tp,eg,ep,ev,re,r,rp)
-	local var=e:GetLabel()
 	local g=Duel.GetMatchingGroup(c101105074.thfilter,tp,LOCATION_DECK,0,nil)
 	if g:GetCount()>=3 then
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CONFIRM)
 		local sg=g:Select(tp,3,3,nil)
 		Duel.ConfirmCards(1-tp,sg)
-		local p
-		if var==0 then
-			p=1-tp
-		elseif var==1 then
-			p=tp
-		end
+		local p=1-tp
+		if e:GetLabel()==1 then p=tp end
 		Duel.Hint(HINT_SELECTMSG,p,HINTMSG_ATOHAND)
 		local tg=sg:Select(p,1,1,nil)
 		Duel.SendtoHand(tg,nil,REASON_EFFECT)
@@ -80,12 +76,12 @@ function c101105074.tdtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 end
 function c101105074.tdop(e,tp,eg,ep,ev,re,r,rp)
 	local tg=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS)
-	if not tg or tg:FilterCount(Card.IsRelateToEffect,nil,e)~=3 then return end
+	if not tg or tg:FilterCount(Card.IsRelateToEffect,nil,e)==0 then return end
 	Duel.SendtoDeck(tg,nil,0,REASON_EFFECT)
 	local g=Duel.GetOperatedGroup()
 	if g:IsExists(Card.IsLocation,1,nil,LOCATION_DECK) then Duel.ShuffleDeck(tp) end
 	local ct=g:FilterCount(Card.IsLocation,nil,LOCATION_DECK+LOCATION_EXTRA)
-	if ct==3 then
+	if ct>0 then
 		Duel.BreakEffect()
 		Duel.Draw(tp,1,REASON_EFFECT)
 	end
