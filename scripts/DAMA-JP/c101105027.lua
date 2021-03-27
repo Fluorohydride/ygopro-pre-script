04ea92a167a90585c446b207008b29eb45bca642
Mercury233 <me@mercury233.me>
2021-03-27 06:26:43 +0000

    Update c101105027.lua

--- scripts/DAMA-JP/c101105027.lua
+++ scripts/DAMA-JP/c101105027.lua
@@ -54,9 +54,19 @@ function c101105027.retreg(e,tp,eg,ep,ev,re,r,rp)
 	e1:SetTarget(c101105027.rettg)
 	e1:SetOperation(c101105027.retop)
 	c:RegisterEffect(e1)
+	local e2=e1:Clone()
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCondition(aux.SpiritReturnConditionOptional)
+	c:RegisterEffect(e2)
 end
 function c101105027.rettg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return e:GetHandler():IsAbleToHand() end
+	if chk==0 then
+		if e:IsHasType(EFFECT_TYPE_TRIGGER_F) then
+			return true
+		else
+			return e:GetHandler():IsAbleToHand()
+		end
+	end
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,e:GetHandler(),1,0,0)
 end
 function c101105027.retop(e,tp,eg,ep,ev,re,r,rp)
@@ -69,12 +79,10 @@ function c101105027.cfilter(c)
 	return c:IsType(TYPE_SPIRIT) and c:IsFaceup()
 end
 function c101105027.acttg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_MZONE) and c101105027.cfilter(chkc) end
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingTarget(c101105027.cfilter,tp,LOCATION_MZONE,0,1,nil) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and c101105027.cfilter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(c101105027.cfilter,tp,LOCATION_MZONE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
 	local g=Duel.SelectTarget(tp,c101105027.cfilter,tp,LOCATION_MZONE,0,1,1,nil)
-	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,0,0)
 end
 function c101105027.actop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
