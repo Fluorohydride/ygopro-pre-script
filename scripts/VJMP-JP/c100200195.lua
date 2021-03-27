26cec57e046e4bec0c5367127e1d7c0b49a047ba
Mercury233 <me@mercury233.me>
2021-03-27 00:33:18 +0000

    Update c100200195.lua

--- scripts/VJMP-JP/c100200195.lua
+++ scripts/VJMP-JP/c100200195.lua
@@ -4,19 +4,17 @@
 function c100200195.initial_effect(c)
 	--special summon
 	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_DECKDES)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e1:SetCode(EVENT_BATTLE_DESTROYING)
-	e1:SetCondition(c100200195.spcon)
+	e1:SetCondition(aux.bdocon)
 	e1:SetTarget(c100200195.sptg)
 	e1:SetOperation(c100200195.spop)
 	c:RegisterEffect(e1)
 end
-function c100200195.spcon(e,tp,eg,ep,ev,re,r,rp)
-	return e:GetHandler():IsFaceup() and e:GetHandler():IsRelateToBattle()
-end
 function c100200195.spfilter(c,e,tp)
-	return c:IsRace(RACE_DRAGON) and c:IsAttribute(ATTRIBUTE_WIND) and c:IsLevelBelow(5) and not c:IsCode(100200195) and c:IsCanBeSpecialSummoned(e,0,tp,false,false);
+	return c:IsRace(RACE_DRAGON) and c:IsAttribute(ATTRIBUTE_WIND) and c:IsLevelBelow(5)
+		and not c:IsCode(100200195) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function c100200195.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
@@ -32,7 +30,7 @@ function c100200195.spop(e,tp,eg,ep,ev,re,r,rp)
 		--cannot atk directly this turn
 		local e1=Effect.CreateEffect(e:GetHandler())
 		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_CLIENT_HINT)
+		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
 		e1:SetCode(EFFECT_CANNOT_DIRECT_ATTACK)
 		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
 		tc:RegisterEffect(e1)
