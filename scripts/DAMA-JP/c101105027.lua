b4bd811e8462218e853120c2ea90fada5160d5bd
XyleN5967 <80439628+XyleN5967@users.noreply.github.com>
2021-03-27 14:58:13 +0800

    add DAMA scripts (#708)

---
+++ scripts/DAMA-JP/c101105027.lua
@@ -0,0 +1,96 @@
+--木花咲弥
+--
+--Script by Kohana Sonogami
+function c101105027.initial_effect(c)
+	c:EnableReviveLimit()
+	--special summon
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e1:SetCode(EFFECT_SPSUMMON_PROC)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCountLimit(1,101105027+EFFECT_COUNT_CODE_OATH)
+	e1:SetCondition(c101105027.sprcon)
+	c:RegisterEffect(e1)
+	--spirit return
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e2:SetOperation(c101105027.retreg)
+	c:RegisterEffect(e2)
+	--act limit
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(101105026,0))
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e3:SetCode(EVENT_PHASE+PHASE_END)
+	e3:SetRange(LOCATION_GRAVE)
+	e3:SetCost(aux.bfgcost)
+	e3:SetTarget(c101105027.acttg)
+	e3:SetOperation(c101105027.actop)
+	c:RegisterEffect(e3)
+end
+function c101105027.sprfilter(c)
+	return c:IsFaceup() and c:IsType(TYPE_SPIRIT)
+end
+function c101105027.sprcon(e,c)
+	if c==nil then return true end
+	local tp=c:GetControler()
+	return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(c101105027.sprfilter,tp,LOCATION_MZONE,0,1,nil)
+end
+function c101105027.retreg(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e1:SetDescription(1104)
+	e1:SetCategory(CATEGORY_TOHAND)
+	e1:SetCode(EVENT_PHASE+PHASE_END)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetReset(RESET_EVENT+0x1ee0000+RESET_PHASE+PHASE_END)
+	e1:SetCondition(aux.SpiritReturnConditionForced)
+	e1:SetTarget(c101105027.rettg)
+	e1:SetOperation(c101105027.retop)
+	c:RegisterEffect(e1)
+	local e2=e1:Clone()
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCondition(aux.SpiritReturnConditionOptional)
+	c:RegisterEffect(e2)
+end
+function c101105027.rettg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then
+		if e:IsHasType(EFFECT_TYPE_TRIGGER_F) then
+			return true
+		else
+			return e:GetHandler():IsAbleToHand()
+		end
+	end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,e:GetHandler(),1,0,0)
+end
+function c101105027.retop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) and c:IsFaceup() then
+		Duel.SendtoHand(c,nil,REASON_EFFECT)
+	end
+end
+function c101105027.cfilter(c)
+	return c:IsType(TYPE_SPIRIT) and c:IsFaceup()
+end
+function c101105027.acttg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and c101105027.cfilter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(c101105027.cfilter,tp,LOCATION_MZONE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+	local g=Duel.SelectTarget(tp,c101105027.cfilter,tp,LOCATION_MZONE,0,1,1,nil)
+end
+function c101105027.actop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) then
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_CANNOT_TRIGGER)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		tc:RegisterEffect(e1)
+	end
+end
