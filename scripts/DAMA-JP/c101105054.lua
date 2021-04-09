fea7982fe63da7860ad5e7006a1e2ac31faa22c9
Xy❤️ <80439628+XyleN5967@users.noreply.github.com>
2021-04-09 20:00:12 +0800

    add 2 left by rimi-chan

---
+++ scripts/DAMA-JP/c101105054.lua
@@ -0,0 +1,78 @@
+--烙印開幕
+--Scripted by Rimi-Chan
+function c101105054.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SPECIAL_SUMMON+CATEGORY_HANDES)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCountLimit(1,101105054)
+	e1:SetTarget(c101105054.target)
+	e1:SetOperation(c101105054.activate)
+	c:RegisterEffect(e1)
+	--avoid destruction
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e2:SetCode(EFFECT_DESTROY_REPLACE)
+	e2:SetRange(LOCATION_GRAVE)
+	e2:SetCountLimit(1,101105054+100)
+	e2:SetTarget(c101105054.reptg)
+	e2:SetValue(c101105054.repval)
+	e2:SetOperation(c101105054.repop)
+	c:RegisterEffect(e2)
+end
+function c101105054.selfilter(c,e,tp)
+	return c:IsSetCard(0x3268) and c:IsType(TYPE_MONSTER) and (c:IsAbleToHand() or c:IsCanBeSpecialSummoned(e,0,tp,false,false))
+end
+function c101105054.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	local g=Duel.GetMatchingGroup(c101105054.selfilter,tp,LOCATION_DECK,0,nil)
+	if chk==0 then return g:GetCount()>0 end
+	Duel.SetOperationInfo(0,CATEGORY_HANDES,nil,0,tp,1)
+end
+function c101105054.activate(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if Duel.DiscardHand(tp,nil,1,1,REASON_EFFECT+REASON_DISCARD)~=0 then
+		local g=Duel.GetMatchingGroup(c101105054.selfilter,tp,LOCATION_DECK,0,nil)
+		if g:GetCount()>0 then
+			Duel.BreakEffect()
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_OPERATECARD)
+			g=g:Select(tp,1,1,nil)
+		end
+		local tc=g:GetFirst() 
+		if tc and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+			and tc:IsCanBeSpecialSummoned(e,0,tp,false,false)
+			and Duel.SelectOption(tp,1190,1152)==1 then
+			Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
+		else
+			Duel.SendtoHand(tc,nil,REASON_EFFECT)
+			Duel.ConfirmCards(1-tp,tc)
+		end
+		if e:IsHasType(EFFECT_TYPE_ACTIVATE) then
+			local e1=Effect.CreateEffect(c)
+			e1:SetType(EFFECT_TYPE_FIELD)
+			e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+			e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+			e1:SetTargetRange(1,0)
+			e1:SetTarget(c101105054.splimit)
+			e1:SetReset(RESET_PHASE+PHASE_END)
+			Duel.RegisterEffect(e1,tp)
+		end
+	end
+end
+function c101105054.splimit(e,c)
+	return c:IsLocation(LOCATION_EXTRA) and not c:IsType(TYPE_FUSION) 
+end
+function c101105054.repfilter(c,tp)
+	return c:IsFaceup() and c:IsType(TYPE_FUSION)
+		and c:IsLocation(LOCATION_MZONE) and c:IsControler(tp) and c:IsReason(REASON_EFFECT) and not c:IsReason(REASON_REPLACE)
+end
+function c101105054.reptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsAbleToRemove() and eg:IsExists(c101105054.repfilter,1,nil,tp) end
+	return Duel.SelectEffectYesNo(tp,e:GetHandler(),96)
+end
+function c101105054.repval(e,c)
+	return c101105054.repfilter(c,e:GetHandlerPlayer())
+end
+function c101105054.repop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Remove(e:GetHandler(),POS_FACEUP,REASON_EFFECT)
+end
