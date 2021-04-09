81ab0000074d595c70107e60eb803f0c700a1622
Xy❤️ <80439628+XyleN5967@users.noreply.github.com>
2021-04-10 05:55:29 +0800

    was not checking

--- scripts/DAMA-JP/c101105054.lua
+++ scripts/DAMA-JP/c101105054.lua
@@ -26,7 +26,8 @@ function c101105054.selfilter(c,e,tp)
 end
 function c101105054.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	local g=Duel.GetMatchingGroup(c101105054.selfilter,tp,LOCATION_DECK,0,nil)
-	if chk==0 then return g:GetCount()>0 end
+	if chk==0 then return g:GetCount()>0
+		and Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,1,nil) end
 	Duel.SetOperationInfo(0,CATEGORY_HANDES,nil,0,tp,1)
 end
 function c101105054.activate(e,tp,eg,ep,ev,re,r,rp)
