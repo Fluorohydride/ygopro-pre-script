61ab7c5d074e35a9085281a02e0b2d61f1f8f2ab
mercury233 <me@mercury233.me>
2021-04-11 08:32:04 +0800

    fix

--- scripts/DAMA-JP/c101105030.lua
+++ scripts/DAMA-JP/c101105030.lua
@@ -23,7 +23,7 @@ function c101105030.initial_effect(c)
 	e2:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DELAY)
 	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
 	e2:SetRange(LOCATION_MZONE)
-	e2:SetCountLimit(1,101105031)
+	e2:SetCountLimit(1,101105030+100)
 	e2:SetCondition(c101105030.spcon2)
 	e2:SetTarget(c101105030.sptg2)
 	e2:SetOperation(c101105030.spop2)
@@ -37,14 +37,13 @@ function c101105030.spcon(e,tp,eg,ep,ev,re,r,rp)
 end
 function c101105030.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
-	local tc=Duel.GetFirstMatchingCard(c101105030.filter,tp,LOCATION_PZONE,0,c,c)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and c:IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	local tc=Duel.GetFirstMatchingCard(c101105030.filter,tp,LOCATION_PZONE,0,c,c)
 	Duel.SetOperationInfo(0,CATEGORY_DESTROY,tc,1,0,0)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,0,0)
 end
 function c101105030.spop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	if not c:IsRelateToEffect(e) then return end
 	local tc=Duel.GetFirstMatchingCard(nil,tp,LOCATION_PZONE,0,c)
 	if tc and Duel.Destroy(tc,REASON_EFFECT)~=0 then
 		Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
@@ -57,7 +56,7 @@ function c101105030.spcon2(e,tp,eg,ep,ev,re,r,rp)
 	return not eg:IsContains(e:GetHandler()) and eg:IsExists(c101105030.cfilter,1,nil,tp)
 end
 function c101105030.spfilter2(c,e,tp,tc)
-	return c:IsLevel(tc:GetLevel()) and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP)
+	return c:IsLevel(tc:GetLevel()) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function c101105030.sptg2(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
