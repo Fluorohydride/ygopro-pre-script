96348fc727c2878681887953342ce1b8a3feb1d6
Mercury233 <me@mercury233.me>
2021-04-17 15:15:44 +0800

    Update c100278021.lua

--- scripts/AC01-JP/c100278021.lua
+++ scripts/AC01-JP/c100278021.lua
@@ -10,7 +10,7 @@ function c100278021.initial_effect(c)
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(100278021,0))
 	e1:SetCategory(CATEGORY_DESTROY+CATEGORY_TOKEN)
-	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
 	e1:SetCode(EVENT_SUMMON_SUCCESS)
 	e1:SetRange(LOCATION_FZONE)
 	e1:SetCountLimit(1,100278021)
@@ -21,37 +21,35 @@ function c100278021.initial_effect(c)
 	--destroy
 	local e2=Effect.CreateEffect(c) 
 	e2:SetDescription(aux.Stringid(100278021,1)) 
-	e2:SetType(EFFECT_TYPE_TRIGGER_F+EFFECT_TYPE_SINGLE)
-	e2:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
-	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e2:SetCode(EVENT_LEAVE_FIELD)
-	e2:SetCondition(c100278021.descon)
-	e2:SetTarget(c100278021.destg)
+	e2:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_SINGLE)
+	e2:SetCode(EVENT_LEAVE_FIELD_P)
 	e2:SetOperation(c100278021.desop)
 	c:RegisterEffect(e2)
 end
 function c100278021.tkcon(e,tp,eg,ep,ev,re,r,rp)
 	if #eg~=1 then return false end
 	local tc=eg:GetFirst()
-	return tc~=e:GetHandler() and tc:IsFaceup() and (tc:GetAttack()>0 and tc:GetDefense()>0) and (tc:IsSummonPlayer(1-tp) or tc:IsSummonPlayer(tp))
+	return tc:IsSummonPlayer(Duel.GetTurnPlayer())
 end
 function c100278021.tktg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
-	--not sure the operation info here
-	Duel.SetOperationInfo(0,CATEGORY_TOKEN,nil,1,tp,0)
+	local tc=eg:GetFirst()
+	Duel.SetTargetCard(eg)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,tc,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_TOKEN,nil,1,0,0)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,0,0)
 end
 function c100278021.tkop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local tc=eg:GetFirst()
-	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
-	if ft<=0 or not Duel.IsPlayerCanSpecialSummonMonster(tp,100278121,0,0x4011,-2,-2,1,RACE_SPELLCASTER,ATTRIBUTE_DARK) then return end
-	if tc:IsFaceup() and tc:IsLocation(LOCATION_MZONE) and Duel.Destroy(tc,REASON_EFFECT)~=0 and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 then
+	local p=tc:GetControler()
+	if tc:IsRelateToEffect(e) and Duel.Destroy(tc,REASON_EFFECT)~=0
+		and Duel.GetLocationCount(p,LOCATION_MZONE)>0
+		and Duel.IsPlayerCanSpecialSummonMonster(p,100278121,0,0x4011,-2,-2,1,RACE_SPELLCASTER,ATTRIBUTE_DARK) then
 		local atk=tc:GetAttack() 
 		local def=tc:GetDefense()
-		local p=tc:GetControler()
 		local token=Duel.CreateToken(tp,100278121) 
-		Duel.SpecialSummonStep(token,0,tp,p,false,false,POS_FACEUP)
+		Duel.SpecialSummonStep(token,0,p,p,false,false,POS_FACEUP)
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_SET_ATTACK)
@@ -64,22 +62,14 @@ function c100278021.tkop(e,tp,eg,ep,ev,re,r,rp)
 		token:RegisterEffect(e2)
 		local e3=Effect.CreateEffect(c)
 		e3:SetType(EFFECT_TYPE_SINGLE)
-		e3:SetCode(EFFECT_CANNOT_ATTACK_ANNOUNCE)
+		e3:SetCode(EFFECT_CANNOT_DIRECT_ATTACK)
 		e3:SetReset(RESET_EVENT+RESETS_STANDARD)
 		token:RegisterEffect(e3,true)
 	end
 	Duel.SpecialSummonComplete()
 end
-function c100278021.descon(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	return c:IsPreviousPosition(POS_FACEUP) and c:IsPreviousLocation(LOCATION_ONFIELD)
-end
-function c100278021.destg(e,tp,eg,ep,ev,re,r,rp,chk)
-	local g=Duel.GetMatchingGroup(Card.IsCode,tp,LOCATION_MZONE,LOCATION_MZONE,0,nil,100278121)
-	if chk==0 then return #g>0 end
-	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,#g,0,0)
-end
 function c100278021.desop(e,tp,eg,ep,ev,re,r,rp)
+	if e:GetHandler():IsFacedown() then return end
 	local g=Duel.GetMatchingGroup(Card.IsCode,tp,LOCATION_MZONE,LOCATION_MZONE,0,nil,100278121)
 	Duel.Destroy(g,REASON_EFFECT)
 end
