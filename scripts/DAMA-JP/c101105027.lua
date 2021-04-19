541d47080aa5ab0c069ffb1015e430dfbf98da3c
mallu11 <54900811+mallu11@users.noreply.github.com>
2021-04-19 12:21:19 +0800

    fix 木花咲弥

--- scripts/DAMA-JP/c101105027.lua
+++ scripts/DAMA-JP/c101105027.lua
@@ -3,6 +3,8 @@
 --Script by Kohana Sonogami
 function c101105027.initial_effect(c)
 	c:EnableReviveLimit()
+	--spirit return
+	aux.EnableSpiritReturn(c,EVENT_SPSUMMON_SUCCESS)
 	--special summon
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
@@ -12,13 +14,6 @@ function c101105027.initial_effect(c)
 	e1:SetCountLimit(1,101105027+EFFECT_COUNT_CODE_OATH)
 	e1:SetCondition(c101105027.sprcon)
 	c:RegisterEffect(e1)
-	--spirit return
-	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
-	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
-	e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-	e2:SetOperation(c101105027.retreg)
-	c:RegisterEffect(e2)
 	--act limit
 	local e3=Effect.CreateEffect(c)
 	e3:SetDescription(aux.Stringid(101105026,0))
@@ -40,41 +35,6 @@ function c101105027.sprcon(e,c)
 	return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
 		and Duel.IsExistingMatchingCard(c101105027.sprfilter,tp,LOCATION_MZONE,0,1,nil)
 end
-function c101105027.retreg(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
-	e1:SetDescription(1104)
-	e1:SetCategory(CATEGORY_TOHAND)
-	e1:SetCode(EVENT_PHASE+PHASE_END)
-	e1:SetRange(LOCATION_MZONE)
-	e1:SetCountLimit(1)
-	e1:SetReset(RESET_EVENT+0x1ee0000+RESET_PHASE+PHASE_END)
-	e1:SetCondition(aux.SpiritReturnConditionForced)
-	e1:SetTarget(c101105027.rettg)
-	e1:SetOperation(c101105027.retop)
-	c:RegisterEffect(e1)
-	local e2=e1:Clone()
-	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-	e2:SetCondition(aux.SpiritReturnConditionOptional)
-	c:RegisterEffect(e2)
-end
-function c101105027.rettg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then
-		if e:IsHasType(EFFECT_TYPE_TRIGGER_F) then
-			return true
-		else
-			return e:GetHandler():IsAbleToHand()
-		end
-	end
-	Duel.SetOperationInfo(0,CATEGORY_TOHAND,e:GetHandler(),1,0,0)
-end
-function c101105027.retop(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	if c:IsRelateToEffect(e) and c:IsFaceup() then
-		Duel.SendtoHand(c,nil,REASON_EFFECT)
-	end
-end
 function c101105027.cfilter(c)
 	return c:IsType(TYPE_SPIRIT) and c:IsFaceup()
 end
@@ -86,7 +46,7 @@ function c101105027.acttg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 end
 function c101105027.actop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
-	if tc:IsRelateToEffect(e) then
+	if tc:IsRelateToEffect(e) and tc:IsFaceup() then
 		local e1=Effect.CreateEffect(e:GetHandler())
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_CANNOT_TRIGGER)
