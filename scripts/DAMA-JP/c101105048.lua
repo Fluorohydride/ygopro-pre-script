acb7d6447507f5a286520bc6b7e4fe862519cf82
mallu11 <54900811+mallu11@users.noreply.github.com>
2021-04-21 02:17:59 +0800

    fix グランドレミコード・ミューゼシア

--- scripts/DAMA-JP/c101105048.lua
+++ scripts/DAMA-JP/c101105048.lua
@@ -21,43 +21,53 @@ function c101105048.initial_effect(c)
 	e2:SetCategory(CATEGORY_SEARCH+CATEGORY_TOHAND)
 	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
 	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
-	e2:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DELAY)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e2:SetRange(LOCATION_MZONE)
-	e2:SetCountLimit(1,101105048)
+	e2:SetCountLimit(1,101105048+100)
 	e2:SetCondition(c101105048.thcon)
 	e2:SetTarget(c101105048.thtg)
 	e2:SetOperation(c101105048.thop)
 	c:RegisterEffect(e2)
 end
-function c101105048.tefilter(c)
-	return c:GetOriginalType()&TYPE_PENDULUM~=0
-end
 function c101105048.chkfilter(c,odevity)
 	return c:GetCurrentScale()%2==odevity
 end
-function c101105048.thfilter(c,e,tp,odevity)
-	return c:IsFaceup() and c:IsType(TYPE_PENDULUM) and c:GetCurrentScale()%2==odevity
+function c101105048.thfilter(c,odevity)
+	return c:IsFaceup() and c:IsType(TYPE_PENDULUM) and c:GetCurrentScale()%2==1-odevity
 		and c:IsAbleToHand()
 end
-function c101105048.chkcon(g,e,tp,odevity)
-	return g:IsExists(c101105048.chkfilter,1,nil,odevity) and Duel.IsExistingMatchingCard(c101105048.thfilter,tp,LOCATION_EXTRA,0,1,nil,e,tp,odevity)
+function c101105048.chkcon(g,odevity)
+	return g:IsExists(c101105048.chkfilter,1,nil,odevity) and Duel.IsExistingMatchingCard(c101105048.thfilter,tp,LOCATION_EXTRA,0,1,nil,odevity)
 end
 function c101105048.tetg(e,tp,eg,ep,ev,re,r,rp,chk)
-	local g=Duel.GetMatchingGroup(c101105048.tefilter,tp,LOCATION_HAND,0,nil)
-	if chk==0 then return c101105048.chkcon(g,e,tp,0) or c101105048.chkcon(g,e,tp,1) end
+	local g=Duel.GetMatchingGroup(Card.IsType,tp,LOCATION_HAND,0,nil,TYPE_PENDULUM)
+	if chk==0 then return c101105048.chkcon(g,0) or c101105048.chkcon(g,1) end
 	Duel.SetOperationInfo(0,CATEGORY_TOEXTRA,nil,1,tp,LOCATION_DECK)
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_EXTRA)
 end
 function c101105048.teop(e,tp,eg,ep,ev,re,r,rp)
-	Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(101105048,3))
-	local g=Duel.SelectMatchingCard(tp,c101105048.tefilter,tp,LOCATION_HAND,0,1,1,nil)
-	if g:GetCount()>0 then
-		Duel.SendtoExtraP(g,tp,REASON_EFFECT)
-		local odevity=g:GetFirst():GetCurrentScale()%2
+	local g=Duel.GetMatchingGroup(Card.IsType,tp,LOCATION_HAND,0,nil,TYPE_PENDULUM)
+	local b1=c101105048.chkcon(g,0)
+	local b2=c101105048.chkcon(g,1)
+	local sg=Group.CreateGroup()
+	if b1 and not b2 then
+		Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(101105048,3))
+		sg=g:FilterSelect(tp,c101105048.chkfilter,1,1,nil,0)
+	end
+	if not b1 and b2 then
+		Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(101105048,3))
+		sg=g:FilterSelect(tp,c101105048.chkfilter,1,1,nil,1)
+	end
+	if b1 and b2 then
+		Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(101105048,3))
+		sg=g:Select(tp,1,1,nil)
+	end
+	local tc=sg:GetFirst()
+	if tc and Duel.SendtoExtraP(tc,tp,REASON_EFFECT)~=0 and tc:IsLocation(LOCATION_EXTRA) then
+		local odevity=tc:GetCurrentScale()%2
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
 		local g=Duel.SelectMatchingCard(tp,c101105048.thfilter,tp,LOCATION_EXTRA,0,1,1,nil,odevity)
 		if g:GetCount()>0 then
-			Duel.BreakEffect()
 			Duel.SendtoHand(g,tp,REASON_EFFECT)
 			Duel.ConfirmCards(1-tp,g)
 		end
@@ -70,10 +80,10 @@ function c101105048.tgfilter(c,tp,g)
 	return g:IsContains(c) and Duel.IsExistingMatchingCard(c101105048.adfilter,tp,LOCATION_DECK,0,1,nil,c:GetCode())
 end
 function c101105048.adfilter(c,scale)
-	return c:IsSetCard(0x162) and c:IsType(TYPE_MONSTER) and not c:GetLevel()==scale and c:IsAbleToHand()
+	return c:IsSetCard(0x162) and c:IsType(TYPE_PENDULUM) and c:IsLevel(scale) and c:IsAbleToHand()
 end
 function c101105048.thcon(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.GetTurnPlayer()==tp and eg:IsExists(c101105048.cfilter,1,nil,tp)
+	return eg:IsExists(c101105048.cfilter,1,nil,tp)
 end
 function c101105048.thtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	local g=eg:Filter(c101105048.cfilter,nil,tp)
