d30973050cea7d420e1bde5f5edc415b2cf16f7b
Xy❤️ <80439628+XyleN5967@users.noreply.github.com>
2021-05-11 08:20:11 +0800

    add VP21 scripts

---
+++ scripts/VP21-JP/c100279001.lua
@@ -0,0 +1,91 @@
+--ホーリー・エルフーホーリー・バースト・ストリーム
+--Script by XyLeN
+function c100279001.initial_effect(c)
+	--special summon itself
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(100279001,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_DISABLE)
+	e1:SetType(EFFECT_TYPE_QUICK_O)
+	e1:SetCode(EVENT_CHAINING)
+	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCountLimit(1,100279001)
+	e1:SetCondition(c100279001.spcon1)
+	e1:SetTarget(c100279001.sptg1)
+	e1:SetOperation(c100279001.spop1)
+	c:RegisterEffect(e1)
+	--special summon in grave
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(100279001,1))
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetHintTiming(0,TIMING_BATTLE_START+TIMING_ATTACK)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetCountLimit(1,100279001+100)
+	e2:SetCondition(c100279001.spcon2)
+	e2:SetTarget(c100279001.sptg2)
+	e2:SetOperation(c100279001.spop2)
+	c:RegisterEffect(e2)
+end
+function c100279001.cfilter(c)
+	return c:IsType(TYPE_NORMAL) and c:IsLevelAbove(5) and c:IsFaceup() 
+end
+function c100279001.spcon1(e,tp,eg,ep,ev,re,r,rp)
+	if not Duel.IsExistingMatchingCard(c100279001.cfilter,tp,LOCATION_MZONE,0,1,nil) then return false end
+	if not Duel.IsChainDisablable(ev) then return false end
+	return ep~=tp and re:IsActiveType(TYPE_MONSTER)
+end
+function c100279001.sptg1(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false)
+		and not re:GetHandler():IsDisabled() end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_DISABLE,eg,1,0,0)
+end
+function c100279001.spop1(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) and Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)~=0 then
+		Duel.NegateEffect(ev)
+	end
+end
+function c100279001.spcon2(e,tp,eg,ep,ev,re,r,rp)
+	return (Duel.GetCurrentPhase()>=PHASE_BATTLE_START and Duel.GetCurrentPhase()<=PHASE_BATTLE) and Duel.GetTurnPlayer()==1-tp
+end
+function c100279001.spfilter(c,e,tp)
+	return c:IsType(TYPE_NORMAL) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function c100279001.sptg2(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and c100279001.spfilter(chkc,e,tp) end
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingTarget(c100279001.spfilter,tp,LOCATION_GRAVE,LOCATION_GRAVE,1,nil,e,tp) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectTarget(tp,c100279001.spfilter,tp,LOCATION_GRAVE,LOCATION_GRAVE,1,1,nil,e,tp)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
+end
+function c100279001.spop2(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) and Duel.SpecialSummonStep(tc,0,tp,tp,false,false,POS_FACEUP) then
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_FIELD)
+		e1:SetCode(EFFECT_MUST_ATTACK)
+		e1:SetTargetRange(0,LOCATION_MZONE)
+		e1:SetCondition(c100279001.atkcon)
+		e1:SetRange(LOCATION_MZONE)
+		e1:SetOwnerPlayer(tp)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		tc:RegisterEffect(e1)
+		local e2=e1:Clone()
+		e2:SetCode(EFFECT_MUST_ATTACK_MONSTER)
+		e2:SetValue(c100279001.atklimit)
+		tc:RegisterEffect(e2)
+	end
+	Duel.SpecialSummonComplete()
+end
+function c100279001.atkcon(e)
+	return e:GetHandler():IsControler(e:GetOwnerPlayer())
+end
+function c100279001.atklimit(e,c)
+	return c==e:GetHandler()
+end
