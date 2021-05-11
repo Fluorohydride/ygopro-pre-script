66b809957cc4739a7122fb36a51798f5af06f9ab
TheOnePharaoh <TheOnePharaoh@users.noreply.github.com>
2021-05-11 17:42:36 +1000

    Added Hi-Speedroid Clear Wing Rider

---
+++ scripts/DP25-JP/c100425005.lua
@@ -0,0 +1,88 @@
+--Hi-Speedroid Clear Wing Rider
+function c100425005.initial_effect(c)
+	c:EnableReviveLimit()
+	aux.AddSynchroProcedure(c,aux.FilterBoolFunction(Card.IsAttribute,ATTRIBUTE_WIND),aux.NonTuner(c100425005.sfilter),1,1)
+	--Dice Popboost
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(100425005,1))
+	e1:SetCategory(CATEGORY_TODECK+CATEGORY_DICE+CATEGORY_DESTROY+CATEGORY_ATKCHANGE)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetTarget(c100425005.destg)
+	e1:SetOperation(c100425005.desop)
+	c:RegisterEffect(e1)
+	--special summon
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(100425005,1))
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetHintTiming(0,TIMINGS_CHECK_MONSTER+TIMING_MAIN_END)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetCondition(c100425005.spcon)
+	e2:SetCost(c100425005.spcost)
+	e2:SetTarget(c100425005.sptg)
+	e2:SetOperation(c100425005.spop)
+	c:RegisterEffect(e2)
+end
+c100425005.toss_dice=true
+c100425005.material_type=TYPE_SYNCHRO
+function c100425005.sfilter(c)
+	return c:IsAttribute(ATTRIBUTE_WIND) and c:IsType(TYPE_SYNCHRO)
+end
+
+function c100425005.gyfilter(c)
+	return c:IsAttribute(ATTRIBUTE_WIND) and c:IsAbleToDeck()
+end
+
+function c100425005.destg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then		return Duel.IsExistingMatchingCard(c100425005.gyfilter,tp,LOCATION_GRAVE,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_DICE,nil,0,tp,1)
+end
+
+function c100425005.desop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local dc=Duel.TossDice(tp,1)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
+	local gy=Duel.SelectMatchingCard(tp,c100425005.gyfilter,tp,LOCATION_GRAVE,0,1,dc,nil)
+	local yc=Duel.SendtoDeck(gy,nil,2,REASON_EFFECT)
+	if  yc>0 and Duel.SelectYesNo(tp,aux.Stringid(100425005,2)) then
+	local g=Duel.GetMatchingGroup(aux.TRUE,tp,0,LOCATION_ONFIELD,nil)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local dg=g:Select(tp,1,yc,nil)
+	Duel.HintSelection(dg)
+	local ct=Duel.Destroy(dg,REASON_EFFECT)
+	if ct>0 and c:IsFaceup() and c:IsRelateToEffect(e) then
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetValue(ct*500)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_DISABLE+RESET_PHASE+PHASE_END)
+		c:RegisterEffect(e1)
+	end
+end
+function c100425005.spcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsSummonType(SUMMON_TYPE_SYNCHRO) and Duel.GetTurnPlayer()~=tp
+		and (Duel.GetCurrentPhase()==PHASE_MAIN1 or Duel.GetCurrentPhase()==PHASE_MAIN2)
+
+end
+function c100425005.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsReleasable() end
+	Duel.Release(e:GetHandler(),REASON_COST)
+end
+function c100425005.spfilter(c,e,tp,mc)
+	return c:IsRace(RACE_DRAGON) and c:IsAttribute(ATTRIBUTE_WIND) and c:IsType(TYPE_SYNCHRO)
+		and c:IsLevel(7) and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP)
+		and Duel.GetLocationCountFromEx(tp,tp,mc,c)>0
+end
+function c100425005.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c100425005.spfilter,tp,LOCATION_EXTRA,0,1,nil,e,tp,e:GetHandler()) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
+end
+function c100425005.spop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,c100425005.spfilter,tp,LOCATION_EXTRA,0,1,2,nil,e,tp,nil)
+	Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
