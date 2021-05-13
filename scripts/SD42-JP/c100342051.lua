e4f6047930405ff1637f5339f4eb8f1fca7b2bcf
Xy❤️ <80439628+XyleN5967@users.noreply.github.com>
2021-05-13 21:31:14 +0800

    Update and rename c.lua to c100342051.lua

--- scripts/SD42-JP/c100342051.lua
+++ scripts/SD42-JP/c100342051.lua
@@ -1,42 +1,44 @@
+
 --No.99 希望皇ホープドラグナー
 --Script by XyLeN
-function c.initial_effect(c)
+function c100342051.initial_effect(c)
 	--xyz summon
 	aux.AddXyzProcedure(c,nil,12,3,nil,nil,99)
 	c:EnableReviveLimit()
 	--special summon
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetDescription(aux.Stringid(100342051,0))
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_QUICK_O)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetHintTiming(0,TIMING_END_PHASE)
-	e1:SetCountLimit(1,id)
-	e1:SetCost(c.spcost)
-	e1:SetTarget(c.sptg)
-	e1:SetOperation(c.spop)
+	e1:SetCountLimit(1,100342051)
+	e1:SetCost(c100342051.spcost)
+	e1:SetTarget(c100342051.sptg)
+	e1:SetOperation(c100342051.spop)
 	c:RegisterEffect(e1)
 	--loses ATK
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetDescription(aux.Stringid(100342051,1))
 	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
 	e2:SetRange(LOCATION_MZONE)
 	e2:SetCode(EVENT_ATTACK_ANNOUNCE)
-	e2:SetCountLimit(1,id+100)
-	e2:SetCondition(c.atkcon)
-	e2:SetOperation(c.atkop)
+	e2:SetCountLimit(1,100342051+100)
+	e2:SetCondition(c100342051.atkcon)
+	e2:SetOperation(c100342051.atkop)
 	c:RegisterEffect(e2)
-	Duel.AddCustomActivityCounter(id,ACTIVITY_SPSUMMON,c.counterfilter)
+	Duel.AddCustomActivityCounter(100342051,ACTIVITY_SPSUMMON,c100342051.counterfilter)
 end
-c.xyz_number=99
-function c.counterfilter(c)
+c100342051.xyz_number=99
+function c100342051.counterfilter(c)
 	return c:IsType(TYPE_XYZ)
 end
-function c.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+function c100342051.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
 	if chk==0 then return c:CheckRemoveOverlayCard(tp,2,REASON_COST)
 		and Duel.GetCustomActivityCount(id,tp,ACTIVITY_SPSUMMON)==0 end
+	c:RemoveOverlayCard(tp,2,2,REASON_COST)
 	--splimit
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
@@ -44,7 +46,7 @@ function c.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
 	e1:SetReset(RESET_PHASE+PHASE_END)
 	e1:SetTargetRange(1,0)
-	e1:SetTarget(c.splimit)
+	e1:SetTarget(c100342051.splimit)
 	Duel.RegisterEffect(e1,tp)
 	--cannot attack directly
 	local e2=Effect.CreateEffect(c)
@@ -52,19 +54,18 @@ function c.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	e2:SetCode(EFFECT_CANNOT_DIRECT_ATTACK)
 	e2:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
 	e2:SetTargetRange(LOCATION_MZONE,0)
-	e2:SetTarget(c.ftarget)
+	e2:SetTarget(c100342051.ftarget)
 	e2:SetLabel(c:GetFieldID())
 	e2:SetReset(RESET_PHASE+PHASE_END)
 	Duel.RegisterEffect(e2,tp)
-	c:RemoveOverlayCard(tp,2,2,REASON_COST)
 end
-function c.splimit(e,c,sump,sumtype,sumpos,targetp,se)
-	return not c:IsType(TYPE_XYZ)
+function c100342051.splimit(e,c,sump,sumtype,sumpos,targetp,se)
+	return not c:IsType(TYPE_XYZ) and c:IsLocation(LOCATION_EXTRA)
 end
-function c.ftarget(e,c)
+function c100342051.ftarget(e,c)
 	return e:GetLabel()~=c:GetFieldID()
 end
-function c.spfilter(c,e,tp,mc)
+function c100342051.spfilter(c,e,tp,mc)
 	local m=_G["c"..c:GetCode()]
 	if not m then return false end
 	local no=m.xyz_number
@@ -72,25 +73,25 @@ function c.spfilter(c,e,tp,mc)
 		and mc:IsCanBeXyzMaterial(c)
 		and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_XYZ,tp,false,false) and Duel.GetLocationCountFromEx(tp,tp,mc,c)>0
 end
-function c.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+function c100342051.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return aux.MustMaterialCheck(nil,tp,EFFECT_MUST_BE_SMATERIAL)
 		and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingMatchingCard(c.spfilter,tp,LOCATION_EXTRA,0,1,nil,e,tp,e:GetHandler()) end
+		and Duel.IsExistingMatchingCard(c100342051.spfilter,tp,LOCATION_EXTRA,0,1,nil,e,tp,e:GetHandler()) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
 end
-function c.spop(e,tp,eg,ep,ev,re,r,rp)
+function c100342051.spop(e,tp,eg,ep,ev,re,r,rp)
 	if not aux.MustMaterialCheck(nil,tp,EFFECT_MUST_BE_SMATERIAL) then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local tc=Duel.SelectMatchingCard(tp,c.spfilter,tp,LOCATION_EXTRA,0,1,1,nil,e,tp,e:GetHandler()):GetFirst() 
+	local tc=Duel.SelectMatchingCard(tp,c100342051.spfilter,tp,LOCATION_EXTRA,0,1,1,nil,e,tp,e:GetHandler()):GetFirst() 
 	if tc then
 		Duel.SpecialSummon(tc,SUMMON_TYPE_XYZ,tp,tp,false,false,POS_FACEUP)
 		tc:CompleteProcedure()
 	end
 end
-function c.atkcon(e,tp,eg,ep,ev,re,r,rp)
+function c100342051.atkcon(e,tp,eg,ep,ev,re,r,rp)
 	return tp~=Duel.GetTurnPlayer() and aux.nzatk(Duel.GetAttacker())
 end
-function c.atkop(e,tp,eg,ep,ev,re,r,rp)
+function c100342051.atkop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetAttacker()
 	if tc:IsRelateToBattle() and tc:IsFaceup() then
 		local e1=Effect.CreateEffect(e:GetHandler())
