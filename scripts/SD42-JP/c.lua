1a4c266d5a577e6d53215eb4a6c161651fb43451
Xy❤️ <80439628+XyleN5967@users.noreply.github.com>
2021-05-13 18:21:31 +0800

    No.99 希望皇ホープドラグナー(untested)
    untested and not given id yet

---
+++ scripts/SD42-JP/c.lua
@@ -0,0 +1,102 @@
+--No.99 希望皇ホープドラグナー
+--Script by XyLeN
+function c.initial_effect(c)
+	--xyz summon
+	aux.AddXyzProcedure(c,nil,12,3,nil,nil,99)
+	c:EnableReviveLimit()
+	--special summon
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_QUICK_O)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetHintTiming(0,TIMING_END_PHASE)
+	e1:SetCountLimit(1,id)
+	e1:SetCost(c.spcost)
+	e1:SetTarget(c.sptg)
+	e1:SetOperation(c.spop)
+	c:RegisterEffect(e1)
+	--loses ATK
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCode(EVENT_ATTACK_ANNOUNCE)
+	e2:SetCountLimit(1,id+100)
+	e2:SetCondition(c.atkcon)
+	e2:SetOperation(c.atkop)
+	c:RegisterEffect(e2)
+	Duel.AddCustomActivityCounter(id,ACTIVITY_SPSUMMON,c.counterfilter)
+end
+c.xyz_number=99
+function c.counterfilter(c)
+	return c:IsType(TYPE_XYZ)
+end
+function c.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return c:CheckRemoveOverlayCard(tp,2,REASON_COST)
+		and Duel.GetCustomActivityCount(id,tp,ACTIVITY_SPSUMMON)==0 end
+	--splimit
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_OATH)
+	e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+	e1:SetReset(RESET_PHASE+PHASE_END)
+	e1:SetTargetRange(1,0)
+	e1:SetTarget(c.splimit)
+	Duel.RegisterEffect(e1,tp)
+	--cannot attack directly
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_CANNOT_DIRECT_ATTACK)
+	e2:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
+	e2:SetTargetRange(LOCATION_MZONE,0)
+	e2:SetTarget(c.ftarget)
+	e2:SetLabel(c:GetFieldID())
+	e2:SetReset(RESET_PHASE+PHASE_END)
+	Duel.RegisterEffect(e2,tp)
+	c:RemoveOverlayCard(tp,2,2,REASON_COST)
+end
+function c.splimit(e,c,sump,sumtype,sumpos,targetp,se)
+	return not c:IsType(TYPE_XYZ)
+end
+function c.ftarget(e,c)
+	return e:GetLabel()~=c:GetFieldID()
+end
+function c.spfilter(c,e,tp,mc)
+	local m=_G["c"..c:GetCode()]
+	if not m then return false end
+	local no=m.xyz_number
+	return no and no>=1 and no<=99 and c:IsSetCard(0x48)
+		and mc:IsCanBeXyzMaterial(c)
+		and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_XYZ,tp,false,false) and Duel.GetLocationCountFromEx(tp,tp,mc,c)>0
+end
+function c.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return aux.MustMaterialCheck(nil,tp,EFFECT_MUST_BE_SMATERIAL)
+		and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(c.spfilter,tp,LOCATION_EXTRA,0,1,nil,e,tp,e:GetHandler()) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
+end
+function c.spop(e,tp,eg,ep,ev,re,r,rp)
+	if not aux.MustMaterialCheck(nil,tp,EFFECT_MUST_BE_SMATERIAL) then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local tc=Duel.SelectMatchingCard(tp,c.spfilter,tp,LOCATION_EXTRA,0,1,1,nil,e,tp,e:GetHandler()):GetFirst() 
+	if tc then
+		Duel.SpecialSummon(tc,SUMMON_TYPE_XYZ,tp,tp,false,false,POS_FACEUP)
+		tc:CompleteProcedure()
+	end
+end
+function c.atkcon(e,tp,eg,ep,ev,re,r,rp)
+	return tp~=Duel.GetTurnPlayer() and aux.nzatk(Duel.GetAttacker())
+end
+function c.atkop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetAttacker()
+	if tc:IsRelateToBattle() and tc:IsFaceup() then
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_SET_ATTACK_FINAL)
+		e1:SetValue(0)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		tc:RegisterEffect(e1)
+	end
+end
