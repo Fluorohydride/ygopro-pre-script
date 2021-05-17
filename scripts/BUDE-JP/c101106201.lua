9e3cab5b48d3c8cae26431f5f031d907b5d9067e
mercury233 <me@mercury233.me>
2021-05-17 15:04:31 +0800

    fix

--- scripts/BUDE-JP/c101106201.lua
+++ scripts/BUDE-JP/c101106201.lua
@@ -11,7 +11,7 @@ function c101106201.initial_effect(c)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetCountLimit(1,101106201)
 	e1:SetCost(c101106201.spcost)
-	e1:SetTarget(c101106201.target)
+	e1:SetTarget(c101106201.sptg)
 	e1:SetOperation(c101106201.spop)
 	c:RegisterEffect(e1)
 	--search
@@ -20,7 +20,7 @@ function c101106201.initial_effect(c)
 	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
 	e2:SetType(EFFECT_TYPE_IGNITION)
 	e2:SetRange(LOCATION_GRAVE)
-	e2:SetCountLimit(1,1011062011)
+	e2:SetCountLimit(1,101106201+100)
 	e2:SetCost(aux.bfgcost)
 	e2:SetTarget(c101106201.thtg)
 	e2:SetOperation(c101106201.thop)
@@ -31,12 +31,13 @@ function c101106201.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.Release(e:GetHandler(),REASON_COST)
 end
 function c101106201.spfilter(c,e,tp)
-	return c:IsType(TYPE_MONSTER) and c:IsSetCard(0x102) and c:IsCanBeSpecialSummoned(e,0,tp,false,false) and c:IsCanBeEffectTarget(e)
+	return c:IsType(TYPE_MONSTER) and c:IsSetCard(0x102) and c:IsCanBeEffectTarget(e)
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP_DEFENSE)
 end
-function c101106201.target(e,tp,eg,ep,ev,re,r,rp,chk)
+function c101106201.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local g=Duel.GetMatchingGroup(c101106201.spfilter,tp,LOCATION_GRAVE,0,nil,e,tp)
-	if chk==0 then return g:GetClassCount(Card.GetCode)>=2 end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	if chk==0 then return Duel.GetMZoneCount(tp,c)>=2 and g:GetClassCount(Card.GetCode)>=2 end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 	local g1=g:SelectSubGroup(tp,aux.dncheck,false,2,2)
 	Duel.SetTargetCard(g1)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g1,2,0,0)
@@ -48,17 +49,16 @@ function c101106201.spop(e,tp,eg,ep,ev,re,r,rp)
 		Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP_DEFENSE)
 	end
 end
-
 function c101106201.thfilter(c)
 	return c:IsCode(101106203) or c:IsCode(67526112) and c:IsAbleToHand()
 end
 function c101106201.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(c101106201.thfilter,tp,LOCATION_DECK,0,1,nil) end
-	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+	if chk==0 then return Duel.IsExistingMatchingCard(c101106201.thfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK+LOCATION_GRAVE)
 end
 function c101106201.thop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-	local g=Duel.SelectMatchingCard(tp,c101106201.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+	local g=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(c101106201.thfilter),tp,LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil)
 	if #g>0 then
 		Duel.SendtoHand(g,nil,REASON_EFFECT)
 		Duel.ConfirmCards(1-tp,g)
--- scripts/BUDE-JP/c101106202.lua
+++ scripts/BUDE-JP/c101106202.lua
@@ -17,11 +17,11 @@ function c101106202.initial_effect(c)
 	c:RegisterEffect(e1)
 	--send to hand
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(101106202,0))
+	e2:SetDescription(aux.Stringid(101106202,1))
 	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_DESTROY)
 	e2:SetType(EFFECT_TYPE_IGNITION)
 	e2:SetRange(LOCATION_GRAVE)
-	e2:SetCountLimit(1,1011062022)
+	e2:SetCountLimit(1,101106202+100)
 	e2:SetTarget(c101106202.thtg)
 	e2:SetOperation(c101106202.thop)
 	c:RegisterEffect(e2)
@@ -30,44 +30,50 @@ function c101106202.condition(e,tp,eg,ep,ev,re,r,rp)
 	return tp~=ep and Duel.GetCurrentChain()==0
 end
 function c101106202.filter(c,e)
-	return c:IsSetCard(0x102) and c:IsType(TYPE_MONSTER) 
+	return c:IsFaceup() and c:IsSetCard(0x102) and c:IsType(TYPE_MONSTER)
 end
 function c101106202.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	local tc=e:GetHandler()
-	if chk==0 then return (Duel.IsExistingMatchingCard(c101106202.filter,tp,LOCATION_MZONE,0,1,nil,e) or tc) end
+	local c=e:GetHandler()
+	if chk==0 then return true end
 	Duel.SetOperationInfo(0,CATEGORY_DISABLE_SUMMON,eg,eg:GetCount(),0,0)
-	Duel.SetOperationInfo(0,CATEGORY_DESTROY,eg,eg:GetCount(),0,0)
+	local g=Duel.GetMatchingGroup(c101106202.filter,tp,LOCATION_MZONE,0,1,1,nil)
+	g:AddCard(c)
+	g:Merge(eg)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,eg:GetCount()+1,0,0)
 end
 function c101106202.operation(e,tp,eg,ep,ev,re,r,rp)
 	local tc=e:GetHandler()
 	Duel.NegateSummon(eg)
 	Duel.Destroy(eg,REASON_EFFECT)
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
 	local g=Duel.GetMatchingGroup(c101106202.filter,tp,LOCATION_MZONE,0,1,1,nil)
 	g:AddCard(tc)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
 	local sg=g:Select(tp,1,1,nil)
 	if sg:GetCount()>0 then
 		Duel.BreakEffect()
+		Duel.HintSelection(sg)
 		Duel.Destroy(sg,REASON_EFFECT)
 	end
 end
-
 function c101106202.thfilter(c)
-	return c:IsType(TYPE_MONSTER) and (c:IsSetCard(0x10f) or c:IsSetCard(0x102)) and c:IsAbleToHand()
+	return c:IsType(TYPE_MONSTER) and c:IsSetCard(0x10f,0x102) and c:IsAbleToHand()
 end
 function c101106202.thtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and c101106202.thfilter(chkc) end
 	if chk==0 then return Duel.IsExistingTarget(c101106202.thfilter,tp,LOCATION_GRAVE,0,1,nil)
-		and Duel.IsExistingMatchingCard(nil,tp,LOCATION_HAND,0,2,nil) end
+		and Duel.IsExistingMatchingCard(nil,tp,LOCATION_ONFIELD+LOCATION_HAND,0,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
 	local g1=Duel.SelectTarget(tp,c101106202.thfilter,tp,LOCATION_GRAVE,0,1,1,nil)
+	local g=Duel.GetMatchingGroup(nil,tp,LOCATION_ONFIELD+LOCATION_HAND,0,nil)
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,0,0)
-	Duel.SetOperationInfo(0,CATEGORY_DESTROY,nil,0,tp,2)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
 end
 function c101106202.thop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
-	local g=Duel.SelectMatchingCard(tp,aux.TRUE,tp,LOCATION_ONFIELD+LOCATION_HAND,0,1,1,nil)
+	local g=Duel.SelectMatchingCard(tp,nil,tp,LOCATION_ONFIELD+LOCATION_HAND,0,1,1,nil)
+	if #g==0 then return end
+	Duel.HintSelection(g)
 	if Duel.Destroy(g,REASON_EFFECT)~=0 and tc:IsRelateToEffect(e) then
 		Duel.SendtoHand(tc,nil,REASON_EFFECT)
 	end
