7159e011bbd55a61bef61614369f785126f54c99
mercury233 <me@mercury233.me>
2021-05-17 19:12:11 +0800

    add SR吹持童子

---
+++ scripts/DP25-JP/c100425001.lua
@@ -0,0 +1,82 @@
+--SR吹持童子
+--
+--Script by mercury233
+function c100425001.initial_effect(c)
+	--search
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(100425001,0))
+	e1:SetCategory(CATEGORY_SEARCH+CATEGORY_TOHAND)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_SUMMON_SUCCESS)
+	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
+	e1:SetCountLimit(1,100425001)
+	e1:SetTarget(c100425001.thtg)
+	e1:SetOperation(c100425001.thop)
+	c:RegisterEffect(e1)
+	local e2=e1:Clone()
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	c:RegisterEffect(e2)
+	--decrease level
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(100425001,1))
+	e3:SetType(EFFECT_TYPE_IGNITION)
+	e3:SetRange(LOCATION_GRAVE)
+	e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e3:SetCountLimit(1,100425001+100)
+	e3:SetCost(aux.bfgcost)
+	e3:SetTarget(c100425001.target)
+	e3:SetOperation(c100425001.operation)
+	c:RegisterEffect(e3)
+end
+function c100425001.cfilter(c)
+	return c:IsFaceup() and c:IsAttribute(ATTRIBUTE_WIND)
+end
+function c100425001.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local ct=Duel.GetMatchingGroupCount(c100425001.cfilter,tp,LOCATION_MZONE,0,e:GetHandler())
+	if chk==0 then return Duel.GetFieldGroupCount(tp,LOCATION_DECK,0)>=ct
+		and Duel.GetDecktopGroup(tp,ct):IsExists(Card.IsAbleToHand,1,nil)end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function c100425001.thop(e,tp,eg,ep,ev,re,r,rp)
+	local ct=Duel.GetMatchingGroupCount(c100425001.cfilter,tp,LOCATION_MZONE,0,aux.ExceptThisCard(e))
+	local g=Duel.GetDecktopGroup(tp,ct)
+	if #g>0 then
+		Duel.DisableShuffleCheck()
+		Duel.Hint(HINT_SELECTMSG,p,HINTMSG_ATOHAND)
+		local sc=g:Select(tp,1,1,nil):GetFirst()
+		if sc:IsAbleToHand() then
+			Duel.SendtoHand(sc,nil,REASON_EFFECT)
+			Duel.ConfirmCards(1-tp,sc)
+			Duel.ShuffleHand(tp)
+		else
+			Duel.SendtoGrave(sc,REASON_RULE)
+		end
+	end
+	if #g>1 then
+		Duel.SortDecktop(tp,tp,#g-1)
+		for i=1,#g-1 do
+			local dg=Duel.GetDecktopGroup(tp,1)
+			Duel.MoveSequence(dg:GetFirst(),1)
+		end
+	end
+end
+function c100425001.filter(c)
+	return c:IsFaceup() and c:IsLevelAbove(3) and c:IsAttribute(ATTRIBUTE_WIND)
+end
+function c100425001.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and c100425001.filter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(c100425001.filter,tp,LOCATION_MZONE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	local g=Duel.SelectTarget(tp,c100425001.filter,tp,LOCATION_MZONE,0,1,1,nil,tp)
+end
+function c100425001.operation(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsFaceup() and tc:IsRelateToEffect(e) then
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_LEVEL)
+		e1:SetValue(-2)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		tc:RegisterEffect(e1)
+	end
+end
