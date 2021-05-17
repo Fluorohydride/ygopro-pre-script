399a3d3148d578ebe175200f32f28d2e49dff1c6
Mercury233 <me@mercury233.me>
2021-05-17 10:40:35 +0800

    Update c100342051.lua

--- scripts/SD42-JP/c100342051.lua
+++ scripts/SD42-JP/c100342051.lua
@@ -1,4 +1,3 @@
-
 --No.99 希望皇ホープドラグナー
 --Script by XyLeN
 function c100342051.initial_effect(c)
@@ -29,15 +28,38 @@ function c100342051.initial_effect(c)
 	e2:SetOperation(c100342051.atkop)
 	c:RegisterEffect(e2)
 	Duel.AddCustomActivityCounter(100342051,ACTIVITY_SPSUMMON,c100342051.counterfilter)
+	if not c100342051.global_check then
+		c100342051.global_check=true
+		local ge1=Effect.CreateEffect(c)
+		ge1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		ge1:SetCode(EVENT_ATTACK_ANNOUNCE)
+		ge1:SetOperation(c100342051.dacheck)
+		Duel.RegisterEffect(ge1,0)
+	end
 end
 c100342051.xyz_number=99
 function c100342051.counterfilter(c)
-	return c:IsType(TYPE_XYZ)
+	return not c:IsSummonLocation(LOCATION_EXTRA) or c:IsType(TYPE_XYZ)
+end
+function c100342051.dacheck(e,tp,eg,ep,ev,re,r,rp)
+	local tc=eg:GetFirst()
+	local p=tc:GetControler()
+	if Duel.GetAttackTarget()~=nil then return end
+	if tc:GetFlagEffect(100342051)==0 then
+		tc:RegisterFlagEffect(100342051,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,0,1)
+		if Duel.GetFlagEffect(p,100342051)==0 then
+			Duel.RegisterFlagEffect(p,100342051,RESET_PHASE+PHASE_END,0,1)
+		else
+			Duel.RegisterFlagEffect(p,100342051+100,RESET_PHASE+PHASE_END,0,1)
+		end
+	end
 end
 function c100342051.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
 	if chk==0 then return c:CheckRemoveOverlayCard(tp,2,REASON_COST)
-		and Duel.GetCustomActivityCount(id,tp,ACTIVITY_SPSUMMON)==0 end
+		and Duel.GetCustomActivityCount(id,tp,ACTIVITY_SPSUMMON)==0
+		and Duel.GetFlagEffect(tp,100342051+100)==0
+		and (Duel.GetFlagEffect(tp,100342051)==0 or tc:GetFlagEffect(100342051)~=0) end
 	--splimit
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
@@ -51,7 +73,7 @@ function c100342051.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_FIELD)
 	e2:SetCode(EFFECT_CANNOT_DIRECT_ATTACK)
-	e2:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
+	e2:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE+EFFECT_FLAG_OATH)
 	e2:SetTargetRange(LOCATION_MZONE,0)
 	e2:SetTarget(c100342051.ftarget)
 	e2:SetLabel(c:GetFieldID())
@@ -65,24 +87,23 @@ end
 function c100342051.ftarget(e,c)
 	return e:GetLabel()~=c:GetFieldID()
 end
-function c100342051.spfilter(c,e,tp,mc)
+function c100342051.spfilter(c,e,tp)
 	local m=_G["c"..c:GetCode()]
 	if not m then return false end
 	local no=m.xyz_number
 	return no and no>=1 and no<=100 and c:IsSetCard(0x48)
-		and mc:IsCanBeXyzMaterial(c)
-		and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_XYZ,tp,false,false) and Duel.GetLocationCountFromEx(tp,tp,mc,c)>0
+		and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_XYZ,tp,false,false) and Duel.GetLocationCountFromEx(tp,tp,nil,c)>0
 end
 function c100342051.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return aux.MustMaterialCheck(nil,tp,EFFECT_MUST_BE_SMATERIAL)
 		and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingMatchingCard(c100342051.spfilter,tp,LOCATION_EXTRA,0,1,nil,e,tp,e:GetHandler()) end
+		and Duel.IsExistingMatchingCard(c100342051.spfilter,tp,LOCATION_EXTRA,0,1,nil,e,tp) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
 end
 function c100342051.spop(e,tp,eg,ep,ev,re,r,rp)
 	if not aux.MustMaterialCheck(nil,tp,EFFECT_MUST_BE_SMATERIAL) then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local tc=Duel.SelectMatchingCard(tp,c100342051.spfilter,tp,LOCATION_EXTRA,0,1,1,nil,e,tp,e:GetHandler()):GetFirst() 
+	local tc=Duel.SelectMatchingCard(tp,c100342051.spfilter,tp,LOCATION_EXTRA,0,1,1,nil,e,tp):GetFirst() 
 	if tc then
 		Duel.SpecialSummon(tc,SUMMON_TYPE_XYZ,tp,tp,false,false,POS_FACEUP)
 		tc:CompleteProcedure()
