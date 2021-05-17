26960f2deed00e3e5bcf9c37f65ab37eec6e861f
Mercury233 <me@mercury233.me>
2021-05-17 11:35:19 +0800

    Update c100425005.lua

--- scripts/DP25-JP/c100425005.lua
+++ scripts/DP25-JP/c100425005.lua
@@ -1,10 +1,12 @@
+--HSR/CWライダー
 --Hi-Speedroid Clear Wing Rider
+--Script by TheOnePharaoh
 function c100425005.initial_effect(c)
 	c:EnableReviveLimit()
 	aux.AddSynchroProcedure(c,aux.FilterBoolFunction(Card.IsAttribute,ATTRIBUTE_WIND),aux.NonTuner(c100425005.sfilter),1,1)
 	--Dice Popboost
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(100425005,1))
+	e1:SetDescription(aux.Stringid(100425005,0))
 	e1:SetCategory(CATEGORY_TODECK+CATEGORY_DICE+CATEGORY_DESTROY+CATEGORY_ATKCHANGE)
 	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetRange(LOCATION_MZONE)
@@ -31,49 +33,47 @@ c100425005.material_type=TYPE_SYNCHRO
 function c100425005.sfilter(c)
 	return c:IsAttribute(ATTRIBUTE_WIND) and c:IsType(TYPE_SYNCHRO)
 end
-
 function c100425005.gyfilter(c)
 	return c:IsAttribute(ATTRIBUTE_WIND) and c:IsAbleToDeck()
 end
-
 function c100425005.destg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then		return Duel.IsExistingMatchingCard(c100425005.gyfilter,tp,LOCATION_GRAVE,0,1,nil) end
+	if chk==0 then return Duel.IsExistingMatchingCard(c100425005.gyfilter,tp,LOCATION_GRAVE,0,1,nil) end
 	Duel.SetOperationInfo(0,CATEGORY_DICE,nil,0,tp,1)
 end
-
 function c100425005.desop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local dc=Duel.TossDice(tp,1)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
 	local gy=Duel.SelectMatchingCard(tp,c100425005.gyfilter,tp,LOCATION_GRAVE,0,1,dc,nil)
+	if #gy==0 then return end
 	local yc=Duel.SendtoDeck(gy,nil,2,REASON_EFFECT)
-	if  yc>0 and Duel.SelectYesNo(tp,aux.Stringid(100425005,2)) then
 	local g=Duel.GetMatchingGroup(aux.TRUE,tp,0,LOCATION_ONFIELD,nil)
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
-	local dg=g:Select(tp,1,yc,nil)
-	Duel.HintSelection(dg)
-	local ct=Duel.Destroy(dg,REASON_EFFECT)
-	if ct>0 and c:IsFaceup() and c:IsRelateToEffect(e) then
-		local e1=Effect.CreateEffect(c)
-		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetCode(EFFECT_UPDATE_ATTACK)
-		e1:SetValue(ct*500)
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_DISABLE+RESET_PHASE+PHASE_END)
-		c:RegisterEffect(e1)
+	if yc>0 and #g>0 and Duel.SelectYesNo(tp,aux.Stringid(100425005,2)) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+		local dg=g:Select(tp,1,yc,nil)
+		Duel.HintSelection(dg)
+		local ct=Duel.Destroy(dg,REASON_EFFECT)
+		if ct>0 and c:IsFaceup() and c:IsRelateToEffect(e) then
+			local e1=Effect.CreateEffect(c)
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetCode(EFFECT_UPDATE_ATTACK)
+			e1:SetValue(ct*500)
+			e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_DISABLE+RESET_PHASE+PHASE_END)
+			c:RegisterEffect(e1)
+		end
 	end
 end
 function c100425005.spcon(e,tp,eg,ep,ev,re,r,rp)
 	return e:GetHandler():IsSummonType(SUMMON_TYPE_SYNCHRO) and Duel.GetTurnPlayer()~=tp
 		and (Duel.GetCurrentPhase()==PHASE_MAIN1 or Duel.GetCurrentPhase()==PHASE_MAIN2)
-
 end
 function c100425005.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return e:GetHandler():IsReleasable() end
 	Duel.Release(e:GetHandler(),REASON_COST)
 end
 function c100425005.spfilter(c,e,tp,mc)
-	return c:IsRace(RACE_DRAGON) and c:IsAttribute(ATTRIBUTE_WIND) and c:IsType(TYPE_SYNCHRO)
-		and c:IsLevel(7) and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP)
+	return c:IsLevel(7) and c:IsAttribute(ATTRIBUTE_WIND) and c:IsType(TYPE_SYNCHRO)
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP)
 		and Duel.GetLocationCountFromEx(tp,tp,mc,c)>0
 end
 function c100425005.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
@@ -81,8 +81,11 @@ function c100425005.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
 end
 function c100425005.spop(e,tp,eg,ep,ev,re,r,rp)
+	local ct=Duel.GetLocationCountFromEx(tp,tp,nil,TYPE_SYNCHRO)
+	local g=Duel.GetMatchingGroup(c100425005.spfilter,tp,LOCATION_EXTRA,0,nil,e,tp,nil)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectMatchingCard(tp,c100425005.spfilter,tp,LOCATION_EXTRA,0,1,2,nil,e,tp,nil)
-	Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+	local sg=g:SelectSubGroup(tp,aux.dncheck,false,1,ct)
+	if sg then
+		Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP)
 	end
 end
