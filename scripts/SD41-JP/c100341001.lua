fbd18ca9d80b32b668525378826f06f804ac81d9
mercury233 <me@mercury233.me>
2021-05-19 15:00:51 +0800

    fix アタッチメント・サイバーン

--- scripts/SD41-JP/c100341001.lua
+++ scripts/SD41-JP/c100341001.lua
@@ -40,9 +40,9 @@ end
 function c100341001.eqtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and c100341001.filter(chkc) end
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_SZONE)>0
-		and Duel.IsExistingTarget(c100341001.filter,tp,LOCATION_MZONE,0,1,nil) end
+		and Duel.IsExistingTarget(c100341001.filter,tp,LOCATION_MZONE,0,1,e:GetHandler()) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)
-	Duel.SelectTarget(tp,c100341001.filter,tp,LOCATION_MZONE,0,1,1,nil)
+	Duel.SelectTarget(tp,c100341001.filter,tp,LOCATION_MZONE,0,1,1,e:GetHandler())
 end
 function c100341001.eqop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
