95bee06f68a36095063ed27774da411fbdc2f2ca
mercury233 <me@mercury233.me>
2021-05-19 08:18:28 +0800

    fix ティンクル・ファイブスター

--- scripts/AC01-JP/c100278006.lua
+++ scripts/AC01-JP/c100278006.lua
@@ -2,6 +2,7 @@
 --
 --Script by mercury233
 function c100278006.initial_effect(c)
+	aux.AddCodeList(c,40640057)
 	--Activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
