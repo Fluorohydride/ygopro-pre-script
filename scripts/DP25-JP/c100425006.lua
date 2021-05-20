485bf2bf19cef0b53248d66946e7257d8fc3439c
Xy❤️ <80439628+XyleN5967@users.noreply.github.com>
2021-05-20 08:08:25 +0800

    add SRルーレット

---
+++ scripts/DP25-JP/c100425006.lua
@@ -0,0 +1,43 @@
+--SRルーレット
+--scripted by XyleN
+function c100425006.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_DICE+CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCost(c100425006.cost)
+	e1:SetTarget(c100425006.target)
+	e1:SetOperation(c100425006.activate)
+	c:RegisterEffect(e1)
+end
+c100425006.toss_dice=true
+function c100425006.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DISCARD)
+	local g=Duel.SelectMatchingCard(tp,Card.IsDiscardable,tp,LOCATION_HAND,0,1,1,nil)
+	Duel.SendtoGrave(g,REASON_COST+REASON_DISCARD)
+end
+function c100425006.filter(c,e,tp)
+	return c:IsSetCard(0x2016) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function c100425006.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_DICE,nil,0,tp,1)
+end
+function c100425006.activate(e,tp,eg,ep,ev,re,r,rp)
+	local dc=Duel.TossDice(tp,1)
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	if ft<=0 then return end
+	if ft>2 then ft=2 end
+	if Duel.IsPlayerAffectedByEffect(tp,59822133) then ft=1 end
+	local g=Duel.GetMatchingGroup(c100425006.filter,tp,LOCATION_DECK+LOCATION_HAND,0,nil,e,tp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local sg=g:SelectWithSumEqual(tp,Card.GetLevel,dc,1,ft)
+	if sg and sg:GetCount()>0 then
+		Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP)
+	else
+		local lp=Duel.GetLP(tp)
+		Duel.SetLP(tp,lp-dc*500)
+	end
+end
