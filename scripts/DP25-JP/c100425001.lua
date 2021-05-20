3f036b49fff182bdd4dff7997680fc8b983aded1
mercury233 <me@mercury233.me>
2021-05-20 14:11:14 +0800

    fix SR吹持童子

--- scripts/DP25-JP/c100425001.lua
+++ scripts/DP25-JP/c100425001.lua
@@ -8,7 +8,7 @@ function c100425001.initial_effect(c)
 	e1:SetCategory(CATEGORY_SEARCH+CATEGORY_TOHAND)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e1:SetCode(EVENT_SUMMON_SUCCESS)
-	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
+	e1:SetProperty(EFFECT_FLAG_DELAY)
 	e1:SetCountLimit(1,100425001)
 	e1:SetTarget(c100425001.thtg)
 	e1:SetOperation(c100425001.thop)
@@ -34,7 +34,7 @@ end
 function c100425001.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local ct=Duel.GetMatchingGroupCount(c100425001.cfilter,tp,LOCATION_MZONE,0,e:GetHandler())
 	if chk==0 then return Duel.GetFieldGroupCount(tp,LOCATION_DECK,0)>=ct
-		and Duel.GetDecktopGroup(tp,ct):IsExists(Card.IsAbleToHand,1,nil)end
+		and Duel.GetDecktopGroup(tp,ct):IsExists(Card.IsAbleToHand,1,nil) end
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
 end
 function c100425001.thop(e,tp,eg,ep,ev,re,r,rp)
