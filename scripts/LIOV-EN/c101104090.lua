ae83f56e3b9a9f0f36c31924d8cd9c1d0e842f6c
Xy❤️ <80439628+XyleN5967@users.noreply.github.com>
2021-05-23 20:44:48 +0800

    add War Rock Spirit

---
+++ scripts/LIOV-EN/c101104090.lua
@@ -0,0 +1,73 @@
+--War Rock Spirit
+--scripted by XyleN
+function c101104090.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCountLimit(1,101104090+EFFECT_COUNT_CODE_OATH)
+	e1:SetCondition(c101104090.condition)
+	e1:SetTarget(c101104090.target)
+	e1:SetOperation(c101104090.activate)
+	c:RegisterEffect(e1)
+end
+function c101104090.condition(e,tp,eg,ep,ev,re,r,rp)
+	local ph=Duel.GetCurrentPhase()
+	return ph>=PHASE_BATTLE_START and ph<=PHASE_BATTLE
+end
+function c101104090.filter(c,e,tp)
+	return c:IsSetCard(0x15f) and (c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP_ATTACK) or c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP_DEFENSE))
+end
+function c101104090.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_GRAVE) and c101104090.filter(chkc,e,tp) end
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingTarget(c101104090.filter,tp,LOCATION_GRAVE,LOCATION_GRAVE,1,nil,e,tp) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectTarget(tp,c101104090.filter,tp,LOCATION_GRAVE,LOCATION_GRAVE,1,1,nil,e,tp)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
+end
+function c101104090.activate(e,tp,eg,ep,ev,re,r,rp)
+	local op=0
+	local c=e:GetHandler()
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) then
+		op=Duel.SelectOption(tp,aux.Stringid(101104090,0),aux.Stringid(101104090,1)) 
+	else
+		op=Duel.SelectOption(tp,aux.Stringid(101104090,0))+1
+	end
+	if op==0 then
+		Duel.SpecialSummonStep(tc,0,tp,tp,false,false,POS_FACEUP_ATTACK)
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_DISABLE)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		tc:RegisterEffect(e1)
+		local e2=e1:Clone()
+		e2:SetCode(EFFECT_DISABLE_EFFECT)
+		tc:RegisterEffect(e2)
+		local e3=Effect.CreateEffect(c)
+		e3:SetType(EFFECT_TYPE_SINGLE)
+		e3:SetCode(EFFECT_CANNOT_DIRECT_ATTACK)
+		e3:SetReset(RESET_EVENT+RESETS_STANDARD)
+		tc:RegisterEffect(e3)
+		Duel.SpecialSummonComplete()
+	else
+		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP_DEFENSE)
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_FIELD)
+		e1:SetCode(EFFECT_INDESTRUCTABLE_COUNT)
+		e1:SetTargetRange(LOCATION_MZONE,0)
+		e1:SetTarget(c101104090.indtg)
+		e1:SetValue(c101104090.indct)
+		e1:SetReset(RESET_PHASE+PHASE_END)
+		Duel.RegisterEffect(e1,tp) 
+	end
+end
+function c101104090.indtg(e,c)
+	return c:IsFaceup() and c:IsSetCard(0x15f)
+end
+function c101104090.indct(e,re,r,rp)
+	if bit.band(r,REASON_BATTLE)~=0 then
+		return 1
+	else return 0 end
+end
