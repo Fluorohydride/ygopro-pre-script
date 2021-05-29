0613f0aa631c87ca5b4b2cd40ff6112168897563
JSY1728 <34511983+JSY1728@users.noreply.github.com>
2021-05-29 23:24:30 +0900

    Update c100278032.lua

--- scripts/AC01-JP/c100278032.lua
+++ scripts/AC01-JP/c100278032.lua
@@ -3,7 +3,7 @@
 function c100278032.initial_effect(c)
 	--Destroy
 	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_DESTROY)
+	e1:SetCategory(CATEGORY_DESTROY+CATEGORY_DAMAGE)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetCondition(c100278032.descon)
@@ -25,6 +25,7 @@ function c100278032.destg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(c100278032.desfilter,tp,0,LOCATION_ONFIELD,1,c) end
 	local sg=Duel.GetMatchingGroup(c100278032.desfilter,tp,0,LOCATION_ONFIELD,c)
 	Duel.SetOperationInfo(0,CATEGORY_DESTROY,sg,sg:GetCount(),0,0)
+	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,1000)
 end
 function c100278032.desop(e,tp,eg,ep,ev,re,r,rp)
 	local sg=Duel.GetMatchingGroup(c100278032.desfilter,tp,0,LOCATION_ONFIELD,aux.ExceptThisCard(e))
