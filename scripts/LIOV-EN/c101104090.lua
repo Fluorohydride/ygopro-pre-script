975d893b187f280a429b9fd48c3f49e4c720effe
Mercury233 <me@mercury233.me>
2021-06-05 11:12:11 +0800

    Update c101104090.lua

--- scripts/LIOV-EN/c101104090.lua
+++ scripts/LIOV-EN/c101104090.lua
@@ -1,5 +1,5 @@
 --War Rock Spirit
---scripted by XyleN
+--scripted by XyleN & mercury233
 function c101104090.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
@@ -15,44 +15,63 @@ function c101104090.condition(e,tp,eg,ep,ev,re,r,rp)
 	local ph=Duel.GetCurrentPhase()
 	return ph>=PHASE_BATTLE_START and ph<=PHASE_BATTLE
 end
-function c101104090.filter(c,e,tp)
-	return c:IsSetCard(0x15f) and (c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP_ATTACK) or c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP_DEFENSE))
+function c101104090.afilter(c,e,tp)
+	return c:IsSetCard(0x15f) and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP_ATTACK)
+end
+function c101104090.dfilter(c,e,tp)
+	return c:IsSetCard(0x15f) and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP_DEFENSE)
 end
 function c101104090.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_GRAVE) and c101104090.filter(chkc,e,tp) end
+	if chkc then
+		local op=e:GetLabel()
+		return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_GRAVE)
+			and (op==0 and c101104090.afilter(chkc,e,tp) or op==1 and c101104090.dfilter(chkc,e,tp))
+	end
+	local b1=Duel.IsExistingTarget(c101104090.afilter,tp,LOCATION_GRAVE,0,1,nil,e,tp)
+	local b2=Duel.IsExistingTarget(c101104090.dfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingTarget(c101104090.filter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
+		and (b1 or b2) end
+	local op=0
+	if b1 and b2 then
+		op=Duel.SelectOption(tp,aux.Stringid(101104090,0),aux.Stringid(101104090,1))
+	elseif b1 then
+		op=Duel.SelectOption(tp,aux.Stringid(101104090,0))
+	else
+		op=Duel.SelectOption(tp,aux.Stringid(101104090,1))+1
+	end
+	e:SetLabel(op)
+	local filter=c101104090.afilter
+	if op==1 then filter=c101104090.dfilter end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectTarget(tp,c101104090.filter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
+	local g=Duel.SelectTarget(tp,filter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
 end
 function c101104090.activate(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local tc=Duel.GetFirstTarget()
-	local op=0
+	local op=e:GetLabel()
 	if tc:IsRelateToEffect(e) then
-		op=Duel.SelectOption(tp,aux.Stringid(101104090,0),aux.Stringid(101104090,1)) 
-	else
-		op=Duel.SelectOption(tp,aux.Stringid(101104090,0))+1
+		if op==0 then
+			Duel.SpecialSummonStep(tc,0,tp,tp,false,false,POS_FACEUP_ATTACK)
+			local e1=Effect.CreateEffect(c)
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetCode(EFFECT_DISABLE)
+			e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+			tc:RegisterEffect(e1)
+			local e2=e1:Clone()
+			e2:SetCode(EFFECT_DISABLE_EFFECT)
+			tc:RegisterEffect(e2)
+			local e3=Effect.CreateEffect(c)
+			e3:SetType(EFFECT_TYPE_SINGLE)
+			e3:SetCode(EFFECT_CANNOT_DIRECT_ATTACK)
+			e3:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+			tc:RegisterEffect(e3)
+			Duel.SpecialSummonComplete()
+		else
+			Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP_DEFENSE)
+		end
 	end
-	if op==0 then
-		Duel.SpecialSummonStep(tc,0,tp,tp,false,false,POS_FACEUP_ATTACK)
-		local e1=Effect.CreateEffect(c)
-		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetCode(EFFECT_DISABLE)
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
-		tc:RegisterEffect(e1)
-		local e2=e1:Clone()
-		e2:SetCode(EFFECT_DISABLE_EFFECT)
-		tc:RegisterEffect(e2)
-		local e3=Effect.CreateEffect(c)
-		e3:SetType(EFFECT_TYPE_SINGLE)
-		e3:SetCode(EFFECT_CANNOT_DIRECT_ATTACK)
-		e3:SetReset(RESET_EVENT+RESETS_STANDARD)
-		tc:RegisterEffect(e3)
-		Duel.SpecialSummonComplete()
-	else
-		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP_DEFENSE)
+	if op==1 then
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_FIELD)
 		e1:SetCode(EFFECT_INDESTRUCTABLE_COUNT)
@@ -60,7 +79,7 @@ function c101104090.activate(e,tp,eg,ep,ev,re,r,rp)
 		e1:SetTarget(c101104090.indtg)
 		e1:SetValue(c101104090.indct)
 		e1:SetReset(RESET_PHASE+PHASE_END)
-		Duel.RegisterEffect(e1,tp) 
+		Duel.RegisterEffect(e1,tp)
 	end
 end
 function c101104090.indtg(e,c)
