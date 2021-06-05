6ccea3d1022112829db821903be06793197786f5
Mercury233 <me@mercury233.me>
2021-06-05 14:47:37 +0800

    Update c100278033.lua

--- scripts/AC01-JP/c100278033.lua
+++ scripts/AC01-JP/c100278033.lua
@@ -1,5 +1,5 @@
 --人形の家
---scripted by XyLeN
+--scripted by XyLeN & mercury233
 function c100278033.initial_effect(c)
 	--Activate
 	local e0=Effect.CreateEffect(c)
@@ -29,56 +29,79 @@ function c100278033.initial_effect(c)
 	c:RegisterEffect(e2)
 end
 function c100278033.filter(c,e,tp)
-	return c:IsFaceup() and c:IsType(TYPE_NORMAL) and c:IsAttack(0) or c:IsDefense(0)
-		and Duel.IsExistingMatchingCard(c100278033.filter2,tp,LOCATION_DECK,0,1,nil,c:GetCode(),e,tp)
+	return c:IsType(TYPE_NORMAL) and (c:IsAttack(0) or c:IsDefense(0)) and c:IsCanBeEffectTarget(e)
+		and Duel.IsExistingMatchingCard(c100278033.spfilter,tp,LOCATION_DECK,0,1,nil,e,tp,c:GetCode(),nil)
 end
-function c100278033.filter2(c,code,e,tp)
-	return c:IsCode(code) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+function c100278033.spfilter(c,e,tp,code,code2)
+	return c:IsCode(code,code2) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function c100278033.cfilter(c)
-	return c:IsFaceup() and c:IsCode(75574498) 
+	return c:IsFaceup() and c:IsCode(75574498)
+end
+function c100278033.gcheck(sg,e,tp)
+	if #sg==1 then return true end
+	local code1=sg:GetFirst():GetCode()
+	local code2=sg:GetNext():GetCode()
+	local tg=Duel.GetMatchingGroup(c100278033.spfilter,tp,LOCATION_DECK,0,nil,e,tp,code1,code2)
+	return tg:CheckSubGroup(aux.gfcheck,2,2,Card.IsCode,code1,code2)
 end
 function c100278033.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and c100278033.filter(chkc,e,tp) end
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingTarget(c100278033.filter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
-	local value=math.min(2,(Duel.GetLocationCount(tp,LOCATION_MZONE)))
-	if Duel.IsPlayerAffectedByEffect(tp,59822133) or not Duel.IsExistingMatchingCard(c100278033.cfilter,tp,LOCATION_MZONE,0,1,nil) then value=1 end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
-	local g=Duel.SelectTarget(tp,c100278033.filter,tp,LOCATION_GRAVE,0,1,value,nil,e,tp)
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and c100278033.filter(chkc,e,tp) end
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	if chk==0 then return ft>0 and Duel.IsExistingTarget(c100278033.filter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
+	local ct=math.min(2,ft)
+	if Duel.IsPlayerAffectedByEffect(tp,59822133)
+		or not Duel.IsExistingMatchingCard(c100278033.cfilter,tp,LOCATION_MZONE,0,1,nil) then ct=1 end
+	local g=Duel.GetMatchingGroup(c100278033.filter,tp,LOCATION_GRAVE,0,nil,e,tp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+	local tg=g:SelectSubGroup(tp,c100278033.gcheck,false,1,ct,e,tp)
+	Duel.SetTargetCard(tg)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,#tg,tp,LOCATION_DECK)
 end
 function c100278033.spop(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS):Filter(Card.IsRelateToEffect,nil,e)
-	local tc=g:GetFirst()
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	if ft<=0 then return end
+	local tg=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS):Filter(Card.IsRelateToEffect,nil,e)
+	local code1,code2
+	local tc=tg:GetFirst()
 	while tc do
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-		local sg=Duel.SelectMatchingCard(tp,c100278033.filter2,tp,LOCATION_DECK,0,1,1,nil,tc:GetCode(),e,tp)
-		local sc=sg:GetFirst() 
-		while sc do
-			Duel.SpecialSummonStep(sc,0,tp,tp,false,false,POS_FACEUP)
-			local e1=Effect.CreateEffect(sc)
-			e1:SetType(EFFECT_TYPE_SINGLE)
-			e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-			e1:SetRange(LOCATION_MZONE)
-			e1:SetCode(EFFECT_CHANGE_LEVEL)
-			e1:SetValue(6)
-			e1:SetReset(RESET_EVENT+RESETS_STANDARD)
-			sc:RegisterEffect(e1)
-			local e2=Effect.CreateEffect(sc)
-			e2:SetType(EFFECT_TYPE_SINGLE)
-			e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-			e2:SetRange(LOCATION_MZONE)
-			e2:SetCode(EFFECT_CHANGE_ATTRIBUTE)
-			e2:SetValue(ATTRIBUTE_DARK)
-			e2:SetReset(RESET_EVENT+RESETS_STANDARD)
-			sc:RegisterEffect(e2)
-			sc=sg:GetNext()
+		if Duel.IsExistingMatchingCard(c100278033.spfilter,tp,LOCATION_DECK,0,1,nil,e,tp,tc:GetCode(),nil) then
+			if code1==nil then code1=tc:GetCode() else code2=tc:GetCode() end
 		end
-		Duel.SpecialSummonComplete()
-		tc=g:GetNext()
+		tc=tg:GetNext()
 	end
+	if code1==nil then return end
+	local sg
+	if Duel.IsPlayerAffectedByEffect(tp,59822133) or ft==1 or code2==nil then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+		sg=Duel.SelectMatchingCard(tp,c100278033.spfilter,tp,LOCATION_DECK,0,1,1,sg,e,tp,code1,code2)
+	else
+		local g=Duel.GetMatchingGroup(c100278033.spfilter,tp,LOCATION_DECK,0,nil,e,tp,code1,code2)
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+		sg=g:SelectSubGroup(tp,aux.gfcheck,2,2,false,Card.IsCode,code1,code2)
+	end
+	local sc=sg:GetFirst() 
+	while sc do
+		Duel.SpecialSummonStep(sc,0,tp,tp,false,false,POS_FACEUP)
+		local e1=Effect.CreateEffect(sc)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+		e1:SetRange(LOCATION_MZONE)
+		e1:SetCode(EFFECT_CHANGE_LEVEL)
+		e1:SetValue(6)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		sc:RegisterEffect(e1)
+		local e2=Effect.CreateEffect(sc)
+		e2:SetType(EFFECT_TYPE_SINGLE)
+		e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+		e2:SetRange(LOCATION_MZONE)
+		e2:SetCode(EFFECT_CHANGE_ATTRIBUTE)
+		e2:SetValue(ATTRIBUTE_DARK)
+		e2:SetReset(RESET_EVENT+RESETS_STANDARD)
+		sc:RegisterEffect(e2)
+		sc=sg:GetNext()
+	end
+	Duel.SpecialSummonComplete()
 end
 function c100278033.matcon(e,tp,eg,ep,ev,re,r,rp)
 	return Duel.GetTurnPlayer()~=tp
@@ -91,12 +114,11 @@ function c100278033.mattg(e,tp,eg,ep,ev,re,r,rp,chk)
 		and Duel.IsExistingMatchingCard(c100278033.cfilter,tp,LOCATION_MZONE,0,1,nil) end
 end
 function c100278033.matop(e,tp,eg,ep,ev,re,r,rp)
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SELECT)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_XMATERIAL)
 	local g1=Duel.SelectMatchingCard(tp,c100278033.matfilter,tp,LOCATION_MZONE,0,1,1,nil)
 	if g1:GetCount()>0 then
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_XMATERIAL)
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
 		local g2=Duel.SelectMatchingCard(tp,c100278033.cfilter,tp,LOCATION_MZONE,0,1,1,nil)
-		Duel.HintSelection(g2)
 		local tc=g2:GetFirst() 
 		if tc and Duel.Overlay(tc,g1)~=0 then
 			Duel.BreakEffect()
