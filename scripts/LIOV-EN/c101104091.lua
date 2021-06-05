5014463a87d173edcd1dae25a245a5436bbc80f4
Mercury233 <me@mercury233.me>
2021-06-05 13:57:37 +0800

    Update c101104091.lua

--- scripts/LIOV-EN/c101104091.lua
+++ scripts/LIOV-EN/c101104091.lua
@@ -14,7 +14,8 @@ function c101104091.initial_effect(c)
 end
 function c101104091.condition(e,tp,eg,ep,ev,re,r,rp)
 	local ph=Duel.GetCurrentPhase()
-	return (ph>PHASE_MAIN1 and ph<PHASE_MAIN2) and Duel.GetFieldGroupCount(tp,0,LOCATION_MZONE)>Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0)
+	return ph>=PHASE_BATTLE_START and ph<=PHASE_BATTLE
+		and Duel.GetFieldGroupCount(tp,0,LOCATION_MZONE)>Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0)
 end
 function c101104091.filter(c,e,tp)
 	return c:IsSetCard(0x15f) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
@@ -29,25 +30,17 @@ function c101104091.operation(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 	local g=Duel.SelectMatchingCard(tp,c101104091.filter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
 	local tc=g:GetFirst()
-	if tc and Duel.SpecialSummonStep(tc,0,tp,tp,false,false,POS_FACEUP) then
-		local sg=Duel.GetMatchingGroup(c101104091.disfilter,tp,LOCATION_MZONE,0,nil)
-		if #sg>0 and Duel.GetTurnPlayer()==1-tp and (Duel.GetCurrentPhase()>=PHASE_BATTLE_START and Duel.GetCurrentPhase()<=PHASE_BATTLE) then
-			local e1=Effect.CreateEffect(c)
-			e1:SetType(EFFECT_TYPE_FIELD)
-			e1:SetRange(LOCATION_MZONE)
-			e1:SetTargetRange(0,LOCATION_MZONE)
-			e1:SetCode(EFFECT_CANNOT_SELECT_BATTLE_TARGET)
-			e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
-			e1:SetValue(c101104091.atklimit)
-			tc:RegisterEffect(e1)
-			local fid=e:GetHandler():GetFieldID()
-			tc:RegisterFlagEffect(101104091,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,0,1,fid)
-			Duel.SpeclaiSummonComplete()
-		end
+	if tc and Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)>0
+		and Duel.GetTurnPlayer()==1-tp then
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_FIELD)
+		e1:SetRange(LOCATION_MZONE)
+		e1:SetTargetRange(0,LOCATION_MZONE)
+		e1:SetCode(EFFECT_CANNOT_SELECT_BATTLE_TARGET)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		e1:SetValue(c101104091.atklimit)
+		tc:RegisterEffect(e1)
 	end
-end
-function c101104091.disfilter(c)
-	return c:IsFaceup() and c:IsSummonType(SUMMON_TYPE_SPECIAL)
 end
 function c101104091.atklimit(e,c)
 	return c~=e:GetHandler()
