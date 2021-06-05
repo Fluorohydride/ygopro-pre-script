6e6fe0753d22e60f09086157f9308d4e4d39ca33
Mercury233 <me@mercury233.me>
2021-06-05 15:12:33 +0800

    Update c100278010.lua

--- scripts/AC01-JP/c100278010.lua
+++ scripts/AC01-JP/c100278010.lua
@@ -5,7 +5,7 @@ function c100278010.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(100278010,0))
-	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SPECIAL_SUMMON)
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SPECIAL_SUMMON+CATEGORY_GRAVE_ACTION+CATEGORY_GRAVE_SPSUMMON)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetCost(c100278010.cost)
@@ -41,13 +41,12 @@ function c100278010.thfilter(c,e,tp)
 	return c:IsAbleToHand() or (ft>0 and c:IsCanBeSpecialSummoned(e,0,tp,false,false))
 end
 function c100278010.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	local res=e:GetLabel()==1
 	if chk==0 then
+		local res=e:GetLabel()==1
 		e:SetLabel(0)
 		return res or Duel.IsExistingMatchingCard(c100278010.thfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp)
 	end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE)
-	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_GRAVE)
+	e:SetLabel(0)
 end
 function c100278010.activate(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_OPERATECARD)
@@ -55,9 +54,9 @@ function c100278010.activate(e,tp,eg,ep,ev,re,r,rp)
 	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
 	local tc=g:GetFirst()
 	if tc then
-		if tc:IsAbleToHand() and (not tc:IsCanBeSpecialSummoned(e,0,tp,false,false) or ft<=0 or Duel.SelectOption(tp,1190,1152)==0) then
+		if tc:IsAbleToHand()
+			and (not tc:IsCanBeSpecialSummoned(e,0,tp,false,false) or ft<=0 or Duel.SelectOption(tp,1190,1152)==0) then
 			Duel.SendtoHand(tc,nil,REASON_EFFECT)
-			Duel.ConfirmCards(1-tp,tc)
 		else
 			Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
 		end
@@ -65,7 +64,7 @@ function c100278010.activate(e,tp,eg,ep,ev,re,r,rp)
 end
 function c100278010.atkfilter1(c,tp)
 	return c:IsFaceup() and c:IsSetCard(0xc008)
-		and Duel.IsExistingTarget(c100278010.atkfilter1,tp,LOCATION_MZONE,0,1,c,c)
+		and Duel.IsExistingTarget(c100278010.atkfilter2,tp,LOCATION_MZONE,0,1,c,c)
 end
 function c100278010.atkfilter2(c,tc)
 	return c:IsFaceup() and c:IsSetCard(0xc008) and not c:IsAttack(tc:GetAttack())
@@ -73,11 +72,12 @@ end
 function c100278010.atktg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return false end
 	if chk==0 then return Duel.IsExistingTarget(c100278010.atkfilter1,tp,LOCATION_MZONE,0,1,nil,tp) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(100278010,2))
 	local g1=Duel.SelectTarget(tp,c100278010.atkfilter1,tp,LOCATION_MZONE,0,1,1,nil,tp)
-	e:SetLabelObject(g1:GetFirst())
+	local tc=g1:GetFirst()
+	e:SetLabelObject(tc)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
-	Duel.SelectTarget(tp,c100278010.atkfilter2,tp,LOCATION_MZONE,0,1,1,g1:GetFirst(),g1:GetFirst())
+	Duel.SelectTarget(tp,c100278010.atkfilter2,tp,LOCATION_MZONE,0,1,1,tc,tc)
 end
 function c100278010.atkop(e,tp,eg,ep,ev,re,r,rp)
 	local hc=e:GetLabelObject()
