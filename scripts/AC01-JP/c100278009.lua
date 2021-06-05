e6ed70c240e970984ec4d0f974a011dc09d613e4
Chrono-Genex <79453236+chronogenexx@users.noreply.github.com>
2021-06-05 18:03:35 +0900

    add script AC01 (#761)

---
+++ scripts/AC01-JP/c100278009.lua
@@ -0,0 +1,77 @@
+--D－フォース
+
+--Script by Chrono-Genex
+function c100278009.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH+CATEGORY_GRAVE_ACTION)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetOperation(c100278009.activate)
+	c:RegisterEffect(e1)
+	--draw skip
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e2:SetCode(EFFECT_CANNOT_DRAW)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetTargetRange(1,0)
+	e2:SetCondition(c100278009.skipcon)
+	c:RegisterEffect(e2)
+	local e3=e2:Clone()
+	e3:SetCode(EFFECT_DRAW_COUNT)
+	e3:SetValue(0)
+	c:RegisterEffect(e3)
+	--cannot be target
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_FIELD)
+	e4:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE+EFFECT_FLAG_SET_AVAILABLE)
+	e4:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
+	e4:SetRange(LOCATION_SZONE)
+	e4:SetTargetRange(LOCATION_ONFIELD,0)
+	e4:SetCondition(c100278009.condition)
+	e4:SetValue(aux.tgoval)
+	c:RegisterEffect(e4)
+	--atkup
+	local e5=Effect.CreateEffect(c)
+	e5:SetType(EFFECT_TYPE_FIELD)
+	e5:SetCode(EFFECT_UPDATE_ATTACK)
+	e5:SetRange(LOCATION_SZONE)
+	e5:SetTargetRange(LOCATION_MZONE,0)
+	e5:SetTarget(aux.TargetBoolFunction(Card.IsCode,83965310))
+	e5:SetValue(c100278009.atkval)
+	c:RegisterEffect(e5)	
+	local e6=e5:Clone()
+	e6:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+	e6:SetValue(aux.indoval)
+	c:RegisterEffect(e6)
+	local e7=e5:Clone()
+	e7:SetCode(EFFECT_EXTRA_ATTACK)
+	e7:SetValue(1)
+	c:RegisterEffect(e7)
+end
+function c100278009.thfilter(c)
+	return c:IsCode(83965310) and c:IsAbleToHand()
+end
+function c100278009.activate(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(aux.NecroValleyFilter(c100278009.thfilter),tp,LOCATION_GRAVE+LOCATION_DECK,0,nil)
+	if g:GetCount()>0 and Duel.SelectYesNo(tp,aux.Stringid(100278009,0)) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+		local sg=g:Select(tp,1,1,nil)
+		Duel.SendtoHand(sg,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,sg)
+	end
+end
+function c100278009.cfilter(c)
+	return c:IsFaceup() and c:IsCode(83965310)
+end
+function c100278009.condition(e)
+	local tp=e:GetHandlerPlayer()
+	return Duel.IsExistingMatchingCard(c100278009.cfilter,tp,LOCATION_ONFIELD,0,1,nil)
+end
+function c100278009.skipcon(e)
+	return c100278009.condition(e) and Duel.GetCurrentPhase()==PHASE_DRAW
+end
+function c100278009.atkval(e,c)
+	return Duel.GetMatchingGroupCount(Card.IsType,0,LOCATION_GRAVE,LOCATION_GRAVE,nil,TYPE_MONSTER)*100
+end
