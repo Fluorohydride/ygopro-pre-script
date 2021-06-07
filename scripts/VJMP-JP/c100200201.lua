c2d8469d30da9d5f6f17aeb5d1820fe24d6def56
JSY1728 <34511983+JSY1728@users.noreply.github.com>
2021-06-07 14:11:18 +0900

    Update c100200201.lua

--- scripts/VJMP-JP/c100200201.lua
+++ scripts/VJMP-JP/c100200201.lua
@@ -82,4 +82,12 @@ function c100200201.efop(e,tp,eg,ep,ev,re,r,rp)
 	e2:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
 	e2:SetValue(aux.indoval)
 	rc:RegisterEffect(e2,true)
+		if not rc:IsType(TYPE_EFFECT) then
+		local e3=Effect.CreateEffect(c)
+		e3:SetType(EFFECT_TYPE_SINGLE)
+		e3:SetCode(EFFECT_ADD_TYPE)
+		e3:SetValue(TYPE_EFFECT)
+		e3:SetReset(RESET_EVENT+RESETS_STANDARD)
+		rc:RegisterEffect(e3,true)
+	end
 end
