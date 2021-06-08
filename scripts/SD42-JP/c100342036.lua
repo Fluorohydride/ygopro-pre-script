fee7ab5a9803044311df2785b8bd1b3b976d18b8
Kohana <80439628+XyleN5967@users.noreply.github.com>
2021-06-08 17:04:43 +0800

    add 運命の契約

---
+++ scripts/SD42-JP/c100342036.lua
@@ -0,0 +1,83 @@
+--運命の契約
+--scripted by XyLeN
+function c100342036.initial_effect(c)
+	c:EnableCounterPermit(0x70,LOCATION_SZONE)
+	c:SetCounterLimit(0x70,1)
+	--Activate
+	local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_ACTIVATE)
+	e0:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e0)
+	--counter
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetCode(EVENT_DESTROYED)
+	e1:SetRange(LOCATION_SZONE)
+	e1:SetOperation(c100342036.ctop)
+	c:RegisterEffect(e1)
+	--special summon
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(100342036,0))
+	e2:SetCategory(CATEGORY_TOGRAVE+CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e2:SetProperty(EFFECT_FLAG_DELAY)
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetCountLimit(1,100342036)
+	e2:SetCondition(c100342036.spcon)
+	e2:SetCost(c100342036.spcost)
+	e2:SetTarget(c100342036.sptg)
+	e2:SetOperation(c100342036.spop)
+	c:RegisterEffect(e2)
+end
+function c100342036.cfilter(c,tp)
+	return c:IsPreviousControler(tp) and c:IsReason(REASON_BATTLE+REASON_EFFECT) 
+end
+function c100342036.ctop(e,tp,eg,ep,ev,re,r,rp)
+	if eg:IsExists(c100342036.cfilter,1,nil,tp) then
+		e:GetHandler():AddCounter(0x70,1)
+	end
+end
+function c100342036.cfilter2(c,tp)
+	return c:IsSummonLocation(LOCATION_EXTRA) and c:IsSummonPlayer(tp)
+end
+function c100342036.spcon(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(c100342036.cfilter2,1,nil,tp)
+end
+function c100342036.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsCanRemoveCounter(tp,0x70,1,REASON_COST) end
+	e:GetHandler():RemoveCounter(tp,0x70,1,REASON_COST)
+end
+function c100342036.tgfilter(c)
+	return (c:IsLocation(LOCATION_HAND+LOCATION_DECK) or c:IsFaceup())
+		and c:IsCode(27062594) and c:IsAbleToGrave()
+end
+function c100342036.spfilter(c,e,tp)
+	return c:IsSetCard(0x107f,0x7f) and c:IsType(TYPE_XYZ) and c:IsAttribute(ATTRIBUTE_LIGHT)
+		and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_XYZ,tp,false,false) and Duel.GetLocationCountFromEx(tp,tp,nil,c)>0
+end
+function c100342036.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c100342036.tgfilter,tp,LOCATION_HAND+LOCATION_DECK+LOCATION_ONFIELD,0,1,nil)
+		and aux.MustMaterialCheck(nil,tp,EFFECT_MUST_BE_XMATERIAL)
+		and e:GetHandler():IsCanOverlay()
+		and Duel.IsExistingMatchingCard(c100342036.spfilter,tp,LOCATION_EXTRA,0,1,nil,e,tp,e:GetHandler()) end
+	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_HAND+LOCATION_DECK+LOCATION_ONFIELD)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
+end
+function c100342036.spop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local g=Duel.SelectMatchingCard(tp,c100342036.tgfilter,tp,LOCATION_HAND+LOCATION_DECK+LOCATION_ONFIELD,0,1,1,nil)
+	if g:GetCount()>0 and Duel.SendtoGrave(g,REASON_EFFECT)~=0 then
+		if not aux.MustMaterialCheck(nil,tp,EFFECT_MUST_BE_XMATERIAL) then return end
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+		local sg=Duel.SelectMatchingCard(tp,c100342036.spfilter,tp,LOCATION_EXTRA,0,1,1,nil,e,tp)
+		local sc=sg:GetFirst()
+		if sc and Duel.SpecialSummon(sc,SUMMON_TYPE_XYZ,tp,tp,false,false,POS_FACEUP)~=0 then
+			sc:CompleteProcedure()
+			if c:IsRelateToEffect(e) then
+				Duel.Overlay(sc,Group.FromCards(c))
+			end
+		end
+	end
+end
