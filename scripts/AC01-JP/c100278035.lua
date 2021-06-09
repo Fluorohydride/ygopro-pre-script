9a74f66661cf8b8e20d59d7ef36ca3c177c2e667
TheOnePharaoh <TheOnePharaoh@users.noreply.github.com>
2021-06-10 00:28:49 +1000

    Added Speedroid scratch

---
+++ scripts/AC01-JP/c100278035.lua
@@ -0,0 +1,35 @@
+--Speedroid Scratch
+function c100278035.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCountLimit(1,100278035+EFFECT_COUNT_CODE_OATH)
+	e1:SetCost(c100278035.cost)
+	e1:SetTarget(c100278035.target)
+	e1:SetOperation(c100278035.activate)
+	c:RegisterEffect(e1)
+end
+function c100278035.costfilter(c)
+    return c:IsSetCard(0x2016) and c:IsAbleToGraveAsCost()
+end
+function c100278035.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c100278035.costfilter,tp,LOCATION_HAND,0,1,e:GetHandler()) end
+	Duel.DiscardHand(tp,c100278035.costfilter,1,1,REASON_COST)
+end
+function c100278035.filter(c)
+    return c:IsSetCard(0x2016) and c:IsType(TYPE_MONSTER) and c:IsAbleToHand()
+end
+function c100278035.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c100278035.filter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function c100278035.activate(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,c100278035.filter,tp,LOCATION_DECK,0,1,1,nil)
+	if g:GetCount()>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end
