62fe69caf9eb151a25e394c373bdef23d13a7fff
JSY1728 <34511983+JSY1728@users.noreply.github.com>
2021-06-09 17:56:01 +0900

    Added "翼の恩返し"
    c100278016.lua = "翼の恩返し"

---
+++ scripts/AC01-JP/c100278016.lua
@@ -0,0 +1,36 @@
+--翼の恩返し
+--Script By JSY1728
+function c100278016.initial_effect(c)
+	--Draw
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_DRAW)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCountLimit(1,100278016+EFFECT_COUNT_CODE_OATH)
+	e1:SetCondition(c100278016.drcon)
+	e1:SetCost(c100278016.drcost)
+	e1:SetTarget(c100278016.drtg)
+	e1:SetOperation(c100278016.drop)
+	c:RegisterEffect(e1)
+end
+function c100278016.cfilter(c)
+	return c:IsFaceup() and c:IsRace(RACE_WINDBEAST) and not c:IsOriginalCodeRule()
+end
+function c100278016.drcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsExistingMatchingCard(c100278016.cfilter,tp,LOCATION_MZONE,0,2,nil)
+end
+function c100278016.drcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.CheckLPCost(tp,600) end
+	Duel.PayLPCost(tp,600)
+end
+function c100278016.drtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsPlayerCanDraw(tp,2) end
+	Duel.SetTargetPlayer(tp)
+	Duel.SetTargetParam(2)
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,2)
+end
+function c100278016.drop(e,tp,eg,ep,ev,re,r,rp)
+	local p,d=Duel.GetChainIno(0,CHAININFO_TARGET_PLAYER+CHAININFO_TARGET_PARAM)
+	Duel.Draw(p,d,REASON_EFFECT)
+end
