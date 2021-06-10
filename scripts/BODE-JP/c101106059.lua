76f58a6110705a5781818b8f4e29d7e3666bbb25
Mercury233 <me@mercury233.me>
2021-06-10 10:42:34 +0800

    Update c101106059.lua

--- scripts/BODE-JP/c101106059.lua
+++ scripts/BODE-JP/c101106059.lua
@@ -1,10 +1,15 @@
 --ふわんだりぃずと未知の風
 --Script By JSY1728
 function c101106059.initial_effect(c)
+	--Activate
+	local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_ACTIVATE)
+	e0:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e0)
 	--Tribute Summon
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(101106059,0))
-	e1:SetTyoe(EFFECT_TYPE_FIELD)
+	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetCode(EFFECT_SUMMON_PROC)
 	e1:SetTargetRange(LOCATION_HAND,0)
 	e1:SetRange(LOCATION_SZONE)
@@ -13,36 +18,39 @@ function c101106059.initial_effect(c)
 	e1:SetOperation(c101106059.otop)
 	e1:SetValue(SUMMON_TYPE_ADVANCE)
 	c:RegisterEffect(e1)
-	local e3=e1:Clone()
-	e3:SetCode(EFFECT_SET_PROC)
-	c:RegisterEffect(e3)
-	--Send to Deck & Draw
-	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(101106059,1))
-	e2:SetCategory(CATEGORY_TODECK+CATEGORY_DRAW)
-	e2:SetType(EFFECT_TYPE_IGNITION)
-	e2:SetRange(LOCATION_SZONE)
-	e2:SetCountLimit(1,101106059)
-	e2:SetTarget(c101106059.drtg)
-	e2:SetOperation(c101106059.drop)
+	local e2=e1:Clone()
+	e2:SetCode(EFFECT_SET_PROC)
 	c:RegisterEffect(e2)
+	--Send to Deck & Draw
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(101106059,1))
+	e3:SetCategory(CATEGORY_TODECK+CATEGORY_DRAW)
+	e3:SetType(EFFECT_TYPE_IGNITION)
+	e3:SetRange(LOCATION_SZONE)
+	e3:SetCountLimit(1,101106059)
+	e3:SetTarget(c101106059.drtg)
+	e3:SetOperation(c101106059.drop)
+	c:RegisterEffect(e3)
+end
+function c101106059.otfilter(c,tp)
+	return c:IsAbleToGrave() and Duel.GetMZoneCount(tp,c)>0
 end
 function c101106059.otcon(e,c,minc)
 	if c==nil then return true end
 	local tp=c:GetControler()
-	return minc<=2 and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingMatchingCard(Card.IsAbleToGraveAsCost,tp,LOCATION_MZONE,0,1,nil)
-		and Duel.IsExistingMatchingCard(Card.IsAbleToGraveAsCost,tp,0,LOCATION_ONFIELD,1,nil)
+	return minc<=2
+		and Duel.IsExistingMatchingCard(c101106059.otfilter,tp,LOCATION_MZONE,0,1,nil,tp)
+		and Duel.IsExistingMatchingCard(Card.IsAbleToGrave,tp,0,LOCATION_ONFIELD,1,nil)
 end
 function c101106059.ottg(e,c)
 	local mi,ma=c:GetTributeRequirement()
-	return mi<=2 and ma>=2 and c:IsType(TYPE_MONSTER)
+	return mi<=2 and ma>=2
 end
 function c101106059.otop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
-	local g1=Duel.SelectMatchingCard(tp,Card.IsAbleToGraveAsCost,tp,LOCATION_MZONE,0,1,1,nil)
+	local g1=Duel.SelectMatchingCard(tp,c101106059.otfilter,tp,LOCATION_MZONE,0,1,1,nil,tp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
-	local g2=Duel.SelectMatchingCard(tp,Card.IsAbleToGraveAsCost,tp,0,LOCATION_ONFIELD,1,1,nil)
+	local g2=Duel.SelectMatchingCard(tp,Card.IsAbleToGrave,tp,0,LOCATION_ONFIELD,1,1,nil)
 	g1:Merge(g2)
 	Duel.SendtoGrave(g1,REASON_COST)
 end
@@ -59,16 +67,18 @@ function c101106059.drop(e,tp,eg,ep,ev,re,r,rp)
 	local p=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
 	local g=Duel.SelectMatchingCard(p,c101106059.drfilter,p,LOCATION_HAND,0,1,2,nil)
-	local ct=g:GetCount()
-	if ct>0 then
-		Duel.ConfirmCards(1-p,ct)
+	if g:GetCount()>0 then
+		Duel.ConfirmCards(1-p,g)
+		Duel.SendtoDeck(g,nil,0,REASON_EFFECT)
+		local og=Duel.GetOperatedGroup()
+		local ct=og:FilterCount(Card.IsLocation,nil,LOCATION_DECK)
+		if ct==0 then return end
 		Duel.SortDecktop(p,p,ct)
 		for i=1,ct do
 			local mg=Duel.GetDecktopGroup(p,1)
 			Duel.MoveSequence(mg:GetFirst(),1)
-			Duel.BreakEffect()
-			Duel.Draw(p,ct,REASON_EFFECT)
-			Duel.ShuffleHand(p)
 		end
+		Duel.BreakEffect()
+		Duel.Draw(p,ct,REASON_EFFECT)
 	end
 end
