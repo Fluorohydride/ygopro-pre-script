a5aff6525124ceabd55f42d12ffa217ea75f13e1
Mercury233 <me@mercury233.me>
2021-06-10 10:32:29 +0800

    Update c101106058.lua

--- scripts/BODE-JP/c101106058.lua
+++ scripts/BODE-JP/c101106058.lua
@@ -20,7 +20,7 @@ function c101106058.initial_effect(c)
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(101106058,1))
 	e2:SetCategory(CATEGORY_SUMMON)
-	e2:SetType(EFEECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
 	e2:SetRange(LOCATION_FZONE)
 	e2:SetCode(EVENT_SUMMON_SUCCESS)
 	e2:SetCountLimit(1,101106058+100)
@@ -29,12 +29,12 @@ function c101106058.initial_effect(c)
 	e2:SetOperation(c101106058.sumop)
 	c:RegisterEffect(e2)
 end
-function c101106058.rmcfilter(c,tp,code)
-	return c:IsLevel(1) and c:IsSetCard(0x270) and c:IsSummonable(true,nil) not c:IsPublic()
+function c101106058.rmcfilter(c,tp)
+	return c:IsLevel(1) and c:IsSetCard(0x26a) and c:IsSummonable(true,nil) and not c:IsPublic()
 		and Duel.IsExistingMatchingCard(c101106058.rmfilter,tp,LOCATION_DECK,0,1,nil,c:GetCode())
 end
 function c101106058.rmfilter(c,code)
-	return c:IsSetCard(0x270) and not c:IsCode(code) and c:IsAbleToRemove()
+	return c:IsSetCard(0x26a) and not c:IsCode(code) and c:IsAbleToRemove()
 end
 function c101106058.rmtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(c101106058.rmcfilter,tp,LOCATION_HAND,0,1,nil,tp) end
@@ -44,22 +44,21 @@ end
 function c101106058.rmop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CONFIRM)
 	local g1=Duel.SelectMatchingCard(tp,c101106058.rmcfilter,tp,LOCATION_HAND,0,1,1,nil,tp)
-	Duel.ConfirmCards(1-tp,g1)
-	if g1:GetCount()==0 then return end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
-	local g2=Duel.SelectMatchingCard(tp,c101106058.rmfilter,tp,LOCATION_DECK,0,1,1,nil,g1:GetFirst():GetCode())
-	if g2:GetCount()==0 then return end
-	Duel.BreakEffect()
-	if Duel.Remove(g2,nil,REASON_EFFECT)==0 then return end
-	Duel.ConfirmCards(1-tp,g2)
-	Duel.BreakEffect()
-	Duel.Summon(tp,g1,true,nil)
+	if g1:GetCount()>0 then
+		Duel.ConfirmCards(1-tp,g1)
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+		local g2=Duel.SelectMatchingCard(tp,c101106058.rmfilter,tp,LOCATION_DECK,0,1,1,nil,g1:GetFirst():GetCode())
+		if Duel.Remove(g2,POS_FACEUP,REASON_EFFECT)>0 then
+			Duel.BreakEffect()
+			Duel.Summon(tp,g1:GetFirst(),true,nil)
+		end
+	end
 end
 function c101106058.sumcon(e,tp,eg,ep,ev,re,r,rp)
 	return ep~=tp
 end
 function c101106058.sumfilter(c)
-	return c:IsSetCard(0x270) and c:IsSummonable(true,nil)
+	return c:IsSetCard(0x26a) and c:IsSummonable(true,nil)
 end
 function c101106058.sumtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(c101106058.sumfilter,tp,LOCATION_HAND+LOCATION_MZONE,0,1,nil) end
