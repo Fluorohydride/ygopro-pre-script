8aab444114f7e0cab81624a541484efd7f8bf1cc
Mercury233 <me@mercury233.me>
2021-06-12 09:56:09 +0800

    Update c101106062.lua

--- scripts/BODE-JP/c101106062.lua
+++ scripts/BODE-JP/c101106062.lua
@@ -16,8 +16,7 @@ function c101106062.filter(c)
 end
 function c101106062.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and c101106062.filter(chkc) end
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingTarget(c101106062.filter,tp,LOCATION_GRAVE,0,1,nil) end
+	if chk==0 then return Duel.IsExistingTarget(c101106062.filter,tp,LOCATION_GRAVE,0,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
 	local g=Duel.SelectTarget(tp,c101106062.filter,tp,LOCATION_GRAVE,0,1,1,nil)
 	Duel.SetOperationInfo(0,CATEGORY_TODECK,g,1,0,0)
@@ -28,8 +27,8 @@ function c101106062.activate(e,tp,eg,ep,ev,re,r,rp)
 	if tc:IsRelateToEffect(e) and Duel.SendtoDeck(tc,nil,0,REASON_EFFECT)~=0 then
 		local ct=Duel.GetCurrentChain()
 		if ct<2 then return end
-		local p=Duel.GetChainInfo(ct-1,CHAININFO_TRIGGERING_EFFECT)
-		if p==tp and rp==1-tp then
+		local te,tep=Duel.GetChainInfo(ct-1,CHAININFO_TRIGGERING_EFFECT,CHAININFO_TRIGGERING_PLAYER)
+		if tep==1-tp and e:IsHasType(EFFECT_TYPE_ACTIVATE) then
 			Duel.BreakEffect()
 			local e1=Effect.CreateEffect(c)
 			e1:SetType(EFFECT_TYPE_FIELD)
@@ -37,7 +36,7 @@ function c101106062.activate(e,tp,eg,ep,ev,re,r,rp)
 			e1:SetTargetRange(LOCATION_MZONE,0)
 			e1:SetTarget(c101106062.etarget)
 			e1:SetValue(c101106062.efilter)
-			e1:SetLabelObject(p)
+			e1:SetLabelObject(te)
 			e1:SetReset(RESET_EVENT+RESET_CHAIN)
 			Duel.RegisterEffect(e1,tp)
 		end
