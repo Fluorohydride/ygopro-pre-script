297f6b40625e8ad018f842e597534f56fdb5534c
Mercury233 <me@mercury233.me>
2021-06-12 11:20:56 +0800

    Update c101106048.lua

--- scripts/BODE-JP/c101106048.lua
+++ scripts/BODE-JP/c101106048.lua
@@ -1,4 +1,4 @@
---超弩級軍貴一うに型二番艦
+--超弩級軍貫－うに型二番艦
 --scripted by XyLeN
 function c101106048.initial_effect(c)
 	--xyz summon
@@ -7,7 +7,6 @@ function c101106048.initial_effect(c)
 	--apply the effect
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(101106048,0))
-	e1:SetCategory(CATEGORY_DRAW)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
 	e1:SetCountLimit(1,101106048)
@@ -17,7 +16,7 @@ function c101106048.initial_effect(c)
 	c:RegisterEffect(e1)
 	--disable
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(101106048,0))
+	e2:SetDescription(aux.Stringid(101106048,1))
 	e2:SetCategory(CATEGORY_DISABLE)
 	e2:SetType(EFFECT_TYPE_QUICK_O)
 	e2:SetCode(EVENT_FREE_CHAIN)
@@ -39,7 +38,10 @@ function c101106048.efftg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local chk2=c:GetMaterial():FilterCount(Card.IsCode,nil,101106022)>0
 	if chk==0 then return (chk1 and Duel.IsPlayerCanDraw(tp,1) or chk2) end
 	if chk1 then
+		e:SetCategory(CATEGORY_DRAW)
 		Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
+	else
+		e:SetCategory(0)
 	end
 end
 function c101106048.effop(e,tp,eg,ep,ev,re,r,rp)
@@ -49,7 +51,8 @@ function c101106048.effop(e,tp,eg,ep,ev,re,r,rp)
 	if chk1 then
 		Duel.Draw(tp,1,REASON_EFFECT)
 	end
-	if chk2 then
+	if chk2 and c:IsRelateToEffect(e) then
+		Duel.BreakEffect()
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_DIRECT_ATTACK)
@@ -63,17 +66,14 @@ function c101106048.discon(e,tp,eg,ep,ev,re,r,rp)
 	return (turn==tp and (ph==PHASE_MAIN1 or ph==PHASE_MAIN2) or turn==1-tp and (ph>=PHASE_BATTLE_START and ph<=PHASE_BATTLE))
 end
 function c101106048.ctfilter(c)
-	return c:IsSummonLocation(LOCATION_EXTRA) and c:IsSetCard(0x166)
-end
-function c101106048.disfilter(c)
-	return c:IsFaceup() and not c:IsDisabled()
+	return c:IsSummonLocation(LOCATION_EXTRA) and c:IsFaceup() and c:IsSetCard(0x166)
 end
 function c101106048.distg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	local ct=Duel.GetMatchingGroupCount(c101106048.ctfilter,tp,LOCATION_MZONE,0,nil)
-	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) and c101106048.disfilter(chkc) end
-	if chk==0 then return ct>0 and Duel.IsExistingTarget(c101106048.disfilter,tp,0,LOCATION_MZONE,1,nil) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
-	local g=Duel.SelectTarget(tp,c101106048.disfilter,tp,0,LOCATION_MZONE,1,ct,nil)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) and aux.disfilter1(chkc) end
+	if chk==0 then return ct>0 and Duel.IsExistingTarget(aux.disfilter1,tp,0,LOCATION_MZONE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DISABLE)
+	local g=Duel.SelectTarget(tp,aux.disfilter1,tp,0,LOCATION_MZONE,1,ct,nil)
 	Duel.SetOperationInfo(0,CATEGORY_DISABLE,g,g:GetCount(),0,0)
 end
 function c101106048.disop(e,tp,eg,ep,ev,re,r,rp)
