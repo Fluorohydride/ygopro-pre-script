3af4d198a3583ac59869e878804dfcd8c43e4d16
Kohana <80439628+XyleN5967@users.noreply.github.com>
2021-06-14 13:44:59 +0800

    add マジェスティ・ヒュペリオン

---
+++ scripts/SR12-JP/c100312001.lua
@@ -0,0 +1,90 @@
+--マジェスティ・ヒュペリオン
+--Majesty Hyperion
+--scripted by XyLeN
+function c100312001.initial_effect(c)
+	aux.AddCodeList(c,56433456)
+	--spsummon proc
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_SPSUMMON_PROC)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e1:SetRange(LOCATION_HAND+LOCATION_GRAVE)
+	e1:SetCountLimit(1,100312001+EFFECT_COUNT_CODE_OATH)
+	e1:SetCondition(c100312001.hspcon)
+	e1:SetOperation(c100312001.hspop)
+	c:RegisterEffect(e1)
+	--reflect damage
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_REFLECT_BATTLE_DAMAGE)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetTargetRange(LOCATION_MZONE,0)
+	e2:SetTarget(aux.TargetBoolFunction(Card.IsRace,RACE_FAIRY))
+	e2:SetValue(1)
+	c:RegisterEffect(e2)
+	--banish
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(100312001,0))
+	e3:SetCategory(CATEGORY_REMOVE)
+	e3:SetType(EFFECT_TYPE_IGNITION)
+	e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCondition(c100312001.rmcon)
+	e3:SetCost(c100312001.rmcost)
+	e3:SetTarget(c100312001.rmtg)
+	e3:SetOperation(c100312001.rmop)
+	c:RegisterEffect(e3)
+end
+function c100312001.spfilter(c)
+	return c:IsSetCard(0x44) and c:IsAbleToRemoveAsCost() and (not c:IsLocation(LOCATION_MZONE) or c:IsFaceup())
+end
+function c100312001.hspcon(e,c)
+	if c==nil then return true end
+	local tp=c:GetControler()
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	if ft<=-1 then return false end
+	if ft<=0 then
+		return Duel.IsExistingMatchingCard(c100312001.spfilter,tp,LOCATION_MZONE,0,1,nil)
+	else return Duel.IsExistingMatchingCard(c100312001.spfilter,tp,0x16,0,1,nil) end
+end
+function c100312001.hspop(e,tp,eg,ep,ev,re,r,rp,c)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then
+		local g=Duel.SelectMatchingCard(tp,c100312001.spfilter,tp,LOCATION_MZONE,0,1,1,nil)
+		Duel.Remove(g,POS_FACEUP,REASON_COST)
+	else
+		local g=Duel.SelectMatchingCard(tp,c100312001.spfilter,tp,0x16,0,1,1,nil)
+		Duel.Remove(g,POS_FACEUP,REASON_COST)
+	end
+end
+function c100312001.cfilter(c)
+	return c:IsFaceup() and c:IsCode(56433456)
+end
+function c100312001.rmcon(e,tp,eg,ep,ev,re,r,rp)
+	local check=Duel.IsExistingMatchingCard(c100312001.cfilter,tp,LOCATION_ONFIELD+LOCATION_GRAVE,0,1,nil)
+	if check then return e:GetHandler():GetFlagEffect(100312001)<2
+	else return e:GetHandler():GetFlagEffect(100312001)<1 end
+end
+function c100312001.costfilter(c)
+	return c:IsRace(RACE_FAIRY) and c:IsAbleToRemoveAsCost()
+end
+function c100312001.rmcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c100312001.costfilter,tp,LOCATION_HAND+LOCATION_GRAVE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local g=Duel.SelectMatchingCard(tp,c100312001.costfilter,tp,LOCATION_HAND+LOCATION_GRAVE,0,1,1,nil)
+	Duel.Remove(g,POS_FACEUP,REASON_COST)
+end
+function c100312001.rmtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsAbleToRemove() end
+	if chk==0 then return Duel.IsExistingTarget(Card.IsAbleToRemove,tp,LOCATION_GRAVE,LOCATION_GRAVE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local g=Duel.SelectTarget(tp,Card.IsAbleToRemove,tp,LOCATION_GRAVE,LOCATION_GRAVE,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_REMOVE,g,1,0,0)
+	e:GetHandler():RegisterFlagEffect(100312001,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,0,1)
+end
+function c100312001.rmop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) then
+		Duel.Remove(tc,POS_FACEUP,REASON_EFFECT)
+	end
+end
