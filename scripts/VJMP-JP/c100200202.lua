2d9ede589db857c717b68bcf25b9bdd21ea659d1
Mercury233 <me@mercury233.me>
2021-06-17 20:32:45 +0800

    Update c100200202.lua

--- scripts/VJMP-JP/c100200202.lua
+++ scripts/VJMP-JP/c100200202.lua
@@ -8,6 +8,7 @@ function c100200202.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e1:SetCode(EVENT_SUMMON_SUCCESS)
 	e1:SetRange(LOCATION_MZONE)
+	e1:SetTarget(c100200202.atktg)
 	e1:SetOperation(c100200202.atkop)
 	c:RegisterEffect(e1)
 	--DEF DOWN
@@ -15,26 +16,28 @@ function c100200202.initial_effect(c)
 	e2:SetDescription(aux.Stringid(100200202,1))
 	e2:SetCategory(CATEGORY_DEFCHANGE)
 	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e2:SetRange(LOCATION_MZONE)
 	e2:SetCountLimit(1,100200202)
 	e2:SetTarget(c100200202.destg)
 	e2:SetOperation(c100200202.desop)
 	c:RegisterEffect(e2)
 end
+function c100200202.atktg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsType,tp,LOCATION_GRAVE,0,1,nil,TYPE_MONSTER) end
+end
 function c100200202.atkop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_SINGLE)
-	e1:SetCode(EFFECT_UPDATE_ATTACK)
-	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-	e1:SetRange(LOCATION_MZONE)
-	e1:SetValue(c100200202.atkval)
-	e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
-	c:RegisterEffect(e1)
-end
-function c100200202.atkval(e,c)
-	local g=Duel.GetMatchingGroup(Card.IsType,e:GetHandlerPlayer(),LOCATION_GRAVE,0,nil,TYPE_MONSTER)
-	return g:GetClassCount(Card.GetRace)*100
+	local g=Duel.GetMatchingGroup(Card.IsType,tp,LOCATION_GRAVE,0,nil,TYPE_MONSTER)
+	local val=g:GetClassCount(Card.GetRace)*100
+	if c:IsFaceup() and c:IsRelateToEffect(e) and val>0 then
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetValue(val)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_DISABLE+RESET_PHASE+PHASE_END)
+		c:RegisterEffect(e1)
+	end
 end
 function c100200202.desfilter(c)
 	return c:IsFaceup() and c:GetDefense()>0
@@ -42,15 +45,15 @@ end
 function c100200202.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	local c=e:GetHandler()
 	if chkc then return chkc:IsLocation(LOCATION_MZONE) and c100200202.desfilter(chkc) and chkc~=c end
-	if chk==0 then return e:GetHandler():GetAttack()>0 and Duel.IsExistingTarget(c100200202.desfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,c) end
+	if chk==0 then return c:GetAttack()>0 and Duel.IsExistingTarget(c100200202.desfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,c) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
 	local g=Duel.SelectTarget(tp,c100200202.desfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,c)
 	Duel.SetOperationInfo(0,CATEGORY_DEFCHANGE,g,1,0,0)
 end
 function c100200202.desop(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS):Filter(Card.IsRelateToEffect,nil,e)
-	local dg=Group.CreateGroup()
 	local c=e:GetHandler()
-	if c:IsRelateToEffect(e) then
+	local tc=Duel.GetFirstTarget()
+	if c:IsFaceup() and c:IsRelateToEffect(e) then
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
@@ -58,8 +61,7 @@ function c100200202.desop(e,tp,eg,ep,ev,re,r,rp)
 		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
 		c:RegisterEffect(e1)
 	end
-	local tc=g:GetFirst()
-	while tc do
+	if tc:IsFaceup() and tc:IsRelateToEffect(e) then
 		local atk=c:GetAttack()
 		local e2=Effect.CreateEffect(c)
 		e2:SetType(EFFECT_TYPE_SINGLE)
@@ -67,8 +69,9 @@ function c100200202.desop(e,tp,eg,ep,ev,re,r,rp)
 		e2:SetValue(-atk)
 		e2:SetReset(RESET_EVENT+RESETS_STANDARD)
 		tc:RegisterEffect(e2)
-		if tc:IsDefense(0) then dg:AddCard(tc) end
-		tc=g:GetNext()
+		if tc:IsDefense(0) then
+			Duel.BreakEffect()
+			Duel.Destroy(tc,REASON_EFFECT)
+		end
 	end
-	Duel.Destroy(dg,REASON_EFFECT)
 end
