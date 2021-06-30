f6873b2c99bddd5d790322e52f56fa93f20f159b
Mercury233 <me@mercury233.me>
2021-06-30 20:50:56 +0800

    Update c100280010.lua

--- scripts/KICO-EN/c100280010.lua
+++ scripts/KICO-EN/c100280010.lua
@@ -1,4 +1,5 @@
---XYZ超加农炮
+--XYZ Hyper Cannon
+--script by 222DIY
 function c100280010.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
@@ -13,21 +14,21 @@ function c100280010.initial_effect(c)
 	e2:SetRange(LOCATION_SZONE)
 	e2:SetCode(EVENT_FREE_CHAIN)
 	e2:SetCountLimit(1,100280010)
-	e2:SetCondition(c100280010.con)
-	e2:SetTarget(c100280010.tg)
-	e2:SetOperation(c100280010.op)
+	e2:SetCondition(c100280010.condition)
+	e2:SetTarget(c100280010.target)
+	e2:SetOperation(c100280010.operation)
 	c:RegisterEffect(e2)
 end
 function c100280010.cfilter(c)
 	return c:IsFaceup() and (c:IsCode(91998119) or aux.IsMaterialListCode(c,91998119))
 end
-function c100280010.con(e,tp,eg,ep,ev,re,r,rp)
+function c100280010.condition(e,tp,eg,ep,ev,re,r,rp)
 	return Duel.IsExistingMatchingCard(c100280010.cfilter,tp,LOCATION_MZONE,0,1,nil)
 end
 function c100280010.tdfilter(c)
 	return c:IsFaceup() and c:IsType(TYPE_UNION) and c:IsAbleToDeck()
 end
-function c100280010.tg(e,tp,eg,ep,ev,re,r,rp,chk)
+function c100280010.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if Duel.GetTurnPlayer()==tp then
 		if chkc then return chkc:IsLocation(LOCATION_REMOVED) and chkc:IsControler(tp) and c100280010.tdfilter(chkc) end
 		if chk==0 then return Duel.IsExistingTarget(c100280010.tdfilter,tp,LOCATION_REMOVED,0,1,nil,e,tp) 
@@ -39,7 +40,8 @@ function c100280010.tg(e,tp,eg,ep,ev,re,r,rp,chk)
 		Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
 	else
 		if chkc then return chkc:IsOnField() and chkc:IsControler(1-tp) end
-		if chk==0 then return Duel.IsExistingTarget(nil,tp,0,LOCATION_ONFIELD,1,nil) and Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,1,nil) end
+		if chk==0 then return Duel.IsExistingTarget(nil,tp,0,LOCATION_ONFIELD,1,nil)
+			and Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,1,nil) end
 		e:SetCategory(CATEGORY_DESTROY)
 		local rt=Duel.GetTargetCount(nil,tp,0,LOCATION_ONFIELD,nil)
 		local ct=Duel.DiscardHand(tp,Card.IsDiscardable,1,rt,REASON_COST+REASON_DISCARD,nil)
@@ -48,7 +50,7 @@ function c100280010.tg(e,tp,eg,ep,ev,re,r,rp,chk)
 		Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,ct,0,0)
 	end
 end
-function c100280010.op(e,tp,eg,ep,ev,re,r,rp)
+function c100280010.operation(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetTurnPlayer()==tp then
 		local tc=Duel.GetFirstTarget()
 		if tc and tc:IsRelateToEffect(e) and Duel.SendtoDeck(tc,nil,1,REASON_EFFECT)>0 and tc:IsLocation(LOCATION_DECK) then
@@ -62,18 +64,3 @@ function c100280010.op(e,tp,eg,ep,ev,re,r,rp)
 		end
 	end
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
