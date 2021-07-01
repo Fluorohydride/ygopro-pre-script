bbeb9cae6234a36bd6786d98e4177fa23eaead49
XyleN5967 <80439628+XyleN5967@users.noreply.github.com>
2021-07-01 09:54:26 +0800

    add 擬態する人喰い虫 (untested)

---
+++ scripts/BODE-JP/c101106029.lua
@@ -0,0 +1,59 @@
+--擬態する人喰い虫
+--Mimetizing Man-Eater Bug
+--scripted by XyLeN
+function c101106029.initial_effect(c)
+	--flip
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(101106029,0))
+	e1:SetCategory(CATEGORY_DESTROY+CATEGORY_ATKCHANGE)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_FLIP+EFFECT_TYPE_TRIGGER_O)
+	e1:SetProperty(EFFECT_FLAG_DELAY)
+	e1:SetTarget(c101106029.destg)
+	e1:SetOperation(c101106029.desop)
+	c:RegisterEffect(e1)
+	--indes
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetValue(1)
+	c:RegisterEffect(e2)
+	local e3=e2:Clone() 
+	e3:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+	e3:SetValue(c101106029.efilter)
+	c:RegisterEffect(e3)
+end
+function c101106029.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) end
+	if chk==0 then return Duel.IsExistingTarget(aux.TRUE,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local g=Duel.SelectTarget(tp,aux.TRUE,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
+end
+function c101106029.desop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) and Duel.Destroy(tc,REASON_EFFECT)~=0 and c:IsRelateToEffect(e) and c:IsFaceup() then
+		local atk=tc:GetBaseAttack()
+		local race=tc:GetOriginalRace()
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetValue(atk)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_DISABLE)
+		c:RegisterEffect(e1)
+		if not c:IsRace(race) and Duel.SelectYesNo(tp,aux.Stringid(101106029,1)) then
+			Duel.BreakEffect()
+			local e2=e1:Clone()
+			e2:SetCode(EFFECT_CHANGE_RACE)
+			e2:SetProperty(EFFECT_FLAG_COPY_INHERIT)
+			e2:SetValue(race)
+			c:RegisterEffect(e2)
+		end
+	end
+end
+function c101106029.efilter(e,re,rp)
+	local rc=re:GetHandler()
+	return rc and e:GetHandler():IsRace(rc)
+end
