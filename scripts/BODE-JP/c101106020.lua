33787be8947e4e547636a7e7abe7af73289c94a0
Mercury233 <me@mercury233.me>
2021-07-07 09:48:22 +0800

    Update c101106020.lua

--- scripts/BODE-JP/c101106020.lua
+++ scripts/BODE-JP/c101106020.lua
@@ -21,7 +21,7 @@ function c101106020.initial_effect(c)
 	c:RegisterEffect(e2)
 end
 function c101106020.cfilter(c) 
-	return c:IsFaceup() and c:IsRace(RACE_REPTILE) 
+	return c:IsFaceup() and c:IsRace(RACE_REPTILE) and c:IsAttribute(ATTRIBUTE_DARK)
 end
 function c101106020.spcon(e,tp,eg,ep,ev,re,r,rp)
 	return Duel.IsExistingMatchingCard(c101106020.cfilter,tp,LOCATION_MZONE,0,1,nil)
@@ -36,18 +36,17 @@ function c101106020.cgfilter(c)
 	return c:IsFaceup() and c:IsAttack(0)
 end
 function c101106020.spfilter(c,e,tp)
-	return c:IsSetCard(SET_REPTILIANNE) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+	return c:IsSetCard(0x3c) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function c101106020.spop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if c:IsRelateToEffect(e) and Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)~=0 then
 		local cg=Duel.GetMatchingGroup(c101106020.cgfilter,tp,0,LOCATION_MZONE,nil)
 		local ct=math.min(#cg,(Duel.GetLocationCount(tp,LOCATION_MZONE)))
-		if Duel.IsPlayerAffectedByEffect(tp,59822133) then ct=1 end
+		if ct>1 and Duel.IsPlayerAffectedByEffect(tp,59822133) then ct=1 end
 		local g=Duel.GetMatchingGroup(c101106020.spfilter,tp,LOCATION_HAND,0,nil,e,tp)
 		if g:GetCount()>0 and ct>0 and Duel.SelectYesNo(tp,aux.Stringid(101106020,1)) then
 			Duel.BreakEffect()
-			if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
 			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 			local sg=g:Select(tp,1,ct,nil)
 			Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP)
