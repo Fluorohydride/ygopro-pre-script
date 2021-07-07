0d6600436122077117dee811bb3c4e5e46c8d01d
mercury233 <me@mercury233.me>
2021-07-07 16:19:38 +0800

    add scripts BODE by 222DIY

---
+++ scripts/BODE-JP/c101106011.lua
@@ -0,0 +1,66 @@
+--デスピアの凶劇
+function c101106011.initial_effect(c)
+	--atk change
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(101106011,0))
+	e1:SetCategory(CATEGORY_ATKCHANGE)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1,101106011)
+	e1:SetTarget(c101106011.atktg)
+	e1:SetOperation(c101106011.atkop)
+	c:RegisterEffect(e1)
+	--special summon itself
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(101106011,1))
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DELAY)
+	e2:SetCode(EVENT_BE_MATERIAL)
+	e2:SetCountLimit(1,101106011+100)
+	e2:SetCondition(c101106011.spcon)
+	e2:SetTarget(c101106011.sptg)
+	e2:SetOperation(c101106011.spop)
+	c:RegisterEffect(e2)
+end
+function c101106011.atktg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsFaceup,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
+end
+function c101106011.atkop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(Card.IsFaceup,tp,LOCATION_MZONE,LOCATION_MZONE,nil)
+	local tc=g:GetFirst()
+	for tc in aux.Next(g) do
+		local lv=tc:GetLevel()
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetValue(lv*100)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_DISABLE+RESET_PHASE+PHASE_END,2)
+		tc:RegisterEffect(e1)
+	end
+end
+function c101106011.spcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return c:IsPreviousLocation(LOCATION_HAND+LOCATION_ONFIELD)
+		and c:IsLocation(LOCATION_GRAVE+LOCATION_REMOVED) and r==REASON_FUSION
+end
+function c101106011.spfilter(c,e,tp)
+	return (c:IsFaceup() or c:IsLocation(LOCATION_GRAVE)) and ((not c:IsCode(101106011) and c:IsSetCard(0x164) and c:IsType(TYPE_MONSTER)) or (c:IsLevelAbove(8) and c:IsType(TYPE_FUSION))) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function c101106011.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_GRAVE+LOCATION_REMOVED) and c101106011.spfilter(chkc,e,tp) end
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingTarget(c101106011.spfilter,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,1,nil,e,tp) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectTarget(tp,c101106011.spfilter,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,1,1,nil,e,tp)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
+end
+function c101106011.spop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) then
+		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
+
+
+
