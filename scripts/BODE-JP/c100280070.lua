37b22c52885778ff8d082ddded1db1891f8ee19c
XyleN5967 <80439628+XyleN5967@users.noreply.github.com>
2021-07-10 10:04:52 +0800

    remove the hardcode effect

--- scripts/BODE-JP/c100280070.lua
+++ scripts/BODE-JP/c100280070.lua
@@ -50,12 +50,15 @@ function c100280070.ddcon(e,tp,eg,ep,ev,re,r,rp)
 end
 function c100280070.ddop(e,tp,eg,ep,ev,re,r,rp)
 	local rc=eg:GetFirst()
-	if rc then
-		--double damage
-		local e1=Effect.CreateEffect(e:GetHandler())
-		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetCode(100280070)
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_DAMAGE)
-		rc:RegisterEffect(e1)
-	end
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_CHANGE_DAMAGE)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetTargetRange(0,1)
+	e1:SetValue(c100280070.damval)
+	e1:SetReset(RESET_CHAIN)
+	Duel.RegisterEffect(e1,tp)
+end
+function c100280070.damval(e,re,val,r,rp,rc)
+	return val*2
 end
