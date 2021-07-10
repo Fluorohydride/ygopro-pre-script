558792ca9f9926df5d95759fb15639627fe47e9b
mercury233 <me@mercury233.me>
2021-07-10 14:48:12 +0800

    fix

---
+++ scripts/BODE-JP/oldcards/076/c75041269.lua
@@ -0,0 +1,82 @@
+--幽獄の時計塔
+function c75041269.initial_effect(c)
+	aux.AddCodeList(c,76263644)
+	c:EnableCounterPermit(0x1b)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e1)
+	--add counter
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(75041269,0))
+	e2:SetCategory(CATEGORY_COUNTER)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e2:SetRange(LOCATION_FZONE)
+	e2:SetCountLimit(1)
+	e2:SetCode(EVENT_PHASE+PHASE_STANDBY)
+	e2:SetCondition(c75041269.ctcon)
+	e2:SetOperation(c75041269.ctop)
+	c:RegisterEffect(e2)
+	--avoid battle damage
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetCode(EFFECT_AVOID_BATTLE_DAMAGE)
+	e3:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e3:SetRange(LOCATION_FZONE)
+	e3:SetTargetRange(1,0)
+	e3:SetCondition(c75041269.dcon)
+	c:RegisterEffect(e3)
+	--spsummon
+	local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e0:SetCode(EVENT_LEAVE_FIELD_P)
+	e0:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e0:SetOperation(c75041269.regop)
+	c:RegisterEffect(e0)
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(75041269,1))
+	e4:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e4:SetCode(EVENT_TO_GRAVE)
+	e4:SetCondition(c75041269.spcon)
+	e4:SetTarget(c75041269.sptg)
+	e4:SetOperation(c75041269.spop)
+	e4:SetLabelObject(e0)
+	c:RegisterEffect(e4)
+end
+function c75041269.dcon(e)
+	return e:GetHandler():GetCounter(0x1b)>=4
+end
+function c75041269.ctcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetTurnPlayer()~=tp
+end
+function c75041269.ctop(e,tp,eg,ep,ev,re,r,rp)
+	e:GetHandler():AddCounter(0x1b,1)
+end
+function c75041269.regop(e,tp,eg,ep,ev,re,r,rp)
+	if e:GetHandler():GetCounter(0x1b)>=4 then
+		e:SetLabel(1)
+	else
+		e:SetLabel(0)
+	end
+end
+function c75041269.spcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return c:IsReason(REASON_DESTROY) and e:GetLabelObject():GetLabel()==1
+end
+function c75041269.spfilter(c,e,tp)
+	return c:IsCode(40591390) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function c75041269.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND+LOCATION_DECK)
+end
+function c75041269.spop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,c75041269.spfilter,tp,LOCATION_HAND+LOCATION_DECK,0,1,1,nil,e,tp)
+	if g:GetCount()~=0 then
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
