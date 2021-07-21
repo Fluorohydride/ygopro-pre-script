af920434a418d32aa8fff1bb04052a2ee65c15d8
mercury233 <me@mercury233.me>
2021-07-21 11:12:06 +0800

    fix ふわんだりぃずと未知の風

--- scripts/BODE-JP/c101106059.lua
+++ scripts/BODE-JP/c101106059.lua
@@ -35,12 +35,15 @@ end
 function c101106059.otfilter(c,tp)
 	return c:IsAbleToGrave() and Duel.GetMZoneCount(tp,c)>0
 end
+function c101106059.otfilter2(c,e)
+	return c:IsAbleToGrave() and not c:IsImmuneToEffect(e)
+end
 function c101106059.otcon(e,c,minc)
 	if c==nil then return true end
 	local tp=c:GetControler()
 	return minc<=2
 		and Duel.IsExistingMatchingCard(c101106059.otfilter,tp,LOCATION_MZONE,0,1,nil,tp)
-		and Duel.IsExistingMatchingCard(Card.IsAbleToGrave,tp,0,LOCATION_ONFIELD,1,nil)
+		and Duel.IsExistingMatchingCard(c101106059.otfilter2,tp,0,LOCATION_ONFIELD,1,nil,e)
 end
 function c101106059.ottg(e,c)
 	local mi,ma=c:GetTributeRequirement()
@@ -50,7 +53,7 @@ function c101106059.otop(e,tp,eg,ep,ev,re,r,rp,c)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
 	local g1=Duel.SelectMatchingCard(tp,c101106059.otfilter,tp,LOCATION_MZONE,0,1,1,nil,tp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
-	local g2=Duel.SelectMatchingCard(tp,Card.IsAbleToGrave,tp,0,LOCATION_ONFIELD,1,1,nil)
+	local g2=Duel.SelectMatchingCard(tp,c101106059.otfilter2,tp,0,LOCATION_ONFIELD,1,1,nil,e)
 	g1:Merge(g2)
 	Duel.SendtoGrave(g1,REASON_COST)
 	c:SetMaterial(nil)
