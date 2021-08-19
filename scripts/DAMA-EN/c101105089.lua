d387b36c37cf6bde4cdbfdce8e9e512f62339f78
Mercury233 <me@mercury233.me>
2021-08-19 10:37:25 +0800

    Update c101105089.lua

--- scripts/DAMA-EN/c101105089.lua
+++ scripts/DAMA-EN/c101105089.lua
@@ -1,6 +1,6 @@
 --Giant Beetrooper Invincible Atlas
 --
---scripted by RayeHikawa
+--scripted by RayeHikawa & mercury233
 function c101105089.initial_effect(c)
 	--link summon
 	c:EnableReviveLimit()
@@ -27,83 +27,81 @@ function c101105089.initial_effect(c)
 	e3:SetTargetRange(1,0)
 	e3:SetTarget(c101105089.splimit)
 	c:RegisterEffect(e3)
-	--choose effect
+	--special summon
 	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(101105089,0))
+	e4:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e4:SetType(EFFECT_TYPE_IGNITION)
 	e4:SetRange(LOCATION_MZONE)
 	e4:SetCountLimit(1,101105089)
-	e4:SetCost(c101105089.effcost)
-	e4:SetTarget(c101105089.efftg)
-	e4:SetOperation(c101105089.effop)
+	e4:SetCost(c101105089.spcost)
+	e4:SetTarget(c101105089.sptg)
+	e4:SetOperation(c101105089.spop)
 	c:RegisterEffect(e4)
+	--atk up
+	local e5=Effect.CreateEffect(c)
+	e5:SetDescription(aux.Stringid(101105089,1))
+	e5:SetCategory(CATEGORY_ATKCHANGE)
+	e5:SetType(EFFECT_TYPE_IGNITION)
+	e5:SetRange(LOCATION_MZONE)
+	e5:SetCountLimit(1,101105089)
+	e5:SetCost(c101105089.atkcost)
+	e5:SetTarget(c101105089.atktg)
+	e5:SetOperation(c101105089.atkop)
+	c:RegisterEffect(e5)
 end
---cannot target & indes
 function c101105089.condition(e)
 	local c=e:GetHandler()
 	return c:IsSummonType(SUMMON_TYPE_LINK) and c:IsAttackBelow(3000)
 end
---splimit
 function c101105089.splimit(e,c,sump,sumtype,sumpos,targetp,se)
 	return not c:IsRace(RACE_INSECT)
 end
---choose effect
-function c101105089.costfilter(c)
-	return c:IsRace(RACE_INSECT)
+function c101105089.costfilter(c,tp)
+	return c:IsRace(RACE_INSECT) and (c:IsControler(tp) or c:IsFaceup())
 end
-function c101105089.effcost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.CheckReleaseGroup(tp,c101105089.costfilter,1,nil) end
-	local g=Duel.SelectReleaseGroup(tp,c101105089.costfilter,1,1,nil)
+function c101105089.spcostfilter(c,tp)
+	return c101105089.costfilter(c,tp) and Duel.GetMZoneCount(tp,c)>0
+end
+function c101105089.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.CheckReleaseGroup(tp,c101105089.spcostfilter,1,nil,tp) end
+	local g=Duel.SelectReleaseGroup(tp,c101105089.spcostfilter,1,1,nil,tp)
 	Duel.Release(g,REASON_COST)
 end
 function c101105089.spfilter(c,e,tp)
-	return c:IsSetCard(SET_BEETROOPER) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+	return c:IsSetCard(0x270) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
-function c101105089.efftg(e,tp,eg,ep,ev,re,r,rp,chk)
-	local b1=Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.IsExistingMatchingCard(c101105089.spfilter,tp,LOCATION_DECK,0,1,nil,e,tp) end
-	local b2=e:GetHandler():GetFlagEffect(101105089)==0 end
-	if chk==0 then return b1 or b2 end
-	local off=1
-	local ops,opval={},{}
-	if b1 then
-		ops[off]=aux.Stringid(101105089,0)
-		opval[off]=0
-		off=off+1
-	end
-	if b2 then
-		ops[off]=aux.Stringid(101105089,1)
-		opval[off]=1
-		off=off+1
-	end
-	local op=Duel.SelectOption(tp,table.unpack(ops))+1
-	local sel=opval[op]
-	e:SetLabel(sel)
-	if sel==0 then
-		e:SetCategory(CATEGORY_SPECIAL_SUMMON)
-		Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
-	else
-		e:SetCategory(CATEGORY_ATKCHANGE)
+function c101105089.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c101105089.spfilter,tp,LOCATION_DECK,0,1,nil,e,tp) end
+	Duel.Hint(HINT_OPSELECTED,1-tp,e:GetDescription())
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
+end
+function c101105089.spop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)>0 then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+		local g=Duel.SelectMatchingCard(tp,c101105089.spfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
+		if g:GetCount()>0 then
+			Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+		end
 	end
 end
-function c101105089.effop(e,tp,eg,ep,ev,re,r,rp)
+function c101105089.atkcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.CheckReleaseGroup(tp,c101105089.costfilter,1,e:GetHandler(),tp) end
+	local g=Duel.SelectReleaseGroup(tp,c101105089.costfilter,1,1,e:GetHandler(),tp)
+	Duel.Release(g,REASON_COST)
+end
+function c101105089.atktg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.Hint(HINT_OPSELECTED,1-tp,e:GetDescription())
+end
+function c101105089.atkop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	local sel=e:GetLabel()
-	if sel==0 then
-		if Duel.GetLocationCount(tp,LOCATION_MZONE)>0 then
-			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-			local g=Duel.SelectMatchingCard(tp,c101105089.spfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
-			if g:GetCount()>0 then
-				Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
-			end
-		end
-	else
-		if c:IsFaceup() and c:IsRelateToEffect(e) then
-			c:RegisterFlagEffect(101105089,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,0,0)
-			local e1=Effect.CreateEffect(c)
-			e1:SetType(EFFECT_TYPE_SINGLE)
-			e1:SetCode(EFFECT_UPDATE_ATTACK)
-			e1:SetValue(2000)
-			e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
-			c:RegisterEffect(e1)
-		end
+	if c:IsFaceup() and c:IsRelateToEffect(e) then
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetValue(2000)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_DISABLE+RESET_PHASE+PHASE_END)
+		c:RegisterEffect(e1)
 	end
 end
