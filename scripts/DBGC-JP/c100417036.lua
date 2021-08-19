2094b2bbf5533760db2406627f83d153b51b1f57
Mercury233 <me@mercury233.me>
2021-08-19 14:48:18 +0800

    Update c100417036.lua

--- scripts/DBGC-JP/c100417036.lua
+++ scripts/DBGC-JP/c100417036.lua
@@ -1,7 +1,9 @@
---coded by Lyris
+--リザレクション・ブレス
 --Resurrection Breath
+--coded by Lyris
 function c100417036.initial_effect(c)
-	--If you control "Brave Token": Special Summon up to 2 monsters with different names from your hand and/or GY (but banish them when they leave the field), then you can equip 1 Equip Spell that specifically lists "Brave Token" in its text from your hand or GY to 1 appropriate monster you control. You can only activate 1 "Resurrection Breath" per turn.
+	aux.AddCodeList(c,100417125)
+	--spsummon & equip
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_EQUIP)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
@@ -12,44 +14,45 @@ function c100417036.initial_effect(c)
 	e1:SetOperation(c100417036.activate)
 	c:RegisterEffect(e1)
 end
+function c100417036.cfilter(c)
+	return c:IsFaceup() and c:IsCode(100417125)
+end
 function c100417036.condition(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.IsExistingMatchingCard(aux.AND(Card.IsFaceup,Card.IsCode),tp,LOCATION_ONFIELD,0,1,nil,100417125)
+	return Duel.IsExistingMatchingCard(c100417036.cfilter,tp,LOCATION_ONFIELD,0,1,nil)
 end
 function c100417036.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsCanBeSpecialSummoned,tp,LOCATION_HAND+LOCATION_GRAVE,0,1,nil,e,0,tp,false,false) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND+LOCATION_GRAVE)
 end
 function c100417036.filter(c,tp)
-	return c:IsFaceup() and Duel.IsExistingMatchingCard(aux.NecroValleyFilter(c100417036.eqfilter),tp,LOCATION_DECK,0,1,nil,tp,c)
+	return c:IsFaceup() and Duel.IsExistingMatchingCard(aux.NecroValleyFilter(c100417036.eqfilter),tp,LOCATION_HAND+LOCATION_GRAVE,0,1,nil,tp,c)
 end
 function c100417036.eqfilter(c,tp,tc)
 	return c:CheckUniqueOnField(tp) and not c:IsForbidden() and c:CheckEquipTarget(tc) and aux.IsCodeListed(c,100417125)
 end
 function c100417036.activate(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
-	local ft=math.min(2,Duel.GetLocationCount(tp,LOCATION_MZONE))
-	if Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) then ft=1 end
+	local ft=math.min(2,(Duel.GetLocationCount(tp,LOCATION_MZONE)))
+	if ft<=0 then return end
+	if Duel.IsPlayerAffectedByEffect(tp,59822133) then ft=1 end
 	local tg=Duel.GetMatchingGroup(aux.NecroValleyFilter(Card.IsCanBeSpecialSummoned),tp,LOCATION_HAND+LOCATION_GRAVE,0,nil,e,0,tp,false,false)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 	local g1=tg:SelectSubGroup(tp,aux.dncheck,false,1,ft)
-	local ct=0
+	if not g1 then return end
 	for tc in aux.Next(g1) do
 		if Duel.SpecialSummonStep(tc,0,tp,tp,false,false,POS_FACEUP) then
 			local e1=Effect.CreateEffect(c)
 			e1:SetType(EFFECT_TYPE_SINGLE)
 			e1:SetCode(EFFECT_LEAVE_FIELD_REDIRECT)
 			e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-			e1:SetReset(RESET_EVENT+RESETS_STANDARD-RESET_TOFIELD)
+			e1:SetReset(RESET_EVENT+RESETS_STANDARD)
 			e1:SetValue(LOCATION_REMOVED)
 			tc:RegisterEffect(e1,true)
-			ct=ct+1
 		end
 	end
 	Duel.SpecialSummonComplete()
-	if ct==0 then return end
 	local g2=Duel.GetMatchingGroup(c100417036.filter,tp,LOCATION_MZONE,0,1,nil,tp)
-	if Duel.GetLocationCount(tp,LOCATION_SZONE)>0 and #g2>0 and Duel.SelectEffectYesNo(tp,c) then
+	if Duel.GetLocationCount(tp,LOCATION_SZONE)>0 and #g2>0 and Duel.SelectYesNo(tp,aux.Stringid(100417036,0)) then
 		Duel.BreakEffect()
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
 		local sc=g2:Select(tp,1,1,nil):GetFirst()
