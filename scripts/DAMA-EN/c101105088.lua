cd26aacb9c3730055cf437ad9126d3637edb4971
Mercury233 <me@mercury233.me>
2021-08-19 14:31:57 +0800

    Update c101105088.lua

--- scripts/DAMA-EN/c101105088.lua
+++ scripts/DAMA-EN/c101105088.lua
@@ -2,9 +2,8 @@
 --coded by Lyris
 function c101105088.initial_effect(c)
 	c:EnableReviveLimit()
-	--mat=2 Insect monsters
 	aux.AddLinkProcedure(c,aux.FilterBoolFunction(Card.IsRace,RACE_INSECT),2,2)
-	--You cannot Special Summon monsters, except Insect monsters.
+	--splimit
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
@@ -13,25 +12,24 @@ function c101105088.initial_effect(c)
 	e1:SetTargetRange(1,0)
 	e1:SetTarget(aux.TargetBoolFunction(aux.NOT(Card.IsRace),RACE_INSECT))
 	c:RegisterEffect(e1)
-	--You can only use each of the following effects of "Beetrooper Armor Horn" once per turn.
-	--During your Main Phase, you can: Immediately after this effect resolves, Normal Summon 1 Insect monster.
+	--summon
 	local e2=Effect.CreateEffect(c)
+	e2:SetCategory(CATEGORY_SUMMON)
 	e2:SetType(EFFECT_TYPE_IGNITION)
 	e2:SetRange(LOCATION_MZONE)
 	e2:SetCountLimit(1,101105088)
-	e2:SetCategory(CATEGORY_SUMMON)
 	e2:SetTarget(c101105088.target)
 	e2:SetOperation(c101105088.operation)
 	c:RegisterEffect(e2)
-	--If this card is in your GY: You can banish 3 other Insect monsters from your GY; Special Summon this card, but banish it when it leaves the field.
+	--spsummon
 	local e3=Effect.CreateEffect(c)
+	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e3:SetType(EFFECT_TYPE_IGNITION)
 	e3:SetRange(LOCATION_GRAVE)
 	e3:SetCountLimit(1,101105188)
-	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e3:SetCost(c101105088.cost)
-	e3:SetTarget(c101105088.tg)
-	e3:SetOperation(c101105088.op)
+	e3:SetCost(c101105088.spcost)
+	e3:SetTarget(c101105088.sptg)
+	e3:SetOperation(c101105088.spop)
 	c:RegisterEffect(e3)
 end
 function c101105088.filter(c)
@@ -51,20 +49,20 @@ end
 function c101105088.cfilter(c)
 	return c:IsRace(RACE_INSECT) and c:IsAbleToRemoveAsCost()
 end
-function c101105088.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+function c101105088.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
 	if chk==0 then return Duel.IsExistingMatchingCard(c101105088.cfilter,tp,LOCATION_GRAVE,0,3,c) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
 	local g=Duel.SelectMatchingCard(tp,c101105088.cfilter,tp,LOCATION_GRAVE,0,3,3,c)
 	Duel.Remove(g,POS_FACEUP,REASON_COST)
 end
-function c101105088.tg(e,tp,eg,ep,ev,re,r,rp,chk)
+function c101105088.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
 		and c:IsCanBeSpecialSummoned(e,0,tp,false,false) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,0,0)
 end
-function c101105088.op(e,tp,eg,ep,ev,re,r,rp)
+function c101105088.spop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if c:IsRelateToEffect(e) and Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)>0 then
 		local e1=Effect.CreateEffect(c)
