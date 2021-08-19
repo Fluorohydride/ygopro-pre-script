9ed2ad027264c0e42673333d087eb610128ab44b
mercury233 <me@mercury233.me>
2021-08-19 22:56:41 +0800

    add scripts by REIKAI

---
+++ scripts/VJMP-JP/c100200206.lua
@@ -0,0 +1,100 @@
+--勇气之天使 维多利卡
+function c100200206.initial_effect(c)
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(100200206,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
+	e1:SetCountLimit(1,100200206)
+	e1:SetTarget(c100200206.sptg)
+	e1:SetOperation(c100200206.spop)
+	c:RegisterEffect(e1) 
+--
+	local e2=Effect.CreateEffect(c)
+	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_DESTROYED)
+	e2:SetProperty(EFFECT_FLAG_DELAY)
+	e2:SetCountLimit(1,100200206+100)
+	e2:SetCost(c100200206.thcost)
+	e2:SetCondition(c100200206.thcon)
+	e2:SetTarget(c100200206.thtg)
+	e2:SetOperation(c100200206.thop)
+	c:RegisterEffect(e2) 
+end
+function c100200206.spfilter(c,e,tp)
+	return c:IsCanBeSpecialSummoned(e,0,tp,false,false) and c:IsLevelAbove(5) and c:IsAttribute(ATTRIBUTE_LIGHT) and Duel.GetLP(tp)>=c:GetTextAttack()
+end
+function c100200206.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(c100200206.spfilter,tp,LOCATION_HAND,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
+end
+function c100200206.spop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,c100200206.spfilter,tp,LOCATION_HAND,0,1,1,nil,e,tp)
+	if g:GetCount()>0 then
+		local tc=g:GetFirst()
+		local atk=tc:GetAttack()
+		if Duel.SpecialSummonStep(tc,0,tp,tp,false,false,POS_FACEUP) then
+			local e1=Effect.CreateEffect(e:GetHandler())
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
+			e1:SetCode(EFFECT_SET_ATTACK)
+			e1:SetValue(math.ceil(atk*2))
+			e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+			tc:RegisterEffect(e1,true)
+			Duel.SpecialSummonComplete()
+			local lp=Duel.GetLP(tp)
+			local num=lp-tc:GetTextAttack()
+			if num<0 then
+				num=0
+			end
+			Duel.SetLP(tp,num)
+		end
+	end
+end
+function c100200206.cfilter(c,tp)
+	return c:IsLevelAbove(1) and c:IsRace(RACE_FAIRY) and c:IsAbleToRemoveAsCost() and Duel.IsExistingMatchingCard(c100200206.thfilter,tp,LOCATION_DECK,0,1,nil,c:GetLevel())
+end
+function c100200206.thfilter(c,lv)
+	return c:IsRace(RACE_FAIRY) and c:IsAbleToHand() and c:IsLevel(lv)
+end
+function c100200206.thcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	e:SetLabel(100)
+	return true
+	
+end
+function c100200206.thcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return bit.band(r,REASON_EFFECT+REASON_BATTLE)~=0 and c:IsPreviousLocation(LOCATION_MZONE) and c:IsFaceup()
+end
+function c100200206.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then
+		if e:GetLabel()~=100 then return false end
+		e:SetLabel(0)
+		local cg=Duel.GetMatchingGroup(c100200206.cfilter,tp,LOCATION_GRAVE,0,e:GetHandler(),tp)
+		return cg:GetCount()>0
+	end
+	local cg=Duel.GetMatchingGroup(c100200206.cfilter,tp,LOCATION_GRAVE,0,e:GetHandler(),tp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local rg=cg:Select(tp,1,1,nil)
+	Duel.Remove(rg,POS_FACEUP,REASON_COST)
+	local og=Duel.GetOperatedGroup()
+	local tc=og:GetFirst()
+	local lv=tc:GetLevel()
+	e:SetLabel(lv)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function c100200206.thop(e,tp,eg,ep,ev,re,r,rp)
+	local lv=e:GetLabel()
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,c100200206.thfilter,tp,LOCATION_DECK,0,1,1,nil,lv)
+	if g:GetCount()>0 then
+		Duel.SendtoHand(g,tp,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end
