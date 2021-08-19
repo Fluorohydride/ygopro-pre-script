10c360cf23615a3db234e4af506a7d05b34b4cd6
Mercury233 <me@mercury233.me>
2021-08-19 14:28:02 +0800

    Update c101105086.lua

--- scripts/DAMA-EN/c101105086.lua
+++ scripts/DAMA-EN/c101105086.lua
@@ -1,48 +1,48 @@
---coded by Lyris
 --Beetrooper Scale Bomber
+--coded by Lyris
 function c101105086.initial_effect(c)
-	--You can only use each effect of "Beetrooper Scale Bomber" once per turn.
-	--If an Insect monster(s) is Normal or Special Summoned to your field (except during the Damage Step): You can Special Summon this card from your hand.
-	local e0=Effect.CreateEffect(c)
-	e0:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-	e0:SetCode(EVENT_SUMMON_SUCCESS)
-	e0:SetRange(LOCATION_HAND)
-	e0:SetCountLimit(1,101105086)
-	e0:SetProperty(EFFECT_FLAG_DELAY)
-	e0:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e0:SetCondition(c101105086.con)
-	e0:SetTarget(c101105086.tg)
-	e0:SetOperation(c101105086.op)
-	c:RegisterEffect(e0)
-	local e1=e0:Clone()
-	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	--spsummon
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(101105086,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_SUMMON_SUCCESS)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCountLimit(1,101105086)
+	e1:SetProperty(EFFECT_FLAG_DELAY)
+	e1:SetCondition(c101105086.spcon)
+	e1:SetTarget(c101105086.sptg)
+	e1:SetOperation(c101105086.spop)
 	c:RegisterEffect(e1)
-	--When a monster your opponent controls activates its effect (Quick Effect): You can Tribute 1 Insect monster; destroy it.
-	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_QUICK_O)
-	e2:SetCode(EVENT_CHAINING)
-	e2:SetRange(LOCATION_MZONE)
-	e2:SetCountLimit(1,39041551)
-	e2:SetCategory(CATEGORY_DESTROY)
-	e2:SetCondition(c101105086.condition)
-	e2:SetCost(c101105086.cost)
-	e2:SetTarget(c101105086.target)
-	e2:SetOperation(c101105086.operation)
+	local e2=e1:Clone()
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
 	c:RegisterEffect(e2)
+	--When a monster your opponent controls activates its effect (Quick Effect): You can Tribute 1 Insect monster; destroy it.
+	local e3=Effect.CreateEffect(c)
+	e3:SetCategory(CATEGORY_DESTROY)
+	e3:SetType(EFFECT_TYPE_QUICK_O)
+	e3:SetCode(EVENT_CHAINING)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCountLimit(1,39041551)
+	e3:SetCondition(c101105086.condition)
+	e3:SetCost(c101105086.cost)
+	e3:SetTarget(c101105086.target)
+	e3:SetOperation(c101105086.operation)
+	c:RegisterEffect(e3)
 end
 function c101105086.filter(c,tp)
 	return c:IsFaceup() and c:IsRace(RACE_INSECT) and c:IsControler(tp)
 end
-function c101105086.con(e,tp,eg,ep,ev,re,r,rp)
+function c101105086.spcon(e,tp,eg,ep,ev,re,r,rp)
 	return eg:IsExists(c101105086.filter,1,nil,tp)
 end
-function c101105086.tg(e,tp,eg,ep,ev,re,r,rp,chk)
+function c101105086.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
 		and c:IsCanBeSpecialSummoned(e,0,tp,false,false) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,0,0)
 end
-function c101105086.op(e,tp,eg,ep,ev,re,r,rp)
+function c101105086.spop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if c:IsRelateToEffect(e) then
 		Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
@@ -51,9 +51,13 @@ end
 function c101105086.condition(e,tp,eg,ep,ev,re,r,rp)
 	return re:GetHandler():IsOnField() and re:GetHandler():IsRelateToEffect(re) and re:IsActiveType(TYPE_MONSTER)
 end
+function c101105086.cfilter(c,tp)
+	return c:IsRace(RACE_INSECT) and (c:IsControler(tp) or c:IsFaceup())
+end
 function c101105086.cost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.CheckReleaseGroup(tp,Card.IsRace,1,nil,RACE_INSECT) end
-	Duel.Release(Duel.SelectReleaseGroup(tp,Card.IsRace,1,1,nil,RACE_INSECT),REASON_COST)
+	if chk==0 then return Duel.CheckReleaseGroup(tp,c101105086.cfilter,1,re:GetHandler(),tp) end
+	local g=Duel.SelectReleaseGroup(tp,c101105086.cfilter,1,1,re:GetHandler(),tp)
+	Duel.Release(g,REASON_COST)
 end
 function c101105086.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return re:GetHandler():IsDestructable() end
