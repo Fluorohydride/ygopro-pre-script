60265dd266808f346c8d663e21538a8054f75294
Mercury233 <me@mercury233.me>
2021-08-19 11:13:49 +0800

    Update c101105083.lua

--- scripts/DAMA-EN/c101105083.lua
+++ scripts/DAMA-EN/c101105083.lua
@@ -1,10 +1,11 @@
 --Night's End Administrator
+--coded by Lyris
 function c101105083.initial_effect(c)
     c:EnableReviveLimit()
-    --mat="Night's End Sorcerer" + 1+ non-Tuner monsters
     aux.AddSynchroProcedure(c,aux.FilterBoolFunction(Card.IsCode,36107810),aux.NonTuner(nil),1)
-    --If this card is Special Summoned, or if another Spellcaster monster(s) is Special Summoned to your field: You can target 1 card in your opponent's GY; banish it.
+    --banish
     local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(101105083,0))
     e1:SetCategory(CATEGORY_REMOVE)
     e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
     e1:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
@@ -15,26 +16,26 @@ function c101105083.initial_effect(c)
     local e2=e1:Clone()
     e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
     e2:SetRange(LOCATION_MZONE)
-    e2:SetCondition(c101105083.contition)
+    e2:SetCondition(c101105083.condition)
     c:RegisterEffect(e2)
-    --If this card is destroyed by battle, or if this card in its owner's Monster Zone is destroyed by an opponent's card effect: You can target 1 Level 4 or lower Spellcaster monster in your GY; Special Summon it. You can only use this effect of "Night's End Administrator" once per turn.2
+    --spsummon
     local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(101105083,1))
+    e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
     e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
     e3:SetCode(EVENT_DESTROYED)
-    e3:SetCountLimit(1,101105083)
     e3:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
-    e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
-    e3:SetCondition(c101105083.con)
-    e3:SetTarget(c101105083.tg)
-    e3:SetOperation(c101105083.op)
+    e3:SetCountLimit(1,101105083)
+    e3:SetCondition(c101105083.spcon)
+    e3:SetTarget(c101105083.sptg)
+    e3:SetOperation(c101105083.spop)
     c:RegisterEffect(e3)
 end
 function c101105083.cfilter(c,tp)
-    return c:IsRace(RACE_SPELLCASTER) and c:IsControler(tp)
+    return c:IsRace(RACE_SPELLCASTER) and c:IsFaceup() and c:IsControler(tp)
 end
-function c101105083.contition(e,tp,eg,ep,ev,re,r,rp)
-    local c=e:GetHandler()
-    return not eg:IsContains(c) and eg:IsExists(c101105083.cfilter,1,nil,tp)
+function c101105083.condition(e,tp,eg,ep,ev,re,r,rp)
+    return eg:IsExists(c101105083.cfilter,1,e:GetHandler(),tp)
 end
 function c101105083.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
     if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(1-tp) and chkc:IsAbleToRemove() end
@@ -49,14 +50,14 @@ function c101105083.operation(e,tp,eg,ep,ev,re,r,rp)
         Duel.Remove(tc,POS_FACEUP,REASON_EFFECT)
     end
 end
-function c101105083.con(e,tp,eg,ep,ev,re,r,rp)
+function c101105083.spcon(e,tp,eg,ep,ev,re,r,rp)
     local c=e:GetHandler()
-    return (rp~=tp and c:IsReason(REASON_EFFECT) and c:IsPreviousControler(tp) and c:IsPreviousLocation(LOCATION_MZONE)) or c:IsReason(REASON_BATTLE)
+    return c:IsPreviousLocation(LOCATION_MZONE) and (rp~=tp and c:IsReason(REASON_EFFECT) and c:IsPreviousControler(tp) or c:IsReason(REASON_BATTLE))
 end
 function c101105083.filter(c,e,tp)
     return c:IsLevelBelow(4) and c:IsRace(RACE_SPELLCASTER) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
-function c101105083.tg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+function c101105083.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
     if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and c101105083.filter(chkc,e,tp) end
     if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
         and Duel.IsExistingTarget(c101105083.filter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
@@ -64,7 +65,7 @@ function c101105083.tg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
     local g=Duel.SelectTarget(tp,c101105083.filter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
     Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
 end
-function c101105083.op(e,tp,eg,ep,ev,re,r,rp)
+function c101105083.spop(e,tp,eg,ep,ev,re,r,rp)
     local tc=Duel.GetFirstTarget()
     if tc:IsRelateToEffect(e) then
         Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
