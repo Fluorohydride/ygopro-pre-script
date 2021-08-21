f7ebbedd999ffbdbde5ad0affe89bb49b389ffaf
mercury233 <me@mercury233.me>
2021-08-21 22:41:04 +0800

    fix

--- scripts/DBGC-JP/c100417028.lua
+++ scripts/DBGC-JP/c100417028.lua
@@ -2,6 +2,7 @@
 --Wandering Gryphon Rider
 --scripted by RayeHikawa
 function c100417028.initial_effect(c)
+	aux.AddCodeList(c,100417125)
 	--special summon
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(100417028,0))
