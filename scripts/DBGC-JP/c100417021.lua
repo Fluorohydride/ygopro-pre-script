398bf6476475a69cca2a6d24e08d83a1a4dd89ce
wzh31891579 <31891579@qq.com>
2021-08-25 21:40:50 +0800

    format

--- scripts/DBGC-JP/c100417021.lua
+++ scripts/DBGC-JP/c100417021.lua
@@ -8,37 +8,39 @@ function c100417021.initial_effect(c)
 	e:SetCode(EVENT_FREE_CHAIN)
 	e:SetCountLimit(1,100417021+EFFECT_COUNT_CODE_OATH)
 	e:SetHintTiming(TIMING_MAIN_END)
-	e:SetCondition(c100417021.thcon)
-	e:SetCost(c100417021.thcost)
-	e:SetTarget(c100417021.thtg)
-	e:SetOperation(c100417021.thop)
+	e:SetCondition(c100417021.condition)
+	e:SetCost(c100417021.cost)
+	e:SetTarget(c100417021.target)
+	e:SetOperation(c100417021.operation)
 	c:RegisterEffect(e)
 end
-function c100417021.thcon(e,tp,eg,ep,ev,re,r,rp)
+function c100417021.condition(e,tp,eg,ep,ev,re,r,rp)
 	return Duel.GetCurrentPhase()==PHASE_MAIN1 or Duel.GetCurrentPhase()==PHASE_MAIN2
 end
-function c100417021.thcost(e,tp,eg,ep,ev,re,r,rp,chk)
+function c100417021.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.CheckLPCost(tp,800) end
 	Duel.PayLPCost(tp,800)
 end
 function c100417021.thfilter(c)
 	return c:IsSetCard(0x271) and not c:IsCode(100417021) and c:IsAbleToHand()
 end
-function c100417021.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+function c100417021.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(c100417021.thfilter,tp,LOCATION_DECK,0,1,nil) end
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
 end
 function c100417021.spfilter(c,code)
 	return c:IsType(TYPE_MONSTER) and c:IsSetCard(0x271) and aux.IsCodeListed(c,code)
 end
-function c100417021.thop(e,tp,eg,ep,ev,re,r,rp)
+function c100417021.operation(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
 	local tc=Duel.SelectMatchingCard(tp,c100417021.thfilter,tp,LOCATION_DECK,0,1,1,nil):GetFirst()
-	if tc and Duel.SendtoHand(tc,nil,REASON_EFFECT) then
+	local res=false
+	if tc and Duel.SendtoHand(tc,nil,REASON_EFFECT)>0 and tc:IsLocation(LOCATION_HAND) then
 		Duel.ConfirmCards(1-tp,tc)
-		if tc:IsType(TYPE_MONSTER) and Duel.IsExistingMatchingCard(c100417021.spfilter,tp,LOCATION_MZONE+LOCATION_GRAVE,0,1,nil,tc:GetCode()) and tc:IsCanBeSpecialSummoned(e,0,tp,false,false) and Duel.SelectYesNo(tp,aux.Stringid(100417021,0)) then
-			Duel.BreakEffect()
-			Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
-		end
+		res=true
+	end
+	if res and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and tc:IsType(TYPE_MONSTER) and Duel.IsExistingMatchingCard(c100417021.spfilter,tp,LOCATION_MZONE+LOCATION_GRAVE,0,1,nil,tc:GetCode()) and tc:IsCanBeSpecialSummoned(e,0,tp,false,false) and Duel.SelectYesNo(tp,aux.Stringid(100417021,0)) then
+		Duel.BreakEffect()
+		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
 	end
 end
