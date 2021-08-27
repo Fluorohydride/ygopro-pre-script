685f41d5f283ee8e57aea47e37c1e9d1b9c5e2b7
mercury233 <me@mercury233.me>
2021-08-27 21:09:40 +0800

    fix Beetrooper Formation

--- scripts/DAMA-EN/c101105090.lua
+++ scripts/DAMA-EN/c101105090.lua
@@ -42,7 +42,8 @@ function c101105090.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 end
 function c101105090.spop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
-	if tc:IsRelateToEffect(e) and Duel.SpecialSummonStep(tc,0,tp,tp,false,false,POS_FACEUP) then
+	if not tc:IsRelateToEffect(e) then return end
+	if Duel.SpecialSummonStep(tc,0,tp,tp,false,false,POS_FACEUP) then
 		local e1=Effect.CreateEffect(e:GetHandler())
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_CANNOT_ATTACK)
