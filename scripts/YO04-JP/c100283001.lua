d2cb306194d21526b669f706ec8f7070925d0b3d
Mercury233 <me@mercury233.me>
2021-09-25 20:48:05 +0800

    Update c100283001.lua

--- scripts/YO04-JP/c100283001.lua
+++ scripts/YO04-JP/c100283001.lua
@@ -52,9 +52,9 @@ function c100283001.ovfilter(c)
 end
 function c100283001.ovtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and c100283001.ovfilter(chkc) end
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and
-		Duel.IsExistingTarget(c100283001.ovfilter,tp,LOCATION_MZONE,0,1,nil) and
-		e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingTarget(c100283001.ovfilter,tp,LOCATION_MZONE,0,1,nil)
+		and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
 	local g=Duel.SelectTarget(tp,c100283001.ovfilter,tp,LOCATION_MZONE,0,1,1,nil)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
@@ -63,7 +63,8 @@ function c100283001.ovop(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
 	local c=e:GetHandler()
 	local tc=Duel.GetFirstTarget()
-	if c:IsRelateToEffect(e) and tc:IsRelateToEffect(e) and tc:IsFaceup() and Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP) then
+	if c:IsRelateToEffect(e) and Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
+		and tc:IsRelateToEffect(e) and tc:IsFaceup() then
 		Duel.Overlay(c,Group.FromCards(tc))
 	end
 end
