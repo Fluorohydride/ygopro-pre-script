b249497ebd0db3349926bb5577b73abd6f4b16ff
Mercury233 <me@mercury233.me>
2021-09-25 16:41:49 +0800

    Update c101107053.lua

--- scripts/BACH-JP/c101107053.lua
+++ scripts/BACH-JP/c101107053.lua
@@ -1,3 +1,6 @@
+--烙印の使徒
+--
+--Script by Trishula9
 function c101107053.initial_effect(c)
 	--activate 
 	local e1=Effect.CreateEffect(c)
@@ -22,27 +25,26 @@ function c101107053.initial_effect(c)
 	e3:SetOperation(c101107053.desop)
 	c:RegisterEffect(e3)
 end
-c101107053[0]=0
 function c101107053.disfilter(c)
 	return c:IsType(TYPE_FUSION) and c:IsRace(RACE_FIEND) and c:IsLevelAbove(8) and c:IsFaceup()
 end
 function c101107053.disop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	local id=Duel.GetChainInfo(ev,CHAININFO_CHAIN_ID)
-	if ep==tp or id==c101107053[0] or not re:IsActiveType(TYPE_MONSTER)
-		or (not re:GetHandler():IsAttack(0) and not re:GetHandler():IsDefense(0))
-		or not Duel.IsExistingMatchingCard(c101107053.disfilter,tp,LOCATION_MZONE,0,1,nil)
-		or c:GetFlagEffect(101107053)>0
-		then return end
-	c101107053[0]=id
-	if Duel.SelectYesNo(tp,aux.Stringid(101107053,0)) then
+	local rc=re:GetHandler()
+	if rp==1-tp and c:GetFlagEffect(101107053)==0
+		and re:IsActiveType(TYPE_MONSTER) and (rc:IsAttack(0) or rc:IsDefense(0))
+		--and rc:IsRelateToEffect(re) and rc:IsLocation(LOCATION_MZONE+LOCATION_GRAVE+LOCATION_REMOVED)
+		--and (rc:IsFaceup() or rc:IsLocation(LOCATION_GRAVE))
+		and Duel.IsExistingMatchingCard(c101107053.disfilter,tp,LOCATION_MZONE,0,1,nil)
+		and Duel.SelectEffectYesNo(tp,c,aux.Stringid(101107053,0)) then
+		Duel.Hint(HINT_CARD,0,101107053)
 		Duel.NegateEffect(ev)
 		c:RegisterFlagEffect(101107053,RESET_PHASE+PHASE_END,0,0)
 	end
 end
 function c101107053.descon(e,tp,eg,ep,ev,re,r,rp)
 	local a,d=Duel.GetBattleMonster(tp)
-	return a:IsAttack(0) and d:IsAttack(0)
+	return a and d and a:IsAttack(0) and d:IsAttack(0)
 end
 function c101107053.desop(e,tp,eg,ep,ev,re,r,rp)
 	local a,d=Duel.GetBattleMonster(tp)
