00a96d6b91eeabf98cd7275ffa47b466e98d7992
wzh31891579 <31891579@qq.com>
2021-10-05 22:13:18 +0800

    update ダイノルフィア cards (c101107073.lua is not included)

---
+++ scripts/BACH-JP/c101107010.lua
@@ -0,0 +1,73 @@
+--ダイノルフィア・ディプロス
+--
+--Script by Trishula9
+function c101107010.initial_effect(c)
+	--to grave
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_TOGRAVE)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_SUMMON_SUCCESS)
+	e1:SetProperty(EFFECT_FLAG_DELAY)
+	e1:SetCountLimit(1,101107010)
+	e1:SetTarget(c101107010.tgtg)
+	e1:SetOperation(c101107010.tgop)
+	c:RegisterEffect(e1)
+	local e2=e1:Clone()
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	c:RegisterEffect(e2)
+	--spsummon
+	local e3=Effect.CreateEffect(c)
+	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e3:SetCode(EVENT_DESTROYED)
+	e3:SetProperty(EFFECT_FLAG_DELAY)
+	e3:SetCountLimit(1,101107010+100)
+	e3:SetCondition(c101107010.spcon)
+	e3:SetCost(c101107010.spcost)
+	e3:SetTarget(c101107010.sptg)
+	e3:SetOperation(c101107010.spop)
+	c:RegisterEffect(e3)
+end
+function c101107010.tgfilter(c)
+	return c:IsSetCard(0x173) and c:IsAbleToGrave()
+end
+function c101107010.tgtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c101107010.tgfilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_GRAVE)
+	
+end
+function c101107010.tgop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local g=Duel.SelectMatchingCard(tp,c101107010.tgfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if g:GetCount()>0 and Duel.SendtoGrave(g,REASON_EFFECT)~=0 and Duel.GetLP(tp)<=2000 then
+		Duel.Damage(1-tp,500,REASON_EFFECT)
+	end
+end
+function c101107010.spcon(e,tp,eg,ep,ev,re,r,rp)
+	return bit.band(r,REASON_EFFECT+REASON_BATTLE)~=0
+end
+function c101107010.costfilter(c)
+	return c:IsType(TYPE_TRAP) and c:IsAbleToRemoveAsCost()
+end
+function c101107010.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c101107010.costfilter,tp,LOCATION_GRAVE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local g=Duel.SelectMatchingCard(tp,c101107010.costfilter,tp,LOCATION_GRAVE,0,1,1,nil)
+	Duel.Remove(g,POS_FACEUP,REASON_COST)
+end
+function c101107010.spfilter(c,e,tp)
+	return c:IsSetCard(0x173) and not c:IsCode(101107010) and c:IsLevelBelow(4) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function c101107010.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(c101107010.spfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE)
+end
+function c101107010.spop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,c101107010.spfilter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
+	if g:GetCount()>0 then
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
---
+++ scripts/BACH-JP/c101107069.lua
@@ -0,0 +1,95 @@
+--ダイノルフィア・アラート
+--
+--Script by Trishula9
+function c101107069.initial_effect(c)
+	--activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetHintTiming(0,TIMING_MAIN_END)
+	e1:SetCountLimit(1,101107069+EFFECT_COUNT_CODE_OATH)
+	e1:SetCost(c101107069.cost)
+	e1:SetTarget(c101107069.target)
+	e1:SetOperation(c101107069.operation)
+	c:RegisterEffect(e1)
+	--change damage
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetCode(EVENT_CHAINING)
+	e2:SetRange(LOCATION_GRAVE)
+	e2:SetCondition(c101107069.cdcon)
+	e2:SetCost(aux.bfgcost)
+	e2:SetOperation(c101107069.cdop)
+	c:RegisterEffect(e2)
+end
+function c101107069.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.PayLPCost(tp,math.floor(Duel.GetLP(tp)/2))
+end
+function c101107069.spfilter(c,e,tp)
+	return c:IsSetCard(0x173) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function c101107069.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(c101107069.spfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE)
+end
+function c101107069.spcheck(g)
+	return g:GetSum(Card.GetLevel)<=8
+end
+function c101107069.operation(e,tp,eg,ep,ev,re,r,rp)
+	local ft=math.min((Duel.GetLocationCount(tp,LOCATION_MZONE)),2)
+	local tg=Duel.GetMatchingGroup(c101107069.spfilter,tp,LOCATION_GRAVE,0,nil,e,tp)
+	if ft<=0 or #tg==0 then return end
+	if Duel.IsPlayerAffectedByEffect(tp,59822133) then ft=1 end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	aux.GCheckAdditional=c101107069.spcheck
+	local g=tg:SelectSubGroup(tp,aux.TRUE,false,1,ft)
+	aux.GCheckAdditional=nil
+	local tc=g:GetFirst()
+	while tc do
+		if Duel.SpecialSummonStep(tc,0,tp,tp,false,false,POS_FACEUP) then
+			local e1=Effect.CreateEffect(e:GetHandler())
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetCode(EFFECT_CANNOT_ATTACK)
+			e1:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
+			e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+			tc:RegisterEffect(e1)
+		end
+		tc=g:GetNext()	
+	end
+	Duel.SpecialSummonComplete()
+	local e2=Effect.CreateEffect(e:GetHandler())
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e2:SetTargetRange(1,0)
+	e2:SetTarget(c101107069.splimit)
+	e2:SetReset(RESET_PHASE+PHASE_END)
+	Duel.RegisterEffect(e2,tp)
+end
+function c101107069.splimit(e,c)
+	return not c:IsSetCard(0x173)
+end
+function c101107069.cdcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetLP(tp)<=2000 and rp==1-tp
+end
+function c101107069.cdop(e,tp,eg,ep,ev,re,r,rp)
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_CHANGE_DAMAGE)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetTargetRange(1,0)
+	e1:SetValue(c101107069.damval1)
+	e1:SetReset(RESET_PHASE+PHASE_END)
+	Duel.RegisterEffect(e1,tp)
+	local e2=e1:Clone()
+	e2:SetCode(EFFECT_NO_EFFECT_DAMAGE)
+	e2:SetReset(RESET_PHASE+PHASE_END)
+	Duel.RegisterEffect(e2,tp)
+end
+function c101107069.damval1(e,re,val,r,rp,rc)
+	if bit.band(r,REASON_EFFECT)~=0 and rp==1-e:GetOwnerPlayer() then return 0
+	else return val end
+end
