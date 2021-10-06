d2814ee33f11e6dbe32701d7fd0862cfe986ba79
Mercury233 <me@mercury233.me>
2021-10-06 09:19:09 +0800

    Update c101107022.lua

--- scripts/BACH-JP/c101107022.lua
+++ scripts/BACH-JP/c101107022.lua
@@ -15,6 +15,7 @@ function c101107022.initial_effect(c)
 	c:RegisterEffect(e1)
 	--to grave
 	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(101107022,0))
 	e2:SetCategory(CATEGORY_TOGRAVE)
 	e2:SetType(EFFECT_TYPE_IGNITION)
 	e2:SetRange(LOCATION_MZONE)
@@ -24,19 +25,18 @@ function c101107022.initial_effect(c)
 	e2:SetOperation(c101107022.tgop)
 	c:RegisterEffect(e2)
 end
-function c101107022.spfilter(c,ft)
-	return c:IsRace(RACE_WARRIOR) and c:IsAttribute(ATTRIBUTE_LIGHT|ATTRIBUTE_EARTH) and c:IsFaceup() and (ft>0 or c:GetSequence()<5) 
+function c101107022.spfilter(c,tp)
+	return c:IsRace(RACE_WARRIOR) and c:IsAttribute(ATTRIBUTE_LIGHT|ATTRIBUTE_EARTH) and c:IsFaceup() and Duel.GetMZoneCount(tp,c)>0
 end
 function c101107022.spcon(e,c)
 	if c==nil then return true end
 	local tp=c:GetControler()
-	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
-	return ft>-1 and Duel.CheckReleaseGroup(tp,c101107022.spfilter,1,nil,ft)
+	return Duel.CheckReleaseGroup(tp,c101107022.spfilter,1,nil)
 end
 function c101107022.spop(e,tp,eg,ep,ev,re,r,rp,c)
-	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
-	local g=Duel.SelectReleaseGroup(tp,c101107022.spfilter,1,1,nil,ft)
+	local g=Duel.SelectReleaseGroup(tp,c101107022.spfilter,1,1,nil)
 	Duel.Release(g,REASON_COST)
+	c:RegisterFlagEffect(0,RESET_EVENT+0x4fc0000,EFFECT_FLAG_CLIENT_HINT,1,0,aux.Stringid(101107022,1))
 	local atk=g:GetFirst():GetBaseAttack()
 	if atk<0 then return end
 	local e1=Effect.CreateEffect(c)
