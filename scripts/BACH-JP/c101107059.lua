c9119f86c1fe2779f976aa55abd20619e89aa9fa
mercury233 <me@mercury233.me>
2021-10-12 17:29:19 +0800

    add scripts by JoyJ

---
+++ scripts/BACH-JP/c101107059.lua
@@ -0,0 +1,92 @@
+--ベアルクティ・ラディエーション
+RADIATION_COUNTER = 0x99
+function c101107059.initial_effect(c)
+	c:SetUniqueOnField(1,0,101107059)
+	c:EnableCounterPermit(RADIATION_COUNTER)
+	--activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_COUNTER)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetTarget(c101107059.target)
+	c:RegisterEffect(e1)
+	--special counter permit
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetCode(EFFECT_COUNTER_PERMIT+RADIATION_COUNTER)
+	e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e2:SetCondition(c101107059.ctpermit)
+	c:RegisterEffect(e2)
+	--draw
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(101107059,1))
+	e3:SetCategory(CATEGORY_DRAW)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e3:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e3:SetCode(EVENT_SUMMON_SUCCESS)
+	e3:SetRange(LOCATION_SZONE)
+	e3:SetCondition(c101107059.drcon)
+	e3:SetCost(c101107059.drcost)
+	e3:SetTarget(c101107059.drtg)
+	e3:SetOperation(c101107059.drop)
+	c:RegisterEffect(e2)
+	local e3=e2:Clone()
+	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
+	c:RegisterEffect(e3)
+	--special summon equip
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(101107059,2))
+	e4:SetCategory(CATEGORY_TODECK)
+	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e4:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e4:SetRange(LOCATION_SZONE)
+	e4:SetCode(EVENT_PHASE+PHASE_END)
+	e4:SetCountLimit(1)
+	e4:SetTarget(c101107059.tdtg)
+	e4:SetOperation(c101107059.tdop)
+	c:RegisterEffect(e4)
+end
+function c101107059.tdfilter(c)
+	return c:IsSetCard(0x163) and not c:IsCode(101107059) and c:IsAbleToDeck()
+end
+function c101107059.tdtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingTarget(c101107059.tdfilter,tp,LOCATION_GRAVE,0,1,nil) end
+	local g=Duel.SelectTarget(tp,c101107059.tdfilter,tp,LOCATION_GRAVE,0,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_TODECK,g,1,0,0)
+end
+function c101107059.tdop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) then
+		Duel.SendtoDeck(tc,nil,2,REASON_EFFECT)
+	end
+end
+function c101107059.ctpermit(e)
+	local c=e:GetHandler()
+	return c:IsLocation(LOCATION_SZONE) and c:IsStatus(STATUS_CHAINING)
+end
+function c101107059.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return Duel.IsCanAddCounter(tp,RADIATION_COUNTER,7,c) end
+	c:AddCounter(RADIATION_COUNTER,7)
+end
+function c101107059.cfilter(c,tp)
+	return c:IsSummonPlayer(tp) and c:IsSetCard(0x163) and c:IsFaceup()
+end
+function c101107059.drcon(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(c101107059.cfilter,1,nil,tp)
+end
+
+function c101107059.drcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return c:IsCanRemoveCounter(tp,RADIATION_COUNTER,1,REASON_COST) end
+end
+function c101107059.drtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsPlayerCanDraw(tp,1) end
+	Duel.SetTargetPlayer(tp)
+	Duel.SetTargetParam(2)
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
+end
+function c101107059.drop(e,tp,eg,ep,ev,re,r,rp)
+	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+	Duel.Draw(p,d,REASON_EFFECT)
+end
