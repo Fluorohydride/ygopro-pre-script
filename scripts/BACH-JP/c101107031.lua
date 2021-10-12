1579004c39183e5162e27582a0a5b323856472b4
Mercury233 <me@mercury233.me>
2021-10-12 09:51:09 +0800

    Update c101107031.lua

--- scripts/BACH-JP/c101107031.lua
+++ scripts/BACH-JP/c101107031.lua
@@ -23,7 +23,7 @@ function c101107031.spfilter(c,e,tp)
 end
 function c101107031.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and c101107031.spfilter(chkc,e,tp) end
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+	if chk==0 then return Duel.GetMZoneCount(tp,e:GetHandler())>0
 		and Duel.IsExistingTarget(c101107031.spfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 	local g=Duel.SelectTarget(tp,c101107031.spfilter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
@@ -33,12 +33,11 @@ function c101107031.spfilter2(c,e,tp,attr)
 	return c:IsCanBeSpecialSummoned(e,0,tp,false,false) and c:IsLevel(2) and not c:IsAttribute(attr)
 end
 function c101107031.operation(e,tp,eg,ep,ev,re,r,rp)
-	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
-	if ft<=0 then return end
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
 	local tc=Duel.GetFirstTarget()
 	if tc:IsRelateToEffect(e) and Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)>0
 		and Duel.IsExistingMatchingCard(c101107031.spfilter2,tp,LOCATION_HAND,0,1,nil,e,tp,tc:GetAttribute())
-		and ft>1 and Duel.SelectYesNo(tp,aux.Stringid(101107031,0)) then
+		and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.SelectYesNo(tp,aux.Stringid(101107031,0)) then
 		Duel.BreakEffect()
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 		local sg=Duel.SelectMatchingCard(tp,c101107031.spfilter2,tp,LOCATION_HAND,0,1,1,nil,e,tp,tc:GetAttribute())
