7982629db75e7f4a8962e3efe74d3b8bb227ec89
mercury233 <me@mercury233.me>
2021-10-14 19:52:14 +0800

    add "Sea" code list

---
+++ scripts/DP26-JP/old cards/020/c24435369.lua
@@ -0,0 +1,13 @@
+--マーメイド・ナイト
+function c24435369.initial_effect(c)
+	aux.AddCodeList(c,22702055)
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_EXTRA_ATTACK)
+	e1:SetCondition(c24435369.dircon)
+	e1:SetValue(1)
+	c:RegisterEffect(e1)
+end
+function c24435369.dircon(e)
+	return Duel.IsEnvironment(22702055)
+end
