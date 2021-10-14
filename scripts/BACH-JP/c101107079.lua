404c2f67799b4ef5383afce363b1036b7f762797
Mercury233 <me@mercury233.me>
2021-10-14 08:30:09 +0800

    Update c101107079.lua

--- scripts/BACH-JP/c101107079.lua
+++ scripts/BACH-JP/c101107079.lua
@@ -4,7 +4,6 @@
 function c101107079.initial_effect(c)
 	--activate 
 	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_DESTROY+CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetCondition(c101107079.condition)
@@ -40,9 +39,11 @@ function c101107079.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	end
 	e:SetLabel(s)
 	if s==0 then
+		e:SetCategory(CATEGORY_DESTROY)
 		Duel.SetOperationInfo(0,CATEGORY_DESTROY,nil,1,1-tp,LOCATION_MZONE)
 	end
 	if s==1 then
+		e:SetCategory(CATEGORY_SPECIAL_SUMMON)
 		Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE)
 	end
 end
@@ -53,12 +54,13 @@ function c101107079.operation(e,tp,eg,ep,ev,re,r,rp)
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
 		local g=Duel.SelectMatchingCard(tp,c101107079.desfilter,tp,0,LOCATION_MZONE,1,1,nil,dif)
 		if g:GetCount()>0 then
+			Duel.HintSelection(g)
 			Duel.Destroy(g,REASON_EFFECT)
 		end
 	end
 	if e:GetLabel()==1 and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 then
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-		local g=Duel.SelectMatchingCard(tp,c101107079.spfilter,tp,LOCATION_GRAVE,0,1,1,nil,dif,e,tp)
+		local g=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(c101107079.spfilter),tp,LOCATION_GRAVE,0,1,1,nil,dif,e,tp)
 		if g:GetCount()>0 then
 			Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
 		end
