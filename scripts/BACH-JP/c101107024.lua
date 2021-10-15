43c2a02c07bab4aad0ff10ab83314a84341e032d
Mercury233 <me@mercury233.me>
2021-10-15 15:00:57 +0800

    Update c101107024.lua

--- scripts/BACH-JP/c101107024.lua
+++ scripts/BACH-JP/c101107024.lua
@@ -2,7 +2,6 @@
 --
 --Script by 虚子
 function c101107024.initial_effect(c)
-	aux.AddCodeList(c,40640057)
 	c:EnableCounterPermit(0x1)
 	c:SetCounterLimit(0x1,3)
 	--add counter
@@ -19,27 +18,26 @@ function c101107024.initial_effect(c)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetOperation(c101107024.acop)
 	c:RegisterEffect(e1)
-	--Search
+	--atk/lv up
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(101107024,1))
-	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e2:SetDescription(aux.Stringid(101107024,0))
+	e2:SetCategory(CATEGORY_ATKCHANGE)
 	e2:SetType(EFFECT_TYPE_IGNITION)
 	e2:SetRange(LOCATION_MZONE)
 	e2:SetCountLimit(1,101107024)
-	e2:SetCost(c101107024.thcost)
-	e2:SetTarget(c101107024.thtg)
-	e2:SetOperation(c101107024.thop)
-	c:RegisterEffect(e2) 
-	--atk/lv up
+	e2:SetCost(c101107024.cost)
+	e2:SetOperation(c101107024.atkop)
+	c:RegisterEffect(e2)
+	--Search
 	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(101107024,0))
-	e3:SetProperty(EFFECT_FLAG_NO_TURN_RESET)
-	e3:SetCategory(CATEGORY_ATKCHANGE)
+	e3:SetDescription(aux.Stringid(101107024,1))
+	e3:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH+CATEGORY_GRAVE_ACTION)
 	e3:SetType(EFFECT_TYPE_IGNITION)
 	e3:SetRange(LOCATION_MZONE)
 	e3:SetCountLimit(1,101107024)
-	e3:SetCost(c101107024.thcost)
-	e3:SetOperation(c101107024.operation)
+	e3:SetCost(c101107024.cost)
+	e3:SetTarget(c101107024.thtg)
+	e3:SetOperation(c101107024.thop)
 	c:RegisterEffect(e3)
 end
 function c101107024.acop(e,tp,eg,ep,ev,re,r,rp)
@@ -47,9 +45,27 @@ function c101107024.acop(e,tp,eg,ep,ev,re,r,rp)
 		e:GetHandler():AddCounter(0x1,1)
 	end
 end
-function c101107024.thcost(e,tp,eg,ep,ev,re,r,rp,chk)
+function c101107024.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return e:GetHandler():IsCanRemoveCounter(tp,0x1,1,REASON_COST) end
 	e:GetHandler():RemoveCounter(tp,0x1,1,REASON_COST)
+	Duel.Hint(HINT_OPSELECTED,1-tp,e:GetDescription())
+end
+function c101107024.atkop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsFaceup() and c:IsRelateToEffect(e) then
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetValue(1500)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_DISABLE)
+		c:RegisterEffect(e1)
+		local e2=Effect.CreateEffect(c)
+		e2:SetType(EFFECT_TYPE_SINGLE)
+		e2:SetCode(EFFECT_UPDATE_LEVEL)
+		e2:SetValue(1)
+		e2:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_DISABLE)
+		c:RegisterEffect(e2)
+	end
 end
 function c101107024.thfilter(c)
 	return (c:IsSetCard(0xa4) and c:IsType(TYPE_MONSTER) or c:IsCode(40703222)) and c:IsAbleToHand()  
@@ -60,24 +76,9 @@ function c101107024.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
 end
 function c101107024.thop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-	local g=Duel.SelectMatchingCard(tp,c101107024.thfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil)
+	local g=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(c101107024.thfilter),tp,LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil)
 	if g:GetCount()>0 then
 		Duel.SendtoHand(g,nil,REASON_EFFECT)
 		Duel.ConfirmCards(1-tp,g)
-	end
-end
-function c101107024.operation(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	if c:IsFaceup() and c:IsRelateToEffect(e) then
-		local e1=Effect.CreateEffect(c)
-		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetCode(EFFECT_UPDATE_ATTACK)
-		e1:SetValue(1500)
-		c:RegisterEffect(e1)
-		local e2=Effect.CreateEffect(c)
-		e2:SetType(EFFECT_TYPE_SINGLE)
-		e2:SetCode(EFFECT_UPDATE_LEVEL)
-		e2:SetValue(1)
-		c:RegisterEffect(e2)
 	end
 end
