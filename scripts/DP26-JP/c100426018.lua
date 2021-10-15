bfd7fd46474a341d4b1fe2c311012b0cd4fb76d0
Mercury233 <me@mercury233.me>
2021-10-15 09:40:29 +0800

    Update c100426018.lua

--- scripts/DP26-JP/c100426018.lua
+++ scripts/DP26-JP/c100426018.lua
@@ -5,12 +5,12 @@ function c100426018.initial_effect(c)
 	aux.AddCodeList(c,22702055)
 	--SpecialSummon
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(100426018,1))
+	e1:SetDescription(aux.Stringid(100426018,0))
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOHAND+CATEGORY_DESTROY)
 	e1:SetType(EFFECT_TYPE_QUICK_O)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetRange(LOCATION_HAND)
-	e1:SetHintTiming(0,TIMING_BATTLE_START)
+	e1:SetHintTiming(0,TIMINGS_CHECK_MONSTER+TIMING_END_PHASE)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetCountLimit(1,100426018)
 	e1:SetCondition(c100426018.spcon)
@@ -19,7 +19,7 @@ function c100426018.initial_effect(c)
 	c:RegisterEffect(e1)
 	--NegateAttack
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(100426018,0))
+	e2:SetDescription(aux.Stringid(100426018,1))
 	e2:SetType(EFFECT_TYPE_QUICK_O)
 	e2:SetRange(LOCATION_MZONE)
 	e2:SetCode(EVENT_ATTACK_ANNOUNCE)
@@ -32,28 +32,34 @@ end
 function c100426018.spcon(e,tp,eg,ep,ev,re,r,rp)
 	return Duel.IsEnvironment(22702055)
 end
-function c100426018.sptg(e,tp,eg,ep,ev,re,r,rp,chkchkc)
+function c100426018.thfilter(c)
+	return c:IsFaceup() and not c:IsCode(100426018) and c:IsAttribute(ATTRIBUTE_WATER) and c:IsAbleToHand()
+end
+function c100426018.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return false end
-	if chk==0 then return Duel.IsExistingTarget(c100426018.thfilter,tp,LOCATION_MZONE,0,1,nil) and Duel.IsExistingTarget(aux.TRUE,tp,0,LOCATION_MZONE,1,nil) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+	if chk==0 then return Duel.IsExistingTarget(c100426018.thfilter,tp,LOCATION_MZONE,0,1,nil)
+		and Duel.IsExistingTarget(aux.TRUE,tp,0,LOCATION_MZONE,1,nil)
+		and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
 		and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RTOHAND)
 	local g1=Duel.SelectTarget(tp,c100426018.thfilter,tp,LOCATION_MZONE,0,1,1,nil)
 	e:SetLabelObject(g1:GetFirst())
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
 	local g2=Duel.SelectTarget(tp,aux.TRUE,tp,0,LOCATION_MZONE,1,1,nil)
-	Duel.SetOperationInfo(0,CATEGORY_POSITION,g1,1,0,0)
-	Duel.SetOperationInfo(0,CATEGORY_DISABLE,g2,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g1,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g2,1,0,0)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
 end
 function c100426018.spop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
+	if not c:IsRelateToEffect(e) then return end
 	local hc=e:GetLabelObject()
 	local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS)
-	if not c:IsRelateToEffect(e) then return end
 	if Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)~=0 then
 		local tc=g:GetFirst()
 		if tc==hc then tc=g:GetNext() end
-		if hc:IsRelateToEffect(e) and hc:IsControler(tp) and Duel.SendtoHand(hc,nil,REASON_EFFECT)~=0
+		if hc:IsRelateToEffect(e) and hc:IsControler(tp)
+			and Duel.SendtoHand(hc,nil,REASON_EFFECT)~=0 and hc:IsLocation(LOCATION_HAND)
 			and tc:IsRelateToEffect(e) and tc:IsControler(1-tp) then
 			Duel.Destroy(tc,REASON_EFFECT)
 		end
