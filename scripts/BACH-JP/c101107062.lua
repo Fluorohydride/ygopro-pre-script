b6c7434b453e774570785ce856d3aa2bdf2a6f88
Mercury233 <me@mercury233.me>
2021-10-17 11:20:32 +0800

    Update c101107062.lua

--- scripts/BACH-JP/c101107062.lua
+++ scripts/BACH-JP/c101107062.lua
@@ -8,24 +8,24 @@ function c101107062.initial_effect(c)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCountLimit(1,101107062+EFFECT_COUNT_CODE_OATH)
 	e1:SetTarget(c101107062.target)
 	e1:SetOperation(c101107062.activate)
 	c:RegisterEffect(e1)
+end
+function c101107062.filter(c,e,tp)
+	return c:IsFaceup() and c:GetOriginalLevel()>1 and Duel.GetMZoneCount(tp,c)>0
+		and Duel.IsExistingMatchingCard(c101107062.filter2,tp,LOCATION_DECK,0,1,nil,e,tp,c)
 end
 function c101107062.filter2(c,e,tp,tc)
 	return c:IsLevelBelow(tc:GetOriginalLevel()-1)
 		and not c:IsRace(tc:GetOriginalRace())
 		and not c:IsAttribute(tc:GetOriginalAttribute())
-		and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
-end
-function c101107062.filter(c,e,tp)
-	return Duel.IsExistingMatchingCard(c101107062.filter2,tp,LOCATION_DECK,0,1,nil,e,tp,c)
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP_DEFENSE)
 end
 function c101107062.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp)
-		and c101107062.filter(chkc,e,tp) end
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingTarget(c101107062.filter,tp,LOCATION_MZONE,0,1,nil,e,tp) end
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and c101107062.filter(chkc,e,tp) end
+	if chk==0 then return Duel.IsExistingTarget(c101107062.filter,tp,LOCATION_MZONE,0,1,nil,e,tp) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
 	local g=Duel.SelectTarget(tp,c101107062.filter,tp,LOCATION_MZONE,0,1,1,nil,e,tp)
 	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
@@ -34,12 +34,12 @@ end
 function c101107062.activate(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local tc=Duel.GetFirstTarget()
-	if tc:IsRelateToEffect(e) and Duel.Destroy(tc)>0 then
+	if tc:IsRelateToEffect(e) and Duel.Destroy(tc,REASON_EFFECT)>0 then
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 		local g=Duel.SelectMatchingCard(tp,c101107062.filter2,tp,LOCATION_DECK,0,1,1,nil,e,tp,tc)
 		if #g>0 then
 			local sc=g:GetFirst()
-			if Duel.SpecialSummonStep(sc,0,tp,tp,false,false,POS_FACEUP) then
+			if Duel.SpecialSummonStep(sc,0,tp,tp,false,false,POS_FACEUP_DEFENSE) then
 				local e1=Effect.CreateEffect(c)
 				e1:SetType(EFFECT_TYPE_SINGLE)
 				e1:SetCode(EFFECT_DISABLE)
