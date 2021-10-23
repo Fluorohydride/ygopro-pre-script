309d632ff29afff36ab54e46ab7ec24fbf083ac0
mercury233 <me@mercury233.me>
2021-10-23 15:24:31 +0800

    update 死者所生

--- scripts/BACH-JP/c101107077.lua
+++ scripts/BACH-JP/c101107077.lua
@@ -1,8 +1,6 @@
 --死者所生
 --
---Script by JoyJ
---not fully implemented
-SUMTYPE_MONSTER_REBORN = 83764718
+--Script by JoyJ & mercury233
 function c101107077.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
@@ -15,20 +13,21 @@ function c101107077.initial_effect(c)
 	e1:SetTarget(c101107077.target)
 	e1:SetOperation(c101107077.activate)
 	c:RegisterEffect(e1)
-	if not c101107077.last_turn then
-		c101107077.last_turn=-1
+	if not c101107077.global_check then
+		c101107077.globle_check=true
 		local ge1=Effect.CreateEffect(c)
 		ge1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 		ge1:SetCode(EVENT_BATTLE_DESTROYED)
-		ge1:SetOperation(c101107077.checkop1)
+		ge1:SetOperation(c101107077.checkop)
 		Duel.RegisterEffect(ge1,0)
 	end
 end
-function c101107077.checkop1(e,tp,eg,ep,ev,re,r,rp)
-	c101107077.last_turn=Duel.GetTurnCount()
+SUMMON_VALUE_MONSTER_REBORN=0x13
+function c101107077.checkop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.RegisterFlagEffect(0,101107077,RESET_PHASE+PHASE_END,0,1)
 end
 function c101107077.condition(e,tp,eg,ep,ev,re,r,rp)
-	return c101107077.last_turn==Duel.GetTurnCount()
+	return Duel.GetFlagEffect(0,101107077)~=0
 end
 function c101107077.cfilter(c)
 	return c:IsCode(83764718) and c:IsAbleToGraveAsCost()
@@ -40,18 +39,18 @@ function c101107077.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.SendtoGrave(g,REASON_COST)
 end
 function c101107077.tgfilter(c,e,tp)
-	return c:IsCanBeSpecialSummoned(e,SUMTYPE_MONSTER_REBORN,tp,false,false)
+	return c:IsCanBeSpecialSummoned(e,SUMMON_VALUE_MONSTER_REBORN,tp,false,false)
 end
 function c101107077.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return c101107077.tgfilter(chkc,e,tp) and chkc:IsLocation(LOCATION_GRAVE) end
 	if chk==0 then return Duel.IsExistingTarget(c101107077.tgfilter,tp,LOCATION_GRAVE,LOCATION_GRAVE,1,nil,e,tp) and Duel.GetMZoneCount(tp)>0 end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectMatchingCard(tp,c101107077.tgfilter,tp,LOCATION_GRAVE,LOCATION_GRAVE,1,1,nil,e,tp)
+	local g=Duel.SelectTarget(tp,c101107077.tgfilter,tp,LOCATION_GRAVE,LOCATION_GRAVE,1,1,nil,e,tp)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
 end
-function c101107077.operation(e,tp,eg,ep,ev,re,r,rp)
+function c101107077.activate(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
 	if tc:IsRelateToEffect(e) then
-		Duel.SpecialSummon(tc,SUMTYPE_MONSTER_REBORN,tp,tp,false,false,POS_FACEUP)
+		Duel.SpecialSummon(tc,SUMMON_VALUE_MONSTER_REBORN,tp,tp,false,false,POS_FACEUP)
 	end
 end
