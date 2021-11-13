c455659278866321b8e82b00d92005aeaeafa076
Mercury233 <me@mercury233.me>
2021-11-13 11:45:40 +0800

    Update and rename c25573115.lua to c101106084.lua

--- scripts/BODE-EN/c101106084.lua
+++ scripts/BODE-EN/c101106084.lua
@@ -1,28 +1,28 @@
 --Apex Predation
 --Scripted by: XGlitchy30
-function c25573115.initial_effect(c)
+function c101106084.initial_effect(c)
 	--activate
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(25573115,0))
+	e1:SetDescription(aux.Stringid(101106084,0))
 	e1:SetCategory(CATEGORY_DESTROY)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetCountLimit(1,25573115+EFFECT_COUNT_CODE_OATH)
+	e1:SetCountLimit(1,101106084+EFFECT_COUNT_CODE_OATH)
 	e1:SetHintTiming(0,TIMINGS_CHECK_MONSTER+TIMING_END_PHASE)
-	e1:SetCondition(c25573115.condition)
-	e1:SetTarget(c25573115.target)
-	e1:SetOperation(c25573115.activate)
+	e1:SetCondition(c101106084.condition)
+	e1:SetTarget(c101106084.target)
+	e1:SetOperation(c101106084.activate)
 	c:RegisterEffect(e1)
 end
-function c25573115.condition(e,tp,eg,ep,ev,re,r,rp)
+function c101106084.condition(e,tp,eg,ep,ev,re,r,rp)
 	return Duel.IsExistingMatchingCard(Card.IsSummonType,tp,LOCATION_MZONE,0,1,nil,SUMMON_TYPE_NORMAL)
 end
-function c25573115.target(e,tp,eg,ep,ev,re,r,rp,chk)
+function c101106084.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	local g=Duel.GetMatchingGroup(Card.IsSummonType,tp,LOCATION_MZONE,LOCATION_MZONE,nil,SUMMON_TYPE_SPECIAL)
 	if chk==0 then return #g>0 end
 	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,#g,0,0)
 end
-function c25573115.activate(e,tp,eg,ep,ev,re,r,rp)
+function c101106084.activate(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if e:IsHasType(EFFECT_TYPE_ACTIVATE) then
 		local ct=(Duel.GetTurnPlayer()==tp) and 2 or 1
