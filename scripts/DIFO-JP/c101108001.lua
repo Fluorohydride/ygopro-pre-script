b705d5bd611347d310090cf0df6c306f8e243253
wzh31891579 <31891579@qq.com>
2021-11-15 20:28:48 +0800

    add DIFO cards

---
+++ scripts/DIFO-JP/c101108001.lua
@@ -0,0 +1,106 @@
+--EMジェントルード
+--
+--Script by Trishula9
+function c101108001.initial_effect(c)
+	aux.EnablePendulumAttribute(c)
+	--search
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_SEARCH+CATEGORY_TOHAND)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_PZONE)
+	e1:SetCountLimit(1,101108001)
+	e1:SetCondition(c101108001.scon)
+	e1:SetTarget(c101108001.stg)
+	e1:SetOperation(c101108001.sop)
+	c:RegisterEffect(e1)
+	--pendulum set
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_DESTROYED)
+	e2:SetProperty(EFFECT_FLAG_DELAY)
+	e2:SetCountLimit(1,101108001+100)
+	e2:SetTarget(c101108001.pentg)
+	e2:SetOperation(c101108001.penop)
+	c:RegisterEffect(e2)
+	--to hand
+	local e3=Effect.CreateEffect(c)
+	e3:SetCategory(CATEGORY_TOHAND)
+	e3:SetType(EFFECT_TYPE_IGNITION)
+	e3:SetRange(LOCATION_EXTRA)
+	e3:SetCountLimit(1,101108001+100)
+	e3:SetCondition(c101108001.thcon)
+	e3:SetCost(c101108001.thcost)
+	e3:SetTarget(c101108001.thtg)
+	e3:SetOperation(c101108001.thop)
+	c:RegisterEffect(e3)
+end
+function c101108001.cfilter(c)
+	return c:IsCode(101108002)
+end
+function c101108001.gfilter(c)
+	return c:IsFaceup() and c:IsType(TYPE_PENDULUM)
+end
+function c101108001.scon(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetFieldGroup(tp,LOCATION_MZONE,0)
+	return Duel.IsExistingMatchingCard(c101108001.cfilter,tp,LOCATION_PZONE,0,1,e:GetHandler())
+		and (g:GetCount()==0 or g:FilterCount(c101108001.gfilter,nil)==g:GetCount())
+end
+function c101108001.sfilter(c)
+	return c:IsSetCard(0x99) and c:IsAbleToHand()
+end
+function c101108001.stg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c101108001.sfilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function c101108001.sop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,c101108001.sfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if g:GetCount()>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end
+function c101108001.penfilter(c)
+	return c:IsSetCard(0x9f) and c:IsType(TYPE_PENDULUM) and not c:IsCode(101108001) and not c:IsForbidden()
+end
+function c101108001.pentg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c101108001.penfilter,tp,LOCATION_DECK,0,1,nil) end
+end
+function c101108001.penop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOFIELD)
+	local g=Duel.SelectMatchingCard(tp,c101108001.penfilter,tp,LOCATION_DECK,0,1,1,nil)
+	local tc=g:GetFirst()
+	if tc then
+		Duel.MoveToField(tc,tp,tp,LOCATION_PZONE,POS_FACEUP,true)
+	end
+end
+function c101108001.costfilter(c)
+	return c:IsType(TYPE_PENDULUM) and c:IsDiscardable()
+end
+function c101108001.thcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsFaceup()
+end
+function c101108001.thcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c101108001.costfilter,tp,LOCATION_HAND,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DISCARD)
+	local g=Duel.SelectMatchingCard(tp,c101108001.costfilter,tp,LOCATION_HAND,0,1,1,nil)
+	Duel.SendtoGrave(g,REASON_COST)
+end
+function c101108001.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsAbleToHand() end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,e:GetHandler(),1,0,0)
+end
+function c101108001.thop(e,tp,eg,ep,ev,re,r,rp)
+	if e:GetHandler():IsRelateToEffect(e) then
+		Duel.SendtoHand(e:GetHandler(),nil,REASON_EFFECT)
+		local g=Duel.GetMatchingGroup(c101108001.thfilter,tp,LOCATION_PZONE,0,nil)
+		if g:GetCount()>0 and Duel.SelectYesNo(tp,aux.Stringid(101108001,0)) then
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RTOHAND)
+			local sg=g:Select(tp,1,1,nil)
+			Duel.SendtoHand(sg,nil,REASON_EFFECT)
+		end
+	end
+end
+function c101108001.thfilter(c)
+	return c:IsSetCard(0x9f,0x99) and c:IsAbleToHand()
+end
