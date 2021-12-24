b79c90bf987f4799dd909c56228162d1ed780133
Mercury233 <me@mercury233.me>
2021-12-24 09:13:35 +0800

    Update c101108018.lua

--- scripts/DIFO-JP/c101108018.lua
+++ scripts/DIFO-JP/c101108018.lua
@@ -20,7 +20,7 @@ function c101108018.initial_effect(c)
 	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e2:SetProperty(EFFECT_FLAG_DELAY)
 	e2:SetCode(EVENT_BE_MATERIAL)
-	e2:SetCountLimit(1,101108018)
+	e2:SetCountLimit(1,101108018+100)
 	e2:SetCondition(c101108018.thcon)
 	e2:SetTarget(c101108018.thtg)
 	e2:SetOperation(c101108018.thop)
@@ -36,7 +36,7 @@ end
 function c101108018.fustg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then
 		local chkf=tp
-		local mg1=Duel.GetFustionMaterial(tp)
+		local mg1=Duel.GetFusionMaterial(tp)
 		local res=Duel.IsExistingMatchingCard(c101108018.fusfilter2,tp,LOCATION_EXTRA,0,1,nil,e,tp,mg1,nil,chkf)
 		if not res then
 			local ce=Duel.GetChainMaterial(tp)
@@ -53,7 +53,7 @@ function c101108018.fustg(e,tp,eg,ep,ev,re,r,rp,chk)
 end
 function c101108018.fusop(e,tp,eg,ep,ev,re,r,rp)
 	local chkf=tp
-	local mg1=Duel.GetFustionMaterial(tp):Filter(c101108018.fusfilter1,nil,e)
+	local mg1=Duel.GetFusionMaterial(tp):Filter(c101108018.fusfilter1,nil,e)
 	local sg1=Duel.GetMatchingGroup(c101108018.fusfilter2,tp,LOCATION_EXTRA,0,nil,e,tp,mg1,nil,chkf)
 	local mg2=nil
 	local sg2=nil
@@ -71,7 +71,7 @@ function c101108018.fusop(e,tp,eg,ep,ev,re,r,rp)
 		local tg=sg:Select(tp,1,1,nil)
 		local tc=tg:GetFirst()
 		if sg1:IsContains(tc) and (sg2==nil or not sg2:IsContains(tc) or not Duel.SelectYesNo(tp,ce:GetDescription())) then
-			local mat1=Duel.SelectFustionMaterial(tp,tc,mg1,nil,chkf)
+			local mat1=Duel.SelectFusionMaterial(tp,tc,mg1,nil,chkf)
 			tc:SetMaterial(mat1)
 			Duel.SendtoGrave(mat1,REASON_EFFECT+REASON_MATERIAL+REASON_FUSION)
 			Duel.BreakEffect()
@@ -86,11 +86,10 @@ function c101108018.fusop(e,tp,eg,ep,ev,re,r,rp)
 end
 function c101108018.thcon(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	return c:IsPreviousLocation(LOCATION_DECK+LOCATION_EXTRA+LOCATION_HAND+LOCATION_ONFIELD+LOCATION_REMOVED)
-		and c:IsLocation(LOCATION_GRAVE+LOCATION_EXTRA) and r==REASON_FUSION
+	return (c:IsLocation(LOCATION_GRAVE) or c:IsFaceup() and c:IsLocation(LOCATION_EXTRA)) and r==REASON_FUSION
 end
 function c101108018.thfilter(c)
-	return c:IsAttribute(ATTRIBUTE_DARK) and c:IsType(TYPE_PENDULUM) and not c:IsCode(101108018) and c:IsAbleToHand()
+	return c:IsFaceup() and c:IsAttribute(ATTRIBUTE_DARK) and c:IsType(TYPE_PENDULUM) and not c:IsCode(101108018) and c:IsAbleToHand()
 end
 function c101108018.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(c101108018.thfilter,tp,LOCATION_EXTRA,0,1,nil) end
@@ -101,6 +100,5 @@ function c101108018.thop(e,tp,eg,ep,ev,re,r,rp)
 	local g=Duel.SelectMatchingCard(tp,c101108018.thfilter,tp,LOCATION_EXTRA,0,1,1,nil)
 	if g:GetCount()>0 then
 		Duel.SendtoHand(g,nil,REASON_EFFECT)
-		Duel.ConfirmCards(1-tp,g)
 	end
 end
