2038bce4c3162d312c6b24cfc0c8beea576af025
Mercury233 <me@mercury233.me>
2021-12-24 10:28:23 +0800

    Update c101108037.lua

--- scripts/DIFO-JP/c101108037.lua
+++ scripts/DIFO-JP/c101108037.lua
@@ -10,7 +10,7 @@ function c101108037.initial_effect(c)
 	e1:SetDescription(aux.Stringid(101108037,0))
 	e1:SetCategory(CATEGORY_SEARCH+CATEGORY_TOHAND)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e1:SetProperty(EFFECT_TYPE_FLAG)
+	e1:SetProperty(EFFECT_FLAG_DELAY)
 	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
 	e1:SetCountLimit(1,101108037)
 	e1:SetCondition(c101108037.thcon)
@@ -23,8 +23,8 @@ function c101108037.initial_effect(c)
 	e2:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_RELEASE)
 	e2:SetType(EFFECT_TYPE_IGNITION)
 	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
-	e2:SetRamge(LOCATION_MZONE)
-	e2:SetCountLimit(1,101108037)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1,101108037+100)
 	e2:SetTarget(c101108037.sptg)
 	e2:SetOperation(c101108037.spop)
 	c:RegisterEffect(e2)
@@ -33,11 +33,11 @@ function c101108037.thcon(e,tp,eg,ep,ev,re,r,rp)
 	return e:GetHandler():IsSummonType(SUMMON_TYPE_FUSION)
 end
 function c101108037.thfilter(c)
-	return c:IsAbleToHand() and c:IsSetCard(0x10f3) and (not c:IsType(TYPE_MONSTER) or c:IsSetCard(0x10f3)) and (c:IsFaceup() or not c:IsLocation(LOCATION_EXTRA))
+	return c:IsAbleToHand() and c:IsSetCard(0xf3) and (not c:IsType(TYPE_MONSTER) or c:IsSetCard(0x10f3)) and (c:IsFaceup() or not c:IsLocation(LOCATION_EXTRA))
 end
 function c101108037.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(c101108037.thfilter,tp,LOCATION_DECK+LOCATION_GRAVE+LOCATION_EXTRA,0,1,nil) end
-	Duel.SetOperationInfo(0,CATEGORY_SEARCH+CATEGORY_TOHAND,1,nil,tp,LOCATION_DECK+LOCATION_GRAVE+LOCATION_EXTRA)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,1,nil,tp,LOCATION_DECK+LOCATION_GRAVE+LOCATION_EXTRA)
 end
 function c101108037.thop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
@@ -48,14 +48,7 @@ function c101108037.thop(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 function c101108037.relfilter(c,tp)
-	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
-	if c:GetControler()==tp then
-		return c:IsReleasableByEffect() and ((c:GetSequence()>4 and ft>=1) or (c:GetSequence()<=4 and (ft+1)>=1))
-	elseif c:GetControler()==1-tp then
-		return c:IsFaceup() and c:GetCounter(0x1041)>0 and c:IsReleasableByEffect() and ft>=1
-	else
-		return false
-	end
+	return c:IsReleasableByEffect() and (c:GetCounter(0x1041)>0 or c:IsControler(tp)) and Duel.GetMZoneCount(tp,c)>0
 end
 function c101108037.spfilter(c,e,tp)
 	return c:IsSetCard(0x10f3) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
@@ -64,13 +57,13 @@ function c101108037.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_MZONE) and c101108037.relfilter(chkc,tp) end
 	if chk==0 then return Duel.IsExistingTarget(c101108037.relfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil,tp)
 		and Duel.IsExistingMatchingCard(c101108037.spfilter,tp,LOCATION_DECK,0,1,nil,e,tp) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
-	local g=Duel.SelectTarget(tp,c101108037.relfilter,tp,0,LOCATION_MZONE,1,1,nil,tp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
+	local g=Duel.SelectTarget(tp,c101108037.relfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil,tp)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
 end
 function c101108037.spop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
-	if tc and tc:IsRelateToEffect(e) and not tc:IsImmuneToEffect(e)
+	if tc:IsRelateToEffect(e) and not tc:IsImmuneToEffect(e)
 		and Duel.Release(tc,REASON_EFFECT)>0 and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 then
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 		local g=Duel.SelectMatchingCard(tp,c101108037.spfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
