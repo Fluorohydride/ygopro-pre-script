0fad50ba2d4e6072d83517654ed056e5d48c198e
Mercury233 <me@mercury233.me>
2021-12-24 15:03:11 +0800

    Update c101108046.lua

--- scripts/DIFO-JP/c101108046.lua
+++ scripts/DIFO-JP/c101108046.lua
@@ -1,6 +1,6 @@
 --エクンシスターズ・マニフィカ
 --
---Script by Trishula9
+--Script by Trishula9 & mercury233
 function c101108046.initial_effect(c)
 	--xyz summon
 	c:EnableReviveLimit()
@@ -10,8 +10,7 @@ function c101108046.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
 	e1:SetCode(EFFECT_SPSUMMON_CONDITION)
-	--e1:SetValue(aux.xyzlimit)
-	e1:SetValue(c101108046.xyzlimit)
+	e1:SetValue(aux.FALSE)
 	c:RegisterEffect(e1)
 	--extra attack
 	local e2=Effect.CreateEffect(c)
@@ -47,9 +46,6 @@ end
 function c101108046.mfilter(c)
 	return c:IsSetCard(0x172) and c:IsRank(4)
 end
-function c101108046.xyzlimit(e,se,sp,st)
-	return st&SUMMON_TYPE_XYZ==SUMMON_TYPE_XYZ and not se:IsActiveType(TYPE_MONSTER+TYPE_SPELL+TYPE_TRAP)
-end
 function c101108046.rmcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return e:GetHandler():CheckRemoveOverlayCard(tp,1,REASON_COST) end
 	e:GetHandler():RemoveOverlayCard(tp,1,1,REASON_COST)
@@ -70,25 +66,24 @@ end
 function c101108046.spcon(e,tp,eg,ep,ev,re,r,rp)
 	return rp==1-tp and not e:GetHandler():IsStatus(STATUS_BATTLE_DESTROYED)
 end
-function c101108046.spfilter(c,e,tp,mc)
-	return c:IsType(TYPE_XYZ) and c:IsAbleToExtra() and mc:IsCanBeXyzMaterial(c)
-		and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_XYZ,tp,false,false) and Duel.GetLocationCountFromEx(tp,tp,mc,c)>0
+function c101108046.toexfilter(c)
+	return c:IsType(TYPE_XYZ) and c:IsAbleToExtra()
 end
 function c101108046.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
 	local g=c:GetOverlayGroup()
-	if chk==0 then return aux.MustMaterialCheck(c,tp,EFFECT_MUST_BE_XMATERIAL)
-		and g:IsExists(c101108046.spfilter,1,nil,e,tp,c) end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
+	if chk==0 then return g:IsExists(c101108046.toexfilter,1,nil) end
 end
 function c101108046.spop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local g=c:GetOverlayGroup()
-	if not aux.MustMaterialCheck(c,tp,EFFECT_MUST_BE_XMATERIAL) or not g:IsExists(c101108046.spfilter,1,nil,e,tp,c) then return end
-	if c:IsFaceup() and c:IsRelateToEffect(e) and c:IsControler(tp) and not c:IsImmuneToEffect(e) then
+	if c:IsRelateToEffect(e) then
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
-		local sc=g:Filter(c101108046.spfilter,nil,e,tp,c):Select(tp,1,1,nil):GetFirst()
-		if sc and Duel.SendtoDeck(sc,nil,0,REASON_EFFECT) and sc:IsLocation(LOCATION_EXTRA)
+		local sc=g:FilterSelect(tp,c101108046.toexfilter,1,1,nil):GetFirst()
+		if sc and Duel.SendtoDeck(sc,nil,0,REASON_EFFECT)>0 and sc:IsLocation(LOCATION_EXTRA)
+			and c:IsFaceup() and c:IsControler(tp) and not c:IsImmuneToEffect(e)
+			and aux.MustMaterialCheck(c,tp,EFFECT_MUST_BE_XMATERIAL) and c:IsCanBeXyzMaterial(sc)
+			and sc:IsCanBeSpecialSummoned(e,SUMMON_TYPE_XYZ,tp,false,false) and Duel.GetLocationCountFromEx(tp,tp,c,sc)>0
 			and Duel.SelectYesNo(tp,aux.Stringid(101108046,2)) then
 			Duel.BreakEffect()
 			local mg=c:GetOverlayGroup()
