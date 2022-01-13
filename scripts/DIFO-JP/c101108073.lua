6040cbb1ab905534d416ede972bcb86508ed2118
Mercury233 <me@mercury233.me>
2022-01-13 10:42:08 +0800

    Update c101108073.lua

--- scripts/DIFO-JP/c101108073.lua
+++ scripts/DIFO-JP/c101108073.lua
@@ -8,6 +8,7 @@ function c101108073.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetCountLimit(1,101108073)
+	e1:SetHintTiming(0,TIMINGS_CHECK_MONSTER+TIMING_END_PHASE)
 	e1:SetTarget(c101108073.target)
 	e1:SetOperation(c101108073.activate)
 	c:RegisterEffect(e1)
@@ -19,6 +20,7 @@ function c101108073.initial_effect(c)
 	e2:SetRange(LOCATION_GRAVE)
 	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e2:SetCountLimit(1,101108073)
+	e2:SetHintTiming(0,TIMING_END_PHASE)
 	e2:SetCost(aux.bfgcost)
 	e2:SetTarget(c101108073.thtg)
 	e2:SetOperation(c101108073.thop)
@@ -87,8 +89,8 @@ function c101108073.thop(e,tp,eg,ep,ev,re,r,rp)
 	local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS)
 	if g:FilterCount(Card.IsRelateToEffect,nil,e)<2 or not g:IsExists(c101108073.fcheck,1,nil,g) then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-	local sg=g:Filter(c101108073.fcheck,nil,g):Select(tp,1,1,nil)
-	if sg and Duel.SendtoHand(sg,nil,REASON_EFFECT) and sg:GetFirst():IsLocation(LOCATION_HAND) then
+	local sg=g:FilterSelect(tp,c101108073.fcheck,1,1,nil,g)
+	if Duel.SendtoHand(sg,nil,REASON_EFFECT)>0 and sg:GetFirst():IsLocation(LOCATION_HAND) then
 		Duel.SendtoDeck(g-sg,nil,SEQ_DECKBOTTOM,REASON_EFFECT)
 	end
 end
