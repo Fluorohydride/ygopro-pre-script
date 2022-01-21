166ff905f7759417f12f2b84f63b3450006ad262
Mercury233 <me@mercury233.me>
2022-01-21 16:52:30 +0800

    Update c101108068.lua

--- scripts/DIFO-JP/c101108068.lua
+++ scripts/DIFO-JP/c101108068.lua
@@ -4,7 +4,6 @@
 function c101108068.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_DRAW)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
 	e1:SetCode(EVENT_FREE_CHAIN)
@@ -15,20 +14,36 @@ function c101108068.initial_effect(c)
 	c:RegisterEffect(e1)
 end
 function c101108068.condition(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.GetCurrentChain()>=2
+	return Duel.GetCurrentChain()>0 and Duel.CheckChainUniqueness()
 end
 function c101108068.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	local cl=Duel.GetCurrentChain()
-	if chk==0 then return cl>=2 and Duel.GetFieldGroupCount(tp,LOCATION_DECK,0)>=cl
-		or cl>=3 and Duel.IsPlayerCanDiscardDeck(tp,1)
-		or cl>=4 and Duel.IsPlayerCanDraw(tp,1)
+	if chk==0 then return Duel.GetFieldGroupCount(tp,LOCATION_DECK,0)>=cl
+		and (cl<3 or Duel.IsPlayerCanDiscardDeck(tp,1))
+		and (cl<4 or Duel.IsPlayerCanDraw(tp,1))
 	end
-	if cl>=3 then Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_DECK) end
-	if cl>=4 then Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1) end
+	local cat=0
+	if cl>=3 then
+		cat=cat|CATEGORY_TOGRAVE
+		Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_DECK)
+	end
+	if cl>=4 then
+		cat=cat|CATEGORY_DRAW
+		Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
+	end
+	e:SetCategory(cat)
 end
 function c101108068.activate(e,tp,eg,ep,ev,re,r,rp)
 	local cl=Duel.GetCurrentChain()
-	if cl>=2 then Duel.SortDecktop(tp,tp,cl) end
-	if cl>=3 then Duel.SendtoGrave(Duel.GetDecktopGroup(tp,1),REASON_EFFECT) end
-	if cl>=4 then Duel.Draw(tp,1,REASON_EFFECT) end
+	if cl>=2 then
+		Duel.SortDecktop(tp,tp,cl)
+	end
+	if cl>=3 then
+		Duel.BreakEffect()
+		Duel.SendtoGrave(Duel.GetDecktopGroup(tp,1),REASON_EFFECT)
+	end
+	if cl>=4 then
+		Duel.BreakEffect()
+		Duel.Draw(tp,1,REASON_EFFECT)
+	end
 end
