9ee6fa6617b4da62af57ee776e87329ad58de152
Mercury233 <me@mercury233.me>
2022-01-21 11:04:37 +0800

    Update c100418205.lua

--- scripts/DBTM-JP/c100418205.lua
+++ scripts/DBTM-JP/c100418205.lua
@@ -61,7 +61,7 @@ function c100418205.splimit(e,c)
 	return not c:IsSetCard(0x27c) and not c:IsLocation(LOCATION_EXTRA)
 end
 function c100418205.seqcon(e,tp,eg,ep,ev,re,r,rp)
-	return e:GetHandler():GetSummonType()==SUMMON_TYPE_SPECIAL
+	return e:GetHandler():IsSummonType(SUMMON_TYPE_SPECIAL)
 end
 function c100418205.filter(c)
 	local seq=c:GetSequence()
@@ -86,7 +86,7 @@ function c100418205.seqop(e,tp,eg,ep,ev,re,r,rp)
 	if seq<4 and Duel.CheckLocation(tp,LOCATION_MZONE,seq+1) then flag=flag|(1<<(seq+1)) end
 	if flag==0 then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOZONE)
-	local s=Duel.SelectDisableField(tp,1,LOCATION_MZONE,0,~flag)
+	local s=Duel.SelectField(tp,1,LOCATION_MZONE,0,~flag)
 	local nseq=math.log(s,2)
 	Duel.MoveSequence(tc,nseq)
 end
