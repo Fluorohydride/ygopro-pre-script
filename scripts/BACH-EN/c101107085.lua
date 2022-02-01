ac00e883e8e0ab1373bf3462397b177c7ce34a8f
HKunogi <hima.kunogi@hotmail.com>
2022-02-02 02:12:10 +0900

    Set description of effect

--- scripts/BACH-EN/c101107085.lua
+++ scripts/BACH-EN/c101107085.lua
@@ -2,6 +2,7 @@
 local s,id=GetID()
 function s.initial_effect(c)
 	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_POSITION)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
