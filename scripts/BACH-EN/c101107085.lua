3e973d83ab288b0b3fd6853c7dac4b0bcd664af0
HKunogi <hima.kunogi@hotmail.com>
2022-02-02 01:01:07 +0900

    Update BACH-EN Sol and Luna

--- scripts/BACH-EN/c101107085.lua
+++ scripts/BACH-EN/c101107085.lua
@@ -12,19 +12,22 @@ function s.initial_effect(c)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 end
+function s.filter(c)
+	return (c:IsFaceup() and c:IsCanTurnSet()) or c:IsFacedown()
+end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsOnField() and chkc:IsCanChangePosition() end
-	if chk==0 then return Duel.IsExistingTarget(Card.IsCanChangePosition,tp,LOCATION_MZONE,0,1,nil) and Duel.IsExistingTarget(Card.IsCanChangePosition,tp,0,LOCATION_MZONE,1,nil) end
+	if chk==0 then return Duel.IsExistingTarget(s.filter,tp,LOCATION_MZONE,0,1,nil) and Duel.IsExistingTarget(s.filter,tp,0,LOCATION_MZONE,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_POSCHANGE)
-	local g1=Duel.SelectTarget(tp,Card.IsCanChangePosition,tp,LOCATION_MZONE,0,1,1,nil)
+	local g1=Duel.SelectTarget(tp,s.filter,tp,LOCATION_MZONE,0,1,1,nil)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_POSCHANGE)
-	local g2=Duel.SelectTarget(tp,Card.IsCanChangePosition,tp,0,LOCATION_MZONE,1,1,nil)
-	Duel.SetOperationInfo(0,CATEGORY_POSITION,g1+g2,2,0,0)
+	local g2=Duel.SelectTarget(tp,s.filter,tp,0,LOCATION_MZONE,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_POSITION,g1+g2:GetFirst(),2,0,0)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	local gs=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS)
 	for c in aux.Next(gs) do
-		if c:IsFacedown() then Duel.ChangePosition(c,POS_FACEUP_DEFENSE)
-		else Duel.ChangePosition(c,POS_FACEDOWN_DEFENSE) end
+		if c:IsRelateToEffect(e) and c:IsLocation(LOCATION_MZONE) then 
+			Duel.ChangePosition(c,c:IsFaceup() and POS_FACEDOWN_DEFENSE or POS_FACEUP_DEFENSE)
+		end
 	end
 end
