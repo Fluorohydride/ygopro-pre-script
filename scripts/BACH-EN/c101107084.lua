47ff07683762b1227dc103a02579916003adf6e5
HKunogi <hima.kunogi@hotmail.com>
2022-02-02 20:43:16 +0900

    Double Casted Caster

---
+++ scripts/BACH-EN/c101107084.lua
@@ -0,0 +1,77 @@
+--The Great Double Casted Caster
+local s,id=GetID()
+function s.initial_effect(c)
+	c:EnableReviveLimit()
+    --fusion eff
+    aux.AddFusionProcCodeRep(c,s.noeff,2,true)
+	--atk buff
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_MATERIAL_CHECK)
+	e1:SetValue(s.atkcalc)
+	c:RegisterEffect(e1)
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e2:SetCondition(s.atkfusonly)
+	e2:SetOperation(s.activate1)
+	e2:SetLabelObject(e1)
+	c:RegisterEffect(e2)
+	--direct atk
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_SINGLE)
+	e3:SetCode(EFFECT_DIRECT_ATTACK)
+	c:RegisterEffect(e3)
+	--sp sum
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(id,1))
+	e4:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e4:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
+	e4:SetCode(EVENT_DESTROYED)
+	e4:SetCountLimit(1,id)
+	e4:SetCondition(s.spdestroyeffbat)
+	e4:SetTarget(s.target2)
+	e4:SetOperation(s.activate2)
+	c:RegisterEffect(e4)
+end
+function s.noeff(c) return not c:IsType(TYPE_EFFECT) end
+function s.atkcalc(e,c)
+	local g=c:GetMaterial()
+	local atk=0
+	for tc in aux.Next(g) do
+		if tc:IsType(TYPE_RITUAL+TYPE_FUSION+TYPE_SYNCHRO+TYPE_XYZ+TYPE_LINK) then
+			atk=atk+tc:GetBaseAttack()
+		end
+	end
+	e:SetLabel(atk)
+end
+function s.atkfusonly(e,tp,eg,ep,ev,re,r,rp) return e:GetHandler():IsSummonType(SUMMON_TYPE_FUSION) end
+function s.activate1(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local atk=e:GetLabelObject():GetLabel()
+	if atk>0 then
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetValue(atk)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_DISABLE)
+		c:RegisterEffect(e1)
+	end
+end
+function s.spdestroyeffbat(e,tp,eg,ep,ev,re,r,rp) return r&(REASON_EFFECT+REASON_BATTLE) end
+function s.filter2(c,e,tp) return s.noeff(c) and c:IsFaceup() and c:IsCanBeSpecialSummoned(e,0,tp,false,false) end
+function s.target2(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE+LOCATION_REMOVED) and chkc:IsControler(tp) and s.filter2(chkc,e,tp) end
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingTarget(s.filter2,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,1,nil,e,tp) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectTarget(tp,s.filter2,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,1,1,nil,e,tp)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
+end
+function s.activate2(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) then
+		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
