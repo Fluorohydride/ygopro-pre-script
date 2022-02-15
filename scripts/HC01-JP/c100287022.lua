85d44ccd50df3b9f0728f19d7b6b879f5a69dbac
Mercury233 <me@mercury233.me>
2022-02-15 10:20:15 +0800

    Update c100287022.lua

--- scripts/HC01-JP/c100287022.lua
+++ scripts/HC01-JP/c100287022.lua
@@ -40,7 +40,8 @@ function c100287022.spfilter(c,e,tp)
 	return c:IsLevelBelow(2) and c:IsType(TYPE_TUNER) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function c100287022.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.IsExistingMatchingCard(c100287022.spfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(c100287022.spfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE)
 end
 function c100287022.spop(e,tp,eg,ep,ev,re,r,rp)
@@ -56,11 +57,11 @@ end
 function c100287022.excostfilter(c,tp)
 	return c:IsAbleToRemoveAsCost() and c:IsHasEffect(84012625,tp)
 end
-function c100287022.chkfilter(c,tp,mc,mg,opchk)
-	return Duel.GetLocationCountFromEx(tp,tp,mc,c)>0
+function c100287022.spcheck(c,tp,rc,mg,opchk)
+	return Duel.GetLocationCountFromEx(tp,tp,rc,c)>0
 		and (opchk or Duel.IsExistingMatchingCard(Card.IsSynchroSummonable,tp,LOCATION_EXTRA,0,1,c,nil,mg))
 end
-function c100287022.scfilter(c,e,tp,mc,b1,b2,tgchk,opchk)
+function c100287022.scfilter(c,e,tp,rc,chkrel,chknotrel,tgchk,opchk)
 	if not (c:IsCode(44508094) and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_SYNCHRO,tp,false,false)) then return false end
 	local mg=Duel.GetMatchingGroup(Card.IsFaceup,tp,LOCATION_MZONE,LOCATION_MZONE,nil)
 	if mg:IsExists(Card.GetHandSynchro,1,nil) then
@@ -69,9 +70,9 @@ function c100287022.scfilter(c,e,tp,mc,b1,b2,tgchk,opchk)
 	end
 	mg:AddCard(c)
 	if tgchk then
-		return c100287022.chkfilter(c,tp,nil,mg,opchk)
+		return c100287022.spcheck(c,tp,nil,mg,opchk)
 	else
-		return (b1 and c100287022.chkfilter(c,tp,mc,mg-mc)) or (b2 and c100287022.chkfilter(c,tp,nil,mg))
+		return (chkrel and c100287022.spcheck(c,tp,rc,mg-rc)) or (chknotrel and c100287022.spcheck(c,tp,nil,mg))
 	end
 end
 function c100287022.sccost(e,tp,eg,ep,ev,re,r,rp,chk)
@@ -81,41 +82,40 @@ function c100287022.sccost(e,tp,eg,ep,ev,re,r,rp,chk)
 	local ect2=aux.ExtraDeckSummonCountLimit and Duel.IsPlayerAffectedByEffect(tp,92345028)
 		and aux.ExtraDeckSummonCountLimit[tp]
 	local g=Duel.GetMatchingGroup(c100287022.excostfilter,tp,LOCATION_GRAVE,0,nil,tp)
-	local b1=c:IsReleasable()
-	local b2=g:GetCount()>0
-	local b3=b1 and Duel.IsExistingMatchingCard(c100287022.scfilter,tp,LOCATION_EXTRA,0,1,nil,e,tp,c,b1,nil)
-	local b4=b2 and Duel.IsExistingMatchingCard(c100287022.scfilter,tp,LOCATION_EXTRA,0,1,nil,e,tp,c,nil,b2)
+	local chkrel=c:IsReleasable()
+	local chknotrel=g:GetCount()>0
+	local b1=chkrel and Duel.IsExistingMatchingCard(c100287022.scfilter,tp,LOCATION_EXTRA,0,1,nil,e,tp,c,chkrel,nil)
+	local b2=chknotrel and Duel.IsExistingMatchingCard(c100287022.scfilter,tp,LOCATION_EXTRA,0,1,nil,e,tp,c,nil,chknotrel)
 	if chk==0 then return Duel.IsPlayerCanSpecialSummonCount(tp,2)
 		and (not ect1 or ect1>1) and (not ect2 or ect2>1) and (b1 or b2)
-		and aux.MustMaterialCheck(nil,tp,EFFECT_MUST_BE_SMATERIAL)
-		and (b3 or b4) end
-	local mg=Group.CreateGroup()
-	local tc=nil
-	if b3 then mg:AddCard(c) end
-	if b4 then mg:Merge(g) end
-	if mg:GetCount()>1 then
+		and aux.MustMaterialCheck(nil,tp,EFFECT_MUST_BE_SMATERIAL) end
+	local rg=Group.CreateGroup()
+	local rc=nil
+	if b1 then rg:AddCard(c) end
+	if b2 then rg:Merge(g) end
+	if rg:GetCount()>1 then
 		Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(84012625,0))
-		tc=mg:Select(tp,1,1,nil):GetFirst()
+		rc=rg:Select(tp,1,1,nil):GetFirst()
 	else
-		tc=mg:GetFirst()
+		rc=rg:GetFirst()
 	end
-	local te=tc:IsHasEffect(84012625,tp)
+	local te=rc:IsHasEffect(84012625,tp)
 	if te then
-		Duel.Remove(tc,POS_FACEUP,REASON_COST+REASON_REPLACE)
+		Duel.Remove(rc,POS_FACEUP,REASON_COST+REASON_REPLACE)
 	else
-		Duel.Release(tc,REASON_COST)
+		Duel.Release(rc,REASON_COST)
 	end
 end
 function c100287022.sctg(e,tp,eg,ep,ev,re,r,rp,chk)
-	local c=e:GetHandler()
-	local ect1=c29724053 and Duel.IsPlayerAffectedByEffect(tp,29724053) and c29724053[tp]
-	local ect2=aux.ExtraDeckSummonCountLimit and Duel.IsPlayerAffectedByEffect(tp,92345028)
-		and aux.ExtraDeckSummonCountLimit[tp]
 	if chk==0 then
 		if e:GetLabel()==100 then
 			e:SetLabel(0)
 			return true
 		end
+		local c=e:GetHandler()
+		local ect1=c29724053 and Duel.IsPlayerAffectedByEffect(tp,29724053) and c29724053[tp]
+		local ect2=aux.ExtraDeckSummonCountLimit and Duel.IsPlayerAffectedByEffect(tp,92345028)
+			and aux.ExtraDeckSummonCountLimit[tp]
 		return Duel.IsPlayerCanSpecialSummonCount(tp,2)
 			and (not ect1 or ect1>1) and (not ect2 or ect2>1)
 			and aux.MustMaterialCheck(nil,tp,EFFECT_MUST_BE_SMATERIAL)
@@ -129,7 +129,7 @@ function c100287022.scop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 	local g=Duel.SelectMatchingCard(tp,c100287022.scfilter,tp,LOCATION_EXTRA,0,1,1,nil,e,tp,c,nil,nil,true,true)
 	local tc=g:GetFirst()
-	local res=nil
+	local res=false
 	if tc and Duel.SpecialSummonStep(tc,SUMMON_TYPE_SYNCHRO,tp,tp,false,false,POS_FACEUP) then
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
@@ -145,11 +145,11 @@ function c100287022.scop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.SpecialSummonComplete()
 	tc:CompleteProcedure()
 	Duel.RaiseEvent(c,EVENT_ADJUST,nil,0,PLAYER_NONE,PLAYER_NONE,0)
-	local mg=Duel.GetMatchingGroup(Card.IsSynchroSummonable,tp,LOCATION_EXTRA,0,nil,nil)
-	if res and mg:GetCount()>0 then
+	local tg=Duel.GetMatchingGroup(Card.IsSynchroSummonable,tp,LOCATION_EXTRA,0,nil,nil)
+	if res and tg:GetCount()>0 then
 		Duel.BreakEffect()
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-		local sg=mg:Select(tp,1,1,nil)
+		local sg=tg:Select(tp,1,1,nil)
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_IMMUNE_EFFECT)
@@ -163,5 +163,5 @@ function c100287022.scop(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 function c100287022.immval(e,te)
-	return te:GetHandlerPlayer()~=e:GetOwnerPlayer() and te:IsActivated()
+	return te:GetOwnerPlayer()~=e:GetOwnerPlayer() and te:IsActivated()
 end
