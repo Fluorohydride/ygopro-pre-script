c3f8dcb4e25d99310f3f588e1e5bf041ff5c56ad
JSY1728 <34511983+JSY1728@users.noreply.github.com>
2022-02-16 23:20:03 +0900

    Update c100287004.lua
    Fix Script

--- scripts/HC01-JP/c100287004.lua
+++ scripts/HC01-JP/c100287004.lua
@@ -34,6 +34,7 @@ function s.initial_effect(c)
 	c:RegisterEffect(e3)
 	--Return to Hand
 	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(id,1))
 	e4:SetCategory(CATEGORY_TOHAND)
 	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e4:SetCode(EVENT_BATTLE_DESTROYING)
