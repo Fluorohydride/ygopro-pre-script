c74be79d37e788cd4f1047a5f1bcebc6d22cd570
JSY1728 <34511983+JSY1728@users.noreply.github.com>
2022-02-16 23:12:28 +0900

    Update c100287004.lua
    Fix Script

--- scripts/HC01-JP/c100287004.lua
+++ scripts/HC01-JP/c100287004.lua
@@ -7,6 +7,7 @@ function s.initial_effect(c)
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e1:SetCode(EFFECT_SPSUMMON_CONDITION)
 	e1:SetValue(aux.ritlimit)
 	c:RegisterEffect(e1)
 	--Deck Search
