98acb6b4049a527468499aefb48583342524d4a9
mercury233 <me@mercury233.me>
2022-02-19 17:04:23 +0800

    AddSetNameMonsterList EHERO

---
+++ scripts/POTE-JP/old cards/001/c97362768.lua
@@ -0,0 +1,83 @@
+--スパークガン
+function c97362768.initial_effect(c)
+	aux.AddSetNameMonsterList(c,0x3008)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_EQUIP)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_CONTINUOUS_TARGET)
+	e1:SetTarget(c97362768.target)
+	e1:SetOperation(c97362768.operation)
+	c:RegisterEffect(e1)
+	--Equip limit
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetCode(EFFECT_EQUIP_LIMIT)
+	e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e2:SetValue(c97362768.eqlimit)
+	c:RegisterEffect(e2)
+	--pos
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(97362768,0))
+	e3:SetCategory(CATEGORY_POSITION)
+	e3:SetType(EFFECT_TYPE_IGNITION)
+	e3:SetRange(LOCATION_SZONE)
+	e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e3:SetCost(c97362768.poscost)
+	e3:SetTarget(c97362768.postg)
+	e3:SetOperation(c97362768.posop)
+	c:RegisterEffect(e3)
+	--self destroy
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e4:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e4:SetCode(EVENT_CHAIN_SOLVED)
+	e4:SetRange(LOCATION_SZONE)
+	e4:SetOperation(c97362768.sdesop)
+	c:RegisterEffect(e4)
+end
+function c97362768.eqlimit(e,c)
+	return c:IsCode(20721928)
+end
+function c97362768.filter(c)
+	return c:IsFaceup() and c:IsCode(20721928)
+end
+function c97362768.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and c97362768.filter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(c97362768.filter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)
+	Duel.SelectTarget(tp,c97362768.filter,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_EQUIP,e:GetHandler(),1,0,0)
+end
+function c97362768.operation(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if e:GetHandler():IsRelateToEffect(e) and tc:IsRelateToEffect(e) and tc:IsFaceup() then
+		Duel.Equip(tp,e:GetHandler(),tc)
+	end
+end
+function c97362768.poscost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	e:GetHandler():RegisterFlagEffect(97362768,RESET_EVENT+RESETS_STANDARD,0,0)
+end
+function c97362768.posfilter(c)
+	return c:IsFaceup() and c:IsCanChangePosition()
+end
+function c97362768.postg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and c97362768.posfilter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(c97362768.posfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_POSCHANGE)
+	local g=Duel.SelectTarget(tp,c97362768.posfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_POSITION,g,1,0,0)
+end
+function c97362768.posop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if e:GetHandler():IsRelateToEffect(e) and tc:IsRelateToEffect(e) and tc:IsFaceup() then
+		Duel.ChangePosition(tc,POS_FACEUP_DEFENSE,0,POS_FACEUP_ATTACK,0)
+	end
+end
+function c97362768.sdesop(e,tp,eg,ep,ev,re,r,rp)
+	if e:GetHandler():GetFlagEffect(97362768)==3 then
+		Duel.Destroy(e:GetHandler(),REASON_RULE)
+	end
+end
