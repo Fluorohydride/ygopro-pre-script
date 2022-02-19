301f76926520e76ff58a73137920c747b7c6a135
Mercury233 <me@mercury233.me>
2022-02-19 16:23:12 +0800

    Update c101109051.lua

--- scripts/POTE-JP/c101109051.lua
+++ scripts/POTE-JP/c101109051.lua
@@ -6,7 +6,7 @@ function s.initial_effect(c)
 	aux.AddSetNameMonsterList(c,0x3008)
 	--Activate
 	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOHAND)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOHAND+CATEGORY_SEARCH)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetCountLimit(1,id+EFFECT_COUNT_CODE_OATH)
@@ -20,10 +20,14 @@ function s.counterfilter(c)
 	return not c:IsSummonLocation(LOCATION_EXTRA) or c:IsType(TYPE_FUSION)
 end
 function s.costfilter(c)
-	return (c:IsSetCard(0x1f) or c:IsSetCard(0x3008)) and c:IsType(TYPE_MONSTER) and c:IsAbleToGraveAsCost()
+	return c:IsSetCard(0x1f,0x3008) and c:IsType(TYPE_MONSTER) and c:IsAbleToGraveAsCost()
 end
 function s.fselect(g,e,tp)
-	return g:GetClassCount(Card.GetLocation)==g:GetCount() and (g:IsExists(Card.IsSetCard,1,nil,0x1f) and g:IsExists(Card.IsSetCard,1,nil,0x3008)) and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK,0,1,nil,e,tp)
+	return g:GetClassCount(Card.GetLocation)==g:GetCount() and aux.gfcheck(g,Card.IsSetCard,0x1f,0x3008)
+		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK,0,1,g,e,tp)
+end
+function s.spfilter(c,e,tp)
+	return (c:IsSetCard(0x1f) or (c:IsLevelAbove(5) and c:IsSetCard(0x3008))) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	local g=Duel.GetMatchingGroup(s.costfilter,tp,LOCATION_HAND+LOCATION_DECK,0,nil)
@@ -44,22 +48,21 @@ end
 function s.splimit(e,c)
 	return c:IsLocation(LOCATION_EXTRA) and not c:IsType(TYPE_FUSION)
 end
-function s.spfilter(c,e,tp)
-	return c:IsSetCard(0x1f) or (c:IsLevelAbove(5) and c:IsSetCard(0x3008)) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
-end
 function s.thfilter(c)
 	return c:IsCode(24094653) and c:IsAbleToHand()
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK,0,1,nil,e,tp) and Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,nil,e,tp) end
+		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK,0,1,nil,e,tp)
+		and Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,nil) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK+LOCATION_GRAVE)
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 	local sc=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp):GetFirst()
-	if sc and Duel.SpecialSummonStep(sc,0,tp,tp,false,false,POS_FACEUP) then
+	if sc and Duel.SpecialSummon(sc,0,tp,tp,false,false,POS_FACEUP)>0 then
 		if sc:IsCode(89943723) then
 			local e1=Effect.CreateEffect(e:GetHandler())
 			e1:SetType(EFFECT_TYPE_SINGLE)
@@ -68,9 +71,8 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
 			e1:SetReset(RESET_EVENT+RESETS_STANDARD)
 			sc:RegisterEffect(e1)
 		end
-		Duel.SpecialSummonComplete()
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOHAND)
-		local hg=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil)
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+		local hg=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.thfilter),tp,LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil)
 		local tc=hg:GetFirst()
 		if tc then
 			Duel.SendtoHand(tc,nil,REASON_EFFECT)
