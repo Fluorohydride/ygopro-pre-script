0e194c8a4cb0f67c59b2c1ffcc52c4505b4d4840
JSY1728 <34511983+JSY1728@users.noreply.github.com>
2022-02-19 14:29:51 +0900

    Update c101109054.lua
    Fix Script

--- scripts/POTE-JP/c101109054.lua
+++ scripts/POTE-JP/c101109054.lua
@@ -2,6 +2,8 @@
 --Script by JSY1728
 local s,id,o=GetID()
 function s.initial_effect(c)
+	aux.AddCodeList(c,89943723)
+	aux.AddSetNameMonsterList(c,0x3008)
 	--Activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_TODECK+CATEGORY_TOHAND)
