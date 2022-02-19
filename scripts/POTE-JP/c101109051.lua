110a460587f0ca6c935d70c6107560afb72c6aa9
JSY1728 <34511983+JSY1728@users.noreply.github.com>
2022-02-19 14:38:26 +0900

    Update c101109051.lua
    Fxi Script

--- scripts/POTE-JP/c101109051.lua
+++ scripts/POTE-JP/c101109051.lua
@@ -2,6 +2,8 @@
 --Script by JSY1728
 local s,id,o=GetID()
 function s.initial_effect(c)
+	aux.AddCodeList(c,89943723)
+	aux.AddSetNameMonsterList(c,0x3008)
 	--Activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOHAND)
@@ -17,16 +19,16 @@ end
 function s.counterfilter(c)
 	return not c:IsSummonLocation(LOCATION_EXTRA) or c:IsType(TYPE_FUSION)
 end
-function s.spcfilter1(c,tp)
-	return c:IsSetCard(0x1f) and c:IsType(TYPE_MONSTER) and c:IsAbleToGraveAsCost()
-		and Duel.IsExistingMatchingCard(s.spcfilter2,tp,LOCATION_HAND+LOCATION_DECK,0,1,c)
+function s.costfilter(c)
+	return (c:IsSetCard(0x1f) or c:IsSetCard(0x3008)) and c:IsType(TYPE_MONSTER) and c:IsAbleToGraveAsCost()
 end
-function s.spcfilter2(c)
-	return c:IsSetCard(0x3008) and c:IsType(TYPE_MONSTER) and c:IsAbleToGraveAsCost()
+function s.fselect(g,e,tp)
+	return g:GetClassCount(Card.GetLocation)==g:GetCount() and (g:IsExists(Card.IsSetCard,1,nil,0x1f) and g:IsExists(Card.IsSetCard,1,nil,0x3008)) and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK,0,1,nil,e,tp)
 end
 function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	local g=Duel.GetMatchingGroup(s.costfilter,tp,LOCATION_HAND+LOCATION_DECK,0,nil)
 	if chk==0 then return Duel.GetCustomActivityCount(id,tp,ACTIVITY_SPSUMMON)==0
-		and Duel.IsExistingMatchingCard(s.spcfilter1,tp,LOCATION_HAND+LOCATION_DECK,0,1,nil,tp) end
+		and g:CheckSubGroup(s.fselect,2,2,e,tp) end
 	local e1=Effect.CreateEffect(e:GetHandler())
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_OATH)
@@ -36,11 +38,11 @@ function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	e1:SetTarget(s.splimit)
 	Duel.RegisterEffect(e1,tp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
-	local g=Duel.SelectMatchingCard(tp,spcfilter1,tp,LOCATION_HAND+LOCATION_DECK,0,1,1,nil,tp)
-	Duel.SendtoGrave(g,REASON_COST)
+	local sg=g:SelectSubGroup(tp,s.fselect,false,2,2,e,tp)
+	Duel.SendtoGrave(sg,REASON_COST)
 end
 function s.splimit(e,c)
-	return c:IsSummonLocation(LOCATION_EXTRA) or c:IsType(TYPE_FUSION)
+	return c:IsLocation(LOCATION_EXTRA) and not c:IsType(TYPE_FUSION)
 end
 function s.spfilter(c,e,tp)
 	return c:IsSetCard(0x1f) or (c:IsLevelAbove(5) and c:IsSetCard(0x3008)) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
@@ -50,28 +52,29 @@ function s.thfilter(c)
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK,0,1,nil,e,tp) end
+		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK,0,1,nil,e,tp) and Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,nil,e,tp) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
-	if g:GetCount()>0 and Duel.SpecialSummonStep(g,0,tp,tp,false,false,POS_FACEUP) then
-		if g:IsCode(89943723) then
+	local sc=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp):GetFirst()
+	if sc and Duel.SpecialSummonStep(sc,0,tp,tp,false,false,POS_FACEUP) then
+		if sc:IsCode(89943723) then
 			local e1=Effect.CreateEffect(e:GetHandler())
 			e1:SetType(EFFECT_TYPE_SINGLE)
 			e1:SetCode(EFFECT_UPDATE_ATTACK)
 			e1:SetValue(1000)
 			e1:SetReset(RESET_EVENT+RESETS_STANDARD)
-			g:RegisterEffect(e1)
+			sc:RegisterEffect(e1)
 		end
+		Duel.SpecialSummonComplete()
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOHAND)
 		local hg=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil)
 		local tc=hg:GetFirst()
 		if tc then
 			Duel.SendtoHand(tc,nil,REASON_EFFECT)
+			Duel.ConfirmCards(1-tp,tc)
 		end
 	end
-	Duel.SpecialSummonComplete()
 end
