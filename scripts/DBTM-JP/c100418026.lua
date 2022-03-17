09e84448b583be6f69cf0627f2dbb8e9b12f4bf7
Mercury233 <me@mercury233.me>
2022-03-17 14:44:40 +0800

    Update c100418026.lua

--- scripts/DBTM-JP/c100418026.lua
+++ scripts/DBTM-JP/c100418026.lua
@@ -4,7 +4,7 @@
 function c100418026.initial_effect(c)
 	--activate
 	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_DESTROY+CATEGORY_TOGRAVE)
+	e1:SetCategory(CATEGORY_DESTROY+CATEGORY_DECKDES)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
