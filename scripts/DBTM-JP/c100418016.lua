88cae1b763d13812f8a6d3ac993558ae3eff47d9
Mercury233 <me@mercury233.me>
2022-03-17 10:08:58 +0800

    Update c100418016.lua

--- scripts/DBTM-JP/c100418016.lua
+++ scripts/DBTM-JP/c100418016.lua
@@ -4,6 +4,7 @@
 function c100418016.initial_effect(c)
 	--spsummon
 	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(100418016,0))
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetRange(LOCATION_MZONE)
@@ -14,6 +15,7 @@ function c100418016.initial_effect(c)
 	c:RegisterEffect(e1)
 	--draw
 	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(100418016,1))
 	e2:SetCategory(CATEGORY_DRAW+CATEGORY_SPECIAL_SUMMON)
 	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
 	e2:SetCode(EVENT_LEAVE_FIELD)
@@ -35,7 +37,8 @@ function c100418016.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.SendtoGrave(g,REASON_COST)
 end
 function c100418016.spfilter(c,e,tp)
-	return not c:IsCode(100418016) and c:IsRace(RACE_FIEND) and c:IsLevelBelow(4) and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP_DEFENSE)
+	return not c:IsCode(100418016) and c:IsRace(RACE_FIEND) and c:IsLevelBelow(4)
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP_DEFENSE)
 end
 function c100418016.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
@@ -55,7 +58,7 @@ function c100418016.cfilter(c,tp,re,r,rp)
 		and re:GetHandler():GetType()==TYPE_TRAP and re:IsActiveType(TYPE_TRAP)
 end
 function c100418016.drcon(e,tp,eg,ep,ev,re,r,rp)
-	return eg:IsExists(c100418016.cfilter,1,nil,tp,re,r,rp) and not eg:IsContains(e:GetHandler())
+	return eg:IsExists(c100418016.cfilter,1,nil,tp,re,r,rp)
 end
 function c100418016.drtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsPlayerCanDraw(tp,1) end
@@ -69,30 +72,35 @@ end
 function c100418016.drop(e,tp,eg,ep,ev,re,r,rp)
 	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
 	if Duel.Draw(p,d,REASON_EFFECT)>0 then
-		Duel.BreakEffect()
+		local off=1
+		local ops={}
+		local opval={}
 		local spg=Duel.GetMatchingGroup(c100418016.spfilter2,tp,LOCATION_HAND,0,nil,e,tp)
 		local stg=Duel.GetMatchingGroup(Card.IsSSetable,tp,LOCATION_HAND,0,nil)
-		if (#spg>0 or #stg>0) and Duel.SelectYesNo(tp,aux.Stringid(100418016,0)) then
-			local op=0
-			if #spg>0 and #stg==0 then
-				op=Duel.SelectOption(tp,aux.Stringid(100418016,1))+1
-			end
-			if #spg==0 and #stg>0 then
-				op=Duel.SelectOption(tp,aux.Stringid(100418016,2))+2
-			end
-			if #spg>0 and #stg>0 then
-				op=Duel.SelectOption(tp,aux.Stringid(100418016,1),aux.Stringid(100418016,2))+1
-			end
-			if op==1 then
-				Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-				local sg=spg:Select(tp,1,1,nil)
-				Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP)
-			end
-			if op==2 then
-				Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SET)
-				local sg=stg:Select(tp,1,1,nil)
-				Duel.SSet(tp,sg)
-			end
+		if #spg>0 then
+			ops[off]=aux.Stringid(100418016,2)
+			opval[off-1]=1
+			off=off+1
+		end
+		if #stg>0 then
+			ops[off]=aux.Stringid(100418016,3)
+			opval[off-1]=2
+			off=off+1
+		end
+		ops[off]=aux.Stringid(100418016,4)
+		opval[off-1]=0
+		local op=Duel.SelectOption(tp,table.unpack(ops))
+		if opval[op]==1 then
+			Duel.BreakEffect()
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+			local sg=spg:Select(tp,1,1,nil)
+			Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP)
+		end
+		if opval[op]==2 then
+			Duel.BreakEffect()
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SET)
+			local sg=stg:Select(tp,1,1,nil)
+			Duel.SSet(tp,sg,tp,false)
 		end
 	end
 end
