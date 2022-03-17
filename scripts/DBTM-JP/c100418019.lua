4ef7d4496eb937ee0c31cf0f550eeb5450773cd9
Mercury233 <me@mercury233.me>
2022-03-17 14:27:36 +0800

    Update c100418019.lua

--- scripts/DBTM-JP/c100418019.lua
+++ scripts/DBTM-JP/c100418019.lua
@@ -4,6 +4,7 @@
 function c100418019.initial_effect(c)
 	--set
 	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(100418019,0))
 	e1:SetType(EFFECT_TYPE_QUICK_O)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetRange(LOCATION_HAND+LOCATION_MZONE)
@@ -15,6 +16,7 @@ function c100418019.initial_effect(c)
 	c:RegisterEffect(e1)
 	--spsummon
 	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(100418019,1))
 	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
 	e2:SetCode(EVENT_LEAVE_FIELD)
