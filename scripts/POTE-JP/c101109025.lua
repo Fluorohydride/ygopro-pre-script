b1a7954c32cf3f01338d0135489fa7588224b062
mercury233 <me@mercury233.me>
2022-03-18 14:54:50 +0800

    update エクソシスター・マルファ
    close #922

--- scripts/POTE-JP/c101109025.lua
+++ scripts/POTE-JP/c101109025.lua
@@ -10,10 +10,10 @@ function c101109025.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetRange(LOCATION_HAND)
 	e1:SetCountLimit(1,101109025)
-	e1:SetCondition(c101109025.effcon)
-	e1:SetCost(c101109025.effcost)
-	e1:SetTarget(c101109025.efftg)
-	e1:SetOperation(c101109025.effop)
+	e1:SetCondition(c101109025.spcon)
+	e1:SetCost(c101109025.spcost)
+	e1:SetTarget(c101109025.sptg)
+	e1:SetOperation(c101109025.spop)
 	c:RegisterEffect(e1)
 	--special summon
 	local e2=Effect.CreateEffect(c)
@@ -24,8 +24,8 @@ function c101109025.initial_effect(c)
 	e2:SetCode(EVENT_LEAVE_GRAVE)
 	e2:SetRange(LOCATION_MZONE)
 	e2:SetCountLimit(1,101109025+100)
-	e2:SetTarget(c101109025.sptg)
-	e2:SetOperation(c101109025.spop)
+	e2:SetTarget(c101109025.xyztg)
+	e2:SetOperation(c101109025.xyzop)
 	c:RegisterEffect(e2)
 	Duel.AddCustomActivityCounter(101109025,ACTIVITY_SPSUMMON,c101109025.counterfilter)
 end
@@ -35,14 +35,11 @@ end
 function c101109025.cfilter(c)
 	return c:IsFaceup() and c:IsType(TYPE_XYZ)
 end
-function c101109025.effcon(e,tp,eg,ep,ev,re,r,rp)
+function c101109025.spcon(e,tp,eg,ep,ev,re,r,rp)
 	local ct=Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0)
 	return ct==0 or ct==Duel.GetMatchingGroupCount(c101109025.cfilter,tp,LOCATION_MZONE,0,nil)
 end
-function c101109025.effilter(c,e,tp)
-	return c:IsCode(16474916) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
-end
-function c101109025.effcost(e,tp,eg,ep,ev,re,r,rp,chk)
+function c101109025.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetCustomActivityCount(101109025,tp,ACTIVITY_SPSUMMON)==0 end
 	local e1=Effect.CreateEffect(e:GetHandler())
 	e1:SetType(EFFECT_TYPE_FIELD)
@@ -56,41 +53,45 @@ end
 function c101109025.splimit(e,c)
 	return not c:IsSetCard(0x172)
 end
-function c101109025.efftg(e,tp,eg,ep,ev,re,r,rp,chk)
-	local c=e:GetHandler()
-	if chk==0 then return Duel.IsPlayerCanSpecialSummonCount(tp,2)
-		and Duel.GetLocationCount(tp,LOCATION_MZONE)>1
-		and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
-		and Duel.IsExistingMatchingCard(c101109025.effilter,tp,LOCATION_DECK,0,1,nil,e,tp) end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,0,0)
-end
-function c101109025.effop(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	if c:IsRelateToEffect(e) and Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)~=0
-		and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 then
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-		local g=Duel.SelectMatchingCard(tp,c101109025.effilter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
-		if g:GetCount()>0 then
-			Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
-		end
-	end
-end
-function c101109025.spfilter(c,e,tp,mc)
-	return c:IsSetCard(0x172) and c:IsType(TYPE_XYZ) and mc:IsCanBeXyzMaterial(c)
-		and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_XYZ,tp,false,false) and Duel.GetLocationCountFromEx(tp,tp,mc,c)>0
+function c101109025.spfilter(c,e,tp)
+	return c:IsCode(16474916) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function c101109025.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
-	if chk==0 then return aux.MustMaterialCheck(c,tp,EFFECT_MUST_BE_XMATERIAL)
-		and Duel.IsExistingMatchingCard(c101109025.spfilter,tp,LOCATION_EXTRA,0,1,nil,e,tp,c) end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
+	if chk==0 then return not Duel.IsPlayerAffectedByEffect(tp,59822133)
+		and Duel.GetLocationCount(tp,LOCATION_MZONE)>1
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+		and Duel.IsExistingMatchingCard(c101109025.spfilter,tp,LOCATION_DECK,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,0,0)
 end
 function c101109025.spop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.IsPlayerAffectedByEffect(tp,59822133) or Duel.GetLocationCount(tp,LOCATION_MZONE)<2 then return end
+	local c=e:GetHandler()
+	if not c:IsRelateToEffect(e) or not c:IsCanBeSpecialSummoned(e,0,tp,false,false) then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,c101109025.spfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
+	if g:GetCount()>0 then
+		Duel.SpecialSummonStep(c,0,tp,tp,false,false,POS_FACEUP)
+		Duel.SpecialSummonStep(g:GetFirst(),0,tp,tp,false,false,POS_FACEUP)
+		Duel.SpecialSummonComplete()
+	end
+end
+function c101109025.xyzfilter(c,e,tp,mc)
+	return c:IsSetCard(0x172) and c:IsType(TYPE_XYZ) and mc:IsCanBeXyzMaterial(c)
+		and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_XYZ,tp,false,false) and Duel.GetLocationCountFromEx(tp,tp,mc,c)>0
+end
+function c101109025.xyztg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return aux.MustMaterialCheck(c,tp,EFFECT_MUST_BE_XMATERIAL)
+		and Duel.IsExistingMatchingCard(c101109025.xyzfilter,tp,LOCATION_EXTRA,0,1,nil,e,tp,c) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
+end
+function c101109025.xyzop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if not aux.MustMaterialCheck(c,tp,EFFECT_MUST_BE_XMATERIAL) then return end
 	if c:IsFaceup() and c:IsRelateToEffect(e) and c:IsControler(tp) and not c:IsImmuneToEffect(e) then
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-		local g=Duel.SelectMatchingCard(tp,c101109025.spfilter,tp,LOCATION_EXTRA,0,1,1,nil,e,tp,c)
+		local g=Duel.SelectMatchingCard(tp,c101109025.xyzfilter,tp,LOCATION_EXTRA,0,1,1,nil,e,tp,c)
 		local sc=g:GetFirst()
 		if sc then
 			local mg=c:GetOverlayGroup()
