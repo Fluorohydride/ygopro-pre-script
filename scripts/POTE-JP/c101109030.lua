e5373182c336d2ee2d21f19f44c365b76e902dba
wzh31891579 <31891579@qq.com>
2022-04-05 15:58:47 +0800

    add POTE cards

---
+++ scripts/POTE-JP/c101109030.lua
@@ -0,0 +1,48 @@
+--はぐれ・もけもけ
+--
+--Script by Trishula9
+function c101109030.initial_effect(c)
+	--change name
+	aux.EnableChangeCode(c,27288416,LOCATION_MZONE+LOCATION_GRAVE)
+	--normal monster
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_ADD_TYPE)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetRange(LOCATION_MZONE+LOCATION_GRAVE)
+	e1:SetValue(TYPE_NORMAL)
+	c:RegisterEffect(e1)
+	local e2=e1:Clone()
+	e2:SetCode(EFFECT_REMOVE_TYPE)
+	e2:SetValue(TYPE_EFFECT)
+	c:RegisterEffect(e2)
+	--to hand
+	local e3=Effect.CreateEffect(c)
+	e3:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e3:SetType(EFFECT_TYPE_IGNITION)
+	e3:SetRange(LOCATION_HAND)
+	e3:SetCost(c101109030.thcost)
+	e3:SetTarget(c101109030.thtg)
+	e3:SetOperation(c101109030.thop)
+	c:RegisterEffect(e3)
+end
+function c101109030.thcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return c:IsDiscardable() end
+	Duel.SendtoGrave(c,REASON_COST+REASON_DISCARD)
+end
+function c101109030.thfilter(c)
+	return not c:IsCode(101109030) and c:IsSetCard(0x282) and c:IsAbleToHand()
+end
+function c101109030.thtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chk==0 then return Duel.IsExistingMatchingCard(c101109030.thfilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function c101109030.thop(e,tp,eg,ep,ev,re,r,rp,chk)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,c101109030.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if g:GetCount()>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end
