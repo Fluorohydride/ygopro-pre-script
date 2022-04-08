5ab16726a2770cc0369b014754378b830ee997cf
mercury233 <me@mercury233.me>
2022-04-08 21:19:47 +0800

    format
    blame github copilot

--- scripts/SD44-JP/c100344051.lua
+++ scripts/SD44-JP/c100344051.lua
@@ -18,10 +18,10 @@ function s.initial_effect(c)
 	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
 	e1:SetRange(LOCATION_EXTRA)
 	e1:SetCondition(s.spcon)
-    e1:SetTarget(s.sptg)
+	e1:SetTarget(s.sptg)
 	e1:SetOperation(s.spop)
 	c:RegisterEffect(e1)
-   --atk up
+	--atk up
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_SINGLE)
 	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
@@ -30,19 +30,19 @@ function s.initial_effect(c)
 	e2:SetValue(7000)
 	e2:SetCondition(s.atkcon)
 	c:RegisterEffect(e2)
-    --to deck
-    local e3=Effect.CreateEffect(c)
-    e3:SetDescription(aux.Stringid(id,0))
-    e3:SetCategory(CATEGORY_TODECK+CATEGORY_SPECIAL_SUMMON)
-    e3:SetType(EFFECT_TYPE_QUICK_O)
-    e3:SetCode(EVENT_FREE_CHAIN)
-    e3:SetRange(LOCATION_MZONE)
-    e3:SetHintTiming(0,TIMINGS_CHECK_MONSTER+TIMING_END_PHASE)
-    e3:SetCost(s.tdcost)
-    e3:SetTarget(s.tdtg)
-    e3:SetOperation(s.tdop)
-    c:RegisterEffect(e3)
-    --
+	--to deck
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,0))
+	e3:SetCategory(CATEGORY_TODECK+CATEGORY_SPECIAL_SUMMON)
+	e3:SetType(EFFECT_TYPE_QUICK_O)
+	e3:SetCode(EVENT_FREE_CHAIN)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetHintTiming(0,TIMINGS_CHECK_MONSTER+TIMING_END_PHASE)
+	e3:SetCost(s.tdcost)
+	e3:SetTarget(s.tdtg)
+	e3:SetOperation(s.tdop)
+	c:RegisterEffect(e3)
+	--
 	if not s.global_flag then
 		s.global_flag=true
 		local ge1=Effect.CreateEffect(c)
@@ -60,69 +60,69 @@ function s.regop(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 function s.cfilter1(c,g)
-    return c:IsFusionSetCard(0x2034) and g:FilterCount(Card.IsFusionSetCard,c,0x1034)>=7
+	return c:IsFusionSetCard(0x2034) and g:FilterCount(Card.IsFusionSetCard,c,0x1034)>=7
 end
 function s.spcon(e,c)
 	if c==nil then return true end
 	local tp=c:GetControler()
-    if Duel.GetFlagEffect(tp,id)==0 then return false end
+	if Duel.GetFlagEffect(tp,id)==0 then return false end
 	local g=Duel.GetMatchingGroup(Card.IsAbleToRemoveAsCost,tp,LOCATION_ONFIELD+LOCATION_GRAVE,0,nil)
 	return g:IsExists(s.cfilter1,1,nil,g)
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,c)
 	local g=Duel.GetMatchingGroup(Card.IsAbleToRemoveAsCost,tp,LOCATION_ONFIELD+LOCATION_GRAVE,0,nil)
-    local sg=Group.CreateGroup()
-    local cg=g:Filter(s.cfilter1,nil,g)
-    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
-    local tc1=cg:SelectUnselect(sg,tp,false,true,1,1)
-    if not tc1 then return false end
-    cg=g:Filter(Card.IsFusionSetCard,tc1,0x1034)
-    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
-    local sg2=cg:SelectSubGroup(tp,aux.TRUE,true,7,7)
-    if not sg2 then return false end
-    sg:AddCard(tc1)
-    sg:Merge(sg2)
-    sg:KeepAlive()
+	local sg=Group.CreateGroup()
+	local cg=g:Filter(s.cfilter1,nil,g)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local tc1=cg:SelectUnselect(sg,tp,false,true,1,1)
+	if not tc1 then return false end
+	cg=g:Filter(Card.IsFusionSetCard,tc1,0x1034)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local sg2=cg:SelectSubGroup(tp,aux.TRUE,true,7,7)
+	if not sg2 then return false end
+	sg:AddCard(tc1)
+	sg:Merge(sg2)
+	sg:KeepAlive()
 	e:SetLabelObject(sg)
-    return true
+	return true
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp,c)
 	local g=e:GetLabelObject()
 	c:SetMaterial(g)
 	Duel.Remove(g,POS_FACEUP,REASON_COST)
-    g:DeleteGroup()
+	g:DeleteGroup()
 end
 function s.atkfilter(c)
-    return c:IsFaceup() and c:IsSetCard(0x1034)
+	return c:IsFaceup() and c:IsSetCard(0x1034)
 end
 function s.atkcon(e)
-    local tp=e:GetHandlerPlayer()
-    local g=Duel.GetMatchingGroup(s.atkfilter,tp,LOCATION_REMOVED,0,nil)
+	local tp=e:GetHandlerPlayer()
+	local g=Duel.GetMatchingGroup(s.atkfilter,tp,LOCATION_REMOVED,0,nil)
 	return g:GetClassCount(Card.GetCode)>=7
 end
 function s.tdcost(e,tp,eg,ep,ev,re,r,rp,chk)
-    local c=e:GetHandler()
-    if chk==0 then return c:GetBattledGroupCount()==0 and c:IsReleasable() end
-    Duel.Release(c,REASON_COST)
+	local c=e:GetHandler()
+	if chk==0 then return c:GetBattledGroupCount()==0 and c:IsReleasable() end
+	Duel.Release(c,REASON_COST)
 end
 function s.spfilter(c,e,tp)
-    return c:IsFaceup() and c:IsSetCard(0x1034) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+	return c:IsFaceup() and c:IsSetCard(0x1034) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function s.tdtg(e,tp,eg,ep,ev,re,r,rp,chk)
-    local c=e:GetHandler()
-    if chk==0 then return Duel.IsExistingMatchingCard(Card.IsAbleToDeck,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,c)
-        and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_REMOVED,0,1,nil,e,tp) end
-    local g=Duel.GetMatchingGroup(Card.IsAbleToDeck,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,nil)
-    Duel.SetOperationInfo(0,CATEGORY_TODECK,g,#g,0,0)
+	local c=e:GetHandler()
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsAbleToDeck,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,c)
+		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_REMOVED,0,1,nil,e,tp) end
+	local g=Duel.GetMatchingGroup(Card.IsAbleToDeck,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,nil)
+	Duel.SetOperationInfo(0,CATEGORY_TODECK,g,#g,0,0)
 end
 function s.tdop(e,tp,eg,ep,ev,re,r,rp)
-    local g=Duel.GetMatchingGroup(Card.IsAbleToDeck,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,nil)
+	local g=Duel.GetMatchingGroup(Card.IsAbleToDeck,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,nil)
 	if Duel.SendtoDeck(g,nil,SEQ_DECKSHUFFLE,REASON_EFFECT)>0 then
-        local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
-        if ft<=0 then return end
-        if Duel.IsPlayerAffectedByEffect(tp,59822133) then ft=1 end
-        Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-        local sg=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_REMOVED,0,1,ft,nil,e,tp)
-        Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP)
-    end
+		local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+		if ft<=0 then return end
+		if Duel.IsPlayerAffectedByEffect(tp,59822133) then ft=1 end
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+		local sg=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_REMOVED,0,1,ft,nil,e,tp)
+		Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP)
+	end
 end
