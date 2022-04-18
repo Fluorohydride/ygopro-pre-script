a0c1cfb42a5b52d1964f7ba1bd71ac47a7c1cce6
mallu11 <54900811+mallu11@users.noreply.github.com>
2022-04-18 21:25:42 +0800

    Update c101109031.lua

--- scripts/POTE-JP/c101109031.lua
+++ scripts/POTE-JP/c101109031.lua
@@ -83,7 +83,7 @@ function c101109031.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 end
 function c101109031.spop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
-	if tc:IsRelateToEffect(e) and aux.NecroValleyFilter()(tc) then
+	if tc:IsRelateToEffect(e) then
 		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
 	end
 end
