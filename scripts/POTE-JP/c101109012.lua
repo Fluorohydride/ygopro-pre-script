f78df9c8e2374ff72274cadb00e939aa85a33a70
Mercury233 <me@mercury233.me>
2022-04-21 17:36:15 +0800

    add scripts POTE by JoyJ (#938)

---
+++ scripts/POTE-JP/c101109012.lua
@@ -0,0 +1,120 @@
+--ティアラメンツ・メイルゥ
+--
+--Script by JoyJ
+function c101109012.initial_effect(c)
+	--deckdes
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(101109012,0))
+	e1:SetCategory(CATEGORY_DECKDES)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetProperty(EFFECT_FLAG_DELAY)
+	e1:SetCode(EVENT_SUMMON_SUCCESS)
+	e1:SetCountLimit(1,101109012)
+	e1:SetTarget(c101109012.ddtg)
+	e1:SetOperation(c101109012.ddop)
+	c:RegisterEffect(e1)
+	local e2=e1:Clone()
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	c:RegisterEffect(e2)
+	--special summon
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(101109012,1))
+	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e3:SetCode(EVENT_TO_GRAVE)
+	e3:SetCountLimit(1,101109012+100)
+	e3:SetProperty(EFFECT_FLAG_DELAY)
+	e3:SetCondition(c101109012.condition)
+	e3:SetTarget(c101109012.target)
+	e3:SetOperation(c101109012.activate)
+	c:RegisterEffect(e3)
+end
+function c101109012.ddtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsPlayerCanDiscardDeck(tp,3) end
+	Duel.SetOperationInfo(0,CATEGORY_DECKDES,nil,0,tp,3)
+end
+function c101109012.ddop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.DiscardDeck(tp,3,REASON_EFFECT)
+end
+function c101109012.filter0(c,e)
+	return c:IsType(TYPE_MONSTER) and c:IsCanBeFusionMaterial() and c:IsAbleToDeck() and not c:IsImmuneToEffect(e)
+end
+function c101109012.filter1(c,e,tp,m,f,chkf)
+	if not (c:IsType(TYPE_FUSION) and (not f or f(c))
+		and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_FUSION,tp,false,false)) then return false end
+	local res=c:CheckFusionMaterial(m,e:GetHandler(),chkf)
+	return res
+end
+function c101109012.condition(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsReason(REASON_EFFECT)
+end
+function c101109012.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then
+		local chkf=tp
+		local mg=Duel.GetMatchingGroup(c101109012.filter0,tp,LOCATION_HAND+LOCATION_MZONE+LOCATION_GRAVE,0,nil,e)
+		local res=Duel.IsExistingMatchingCard(c101109012.filter1,tp,LOCATION_EXTRA,0,1,nil,e,tp,mg,nil,chkf)
+		if not res then
+			local ce=Duel.GetChainMaterial(tp)
+			if ce~=nil then
+				local fgroup=ce:GetTarget()
+				local mg2=fgroup(ce,e,tp)
+				local mf=ce:GetValue()
+				res=Duel.IsExistingMatchingCard(c101109012.filter1,tp,LOCATION_EXTRA,0,1,nil,e,tp,mg2,mf,chkf)
+			end
+		end
+		return res
+	end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
+end
+function c101109012.activate(e,tp,eg,ep,ev,re,r,rp)
+	local chkf=tp
+	local mg=Duel.GetMatchingGroup(aux.NecroValleyFilter(c101109012.filter0),tp,LOCATION_HAND+LOCATION_MZONE+LOCATION_GRAVE,0,nil,e)
+	local sg1=Duel.GetMatchingGroup(c101109012.filter1,tp,LOCATION_EXTRA,0,nil,e,tp,mg,nil,chkf)
+	local mg2=nil
+	local sg2=nil
+	local ce=Duel.GetChainMaterial(tp)
+	if ce~=nil then
+		local fgroup=ce:GetTarget()
+		mg2=fgroup(ce,e,tp)
+		local mf=ce:GetValue()
+		sg2=Duel.GetMatchingGroup(c101109012.filter1,tp,LOCATION_EXTRA,0,nil,e,tp,mg2,mf,chkf)
+	end
+	if sg1:GetCount()>0 or (sg2~=nil and sg2:GetCount()>0) then
+		local sg=sg1:Clone()
+		if sg2 then sg:Merge(sg2) end
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+		local tg=sg:Select(tp,1,1,nil)
+		local tc=tg:GetFirst()
+		if sg1:IsContains(tc) and (sg2==nil or not sg2:IsContains(tc) or not Duel.SelectYesNo(tp,ce:GetDescription())) then
+			local mat1=Duel.SelectFusionMaterial(tp,tc,mg,e:GetHandler(),chkf)
+			tc:SetMaterial(mat1)
+			if mat1:IsExists(Card.IsFacedown,1,nil) then
+				local cg=mat1:Filter(Card.IsFacedown,nil)
+				Duel.ConfirmCards(1-tp,cg)
+			end
+			Duel.SendtoDeck(mat1,nil,SEQ_DECKTOP,REASON_EFFECT+REASON_MATERIAL+REASON_FUSION)
+			local p=tp
+			for i=1,2 do
+				local dg=mat1:Filter(c101109012.seqfilter,nil,p)
+				if #dg>1 then
+					Duel.SortDecktop(tp,p,#dg)
+				end
+				for i=1,#dg do
+					local mg=Duel.GetDecktopGroup(p,1)
+					Duel.MoveSequence(mg:GetFirst(),SEQ_DECKBOTTOM)
+				end
+				p=1-tp
+			end
+			Duel.BreakEffect()
+			Duel.SpecialSummon(tc,SUMMON_TYPE_FUSION,tp,tp,false,false,POS_FACEUP)
+		else
+			local mat2=Duel.SelectFusionMaterial(tp,tc,mg2,e:GetHandler(),chkf)
+			local fop=ce:GetOperation()
+			fop(ce,e,tp,tc,mat2)
+		end
+		tc:CompleteProcedure()
+	end
+end
+function c101109012.seqfilter(c,tp)
+	return c:IsLocation(LOCATION_DECK) and c:IsControler(tp)
+end
---
+++ scripts/POTE-JP/c101109022.lua
@@ -0,0 +1,83 @@
+--メルフィー・ワラビィ
+--
+--Script by JoyJ
+function c101109022.initial_effect(c)
+	--to hand
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(101109022,0))
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SPECIAL_SUMMON+CATEGORY_DECKDES)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_SUMMON_SUCCESS)
+	e1:SetProperty(EFFECT_FLAG_DELAY)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1,101109022)
+	e1:SetCondition(c101109022.thcon)
+	e1:SetTarget(c101109022.thtg)
+	e1:SetOperation(c101109022.thop)
+	c:RegisterEffect(e1)
+	local e2=e1:Clone()
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	c:RegisterEffect(e2)
+	local e3=e1:Clone()
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e3:SetCode(EVENT_BE_BATTLE_TARGET)
+	e3:SetCondition(c101109022.thcon2)
+	c:RegisterEffect(e3)
+	--spsummon
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(101109022,1))
+	e4:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e4:SetCode(EVENT_PHASE+PHASE_END)
+	e4:SetRange(LOCATION_HAND)
+	e4:SetCountLimit(1,101109022+100)
+	e4:SetCondition(c101109022.spcon)
+	e4:SetTarget(c101109022.sptg)
+	e4:SetOperation(c101109022.spop)
+	c:RegisterEffect(e4)
+end
+function c101109022.cfilter(c,sp)
+	return c:IsSummonPlayer(sp)
+end
+function c101109022.thcon(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(c101109022.cfilter,1,nil,1-tp)
+end
+function c101109022.thcon2(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetAttacker():IsControler(1-tp)
+end
+function c101109022.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return c:IsAbleToHand() end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,c,1,0,0)
+end
+function c101109022.spfilter(c,e,tp)
+	return c:IsSetCard(0x146) and not c:IsCode(101109022) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function c101109022.thop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local g=Duel.GetMatchingGroup(c101109022.spfilter,tp,LOCATION_DECK,0,nil,e,tp)
+	if c:IsRelateToEffect(e) and Duel.SendtoHand(c,nil,REASON_EFFECT)~=0 and c:IsLocation(LOCATION_HAND)
+		and Duel.GetLocationCount(tp,LOCATION_MZONE)>1
+		and not Duel.IsPlayerAffectedByEffect(tp,59822133)
+		and g:GetClassCount(Card.GetCode)>1
+		and Duel.SelectYesNo(tp,aux.Stringid(101109022,2)) then
+		Duel.BreakEffect()
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+		local sg=g:SelectSubGroup(tp,aux.dncheck,false,2,2)
+		Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
+function c101109022.spcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetTurnPlayer()==tp
+end
+function c101109022.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and c:IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,0,0)
+end
+function c101109022.spop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) then
+		Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
