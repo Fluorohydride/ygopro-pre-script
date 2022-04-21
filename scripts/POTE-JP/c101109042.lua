b8783bf728cff811877ce0b6b96a986dac5a6de8
Mercury233 <me@mercury233.me>
2022-04-22 06:59:03 +0800

    Update c101109042.lua

--- scripts/POTE-JP/c101109042.lua
+++ scripts/POTE-JP/c101109042.lua
@@ -44,8 +44,6 @@ function c101109042.cfilter(c)
 end
 function c101109042.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(c101109042.cfilter,tp,LOCATION_DECK,0,1,nil) end
-	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
-	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_DECK)
 end
 function c101109042.operation(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_OPERATECARD)
