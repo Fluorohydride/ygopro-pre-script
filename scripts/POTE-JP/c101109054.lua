689b012e2d9a2b1c79997bcc3cc190f81af75066
mercury233 <me@mercury233.me>
2022-04-26 09:34:13 +0800

    fix 融合超渦

--- scripts/POTE-JP/c101109054.lua
+++ scripts/POTE-JP/c101109054.lua
@@ -24,7 +24,7 @@ function s.cfilter(c,tc,tp)
 end
 function s.fstg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and s.tgfilter(chkc,tp) end
-	if chk==0 then return Duel.IsExistingMatchingCard(s.tgfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil,tp) end
+	if chk==0 then return Duel.IsExistingTarget(s.tgfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil,tp) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
 	Duel.SelectTarget(tp,s.tgfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil,tp)
 end
