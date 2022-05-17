280d3d817f28fa6d3e4f2e64ae37e3a68eb6a38e
Lyris <Lyris12@users.noreply.github.com>
2022-05-17 08:48:48 -0400

    Update c101108091.lua

--- scripts/DIFO-EN/c101108091.lua
+++ scripts/DIFO-EN/c101108091.lua
@@ -1,5 +1,6 @@
 --Libromancer Prevented
-function c101108091.initial_effect(c)
+local s,id,o=GetID()
+function s.initial_effect(c)
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
