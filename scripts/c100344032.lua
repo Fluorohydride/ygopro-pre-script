eb58856ece94551fcc6594ff66698c43faa506cb
Lyris <Lyris12@users.noreply.github.com>
2022-06-01 15:15:26 -0400

    Add new Structure Deck cards
    "Crystal Protection"
    "Crystal Grace"
    "Crystal Miracle"

---
+++ scripts/c100344032.lua
@@ -0,0 +1,77 @@
+--Crystal Grace
+local s,id,o=GetID()
+function s.initial_effect(c)
+	--Special Summon up to 2 "Crystal Beast" Monster Cards from your Spell & Trap Zone, and if you do, gain LP equal to their total original ATK.
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_RECOVER)
+	e1:SetTarget(s.tg)
+	e1:SetOperation(s.op)
+	c:RegisterEffect(e1)
+	--If a "Crystal Beast" card(s) is placed in your Spell & Trap Zone while this card is in your GY (even during the Damage Step): You can banish this card; excavate the top card of your Deck, and if it is a "Crystal Beast" monster, either add it to your hand or Special Summon it. Otherwise, send it to the GY. You can only use this effect of "Crystal Grace" once per turn.
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetCode(EVENT_MOVE)
+	e2:SetRange(LOCATION_GRAVE)
+	e2:SetCountLimit(1,id)
+	e2:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_DAMAGE_STEP)
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_SEARCH+CATEGORY_TOHAND+CATEGORY_DECKDES)
+	e2:SetCondition(s.con)
+	e2:SetCost(aux.bfgcost)
+	e2:SetTarget(s.stg)
+	e2:SetOperation(s.sop)
+	c:RegisterEffect(e2)
+end
+function s.filter(c,e,tp)
+	return c:IsFaceup() and c:IsSetCard(0x1034) and c:GetOriginalType()&TYPE_MONSTER>0 and c:GetSequence()<5
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.tg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_SZONE,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_SZONE)
+	Duel.SetOperationInfo(0,CATEGORY_RECOVER,nil,0,tp,0)
+end
+function s.op(e,tp,eg,ep,ev,re,r,rp)
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	if ft<=0 then return end
+	if Duel.IsPlayerAffectedByEffect(tp,59822133) then ft=1 end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_SZONE,0,1,math.min(ft,2),nil,e,tp)
+	Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+	local atk=0
+	for tc in aux.Next(g) do atk=atk+tc:GetBaseAttack() end
+	Duel.Recover(tp,atk,REASON_EFFECT)
+end
+function s.cfilter(c)
+	return c:IsFaceup() and c:IsSetCard(0x1034) and c:IsLocation(LOCATION_SZONE) and c:GetSequence()<5
+end
+function s.con(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(s.cfilter,1,nil)
+end
+function s.stg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetFieldGroupCount(tp,LOCATION_DECK,0)>0
+		and (Duel.GetDecktopGroup(tp,1):IsExists(Card.IsAbleToHand,1,nil)
+		or Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.IsPlayerCanSpecialSummon(tp)
+		and not Duel.IsPlayerAffectedByEffect(tp,63060238) and not Duel.IsPlayerAffectedByEffect(tp,97148796)) end
+end
+function s.sop(e,tp,eg,ep,ev,re,r,rp)
+	if not Duel.IsPlayerCanDiscardDeck(tp,1) then return end
+	Duel.ConfirmDecktop(tp,1)
+	local tc=Duel.GetDecktopGroup(tp,1):GetFirst()
+	local ft,b1=Duel.GetLocationCount(tp,LOCATION_MZONE),tc:IsAbleToHand()
+	local b2=ft>0 and tc:IsCanBeSpecialSummoned(e,0,tp,false,false)
+	if tc:IsSetCard(0x1034) and tc:IsType(TYPE_MONSTER) and (b1 or b2) then
+		if b1 and (not b2 or Duel.SelectOption(tp,1190,1152)==0) then
+			Duel.DisableShuffleCheck()
+			Duel.SendtoHand(tc,nil,REASON_EFFECT)
+		else
+			Duel.DisableShuffleCheck()
+			Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
+		end
+	else
+		Duel.DisableShuffleCheck()
+		Duel.SendtoGrave(tc,REASON_EFFECT+REASON_REVEAL)
+	end
+end
---
+++ scripts/c100344033.lua
@@ -0,0 +1,74 @@
+--Crystal Miracle
+local s,id,o=GetID()
+function s.initial_effect(c)
+	--When a Spell/Trap Card, or monster effect, is activated: Destroy 1 "Crystal Beast" card you control, and if you do, negate the activation, and if you do that, destroy that card.
+	local e4=Effect.CreateEffect(c)
+	e4:SetCategory(CATEGORY_NEGATE+CATEGORY_DESTROY)
+	e4:SetType(EFFECT_TYPE_ACTIVATE)
+	e4:SetCode(EVENT_CHAINING)
+	e4:SetCondition(s.con)
+	e4:SetTarget(s.tg)
+	e4:SetOperation(s.act)
+	c:RegisterEffect(e4)
+	--If a "Crystal Beast" card(s) is placed in your Spell & Trap Zone while this card is in your GY (even during the Damage Step): You can banish this card; place 1 "Crystal Beast" monster from your hand, Deck, or GY face-up in your Spell & Trap Zone as a Continuous Spell. You can only use this effect of "Crystal Miracle" once per turn.
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetCode(EVENT_MOVE)
+	e2:SetRange(LOCATION_GRAVE)
+	e2:SetCountLimit(1,id)
+	e2:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_DAMAGE_STEP)
+	e2:SetCategory()
+	e2:SetCondition(s.pcon)
+	e2:SetCost(aux.bfgcost)
+	e2:SetTarget(s.ptg)
+	e2:SetOperation(s.op)
+	c:RegisterEffect(e2)
+end
+function s.con(e,tp,eg,ep,ev,re,r,rp)
+	return (re:IsHasType(EFFECT_TYPE_ACTIVATE) or re:IsActiveType(TYPE_MONSTER)) and Duel.IsChainNegatable(ev)
+end
+function s.tg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local g=Duel.GetMatchingGroup(aux.AND(Card.IsFaceup,Card.IsSetCard),tp,LOCATION_ONFIELD,0,nil,0x1034)
+	if chk==0 then return #g>0 end
+	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,g,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
+	if re:GetHandler():IsDestructable() and re:GetHandler():IsRelateToEffect(re) then
+		Duel.SetOperationInfo(0,CATEGORY_DESTROY,eg,1,0,0)
+	end
+end
+function s.act(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local g=Duel.SelectMatchingCard(tp,aux.AND(Card.IsFaceup,Card.IsSetCard),tp,LOCATION_ONFIELD,0,1,1,nil,0x1034)
+	if Duel.Destroy(g,REASON_EFFECT)>0 and Duel.NegateActivation(ev) and re:GetHandler():IsRelateToEffect(re) then
+		Duel.Destroy(eg,REASON_EFFECT)
+	end
+end
+function s.cfilter(c)
+	return c:IsFaceup() and c:IsSetCard(0x1034) and c:IsLocation(LOCATION_SZONE) and c:GetSequence()<5
+end
+function s.con(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(s.cfilter,1,nil)
+end
+function s.filter(c)
+	return c:IsSetCard(0x1034) and c:IsType(TYPE_MONSTER) and not c:IsForbidden()
+end
+function s.ptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK+LOCATION_GRAVE+LOCATION_HAND,0,1,nil)
+		and Duel.GetLocationCount(tp,LOCATION_SZONE)>0 end
+end
+function s.op(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_SZONE)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOFIELD)
+	local g=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.filter),tp,LOCATION_DECK+LOCATION_GRAVE+LOCATION_HAND,0,1,1,nil)
+	local tc=g:GetFirst()
+	if tc then
+		Duel.MoveToField(tc,tp,tp,LOCATION_SZONE,POS_FACEUP,true)
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetCode(EFFECT_CHANGE_TYPE)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD-RESET_TURN_SET)
+		e1:SetValue(TYPE_SPELL+TYPE_CONTINUOUS)
+		tc:RegisterEffect(e1)
+	end
+end
