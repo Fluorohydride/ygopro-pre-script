614eec59af57f79647848b682a896056496eb6d2
wzh31891579 <31891579@qq.com>
2022-06-01 11:10:26 +0800

    add c100290031.lua

---
+++ scripts/AC02-JP/c100290031.lua
@@ -0,0 +1,101 @@
+--EMオッドアイズ・プリースト
+--
+--Script by Trishula9
+function c100290031.initial_effect(c)
+	aux.EnablePendulumAttribute(c)
+	--to hand
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_DESTROY)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_PZONE)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetCountLimit(1,100290031)
+	e1:SetTarget(c100290031.thtg)
+	e1:SetOperation(c100290031.thop)
+	c:RegisterEffect(e1)
+	--spsummon
+	local e2=Effect.CreateEffect(c)
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetCountLimit(1,100290031+100)
+	e2:SetCondition(c100290031.spcon)
+	e2:SetCost(c100290031.spcost)
+	e2:SetTarget(c100290031.sptg)
+	e2:SetOperation(c100290031.spop)
+	c:RegisterEffect(e2)
+	--to extra or grave
+	local e3=Effect.CreateEffect(c)
+	e3:SetCategory(CATEGORY_TOEXTRA+CATEGORY_TOGRAVE)
+	e3:SetType(EFFECT_TYPE_IGNITION)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCountLimit(1,100290031+100)
+	e3:SetCondition(c100290031.tgcon)
+	e3:SetTarget(c100290031.tgtg)
+	e3:SetOperation(c100290031.tgop)
+	c:RegisterEffect(e3)
+end
+function c100290031.thfilter(c)
+	return c:IsSetCard(0x9f,0x99) and c:IsAbleToHand()
+end
+function c100290031.thtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and c100290031.thfilter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(c100290031.thfilter,tp,LOCATION_GRAVE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectTarget(tp,c100290031.thfilter,tp,LOCATION_GRAVE,0,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,0,0)
+end
+function c100290031.thop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) and Duel.SendtoHand(tc,nil,REASON_EFFECT)>0 and tc:IsLocation(LOCATION_HAND) then
+		Duel.BreakEffect()
+		Duel.Destroy(e:GetHandler(),REASON_EFFECT)
+	end
+end
+function c100290031.spcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsSummonType(SUMMON_TYPE_PENDULUM) and e:GetHandler():IsAttackPos()
+end
+function c100290031.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsAbleToRemoveAsCost() end
+	Duel.Remove(e:GetHandler(),POS_FACEUP,REASON_COST)
+end
+function c100290031.spfilter(c,e,tp)
+	return c:IsSetCard(0x9f,0x99) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function c100290031.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and c100290031.spfilter(chkc,e,tp) end
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingTarget(c100290031.spfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectTarget(tp,c100290031.spfilter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
+end
+function c100290031.spop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) then
+		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
+function c100290031.tgcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsSummonType(SUMMON_TYPE_PENDULUM) and e:GetHandler():IsDefensePos()
+end
+function c100290031.tgfilter(c)
+	return c:IsSetCard(0x9f,0x99) and c:IsType(TYPE_PENDULUM)
+end
+function c100290031.tgtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c100290031.tgfilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOEXTRA,nil,1,tp,LOCATION_DECK)
+end
+function c100290031.tgop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_OPERATECARD)
+	local g=Duel.SelectMatchingCard(tp,c100290031.tgfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if g:GetCount()<=0 then return end
+	local tc=g:GetFirst()
+	local res=false
+	if tc:IsAbleToGrave() and Duel.SelectOption(tp,aux.Stringid(100290031,0),1191)==1 then
+		Duel.SendtoGrave(tc,REASON_EFFECT)
+	else
+		Duel.SendtoExtraP(tc,tp,REASON_EFFECT)
+	end
+end
