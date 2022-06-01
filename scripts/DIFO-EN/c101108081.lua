3a8c076ee3d9e31eaff1d61977e5c8ababaf3e35
Lyris <Lyris12@users.noreply.github.com>
2022-06-01 15:19:55 -0400

    Fix cards
    "Libromancer":
    * "Realized" 2
    * "Bonded"
    "Colonel on C-String"

--- scripts/DIFO-EN/c101108081.lua
+++ scripts/DIFO-EN/c101108081.lua
@@ -14,14 +14,14 @@ function s.filter(c,e,tp)
 	return c:IsLevelBelow(4) and c:IsRace(RACE_WARRIOR) and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP_DEFENSE)
 end
 function s.tg(e,tp,eg,ep,ev,re,r,rp,chk)
-	local g=Duel.GetMatchingGroup(s.filter,tp,LOCATION_HAND,0,1,nil)
+	local g=Duel.GetMatchingGroup(s.filter,tp,LOCATION_HAND,0,nil,e,tp)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>1 and g:CheckSubGroup(aux.dncheck,2,2)
-		and not Duel.IsPlayerAffectedByEffect(tp,BESpirit) end
+		and not Duel.IsPlayerAffectedByEffect(tp,59822133) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,2,tp,LOCATION_HAND)
 end
 function s.op(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.GetMatchingGroup(s.filter,tp,LOCATION_HAND,0,1,nil)
-	if Duel.GetLocationCount(tp,LOCATION_MZONE)<2 or Duel.IsPlayerAffectedByEffect(tp,BESpirit) then return end
+	local g=Duel.GetMatchingGroup(s.filter,tp,LOCATION_HAND,0,nil,e,tp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<2 or Duel.IsPlayerAffectedByEffect(tp,59822133) then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 	Duel.SpecialSummon(g:SelectSubGroup(tp,aux.dncheck,false,2,2),0,tp,tp,false,false,POS_FACEUP_DEFENSE)
 end
--- scripts/DIFO-EN/c101108089.lua
+++ scripts/DIFO-EN/c101108089.lua
@@ -1,12 +1,15 @@
 --Libromancer Bonded
 local s,id,o=GetID()
 function s.initial_effect(c)
+	aux.AddCodeList(c,45001322,88106656)
 	--This card can be used to Ritual Summon any "Libromancer" Ritual Monster. You must also Tribute monsters from your hand or field whose total Levels equal or exceed the Level of the Ritual Monster you Ritual Summon. If you Ritual Summon "Libromancer Fireburst" with this effect, using "Libromancer Firestarter" on the field, it cannot be destroyed, or banished, by card effects.
 	aux.AddRitualProcGreater(c,aux.FilterBoolFunction(Card.IsSetCard,0x17c),LOCATION_HAND,nil,nil,false,s.op)
 end
+function s.filter(c)
+	return c:IsCode(45001322) and c:IsPreviousLocation(LOCATION_ONFIELD)
+end
 function s.op(e,tp,eg,ep,ev,re,r,rp,tc,g)
-	if not (g:IsExists(aux.AND(Card.IsCode,Card.IsOnField),1,nil,45001322)
-		and tc:IsCode(101108087,id-2)) then return end
+	if not (g:IsExists(s.filter,1,nil) and tc:IsCode(88106656)) then return end
 	local c=e:GetHandler()
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_SINGLE)
@@ -16,8 +19,9 @@ function s.op(e,tp,eg,ep,ev,re,r,rp,tc,g)
 	tc:RegisterEffect(e2)
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
 	e1:SetCode(EFFECT_CANNOT_REMOVE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
 	e1:SetTargetRange(1,1)
 	e1:SetTarget(s.rmlimit)
 	e1:SetReset(RESET_EVENT+RESETS_STANDARD)
