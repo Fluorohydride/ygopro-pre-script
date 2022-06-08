dc9eee09ce5c7e67a326b47a95f651fdbb0ec77c
Mercury233 <me@mercury233.me>
2022-06-08 08:38:35 +0800

    Update c100290030.lua

--- scripts/AC02-JP/c100290030.lua
+++ scripts/AC02-JP/c100290030.lua
@@ -2,7 +2,7 @@
 --Ninjitsu Art of Mosquito Marching
 --Scripted by Yuzurisa
 function c100290030.initial_effect(c)
-	aux.AddCodeList(c,100290029)
+	--special summon
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(100290030,0))
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
@@ -12,6 +12,7 @@ function c100290030.initial_effect(c)
 	e1:SetTarget(c100290030.sptg)
 	e1:SetOperation(c100290030.spop)
 	c:RegisterEffect(e1)
+	--counter
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(100290030,1))
 	e2:SetCategory(CATEGORY_COUNTER)
@@ -25,7 +26,6 @@ function c100290030.initial_effect(c)
 	e2:SetOperation(c100290030.counterop)
 	c:RegisterEffect(e2)
 end
-
 function c100290030.spfilter(c,e,tp)
 	return c:IsLevelBelow(4) and c:IsRace(RACE_INSECT) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
@@ -36,48 +36,61 @@ function c100290030.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 end
 function c100290030.spop(e,tp,eg,ep,ev,re,r,rp)
 	local g=Duel.GetMatchingGroup(c100290030.spfilter,tp,LOCATION_HAND,0,nil,e,tp)
-	local ft=math.min(Duel.GetLocationCount(tp,LOCATION_MZONE),#g,2)
-	if ft==0 then return end
+	local ft=math.min((Duel.GetLocationCount(tp,LOCATION_MZONE)),#g,2)
+	if ft<=0 then return end
 	if Duel.IsPlayerAffectedByEffect(tp,59822133) then ft=1 end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 	local sg=g:SelectSubGroup(tp,c100290030.spcheck,false,1,ft)
-	if #sg>0 then
+	if sg then
 		Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP)
 	end
 end
 function c100290030.spcheck(sg)
 	return sg:GetClassCount(Card.GetLevel)==1
 end
-function c100290030.counter_filter(c)
+function c100290030.cfilter(c)
 	return c:IsFaceup() and c:IsCode(100290029)
 end
 function c100290030.countercond(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.IsExistingMatchingCard(c100290030.counter_filter,tp,LOCATION_ONFIELD,0,1,nil)
+	return Duel.IsExistingMatchingCard(c100290030.cfilter,tp,LOCATION_ONFIELD,0,1,nil)
+end
+function c100290030.ctfilter(c)
+	return c:IsFaceup() and c:IsRace(RACE_INSECT)
+end
+function c100290030.tgfilter(c)
+	return c:IsFaceup() and c:IsCanAddCounter(0x1161,1)
 end
 function c100290030.countertg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) and chkc:IsFaceup() end
-	local ct=Duel.GetMatchingGroupCount(c100290030.counter_filter,tp,LOCATION_MZONE,0,nil)
-	if chk==0 then return ct>0 and Duel.IsExistingTarget(Card.IsFaceup,tp,0,LOCATION_MZONE,1,nil) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
-	local g=Duel.SelectTarget(tp,Card.IsFaceup,tp,0,LOCATION_MZONE,1,ct,nil)
-	Duel.SetOperationInfo(0,CATEGORY_COUNTER,g,1,0,0x1101)
+	local ct=Duel.GetMatchingGroupCount(c100290030.ctfilter,tp,LOCATION_MZONE,0,nil)
+	if chk==0 then return ct>0 and Duel.IsExistingTarget(c100290030.tgfilter,tp,0,LOCATION_MZONE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_COUNTER)
+	local g=Duel.SelectTarget(tp,c100290030.tgfilter,tp,0,LOCATION_MZONE,1,ct,nil)
+	Duel.SetOperationInfo(0,CATEGORY_COUNTER,nil,0,0,0)
 end
-function c100290030.disable_condition(e)
-	return e:GetHandler():GetCounter(0x1101)>0
+function c100290030.ctcon(e)
+	return e:GetHandler():GetCounter(0x1161)>0
 end
 function c100290030.counterop(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.GetTargetCards(e)
+	local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS):Filter(Card.IsRelateToEffect,nil,e)
 	if #g==0 then return end
 	local c=e:GetHandler()
 	for tc in aux.Next(g) do
-		if tc:AddCounter(0x1101,1) then
+		if tc:AddCounter(0x1161,1) then
 			--Negate its effects
 			local e1=Effect.CreateEffect(c)
 			e1:SetType(EFFECT_TYPE_SINGLE)
 			e1:SetCode(EFFECT_DISABLE)
-			e1:SetCondition(c100290030.disable_condition)
+			e1:SetCondition(c100290030.ctcon)
 			e1:SetReset(RESET_EVENT+RESETS_STANDARD)
 			tc:RegisterEffect(e1)
+			local e2=Effect.CreateEffect(c)
+			e2:SetType(EFFECT_TYPE_SINGLE)
+			e2:SetCode(EFFECT_DISABLE_EFFECT)
+			e2:SetValue(RESET_TURN_SET)
+			e2:SetCondition(c100290030.ctcon)
+			e2:SetReset(RESET_EVENT+RESETS_STANDARD)
+			tc:RegisterEffect(e2)
 		end
 	end
 end
