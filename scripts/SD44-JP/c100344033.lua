94c0cac8f883792ebf7a6dab61687037a973254a
mercury233 <me@mercury233.me>
2022-06-24 15:28:28 +0800

    fix Crystal Miracle
    close #979

--- scripts/SD44-JP/c100344033.lua
+++ scripts/SD44-JP/c100344033.lua
@@ -1,6 +1,7 @@
 --宝玉の奇跡
 --Crystal Miracle
---Script by Lyris12
+--Script by Lyris12 & mercury233
+--not fully implemented
 local s,id,o=GetID()
 function s.initial_effect(c)
 	--negate
@@ -8,30 +9,39 @@ function s.initial_effect(c)
 	e1:SetCategory(CATEGORY_NEGATE+CATEGORY_DESTROY)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_CHAINING)
-	e1:SetCondition(s.con)
-	e1:SetTarget(s.tg)
-	e1:SetOperation(s.act)
+	e1:SetCondition(s.condition)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 	--place
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_QUICK_O)
-	e2:SetCode(EVENT_MOVE)
+	e2:SetCode(EVENT_CUSTOM+id)
 	e2:SetRange(LOCATION_GRAVE)
 	e2:SetCountLimit(1,id)
 	e2:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_DAMAGE_STEP)
-	e2:SetCondition(s.pcon)
+	e2:SetCondition(s.placecon)
 	e2:SetCost(aux.bfgcost)
-	e2:SetTarget(s.ptg)
-	e2:SetOperation(s.op)
+	e2:SetTarget(s.placetg)
+	e2:SetOperation(s.placeop)
 	c:RegisterEffect(e2)
+	if not s.global_check then
+		s.global_check=true
+		local ge1=Effect.CreateEffect(c)
+		ge1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		ge1:SetCode(EVENT_MOVE)
+		ge1:SetCondition(s.regcon)
+		ge1:SetOperation(s.regop)
+		Duel.RegisterEffect(ge1,0)
+	end
 end
-function s.con(e,tp,eg,ep,ev,re,r,rp)
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
 	return (re:IsHasType(EFFECT_TYPE_ACTIVATE) or re:IsActiveType(TYPE_MONSTER)) and Duel.IsChainNegatable(ev)
 end
 function s.desfilter(c)
 	return c:IsFaceup() and c:IsSetCard(0x1034)
 end
-function s.tg(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	local g=Duel.GetMatchingGroup(s.desfilter,tp,LOCATION_ONFIELD,0,nil)
 	if chk==0 then return #g>0 end
 	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
@@ -40,7 +50,7 @@ function s.tg(e,tp,eg,ep,ev,re,r,rp,chk)
 		Duel.SetOperationInfo(0,CATEGORY_DESTROY,g+eg,2,0,0)
 	end
 end
-function s.act(e,tp,eg,ep,ev,re,r,rp)
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
 	local g=Duel.SelectMatchingCard(tp,s.desfilter,tp,LOCATION_ONFIELD,0,1,1,nil)
 	if Duel.Destroy(g,REASON_EFFECT)>0
@@ -51,17 +61,29 @@ end
 function s.cfilter(c,tp)
 	return c:IsFaceup() and c:IsSetCard(0x1034) and c:IsControler(tp) and c:IsLocation(LOCATION_SZONE) and c:GetSequence()<5
 end
-function s.pcon(e,tp,eg,ep,ev,re,r,rp)
-	return eg:IsExists(s.cfilter,1,nil,tp)
+function s.regcon(e,tp,eg,ep,ev,re,r,rp)
+	local v=0
+	if eg:IsExists(s.cfilter,1,nil,0) then v=v+1 end
+	if eg:IsExists(s.cfilter,1,nil,1) then v=v+2 end
+	if v==0 then return false end
+	e:SetLabel(({0,1,PLAYER_ALL})[v])
+	return true
+end
+function s.regop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(Card.IsCode,tp,LOCATION_GRAVE,LOCATION_GRAVE,nil,id)
+	Duel.RaiseEvent(g,EVENT_CUSTOM+id,re,r,rp,ep,e:GetLabel())
+end
+function s.placecon(e,tp,eg,ep,ev,re,r,rp)
+	return (ev==tp or ev==PLAYER_ALL) and eg:IsContains(e:GetHandler())
 end
 function s.filter(c)
 	return c:IsSetCard(0x1034) and c:IsType(TYPE_MONSTER) and not c:IsForbidden()
 end
-function s.ptg(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.placetg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK+LOCATION_GRAVE+LOCATION_HAND,0,1,nil)
 		and Duel.GetLocationCount(tp,LOCATION_SZONE)>0 end
 end
-function s.op(e,tp,eg,ep,ev,re,r,rp)
+function s.placeop(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(tp,LOCATION_SZONE)<=0 then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOFIELD)
 	local g=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.filter),tp,LOCATION_DECK+LOCATION_GRAVE+LOCATION_HAND,0,1,1,nil)
