529a0614fc4b5a7037d8bb68ea0e439db21e2ccd
Mercury233 <me@mercury233.me>
2022-07-26 09:18:51 +0800

    Update c100200201.lua

--- scripts/VJMP-JP/c100200201.lua
+++ scripts/VJMP-JP/c100200201.lua
@@ -1,40 +1,42 @@
+--ダイダラボッチ
 --Daidara-Bocchi
+--Script by Lyris12
 local s,id,o=GetID()
 function s.initial_effect(c)
-	--Cannot be Special Summoned.
+	--cannot special summon
 	local e1=Effect.CreateEffect(c)
 	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetCode(EFFECT_SPSUMMON_CONDITION)
 	c:RegisterEffect(e1)
-	--You can Tribute Summon this card face-up by Tributing 1 Zombie monster.e2
+	--tribute 1
 	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
 	e2:SetType(EFFECT_TYPE_SINGLE)
 	e2:SetCode(EFFECT_SUMMON_PROC)
 	e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
-	e2:SetDescription(aux.Stringid(id,0))
 	e2:SetCondition(s.otcon)
 	e2:SetOperation(s.otop)
 	e2:SetValue(SUMMON_TYPE_ADVANCE)
 	c:RegisterEffect(e2)
-	--Gains 200 ATK for every other Zombie monster you control.
+	--atk up
 	local e3=Effect.CreateEffect(c)
 	e3:SetType(EFFECT_TYPE_SINGLE)
 	e3:SetCode(EFFECT_UPDATE_ATTACK)
 	e3:SetRange(LOCATION_MZONE)
 	e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-	e3:SetValue(function() return 200*Duel.GetMatchingGroupCount(aux.AND(Card.IsFaceup,Card.IsRace),c:GetControler(),LOCATION_MZONE,0,c,RACE_ZOMBIE) end)
+	e3:SetValue(s.atkval)
 	c:RegisterEffect(e3)
-	--At the start of your Main Phase 1, if this card, and 2 or more Zombie monsters other than "Daidara-Bocchi", are in your GY: You can discard 1 card; add this card to your hand.
+	--to hand
 	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(id,1))
+	e4:SetCategory(CATEGORY_TOHAND)
 	e4:SetType(EFFECT_TYPE_IGNITION)
 	e4:SetRange(LOCATION_GRAVE)
-	e4:SetCategory(CATEGORY_TOHAND)
-	e4:SetDescription(aux.Stringid(id,1))
-	e4:SetCondition(function(_,tp) return Duel.GetCurrentPhase()==PHASE_MAIN1 and not Duel.CheckPhaseActivity() and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_GRAVE,0,2,nil) end)
-	e4:SetCost(s.cost)
-	e4:SetTarget(s.tg)
-	e4:SetOperation(s.op)
+	e4:SetCondition(s.thcon)
+	e4:SetCost(s.thcost)
+	e4:SetTarget(s.thtg)
+	e4:SetOperation(s.thop)
 	c:RegisterEffect(e4)
 end
 function s.otfilter(c,tp)
@@ -51,19 +53,29 @@ function s.otop(e,tp,eg,ep,ev,re,r,rp,c)
 	local sg=Duel.SelectTribute(tp,c,1,1,mg)
 	Duel.Release(sg,REASON_SUMMON+REASON_MATERIAL)
 end
-function s.filter(c)
+function s.atkfilter(c)
+	return c:IsFaceup() and c:IsRace(RACE_ZOMBIE)
+end
+function s.atkval(e)
+	return Duel.GetMatchingGroupCount(s.atkfilter,e:GetHandlerPlayer(),LOCATION_MZONE,0,e:GetHandler())*200
+end
+function s.cfilter(c)
 	return c:IsRace(RACE_ZOMBIE) and not c:IsCode(id)
 end
-function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.thcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetCurrentPhase()==PHASE_MAIN1 and not Duel.CheckPhaseActivity()
+		and Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_GRAVE,0,2,nil)
+end
+function s.thcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,1,nil) end
 	Duel.DiscardHand(tp,Card.IsDiscardable,1,1,REASON_COST)
 end
-function s.tg(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
 	if chk==0 then return c:IsAbleToHand() end
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,c,1,0,0)
 end
-function s.op(e,tp,eg,ep,ev,re,r,rp)
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	if c:IsRelateToChain(0) then Duel.SendtoHand(c,nil,REASON_EFFECT) end
+	if c:IsRelateToEffect(e) then Duel.SendtoHand(c,nil,REASON_EFFECT) end
 end
