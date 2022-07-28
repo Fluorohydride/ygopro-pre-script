6390a10f70293bc8b9db67cea7d2a2799182d3e3
Lyris <Lyris12@users.noreply.github.com>
2022-07-28 05:27:35 -0400

    add "Ghoti" cards from POTE-EN
    * `Ghoti of the Deep Beyond`
    * `Ghoti Chain`
    * `Askaan, the Bicorned Ghoti`
    * `Eanoc, Sentry of the Ghoti`
    * `Paces, Light of the Ghoti`
    * `Shif, Fairy of the Ghoti`
    * `The Most Distant, Deepest Depths`

---
+++ scripts/POTE-EN/c101109091.lua
@@ -0,0 +1,54 @@
+--Ghoti Chain
+--Script by Lyris12
+local s,id,o=GetID()
+function s.initial_effect(c)
+	--spsummon
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCountLimit(1,id+EFFECT_COUNT_CODE_OATH)
+	e1:SetCost(s.cost)
+	e1:SetTarget(s.tg)
+	e1:SetOperation(s.op)
+	c:RegisterEffect(e1)
+end
+function s.cost(e)
+	e:SetLabel(1)
+	return true
+end
+function s.cfilter(c,e,tp)
+	return c:IsFaceup() and c:IsRace(RACE_FISH) and c:IsAbleToRemoveAsCost() and Duel.GetMZoneCount(tp,c)>0
+		and Duel.IsExistingMatchingCard(s.filter,tp,0x33,0,1,nil,e,tp,c:GetOriginalCode())
+end
+function s.filter(c,e,tp,code)
+	return (c:IsFaceup() or not c:IsLocation(LOCATION_REMOVED)) and c:IsSetCard(0x28b)
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false) and not c:IsOriginalCodeRule(code)
+end
+function s.tg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local l=e:GetLabel()==1
+	if chk==0 then
+		e:SetLabel(0)
+		return l and Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_MZONE,0,1,nil,e,tp)
+	end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local tc=Duel.SelectMatchingCard(tp,s.cfilter,tp,LOCATION_MZONE,0,1,1,nil,e,tp):GetFirst()
+	Duel.Remove(tc,POS_FACEUP,REASON_COST)
+	e:SetLabel(tc:GetOriginalCode())
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,0x33)
+end
+function s.op(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<1 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.filter),tp,0x33,0,1,1,nil,e,tp,e:GetLabel())
+	if Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)>0 then
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_LEAVE_FIELD_REDIRECT)
+		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e1:SetReset(RESET_EVENT+RESETS_REDIRECT)
+		e1:SetValue(LOCATION_REMOVED)
+		g:GetFirst():RegisterEffect(e1,true)
+	end
+end
