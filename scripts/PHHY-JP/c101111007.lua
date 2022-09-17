6511b9e49d8571dc754b2190756348945a14348e
Mercury233 <me@mercury233.me>
2022-09-17 21:53:49 +0800

    add PHHY by 奥克斯 (#1012)

---
+++ scripts/PHHY-JP/c101111007.lua
@@ -0,0 +1,91 @@
+--スケアクロー・クシャトリラ
+--Script by 奥克斯
+local s,id,o=GetID()
+function s.initial_effect(c)
+	--spsummon
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_REMOVE)
+	e1:SetType(EFFECT_TYPE_QUICK_O)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetHintTiming(0,TIMINGS_CHECK_MONSTER+TIMING_MAIN_END)
+	e1:SetCountLimit(1,id)
+	e1:SetCondition(s.spcon)
+	e1:SetTarget(s.sptg)
+	e1:SetOperation(s.spop)
+	c:RegisterEffect(e1)
+	--defense attack
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetCode(EFFECT_DEFENSE_ATTACK)
+	e2:SetValue(1)
+	c:RegisterEffect(e2)
+	--bp disable
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e3:SetCode(EVENT_ATTACK_ANNOUNCE)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e3:SetCondition(s.discon)
+	e3:SetOperation(s.disop)
+	c:RegisterEffect(e3)
+	local e4=e3:Clone()
+	e4:SetCode(EVENT_BE_BATTLE_TARGET)
+	c:RegisterEffect(e4)
+	local e5=Effect.CreateEffect(c)
+	e5:SetType(EFFECT_TYPE_FIELD)
+	e5:SetCode(EFFECT_DISABLE)
+	e5:SetRange(LOCATION_MZONE)
+	e5:SetTargetRange(0,LOCATION_MZONE)
+	e5:SetTarget(s.distg)
+	c:RegisterEffect(e5)
+	local e6=e5:Clone()
+	e6:SetCode(EFFECT_DISABLE_EFFECT)
+	c:RegisterEffect(e6)
+end
+function s.spcon(e,tp,eg,ep,ev,re,r,rp)
+	local ph=Duel.GetCurrentPhase()
+	return ph==PHASE_MAIN1 or ph==PHASE_MAIN2
+end
+function s.rmfilter(c)
+	return c:IsSetCard(0x189,0x17a) and c:IsAbleToRemove()
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	local g=Duel.GetMatchingGroup(s.rmfilter,tp,LOCATION_HAND+LOCATION_GRAVE,0,c)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false) and #g>0 end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_REMOVE,g,1,0,0)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e)
+		and Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)>0 then
+		local g=Duel.GetMatchingGroup(aux.NecroValleyFilter(s.rmfilter),tp,LOCATION_HAND+LOCATION_GRAVE,0,nil)
+		if #g>0 then
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+			local sg=g:Select(tp,1,1,nil)
+			Duel.Remove(sg,POS_FACEUP,REASON_EFFECT)
+		end
+	end
+end
+function s.cfilter(c,tp)
+	return c:IsFaceup() and c:IsSetCard(0x189,0x17a) and c:IsControler(tp)
+end
+function s.discon(e,tp,eg,ep,ev,re,r,rp)
+	local c=Duel.GetAttackTarget()
+	if not c then return false end
+	if c:IsControler(1-tp) then c=Duel.GetAttacker() end
+	return c and s.cfilter(c,tp)
+end
+function s.disop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tc=Duel.GetAttackTarget()
+	if tc:IsControler(tp) then tc=Duel.GetAttacker() end
+	tc:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,0,1)
+	Duel.AdjustInstantly(c)
+end
+function s.distg(e,c)
+	return c:GetFlagEffect(id)~=0
+end
---
+++ scripts/PHHY-JP/c101111075.lua
@@ -0,0 +1,95 @@
+--六世壊根清净
+--Script by 奥克斯
+local s,id,o=GetID()
+function s.initial_effect(c)
+	--activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_REMOVE)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetHintTiming(0,TIMINGS_CHECK_MONSTER+TIMING_END_PHASE)
+	e1:SetCountLimit(1,id)
+	e1:SetCondition(s.condition)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+	--xyzmat to hand
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_TOHAND)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_REMOVE)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DELAY)
+	e2:SetCountLimit(1,id+o)
+	e2:SetTarget(s.thtg)
+	e2:SetOperation(s.thop)
+	c:RegisterEffect(e2)
+end
+function s.cfilter(c)
+	return c:IsFaceup() and c:IsSetCard(0x189) and c:IsType(TYPE_XYZ)
+end
+function s.condition(e)
+	return Duel.IsExistingMatchingCard(s.cfilter,0,LOCATION_MZONE,LOCATION_MZONE,1,nil)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	local g1=Duel.GetFieldGroup(tp,LOCATION_MZONE,0)
+	local g2=Duel.GetFieldGroup(tp,0,LOCATION_MZONE)
+	local b1=#g1>1 and g1:IsExists(Card.IsAbleToRemove,1,nil,tp,POS_FACEDOWN,REASON_RULE)
+	local b2=#g2>1 and g2:IsExists(Card.IsAbleToRemove,1,nil,1-tp,POS_FACEDOWN,REASON_RULE)
+	if chk==0 then return Duel.IsPlayerCanRemove(tp) and (b1 or b2) end
+	local g3=g1+g2
+	Duel.SetOperationInfo(0,CATEGORY_REMOVE,g3,1,0,0)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	if not Duel.IsPlayerCanRemove(tp) then return end
+	local g1=Duel.GetFieldGroup(tp,LOCATION_MZONE,0)
+	local g2=Duel.GetFieldGroup(tp,0,LOCATION_MZONE)
+	local b1=#g1>1 and g1:IsExists(Card.IsAbleToRemove,1,nil,tp,POS_FACEDOWN,REASON_RULE)
+	local b2=#g2>1 and g2:IsExists(Card.IsAbleToRemove,1,nil,1-tp,POS_FACEDOWN,REASON_RULE)
+	if b1 then
+		local ct=#g1-1
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+		local sg=g1:FilterSelect(tp,Card.IsAbleToRemove,ct,ct,nil,tp,POS_FACEDOWN,REASON_RULE)
+		Duel.Remove(sg,POS_FACEDOWN,REASON_RULE)
+	end
+	if b2 then
+		local ct=#g2-1
+		Duel.Hint(HINT_SELECTMSG,1-tp,HINTMSG_REMOVE)
+		local sg=g2:FilterSelect(1-tp,Card.IsAbleToRemove,ct,ct,nil,1-tp,POS_FACEDOWN,REASON_RULE)
+		Duel.Remove(sg,POS_FACEDOWN,REASON_RULE)
+	end
+end
+
+function s.thfilter(c)
+	return c:IsType(TYPE_MONSTER) and c:IsSetCard(0x189)
+		and c:IsAbleToHand()
+end
+function s.xfilter(c)
+	return c:IsFaceup() and c:IsType(TYPE_XYZ) and c:IsSetCard(0x189)
+		and c:GetOverlayGroup():IsExists(s.thfilter,1,nil)
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_MZONE) and s.xfilter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(s.xfilter,tp,LOCATION_MZONE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+	Duel.SelectTarget(tp,s.xfilter,tp,LOCATION_MZONE,0,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_OVERLAY)
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	local mg=tc:GetOverlayGroup():Filter(s.thfilter,nil)
+	if tc:IsRelateToEffect(e) and tc:IsFaceup() and #mg>0 then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+		local bc=mg:Select(tp,1,1,nil):GetFirst()
+		if Duel.SendtoHand(bc,nil,REASON_EFFECT)>0
+			and bc:IsLocation(LOCATION_HAND) then
+			Duel.ConfirmCards(1-tp,bc)
+			Duel.ShuffleHand(tp)
+			if bc:IsCanBeSpecialSummoned(e,0,tp,false,false)
+				and Duel.SelectYesNo(tp,aux.Stringid(id,2)) then
+				Duel.SpecialSummon(bc,0,tp,tp,false,false,POS_FACEUP)
+			end
+		end
+	end
+end
