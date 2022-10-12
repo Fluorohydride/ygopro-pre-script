4700bd9f7f9b6d028e1de97d6bf38bf8176b34db
Lyris <lyris89@live.com>
2022-10-12 07:35:55 -0400

    add "Destructive Daruma Karma Cannon"

---
+++ scripts/DABL-EN/c101110084.lua
@@ -0,0 +1,31 @@
+--coded by Lyris
+--Destructive Daruma Karma Cannon
+local s,id,o=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(1111)
+	e1:SetCategory(CATEGORY_POSITION+CATEGORY_TOGRAVE)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	local g=Duel.GetMatchingGroup(Card.IsCanTurnSet,tp,LOCATION_MZONE,LOCATION_MZONE,nil)
+	if chk==0 then return #g>0 end
+	Duel.SetOperationInfo(0,CATEGORY_POSITION,g,#g,0,0)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(Card.IsCanTurnSet,tp,LOCATION_MZONE,LOCATION_MZONE,nil)
+	if Duel.ChangePosition(g,POS_FACEDOWN_DEFENSE)<1 then return end
+	local tg=Duel.GetMatchingGroup(Card.IsFaceup,tp,LOCATION_MZONE,LOCATION_MZONE,nil)
+	for p=0,1 do
+		if not Duel.IsPlayerCanSendtoGrave(p) then tg:Remove(Card.IsControler,nil,p) end
+	end
+	if #tg>0 then
+		Duel.BreakEffect()
+		Duel.SendtoGrave(tg,REASON_RULE)
+	end
+end
