54780aca3ca451f6c43e9bbb6aa5b53cc2d62c77
Lyris <lyris89@live.com>
2022-10-13 15:38:58 -0400

    fix `A World Shrouded in Darkness` 1

--- scripts/PHHY-JP/c101111065.lua
+++ scripts/PHHY-JP/c101111065.lua
@@ -60,7 +60,7 @@ function s.spchk(g,tp)
 	return aux.dncheck(g) and Duel.GetLocationCount(tp,LOCATION_MZONE)>=#g
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and s.filter(chkc) end
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and s.filter(chkc,e,tp) end
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
 		and Duel.IsExistingTarget(s.filter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
 	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
