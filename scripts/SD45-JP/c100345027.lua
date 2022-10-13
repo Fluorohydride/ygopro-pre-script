e97d4ecddd6e304148cc0d31064803900022f539
mercury233 <me@mercury233.me>
2022-10-13 16:42:18 +0800

    new

---
+++ scripts/SD45-JP/c100345027.lua
@@ -0,0 +1,72 @@
+--狂惑の落とし穴
+--Script by 奥克斯
+local s,id,o=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_DESTROY+CATEGORY_REMOVE+CATEGORY_GRAVE_ACTION)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetHintTiming(0,TIMINGS_CHECK_MONSTER+TIMING_END_PHASE)
+	e1:SetCondition(s.condition)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+	if not s.global_check then
+		s.global_check=true
+		local ge1=Effect.CreateEffect(c)
+		ge1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		ge1:SetCode(EVENT_SPSUMMON_SUCCESS)
+		ge1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		ge1:SetOperation(s.checkop)
+		Duel.RegisterEffect(ge1,0)
+	end
+end
+function s.checkop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=eg:GetFirst()
+	while tc do
+		if Duel.GetFlagEffect(tc:GetSummonPlayer(),id)==0 then
+			Duel.RegisterFlagEffect(tc:GetSummonPlayer(),id,RESET_PHASE+PHASE_END,0,1)
+		end
+		if Duel.GetFlagEffect(0,id)>0 and Duel.GetFlagEffect(1,id)>0 then
+			break
+		end
+		tc=eg:GetNext()
+	end
+end
+
+function s.condition(e)
+	local tp=e:GetHandlerPlayer()
+	return Duel.GetFlagEffect(1-tp,id)>0
+end
+function s.filter(c)
+	return c:IsFaceup() and c:GetAttack()>=2000
+end
+function s.cfilter(c)
+	return c:GetType()==TYPE_TRAP and c:IsSetCard(0x4c,0x89)
+end
+function s.rmfilter(c)
+	return c:IsAbleToRemove() and c:IsType(TYPE_MONSTER)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and s.filter(chkc)and chkc:IsControler(1-tp) end
+	if chk==0 then return Duel.IsExistingTarget(s.filter,tp,0,LOCATION_MZONE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESREPLACE)
+	local g=Duel.SelectTarget(tp,s.filter,tp,0,LOCATION_MZONE,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if  tc:IsRelateToEffect(e) 
+		and Duel.Destroy(tc,REASON_EFFECT)>0 then
+		local g=Duel.GetMatchingGroup(s.cfilter,tp,LOCATION_GRAVE,0,nil)
+		local rg=Duel.GetMatchingGroup(aux.NecroValleyFilter(s.rmfilter),tp,0,LOCATION_GRAVE,nil)
+		if #g>0 and #rg>0 and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
+			Duel.BreakEffect()
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+			local mg=rg:Select(tp,1,1,nil)
+			Duel.Remove(mg,POS_FACEUP,REASON_EFFECT)
+		end
+	end
+end
---
+++ scripts/SD45-JP/c100345051.lua
@@ -0,0 +1,109 @@
+--シトリスの蟲惑魔
+--Script by 奥克斯
+local s,id,o=GetID()
+function s.initial_effect(c)
+	--xyz summon
+	aux.AddXyzProcedure(c,nil,4,2)
+	c:EnableReviveLimit()
+	--immune
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCode(EFFECT_IMMUNE_EFFECT)
+	e1:SetCondition(s.imcon)
+	e1:SetValue(s.efilter1)
+	c:RegisterEffect(e1)
+	local e2=e1:Clone()
+	e2:SetValue(s.efilter2)
+	c:RegisterEffect(e2)
+	--Search
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,0))
+	e3:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e3:SetType(EFFECT_TYPE_IGNITION)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCountLimit(1,id)
+	e3:SetCost(s.thcost)
+	e3:SetTarget(s.thtg)
+	e3:SetOperation(s.thop)
+	c:RegisterEffect(e3)
+	--material
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(id,1))
+	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e4:SetProperty(EFFECT_FLAG_DELAY)
+	e4:SetCode(EVENT_TO_GRAVE)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetCountLimit(1,id+o)
+	e4:SetCondition(s.mtcon)
+	e4:SetTarget(s.mttg)
+	e4:SetOperation(s.mtop)
+	c:RegisterEffect(e4)
+	local e5=e4:Clone()
+	e5:SetCode(EVENT_REMOVE)
+	c:RegisterEffect(e5)
+end
+function s.imcon(e)
+	local c=e:GetHandler()
+	return c:GetOverlayCount()>0
+end
+function s.efilter1(e,re)
+	local g=e:GetHandler():GetOverlayGroup():Filter(Card.IsType,nil,TYPE_MONSTER)
+	local race=0
+	for tc in aux.Next(g) do
+		race=race+tc:GetOriginalRace()
+	end
+	local rc=re:GetHandler()
+	return re:GetOwner()~=e:GetOwner() and race~=0 
+		and rc:IsRace(race)
+		and re:IsActivated()
+end
+function s.efilter2(e,te)
+	return te:IsActiveType(TYPE_TRAP)
+end
+
+function s.thcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():CheckRemoveOverlayCard(tp,1,REASON_COST) end
+	e:GetHandler():RemoveOverlayCard(tp,1,1,REASON_COST)
+end
+function s.thfilter(c)
+	return c:IsSetCard(0x108a) and c:IsType(TYPE_MONSTER) and c:IsAbleToHand()
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if g:GetCount()>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end
+
+function s.cfilter(c,tp)
+	return not c:IsType(TYPE_TOKEN) and c:IsType(TYPE_MONSTER)
+		and c:GetOwner()==1-tp and c:IsReason(REASON_EFFECT)
+		and c:IsFaceup()
+end
+function s.mtcon(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(s.cfilter,1,nil,tp) 
+end
+function s.mttg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	local g=eg:Filter(s.cfilter,nil,tp)
+	local mg=g:Filter(Card.IsCanOverlay,nil) 
+	if chk==0 then return #mg>0 end
+end
+function s.mtop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local g=eg:Filter(s.cfilter,nil,tp)
+	local mg=g:Filter(aux.NecroValleyFilter(Card.IsCanOverlay),nil) 
+	if #mg>0 and c:IsRelateToChain() and c:IsType(TYPE_XYZ) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_XMATERIAL)
+		local og=mg:Select(tp,1,1,nil)
+		Duel.Overlay(c,og)
+	end
+end
