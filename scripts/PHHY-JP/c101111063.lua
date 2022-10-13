f5e73a37293a52564061b046857667ad5a09f27f
Lyris <lyris89@live.com>
2022-10-13 03:43:16 -0400

    fix `Abyss Actors' Dress Rehearsal` 1

--- scripts/PHHY-JP/c101111063.lua
+++ scripts/PHHY-JP/c101111063.lua
@@ -39,7 +39,7 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 		Duel.SendtoHand(sg1,nil,REASON_EFFECT)
 		Duel.ConfirmCards(1-tp,sg1)
 	end
-	local e1=Effect.CreateEffect(c)
+	local e1=Effect.CreateEffect(e:GetHandler())
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
 	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
