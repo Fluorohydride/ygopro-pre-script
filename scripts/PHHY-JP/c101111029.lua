38b2d7dc54ad91319773853461b637ef721e7325
mercury233 <me@mercury233.me>
2022-10-15 21:56:35 +0800

    add PHHY by Dr.Chaos

---
+++ scripts/PHHY-JP/c101111029.lua
@@ -0,0 +1,61 @@
+--流星连打-白黑机人
+function c101111029.initial_effect(c)
+	--special summon
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(101111029,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_QUICK_O)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCountLimit(1,101111029)
+	e1:SetCondition(c101111029.spcon)
+	e1:SetTarget(c101111029.sptg)
+	e1:SetOperation(c101111029.spop)
+	c:RegisterEffect(e1)
+	if not c101111029.global_check then
+		c101111029.global_check=true
+		local ge1=Effect.CreateEffect(c)
+		ge1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		ge1:SetCode(EVENT_ATTACK_ANNOUNCE)
+		ge1:SetOperation(c101111029.checkop)
+		Duel.RegisterEffect(ge1,0)
+		local ge2=Effect.CreateEffect(c)
+		ge2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		ge2:SetCode(EVENT_ATTACK_ANNOUNCE)
+		ge2:SetOperation(c101111029.checkop)
+		Duel.RegisterEffect(ge2,1)
+	end  
+	--atkup
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCode(EFFECT_UPDATE_ATTACK)
+	e2:SetCondition(c101111029.atkcon)
+	e2:SetValue(c101111029.atkval)
+	c:RegisterEffect(e2)  
+end
+function c101111029.checkop(e,tp,eg,ep,ev,re,r,rp)
+   Duel.RegisterFlagEffect(tp,101111029,RESET_PHASE+PHASE_END,0,1)
+end
+function c101111029.spcon(e,tp,eg,ep,ev,re,r,rp)
+	local ph=Duel.GetCurrentPhase()
+	return Duel.GetFlagEffect(tp,101111029)>=5 and ph>=PHASE_BATTLE_START and ph<=PHASE_BATTLE
+end
+function c101111029.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>-1
+		and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
+end
+function c101111029.spop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not c:IsRelateToEffect(e) then return end
+	Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
+end
+function c101111029.atkcon(e)
+	local ph=Duel.GetCurrentPhase()
+	return (ph==PHASE_DAMAGE or ph==PHASE_DAMAGE_CAL) and (Duel.GetAttacker()==e:GetHandler() or Duel.GetAttackTarget()==e:GetHandler())
+end
+function c101111029.atkval(e,c,tp)
+	return Duel.GetFlagEffect(tp,101111029)*1000
+end
