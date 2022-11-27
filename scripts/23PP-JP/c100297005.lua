7048baabe5f5d946240186be2e31285afa336633
mercury233 <me@mercury233.me>
2022-11-27 08:08:42 +0800

    add 23PP YOS1 by 奥克斯

---
+++ scripts/23PP-JP/c100297005.lua
@@ -0,0 +1,94 @@
+--表里一体
+--Script by 奥克斯
+function c100297005.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCountLimit(1,100297005)
+	e1:SetCost(c100297005.cost)
+	e1:SetTarget(c100297005.target)
+	e1:SetOperation(c100297005.activate)
+	e1:SetLabel(0)
+	c:RegisterEffect(e1)
+	--to deck
+	local e2=Effect.CreateEffect(c)  
+	e2:SetCategory(CATEGORY_TODECK+CATEGORY_DRAW)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetRange(LOCATION_GRAVE)
+	e2:SetCountLimit(1,100297006)
+	e2:SetCost(aux.bfgcost)
+	e2:SetTarget(c100297005.tg)
+	e2:SetOperation(c100297005.op)
+	c:RegisterEffect(e2)  
+end
+
+function c100297005.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	e:SetLabel(100)
+	if chk==0 then return true end
+end
+function c100297005.costfilter(c,e,tp)
+	return c:IsAttribute(ATTRIBUTE_LIGHT+ATTRIBUTE_DARK) 
+		and c:GetOriginalLevel()>0 
+		and Duel.GetMZoneCount(tp,c)>0
+		and Duel.IsExistingMatchingCard(c100297005.spfilter,tp,LOCATION_HAND+LOCATION_EXTRA,0,1,nil,c,e,tp)
+end
+function c100297005.spfilter(c,tc,e,tp)
+	if c:GetOriginalAttribute()==tc:GetOriginalAttribute() then return end
+	local b1=c:IsLocation(LOCATION_HAND)
+	local b2=c:IsLocation(LOCATION_EXTRA) and Duel.GetLocationCountFromEx(tp,tp,tc,c)>0
+	return  c:IsAttribute(ATTRIBUTE_LIGHT+ATTRIBUTE_DARK) 
+		and c:GetOriginalRace()==tc:GetOriginalRace()
+		and c:GetOriginalLevel()==tc:GetOriginalLevel()
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false) and (b1 or b2)
+end
+function c100297005.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then
+		if e:GetLabel()~=100 then return false end
+		e:SetLabel(0)
+		return e:IsHasType(EFFECT_TYPE_ACTIVATE) and Duel.CheckReleaseGroup(tp,c100297005.costfilter,1,nil,e,tp)
+	end
+	e:SetLabel(0)
+	local g=Duel.SelectReleaseGroup(tp,c100297005.costfilter,1,1,nil,e,tp)
+	Duel.Release(g,REASON_COST)
+	Duel.SetTargetCard(g)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND+LOCATION_EXTRA)
+end
+function c100297005.activate(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	local c=e:GetHandler()
+	local tc=Duel.GetFirstTarget()
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,c100297005.spfilter,tp,LOCATION_HAND+LOCATION_EXTRA,0,1,1,nil,tc,e,tp)
+	if g:GetCount()>0 then
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
+
+function c100297005.tdfilter(c,e)
+	return c:IsCanBeEffectTarget(e) and c:IsAttribute(ATTRIBUTE_LIGHT+ATTRIBUTE_DARK) and c:IsAbleToDeck()
+end
+function c100297005.tg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	local g=Duel.GetMatchingGroup(c100297005.tdfilter,tp,LOCATION_GRAVE,0,nil,e)
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and c100297005.tdfilter(chkc) end
+	if chk==0 then return g:CheckSubGroup(aux.dabcheck,2,2) and Duel.IsPlayerCanDraw(tp,1) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
+	local sg=g:SelectSubGroup(tp,aux.dabcheck,false,2,2)
+	Duel.SetTargetCard(sg)
+	Duel.SetOperationInfo(0,CATEGORY_TODECK,sg,2,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
+end
+function c100297005.op(e,tp,eg,ep,ev,re,r,rp)
+	local tg=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS)
+	if not tg or tg:FilterCount(Card.IsRelateToEffect,nil,e)~=2 then return end
+	Duel.SendtoDeck(tg,nil,SEQ_DECKSHUFFLE,REASON_EFFECT)
+	local g=Duel.GetOperatedGroup()
+	if g:IsExists(Card.IsLocation,1,nil,LOCATION_DECK) then Duel.ShuffleDeck(tp) end
+	local ct=g:FilterCount(Card.IsLocation,nil,LOCATION_DECK+LOCATION_EXTRA)
+	if ct==2 then
+		Duel.BreakEffect()
+		Duel.Draw(tp,1,REASON_EFFECT)
+	end
+end
