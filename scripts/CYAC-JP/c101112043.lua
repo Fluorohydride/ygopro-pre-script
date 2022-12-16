79c57abc185c429a51720943ec012e15bb2647ec
Mercury233 <me@mercury233.me>
2022-12-16 15:09:34 +0800

    add CYAC by 奥克斯 (#1036)

---
+++ scripts/CYAC-JP/c101112043.lua
@@ -0,0 +1,93 @@
+--アナドゥム・プライムハート
+--Script by 奥克斯
+local s,id,o=GetID()
+function s.initial_effect(c)
+	aux.AddCodeList(c,56099748)
+	--synchro summon
+	aux.AddSynchroMixProcedure(c,aux.FilterBoolFunction(Card.IsAttribute,ATTRIBUTE_LIGHT),nil,nil,aux.Tuner(nil),1,99)
+	c:EnableReviveLimit()
+	--synchro summon success
+	local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e0:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e0:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e0:SetCondition(s.regcon)
+	e0:SetOperation(s.regop)
+	c:RegisterEffect(e0)
+	--special summon
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DAMAGE_CAL)
+	e1:SetCode(EVENT_LEAVE_FIELD)
+	e1:SetCondition(s.spcon)
+	e1:SetTarget(s.sptg)
+	e1:SetOperation(s.spop)
+	c:RegisterEffect(e1)
+end
+function s.regcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsSummonType(SUMMON_TYPE_SYNCHRO)
+end
+function s.regop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local g=c:GetMaterial()
+	if #g==0 then return end
+	local ct=g:FilterCount(Card.IsType,nil,TYPE_TUNER)
+	local check=0
+	if g:IsExists(s.filter,1,nil) then
+		check=1
+		c:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD,EFFECT_FLAG_CLIENT_HINT,1,0,aux.Stringid(id,0))
+	end
+	--multi attack
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_EXTRA_ATTACK)
+	e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+	e1:SetLabel(ct)
+	e1:SetValue(s.atkval)
+	c:RegisterEffect(e1)
+	--cannot target
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
+	e2:SetReset(RESET_EVENT+RESETS_STANDARD)
+	e2:SetLabel(check)
+	e2:SetCondition(s.ckcon)
+	e2:SetValue(aux.tgoval)
+	c:RegisterEffect(e2)
+end
+function s.filter(c)
+	return c:IsType(TYPE_TUNER) and c:IsSetCard(0x291)
+end
+function s.atkval(e,c)
+	return e:GetLabel()-1
+end
+function s.ckcon(e)
+	return e:GetLabel()>0
+end
+function s.spcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return c:IsPreviousPosition(POS_FACEUP) and c:IsSummonType(SUMMON_TYPE_SYNCHRO) and c:IsPreviousLocation(LOCATION_MZONE) and c:IsPreviousControler(tp) and c:GetReasonPlayer()==1-tp
+end
+function s.spfilter(c,e,tp)
+	local b1=c:IsCode(56099748)
+	local b2=c:IsAttack(1500) and c:IsDefense(2100)
+	return c:IsFaceupEx() and c:IsCanBeSpecialSummoned(e,0,tp,false,false) and (b1 or b2)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	local g=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,nil,e,tp)
+	if chk==0 then return ft>0 and #g>0 end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE+LOCATION_REMOVED)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	local g=Duel.GetMatchingGroup(aux.NecroValleyFilter(s.spfilter),tp,LOCATION_GRAVE+LOCATION_REMOVED,0,nil,e,tp)
+	if ft==0 or #g==0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local sg=g:Select(tp,1,1,nil)
+	if #sg==0 then return end
+	Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP)
+end
