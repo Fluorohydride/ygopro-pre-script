43a66b293d3b6c3fc10eebb35eae6375f322d603
mercury233 <me@mercury233.me>
2023-01-03 08:07:14 +0800

    add CYAC by 奥克斯

---
+++ scripts/CYAC-JP/c101112016.lua
@@ -0,0 +1,56 @@
+--ネムレリアの夢守り－オレイエ
+--Script by 奥克斯
+function c101112016.initial_effect(c)
+	--special summon
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_SPSUMMON_PROC)
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCountLimit(1,101112016+EFFECT_COUNT_CODE_OATH)
+	e1:SetCondition(c101112016.sprcon)
+	c:RegisterEffect(e1)
+	--atk up  
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(101112016,0))
+	e2:SetCategory(CATEGORY_ATKCHANGE)
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
+	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetHintTiming(0,TIMINGS_CHECK_MONSTER+TIMING_END_PHASE+TIMING_DAMAGE_STEP)
+	e2:SetCountLimit(1,101112016+100)
+	e2:SetCondition(c101112016.atkcon)
+	e2:SetCost(c101112016.atkcost)
+	e2:SetOperation(c101112016.atkop)
+	c:RegisterEffect(e2)  
+end
+function c101112016.sprcon(e,c)
+	if c==nil then return true end
+	local tp=c:GetControler()
+	return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.IsExistingMatchingCard(aux.AND(Card.IsFaceup,Card.IsType),tp,LOCATION_EXTRA,0,1,nil,TYPE_PENDULUM)
+end
+function c101112016.atkcon(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(aux.AND(Card.IsFaceup,Card.IsCode),tp,LOCATION_EXTRA,0,nil,101112015)
+	local ag=Duel.GetFieldGroup(tp,0,LOCATION_MZONE,nil)
+	return #g>0 and #ag>0 and aux.dscon(e,tp,eg,ep,ev,re,r,rp)
+end
+function c101112016.atkcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	local g=Duel.GetMatchingGroup(Card.IsFacedown,tp,LOCATION_EXTRA,0,nil):Filter(Card.IsAbleToRemoveAsCost,nil,POS_FACEDOWN)
+	if chk==0 then return #g>0 end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local rg=g:Select(tp,1,1,nil)
+	Duel.Remove(rg,POS_FACEDOWN,REASON_COST)
+end
+function c101112016.atkop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local ag=Duel.GetFieldGroup(tp,0,LOCATION_MZONE,nil)
+	if c:IsRelateToEffect(e) and #ag>0 then
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetValue(#ag*500)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_DISABLE+RESET_PHASE+PHASE_END)
+		c:RegisterEffect(e1)
+	end
+end
---
+++ scripts/CYAC-JP/c101112060.lua
@@ -0,0 +1,82 @@
+--寝姫の甘い夢
+--Script by 奥克斯
+function c101112060.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCountLimit(1,101112060+EFFECT_COUNT_CODE_OATH)
+	e1:SetTarget(c101112060.target)
+	e1:SetOperation(c101112060.activate)
+	c:RegisterEffect(e1)
+	--Effect 2  
+	local e02=Effect.CreateEffect(c)  
+	e02:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e02:SetType(EFFECT_TYPE_IGNITION)
+	e02:SetRange(LOCATION_GRAVE)
+	e02:SetCost(c101112060.mvcost)
+	e02:SetTarget(c101112060.mvtg)
+	e02:SetOperation(c101112060.mvop)
+	c:RegisterEffect(e02) 
+end
+function c101112060.filter(c)
+	return c:IsSetCard(0x292) and c:IsType(TYPE_MONSTER) and c:IsAbleToHand()
+end
+function c101112060.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c101112060.filter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function c101112060.activate(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,c101112060.filter,tp,LOCATION_DECK,0,1,1,nil)
+	if #g==0 then return end
+	Duel.SendtoHand(g,nil,REASON_EFFECT)
+	Duel.ConfirmCards(1-tp,g)
+	if g:GetFirst():IsLocation(LOCATION_HAND) and Duel.GetMatchingGroupCount(aux.AND(Card.IsFaceup,Card.IsCode),tp,LOCATION_EXTRA,0,nil,101112015)>0 then
+		Duel.Hint(HINT_CARD,0,101112015)
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		e1:SetCode(EVENT_SUMMON_SUCCESS)
+		e1:SetCondition(c101112060.sumcon)
+		e1:SetOperation(c101112060.sumsuc)
+		e1:SetReset(RESET_PHASE+PHASE_END)
+		Duel.RegisterEffect(e1,tp)
+		local e2=e1:Clone()
+		e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+		Duel.RegisterEffect(e2,tp)
+	end
+end
+function c101112060.sumfilter(c)
+	return c:IsFaceup() and c:IsSetCard(0x292)
+end
+function c101112060.sumcon(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(c101112060.sumfilter,1,nil)
+end
+function c101112060.sumsuc(e,tp,eg,ep,ev,re,r,rp)
+	Duel.SetChainLimitTillChainEnd(c101112060.efun)
+end
+function c101112060.efun(e,ep,tp)
+	return ep==tp
+end
+function c101112060.mvcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return c:IsAbleToDeckAsCost() end
+	Duel.DisableShuffleCheck()
+	Duel.SendtoDeck(c,nil,SEQ_DECKBOTTOM,REASON_COST)
+end
+function c101112060.mvfilter(c)
+	return c:IsFaceup() and c:IsCode(101112015) and c:IsType(TYPE_PENDULUM)
+end
+function c101112060.mvtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and c101112060.mvfilter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(c101112060.mvfilter,tp,LOCATION_MZONE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+	Duel.SelectTarget(tp,c101112060.mvfilter,tp,LOCATION_MZONE,0,1,1,nil)
+end
+function c101112060.mvop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) then
+		Duel.SendtoExtraP(tc,nil,REASON_EFFECT)
+	end
+end 
