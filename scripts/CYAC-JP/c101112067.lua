905d95ea19111cea3e6132fe100d151a3b5b8dda
mercury233 <me@mercury233.me>
2023-01-26 20:56:14 +0800

    fix 時を裂く魔瞳

--- scripts/CYAC-JP/c101112067.lua
+++ scripts/CYAC-JP/c101112067.lua
@@ -3,7 +3,6 @@
 function c101112067.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_GRAVE_ACTION)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetCondition(c101112067.condition)
