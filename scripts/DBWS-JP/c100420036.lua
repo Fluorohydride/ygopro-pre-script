d16d4a73aa9dec18baad9903acc36d880deb9d76
JoyJ <316351048@qq.com>
2023-03-17 14:54:16 +0800

    add c100420036

---
+++ scripts/DBWS-JP/c100420036.lua
@@ -0,0 +1,44 @@
+--这是菜单
+--scripted by JoyJ
+function c100420036.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_SEARCH+CATEGORY_TOHAND)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCountLimit(1,100420036+EFFECT_COUNT_CODE_OATH)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetTarget(c100420036.target)
+	e1:SetOperation(c100420036.activate)
+	c:RegisterEffect(e1)
+end
+function c100420036.filter(c)
+	return c:IsSetCard(0x293) and c:IsAbleToHand()
+end
+function c100420036.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c100420036.filter,tp,LOCATION_DECK,0,2,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function c100420036.thfilter(c,tc)
+	local code=0
+	if tc:IsRace(RACE_BEASTWARRIOR) then code=100420037 end
+	if tc:IsRace(RACE_WARRIOR) then code=100420038 end
+	return c:IsCode(code) and c:IsAbleToHand()
+end
+function c100420036.activate(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(c100420036.filter,tp,LOCATION_DECK,0,nil)
+	if g:GetClassCount(Card.GetCode)<2 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local sg=g:SelectSubGroup(tp,aux.dncheck,false,2,2)
+	Duel.Hint(HINT_SELECTMSG,1-tp,HINTMSG_ATOHAND)
+	local tc=sg:Select(1-tp,1,1,nil):GetFirst()
+	local attr=tc:GetAttribute()
+	if Duel.SendtoHand(tc,nil,REASON_EFFECT)>0 and tc:IsRace(RACE_BEASTWARRIOR+RACE_WARRIOR)
+		and Duel.IsExistingMatchingCard(aux.NecroValleyFilter(c100420036.thfilter),tp,LOCATION_DECK+LOCATION_GRAVE,0,1,nil,attr)
+		and Duel.SelectYesNo(tp,aux.Stringid(100420036,1)) then
+		Duel.BreakEffect()
+		g=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(c100420036.thfilter),tp,LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil,attr)
+		if #g>0 then
+			Duel.SendtoHand(g,nil,REASON_EFFECT)
+		end
+	end
+end
