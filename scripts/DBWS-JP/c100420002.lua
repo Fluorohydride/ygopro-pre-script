8ee1219b6b8fb38c4afc9c49b92cbe3a1093f641
Mercury233 <me@mercury233.me>
2023-03-25 11:17:03 +0800

    add DBWS by 奥克斯 (#1055)

---
+++ scripts/DBWS-JP/c100420002.lua
@@ -0,0 +1,114 @@
+--超越竜メテオロス
+--Script by 奥克斯
+function c100420002.initial_effect(c)
+	--splimit
+	local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_SINGLE)
+	e0:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e0:SetCode(EFFECT_SPSUMMON_CONDITION)
+	e0:SetValue(c100420002.splimit)
+	c:RegisterEffect(e0)
+	--destroy and special summon
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(100420002,0))
+	e1:SetCategory(CATEGORY_DESTROY+CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_QUICK_O)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetHintTiming(0,TIMINGS_CHECK_MONSTER+TIMING_END_PHASE)
+	e1:SetCountLimit(1,100420002)
+	e1:SetCondition(c100420002.dspcon)
+	e1:SetTarget(c100420002.dsptg)
+	e1:SetOperation(c100420002.dspop)
+	c:RegisterEffect(e1)
+	--tograve
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(100420002,1))
+	e2:SetCategory(CATEGORY_TOGRAVE)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e2:SetProperty(EFFECT_FLAG_DELAY)
+	e2:SetCountLimit(1,100420002+100)
+	e2:SetTarget(c100420002.tgtg)
+	e2:SetOperation(c100420002.tgop)
+	c:RegisterEffect(e2)
+	--special summon or self
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(100420002,2))
+	e3:SetCategory(CATEGORY_TODECK+CATEGORY_SPECIAL_SUMMON)
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e3:SetProperty(EFFECT_FLAG_DELAY)
+	e3:SetCode(EVENT_DESTROYED)
+	e3:SetCountLimit(1,100420002+200)
+	e3:SetTarget(c100420002.tdtg)
+	e3:SetOperation(c100420002.tdop)
+	c:RegisterEffect(e3)
+end
+function c100420002.splimit(e,se,sp,st)
+	return se:IsHasType(EFFECT_TYPE_ACTIONS)
+end
+function c100420002.dspcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetTurnPlayer()~=tp
+end
+function c100420002.desfilter(c)
+	return c:IsRace(RACE_DINOSAUR) and c:IsFaceupEx()
+end
+function c100420002.dsptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	local g=Duel.GetMatchingGroup(c100420002.desfilter,tp,LOCATION_HAND+LOCATION_MZONE,0,c)
+	if chk==0 then return g:CheckSubGroup(aux.mzctcheck,2,2,tp) and c:IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,2,0,0)
+end
+function c100420002.dspop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local g=Duel.GetMatchingGroup(c100420002.desfilter,tp,LOCATION_HAND+LOCATION_MZONE,0,c)
+	if #g==0 or not g:CheckSubGroup(aux.mzctcheck,2,2,tp) then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local dg=g:SelectSubGroup(tp,aux.mzctcheck,false,2,2,tp)
+	Duel.HintSelection(dg)
+	if Duel.Destroy(dg,REASON_EFFECT)==0 or not c:IsRelateToEffect(e) then return false end
+	Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
+end
+function c100420002.tgfilter(c)
+	return c:IsRace(RACE_DINOSAUR) and c:IsAbleToGrave()
+end
+function c100420002.tgtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c100420002.tgfilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_DECK)
+end
+function c100420002.tgop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local g=Duel.SelectMatchingCard(tp,c100420002.tgfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if g:GetCount()>0 then
+		Duel.SendtoGrave(g,REASON_EFFECT)
+	end
+end
+function c100420002.tdfilter(c)
+	return c:IsType(TYPE_NORMAL) and c:IsAbleToDeck()
+end
+function c100420002.tdtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c100420002.tdfilter,tp,LOCATION_GRAVE,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TODECK,nil,1,tp,LOCATION_GRAVE)
+	if e:GetActivateLocation()==LOCATION_GRAVE then
+		e:SetCategory(CATEGORY_TODECK+CATEGORY_SPECIAL_SUMMON+CATEGORY_GRAVE_SPSUMMON)
+	else
+		e:SetCategory(CATEGORY_TODECK+CATEGORY_SPECIAL_SUMMON)
+	end
+end
+function c100420002.tdop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
+	local g=Duel.SelectMatchingCard(tp,c100420002.tdfilter,tp,LOCATION_GRAVE,0,1,1,nil)
+	if #g>0 then
+		local c=e:GetHandler()
+		Duel.HintSelection(g)
+		if Duel.SendtoDeck(g,nil,SEQ_DECKSHUFFLE,REASON_EFFECT)>0
+			and g:FilterCount(Card.IsLocation,nil,LOCATION_DECK)>0
+			and c:IsRelateToEffect(e) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+			and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+			and Duel.SelectYesNo(tp,aux.Stringid(100420002,3)) then
+			Duel.BreakEffect()
+			Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
+		end
+	end
+end
