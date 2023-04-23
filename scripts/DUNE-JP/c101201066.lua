fc671ca82c7030b5bb8a82393c21355862097a46
Lyris <lyris89@live.com>
2023-04-23 06:58:15 -0400

    fix `Eternal Ice Grave`

--- scripts/DUNE-JP/c101201066.lua
+++ scripts/DUNE-JP/c101201066.lua
@@ -19,7 +19,7 @@ function s.filter(c,tp)
 end
 function s.condition(e,tp,eg,ep,ev,re,r,rp)
 	local ph=Duel.GetCurrentPhase()
-	return (ph==PHASE_MAIN1 or ph==PHASE_MAIN2) and eg:IsExists(s.filter,1,nil)
+	return (ph==PHASE_MAIN1 or ph==PHASE_MAIN2) and eg:IsExists(s.filter,1,nil,tp)
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
