530f2ac712647596eccde2da686fc24030a5419d
Lyris <lyris89@live.com>
2023-05-03 02:37:19 -0400

    add `Cornfield Koator`

---
+++ scripts/DUNE-JP/c101201005.lua
@@ -0,0 +1,87 @@
+--coded by Lyris
+--Cornfield Koator
+local s, id, o = GetID()
+function s.initial_effect(c)
+	aux.AddCodeList(c,101201052,63136489)
+	--indes
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+	e1:SetCondition(s.indcon)
+	e1:SetTarget(s.indtg)
+	c:RegisterEffect(e1)
+	--to hand
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_SEARCH+CATEGORY_TOHAND)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_HAND)
+	e2:SetCountLimit(1,id)
+	e2:SetCost(s.cost)
+	e2:SetTarget(s.thtg)
+	e2:SetOperation(s.thop)
+	c:RegisterEffect(e2)
+	--disable
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,1))
+	e3:SetCategory(CATEGORY_DISABLE+CATEGORY_DESTROY)
+	e3:SetType(EFFECT_TYPE_QUICK_O)
+	e3:SetCode(EVENT_CHAINING)
+	e3:SetRange(LOCATION_GRAVE+LOCATION_MZONE)
+	e3:SetCountLimit(1,id+o)
+	e3:SetCondition(s.discon)
+	e3:SetCost(aux.bfgcost)
+	e3:SetTarget(s.distg)
+	e3:SetOperation(s.disop)
+	c:RegisterEffect(e3)
+end
+function s.indcon(e)
+	local c=e:GetHandler()
+	return (Duel.GetAttacker()==c or Duel.GetAttackTarget()==c) and c:GetBattleTarget()~=nil
+end
+function s.indtg(e,c)
+	local tc=e:GetHandler()
+	return c==tc or c==tc:GetBattleTarget()
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then c:IsDiscardable() end
+	Duel.SendtoGrave(c,REASON_COST+REASON_DISCARD)
+end
+function s.filter(c)
+	return c:IsType(TYPE_MONSTER) and (aux.IsCodeListed(c,101201052) or aux.IsCodeListed(c,63136489))
+		and c:IsAbleToHand() and not c:IsCode(id)
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK,0,1,1,nil)
+	Duel.SendtoHand(g,nil,REASON_EFFECT)
+	Duel.ConfirmCards(1-tp,g)
+end
+function s.cfilter(c)
+	return c:IsFaceup() and c:IsCode(4796100)
+end
+function s.dfilter(c,tp)
+	return c:IsOnField() and c:IsControler(tp)
+end
+function s.discon(e,tp,eg,ep,ev,re,r,rp)
+	if rp==tp or e:GetHandler():IsStatus(STATUS_BATTLE_DESTROYED)
+		or not re:IsHasProperty(EFFECT_FLAG_CARD_TARGET) then return false end
+	local g=Duel.GetChainInfo(ev,CHAININFO_TARGET_CARDS)
+	return g and g:IsExists(s.dfilter,1,nil,tp) and Duel.IsChainDisablable(ev)
+end
+function s.distg(e,tp,eg,ep,ev,re,r,rp)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_DISABLE,eg,1,0,0)
+	local rc=re:GetHandler()
+	if rc:IsDestructable() and rc:IsRelateToEffect(re) then Duel.SetOperationInfo(0,CATEGORY_DESTROY,eg,1,0,0) end
+end
+function s.disop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.NegateEffect(ev) and re:GetHandler():IsRelateToEffect(re) then Duel.Destroy(eg,REASON_EFFECT) end
+end
