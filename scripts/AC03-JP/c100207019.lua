b5d329d8bfaf41952e5dacbd39473d3417bb9278
Lyris <lyris89@live.com>
2023-05-27 09:18:40 -0400

    fix Earthbound Prisoner Line Walker

--- scripts/AC03-JP/c100207019.lua
+++ scripts/AC03-JP/c100207019.lua
@@ -34,7 +34,7 @@ function s.filter(c)
 	return c:IsCode(100207024,100207025) and c:IsAbleToHand()
 end
 function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,nil)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,nil) end
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK+LOCATION_GRAVE)
 end
 function s.thop(e,tp,eg,ep,ev,re,r,rp)
