d26399ce70b13c4f6c334ae18638d1e160aaa2b7
wzh31891579 <31891579@qq.com>
2023-05-29 16:23:57 +0800

    fix c101202047.lua

--- scripts/AGOV-JP/c101202047.lua
+++ scripts/AGOV-JP/c101202047.lua
@@ -92,46 +92,73 @@ function c101202047.thop(e,tp,eg,ep,ev,re,r,rp)
 		Duel.ConfirmCards(1-tp,g)
 	end
 end
-function c101202047.check(e,tp,exc)
+function c101202047.pcon(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetFlagEffect(tp,101202047)<=0 then return false end	
 	local c=e:GetHandler()
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(101202047,3))
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetCode(EFFECT_EXTRA_PENDULUM_SUMMON)
 	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
 	e1:SetTargetRange(1,0)
 	e1:SetValue(aux.TRUE)
 	e1:SetReset(RESET_PHASE+PHASE_END)
-	Duel.RegisterEffect(e1,tp)
+	Duel.RegisterEffect(e1,tp)  
+	local eset={e1}
 	local lpz=Duel.GetFieldCard(tp,LOCATION_PZONE,0)
-	if lpz==nil then return false end
-	local pcon=aux.PendCondition()
-	local res=pcon(e,lpz)
+	local rpz=Duel.GetFieldCard(tp,LOCATION_PZONE,1)
+	if lpz==nil or rpz==nil then return false end
+	local lscale=c:GetLeftScale()
+	local rscale=rpz:GetRightScale()
+	if lscale>rscale then lscale,rscale=rscale,lscale end
+	local loc=0
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)>0 then loc=loc+LOCATION_HAND end
+	if Duel.GetLocationCountFromEx(tp,tp,nil,TYPE_PENDULUM)>0 then loc=loc+LOCATION_EXTRA end
+	if loc==0 then return false end
+	local g=Duel.GetFieldGroup(tp,loc,0)
+	local res=g:IsExists(aux.PConditionFilter,1,nil,e,tp,lscale,rscale,eset)
 	e1:Reset()
 	return res
-end
-function c101202047.pcon(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.GetFlagEffect(tp,101202047)>0 and c101202047.check(e,tp,nil)
-end
-function c101202047.pcost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return c101202047.check(e,tp,nil) end
 end
 function c101202047.pop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(101202047,3))
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetCode(EFFECT_EXTRA_PENDULUM_SUMMON)
 	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
 	e1:SetTargetRange(1,0)
 	e1:SetValue(aux.TRUE)
 	e1:SetReset(RESET_PHASE+PHASE_END)
-	Duel.RegisterEffect(e1,tp)
+	Duel.RegisterEffect(e1,tp)	
+	local eset={e1}
 	local lpz=Duel.GetFieldCard(tp,LOCATION_PZONE,0)
-	if lpz==nil then return end
+	local rpz=Duel.GetFieldCard(tp,LOCATION_PZONE,1)
+	local lscale=lpz:GetLeftScale()
+	local rscale=rpz:GetRightScale()
+	if lscale>rscale then lscale,rscale=rscale,lscale end
+	local loc=0
+	local ft1=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	local ft2=Duel.GetLocationCountFromEx(tp,tp,nil,TYPE_PENDULUM)
+	local ft=Duel.GetUsableMZoneCount(tp)
+	local ect=c29724053 and Duel.IsPlayerAffectedByEffect(tp,29724053) and c29724053[tp]
+	if ect and ect<ft2 then ft2=ect end
+	if Duel.IsPlayerAffectedByEffect(tp,59822133) then
+		if ft1>0 then ft1=1 end
+		if ft2>0 then ft2=1 end
+		ft=1
+	end
+	if ft1>0 then loc=loc|LOCATION_HAND end
+	if ft2>0 then loc=loc|LOCATION_EXTRA end
+	local tg=Duel.GetMatchingGroup(aux.PConditionFilter,tp,loc,0,nil,e,tp,lscale,rscale,eset)
+	tg=tg:Filter(aux.PConditionExtraFilterSpecific,nil,e,tp,lscale,rscale,e1)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	aux.GCheckAdditional=aux.PendOperationCheck(ft1,ft2,ft)
+	local g=tg:SelectSubGroup(tp,aux.TRUE,true,1,math.min(#tg,ft))
+	aux.GCheckAdditional=nil
+	if not g then return end
 	local sg=Group.CreateGroup()
-	local pop=aux.PendOperation()
-	pop(e,tp,eg,ep,ev,re,r,rp,lpz,sg)
+	sg:Merge(g)
+	Duel.HintSelection(Group.FromCards(lpz))
+	Duel.HintSelection(Group.FromCards(rpz))
 	Duel.SpecialSummon(sg,SUMMON_TYPE_PENDULUM,tp,tp,true,true,POS_FACEUP)
 	e1:Reset()
 end
