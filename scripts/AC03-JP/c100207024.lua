0614234a8d03e31c47bd976bf762bde440495a45
Lyris <lyris89@live.com>
2023-06-04 18:42:15 -0400

    fix c100207024.lua

--- scripts/AC03-JP/c100207024.lua
+++ scripts/AC03-JP/c100207024.lua
@@ -21,7 +21,6 @@ function s.initial_effect(c)
 	e2:SetRange(LOCATION_FZONE)
 	e2:SetTargetRange(LOCATION_HAND+LOCATION_MZONE,0)
 	e2:SetTarget(aux.TargetBoolFunction(Card.IsSetCard,0x21))
-	e2:SetValue(0x1)
 	c:RegisterEffect(e2)
 	--halve lp
 	local e3=Effect.CreateEffect(c)
