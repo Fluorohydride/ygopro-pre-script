c3939ba361278c1a43fd49627358d9b56957e989
wzh31891579 <31891579@qq.com>
2023-06-08 16:30:09 +0800

    add c100346032.lua

--- scripts/SD46-JP/c100346032.lua
+++ scripts/SD46-JP/c100346032.lua
@@ -16,23 +16,24 @@ end
 function c100346032.rmfilter(c)
 	return c:IsAttackAbove(2000) and c:IsFaceup() and c:IsAbleToRemove()
 end
+function c100346032.cfilter(c)
+	return (c:IsCode(70902743) or (aux.IsCodeListed(c,70902743) and c:IsType(TYPE_SYNCHRO) and c:IsLocation(LOCATION_MZONE)))
+		and c:IsFaceup()
+end
 function c100346032.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_MZONE) and c100346032.rmfilter(chkc) end
 	if chk==0 then return Duel.IsExistingTarget(c100346032.rmfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
 	local g=Duel.SelectTarget(tp,c100346032.rmfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
 	Duel.SetOperationInfo(0,CATEGORY_REMOVE,g,1,0,0)
-end
-function c100346032.cfilter(c)
-	return (c:IsCode(70902743) or (aux.IsCodeListed(c,70902743) and c:IsType(TYPE_SYNCHRO) and c:IsLocation(LOCATION_MZONE)))
-		and c:IsFaceup()
+	local cg=Duel.GetMatchingGroup(c100346032.cfilter,tp,LOCATION_ONFIELD,0,nil)
+	e:SetLabel(cg:GetCount())
 end
 function c100346032.stfilter(c)
 	return c:IsCode(50078509) and c:IsSSetable()
 end
 function c100346032.activate(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
-	local g1=Duel.GetMatchingGroup(c100346032.cfilter,tp,LOCATION_ONFIELD,0,nil)
 	if tc:IsRelateToEffect(e) and Duel.Remove(tc,0,REASON_EFFECT+REASON_TEMPORARY)~=0 then
 		tc:RegisterFlagEffect(100346032,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,0,2)
 		local e1=Effect.CreateEffect(e:GetHandler())
@@ -45,11 +46,11 @@ function c100346032.activate(e,tp,eg,ep,ev,re,r,rp)
 		e1:SetOperation(c100346032.retop)
 		e1:SetLabel(Duel.GetTurnCount())
 		Duel.RegisterEffect(e1,tp)
-		local g2=Duel.GetMatchingGroup(aux.NecroValleyFilter(c100346032.stfilter),tp,LOCATION_DECK+LOCATION_GRAVE,0,nil)
+		local g=Duel.GetMatchingGroup(aux.NecroValleyFilter(c100346032.stfilter),tp,LOCATION_DECK+LOCATION_GRAVE,0,nil)
 		local ft=Duel.GetLocationCount(tp,LOCATION_SZONE)
-		if g1:GetCount()>0 and g2:GetCount()>0 and ft>0 and Duel.SelectYesNo(tp,aux.Stringid(100346032,0)) then
+		if e:GetLabel()>0 and g:GetCount()>0 and ft>0 and Duel.SelectYesNo(tp,aux.Stringid(100346032,0)) then
 			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SET)
-			local sc=g2:Select(tp,1,1,nil)
+			local sc=g:Select(tp,1,1,nil)
 			if sc then
 				Duel.SSet(tp,sc)
 			end
