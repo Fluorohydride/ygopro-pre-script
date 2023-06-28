52ae397c60a9d219bb94e891b96c1c7b80370ccb
Lyris <lyris89@live.com>
2023-06-28 18:13:03 -0400

    fix redtext on line 77

--- scripts/AGOV-JP/c101201033.lua
+++ scripts/AGOV-JP/c101201033.lua
@@ -74,5 +74,5 @@ function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
-	if tc:IsRelateToEffect(e) then Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
+	if tc:IsRelateToEffect(e) then Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP) end
 end
