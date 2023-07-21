9c436343d975c7af45d7a832161e0b769a545b46
Lyris <lyris89@live.com>
2023-07-21 13:44:44 -0400

    cleanup

--- scripts/DUNE-EN/c101201090.lua
+++ scripts/DUNE-EN/c101201090.lua
@@ -2,6 +2,7 @@
 --Divine Domain Baatistina
 local s, id, o = GetID()
 function s.initial_effect(c)
+	--Activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
