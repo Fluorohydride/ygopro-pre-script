c6c64db37a63fe3f36b543dc21a41440798669ec
Lyris <lyris89@live.com>
2023-07-21 09:10:40 -0400

    add cards
    `Lament of the Immortal Bushi`
    `Oriori no Kamigami`

---
+++ scripts/AGOV-JP/c101202027.lua
@@ -0,0 +1,44 @@
+--coded by Lyris
+--Oriori no Kamigami
+local s,id,o=GetID()
+function s.initial_effect(c)
+	--toss coin
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_COIN+CATEGORY_DRAW)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetTarget(s.cointg)
+	e1:SetOperation(s.coinop)
+	c:RegisterEffect(e1)
+	--double atk
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_ATKCHANGE)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e2:SetCode(EVENT_TOSS_COIN)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetOperation(s.datkop)
+	c:RegisterEffect(e2)
+end
+function s.cointg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_COIN,nil,0,tp,1)
+end
+function s.coinop(e,tp,eg,ep,ev,re,r,rp)
+	local ct=0
+	while Duel.TossCoin(tp,1)>0 do ct=ct+1 end
+	if ct>1 then Duel.Draw(tp,ct//2,REASON_EFFECT) end
+end
+function s.datkop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) and c:IsFaceup() then for i,res in ipairs{Duel.GetCoinResult()} do if res==1 then
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_SET_ATTACK_FINAL)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_DISABLE)
+		e1:SetValue(c:GetAttack()*2)
+		c:RegisterEffect(e1)
+	end end end
+end
