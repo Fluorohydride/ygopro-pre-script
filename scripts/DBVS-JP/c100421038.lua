a3ac9b78881856621fcd5dea975bb7f98be20908
Lyris <lyris89@live.com>
2023-08-13 18:56:37 -0400

    add #25

--- scripts/DBVS-JP/c100421038.lua
+++ scripts/DBVS-JP/c100421038.lua
@@ -1,6 +1,7 @@
---coded by Lyris
+--ヴァルモニカ・イントナーレ
 --Valmonica Intonare
-local s, id, o = GetID()
+--coded by Lyris
+local s,id,o=GetID()
 function s.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
