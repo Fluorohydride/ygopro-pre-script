5b40b97e06e7753154c04c8fbcfe5f90d9afd95d
mercury233 <me@mercury233.me>
2023-08-14 11:43:05 +0800

    fix

--- scripts/DBVS-JP/c100421010.lua
+++ scripts/DBVS-JP/c100421010.lua
@@ -58,7 +58,7 @@ function s.spcon(e,tp,eg,ep,ev,re,r,rp)
 	return rp==1-tp and eg:IsExists(s.mfilter,1,nil)
 end
 function s.sfilter(c,e,tp)
-	return c:IsSetCard(0x2a1) and c:IsCanBeSpecialSummoned(e,0,tp,true,false)
+	return c:IsSetCard(0x2a1) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
