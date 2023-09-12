8b8e32918b9d53ce7d0114aa2e40eaabf2732733
Mercury233 <me@mercury233.me>
2023-09-12 15:13:34 +0800

    Update c101202060.lua

--- scripts/AGOV-JP/c101202060.lua
+++ scripts/AGOV-JP/c101202060.lua
@@ -41,7 +41,6 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
 	if tc:IsRelateToEffect(e) then Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP_DEFENSE) end
 	local e1=Effect.CreateEffect(e:GetHandler())
-	e1:SetDescription(aux.Stringid(id,1))
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
 	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
