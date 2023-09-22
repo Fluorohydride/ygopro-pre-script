69fde2419f660c36c310c6b1d48f0bd9f94ae5e8
mercury233 <me@mercury233.me>
2023-09-22 10:13:08 +0800

    fix

--- scripts/VJMP-JP/c100200239.lua
+++ scripts/VJMP-JP/c100200239.lua
@@ -1,7 +1,7 @@
 --ジャンク・メイル
 --Junk Mail
 --Scripted by: CVen00/ToonyBirb with some code provided by XGlitchy30
-local s,id=GetID()
+local s,id,o=GetID()
 function s.initial_effect(c)
 	--indestructable
 	local e1=Effect.CreateEffect(c)
@@ -16,7 +16,7 @@ function s.initial_effect(c)
 	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e2:SetType(EFFECT_TYPE_IGNITION)
 	e2:SetRange(LOCATION_GRAVE)
-	e2:SetCountLimit(1,id+1)
+	e2:SetCountLimit(1,id+o)
 	e2:SetCondition(aux.exccon)
 	e2:SetTarget(s.sptg)
 	e2:SetOperation(s.spop)
@@ -37,22 +37,24 @@ function s.indop(e,tp,eg,ep,ev,re,r,rp)
 	e1:SetReset(RESET_EVENT+RESETS_STANDARD)
 	rc:RegisterEffect(e1,true)
 end
+function s.spfilter(c)
+	return c:IsFaceup() and c:IsOriginalSetCard(0xa3,0x1017,0x66) and c:IsType(TYPE_SYNCHRO)
+end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_MZONE,0,1,nil) end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,0,0)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false)
+		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_MZONE,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)>0 then
-		local e2=Effect.CreateEffect(c)
-		e2:SetType(EFFECT_TYPE_SINGLE)
-		e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-		e2:SetCode(EFFECT_LEAVE_FIELD_REDIRECT)
-		e2:SetValue(LOCATION_REMOVED)
-		e2:SetReset(RESET_EVENT+RESETS_REDIRECT)
-		c:RegisterEffect(e2, true)
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e1:SetCode(EFFECT_LEAVE_FIELD_REDIRECT)
+		e1:SetValue(LOCATION_REMOVED)
+		e1:SetReset(RESET_EVENT+RESETS_REDIRECT)
+		c:RegisterEffect(e1,true)
 	end
-end
-function s.spfilter(c)
-	return c:IsFaceup() and (c:IsSetCard(0xa3) or c:IsSetCard(0x1017) or c:IsSetCard(0x66)) and c:IsType(TYPE_SYNCHRO)
 end
--- scripts/VJMP-JP/c100200240.lua
+++ scripts/VJMP-JP/c100200240.lua
@@ -1,28 +1,35 @@
 --魔法妖精 バーガンディ
 --Burgundy the Magical Elf
 --Scripted by: CVen00/ToonyBirb with template provided by XGlitchy30
-local s,id=GetID()
+local s,id,o=GetID()
 function s.initial_effect(c)
 	--dmg
 	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_DAMAGE)
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
 	e1:SetCode(EVENT_DISCARD)
 	e1:SetProperty(EFFECT_FLAG_DELAY)
 	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1,id)
 	e1:SetCondition(s.dmgcon)
 	e1:SetOperation(s.dmgop)
-	e1:SetCountLimit(1,id)
 	c:RegisterEffect(e1)
 	--handes
 	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
 	e2:SetCategory(CATEGORY_HANDES)
 	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e2:SetCode(EVENT_BATTLE_DESTROYED)
+	e2:SetCountLimit(1,id+o)
 	e2:SetCondition(s.handescon)
 	e2:SetTarget(s.handestg)
 	e2:SetOperation(s.handesop)
 	c:RegisterEffect(e2)
+end
+function s.dmgfilter(c,tp)
+	return c:IsPreviousLocation(LOCATION_HAND) and c:IsPreviousControler(tp)
+		and c:IsReason(REASON_EFFECT) and c:IsReason(REASON_DISCARD)
 end
 function s.dmgcon(e,tp,eg,ep,ev,re,r,rp)
 	return re and eg:IsExists(s.dmgfilter,1,nil,1-tp)
@@ -31,11 +38,8 @@ function s.dmgop(e,tp,eg,ep,ev,re,r,rp)
 	local ct=eg:FilterCount(s.dmgfilter,nil,1-tp)
 	Duel.Damage(1-tp,ct*400,REASON_EFFECT)
 end
-function s.dmgfilter(c,tp)
-	return c:IsPreviousLocation(LOCATION_HAND) and c:IsControler(tp) and c:IsReason(REASON_EFFECT) and c:IsReason(REASON_DISCARD)
-end
 function s.handescon(e,tp,eg,ep,ev,re,r,rp)
-	return e:GetHandler():IsReason(REASON_BATTLE)
+	return e:GetHandler():IsLocation(LOCATION_GRAVE)
 end
 function s.handestg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetFieldGroupCount(tp,0,LOCATION_HAND)>0 end
