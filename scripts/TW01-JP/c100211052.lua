94f9c65363179581951bf133e61f1e9f8a085f4f
Lyris <lyris89@live.com>
2023-10-15 18:34:55 -0400

    add `R-Genex Undine`

---
+++ scripts/TW01-JP/c100211052.lua
@@ -0,0 +1,96 @@
+--coded by Lyris
+--R-Genex Undine
+local s, id, o = GetID()
+function s.initial_effect(c)
+	--attribute/tuner
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetCost(s.atcost)
+	e1:SetTarget(s.attg)
+	e1:SetOperation(s.atop)
+	c:RegisterEffect(e1)
+	--to hand
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_TOHAND)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_GRAVE)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetCondition(s.thcon)
+	e2:SetTarget(s.thtg)
+	e2:SetOperation(s.thop)
+	c:RegisterEffect(e2)
+end
+function s.rfilter(c)
+	return c:IsSetCard(0x2) and c:IsType(TYPE_MONSTER) and c:IsAbleToRemoveAsCost()
+end
+function s.atcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.rfilter,tp,LOCATION_GRAVE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local tc=Duel.SelectMatchingCard(tp,s.rfilter,tp,LOCATION_GRAVE,0,1,1,nil):GetFirst()
+	e:SetLabel((tc:GetType()&TYPE_TUNER)//TYPE_TUNER)
+	e:SetLabelObject(tc)
+	Duel.Remove(tc,POS_FACEUP,REASON_COST)
+end
+function s.attg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:IsCostChecked() end
+end
+function s.atop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not c:IsRelateToEffect(e) or c:IsFacedown() then return end
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_ADD_ATTRIBUTE)
+	e1:SetProperty(EFFECT_FLAG_COPY_INHERIT)
+	e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_DISABLE)
+	e1:SetValue(e:GetLabelObject():GetAttribute())
+	c:RegisterEffect(e1)
+	if e:GetLabel()>0 then
+		Duel.BreakEffect()
+		local e2=Effect.CreateEffect(c)
+		e2:SetType(EFFECT_TYPE_SINGLE)
+		e2:SetCode(EFFECT_TUNER)
+		e2:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		e2:SetValue(s.tnval)
+		c:RegisterEffect(e2)
+	end
+end
+function s.tnval(e,c)
+	return e:GetHandler():IsControler(c:GetControler())
+end
+function s.cfilter(c)
+	return c:IsFaceup() and c:IsSetCard(0x2) and c:IsType(TYPE_SYNCHRO)
+end
+function s.thcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_MZONE,0,1,nil)
+end
+function s.filter(c)
+	return c:IsSetCard(0x2) and c:IsType(TYPE_MONSTER) and c:IsAbleToHand()
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return false end
+	local c=e:GetHandler()
+	if chk==0 then return c:IsCanBeEffectTarget(e) and s.filter(c) and Duel.IsExistingTarget(s.filter,tp,LOCATION_GRAVE,0,1,c) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectTarget(tp,s.filter,tp,LOCATION_GRAVE,0,1,1,c)
+	Duel.SetTargetCard(c)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g+c,2,0,0)
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetTargetsRelateToChain()
+	Duel.SendtoHand(g,nil,REASON_EFFECT)
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_TO_GRAVE_REDIRECT)
+	e1:SetProperty(EFFECT_FLAG_SET_AVAILABLE+EFFECT_FLAG_IGNORE_RANGE+EFFECT_FLAG_IGNORE_IMMUNE+EFFECT_FLAG_CANNOT_DISABLE)
+	e1:SetTargetRange(0xff,0xfe)
+	e1:SetTarget(s.rmtg)
+	e1:SetReset(RESET_PHASE+PHASE_END)
+	Duel.RegisterEffect(e1,tp)
+end
+function s.rmtg(e,c)
+	return c:GetOwner()==e:GetHandlerPlayer()
+end
