810ef49b7456d979efba1b05c7626d2e414bddcd
1798857101 <1798857101@qq.com>
2023-10-21 20:25:07 +0800

    PHNI 080 鉄騎の雷鎚
    新卡 铁骑的雷锤

---
+++ scripts/PHNI-JP/c101203080.lua
@@ -0,0 +1,37 @@
+--鉄騎の雷鎚
+local s,id,o=GetID()
+function s.initial_effect(c)
+    --Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_NEGATE+CATEGORY_DESTROY)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_CHAINING)
+	e1:SetCondition(s.condition)
+	e1:SetCost(s.cost)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.PayLPCost(tp,math.floor(Duel.GetLP(tp)/2))
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return (re:IsActiveType(TYPE_MONSTER) or re:IsHasType(EFFECT_TYPE_ACTIVATE)) and Duel.IsChainNegatable(ev)
+		and re:GetHandler():IsLocation(LOCATION_ONFIELD)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
+	if re:GetHandler():IsDestructable() and re:GetHandler():IsRelateToEffect(re) then
+		Duel.SetOperationInfo(0,CATEGORY_DESTROY,eg,1,0,0)
+	end
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local rg=re:GetHandler():GetColumnGroup()
+	if Duel.NegateActivation(ev) and re:GetHandler():IsRelateToEffect(re)
+    and Duel.Destroy(eg,REASON_EFFECT)~=0 and rg:GetCount()>0 then
+		Duel.BreakEffect()
+		Duel.Destroy(rg,REASON_EFFECT)
+	end
+end
