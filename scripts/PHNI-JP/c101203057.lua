56a0c0a434d68a3906856b8d02ae553e77a071b6
zengsxing <127161407+zengsxing@users.noreply.github.com>
2023-10-26 21:59:48 +0800

    fix *7
    为什么会这样...

--- scripts/PHNI-JP/c101203057.lua
+++ scripts/PHNI-JP/c101203057.lua
@@ -34,11 +34,11 @@ end
 function s.thfilter(c)
 	return c:IsSetCard(0xba) and c:IsType(TYPE_SPELL+TYPE_TRAP) and not c:IsCode(id) and c:IsAbleToHand()
 end
-function s.cfilter(c)
+function s.cfilter(c,tp)
 	return c:IsSetCard(0xba) and c:IsControler(tp) and c:IsSummonLocation(LOCATION_EXTRA)
 end
 function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return eg:IsExists(s.cfilter,1,nil) and Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
+	if chk==0 then return eg:IsExists(s.cfilter,1,nil,tp) and Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
 end
 function s.thop(e,tp,eg,ep,ev,re,r,rp)
