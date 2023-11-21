25cc83f120740bf86c8fdfd3c8e91969731ef3bd
想象力很好的毛虫 <71340108+Crescent0v0@users.noreply.github.com>
2023-11-21 18:57:42 +0800

    change listed type

--- scripts/old cards/004/c58132856.lua
+++ scripts/old cards/004/c58132856.lua
@@ -1,6 +1,5 @@
 --トイ・マジシャン
 function c58132856.initial_effect(c)
-	aux.AddCodeList(c,58132856)
 	--Set
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
@@ -34,6 +33,7 @@ function c58132856.initial_effect(c)
 	e4:SetOperation(c58132856.desop)
 	c:RegisterEffect(e4)
 end
+s.set_as_spell=true
 function c58132856.regop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if c:IsPreviousLocation(LOCATION_SZONE) and c:IsPreviousPosition(POS_FACEDOWN)
