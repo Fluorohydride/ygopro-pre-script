f27f1d7dbc37e7bf836505de4d43e7d6797393bc
想象力很好的毛虫 <71340108+Crescent0v0@users.noreply.github.com>
2023-11-22 21:41:21 +0800

    delete wrong spsummon limit

--- scripts/PHNI-JP/c101203024.lua
+++ scripts/PHNI-JP/c101203024.lua
@@ -3,12 +3,6 @@ local s,id,o=GetID()
 function s.initial_effect(c)
 	c:SetSPSummonOnce(27134209)
 	c:EnableReviveLimit()
-	--splimit
-	local e0=Effect.CreateEffect(c)
-	e0:SetType(EFFECT_TYPE_SINGLE)
-	e0:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
-	e0:SetCode(EFFECT_SPSUMMON_CONDITION)
-	c:RegisterEffect(e0)
 	--special summon
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
