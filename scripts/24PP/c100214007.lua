5514e430ad0a135724d6b452e449a32446d23e61
想象力很好的毛虫 <71340108+Crescent0v0@users.noreply.github.com>
2023-11-24 13:22:47 +0800

    Fix Battle_Destroy-act-effect①, wrong-act-effect③

--- scripts/24PP/c100214007.lua
+++ scripts/24PP/c100214007.lua
@@ -13,7 +13,7 @@ function s.initial_effect(c)
 	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
 	e2:SetRange(LOCATION_SZONE)
 	e2:SetCode(EVENT_DESTROYED)
-	e2:SetProperty(EFFECT_FLAG_DELAY)
+	e2:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_DAMAGE_STEP)
 	e2:SetCountLimit(1,id)
 	e2:SetCondition(s.reccon)
 	e2:SetTarget(s.rectg)
@@ -48,7 +48,7 @@ function s.initial_effect(c)
 end
 function s.cfilter(c,tp)
 	return c:IsReason(REASON_BATTLE+REASON_EFFECT) and c:IsPreviousControler(tp)
-    and c:IsPreviousLocation(LOCATION_MZONE) and c:IsPreviousPosition(POS_FACEUP)
+	and c:IsPreviousLocation(LOCATION_MZONE) and c:IsPreviousPosition(POS_FACEUP)
 end
 function s.reccon(e,tp,eg,ep,ev,re,r,rp)
   local dg=eg:Filter(s.cfilter,nil,tp)
@@ -68,7 +68,7 @@ function s.recop(e,tp,eg,ep,ev,re,r,rp)
 end
 function s.aufilter(c,tp,e)
 	return c:IsFaceup() and c:IsPreviousControler(tp) and c:IsPreviousLocation(LOCATION_GRAVE)
-    and c:IsLocation(LOCATION_MZONE) and (not e or c:IsCanBeEffectTarget(e))
+	and c:IsLocation(LOCATION_MZONE) and (not e or c:IsCanBeEffectTarget(e))
 end
 function s.autg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	local g=eg:Filter(s.aufilter,nil,tp,e)
@@ -76,7 +76,7 @@ function s.autg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chk==0 then return #g>0 end
 	local tg=g:Clone()
 	if #g>1 then
-    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
 		tg=g:Select(tp,1,1,nil)
 	end
 	Duel.SetTargetCard(tg)
@@ -95,13 +95,13 @@ function s.auop(e,tp,eg,ep,ev,re,r,rp)
 end
 function s.thfilter(c,tp)
 	return c:IsType(TYPE_SPELL+TYPE_TRAP) and c:IsPreviousControler(tp)
-    and c:IsPreviousLocation(LOCATION_HAND) and c:IsReason(REASON_DISCARD)
+	and c:IsPreviousLocation(LOCATION_HAND) and c:IsReason(REASON_DISCARD)
 end
 function s.thcon(e,tp,eg,ep,ev,re,r,rp)
   return eg:FilterCount(s.thfilter,nil,tp)>0
 end
 function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingTarget(s.thfilter,tp,LOCATION_GRAVE,0,1,nil) end
+	if chk==0 then return Duel.IsExistingTarget(s.thfilter,tp,LOCATION_GRAVE,0,1,nil,tp) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
   local g=Duel.SelectTarget(tp,s.thfilter,tp,LOCATION_GRAVE,0,1,1,nil,tp)
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,0,0)
