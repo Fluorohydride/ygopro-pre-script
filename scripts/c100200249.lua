b7f297bcfdb18e2a3f15a52286d679aac6a4feed
FluerLee <144464260+FluerLee@users.noreply.github.com>
2023-12-11 11:45:21 +0800

    Update scripts/c100200249.lua
    Co-authored-by: 八宫一月 <135235980+kiritosoft@users.noreply.github.com>

--- scripts/c100200249.lua
+++ scripts/c100200249.lua
@@ -29,7 +29,6 @@ function s.lmop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
 	local c=e:GetHandler()
 	if tc and tc:IsRelateToEffect(e) then 
-			tc:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,0,1)
 		end
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
