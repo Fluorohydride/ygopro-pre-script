5fb0a96d36c8b801b21f3b24f66213eaa16114a4
1798857101 <1798857101@qq.com>
2023-12-13 20:08:10 +0800

    Update c101203064.lua

--- scripts/PHNI-JP/c101203064.lua
+++ scripts/PHNI-JP/c101203064.lua
@@ -1,17 +1,17 @@
 --地縛融合
 local s,id,o=GetID()
 function s.initial_effect(c)
-    --Activate
+	--Activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_FUSION_SUMMON)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
-    e1:SetCondition(s.condition)
+	e1:SetCondition(s.condition)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
-    --search
+	--search
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,1))
 	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
@@ -19,24 +19,24 @@ function s.initial_effect(c)
 	e2:SetRange(LOCATION_GRAVE)
 	e2:SetCountLimit(1,id)
 	e2:SetCost(aux.bfgcost)
-    e2:SetCondition(s.condition1)
+	e2:SetCondition(s.condition1)
 	e2:SetTarget(s.sptg)
 	e2:SetOperation(s.spop)
 	c:RegisterEffect(e2)
-	
+
 end
 --to spsummon 0x21
 function s.condition1(e,tp,eg,ep,ev,re,r,rp)
-    local ph=Duel.GetCurrentPhase()
+	local ph=Duel.GetCurrentPhase()
 	return Duel.GetTurnPlayer()==tp and (ph==PHASE_MAIN1 or ph==PHASE_MAIN2)
-        and Duel.IsExistingMatchingCard(nil,tp,LOCATION_FZONE,LOCATION_FZONE,1,nil)
+		and Duel.IsExistingMatchingCard(nil,tp,LOCATION_FZONE,LOCATION_FZONE,1,nil)
 end
 function s.spfilter(c,e,tp)
 	return c:IsSetCard(0x21) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-        and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_HAND+LOCATION_GRAVE,0,1,nil,e,tp)end
+		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_HAND+LOCATION_GRAVE,0,1,nil,e,tp)end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
@@ -44,7 +44,7 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 		local tc=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.spfilter),tp,LOCATION_HAND+LOCATION_GRAVE,0,1,1,nil,e,tp):GetFirst()
 		if tc then
-           Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
+		   Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
 		end
 	end
 	local e1=Effect.CreateEffect(e:GetHandler())
@@ -61,9 +61,9 @@ function s.splimit(e,c)
 end
 --fusion
 function s.condition(e,tp,eg,ep,ev,re,r,rp)
-    local ph=Duel.GetCurrentPhase()
+	local ph=Duel.GetCurrentPhase()
 	return Duel.IsExistingMatchingCard(nil,tp,LOCATION_FZONE,LOCATION_FZONE,1,nil)
-     or ph==PHASE_MAIN1 or ph==PHASE_MAIN2
+	 or ph==PHASE_MAIN1 or ph==PHASE_MAIN2
 end
 function s.filter1(c,e)
 	return not c:IsImmuneToEffect(e)
