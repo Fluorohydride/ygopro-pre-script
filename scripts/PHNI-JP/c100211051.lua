a48398b67e65908a823509d240e0bad30364dba6
八宫一月 <135235980+kiritosoft@users.noreply.github.com>
2023-12-13 20:12:02 +0800

    Update scripts/PHNI-JP/c100211051.lua

--- scripts/PHNI-JP/c100211051.lua
+++ scripts/PHNI-JP/c100211051.lua
@@ -19,7 +19,7 @@ function s.initial_effect(c)
 	--summon
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,1))
-	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetCategory(CATEGORY_SUMMON)
 	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
 	e2:SetRange(LOCATION_MZONE)
 	e2:SetCode(EVENT_TO_HAND)
