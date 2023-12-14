ecf426f8ee5ce35a51d00fa21c2d914c4cb3ca28
pokeboyexn <pokeboyexn@gmail.com>
2023-12-13 22:32:05 -0800

    Update c101203013.lua

--- scripts/PHNI-JP/c101203013.lua
+++ scripts/PHNI-JP/c101203013.lua
@@ -2,7 +2,7 @@
 -- By 八宫一月
 local s,id,o=GetID()
 function s.initial_effect(c)
-	aux.AddCodeList(c,25955164,62340868,98434877)
+	aux.AddCodeList(c,25955164,62340868,98434877,101203063)
 	c:EnableReviveLimit()
 	--Cannot special summon
 	local e1=Effect.CreateEffect(c)
