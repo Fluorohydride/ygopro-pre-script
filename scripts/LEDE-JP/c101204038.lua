55145afc616433d34769f8358f0b2a797ef86d8f
zengsxing <127161407+zengsxing@users.noreply.github.com>
2023-12-18 17:40:14 +0800

    fix 38

--- scripts/LEDE-JP/c101204038.lua
+++ scripts/LEDE-JP/c101204038.lua
@@ -124,8 +124,9 @@ function s.thop(e,tp,eg,ep,ev,re,r,rp)
 	local g2=Duel.GetMatchingGroup(s.thfilter2,tp,LOCATION_REMOVED,0,nil)
 	if #g1>0 and #g2>0 then
 		Duel.SendtoHand(Group.__add(g1,g2):SelectSubGroup(tp,s.fselect2,false,2,2),nil,REASON_EFFECT)
-		local tg=Duel.GetOperatedGroup()		
+		local tg=Duel.GetOperatedGroup()
 		if tg:FilterCount(s.spfilter,nil,e,tp)==2 and Duel.GetLocationCount(tp,LOCATION_MZONE)>=2 and Duel.SelectYesNo(tp,aux.Stringid(id,3)) then
+			Duel.BreakEffect()
 			for tc in aux.Next(tg) do
 				Duel.SpecialSummonStep(tc,0,tp,tp,true,false,POS_FACEUP)
 			end
