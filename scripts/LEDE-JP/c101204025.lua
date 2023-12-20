05ac0b3b0f6cdcda0e37f1d602b645db7c5afec7
zengsxing <127161407+zengsxing@users.noreply.github.com>
2023-12-20 21:10:19 +0800

    fix

--- scripts/LEDE-JP/c101204025.lua
+++ scripts/LEDE-JP/c101204025.lua
@@ -57,7 +57,7 @@ function s.spfilter(c,e,tp)
 	return c:IsCode(32274490,36021814) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and s.thfilter(chkc) end
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and s.spfilter(chkc) end
 	if chk==0 then return Duel.IsExistingTarget(s.spfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 	local g=Duel.SelectTarget(tp,s.spfilter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
