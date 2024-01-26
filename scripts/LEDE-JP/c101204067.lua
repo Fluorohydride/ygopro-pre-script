ac5c667a18265ab781551d5a776769cd7cac95d6
八宫一月 <135235980+kiritosoft@users.noreply.github.com>
2024-01-27 07:42:04 +0800

    fix bug2

--- scripts/LEDE-JP/c101204067.lua
+++ scripts/LEDE-JP/c101204067.lua
@@ -34,7 +34,7 @@ end
 function s.drtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local ct=Duel.GetFieldGroupCount(tp,0,LOCATION_ONFIELD)
 	if chk==0 then
-		if Duel.GetFieldGroupCount(tp,LOCATION_DECK,0)<=0 then return false end
+		if Duel.GetFieldGroupCount(tp,LOCATION_DECK,0)==0 then return false end
 		local g=Duel.GetDecktopGroup(tp,ct)
 		local result=g:FilterCount(Card.IsAbleToHand,nil)>0
 		return result
@@ -43,11 +43,17 @@ function s.drtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,0,LOCATION_DECK)
 end
 function s.drop(e,tp,eg,ep,ev,re,r,rp)
-	local ct=Duel.GetFieldGroupCount(tp,0,LOCATION_ONFIELD)
 	local p=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER)
-	Duel.ConfirmDecktop(p,ct)
-	local g=Duel.GetDecktopGroup(p,ct)
-	if g:GetCount()>0 then
+	local ct=Duel.GetFieldGroupCount(p,0,LOCATION_ONFIELD)
+	local dt=Duel.GetFieldGroupCount(p,LOCATION_DECK,0)
+	ct=math.min(ct,dt)
+	if ct==0 then return end
+	local t={}
+	for i=1,ct do t[i]=i end
+	local ac=Duel.AnnounceNumber(tp,table.unpack(t))
+	Duel.ConfirmDecktop(p,ac)
+	local g=Duel.GetDecktopGroup(p,ac)
+	if #g>0 then
 		Duel.Hint(HINT_SELECTMSG,p,HINTMSG_ATOHAND)
 		local sg=g:Select(p,1,1,nil)
 		if sg:GetFirst():IsAbleToHand() then
@@ -63,24 +69,24 @@ function s.drop(e,tp,eg,ep,ev,re,r,rp)
 			Duel.RegisterFlagEffect(p,id,RESET_PHASE+PHASE_END,0,1)
 			e:GetHandler():RegisterFlagEffect(id,RESET_PHASE+PHASE_END,0,1)
 		end
-		Duel.BreakEffect()
-		local hg=Duel.GetMatchingGroup(Card.IsAbleToDeck,p,LOCATION_HAND,0,nil)
-		if #hg>0 then
-			Duel.Hint(HINT_SELECTMSG,p,HINTMSG_TODECK)
-			local sg=hg:Select(p,1,1,nil)
-			if sg:GetFirst():GetOwner()==p then
-				Duel.SendtoDeck(sg,nil,SEQ_DECKTOP,REASON_EFFECT)
-			else
-				Duel.SendtoDeck(sg,nil,SEQ_DECKBOTTOM,REASON_EFFECT)
-				ct=ct-1
-			end
+	end
+	Duel.BreakEffect()
+	local hg=Duel.GetMatchingGroup(Card.IsAbleToDeck,p,LOCATION_HAND,0,nil)
+	if #hg>0 then
+		Duel.Hint(HINT_SELECTMSG,p,HINTMSG_TODECK)
+		local sg=hg:Select(p,1,1,nil)
+		if sg:GetFirst():GetOwner()==p then
+			Duel.SendtoDeck(sg,nil,SEQ_DECKTOP,REASON_EFFECT)
+		else
+			Duel.SendtoDeck(sg,nil,SEQ_DECKBOTTOM,REASON_EFFECT)
+			ac=ac-1
 		end
-		if ct>0 then
-			Duel.SortDecktop(tp,tp,ct)
-			for i=1,ct do
-				local mg=Duel.GetDecktopGroup(tp,1)
-				Duel.MoveSequence(mg:GetFirst(),SEQ_DECKBOTTOM)
-			end
+	end
+	if ac>0 then
+		Duel.SortDecktop(tp,tp,ac)
+		for i=1,ac do
+			local mg=Duel.GetDecktopGroup(tp,1)
+			Duel.MoveSequence(mg:GetFirst(),SEQ_DECKBOTTOM)
 		end
 	end
 end
